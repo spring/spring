be703503ec0599d0d32888d18c9d02f4299c41d7
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-08-04 09:42:36 +0000

    Minor dotfileHandler touchup
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@122 37977431-3df6-0310-b722-df95706aa16b

--- rts/dotfileHandler.cpp
+++ rts/dotfileHandler.cpp
@@ -9,12 +9,12 @@
 
 dotfileHandler regHandler(DOTCONFIGPATH);
 
-dotfileHandler::dotfileHandler(string filename)
+dotfileHandler::dotfileHandler(const string filename)
 {
 	std::ifstream reader(filename.c_str());
 	if (reader.good()) {
-		string read;
 		int idx;
+		string read;
 		while (getline(reader,read))
 			if ((idx = read.find_first_of("=")) > 0)
 				data[read.substr(0,idx)] = read.substr(idx+1);
@@ -23,7 +23,7 @@ dotfileHandler::dotfileHandler(string filename)
 		MessageBox(0,"Could not read from config file","dotfileHandler",MB_ICONEXCLAMATION);
 	file.open(filename.c_str());
 	if (!file)
-		MessageBox(0,"Could not write to config file","dotfileHandler",MB_ICONEXCLAMATION);
+		MessageBox(0,"Could not write to config file","dotfileHandler",0);
 }
 
 dotfileHandler::~dotfileHandler()
@@ -32,13 +32,13 @@ dotfileHandler::~dotfileHandler()
 	file.close();
 }
 
-unsigned int dotfileHandler::GetInt(string name, unsigned int def)
+unsigned int dotfileHandler::GetInt(const string name, const unsigned int def)
 {
 	std::map<string,string>::iterator pos = data.find(name);
 	return ( pos == data.end() ? def : atoi(data[name].c_str()) );
 }
 
-string dotfileHandler::GetString(string name, string def)
+string dotfileHandler::GetString(const string name, const string def)
 {
 	std::map<string,string>::iterator pos = data.find(name);
 	return ( pos == data.end() ? def : data[name] );
@@ -59,13 +59,13 @@ void dotfileHandler::flushfile(void)
 	file.flush();
 }
 
-void dotfileHandler::SetString(string name, string value)
+void dotfileHandler::SetString(const string name, const string value)
 {
 	data[name] = value;
 	flushfile();
 }
 
-void dotfileHandler::SetInt(string name, unsigned int value)
+void dotfileHandler::SetInt(const string name, const unsigned int value)
 {
 	std::stringstream ss;
 	ss << value;
