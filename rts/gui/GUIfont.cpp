6b40d7d28b89703ba136a67a466eeac25a304e4a
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-08-04 16:17:54 +0000

    Fixed deprecated FreeType code
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@123 37977431-3df6-0310-b722-df95706aa16b

--- rts/gui/GUIfont.cpp
+++ rts/gui/GUIfont.cpp
@@ -3,12 +3,8 @@
 
 
 // FreeType Headers
-#ifdef _HAVE_FT2BUILD_H
 #include <ft2build.h>
-#endif
-#include <freetype/freetype.h>
-#include <freetype/ftglyph.h>
-#include <freetype/ftoutln.h>
+#include FT_FREETYPE_H
 
 #include "myGL.h"
 
@@ -26,9 +22,9 @@ GUIfont::GUIfont(const std::string& fontFilename,int fontsize)
 	int tex_x=0,tex_y=0;
 	int tex[TEXSIZE*TEXSIZE];
 
-#ifndef NO_FONT
 	FT_Face face;
 	FT_Library library;
+	FT_GlyphSlot slot = face->glyph;
 
 	memset(tex,0,TEXSIZE*TEXSIZE*4);
 	charHeight=0;
@@ -45,40 +41,32 @@ GUIfont::GUIfont(const std::string& fontFilename,int fontsize)
 
 	for (unsigned int c=0; c<256; c++) 
 	{
-		FT_Load_Glyph(face, FT_Get_Char_Index(face, c), FT_LOAD_DEFAULT);
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+			throw std::runtime_error("FT_Load_Char failed");
 
-		// load glyph
-		FT_Glyph glyph;
-		if(FT_Get_Glyph(face->glyph, &glyph))
-			throw std::runtime_error("FT_Get_Glyph failed");
-
-		// render glyph to bitmap
-		FT_Glyph_To_Bitmap(&glyph, ft_render_mode_normal, NULL, 1);
-		FT_BitmapGlyph glyph_bitmap = (FT_BitmapGlyph)glyph;
-
-		int width  = glyph_bitmap->bitmap.width;
-		int height = glyph_bitmap->bitmap.rows;
+		int width  = slot->bitmap.width;
+		int height = slot->bitmap.rows;
 		if (height>charHeight) charHeight=height;
 
 		if (tex_x+width>=256) 
 		{
-			tex_y += charHeight+1;
+			tex_y += (int)charHeight+1;
 			tex_x=0;
 		}
 
 		// render glyph bitmap to texture
-		for (int y=0; y<glyph_bitmap->bitmap.rows; y++) 
+		for (int y=0; y<slot->bitmap.rows; y++) 
 		{
-			for (int x=0; x<glyph_bitmap->bitmap.width; x++) 
+			for (int x=0; x<slot->bitmap.width; x++) 
 			{
-				char shade=glyph_bitmap->bitmap.buffer[x+y*glyph_bitmap->bitmap.pitch];
+				char shade=slot->bitmap.buffer[x+y*slot->bitmap.pitch];
 				int color=(shade << 24) | 0x00ffffff;
 				tex[(tex_x+x)+(tex_y+y)*TEXSIZE] = color;
 		
 			}
 		}
 
-		charWidth[c]=glyph->advance.x / (float)0x10000;
+		charWidth[c]=slot->advance.x / (float)0x10000;
 
 		// set up display list
 		
@@ -89,10 +77,10 @@ GUIfont::GUIfont(const std::string& fontFilename,int fontsize)
 		float ty2=(tex_y+height)/(float)TEXSIZE;
 
 		// vertex coords
-		float vx1=glyph_bitmap->left;
-		float vy1=-glyph_bitmap->top;
-		float vx2=glyph_bitmap->left+width;
-		float vy2=height-glyph_bitmap->top;
+		float vx1=slot->bitmap_left;
+		float vy1=-slot->bitmap_top;
+		float vx2=slot->bitmap_left+width;
+		float vy2=height-slot->bitmap_top;
 
 		glNewList(displaylist+c,GL_COMPILE);
 
@@ -117,9 +105,8 @@ GUIfont::GUIfont(const std::string& fontFilename,int fontsize)
 		tex_x += width+1;
 
 		// discard glyph
-		FT_Done_Glyph(glyph);
+		//FT_Done_Glyph(glyph);
 	}
-#endif //NO_FONT
 	// create texture
 	glGenTextures(1, &texture);
 
@@ -128,9 +115,7 @@ GUIfont::GUIfont(const std::string& fontFilename,int fontsize)
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, 4, TEXSIZE, TEXSIZE, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex);
 
-#ifndef NO_FONT
 	FT_Done_FreeType(library);
-#endif 
 }
 
 
