718982f5fce56cd6f5bab423766eeaabd72c0bbb
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-08-23 00:05:11 +0000

    Make find_files glob recursive
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@193 37977431-3df6-0310-b722-df95706aa16b

--- rts/include/linux/filefunctions.h
+++ rts/include/linux/filefunctions.h
@@ -10,8 +10,10 @@
 #include <ctype.h>
 #include <iostream>
 #include <windows.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
-static inline std::vector<std::string> find_files(std::string pattern, std::string patternpath)
+static inline std::vector<std::string> find_files(std::string pattern, std::string patternpath, const bool recurse = true)
 {
 	std::string fullpattern(patternpath+pattern);
 	std::string regpattern;
@@ -40,6 +42,43 @@ static inline std::vector<std::string> find_files(std::string pattern, std::stri
 	}
 	globfree(pglob);
         free(pglob);
+	if (recurse) {
+		struct dirent **namelist;
+		int n = scandir(patternpath.c_str(),&namelist,0,alphasort);
+		if (n<0) {
+#ifdef DEBUG
+			MessageBox(0,"Could not read dir " + patternpath,"find_files scandir",0);
+#endif
+		} else {
+			while (n--) {
+				namelist[n]->d_name;
+				if (strcmp(namelist[n]->d_name,".")&&strcmp(namelist[n]->d_name,"..")) {
+					struct stat *st = (struct stat*)malloc(sizeof(struct stat));
+					stat((patternpath+std::string(namelist[n]->d_name)).c_str(),st);
+					if (S_ISDIR(st->st_mode)) {
+						std::vector<std::string> subret = find_files(pattern,(patternpath+std::string(namelist[n]->d_name)+std::string("/")).c_str(),true);
+						if (!subret.empty()) {
+							for (std::vector<std::string>::iterator it=subret.begin(); it != subret.end(); it++) {
+								found.push_back(*it);
+							}
+						}
+					}
+					free(st);
+				}
+				free(namelist[n]);
+			}
+			free(namelist);
+		}
+	}
+#ifdef DEBUG
+	for (std::vector<std::string>::iterator it = found.begin(); it != found.end(); it++) {
+		if (recurse) {
+			printf("Recursive glob %s in %s returned: %s\n",pattern.c_str(),patternpath.c_str(),it->c_str());
+		} else {
+			printf("Glob %s in %s returned: %s\n",pattern.c_str(),patternpath.c_str(),it->c_str());
+		}
+	}
+#endif
 	return found;
 }
 
