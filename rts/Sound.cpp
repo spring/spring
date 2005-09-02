e593c56d673632c07546e6aa3253a81b45195aab
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-09-02 04:16:15 +0000

    Start implementing sound using OpenAL
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@266 37977431-3df6-0310-b722-df95706aa16b

--- rts/Sound.cpp
+++ rts/Sound.cpp
@@ -5,450 +5,118 @@
 #include "StdAfx.h"
 #include "Sound.h"
 
-#ifndef NO_SOUND
-#ifdef USE_DSOUND
- 	#include "WavRead.h"
-#else
-	#ifndef _WINSOCKAPI_
-		#define _WINSOCKAPI_
-		#include <windows.h>
-		#undef _WINSOCKAPI_
-	#else
-		#include <windows.h>
-	#endif
-#endif
-
 #include <math.h>
 #include "InfoConsole.h"
 #include "RegHandler.h"
 #include "Camera.h"
 #include "WorldObject.h"
 #include "InfoConsole.h"
+#include "FileHandler.h"
 //#include "mmgr.h"
-
-extern HWND	hWnd;			// Holds Our Window Handle
-
-#define SAFE_DELETE(p)  { if(p) { delete (p);     (p)=NULL; } }
-#define SAFE_RELEASE(p) { if(p) { (p)->Release(); (p)=NULL; } }
-
-#endif //NO_SOUND
-
 
 CSound* sound;
 
-//////////////////////////////////////////////////////////////////////
-// Construction/Destruction
-//////////////////////////////////////////////////////////////////////
-/*void PlaySound(int num,bool loop,bool reset,CWorldObject* p)
-{
-	return;
-};
-*/
 CSound::CSound()
 {
-#ifndef NO_SOUND
-//	noSound=true;
-	maxSounds=regHandler.GetInt("MaxSounds",16);
-	curThreshhold=0.1;
-	wantedSounds=maxSounds*0.75;
-#ifdef USE_DSOUND
-	m_pDS            = NULL;
-	m_pWaveSoundRead = NULL;
-
-	Sint32             hr;
-	LPDIRECTSOUNDBUFFER pDSBPrimary = NULL;
-	
-	// Initialize COM
-	hr = CoInitialize( NULL );
-	if( hr!=S_OK && hr!=S_FALSE){
-		info->AddLine("Couldnt initialize com");
-		noSound=true;
-		return;
-	}
-	
-	// Create IDirectSound using the primary sound device
-	if( FAILED( hr = DirectSoundCreate( NULL, &m_pDS, NULL ) ) ){
-		info->AddLine("Couldnt create direct sound object");		
-		noSound=true;
-		return;
-	}
-
-    // Set coop level to DSSCL_PRIORITY
-	if( FAILED( hr = m_pDS->SetCooperativeLevel( hWnd, DSSCL_PRIORITY ) ) ){
-		info->AddLine("Couldnt set cooperative level");
-		noSound=true;
-		return;
-	}
-	
-	// Get the primary buffer 
-	DSBUFFERDESC dsbd;
-	ZeroMemory( &dsbd, sizeof(DSBUFFERDESC) );
-	dsbd.dwSize        = sizeof(DSBUFFERDESC);
-	dsbd.dwFlags       = DSBCAPS_PRIMARYBUFFER;
-	dsbd.dwBufferBytes = 0;
-	dsbd.lpwfxFormat   = NULL;
-	
-	if( FAILED( hr = m_pDS->CreateSoundBuffer( &dsbd, &pDSBPrimary, NULL ) ) ){
-		info->AddLine("Couldnt create primary sound buffer");
-		noSound=true;
-		return;
-	}
-	
-	// Set primary buffer format to 22kHz and 16-bit output.
-	WAVEFORMATEX wfx;
-	ZeroMemory( &wfx, sizeof(WAVEFORMATEX) ); 
-	wfx.wFormatTag      = WAVE_FORMAT_PCM; 
-	wfx.nChannels       = 2; 
-	wfx.nSamplesPerSec  = 22050; 
-	wfx.wBitsPerSample  = 16; 
-	wfx.nBlockAlign     = wfx.wBitsPerSample / 8 * wfx.nChannels;
-	wfx.nAvgBytesPerSec = wfx.nSamplesPerSec * wfx.nBlockAlign;
-	
-	if( FAILED( hr = pDSBPrimary->SetFormat(&wfx) ) ){
-		info->AddLine("Couldnt initialize primary sound format");
-		noSound=true;
-		return;
-	}
-	
-	SAFE_RELEASE( pDSBPrimary );
-	noSound=false;
-	waveid[""]=0;
-	SoundInfo* si=new SoundInfo;
-	loadedSounds.push_back(si);
-#endif
-#endif //NO_SOUND
+	noSound = false;
+	alutInit(NULL,0);
+	alGetError();
 }
 
 CSound::~CSound()
 {
-#ifdef USE_DSOUND
-	SAFE_DELETE( m_pWaveSoundRead );
-
-    // Release DirectSound interfaces
-	while(!buffers.empty()){
-		SAFE_RELEASE( buffers.back());
-		buffers.pop_back();
-	}
-  SAFE_RELEASE( m_pDS ); 
-
-	for(vector<SoundInfo*>::iterator si=loadedSounds.begin();si!=loadedSounds.end();++si)
-		delete *si;
-
-	// Release COM
-	CoUninitialize();
-#endif
+	LoadedFiles.clear();
+	for (std::vector<ALuint>::iterator it = Buffers.begin(); it != Buffers.end(); ++it)
+		alDeleteBuffers(1,&(*it));
+	Buffers.clear();
+	if (noSound)
+		return;
+	alutExit();
 }
 
-#ifdef USE_DSOUND
-int CSound::InitFile(const string& name)
-{
-	if((m_pDS==0) || noSound)
-		return -1;
-
-	// Create the sound buffer object from the wave file data
-	if( FAILED( CreateStaticBuffer(name.c_str() ) ) )
-	{        
-		MessageBox(0,"Couldnt create sound buffer","Sound error",0);
-		noSound=true;
-		return -1;
-	}
-	else // The sound buffer was successfully created
-	{
-		// Fill the buffer with wav data
-		FillBuffer();
-	}
-
-	waveid[name]=loadedSounds.size();
-	buf2id.push_back(loadedSounds.size());
-	SoundInfo* si=new SoundInfo;
-	si->firstBuf=buffers.size()-1;
-	si->freebufs.push_back(buffers.size()-1);
-	loadedSounds.push_back(si);
-
-	return buffers.size()-1;
-}
-#endif //NO_SOUND
-
-int CSound::GetWaveId(const string &name)
-{
-#ifndef NO_SOUND
-	PUSH_CODE_MODE;
-	ENTER_MIXED;
-	if(noSound){
-		POP_CODE_MODE;
-		return 0;
-	}
-	map<string,int>::iterator si;
-	if((si=waveid.find(name))==waveid.end()){
-		InitFile(name);
-		si=waveid.find(name);
-	}
-	int ret=si->second;
-	POP_CODE_MODE;
-	return ret;
-#endif //NO_SOUND
-}
-
-#ifndef NO_SOUND
-int CSound::GetBuf(int id,float volume)
-{
-	SoundInfo* s=loadedSounds[id];
-	int num;
-	if(s->freebufs.empty()){
-		LPDIRECTSOUNDBUFFER b=NULL;
-		buffers.push_back(b);
-		Sint32 r=m_pDS->DuplicateSoundBuffer(buffers[s->firstBuf],&(buffers.back()));
-		if(r!=DS_OK){
-			MessageBox(0,"Couldnt duplicate sound buffer","Sound error",0);
-			noSound=true;
-			return -1;
-		}
-		buf2id.push_back(id);
-		s->freebufs.push_back(buffers.size()-1);
-	}
-	num=s->freebufs.back();
-	s->freebufs.pop_back();
-	PlayingSound ps;
-	ps.num=num;
-	ps.volume=volume;
-	playingSounds.push_back(ps);
-
-	return num;
-}
-#endif
-
-#ifdef USE_DSOUND
 void CSound::PlaySound(int id, float volume)
 {
-	PUSH_CODE_MODE;
-	ENTER_MIXED;
-	if(noSound || id<=0 || id>=loadedSounds.size() || playingSounds.size()>=maxSounds){
-		POP_CODE_MODE;
+	if (noSound || !camera)
 		return;
-	}
-
-	float v=0.2-volume*0.2;
-		
-	Sint32 hr;
-	int num=GetBuf(id,v);
-
-	// Restore the buffers if they are lost
-	if( FAILED( hr = RestoreBuffers(num) ) ){
-		POP_CODE_MODE;
-		return;
-	}
-	
-	buffers[num]->SetVolume(DSBVOLUME_MIN*v-100);
-	buffers[num]->SetPan(0);
-//	if(reset)
-//		buffers[num]->SetCurrentPosition( 0L );    
-	
-	// Play buffer 
-//	Uint32 dwLooped = loop ? DSBPLAY_LOOPING : 0L;
-	if( FAILED( hr = buffers[num]->Play( 0, 0, 0/*dwLooped*/ ) ) ){
-
-	}
-	POP_CODE_MODE;
+	PlaySound(id,camera->pos,volume);
 }
 
 void CSound::PlaySound(int id,CWorldObject* p,float volume)
 {
+	if (noSound)
+		return;
 	PlaySound(id,p->pos,volume);
 }
 
 void CSound::PlaySound(int id,const float3& p,float volume)
 {
-	PUSH_CODE_MODE;
-	ENTER_MIXED;
-	if(noSound || id<=0 || id>=loadedSounds.size()){
-		POP_CODE_MODE;
+	if (noSound)
 		return;
-	}
-
-	Sint32 hr;
-	float3 dif=p - camera->pos;
-	float dl=dif.Length();
-	float pan=dif.dot(camera->right)*DSBPAN_RIGHT/dl;
-	float v=0;
-	if(volume!=0)
-		v=dl/(volume*1000);
-	if(v>0.6){
-		POP_CODE_MODE;
-		return;
-	}
-
-//	info->AddLine("%i %i %f",maxSounds,playingSounds.size(),v);
-
-	if(v>curThreshhold+(wantedSounds-playingSounds.size())/wantedSounds){
-		POP_CODE_MODE;
-		return;
-	}
-
-	int num=GetBuf(id,v);
-
-	// Restore the buffers if they are lost
-	if( FAILED( hr = RestoreBuffers(num) ) )
-		return;
-	
-	buffers[num]->SetVolume(DSBVOLUME_MIN*v-100);
-	buffers[num]->SetPan(pan);
-
-	if( FAILED( hr = buffers[num]->Play( 0, 0, 0/*dwLooped*/ ) ) ){
-
-	}
-	POP_CODE_MODE;
+	ALuint source;
+	ALfloat SourcePos[] = {p.x,p.y,p.z};
+	ALfloat SourceVel[] = {0.0,0.0,0.0};
+	alGenSources(1,&source);
+	alSourcei(source, AL_BUFFER, id);
+	alSourcef(source, AL_PITCH, 1.0 );
+	alSourcef(source, AL_GAIN, volume );
+	alSourcefv(source, AL_POSITION, SourcePos);
+	alSourcefv(source, AL_VELOCITY, SourceVel);
+	alSourcei(source, AL_LOOPING, false);
+	alSourcePlay(source);
+	alDeleteSources(1,&source);
 }
-#else
-void CSound::PlaySound(int id,float volume){};
-void CSound::PlaySound(int id,CWorldObject* p,float volume){};
-void CSound::PlaySound(int id,const float3& p,float volume){};
-#endif
-
-#ifdef USE_DSOUND
-Sint32 CSound::CreateStaticBuffer(const string& name)
-{
-	string filename("sounds/"+name);
-	Sint32 hr; 
-	
-	// Free any previous globals 
-	SAFE_DELETE( m_pWaveSoundRead );
-	
-	LPDIRECTSOUNDBUFFER b=NULL;
-	buffers.push_back(b);
-	buffers.back()=0;
-	
-	// Create a new wave file class
-	m_pWaveSoundRead = new CWaveSoundRead();
-	
-	// Load the wave file
-	if( FAILED( m_pWaveSoundRead->Open( filename.c_str() ) ) )
-	{
-		MessageBox(0,"Bad wave file.",filename.c_str(),0);
-	}
-	
-	// Set up the direct sound buffer, and only request the flags needed
-	// since each requires some overhead and limits if the buffer can 
-	// be hardware accelerated
-	DSBUFFERDESC dsbd;
-	ZeroMemory( &dsbd, sizeof(DSBUFFERDESC) );
-	dsbd.dwSize        = sizeof(DSBUFFERDESC);
-	dsbd.dwFlags       = DSBCAPS_STATIC | DSBCAPS_CTRLPAN | DSBCAPS_CTRLVOLUME;
-	dsbd.dwBufferBytes = m_pWaveSoundRead->m_ckIn.cksize;
-	dsbd.lpwfxFormat   = m_pWaveSoundRead->m_pwfx;
-	
-	// Create the static DirectSound buffer 
-	if( FAILED( hr = m_pDS->CreateSoundBuffer( &dsbd, &buffers.back(), NULL ) ) )
-		return hr;
-	
-	// Remember how big the buffer is
-	m_dwBufferBytes = dsbd.dwBufferBytes;
-	
-	return S_OK;
-	
-	return 0;
-}
-
-Sint32 CSound::FillBuffer()
-{
-	Sint32 hr; 
-	Uint8*   pbWavData; // Pointer to actual wav data 
-	UINT    cbWavSize; // Size of data
-	void*   pbData  = NULL;
-	void*   pbData2 = NULL;
-	Uint32   dwLength;
-	Uint32   dwLength2;
-
-	// The size of wave data is in pWaveFileSound->m_ckIn
-	INT nWaveFileSize = m_pWaveSoundRead->m_ckIn.cksize;
-
-	// Allocate that buffer.
-	pbWavData = new Uint8[ nWaveFileSize ];
-	if( NULL == pbWavData )
-		return E_OUTOFMEMORY;
-
-	if( FAILED( hr = m_pWaveSoundRead->Read( nWaveFileSize, 
-		pbWavData, 
-		&cbWavSize ) ) )           
-		return hr;
-
-	// Reset the file to the beginning 
-	m_pWaveSoundRead->Reset();
-
-	// Lock the buffer down
-	if( FAILED( hr = buffers.back()->Lock( 0, m_dwBufferBytes, &pbData, &dwLength, 
-		&pbData2, &dwLength2, 0L ) ) )
-		return hr;
-
-	// Copy the memory to it.
-	memcpy( pbData, pbWavData, m_dwBufferBytes );
-
-	// Unlock the buffer, we don't need it anymore.
-	buffers.back()->Unlock( pbData, m_dwBufferBytes, NULL, 0 );
-	pbData = NULL;
-
-	// We dont need the wav file data buffer anymore, so delete it 
-	delete[] pbWavData ;
-
-	return S_OK;
-
-	return 0;
-}
-
-Sint32 CSound::RestoreBuffers(int num)
-{
-	
-    Sint32 hr;
-
-    if( NULL == buffers[num] )
-        return S_OK;
-
-    Uint32 dwStatus;
-    if( FAILED( hr = buffers[num]->GetStatus( &dwStatus ) ) )
-        return hr;
-
-    if( dwStatus & DSBSTATUS_BUFFERLOST )
-    {
-        // Since the app could have just been activated, then
-        // DirectSound may not be giving us control yet, so 
-        // the restoring the buffer may fail.  
-        // If it does, sleep until DirectSound gives us control.
-        do 
-        {
-            hr = buffers[num]->Restore();
-            if( hr == DSERR_BUFFERLOST )
-                SleepEx(10, true);
-        }
-        while( hr = buffers[num]->Restore() );
-
-        if( FAILED( hr = FillBuffer() ) )
-            return hr;
-    }
-
-    return S_OK;
-
-}
-#endif
 
 void CSound::Update()
 {
-#ifndef NO_SOUND
-	float total=wantedSounds*0.5;
-	for(std::list<PlayingSound>::iterator pi=playingSounds.begin();pi!=playingSounds.end();){
-		int num=pi->num;
-		Uint32 status;
-		buffers[num]->GetStatus(&status);
-		if(!status & DSBSTATUS_PLAYING){
-			pi=playingSounds.erase(pi);
-			loadedSounds[buf2id[num]]->freebufs.push_back(num);
-		} else {
-			total-=(0.5-pi->volume);
-			++pi;
-		}
-	}
-	total/=wantedSounds;
-	curThreshhold=(curThreshhold+total)*0.5;
-//	info->AddLine("curt %.2f",curThreshhold);
-#endif //NO_SOUND
+	if (noSound)
+		return;
+	UpdateListener();
 }
 
+void CSound::UpdateListener()
+{
+	if (noSound || !camera)
+		return;
+	ALfloat ListenerPos[] = {camera->pos.x,camera->pos.y,camera->pos.z};
+	ALfloat ListenerVel[] = {0.0,0.0,0.0};
+	ALfloat ListenerOri[] = {camera->pos.x+camera->forward.x,camera->pos.y+camera->forward.y,camera->pos.z+camera->forward.z,camera->up.x,camera->up.y,camera->up.z};
+	alListenerfv(AL_POSITION,ListenerPos);
+	alListenerfv(AL_VELOCITY,ListenerVel);
+	alListenerfv(AL_ORIENTATION,ListenerOri);
+}
 
+ALuint CSound::LoadALBuffer(string path)
+{
+	if (noSound)
+		return 0;
+	Uint8 *buf;
+	ALuint buffer;
+	alGenBuffers(1,&buffer);
+	CFileHandler file("Sounds/"+path);
+	if(file.FileExists()){
+		buf = new Uint8[file.FileSize()];
+		file.Read(buf, file.FileSize());
+	} else {
+		MessageBox(0,"Couldnt open wav file",path,0);
+		return 0;
+	}
+	alBufferData(buffer,AL_FORMAT_STEREO16,buf,file.FileSize(),22050);
+	delete[] buf;
+	return buffer;
+}
+
+ALuint CSound::GetWaveId(string path)
+{
+	if (noSound)
+		return 0;
+	int count = 0;
+	ALuint buffer;
+	for (vector<string>::iterator it = LoadedFiles.begin(); it != LoadedFiles.end(); ++it,count++) {
+		if (*it == path)
+			return Buffers[count];
+	}
+	buffer = LoadALBuffer(path);
+	Buffers.push_back(buffer);
+	LoadedFiles.push_back(path);
+	return buffer;
+}
--- rts/Sound.h
+++ rts/Sound.h
@@ -1,95 +1,32 @@
 #ifndef SOUND_H
 #define SOUND_H
-// Sound.h: interface for the CSound class.
-//
-//////////////////////////////////////////////////////////////////////
 
-#if !defined(AFX_SOUND_H__D6141CC1_8294_11D4_AD55_0080ADA84DE3__INCLUDED_)
-#define AFX_SOUND_H__D6141CC1_8294_11D4_AD55_0080ADA84DE3__INCLUDED_
-
-#ifndef NO_SOUND
-
-#pragma warning(disable:4786)
-
-#define USE_DSOUND
-
-#if _MSC_VER > 1000
-/*pragma once removed*/
-#endif // _MSC_VER > 1000
-
-#ifdef USE_DSOUND
-	#ifndef _WINSOCKAPI_
-		#define _WINSOCKAPI_
-		#include <dsound.h>
-		#undef _WINSOCKAPI_
-	#else
-		#include <dsound.h>
-	#endif
-#endif
-
-
-#endif //NO_SOUND
-
-
-#include <string>
 #include <vector>
-#include <map>
-#include <deque>
-
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
 
 class CWorldObject;
-class CWaveSoundRead;
 
 using namespace std;
 class CSound  
 {
 public:
-	CSound();
-	virtual ~CSound();
+	ALuint LoadALBuffer(string path);
+	ALuint GetWaveId(string path);
+	void UpdateListener();
 	void Update();
-	int GetWaveId(const string& name);
-	void PlaySound(int id,float volume=1);
+	void PlaySound(int id, float volume=1);
 	void PlaySound(int id,CWorldObject* p,float volume=1);
 	void PlaySound(int id,const float3& p,float volume=1);
-
 	bool noSound;
-	int maxSounds;
-
-	float curThreshhold;
-	float wantedSounds;
-#ifdef USE_DSOUND
-protected:
-	int GetBuf(int id,float volume);
-	int InitFile(const string& name);
-
-	struct SoundInfo {
-		vector<int> freebufs;
-		int firstBuf;
-	};
-	struct PlayingSound {
-		int num;
-		float volume;
-	};
-
-	map<string,int> waveid;
-	vector<SoundInfo*> loadedSounds;
-	list<PlayingSound> playingSounds;
-	vector<int> buf2id;
-
-	std::vector<LPDIRECTSOUNDBUFFER> buffers;
-
-	Sint32 RestoreBuffers(int num);
-	Sint32 CreateStaticBuffer(const string& name);
-	Sint32 FillBuffer();
-
-	LPDIRECTSOUND		m_pDS;
-	CWaveSoundRead*     m_pWaveSoundRead;
-	Uint32               m_dwBufferBytes;
-	LPDIRECTSOUNDNOTIFY g_pDSNotify;
-#endif
+	CSound();
+	virtual ~CSound();
+private:
+	vector<string> LoadedFiles;
+	vector<ALuint> Buffers;
 };
 
 extern CSound* sound;
-#endif // !defined(AFX_SOUND_H__D6141CC1_8294_11D4_AD55_0080ADA84DE3__INCLUDED_)
 
 #endif /* SOUND_H */
