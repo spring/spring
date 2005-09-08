04b5c3a667b3154e9f4f03ca012a024d59a91d51
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-09-08 02:08:58 +0000

    Remove stray winreg.h header
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@316 37977431-3df6-0310-b722-df95706aa16b

--- rts/include/linux/winreg.h
+++
@@ -1,6 +0,0 @@
-//FIXME to be removed when a way to store options for linux will be developed
-#ifndef _WINREG_H_
-#define _WINREG_H_
-typedef void* HKEY;
-#define HKEY_CURRENT_USER 0
-#endif //_WINREG_H_
