9535aca623429a9c3daa675a8d86354793c34491
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-09-09 08:18:49 +0000

    This texture should only be unbound when using FBO
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@341 37977431-3df6-0310-b722-df95706aa16b

--- rts/ShadowHandler.cpp
+++ rts/ShadowHandler.cpp
@@ -99,8 +99,8 @@ CShadowHandler::CShadowHandler(void)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glBindTexture(GL_TEXTURE_2D,0);
-
+	if (fbo)
+		glBindTexture(GL_TEXTURE_2D,0);
 }
 
 CShadowHandler::~CShadowHandler(void)
