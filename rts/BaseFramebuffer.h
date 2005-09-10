0bccfa31e41ec33d2cbac0b7a5e1f1af1bdced6e
xiphux <xiphux@37977431-3df6-0310-b722-df95706aa16b>
2005-09-10 02:30:22 +0000

    Make framebuffer selection system much more modular and flexible
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@345 37977431-3df6-0310-b722-df95706aa16b

--- rts/BaseFramebuffer.h
+++ rts/BaseFramebuffer.h
@@ -11,12 +11,34 @@
 #define FBTYPE_DEPTH 3
 #define FBTYPE_STENCIL 4
 
+#define BUFFER_AUTO 0
+#define BUFFER_EMUL 1
+#define BUFFER_GLXPBUFFER 2
+#define BUFFER_SGIXPBUFFER 3
+#define BUFFER_WGLPBUFFER 4
+#define BUFFER_FBO 5
+
+static unsigned int windows_preferred_buffers[4] = {
+	BUFFER_FBO,
+	BUFFER_WGLPBUFFER,
+	BUFFER_EMUL,
+	0
+};
+
+static unsigned int linux_preferred_buffers[5] = {
+	BUFFER_FBO,
+	BUFFER_SGIXPBUFFER,
+	BUFFER_GLXPBUFFER,
+	BUFFER_EMUL,
+	0
+};
+
 class BaseFramebuffer
 {
 public:
 	BaseFramebuffer(const unsigned int t, const unsigned int w, const unsigned int h);
 	virtual ~BaseFramebuffer();
-	static BaseFramebuffer *initialize(const unsigned int t, const unsigned int w, const unsigned int h);
+	static BaseFramebuffer *initialize(const unsigned int t, const unsigned int w, const unsigned int h, const unsigned int buffertype = BUFFER_AUTO);
 	virtual unsigned int getWidth();
 	virtual unsigned int getHeight();
 	virtual unsigned int getType();
@@ -34,6 +56,8 @@ protected:
 	unsigned int width;
 	unsigned int height;
 	unsigned int texture;
+private:
+	static BaseFramebuffer *resolvetype(const unsigned int t, const unsigned int w, const unsigned int h, const unsigned int buffertype);
 };
 
 #endif /* BASEFRAMEBUFFER_H */
