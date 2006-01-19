c86293462e2d4d0e11f1e1eb88ca4cff99390d57
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2006-01-19 16:48:16 +0000

    Add VSNPRINTF #defines to StdAfx.h (I hope MSVC has v* equivalent of snprintf_s etc.) Use VSNPRINTF in InfoLog.cpp.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@610 37977431-3df6-0310-b722-df95706aa16b

--- rts/System/StdAfx.h
+++ rts/System/StdAfx.h
@@ -15,12 +15,15 @@ Microsoft Visual C++ 7.0: MSC_VER = 1300
 	#if _MSC_VER > 1310 // >= Visual Studio 2005
 		#pragma warning(disable: 4996) // hide warnings about deprecated functions
 		#define SNPRINTF sprintf_s 
+		#define VSNPRINTF vsprintf_s
 	#else              // Visual Studio 2003
 		#define SNPRINTF _snprintf
+		#define SNPRINTF _vsnprintf
 	#endif
 #else
 	// assuming GCC 
 	#define SNPRINTF snprintf
+	#define VSNPRINTF vsnprintf
 #endif
 
 // This reduces compile-time with precompiled headers on msvc
