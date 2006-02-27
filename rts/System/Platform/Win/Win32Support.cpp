6f97d42e17eeee194d2b0faa29c4b7fc21abb3e8
fnordia <fnordia@37977431-3df6-0310-b722-df95706aa16b>
2006-02-27 20:41:37 +0000

    Added translation from spring error flags to win32 dialog flags, and removed some debug output
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@914 37977431-3df6-0310-b722-df95706aa16b

--- rts/System/Platform/Win/Win32Support.cpp
+++ rts/System/Platform/Win/Win32Support.cpp
@@ -10,5 +10,14 @@
 void ErrorMessageBox (const char *msg, const char *caption, unsigned int flags)
 {
 	SDL_Quit();
-	MessageBox (GetActiveWindow(), msg, caption, flags);
+
+	// Translate spring flags to corresponding win32 dialog flags
+	unsigned int winFlags = 0;		// MB_OK is default (0)
+	
+	if (flags & MBF_EXCL)
+		winFlags |= MB_ICONEXCLAMATION;
+	if (flags & MBF_INFO)
+		winFlags |= MB_ICONINFORMATION;
+
+	MessageBox (GetActiveWindow(), msg, caption, winFlags);
 }
