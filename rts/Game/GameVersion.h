8eeb633ab9762fbafa4649e637c109212a9d2abf
jcnossen <jcnossen@37977431-3df6-0310-b722-df95706aa16b>
2006-04-30 19:30:18 +0000

    new version: 0.71b1
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1236 37977431-3df6-0310-b722-df95706aa16b

--- rts/Game/GameVersion.h
+++ rts/Game/GameVersion.h
@@ -1,9 +1,8 @@
 #ifndef GAMEVERSION_H
 #define GAMEVERSION_H
 
-#define VERSION_STRING "0.70b3"
+#define VERSION_STRING "0.71b1"
 
-//#define MOD_FILE "xta_se_v066.sdz"
 #define MOD_FILE "xtape.sd7"
 
 #endif
