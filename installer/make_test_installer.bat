88de292fa1b394558f89d79f01f1347361400a50
jcnossen <jcnossen@37977431-3df6-0310-b722-df95706aa16b>
2006-06-11 15:57:38 +0000

    added automatic building of the vstudio7 projects to the installer bat files.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1455 37977431-3df6-0310-b722-df95706aa16b

--- installer/make_test_installer.bat
+++ installer/make_test_installer.bat
@@ -10,14 +10,8 @@ echo the correct .pdb can be identified!
 echo.
 pause > nul
 
-echo Creating bitmaps.sdz
-del /Q ..\base\bitmaps.sdz
-cd ..\game
-..\installer\pkzip -add -dir=current ..\installer\_temp.zip bitmaps\*
-cd ..\installer
-pkzip -add _temp.zip builddata\modinfo.tdf
-rename _temp.zip bitmaps.sdz
-move /Y bitmaps.sdz ..\game\base\spring
+call build_code
+call pack_bitmaps
 
 echo Creating test installer
 "C:\Program Files\NSIS\makensis.exe" /V3 /DTEST_BUILD taspring.nsi 
