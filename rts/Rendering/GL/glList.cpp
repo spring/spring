6eaed037d554212cb71a3164459d7d264eea445c
krysole <krysole@37977431-3df6-0310-b722-df95706aa16b>
2006-06-29 05:22:57 +0000

    * Fixed isalnum bug (causes assert in debug mode if int out of char range)
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1546 37977431-3df6-0310-b722-df95706aa16b

--- rts/Rendering/GL/glList.cpp
+++ rts/Rendering/GL/glList.cpp
@@ -152,6 +152,9 @@ void CglList::KeyPress(int k)
 	if (k == SDLK_BACKSPACE) {
 		query = query.substr(0, query.length() - 1);
 		Filter(true);
+	} else if ((k & 0xFF) != k) {
+		// This prevents isalnum from asserting on msvc debug crt
+		// We don't actually need to process the key tho;)
 	} else if (isalnum(k)) {
 		query += tolower(k);
 		Filter(false);
