b47dc70f02407ebdeefdccd531f856e3783b045b
tnowell <tnowell@37977431-3df6-0310-b722-df95706aa16b>
2006-07-08 10:06:57 +0000

    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1592 37977431-3df6-0310-b722-df95706aa16b

---
+++ AI/Global/NTAI/Core/GlobalAI.h
@@ -0,0 +1,46 @@
+#if !defined(AFX_GroupAI_H__10718E36_5CDF_4CD4_8D90_F41311DD2694__INCLUDED_)
+#define AFX_GroupAI_H__10718E36_5CDF_4CD4_8D90_F41311DD2694__INCLUDED_
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// Iincludes
+//#include "Sim/Units/UnitDef.h"
+#include "IGlobalAI.h"
+#include "helper.h"
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// The name NTAI gives to the spring engine.
+
+const char AI_NAME[]= {"NTai XE9RC21"};
+
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+using namespace std;
+// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+class CNTai : public IGlobalAI{
+public:
+	CNTai();
+	virtual ~CNTai();
+	void InitAI(IGlobalAICallback* callback, int team);
+	void UnitCreated(int unit);
+	void UnitFinished(int unit);
+	void UnitDestroyed(int unit, int attacker);
+	void EnemyEnterLOS(int enemy);
+	void EnemyLeaveLOS(int enemy);
+	void EnemyEnterRadar(int enemy);
+	void EnemyLeaveRadar(int enemy);
+	void EnemyDestroyed(int enemy,int attacker);
+	void UnitIdle(int unit);
+	void GotChatMsg(const char* msg,int player);
+	void UnitDamaged(int damaged,int attacker,float damage,float3 dir);
+	void UnitMoveFailed(int unit);
+	void Update();
+	int HandleEvent (int msg, const void *data);
+	int tteam;
+	int team;
+	Global* GL;
+	IGlobalAICallback* cg;
+	IAICallback* acallback;
+
+	bool Good;
+};
+
+#endif // !defined(AFX_GroupAI_H__10718E36_5CDF_4CD4_8D90_F41311DD2694__INCLUDED_)
