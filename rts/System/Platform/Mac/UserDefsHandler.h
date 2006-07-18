1b52cb80cd331ec472febbd8095ac43ef9bc0542
krysole <krysole@37977431-3df6-0310-b722-df95706aa16b>
2006-07-18 09:40:13 +0000

    * Fix: User defaults wouldn't accept the dictionary so we're using the   more conventional approach now instead. Also synchronize call was added.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1627 37977431-3df6-0310-b722-df95706aa16b

--- rts/System/Platform/Mac/UserDefsHandler.h
+++ rts/System/Platform/Mac/UserDefsHandler.h
@@ -14,14 +14,12 @@
 class UserDefsHandler: public ConfigHandler
 {
 public:
-	UserDefsHandler();
 	virtual ~UserDefsHandler();
 	virtual void SetInt(std::string name, unsigned int value);
 	virtual void SetString(std::string name, std::string value);
 	virtual std::string GetString(std::string name, std::string def);
 	virtual unsigned int GetInt(std::string name, unsigned int def);
 private:
-	NSMutableDictionary *dictionary_;
 };
 
 #endif // USERDEFSHANDLER_H
--- rts/System/Platform/Mac/UserDefsHandler.mm
+++ rts/System/Platform/Mac/UserDefsHandler.mm
@@ -8,31 +8,9 @@
 
 #import "Foundation/Foundation.h"
 
-UserDefsHandler::UserDefsHandler()
-{
-	NSUserDefaults* defaults;
-	NSString *bundleIdentifier;
-	NSDictionary *defDict;
-
-	bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
-	defaults = [NSUserDefaults standardUserDefaults];
-	
-	defDict = [defaults persistentDomainForName:bundleIdentifier];
-	
-	dictionary_ = [[NSMutableDictionary dictionaryWithDictionary:defDict] retain];
-}
-
 UserDefsHandler::~UserDefsHandler()
 {
-	NSUserDefaults *defaults;
-	NSString *bundleIdentifier;
-	
-	bundleIdentifier = [[NSBundle mainBundle] bundleIdentifier];
-	defaults = [NSUserDefaults standardUserDefaults];
-	
-	[defaults setPersistentDomain:dictionary_ forName:bundleIdentifier];
-	
-	[dictionary_ release];
+	[[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 void UserDefsHandler::SetInt(std::string name, unsigned int value)
@@ -43,7 +21,7 @@ void UserDefsHandler::SetInt(std::string name, unsigned int value)
 	key = [NSString stringWithCString:name.c_str() encoding:NSASCIIStringEncoding];
 	number = [NSNumber numberWithUnsignedInt:value];
 	
-	[dictionary_ setObject:number forKey:key];
+	[[NSUserDefaults standardUserDefaults] setObject:number forKey:key];
 }
 
 void UserDefsHandler::SetString(std::string name, std::string value)
@@ -53,7 +31,7 @@ void UserDefsHandler::SetString(std::string name, std::string value)
 	key = [NSString stringWithCString:name.c_str() encoding:NSASCIIStringEncoding];
 	val = [NSString stringWithCString:value.c_str() encoding:NSASCIIStringEncoding];
 	
-	[dictionary_ setObject:val forKey:key];
+	[[NSUserDefaults standardUserDefaults] setObject:val forKey:key];
 }
 
 std::string UserDefsHandler::GetString(std::string name, std::string def)
@@ -61,7 +39,8 @@ std::string UserDefsHandler::GetString(std::string name, std::string def)
 	NSString *key, *value;
 	
 	key = [NSString stringWithCString:name.c_str() encoding:NSASCIIStringEncoding];
-	value = [dictionary_ objectForKey:key];
+	
+	value = [[NSUserDefaults standardUserDefaults] objectForKey:key];
 	
 	if (value == nil) {
 		return def;
@@ -77,7 +56,8 @@ unsigned int UserDefsHandler::GetInt(std::string name, unsigned int def)
 	NSNumber *number;
 	
 	key = [NSString stringWithCString:name.c_str() encoding:NSASCIIStringEncoding];
-	number = [dictionary_ objectForKey:key];
+	
+	number = [[NSUserDefaults standardUserDefaults] objectForKey:key];
 	
 	if (number == nil) {
 		return def;
