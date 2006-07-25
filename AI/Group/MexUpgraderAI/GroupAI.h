84a386b2886835edd208b368207ec7a3a39b9a33
colorblind <colorblind@37977431-3df6-0310-b722-df95706aa16b>
2006-07-25 14:45:23 +0000

    mexUpdater AI now has two modes: manual and automatic
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1674 37977431-3df6-0310-b722-df95706aa16b

--- AI/Group/MexUpgraderAI/GroupAI.h
+++ AI/Group/MexUpgraderAI/GroupAI.h
@@ -8,6 +8,8 @@
 #include "ExternalAI/IGroupAI.h"
 #include <set>
 #include <map>
+#include <vector>
+#include <deque>
 #include "float3.h"
 
 class IGroupAICallback;
@@ -35,36 +37,54 @@ public:
 
 	virtual void Update();
 
-	vector<CommandDescription> commands;
+	void AutoFindMex(int unitid);			// finds the next upgradable mex for unit "unitid"
+	void ManualFindMex();					// finds the next upgradable mex for the moho builder
+	void Reset();							// clears all orders and stops all units
+	void GuardMohoBuilder(int unitid);		// orders unitid to guard the mohobuilder
+	void ReclaimMex(int unitid, int mex);	// orders unitid to reclaim mex
+	int FindNearestMex(int unitid, int* possibleMexes, int size); // finds the nearest upgradable mex for unit "unitid" from possibleMexes with size "size"
 
+	vector<CommandDescription> commands;	// the commands that can be given to this groupAI ("stop","area upgrade","mode")
+	deque<Command> commandQue;				// queued-up area-upgrade commands
 
 	IGroupAICallback* callback;
 	IAICallback* aicb;
 
-	struct UnitInfo{
-		float maxExtractsMetal;
-		int wantedMohoId;
-		string wantedMohoName;
-
-		int nearestMex;
-		float3 wantedBuildSite;
-
-		bool reclaiming;
-		bool building;
-		bool hasReported;
+	typedef enum ModeType				// this group AI has two modes:
+	{
+		automatic,						// * automically find and replace every mex
+		manual							// * select areas in which to find and replace mexes
 	};
 
-	map<int,UnitInfo*> myUnits;
-	set<int> lockedMexxes;
+	ModeType mode;
+	typedef enum StatusType				// keeps track of the status of units in the groupAI:
+	{
+			idle,						// does nothing
+			reclaiming,					// is reclaiming a mex
+			building,					// is building a moho
+			guarding					// is guarding the mohoBuilder (see below)
+	};
 
-	int lastUpdate;
+	struct UnitInfo{
+		float maxExtractsMetal;			// the maximum amount of metal a unit buildable by othis unit can produce
+		int wantedMohoId;				// build command ID of the building above
+		string wantedMohoName;			// name of the building above
+
+		int nearestMex;					// ID of nearest upgradable mex
+		float3 wantedBuildSite;			// position of the upgradable mex, used to determine the moho position
+
+		StatusType status;
+	};
+
+	map<int,UnitInfo*> myUnits;			// keeps track of the units in the group AI
+	set<int> lockedMexxes;				// id's of mexes that are being reclaimed
+
+	float maxMetal;						// the maximum amount of metal a unit buildable by one of the groupAI's units can produce
+	int mohoBuilderId;					// the id of the unit that can build the unit that has maxMetal (above)
+	bool unitsChanged;					// set this to true if a unit is added to or removed from the group
+
 	int myTeam;
 	int* friendlyUnits;
-	int numFriendlyUnits;
-	int numUpgradableMexxes;
-	float minSqDist;
-
-
 };
 
 #endif // !defined(AFX_GroupAI_H__10718E36_5CDF_4CD4_8D90_F41311DD2694__INCLUDED_)
