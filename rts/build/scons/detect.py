16d86752acba189dd02e6efbbea8af4dbc749ace
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2006-08-01 23:10:46 +0000

    * patch by scribe to fix scons cpu detection on AMD Turion
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1715 37977431-3df6-0310-b722-df95706aa16b

--- rts/build/scons/detect.py
+++ rts/build/scons/detect.py
@@ -129,9 +129,9 @@ def processor(gcc_3_4=True):
 					else:
 						print "WARNING: gcc versions below 3.4 don't support -march=athlon64, using -march=athlon-4 -msse2 -mfpmath=sse instead"
 						archflags=['-march=athlon', '-msse2', '-mfpmath=sse']
-				elif str.find("Athlon") != -1:
+				elif (str.find("Athlon") != -1) or (str.find("Turion") != -1):
 					if str.find("64") != -1:
-						print "  found AMD Athlon 64"
+						print "  found AMD64"
 						if gcc_3_4: archflags=['-march=athlon64']
 						else:
 							print "WARNING: gcc versions below 3.4 don't support -march=athlon64, using -march=athlon-4 -msse2 -mfpmath=sse instead"
