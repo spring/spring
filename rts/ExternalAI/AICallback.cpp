spring/spring

commit 5f4aa9f96215aa109e2cd7ec9d4b619a266b14db

Orders on enemy units (attack and reclaim) get canceled automatically when the unit leaves LOS / radar. This fixes the "i know exactly where your unit is by force-firing my llt on it once and pressing shift"-exploit.
git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1564 37977431-3df6-0310-b722-df95706aa16b

rts/Sim/Units/CommandAI/AirCAI.cpp
@@ -353,7 +353,7 @@ void CAirCAI::SlowUpdate()
 			}
 		}
 		if(inCommand){
-			if(targetDied){
+			if(targetDied || !(uh->units[int(c.params[0])]->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
 				FinishCommand();
 				break;
 			}
@@ -479,12 +479,12 @@ void CAirCAI::DrawCommands(void)
 		case CMD_MOVE:
 			pos=float3(ci->params[0],ci->params[1]+curHeight,ci->params[2]);
 			glColor4f(0.5,1,0.5,0.4);
-			glVertexf3(pos);	
+			glVertexf3(pos);
 			break;
 		case CMD_PATROL:
 			pos=float3(ci->params[0],ci->params[1]+curHeight,ci->params[2]);
 			glColor4f(0.5,0.5,1,0.4);
-			glVertexf3(pos);	
+			glVertexf3(pos);
 			break;
 		case CMD_ATTACK:
 			if(ci->params.size()==1){
@@ -494,7 +494,7 @@ void CAirCAI::DrawCommands(void)
 				pos=float3(ci->params[0],ci->params[1]+curHeight,ci->params[2]);
 			}
 			glColor4f(1,0.5,0.5,0.4);
-			glVertexf3(pos);	
+			glVertexf3(pos);
 			break;
 		case CMD_AREA_ATTACK:
 			pos=float3(ci->params[0],ci->params[1]+curHeight,ci->params[2]);
@@ -520,7 +520,7 @@ void CAirCAI::DrawCommands(void)
 			if(uh->units[int(ci->params[0])]!=0)
 				pos=uh->units[int(ci->params[0])]->pos;
 			glColor4f(0.3,0.3,1,0.4);
-			glVertexf3(pos);	
+			glVertexf3(pos);
 			break;
 		}
 	}

rts/Sim/Units/CommandAI/BuilderCAI.cpp
@@ -157,7 +157,7 @@ void CBuilderCAI::SlowUpdate()
 					if(owner->moveType->progressState==CMoveType::Failed){
 						if(++buildRetries>5){
 							StopMove();
-							FinishCommand();			
+							FinishCommand();
 						}
 					}
 					SetGoal(buildPos,owner->pos, fac->buildDistance*0.4+radius);
@@ -338,7 +338,7 @@ void CBuilderCAI::SlowUpdate()
 
 			} else {							//reclaim unit
 				CUnit* unit=uh->units[id];
-				if(unit && unit!=owner){
+				if(unit && unit!=owner && (unit->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
 					if(unit->pos.distance2D(fac->pos)<fac->buildDistance-1+unit->radius){
 						StopMove();
 						owner->moveType->KeepPointingTo(unit->pos, fac->buildDistance*0.9+unit->radius, false);
@@ -349,7 +349,7 @@ void CBuilderCAI::SlowUpdate()
 						}else{
 							if(owner->moveType->progressState==CMoveType::Failed){
 								StopMove();
-								FinishCommand();			
+								FinishCommand();
 							}
 						}
 					}
@@ -388,7 +388,7 @@ void CBuilderCAI::SlowUpdate()
 						} else {
 							if(owner->moveType->progressState==CMoveType::Failed){
 								StopMove();
-								FinishCommand();			
+								FinishCommand();
 							}
 						}
 					}
@@ -702,7 +702,7 @@ void CBuilderCAI::DrawCommands(void)
 			draw=true;
 		}
 		if(draw){
-			glVertexf3(pos);	
+			glVertexf3(pos);
 		}
 	}
 	glEnd();

rts/Sim/Units/CommandAI/CommandAI.cpp
@@ -60,7 +60,7 @@ CCommandAI::CCommandAI(CUnit* owner)
  	c.onlyKey=true;
  	c.tooltip="Wait: Tells the unit to wait until another units handles him";
  	possibleCommands.push_back(c);
- 
+
 	c.id=CMD_SELFD;
 	c.type=CMDTYPE_ICON;
 	c.name="SelfD";
@@ -84,7 +84,7 @@ CCommandAI::CCommandAI(CUnit* owner)
 		c.params.push_back("Return fire");
 		c.params.push_back("Fire at will");
 		c.tooltip="Fire State: Sets under what conditions an\n unit will start to fire at enemy units\n without an explicit attack order";
-		possibleCommands.push_back(c);	
+		possibleCommands.push_back(c);
 		nonQueingCommands.insert(CMD_FIRE_STATE);
 	}
 
@@ -141,7 +141,7 @@ CCommandAI::CCommandAI(CUnit* owner)
 		c.params.push_back("On");
 
 		c.tooltip="Active State: Sets the active state of the unit to on or off";
-		possibleCommands.push_back(c);	
+		possibleCommands.push_back(c);
 		nonQueingCommands.insert(CMD_ONOFF);
 		c.key=0;
 	}
@@ -162,7 +162,7 @@ CCommandAI::CCommandAI(CUnit* owner)
 		c.params.push_back("Cloaked");
 
 		c.tooltip="Cloak State: Sets wheter the unit is cloaked or not";
-		possibleCommands.push_back(c);	
+		possibleCommands.push_back(c);
 		nonQueingCommands.insert(CMD_CLOAK);
 		c.key=0;
 	}
@@ -397,7 +397,7 @@ std::deque<Command>::iterator CCommandAI::GetCancelQueued(Command &c){
 						}
 					} else {
                         if((cpos-cipos).SqLength2D()<17*17){
-							return ci;				
+							return ci;
 						}
 					}
 				}
@@ -452,9 +452,9 @@ void CCommandAI::SlowUpdate()
 {
 	if(commandQue.empty())
 		return;
-	
+
 	Command& c=commandQue.front();
-	
+
 	switch(c.id){
 	case CMD_WAIT:
 		break;
@@ -462,13 +462,13 @@ void CCommandAI::SlowUpdate()
 		owner->AttackUnit(0,true);
 		std::vector<CWeapon*>::iterator wi;
 		for(wi=owner->weapons.begin();wi!=owner->weapons.end();++wi)
-			(*wi)->HoldFire();			
+			(*wi)->HoldFire();
 		FinishCommand();
 		break;};
 	case CMD_ATTACK:
 	case CMD_DGUN:
 		if(inCommand){
-			if(targetDied){
+			if(targetDied || !(uh->units[int(c.params[0])]->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
 				FinishCommand();
 			}
 		} else {
@@ -538,7 +538,7 @@ void CCommandAI::DrawCommands(void)
 			break;
 		}
 		if(draw){
-			glVertexf3(pos);	
+			glVertexf3(pos);
 		}
 	}
 	glEnd();
@@ -556,13 +556,13 @@ void CCommandAI::FinishCommand(void)
 	orderTarget=0;
 	if(owner->group)
 		owner->group->CommandFinished(owner->id,type);
-	
+
 	if(!owner->group && commandQue.empty())
 		globalAI->UnitIdle(owner);
 
 	if(lastFinishCommand!=gs->frameNum){	//avoid infinite loops
 		lastFinishCommand=gs->frameNum;
-		SlowUpdate();			
+		SlowUpdate();
 	}
 }
 

rts/Sim/Units/CommandAI/MobileCAI.cpp
@@ -252,7 +252,7 @@ void CMobileCAI::SlowUpdate()
 				inCommand=true;
 			}
 		}
-		if(targetDied){
+		if(targetDied || !(uh->units[int(c.params[0])]->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
 			StopMove();		//cancel keeppointingto
 			FinishCommand();
 			break;
@@ -278,7 +278,7 @@ void CMobileCAI::SlowUpdate()
 				owner->moveType->KeepPointingTo(pos, owner->maxRange*0.9, true);
 			} else {
 				if(pos.distance2D(goalPos)>10)
-					SetGoal(pos,curPos);					
+					SetGoal(pos,curPos);
 			}
 		}
 		break;
@@ -380,7 +380,7 @@ void CMobileCAI::DrawCommands(void)
 			break;
 		}
 		if(draw){
-			glVertexf3(pos);	
+			glVertexf3(pos);
 		}
 	}
 	glEnd();

commit 0e1e50deff1110c7aec87907b1ee858d31200b85

* Fix sync errors, one introduced by colorblind in r1564 and one introduced   by SJ/alphabeta in r1567.
  Blame them all, and DO NOT USE gu (GlobalUnsynced) in synced code!!!

* Add script to build a range of revisions. Saves a lot of time when hunting
  down bugs like those ^^^.

  (PS do not use gu in synced code)



git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1749 37977431-3df6-0310-b722-df95706aa16b

rts/ExternalAI/AICallback.cpp
@@ -607,7 +607,7 @@ bool CAICallback::CanBuildAt(const UnitDef* unitDef,float3 pos, int facing)
 	CFeature* f;
 	BuildInfo bi(unitDef, pos, facing);
 	bi.pos=helper->Pos2BuildPos (bi);
-	return !!uh->TestUnitBuildSquare(bi,f);
+	return !!uh->TestUnitBuildSquare(bi,f,gs->AllyTeam(team));
 }
 
 
@@ -659,7 +659,7 @@ float3 CAICallback::ClosestBuildSite(const UnitDef* unitdef,float3 pos,float sea
 		float z = pos.z+ofs[so].dy*SQUARE_SIZE*2;
 		BuildInfo bi(unitdef, float3(x,0,z), facing);
 		bi.pos = helper->Pos2BuildPos (bi);
-		if(uh->TestUnitBuildSquare(bi,feature) && (!feature || feature->allyteam!=allyteam))
+		if(uh->TestUnitBuildSquare(bi,feature,allyteam) && (!feature || feature->allyteam!=allyteam))
 		{
 			int xs=(int)(x/SQUARE_SIZE);
 			int zs=(int)(z/SQUARE_SIZE);

rts/Game/StartScripts/SpawnScript.cpp
@@ -63,18 +63,18 @@ void CSpawnScript::Update()
 	if(!spawns.empty()){
 		while(curSpawn->frame+frameOffset<gs->frameNum){
 			int num = gs->randInt() % spawnPos.size();
+			int team = autonomous ? (num & 1) : 1;
 			float3 pos;
 			float dist=200;
 			CFeature* feature;
 			do {
 				pos=spawnPos[num]+gs->randVector()*dist;
 				dist*=1.05;
-			} while (dist < 500 && uh->TestUnitBuildSquare(BuildInfo(curSpawn->name,pos,0),feature) != 2);
+			} while (dist < 500 && uh->TestUnitBuildSquare(BuildInfo(curSpawn->name,pos,0),feature,team) != 2);
 
 			// Ignore unit if it really doesn't fit.
 			// (within 18 tries, 200*1.05^18 < 500 < 200*1.05^19)
 			if (dist < 500) {
-				int team = autonomous ? (num & 1) : 1;
 				CUnit* u = unitLoader.LoadUnit(curSpawn->name, pos, team, false);
 
 				Unit unit;

rts/Map/BaseGroundDrawer.cpp
@@ -179,7 +179,7 @@ bool CBaseGroundDrawer::UpdateExtraTexture()
 							UnitDef *unitdef = unitDefHandler->GetUnitByID(-guihandler->commands[guihandler->inCommand].id);
 
 							CFeature* f;
-							if(uh->TestUnitBuildSquare(BuildInfo(unitdef,float3(x*16+8,0,y*16+8),guihandler->buildFacing),f))
+							if(uh->TestUnitBuildSquare(BuildInfo(unitdef,float3(x*16+8,0,y*16+8),guihandler->buildFacing),f,gu->myAllyTeam))
 								m=1;
 							else
 								m=0;

rts/Sim/Units/CommandAI/AirCAI.cpp
@@ -434,7 +434,7 @@ void CAirCAI::SlowUpdate()
 			}
 		}
 		if(inCommand){
-			if(targetDied || (c.params.size() == 1 && uh->units[int(c.params[0])] && !(uh->units[int(c.params[0])]->losStatus[gu->myAllyTeam] & LOS_INRADAR))){
+			if(targetDied || (c.params.size() == 1 && uh->units[int(c.params[0])] && !(uh->units[int(c.params[0])]->losStatus[owner->allyteam] & LOS_INRADAR))){
 				FinishCommand();
 				break;
 			}

rts/Sim/Units/CommandAI/BuilderCAI.cpp
@@ -173,7 +173,7 @@ void CBuilderCAI::SlowUpdate()
 				bi.buildFacing = int(c.params[3]);
 			bi.def = unitDefHandler->GetUnitByName(boi->second);
 
-			uh->TestUnitBuildSquare(bi,f);
+			uh->TestUnitBuildSquare(bi,f,owner->allyteam);
 			if(f){
 				Command c2;
 				c2.id=CMD_RECLAIM;
@@ -342,7 +342,7 @@ void CBuilderCAI::SlowUpdate()
 
 			} else {							//reclaim unit
 				CUnit* unit=uh->units[id];
-				if(unit && unit!=owner && (unit->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
+				if(unit && unit!=owner && (unit->losStatus[owner->allyteam] & LOS_INRADAR)){
 					if(unit->pos.distance2D(fac->pos)<fac->buildDistance-1+unit->radius){
 						StopMove();
 						owner->moveType->KeepPointingTo(unit->pos, fac->buildDistance*0.9+unit->radius, false);
@@ -745,7 +745,7 @@ void CBuilderCAI::GiveCommand(Command& c)
 		bi.def = unitDefHandler->GetUnitByName(boi->second);
 		bi.pos=helper->Pos2BuildPos(bi);
 		CFeature* feature;
-		if(!uh->TestUnitBuildSquare(bi,feature))
+		if(!uh->TestUnitBuildSquare(bi,feature,owner->allyteam))
 			return;
 	}
 	CMobileCAI::GiveCommand(c);

rts/Sim/Units/CommandAI/CommandAI.cpp
@@ -510,7 +510,7 @@ void CCommandAI::SlowUpdate()
 	case CMD_ATTACK:
 	case CMD_DGUN:
 		if(inCommand){
-			if(targetDied || (c.params.size() == 1 && uh->units[int(c.params[0])] && !(uh->units[int(c.params[0])]->losStatus[gu->myAllyTeam] & LOS_INRADAR))){
+			if(targetDied || (c.params.size() == 1 && uh->units[int(c.params[0])] && !(uh->units[int(c.params[0])]->losStatus[owner->allyteam] & LOS_INRADAR))){
 				FinishCommand();
 				break;
 			}

rts/Sim/Units/CommandAI/MobileCAI.cpp
@@ -328,7 +328,7 @@ void CMobileCAI::SlowUpdate()
 			break;
 		}
 
-		if(targetDied || (c.params.size() == 1 && uh->units[int(c.params[0])] && (!(uh->units[int(c.params[0])]->losStatus[gu->myAllyTeam] & LOS_INRADAR)))){
+		if(targetDied || (c.params.size() == 1 && uh->units[int(c.params[0])] && !(uh->units[int(c.params[0])]->losStatus[owner->allyteam] & LOS_INRADAR))){
 			StopMove();		//cancel keeppointingto
 			FinishCommand();
 			break;

rts/Sim/Units/UnitHandler.cpp
@@ -297,7 +297,7 @@ float CUnitHandler::GetBuildHeight(float3 pos, const UnitDef* unitdef)
 	return h;
 }
 
-int CUnitHandler::TestUnitBuildSquare(const BuildInfo& buildInfo, CFeature *&feature)
+int CUnitHandler::TestUnitBuildSquare(const BuildInfo& buildInfo, CFeature *&feature, int allyteam)
 {
 	feature=0;
 	int xsize=buildInfo.GetXSize();
@@ -326,7 +326,7 @@ int CUnitHandler::TestUnitBuildSquare(const BuildInfo& buildInfo, CFeature *&fea
 
 	for(int x=x1; x<x2; x+=SQUARE_SIZE){
 		for(int z=z1; z<z2; z+=SQUARE_SIZE){
-			int tbs=TestBuildSquare(float3(x,h,z),buildInfo.def,feature);
+			int tbs=TestBuildSquare(float3(x,h,z),buildInfo.def,feature,allyteam);
 			canBuild=min(canBuild,tbs);
 			if(canBuild==0)
 				return 0;
@@ -336,7 +336,7 @@ int CUnitHandler::TestUnitBuildSquare(const BuildInfo& buildInfo, CFeature *&fea
 	return canBuild;
 }
 
-int CUnitHandler::TestBuildSquare(const float3& pos, const UnitDef *unitdef,CFeature *&feature)
+int CUnitHandler::TestBuildSquare(const float3& pos, const UnitDef *unitdef, CFeature *&feature, int allyteam)
 {
 	int ret=2;
 	if(pos.x<0 || pos.x>=gs->mapx*SQUARE_SIZE || pos.z<0 || pos.z>=gs->mapy*SQUARE_SIZE)
@@ -348,7 +348,7 @@ int CUnitHandler::TestBuildSquare(const float3& pos, const UnitDef *unitdef,CFea
 	if(s=readmap->GroundBlocked(yardypos*gs->mapx+yardxpos)){
 		if(dynamic_cast<CFeature*>(s))
 			feature=(CFeature*)s;
-		else if(!dynamic_cast<CUnit*>(s) || (((CUnit*)s)->losStatus[gu->myAllyTeam] & LOS_INLOS))
+		else if(!dynamic_cast<CUnit*>(s) || (((CUnit*)s)->losStatus[allyteam] & LOS_INLOS))
 		{
 			if(s->immobile)
 				return 0;
@@ -426,7 +426,7 @@ int CUnitHandler::ShowUnitBuildSquare(const BuildInfo& buildInfo, const std::vec
 
 			int square=ground->GetSquare(float3(x,pos.y,z));
 			CFeature* feature=0;
-			int tbs=TestBuildSquare(float3(x,pos.y,z),buildInfo.def,feature);
+			int tbs=TestBuildSquare(float3(x,pos.y,z),buildInfo.def,feature,gu->myAllyTeam);
 			if(tbs){
 				UnitDef* ud;
 				float3 cPos;

rts/Sim/Units/UnitHandler.h
@@ -49,10 +49,10 @@ public:
 	//0 blocked
 	//1 mobile unit in the way
 	//2 free (or if feature is != 0 then with a blocking feature that can be reclaimed)
-	int  TestUnitBuildSquare(const BuildInfo& buildInfo,CFeature *&feature);	//test if a unit can be built at specified position
+	int  TestUnitBuildSquare(const BuildInfo& buildInfo,CFeature *&feature, int allyteam); //test if a unit can be built at specified position
 	int  ShowUnitBuildSquare(const BuildInfo& buildInfo);	//test if a unit can be built at specified position and show on the ground where it's to rough
 	int  ShowUnitBuildSquare(const BuildInfo& buildInfo, const std::vector<Command> &cv);
-	int  TestBuildSquare(const float3& pos, const UnitDef *unitdef,CFeature *&feature);	//test a single mapsquare for build possibility
+	int  TestBuildSquare(const float3& pos, const UnitDef *unitdef,CFeature *&feature, int allyteam); //test a single mapsquare for build possibility
 
 	void AddBuilderCAI(CBuilderCAI*);
 	void RemoveBuilderCAI(CBuilderCAI*);

rts/Sim/Units/UnitTypes/Builder.cpp
@@ -356,7 +356,7 @@ bool CBuilder::StartBuild(BuildInfo& buildInfo)
 	buildInfo.pos=helper->Pos2BuildPos(buildInfo);
 
 	CFeature* feature;
-	int canBuild=uh->TestUnitBuildSquare(buildInfo, feature);
+	int canBuild=uh->TestUnitBuildSquare(buildInfo, feature, allyteam);
 	if(canBuild<2){
 		CUnit* u=helper->GetClosestFriendlyUnit(buildInfo.pos,5,allyteam);
 		if(u && u->unitDef==buildInfo.def){

tools/scripts/build-revisions.sh
@@ -1,0 +1,70 @@
+#!/bin/sh
+# Author: Tobi Vollebregt
+#
+# This script can be used to build every revision in a certain range.
+# Usage: ./build-revisions.sh revision1 revision2 options
+#
+# It starts at revision1, builds spring, goes up one revision, if there were
+# any files in rts/ with .c* or .h* extension modified, it builds spring for
+# that revision. Then it goes up another revision, etc. etc., until it
+# reaches revision2.
+#
+# Very useful to find a regression, using this you can pre-build all
+# revisions and do a binary search later on the readily available executables.
+#
+# Extra options after revision2 are passed literally to scons configure.
+
+
+
+# Configuration
+
+logdir="logs"
+cachedir="cache"
+
+
+
+# Some sanity checks
+
+quit=0
+
+if [ x$(( $# < 2 )) == x1 ]; then
+	echo "Usage: $0 revision1 revision2"
+	quit=1
+fi
+
+if [ ! -d "$logdir" ]; then
+	echo "'$logdir' directory does not exist"
+	quit=1
+fi
+
+if [ "$quit" != "0" ]; then
+	exit "$quit"
+fi
+
+
+
+# Which revisions to build?
+
+left=$1
+right=$2
+shift 2
+
+
+
+# Start building
+
+echo "Building revisions $left..$right !"
+echo "Number of builds : $(( $right - $left + 1 ))"
+
+for (( r=$left ; $r<=$right ; r=$r+1 )); do
+	echo "* Building revision $r ..."
+	logfile="$logdir/log-r$r.txt"
+	( svn up --non-interactive -r "$r" 2>&1 || ( echo "Subversion failure.  Stop."; exit 1; ) ) | tee $logfile
+	if [ "$r" == "$left" ] || [ "x$(egrep "rts/.*\.[ch]" "$logfile")" != "x" ]; then
+		scons -c
+		echo "* scons configure cachedir=$cachedir $@ && scons spring" | tee -a $logfile
+		( scons configure "cachedir=$cachedir" "$@" && scons spring && mv "game/spring" "game/spring-r$r") 2>&1 | tee -a $logfile
+	fi
+done
+
+echo "Done building revisions $left..$right !"