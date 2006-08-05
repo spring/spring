d7caf6fa8fab15ac69f0c03a22957577795ae545
jcnossen <jcnossen@37977431-3df6-0310-b722-df95706aa16b>
2006-08-05 21:28:55 +0000

    * made spring not abort when gamedata/particles.tdf can't be found
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1762 37977431-3df6-0310-b722-df95706aa16b

--- rts/Sim/ModInfo.cpp
+++ rts/Sim/ModInfo.cpp
@@ -12,8 +12,14 @@ CModInfo::CModInfo(const char* modname)
 	name = modname;
 
 	// determine whether the modder allows the user to use team coloured nanospray
-	TdfParser tdfparser("gamedata/particles.tdf");
-	tdfparser.GetDef(allowTeamColors, "1", "nanospray\\allow_team_colours");
+	try {
+		TdfParser tdfparser("gamedata/particles.tdf");
+		tdfparser.GetDef(allowTeamColors, "1", "nanospray\\allow_team_colours");
+	}
+	catch(content_error)
+	{
+		allowTeamColors = true;
+	}
 	if(allowTeamColors) {
 		// Load the users preference for team coloured nanospray
 		gu->teamNanospray = configHandler.GetInt ("TeamNanoSpray", 0);
