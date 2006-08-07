b565c65bc969d93190578334ee01752b99a0287a
jcnossen <jcnossen@37977431-3df6-0310-b722-df95706aa16b>
2006-08-07 18:47:56 +0000

    patch from TheBlasphemer: missing armor.txt does no longer abort loading.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@1771 37977431-3df6-0310-b722-df95706aa16b

--- rts/Sim/Misc/DamageArrayHandler.cpp
+++ rts/Sim/Misc/DamageArrayHandler.cpp
@@ -13,28 +13,36 @@ int DamageArray::numTypes=1;
 
 CDamageArrayHandler::CDamageArrayHandler(void)
 {
-	TdfParser p("Armor.txt");
+	try
+	{
+		TdfParser p("Armor.txt");
 
-	std::vector<std::string> typelist = p.GetSectionList("");
+		std::vector<std::string> typelist = p.GetSectionList("");
 
-	DamageArray::numTypes=typelist.size()+1;
-	numTypes=typelist.size()+1;
+		DamageArray::numTypes=typelist.size()+1;
+		numTypes=typelist.size()+1;
 
-	info->AddLine(1, "Number of damage types: %d", numTypes);
-	int a=1;
-	for(std::vector<std::string>::iterator ti=typelist.begin();ti!=typelist.end();++a,++ti){
-		std::string s=*ti;
-		std::transform(s.begin(), s.end(), s.begin(), (int(*)(int))std::tolower);
-		name2type[s]=a;
-//		info->AddLine("%s has type num %i",(*ti).c_str(),a);
-		const std::map<std::string, std::string>& units=p.GetAllValues(*ti);
-
-		for(std::map<std::string, std::string>::const_iterator ui=units.begin();ui!=units.end();++ui){
-			std::string s=ui->first;
+		info->AddLine(1, "Number of damage types: %d", numTypes);
+		int a=1;
+		for(std::vector<std::string>::iterator ti=typelist.begin();ti!=typelist.end();++a,++ti){
+			std::string s=*ti;
 			std::transform(s.begin(), s.end(), s.begin(), (int(*)(int))std::tolower);
 			name2type[s]=a;
-//			info->AddLine("unit %s has type num %i",ui->first.c_str(),a);
+	//		info->AddLine("%s has type num %i",(*ti).c_str(),a);
+			const std::map<std::string, std::string>& units=p.GetAllValues(*ti);
+
+			for(std::map<std::string, std::string>::const_iterator ui=units.begin();ui!=units.end();++ui){
+				std::string s=ui->first;
+				std::transform(s.begin(), s.end(), s.begin(), (int(*)(int))std::tolower);
+				name2type[s]=a;
+	//			info->AddLine("unit %s has type num %i",ui->first.c_str(),a);
+			}
 		}
+	}
+	catch(content_error) // If the modrules.tdf isnt found
+	{
+		DamageArray::numTypes=1;
+		numTypes=1;
 	}
 }
 
