92a82d7222e57ee02ac62f4591a8ca6420e8b23d
jcnossen <jcnossen@37977431-3df6-0310-b722-df95706aa16b>
2006-09-04 21:32:29 +0000

    * moved gamedata and bitmaps to installer/builddata, so mods can be run in trunk that override files from the gamedata and bitmaps dirs. * run installer/builddata/build.bat on windows to generate the proper springcontent.sdz and bitmaps.sdz.
    * increased update frequency of checking for units on top of geothermals.
    
    This is not a BUGFIX ;)
    
    
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2078 37977431-3df6-0310-b722-df95706aa16b

---
+++ installer/builddata/build.bat
@@ -0,0 +1,21 @@
+
+@echo off
+echo This will build the springcontent.sdz and bitmaps.sdz needed to run spring
+echo Creating bitmaps.sdz
+del /Q ..\..\base\spring\bitmaps.sdz
+cd bitmaps
+..\..\pkzip -add -dir=current ..\..\_temp.zip bitmaps\*
+cd ..\..
+pkzip -add _temp.zip builddata\bitmaps\modinfo.tdf
+rename _temp.zip bitmaps.sdz
+move /Y bitmaps.sdz ..\game\base\spring
+cd builddata
+
+echo Creating springcontent.sdz
+del /Q ..\..\base\springcontent.sdz
+cd springcontent
+..\..\pkzip -add -dir=current ..\..\_temp.zip gamedata\*
+cd ..\..
+pkzip -add _temp.zip builddata\springcontent\modinfo.tdf
+rename _temp.zip springcontent.sdz
+move /Y springcontent.sdz ..\game\base
--- installer/make_gamedata_arch.bat
+++ installer/make_gamedata_arch.bat
@@ -1,18 +1,3 @@
-@echo off
-echo Creating bitmaps.sdz
-del /Q ..\base\spring\bitmaps.sdz
-cd ..\game
-..\installer\pkzip -add -dir=current ..\installer\_temp.zip bitmaps\*
-cd ..\installer
-pkzip -add _temp.zip builddata\bitmaps\modinfo.tdf
-rename _temp.zip bitmaps.sdz
-move /Y bitmaps.sdz ..\game\base\spring
-
-echo Creating springcontent.sdz
-del /Q ..\base\springcontent.sdz
-cd ..\game
-..\installer\pkzip -add -dir=current ..\installer\_temp.zip gamedata\*
-cd ..\installer
-pkzip -add _temp.zip builddata\springcontent\modinfo.tdf
-rename _temp.zip springcontent.sdz
-move /Y springcontent.sdz ..\game\base
+cd builddata
+call build
+cd..
