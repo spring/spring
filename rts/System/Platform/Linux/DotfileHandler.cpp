7f892041eefb1bd99acb69f3cdd903addf290cdf
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2006-09-06 20:38:03 +0000

    BUGFIX, needs r2039 too
    * rewrote DotfileHandler to use file locking & some smarter read-modify-write patterns,
      so having multiple instances open at the same time doesn't cause data loss or "random"
      changes of config entries.
      (needed because unitsync opens a DotfileHandler too for the data directory code)
    
    
    
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2098 37977431-3df6-0310-b722-df95706aa16b

--- rts/System/Platform/Linux/DotfileHandler.cpp
+++ rts/System/Platform/Linux/DotfileHandler.cpp
@@ -2,7 +2,8 @@
  * @file DotfileHandler.cpp
  * @brief Linux dotfile config implementation
  * @author Christopher Han <xiphux@gmail.com>
- * 
+ * @author Tobi Vollebregt <tobivollebregt@gmail.com>
+ *
  * DotfileHandler configuration class implementation
  * Copyright (C) 2005.  Licensed under the terms of the
  * GNU GPL, v2 or later.
@@ -12,40 +13,108 @@
 #include "DotfileHandler.h"
 #include "Platform/errorhandler.h"
 #include <sstream>
+#include <fcntl.h>
 
 /**
- * Attempts to open an existing config file.
- * If none exists, create one
+ * @brief POSIX file locking class
  */
-DotfileHandler::DotfileHandler(const string fname)
+class ScopedFileLock
 {
-	std::ifstream reader(fname.c_str());
-	if (reader.good()) {
-		int idx;
-		string read;
-		while (getline(reader,read))
-			if ((idx = read.find_first_of("=")) > 0)
-				data[read.substr(0,idx)] = read.substr(idx+1);
-		reader.close();
-	}
-	file.open(fname.c_str());
-	if (!file)
-		handleerror(0,"Could not write to config file","DotfileHandler",0);
-	filename = fname;
-	flushfile();
+	public:
+		ScopedFileLock(int fd, bool write);
+		~ScopedFileLock();
+	private:
+		int filedes;
+};
+
+/**
+ * @brief lock fd
+ *
+ * Lock file descriptor fd for reading (write == false) or writing
+ * (write == true).
+ */
+ScopedFileLock::ScopedFileLock(int fd, bool write) : filedes(fd)
+{
+	struct flock lock;
+	lock.l_type = write ? F_WRLCK : F_RDLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	if (fcntl(filedes, F_SETLKW, &lock))
+		handleerror(0, "Could not lock config file", "DotfileHandler", 0);
 }
 
 /**
- * Flushes all changes and closes file
+ * @brief unlock fd
  */
+ScopedFileLock::~ScopedFileLock()
+{
+	struct flock lock;
+	lock.l_type = F_UNLCK;
+	lock.l_whence = SEEK_SET;
+	lock.l_start = 0;
+	lock.l_len = 0;
+	if (fcntl(filedes, F_SETLKW, &lock))
+		handleerror(0, "Could not unlock config file", "DotfileHandler", 0);
+}
+
+/**
+ * @brief strip whitespace
+ *
+ * Strips whitespace off the string [begin, end] by setting the first
+ * non-whitespace character from the end to 0 and returning a pointer
+ * to the first non-whitespace character from the begin.
+ *
+ * Precondition: end must point to the last character of the string,
+ * ie. the one before the terminating null.
+ */
+char* DotfileHandler::Strip(char* begin, char* end) {
+	while (isspace(*begin)) ++begin;
+	while (end >= begin && isspace(*end)) --end;
+	*(end + 1) = 0;
+	return begin;
+}
+
+/**
+ * @brief process one line read from file
+ *
+ * Parses the 'key=value' assignment in buf and sets data[key] to value.
+ */
+void DotfileHandler::AppendLine(char* buf) {
+	char* eq = strchr(buf, '=');
+	if (eq) {
+		char* key = Strip(buf, eq - 1);
+		char* value = Strip(eq + 1, strchr(eq + 1, 0) - 1);
+		data[key] = value;
+	}
+}
+
+/**
+ * @brief Open file
+ *
+ * Attempts to open an existing config file.
+ * If none exists, create one.
+ */
+DotfileHandler::DotfileHandler(const std::string& fname) : filename(fname)
+{
+	FILE* file;
+
+	if ((file = fopen(fname.c_str(), "r"))) {
+		ScopedFileLock scoped_lock(fileno(file), false);
+		Read(file);
+	} else {
+		if (!(file = fopen(fname.c_str(), "w")))
+			handleerror(0, "Could not write to config file", "DotfileHandler", 0);
+	}
+	fclose(file);
+}
+
 DotfileHandler::~DotfileHandler()
 {
-	flushfile();
-	file.close();
 }
 
 /**
- * Gets integer value from config
+ * @brief Gets integer value from config
  */
 int DotfileHandler::GetInt(const string name, const int def)
 {
@@ -58,7 +127,10 @@ int DotfileHandler::GetInt(const string name, const int def)
 }
 
 /**
- * Gets string value from config
+ * @brief Gets string value from config
+ *
+ * If string value isn't set, it returns def AND it sets data[name] = def,
+ * so the defaults end up in the config file.
  */
 string DotfileHandler::GetString(const string name, const string def)
 {
@@ -71,49 +143,69 @@ string DotfileHandler::GetString(const string name, const string def)
 }
 
 /**
- * Closes and reopens the file, truncating it to 0
- * (to prevent extra trailing characters)
+ * @brief Rewind file and re-read it.
  */
-void DotfileHandler::truncatefile(void)
+void DotfileHandler::Read(FILE* file)
 {
-	if (file)
-		file.close();
-	file.open(filename.c_str(),std::ios::out|std::ios::trunc);
+	char buf[500];
+	rewind(file);
+	while (fgets(buf, sizeof(buf), file))
+		AppendLine(buf);
 }
 
 /**
- * Flush changes to disk
+ * @brief Truncate file and write data to it.
+ */
+void DotfileHandler::Write(FILE* file)
+{
+	rewind(file);
+	ftruncate(fileno(file), 0);
+	for(std::map<string,string>::iterator iter = data.begin(); iter != data.end(); ++iter)
+		fprintf(file, "%s=%s\n", iter->first.c_str(), iter->second.c_str());
+}
+
+/**
+ * @brief Sets a config string
+ *
+ * This does:
+ * 1) Lock file.
+ * 2) Read file (in case another program modified something)
+ * 3) Set data[name] = value.
+ * 4) Write file (so we keep the settings in the event of a crash or error)
+ * 5) Unlock file.
  *
  * Pretty hackish, but Windows registry changes
  * save immediately, while with DotfileHandler the
  * data is stored in an internal data structure for
  * fast access.  So we want to keep the settings in
- * the event of a crash
- */
-void DotfileHandler::flushfile(void)
-{
-	truncatefile();
-	for(std::map<string,string>::iterator iter = data.begin(); iter != data.end(); iter++)
-		file << iter->first << "=" << iter->second << std::endl;
-	file.flush();
-}
-
-/**
- * Sets a config string
+ * the event of a crash, but at the same time we don't
+ * want conflicts when multiple instances are running
+ * at the same time (which would cause data loss)
+ * (e.g. unitsync and spring).
  */
 void DotfileHandler::SetString(const string name, const string value)
 {
-	data[name] = value;
-	flushfile();
+	FILE* file = fopen(filename.c_str(), "r+");
+
+	if (file) {
+		ScopedFileLock scoped_lock(fileno(file), true);
+		Read(file);
+		data[name] = value;
+		Write(file);
+	} else
+		data[name] = value;
+
+	// must be outside above 'if (file)' block because of the lock.
+	if (file)
+		fclose(file);
 }
 
 /**
- * Sets a config integer
+ * @brief Sets a config integer
  */
 void DotfileHandler::SetInt(const string name, const int value)
 {
 	std::stringstream ss;
 	ss << value;
-	data[name] = ss.str();
-	flushfile();
+	SetString(name, ss.str());
 }
