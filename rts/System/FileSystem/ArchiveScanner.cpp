d6f4809ddcf8644b575d33b29cd229fadc87136d
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2006-09-14 17:15:16 +0000

    * Don't checksum dependencies of mods anymore:   it conflicts badly with auto updated zip files like   springcontent.sdz & bitmaps.sdz, which store timestamps internally.
    Better design would be to checksum just the unit stuff that may actually
    desync the game.
    
    
    
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2179 37977431-3df6-0310-b722-df95706aa16b

--- rts/System/FileSystem/ArchiveScanner.cpp
+++ rts/System/FileSystem/ArchiveScanner.cpp
@@ -606,8 +606,17 @@ unsigned int CArchiveScanner::GetChecksum(const string& root)
 {
 	unsigned int checksum = 0;
 	vector<string> ars = GetArchives(root);
-	for (vector<string>::iterator i = ars.begin(); i != ars.end(); ++i)
-		checksum  ^= GetArchiveChecksum(*i);
+
+	// Do not include dependencies in the checksum:
+	// it conflicts badly with the auto-updated default content files,
+	// probably because .sdz files store timestamps too, which may differ
+	// each time the file is generated.
+
+	//for (vector<string>::iterator i = ars.begin(); i != ars.end(); ++i)
+	//	checksum  ^= GetArchiveChecksum(*i);
+
+	if (!ars.empty())
+		checksum = GetArchiveChecksum(ars.front());
 	return checksum;
 }
 
