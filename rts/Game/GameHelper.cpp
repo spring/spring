spring/spring

commit de1e63d07fd27b9a89a7fba8379774871b310500

* The icontype of enemy units is remembered if they are under continuous radar coverage. * Ghosted buildings under continuous radar coverage don't have an error vector in their position anymore. * Ghosted buildings also turn into icons when far away. * Improved clickability of unit icons.
git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2386 37977431-3df6-0310-b722-df95706aa16b

rts/Game/GameHelper.cpp
@@ -30,6 +30,7 @@
 #include "Sim/Misc/GeometricObjects.h"
 #include "Rendering/Env/BaseWater.h"
 #include "mmgr.h"
+#include "Game/GameSetup.h"
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
@@ -229,8 +230,15 @@ float CGameHelper::GuiTraceRay(const float3 &start, const float3 &dir, float len
 		for(ui=qf->baseQuads[*qi].units.begin();ui!=qf->baseQuads[*qi].units.end();++ui){
 			if((*ui)==exclude)
 				continue;
-			if((*ui)->allyteam==gu->myAllyTeam || ((*ui)->losStatus[gu->myAllyTeam] & (LOS_INLOS | LOS_CONTRADAR)) || gu->spectating){
-				float3 dif=(*ui)->midPos-start;
+			if((*ui)->allyteam==gu->myAllyTeam || ((*ui)->losStatus[gu->myAllyTeam] & (LOS_INLOS | LOS_CONTRADAR)) || (useRadar && radarhandler->InRadar(*ui,gu->myAllyTeam)) || gu->spectating){
+				float3 pos=GetUnitErrorPos(*ui,gu->myAllyTeam);
+				if((*ui)->isIcon)
+				{
+					float h=ground->GetHeight(pos.x,pos.z);
+					if(pos.y<h+(*ui)->iconRadius)
+						pos.y=h+(*ui)->iconRadius;
+				}
+				float3 dif=pos-start;
 				float closeLength=dif.dot(dir);
 				if(closeLength<0)
 					continue;
@@ -243,22 +251,6 @@ float CGameHelper::GuiTraceRay(const float3 &start, const float3 &dir, float len
 				float tmp = rad * rad - closeVect.SqLength();
 				if(tmp > 0 && length>closeLength+sqrt(tmp)){
 					length=closeLength+sqrt(tmp);		//note that we take the length to the backside of the units, this is so you can select stuff inside factories
-					hit=*ui;
-				}
-			} else if(useRadar && radarhandler->InRadar(*ui,gu->myAllyTeam)){
-				float3 dif=(*ui)->midPos+(*ui)->posErrorVector*radarhandler->radarErrorSize[gu->myAllyTeam]-start;
-				float closeLength=dif.dot(dir);
-				if(closeLength<0)
-					continue;
-				if(closeLength>length)
-					closeLength=length;
-				float3 closeVect=dif-dir*closeLength;
-				float rad=(*ui)->iconRadius;
-				
-				//The argument to sqrt became negative (3.5f*10^-7) for some reason... so tempstoring the value
-				float tmp = rad * rad - closeVect.SqLength();
-				if(tmp > 0 && length>closeLength+sqrt(tmp)){
-					length=closeLength+sqrt(tmp);
 					hit=*ui;
 				}
 			}
@@ -594,6 +586,9 @@ float3 CGameHelper::GetUnitErrorPos(const CUnit* unit, int allyteam)
 {
 	float3 pos=unit->midPos;
 	if(gs->Ally(allyteam,unit->allyteam) || (unit->losStatus[allyteam] & LOS_INLOS)){
+		// ^ it's one of our own, or it's in LOS, so don't add an error ^
+	} else if((!gameSetup || gameSetup->ghostedBuildings) && (unit->losStatus[gu->myAllyTeam] & LOS_PREVLOS) && (unit->losStatus[gu->myAllyTeam] & LOS_CONTRADAR) && !unit->mobility){
+		// ^ this is a ghosted building, so don't add an error ^
 	} else if((unit->losStatus[allyteam] & LOS_INRADAR)){
 		pos+=unit->posErrorVector*radarhandler->radarErrorSize[allyteam];
 	} else {

rts/Game/UI/MiniMap.cpp
@@ -303,13 +303,13 @@ void CMiniMap::DrawUnit(CUnit* unit,float size)
 	if(unit->lastDamage>gs->frameNum-90 && gs->frameNum&8)
 		return;
 
-	float3 pos=unit->pos;
+	float3 pos=helper->GetUnitErrorPos(unit,gu->myAllyTeam);
 /*	if(pos.z<0 || pos.z>gs->mapy*SQUARE_SIZE){
 		logOutput.Print("Errenous position in minimap::drawunit %f %f %f",pos.x,pos.y,pos.z);
 		return;
 	}*/
 	CIcon* icon;
-	if(unit->allyteam==gu->myAllyTeam || (unit->losStatus[gu->myAllyTeam] & LOS_INLOS) || gu->spectating)
+	if(unit->allyteam==gu->myAllyTeam || (unit->losStatus[gu->myAllyTeam] & LOS_INLOS) || ((unit->losStatus[gu->myAllyTeam] & LOS_PREVLOS)&&(unit->losStatus[gu->myAllyTeam] & LOS_CONTRADAR)) || gu->spectating)
 	{
 		icon=iconHandler->GetIcon(unit->unitDef->iconType);
 		if(icon->radiusAdjust)
@@ -318,8 +318,6 @@ void CMiniMap::DrawUnit(CUnit* unit,float size)
 	else if((unit->losStatus[gu->myAllyTeam] & LOS_INRADAR))
 	{
 		icon=iconHandler->GetIcon("default");
-		pos+=unit->posErrorVector*radarhandler->radarErrorSize[gu->myAllyTeam];
-
 	}
 	else
 	{

rts/Rendering/UnitModels/UnitDrawer.cpp
@@ -234,11 +234,24 @@ void CUnitDrawer::Draw(bool drawReflection,bool drawRefraction)
 					}
 				}
 			} else if((*usi)->losStatus[gu->myAllyTeam] & LOS_PREVLOS){
-				if (!gameSetup || gameSetup->ghostedBuildings)
-					drawCloaked.push_back(*usi);
-
-				if(((*usi)->losStatus[gu->myAllyTeam] & LOS_INRADAR) && !((*usi)->losStatus[gu->myAllyTeam] & LOS_CONTRADAR)){
-					drawRadarIcon.push_back(*usi);
+				(*usi)->isIcon = true;
+				if ((!gameSetup || gameSetup->ghostedBuildings) && !((*usi)->mobility))
+				{
+					// it's a building we've had LOS on once
+					float sqDist=((*usi)->pos-camera->pos).SqLength();
+					float iconDistMult=iconHandler->GetDistance((*usi)->unitDef->iconType);
+					float realIconLength=iconLength*iconDistMult*iconDistMult;
+					if(sqDist<realIconLength){
+						drawCloaked.push_back(*usi);
+						(*usi)->isIcon = false;
+					}
+				}
+				if(((*usi)->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
+					if(!((*usi)->losStatus[gu->myAllyTeam] & LOS_CONTRADAR)){
+						drawRadarIcon.push_back(*usi);
+					} else if((*usi)->isIcon){ // this prevents us from drawing icons on top of ghosted buildings
+						drawIcon.push_back(*usi);
+					}
 				}
 			} else if(((*usi)->losStatus[gu->myAllyTeam] & LOS_INRADAR)){
 				drawRadarIcon.push_back(*usi);
@@ -370,9 +383,7 @@ void CUnitDrawer::DrawIcon(CUnit * unit, bool asRadarBlip)
 	//  * The square root of the camera distance.
 	//  * The mod defined 'iconSize' (which acts a multiplier).
 	//  * The unit radius, depending on whether the mod defined 'radiusadjust' is true or false.
-	float3 pos=unit->midPos;
-	if(asRadarBlip)
-		pos+=unit->posErrorVector*radarhandler->radarErrorSize[gu->myAllyTeam];
+	float3 pos=helper->GetUnitErrorPos(unit,gu->myAllyTeam);
 	float dist=sqrt((pos-camera->pos).Length());
 	float scale=0.4f*icon->size*dist;
 	if(icon->radiusAdjust && !asRadarBlip)

rts/Sim/Units/Unit.cpp
@@ -313,7 +313,7 @@ void CUnit::SlowUpdate()
 			if(!(losStatus[a]&LOS_INLOS)){
 				int prevLosStatus = losStatus[a];	
 			
-				if(mobility || beingBuilt){
+				if(beingBuilt){
 					losStatus[a]|=(LOS_INLOS | LOS_INRADAR);
 				} else {
 					losStatus[a]|=(LOS_INLOS | LOS_INRADAR | LOS_PREVLOS | LOS_CONTRADAR);	

commit a277d5307a1bd5cd8820233f2b07278901a0d43f

* Fix sync error introduced in r2386.
==> DO NOT USE CGlobalUnsynced* gu IN SYNCED CODE PLEASE <==



git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2426 37977431-3df6-0310-b722-df95706aa16b

rts/Game/GameHelper.cpp
@@ -587,7 +587,7 @@ float3 CGameHelper::GetUnitErrorPos(const CUnit* unit, int allyteam)
 	float3 pos=unit->midPos;
 	if(gs->Ally(allyteam,unit->allyteam) || (unit->losStatus[allyteam] & LOS_INLOS)){
 		// ^ it's one of our own, or it's in LOS, so don't add an error ^
-	} else if((!gameSetup || gameSetup->ghostedBuildings) && (unit->losStatus[gu->myAllyTeam] & LOS_PREVLOS) && (unit->losStatus[gu->myAllyTeam] & LOS_CONTRADAR) && !unit->mobility){
+	} else if((!gameSetup || gameSetup->ghostedBuildings) && (unit->losStatus[allyteam] & LOS_PREVLOS) && (unit->losStatus[allyteam] & LOS_CONTRADAR) && !unit->mobility){
 		// ^ this is a ghosted building, so don't add an error ^
 	} else if((unit->losStatus[allyteam] & LOS_INRADAR)){
 		pos+=unit->posErrorVector*radarhandler->radarErrorSize[allyteam];