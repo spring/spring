d40049a4384ae07670573dcf8b5d1f46474d6948
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2006-11-01 13:53:53 +0000

    * Better checks for whether zip & 7z are installed
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2517 37977431-3df6-0310-b722-df95706aa16b

--- tools/scripts/bbinit.sh
+++ tools/scripts/bbinit.sh
@@ -10,15 +10,29 @@ if [ "$#" != "1" ]; then
 	exit 1
 fi
 
+quit=0
+
+# 7z installed?
+if [ "x$(which 7z)" = "x" ]; then
+	echo 7z not found
+	quit=1
+fi
+
+# zip installed?
+if [ "x$(which zip)" = "x" ]; then
+	echo zip not found
+	quit=1
+fi
+
+if [ "x$quit" != "x0" ]; then
+	exit 1
+fi
+
 # Extract files only if the list of files doesn't exist (which we use to touch them,
 # so they dont get killed by cleanup scripts)
 if [ ! -f files ]; then
 	root="/home/tvo/Buildbot/spring_slave"
 	# Libs/includes needed for crosscompilation
-	if [ "x$(which 7z)" = "x" ]; then
-		echo 7z not found
-		exit 1
-	fi
 	7z x -y "$root/mingwlibs.exe" | grep 'Extracting  ' | sed 's/Extracting  //g' | tee files
 	# Files needed for installer building
 	tar xzfv "$root/extracontent.tar.gz" | tee -a files
