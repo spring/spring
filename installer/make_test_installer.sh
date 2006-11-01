3387c95de1bcda959eef8b423d8a95c3a45189b1
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2006-11-01 13:26:15 +0000

    * Attempt to make binaries & installers automatically. * Logic is now in tools/scripts/bbinit.sh and bbexit.sh so theres no need to   reconfigure buildbot for every little time.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@2515 37977431-3df6-0310-b722-df95706aa16b

--- installer/make_test_installer.sh
+++ installer/make_test_installer.sh
@@ -7,7 +7,7 @@
 # (Compatible with SConstruct, which is in trunk root)
 
 while [ ! -d installer ]; do
-        if [ "$PWD" == "/" ]; then
+        if [ "$PWD" = "/" ]; then
                 echo "Error: Could not find installer directory."
                 echo "Make sure to run this script from a directory below your checkout directory."
                 exit 1
@@ -15,19 +15,29 @@ while [ ! -d installer ]; do
         cd ..
 done
 
+# TODO: code to d/l & extract extra files for installer
+
 # Follows a slightly modified copy of make_test_installer.bat:
 
 echo This will create the various installers for a TA Spring release
 echo .
 echo Remember to generate all needed path information for the maps to be included
-echo Press any key to continue..
 echo .
 echo Also remember to update the version string in GameVersion.h so that
 echo the correct .pdb can be identified!
 echo .
 
-echo Creating test installer
-makensis -V3 -DTEST_BUILD -DMINGW installer/taspring.nsi
+if [ "$#" != "0" ]; then
+	echo Creating nightly installer of revision $1
+	defines="-DNIGHTLY_BUILD -DREVISION=$1"
+else
+	echo Creating test installer
+	defines="-DTEST_BUILD"
+fi
 
+echo .
+makensis -V3 -DMINGW $defines installer/taspring.nsi || exit 1
+
+echo .
 echo All done.. 
 echo If this is a public release, make sure to save this and tag CVS etc..
---
+++ tools/scripts/bbexit.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Author: Tobi Vollebregt
+#
+# Hook called as last step in buildbot crosscompile build.
+# Gets passed one argument: a revision number.
+#
+
+if [ "$#" != "1" ]; then
+	echo "Usage: $0 <revision>"
+	exit 1
+fi
+
+exitstatus=0
+
+# Zip & put exe online
+cd game
+if zip "spring-r$1.zip" "spring.exe"; then
+	chmod 644 "spring-r$1.zip"
+	mv "spring-r$1.zip" "/home/tvo/public_html/spring/"
+else
+	echo "failed to zip spring.exe into spring-r$1.zip"
+	exitstatus=1
+fi
+cd ..
+
+# Build installer & put online
+if installer/make_test_installer.sh "$1"; then
+	chmod 644 "spring_r$1_nightly.exe"
+	mv "spring_r$1_nightly.exe" "/home/tvo/public_html/spring/installer/"
+else
+	echo "failed to build installer"
+	exitstatus=1
+fi
+
+# Revert version so we don't get conflicts on next update when someone changes actual version.
+svn revert rts/Game/GameVersion.h || exit 1
+
+exit $exitstatus
