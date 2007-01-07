a8fa746dd63448a297f5cf80c446b6b22351c4e3
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2007-01-07 14:02:07 +0000

    * Source package script is include based, instead of exclude based now.   This way we dont put less bloat in the source packages.   Two .exe's are excluded so Debian doesn't have to repack source. * Remove freetype libs, they are in vclibs package anyway. * taspring->Spring in doxygen docs
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3088 37977431-3df6-0310-b722-df95706aa16b

--- installer/make_source_package.sh
+++ installer/make_source_package.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Author: Tobi Vollebregt
 
-if [ "x$1" = "x" ]; then
+# Sanity checks.
+if [ -z "$1" ]; then
 	echo "Usage: $0 <version>"
 	echo "Example: $0 0.72b1"
 	exit 1
@@ -12,42 +13,96 @@ if [ ! -x /usr/bin/svn ]; then
 	exit 1
 fi
 
-version=$1
-svnbase=https://taspring.clan-sy.com/svn/spring
-svnurl=$svnbase/tags/spring_$version
-dir=spring_$version
-tbz=spring_${version}_src.tar.bz2
-tgz=spring_${version}_src.tar.gz
-zip=spring_${version}_src.zip
-seven_zip=spring_${version}_src.7z
+version="$1"
+svnbase="https://taspring.clan-sy.com/svn/spring"
 
-# these bloat the source package way too much
-exclude="backup tools/Upspring tools/MapDesigner"
-
-if [ -d $dir ]; then
-	cd $dir
-	/usr/bin/svn update || exit 1
-	cd ..
+if [ "$version" = "trunk" ]; then
+	svnurl="$svnbase/trunk"
+	#version="trunk-r`svn info $svnbase/trunk | grep Revision | sed 's/^Revision: //g'`"
+	# Find checkout directory.
+	svndir="."
+	while [ ! -d "$svndir/installer" ]; do
+        	if [ "`( cd "$svndir" && echo "$PWD" )`" = "/" ]; then
+                	echo "Error: Could not find installer directory."
+	                echo "Make sure to run this script from a directory below your checkout directory."
+        	        exit 1
+	        fi
+		if [ "$svndir" = "." ]; then
+			svndir=".."
+		else
+	        	svndir="$svndir/.."
+		fi
+	done
 else
-	/usr/bin/svn checkout $svnurl $dir || exit 1
+	svnurl="$svnbase/tags/spring_$version"
+	svndir="spring_$version"
+fi
+
+dir="spring_$version"
+tbz="spring_${version}_src.tar.bz2"
+tgz="spring_${version}_src.tar.gz"
+zip="spring_${version}_src.zip"
+seven_zip="spring_${version}_src.7z"
+
+# This is the list of files/directories that go in the source package.
+# (directories are included recursively)
+include=" \
+ $dir/AI/Group/ \
+ $dir/AI/Global/ \
+ $dir/Documentation/ \
+ $dir/Doxyfile \
+ $dir/game/ \
+ $dir/installer/ \
+ $dir/LICENSE.html \
+ $dir/lua/ \
+ $dir/README.* \
+ $dir/rts/ \
+ $dir/SConstruct \
+ $dir/tools/unitsync/ \
+ $dir/TASClient \
+ $dir/TASServer \
+ $dir/UnityLobby"
+
+# On linux, win32 executables are useless.
+linux_exclude="$dir/installer/pkzip.exe $dir/TASClient/brcc32.exe"
+windows_exclude=""
+
+# Update/create SVN checkout.
+if [ -d "$svndir" ]; then
+	olddir="$PWD"
+	cd "$svndir"
+	echo '-> UPDATING SVN CHECKOUT'
+	/usr/bin/svn update || exit 1
+	cd "$olddir"
+else
+	echo '-> CREATING SVN CHECKOUT'
+	/usr/bin/svn checkout "$svnurl" "$svndir" || exit 1
 fi
 
 # Linux line endings, .tar.{bz2,gz} package.
-mkdir lf
+echo '-> EXPORTING CHECKOUT DIR WITH LF LINE ENDINGS'
+mkdir lf || exit 1
 cd lf
-/usr/bin/svn export ../$dir $dir --native-eol LF || exit 1
-cd $dir; rm -rf $exclude; cd ..
-tar cfj ../$tbz $dir
-tar cfz ../$tgz $dir
+/usr/bin/svn export "../$svndir" "$dir" --native-eol LF || exit 1
+[ -n "$linux_exclude" ] && rm -rf $linux_exclude
+echo '-> CREATING .TAR.BZ2 ARCHIVE'
+tar cfj "../$tbz" $include
+echo '-> CREATING .TAR.GZ ARCHIVE'
+tar cfz "../$tgz" $include
 cd ..
+echo '-> CLEANING'
 rm -rf lf
 
 # Windows line endings, .zip/.7z package
-mkdir crlf
+echo '-> EXPORTING CHECKOUT DIR WITH CRLF LINE ENDINGS'
+mkdir crlf || exit 1
 cd crlf
-/usr/bin/svn export ../$dir $dir --native-eol CRLF || exit 1
-cd $dir; rm -rf $exclude; cd ..
-[ -x /usr/bin/zip ] && /usr/bin/zip -r -9 ../$zip $dir
-[ -x /usr/bin/7z ] && /usr/bin/7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on ../$seven_zip $dir
+/usr/bin/svn export "../$svndir" "$dir" --native-eol CRLF || exit 1
+[ -n "$windows_exclude" ] && rm -rf $windows_exclude
+[ -x /usr/bin/zip ] && echo '-> CREATING .ZIP ARCHIVE' && \
+	/usr/bin/zip -q -r -u -9 "../$zip" $include
+[ -x /usr/bin/7z ] && echo '-> CREATING .7Z ARCHIVE' && \
+	/usr/bin/7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on "../$seven_zip" $include >/dev/null
 cd ..
+echo '-> CLEANING'
 rm -rf crlf
