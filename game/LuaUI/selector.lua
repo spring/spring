e24bb703bdb93f11d605550bac1c1c90b39a5121
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2007-01-08 08:43:15 +0000

    * another future reference
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3096 37977431-3df6-0310-b722-df95706aa16b

--- game/LuaUI/selector.lua
+++ game/LuaUI/selector.lua
@@ -16,7 +16,7 @@ function widget:GetInfo()
     name      = "WidgetSelector",
     desc      = "Widget selection widget",
     author    = "trepan",
-    date      = "Jan 11, 2007",
+    date      = "Jan 8, 2007",
     license   = "GNU GPL, v2 or later",
     drawLayer = 3,
     enabled   = true  --  loaded by default?
