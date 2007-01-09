0f23c34cddceece3ab4ee71fb4932b23a486ac3e
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2007-01-09 23:34:10 +0000

    * wrap the initial icon setting in widget:Initialize()
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3119 37977431-3df6-0310-b722-df95706aa16b

--- game/LuaUI/Widgets/ico_customicons.lua
+++ game/LuaUI/Widgets/ico_customicons.lua
@@ -57,68 +57,70 @@ end
 
 --------------------------------------------------------------------------------
 
--- Setup the unitdef icons
 
-for udid,ud in pairs(UnitDefs) do
-  if (ud ~= nil) then
-    if (ud.origIconType == nil) then
-      ud.origIconType = ud.iconType
-    end
+function widget:Initialize()
+  -- Setup the unitdef icons
+  for udid,ud in pairs(UnitDefs) do
+    if (ud ~= nil) then
+      if (ud.origIconType == nil) then
+        ud.origIconType = ud.iconType
+      end
 
-    if (ud.isCommander or (ud.TEDClass == "COMMANDER")) then
-      -- commanders
-      commanderUnitDef = udid -- save for animation
-      Spring.SetUnitDefIcon(udid, "star.user")
-    elseif (ud.isFactory) then
-      -- factories
-      Spring.SetUnitDefIcon(udid, "square_x.user")
-    elseif (ud.isBuilder) then
-      -- builders
-      if ((ud.speed > 0) and ud.canMove) then
-        Spring.SetUnitDefIcon(udid, "cross.user")     -- mobile
+      if (ud.isCommander or (ud.TEDClass == "COMMANDER")) then
+        -- commanders
+        commanderUnitDef = udid -- save for animation
+        Spring.SetUnitDefIcon(udid, "star.user")
+      elseif (ud.isFactory) then
+        -- factories
+        Spring.SetUnitDefIcon(udid, "square_x.user")
+      elseif (ud.isBuilder) then
+        -- builders
+        if ((ud.speed > 0) and ud.canMove) then
+          Spring.SetUnitDefIcon(udid, "cross.user")     -- mobile
+        else
+          Spring.SetUnitDefIcon(udid, "square_+.user")  -- immobile
+        end
+      elseif (ud.canFly) then
+        -- aircraft
+        Spring.SetUnitDefIcon(udid, "tri-up.user")
+      elseif ((ud.speed <= 0) and ud.hasShield) then
+        -- immobile shields
+        Spring.SetUnitDefIcon(udid, "hemi-up.user")
+      elseif ((ud.extractsMetal > 0) or (ud.makesMetal > 0)) then
+        -- metal extractors and makers
+        Spring.SetUnitDefIcon(udid, "m.user")
+      elseif ((ud.totalEnergyOut > 10) and (ud.speed <= 0)) then
+        -- energy generators
+        Spring.SetUnitDefIcon(udid, "e.user")
+      elseif (ud.isTransport) then
+        -- transports
+        Spring.SetUnitDefIcon(udid, "diamond.user")
+      elseif ((ud.minWaterDepth > 0) and (ud.speed > 0) and (ud.waterline > 10)) then
+        -- submarines
+        Spring.SetUnitDefIcon(udid, "tri-down.user")
+      elseif ((ud.minWaterDepth > 0) and (ud.speed > 0)) then
+        -- ships
+        Spring.SetUnitDefIcon(udid, "hemi-down.user")
+      elseif (((ud.radarRadius > 1) or
+               (ud.sonarRadius > 1) or
+               (ud.seismicRadius > 1)) and (ud.speed <= 0)) then
+        -- sensors
+        Spring.SetUnitDefIcon(udid, "hourglass-side.user")
+      elseif (((ud.jammerRadius > 1) or
+               (ud.sonarJamRadius > 1)) and (ud.speed <= 0)) then
+        -- jammers
+        Spring.SetUnitDefIcon(udid, "hourglass.user")
+      elseif (ud.isBuilding or (ud.speed <= 0)) then
+        -- defenders and other buildings
+        if (ud.weaponCount <= 0) then
+          Spring.SetUnitDefIcon(udid, "square.user")
+        else 
+          Spring.SetUnitDefIcon(udid, "x.user")
+        end
       else
-        Spring.SetUnitDefIcon(udid, "square_+.user")  -- immobile
+        -- fixme: allow unknowns to use mod icons?
+        Spring.SetUnitDefIcon(udid, "default")
       end
-    elseif (ud.canFly) then
-      -- aircraft
-      Spring.SetUnitDefIcon(udid, "tri-up.user")
-    elseif ((ud.speed <= 0) and ud.hasShield) then
-      -- immobile shields
-      Spring.SetUnitDefIcon(udid, "hemi-up.user")
-    elseif ((ud.extractsMetal > 0) or (ud.makesMetal > 0)) then
-      -- metal extractors and makers
-      Spring.SetUnitDefIcon(udid, "m.user")
-    elseif ((ud.totalEnergyOut > 10) and (ud.speed <= 0)) then
-      -- energy generators
-      Spring.SetUnitDefIcon(udid, "e.user")
-    elseif (ud.isTransport) then
-      -- transports
-      Spring.SetUnitDefIcon(udid, "diamond.user")
-    elseif ((ud.minWaterDepth > 0) and (ud.speed > 0) and (ud.waterline > 10)) then
-      -- submarines
-      Spring.SetUnitDefIcon(udid, "tri-down.user")
-    elseif ((ud.minWaterDepth > 0) and (ud.speed > 0)) then
-      -- ships
-      Spring.SetUnitDefIcon(udid, "hemi-down.user")
-    elseif (((ud.radarRadius > 1) or
-             (ud.sonarRadius > 1) or
-             (ud.seismicRadius > 1)) and (ud.speed <= 0)) then
-      -- sensors
-      Spring.SetUnitDefIcon(udid, "hourglass-side.user")
-    elseif (((ud.jammerRadius > 1) or
-             (ud.sonarJamRadius > 1)) and (ud.speed <= 0)) then
-      -- jammers
-      Spring.SetUnitDefIcon(udid, "hourglass.user")
-    elseif (ud.isBuilding or (ud.speed <= 0)) then
-      -- defenders and other buildings
-      if (ud.weaponCount <= 0) then
-        Spring.SetUnitDefIcon(udid, "square.user")
-      else 
-        Spring.SetUnitDefIcon(udid, "x.user")
-      end
-    else
-      -- fixme: allow unknowns to use mod icons?
-      Spring.SetUnitDefIcon(udid, "default")
     end
   end
 end
