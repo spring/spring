fa36bf041722365a584e8607b11514564cf567cf
isokron <isokron@37977431-3df6-0310-b722-df95706aa16b>
2007-01-23 20:50:07 +0000

    Some speed optimizations in quadfield Changes test script to use xta 8.1 unit names
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3294 37977431-3df6-0310-b722-df95706aa16b

--- rts/Game/StartScripts/TestScript.cpp
+++ rts/Game/StartScripts/TestScript.cpp
@@ -36,7 +36,7 @@ void CTestScript::Update()
 {
 	switch(gs->frameNum){
 	case 0:
-
+		{
 		for(int a=0;a<0000;++a){
 			float3 pos((gs->randFloat()+gs->randFloat())*2000,0,(gs->randFloat()+gs->randFloat())*2000);
 			pos.y=ground->GetHeight(pos.x,pos.z);
@@ -44,42 +44,42 @@ void CTestScript::Update()
 			string feature=featureHandler->wreckParser.GetSectionList("")[(int)(gs->randFloat()*num)];
 			featureHandler->CreateWreckage(pos,feature,0,0,1,-1,false,"");
 		}
-		for(int a=0;a<30;++a){
-			unitLoader.LoadUnit("ARMFLASH",float3(2000+a*80,10,2900),0,false);
-			unitLoader.LoadUnit("ARMZEUS",float3(2000+a*80,10,2855),0,false);
-			unitLoader.LoadUnit("ARMMAV",float3(2000+a*80,10,2800),0,false);
-			unitLoader.LoadUnit("CORAK",float3(2000+a*80,10,4560),1,false);
-			unitLoader.LoadUnit("CORCAN",float3(2000+a*80,10,4595),1,false);
-			unitLoader.LoadUnit("CORFAST",float3(2000+a*80,10,4640),1,false);
+ 		for(int a=0;a<30;++a){
+			unitLoader.LoadUnit("ARM_FLASH",float3(2000+a*80,10,2900),0,false);
+			unitLoader.LoadUnit("ARM_ZEUS",float3(2000+a*80,10,2855),0,false);
+			unitLoader.LoadUnit("ARM_MAVERICK",float3(2000+a*80,10,2800),0,false);
+			unitLoader.LoadUnit("CORE_AK",float3(2000+a*80,10,4560),1,false);
+			unitLoader.LoadUnit("CORE_THE_CAN",float3(2000+a*80,10,4595),1,false);
+			unitLoader.LoadUnit("CORE_FREAKER",float3(2000+a*80,10,4640),1,false);
 		}/**/
 
-		unitLoader.LoadUnit("ARMROCK",float3(1800,10,2950),0,false);
-		unitLoader.LoadUnit("ARMROCK",float3(1825,10,2950),0,false);
-		unitLoader.LoadUnit("ARMROCK",float3(1850,10,2950),0,false);
-		unitLoader.LoadUnit("ARMROCK",float3(1875,10,2950),0,false);
-		unitLoader.LoadUnit("ARMLLT",float3(1900,10,2950),0,false);
-		unitLoader.LoadUnit("ARMMART",float3(1800,10,2925),0,false);
-		unitLoader.LoadUnit("ARMMART",float3(1825,10,2925),0,false);
-		unitLoader.LoadUnit("ARMMART",float3(1850,10,2925),0,false);
-		unitLoader.LoadUnit("ARMMERL",float3(1825,10,2910),0,false);
-		unitLoader.LoadUnit("armcv",float3(1850,10,2905),0,false);
+		unitLoader.LoadUnit("ARM_ROCKO",float3(1800,10,2950),0,false);
+		unitLoader.LoadUnit("ARM_ROCKO",float3(1825,10,2950),0,false);
+		unitLoader.LoadUnit("ARM_ROCKO",float3(1850,10,2950),0,false);
+		unitLoader.LoadUnit("ARM_ROCKO",float3(1875,10,2950),0,false);
+		unitLoader.LoadUnit("ARM_LIGHT_LASER_TOWER",float3(1900,10,2950),0,false);
+		unitLoader.LoadUnit("ARM_LUGER",float3(1800,10,2925),0,false);
+		unitLoader.LoadUnit("ARM_LUGER",float3(1825,10,2925),0,false);
+		unitLoader.LoadUnit("ARM_LUGER",float3(1850,10,2925),0,false);
+		unitLoader.LoadUnit("ARM_MERL",float3(1825,10,2910),0,false);
+		unitLoader.LoadUnit("ARM_CONSTRUCTION_VEHICLE",float3(1850,10,2905),0,false);
 
-		unitLoader.LoadUnit("armck",float3(1850,10,2900),0,false);
-		unitLoader.LoadUnit("ARMFLASH",float3(1900,10,2900),0,false);
-		unitLoader.LoadUnit("ARMBULL",float3(1950,10,2900),0,false);
-		unitLoader.LoadUnit("ARMSAM",float3(1875,10,2925),0,false);
-		unitLoader.LoadUnit("ARMJETH",float3(1850,10,2890),0,false);
-		unitLoader.LoadUnit("ARMPNIX",float3(1850,10,2600),0,false);
-		unitLoader.LoadUnit("ARMHAWK",float3(1850,10,2680),0,false);
-		unitLoader.LoadUnit("ARMBRAWL",float3(1950,10,2680),0,false);
-		unitLoader.LoadUnit("ARMBRTHA",float3(1650,10,1550),0,false);
-		unitLoader.LoadUnit("CORKROG",float3(1950,10,2750),0,false);
+		unitLoader.LoadUnit("ARM_CONSTRUCTION_KBOT",float3(1850,10,2900),0,false);
+		unitLoader.LoadUnit("ARM_FLASH",float3(1900,10,2900),0,false);
+		unitLoader.LoadUnit("ARM_BULLDOG",float3(1950,10,2900),0,false);
+		unitLoader.LoadUnit("ARM_SAMSON",float3(1875,10,2925),0,false);
+		unitLoader.LoadUnit("ARM_JETHRO",float3(1850,10,2890),0,false);
+		unitLoader.LoadUnit("ARM_PHOENIX",float3(1850,10,2600),0,false);
+		unitLoader.LoadUnit("ARM_HAWK",float3(1850,10,2680),0,false);
+		unitLoader.LoadUnit("ARM_BRAWLER",float3(1950,10,2680),0,false);
+		unitLoader.LoadUnit("ARM_BIG_BERTHA",float3(1650,10,1550),0,false);
+		unitLoader.LoadUnit("CORE_KROGOTH",float3(1950,10,2750),0,false);
 //		unitLoader.LoadUnit("ARMTHOVR",float3(1750,10,2550),0,false);
 
-		unitLoader.LoadUnit("ARMMEX",float3(2850,10,2300),0,false);
-		unitLoader.LoadUnit("ARMMOHO",float3(2950,10,2380),0,false);
+		unitLoader.LoadUnit("ARM_METAL_EXTRACTOR",float3(2850,10,2300),0,false);
+		unitLoader.LoadUnit("ARM_MOHO_MINE",float3(2950,10,2380),0,false);
 
-		unitLoader.LoadUnit("ARMARAD",float3(2950,10,3780),0,false);
+		unitLoader.LoadUnit("ARM_ADVANCED_RADAR_TOWER",float3(2950,10,3780),0,false);
 
 		std::vector<int> su;
 		for(std::list<CUnit*>::iterator ui=uh->activeUnits.begin();ui!=uh->activeUnits.end();++ui){
@@ -98,32 +98,35 @@ void CTestScript::Update()
 		selectedUnits.NetOrder(c,0);
 		//selectedUnits.GiveCommand(c,false);
 
-		unitLoader.LoadUnit("CORCAN",float3(1770,10,4540),1,false);
-		unitLoader.LoadUnit("CORCAN",float3(1815,10,4540),1,false);
-		unitLoader.LoadUnit("CORAK",float3(1850,10,4540),1,false);
-		unitLoader.LoadUnit("CORAK",float3(1895,10,4540),1,false);
-		unitLoader.LoadUnit("ARMZEUS",float3(2000,10,4580),1,false);
-		unitLoader.LoadUnit("CORTHUD",float3(1750,10,4640),1,false);
-		unitLoader.LoadUnit("CORRAID",float3(1800,10,4640),1,false);
-		unitLoader.LoadUnit("CORGOL",float3(1850,10,4600),1,false);
-		unitLoader.LoadUnit("CORPYRO",float3(1850,10,4700),1,false);
-		unitLoader.LoadUnit("CORMORT",float3(1900,10,4700),1,false);
-		unitLoader.LoadUnit("CORREAP",float3(2050,10,4660),1,false);
-		unitLoader.LoadUnit("CORREAP",float3(1975,10,4580),1,false);
+		unitLoader.LoadUnit("CORE_THE_CAN",float3(1770,10,4540),1,false);
+		unitLoader.LoadUnit("CORE_THE_CAN",float3(1815,10,4540),1,false);
+		unitLoader.LoadUnit("CORE_AK",float3(1850,10,4540),1,false);
+		unitLoader.LoadUnit("CORE_AK",float3(1895,10,4540),1,false);
+		unitLoader.LoadUnit("ARM_ZEUS",float3(2000,10,4580),1,false);
+		unitLoader.LoadUnit("CORE_THUD",float3(1750,10,4640),1,false);
+		unitLoader.LoadUnit("CORE_RAIDER",float3(1800,10,4640),1,false);
+		unitLoader.LoadUnit("CORE_GOLIATH",float3(1850,10,4600),1,false);
+		unitLoader.LoadUnit("CORE_PYRO",float3(1850,10,4700),1,false);
+		unitLoader.LoadUnit("CORE_MORTY",float3(1900,10,4700),1,false);
+		unitLoader.LoadUnit("CORE_REAPER",float3(2050,10,4660),1,false);
+		unitLoader.LoadUnit("CORE_REAPER",float3(1975,10,4580),1,false);
 
-		unitLoader.LoadUnit("CORDOOM",float3(1875,10,4465),1,false);
-		unitLoader.LoadUnit("CORSHAD",float3(1850,10,4840),1,false);
-		unitLoader.LoadUnit("CORVAMP",float3(1850,10,4860),1,false);
-		unitLoader.LoadUnit("CORTOAST",float3(1740,10,4480),1,false);/**/
-		unitLoader.LoadUnit("x1ebcorjtorn",float3(2850,10,4860),1,false);
+		unitLoader.LoadUnit("CORE_DOOMSDAY_MACHINE",float3(1875,10,4465),1,false);
+		unitLoader.LoadUnit("CORE_SHADOW",float3(1850,10,4840),1,false);
+		unitLoader.LoadUnit("CORE_VAMP",float3(1850,10,4860),1,false);
+		unitLoader.LoadUnit("CORE_TOASTER",float3(1740,10,4480),1,false);/**/
+		unitLoader.LoadUnit("ARM_RADAR_JAMMING_TOWER",float3(2850,10,4860),1,false);
 
-		unitLoader.LoadUnit("armsilo",float3(2650,10,2600),0,false);
-		unitLoader.LoadUnit("armscab",float3(2250,10,4800),1,false);
+		unitLoader.LoadUnit("ARM_RETALIATOR",float3(2650,10,2600),0,false);
+		unitLoader.LoadUnit("ARM_SCARAB",float3(2250,10,4800),1,false);
 
-		unitLoader.LoadUnit("ARMANNI",float3(2075,10,5765),1,false);
+		unitLoader.LoadUnit("ARM_ANNIHILATOR",float3(2075,10,5765),1,false);
 //		unitLoader.LoadUnit("ARMANNI",float3(2275,10,5765),1,false);
 //		unitLoader.LoadUnit("ARMANNI",float3(2475,10,5765),1,false);
 //		unitLoader.LoadUnit("ARMROCK",float3(18,10,29),0,false);
+		break;}
+	case 3000:
+		gs->paused = true;
 		break;
 	}
 }
