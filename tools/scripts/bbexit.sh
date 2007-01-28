c73d5e59636a28bc090f881ca229958e23f6396b
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2007-01-28 13:04:48 +0000

    * Doxyfile tuning * Disable nightly installer building
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3340 37977431-3df6-0310-b722-df95706aa16b

--- tools/scripts/bbexit.sh
+++ tools/scripts/bbexit.sh
@@ -17,7 +17,7 @@ echo "Creating spring-r$1.zip containing spring.exe"
 cd game
 if zip "spring-r$1.zip" "spring.exe" "spring.def"; then
 	chmod 644 "spring-r$1.zip" || exit 1
-	mv "spring-r$1.zip" "/home/tvo/public_html/spring/executable/" || exit 1
+	mv "spring-r$1.zip" "~/public_html/spring/executable/" || exit 1
 else
 	echo "failed to zip spring.exe into spring-r$1.zip"
 	exitstatus=1
@@ -27,13 +27,13 @@ echo Done
 echo
 
 # Build installer & put online
-if installer/make_test_installer.sh "$1"; then
-	chmod 644 "installer/spring_r$1_nightly_gpl.exe" || exit 1
-	mv "installer/spring_r$1_nightly_gpl.exe" "/home/tvo/public_html/spring/installer/" || exit 1
-else
-	echo "failed to build installer"
-	exitstatus=1
-fi
+#if installer/make_test_installer.sh "$1"; then
+#	chmod 644 "installer/spring_r$1_nightly_gpl.exe" || exit 1
+#	mv "installer/spring_r$1_nightly_gpl.exe" "~/public_html/spring/installer/" || exit 1
+#else
+#	echo "failed to build installer"
+#	exitstatus=1
+#fi
 
 # Revert version so we don't get conflicts on next update when someone changes actual version.
 svn revert rts/Game/GameVersion.h || exit 1
