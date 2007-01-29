adce7a8bf72d605ec050f6b7a889c69b4b9b632a
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2007-01-29 20:01:28 +0000

    * Commit scripts by SeanHeron, fixed for XTA 8.1. * Set svn:eol-style to native on the scripts. * Rename LUA requestattack to LUA ordertroops, to be consistent with filename.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3358 37977431-3df6-0310-b722-df95706aa16b

--- game/startscripts/cmdrscript.lua
+++ game/startscripts/cmdrscript.lua
@@ -22,19 +22,19 @@ end
 function CmdrScript:SetupRegular()
     local p = TdfParser()
     p:LoadFile("gamedata\\sidedata.tdf")
-    
+
     local s0 = p:GetString("armcom", "side0\\commander");
     local s1 = p:GetString("corcom", "side1\\commander");
-    
+
     p = TdfParser()
     p:LoadFile(map.GetTDFName())
-    
+
     local x0, z0, x1, z1
     x0 = p:GetFloat(1000, "MAP\\TEAM0\\StartPosX")
     z0 = p:GetFloat(1000, "MAP\\TEAM0\\StartPosZ")
     x1 = p:GetFloat(1200, "MAP\\TEAM1\\StartPosX")
     z1 = p:GetFloat(1200, "MAP\\TEAM1\\StartPosZ")
-    
+
     units.Load(s0, float3(x0, 80, z0), 0, false)
     units.Load(s1, float3(x1, 80, z1), 1, false)
 end
