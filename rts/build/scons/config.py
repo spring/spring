6b9e626e636e233fa01cb6cd4d773d22d2ea1f91
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2007-04-08 13:02:43 +0000

    * Add experimental java bindings for unitsync, generated by AF.   Probably they still need a .java class file and it needs renaming.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3582 37977431-3df6-0310-b722-df95706aa16b

--- rts/build/scons/config.py
+++ rts/build/scons/config.py
@@ -279,6 +279,7 @@ def CheckHeadersAndLibraries(env, conf):
 
 	d += [Dependency(['SDL', 'SDL-1.1'], ['SDL/SDL.h', 'SDL11/SDL.h'])]
 	d += [Dependency(['python2.4', 'python24'], ['Python.h'])]
+	d += [Dependency([], ['jni.h'])]
 
 	if env['use_tcmalloc']:
 		d += [Dependency(['tcmalloc'], [])]
