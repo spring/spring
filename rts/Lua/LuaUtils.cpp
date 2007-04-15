3f510c867e560ba15d360ebf43950fa90526295d
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2007-04-15 15:49:01 +0000

    * Missing file
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3623 37977431-3df6-0310-b722-df95706aa16b

---
+++ rts/Lua/LuaUtils.cpp
@@ -0,0 +1,76 @@
+#include "StdAfx.h"
+// LuaUtils.cpp: implementation of the CLuaUtils class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "LuaUtils.h"
+#include <set>
+#include <cctype>
+
+extern "C" {
+	#include "lua.h"
+	#include "lualib.h"
+	#include "lauxlib.h"
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+static bool CopyPushData(lua_State* dst, lua_State* src, int index);
+static bool CopyPushTable(lua_State* dst, lua_State* src, int index);
+
+
+static bool CopyPushData(lua_State* dst, lua_State* src, int index)
+{
+	if (lua_isboolean(src, index)) {
+		lua_pushboolean(dst, lua_toboolean(src, index));
+	}
+	else if (lua_isnumber(src, index)) {
+		lua_pushnumber(dst, lua_tonumber(src, index));
+	}
+	else if (lua_isstring(src, index)) {
+		lua_pushstring(dst, lua_tostring(src, index));
+	}
+	else if (lua_istable(src, index)) {
+		CopyPushTable(dst, src, index);
+	}
+	else {
+		lua_pushnil(dst); // unhandled type
+		return false;
+	}
+	return true;
+}
+
+
+static bool CopyPushTable(lua_State* dst, lua_State* src, int index)
+{
+	lua_newtable(dst);
+	const int table = (index >= 0) ? index : (lua_gettop(src) - index + 1);
+	for (lua_pushnil(src); lua_next(src, table) != 0; lua_pop(src, 1)) {
+		CopyPushData(dst, src, -2); // copy the key
+		CopyPushData(dst, src, -1); // copy the value
+		lua_rawset(dst, -3);
+	}
+	return true;
+}
+
+
+int LuaUtils::CopyData(lua_State* dst, lua_State* src, int count)
+{
+	const int srcTop = lua_gettop(src);
+	if (srcTop < count) {
+		return 0;
+	}
+
+	const int startIndex = (srcTop - count + 1);
+	const int endIndex   = srcTop;
+	for (int i = startIndex; i <= endIndex; i++) {
+		CopyPushData(dst, src, i);
+	}
+
+	return count;
+}
+
+
+/******************************************************************************/
+/******************************************************************************/
