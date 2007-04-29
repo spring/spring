46525932548347cc1f1bc7e0a9a3808a9d7087b8
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2007-04-29 10:22:54 +0000

    * Removed the maximum 8 sides limit  (there's no real limit now)
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@3705 37977431-3df6-0310-b722-df95706aa16b

--- rts/Game/StartScripts/CommanderScript.cpp
+++ rts/Game/StartScripts/CommanderScript.cpp
@@ -4,6 +4,7 @@
 #include "TdfParser.h"
 #include <algorithm>
 #include <cctype>
+#include <map>
 #include "Game/Team.h"
 #include "Game/GameSetup.h"
 #include "Sim/Units/Unit.h"
@@ -12,69 +13,91 @@
 #include "Map/ReadMap.h"
 #include "mmgr.h"
 
+
 extern std::string stupidGlobalMapname;
+
 
 CCommanderScript::CCommanderScript()
 : CScript(std::string("Commanders"))
 {
 }
 
+
 CCommanderScript::~CCommanderScript(void)
 {
 }
 
+
 void CCommanderScript::Update(void)
 {
-	switch(gs->frameNum){
-	case 0:
-		if(gameSetup){
-			TdfParser p("gamedata/SIDEDATA.TDF");
-			for(int a=0;a<gs->activeTeams;++a){
-				if(!gameSetup->aiDlls[a].empty()){
-					if (gu->myPlayerNum == gs->Team (a)->leader)
-						globalAI->CreateGlobalAI(a,gameSetup->aiDlls[a].c_str());
-				}
+	if (gs->frameNum != 0) {
+		return;
+	}
 
-				for(int b=0;b<8;++b){					//loop over all sides
-					char sideText[50];
-					sprintf(sideText,"side%i",b);
-					if(p.SectionExist(sideText)){
-						string sideName = StringToLower(p.SGetValueDef("arm",string(sideText)+"\\name"));
-						if(sideName==gs->Team(a)->side){		//ok found the right side
-							string cmdType=p.SGetValueDef("armcom",string(sideText)+"\\commander");
+	if (gameSetup) {
+		TdfParser p("gamedata/SIDEDATA.TDF");
 
-							UnitDef* ud= unitDefHandler->GetUnitByName(cmdType);
-							ud->metalStorage=gs->Team(a)->metalStorage;			//make sure the cmd has the right amount of storage
-							ud->energyStorage=gs->Team(a)->energyStorage;
-
-							CUnit* u = unitLoader.LoadUnit(cmdType,gs->Team(a)->startPos,a,false,0,NULL);
-							gs->Team(a)->lineageRoot = u->id;
-							break;
-						}
-					}
-				}
-				gs->Team(a)->metalStorage=gs->Team(a)->metalStorage/2+20;		//now remove the preexisting storage except for a small amount
-				gs->Team(a)->energyStorage=gs->Team(a)->energyStorage/2+20;
-			}
-		} else {
-			TdfParser p("gamedata/SIDEDATA.TDF");
-			string s0=p.SGetValueDef("armcom","side0\\commander");
-			string s1=p.SGetValueDef("corcom","side1\\commander");
-
-			TdfParser p2;
-			CReadMap::OpenTDF (stupidGlobalMapname, p2);
-
-			float x0,x1,z0,z1;
-			p2.GetDef(x0,"1000","MAP\\TEAM0\\StartPosX");
-			p2.GetDef(z0,"1000","MAP\\TEAM0\\StartPosZ");
-			p2.GetDef(x1,"1200","MAP\\TEAM1\\StartPosX");
-			p2.GetDef(z1,"1200","MAP\\TEAM1\\StartPosZ");
-
-			unitLoader.LoadUnit(s0,float3(x0,80,z0),0,false,0,NULL);
-			unitLoader.LoadUnit(s1,float3(x1,80,z1),1,false,0,NULL);
-//			unitLoader.LoadUnit("armsam",float3(x0,80,z0)+float3(100,0,0),0,false,0,NULL);
+		// make a map of all side names  (assumes contiguous sections)
+		std::map<string, string> sideMap;
+		char sideText[64];
+		for (int side = 0;
+				 SNPRINTF(sideText, sizeof(sideText), "side%i", side),
+				 p.SectionExist(sideText); // the test
+				 side++) {
+			const string sideName =
+				StringToLower(p.SGetValueDef("arm", string(sideText) + "\\name"));
+			sideMap[sideName] = sideText;
 		}
-		break;
+
+		// setup the teams
+		for (int a = 0; a < gs->activeTeams; ++a) {
+			CTeam* team = gs->Team(a);
+
+			// create a GlobalAI if required
+			if (!gameSetup->aiDlls[a].empty() &&
+			    (gu->myPlayerNum == team->leader)) {
+				globalAI->CreateGlobalAI(a, gameSetup->aiDlls[a].c_str());
+			}
+			
+			std::map<string, string>::const_iterator it = sideMap.find(team->side);
+
+			if (it != sideMap.end()) {
+				const string& sideSection = it->second;
+				const string cmdrType =
+					p.SGetValueDef("armcom", sideSection + "\\commander");
+
+				// make sure the commander has the right amount of storage
+				UnitDef* ud = unitDefHandler->GetUnitByName(cmdrType);
+				ud->metalStorage  = team->metalStorage;
+				ud->energyStorage = team->energyStorage;
+
+				CUnit* unit =
+					unitLoader.LoadUnit(cmdrType, team->startPos, a, false, 0, NULL);
+
+				team->lineageRoot = unit->id;
+			}
+
+			// now remove the pre-existing storage except for a small amount
+			team->metalStorage  = (team->metalStorage  / 2) + 20;
+			team->energyStorage = (team->energyStorage / 2) + 20;
+		}
+	}
+	else {
+		TdfParser p("gamedata/SIDEDATA.TDF");
+		const string s0 = p.SGetValueDef("armcom", "side0\\commander");
+		const string s1 = p.SGetValueDef("corcom", "side1\\commander");
+
+		TdfParser p2;
+		CReadMap::OpenTDF(stupidGlobalMapname, p2);
+
+		float x0, x1, z0, z1;
+		p2.GetDef(x0, "1000","MAP\\TEAM0\\StartPosX");
+		p2.GetDef(z0, "1000","MAP\\TEAM0\\StartPosZ");
+		p2.GetDef(x1, "1200","MAP\\TEAM1\\StartPosX");
+		p2.GetDef(z1, "1200","MAP\\TEAM1\\StartPosZ");
+
+		unitLoader.LoadUnit(s0, float3(x0, 80.0f, z0), 0, false, 0, NULL);
+		unitLoader.LoadUnit(s1, float3(x1, 80.0f, z1), 1, false, 0, NULL);
 	}
 }
 
