96e7a8fae964cadbe216c3b7419b0b955c68537f
kloot <kloot@37977431-3df6-0310-b722-df95706aa16b>
2007-08-15 13:19:04 +0000

    don't say 'content error' if modrules.tdf isn't found
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@4204 37977431-3df6-0310-b722-df95706aa16b

--- rts/Sim/ModInfo.cpp
+++ rts/Sim/ModInfo.cpp
@@ -18,53 +18,50 @@ CModInfo::CModInfo(const char* modname)
 	try {
 		TdfParser tdfparser("gamedata/particles.tdf");
 		tdfparser.GetDef(allowTeamColors, "1", "nanospray\\allow_team_colours");
-	}
-	catch(content_error)
-	{
+	} catch(content_error) {
 		allowTeamColors = true;
 	}
-	if(allowTeamColors) {
+	if (allowTeamColors) {
 		// Load the users preference for team coloured nanospray
 		gu->teamNanospray = !!configHandler.GetInt ("TeamNanoSpray", 0);
 	}
 
+
 	// Get the reclaim options for the mod
 	multiReclaim = 0;
 	reclaimMethod = 1;
+
 	// See if the mod overrides the defaults:
-	try
-	{
+	try {
 		TdfParser reclaimOptions("gamedata/modrules.tdf");
 		multiReclaim = atoi(reclaimOptions.SGetValueDef("0", "RECLAIM\\MultiReclaim").c_str());
 		reclaimMethod = atoi(reclaimOptions.SGetValueDef("1", "RECLAIM\\ReclaimMethod").c_str());
-	}
-	catch(content_error) // If the modrules.tdf isnt found
-	{
+	} catch(content_error) {
+		logOutput << "Mod uses default reclaim rules" << "\n";
+		// If the modrules.tdf isnt found
 		// We already set the defaults so we should be able to ignore this
 		// Other optional mod rules MUST set their defaults...
 	}
-	//Get the transportability options for the mod
+
+	// Get the transportability options for the mod
 	transportGround = 1;
 	transportHover = 0;
 	transportShip = 0;
 	transportAir = 0;
+
 	// See if the mod overrides the defaults:
-	try
-	{
+	try {
 		TdfParser transportOptions("gamedata/modrules.tdf");
 		transportGround = atoi(transportOptions.SGetValueDef("1", "TRANSPORTABILITY\\TransportGround").c_str());
 		transportHover = atoi(transportOptions.SGetValueDef("0", "TRANSPORTABILITY\\TransportHover").c_str());
 		transportShip = atoi(transportOptions.SGetValueDef("0", "TRANSPORTABILITY\\TransportShip").c_str());
 		transportAir = atoi(transportOptions.SGetValueDef("0", "TRANSPORTABILITY\\TransportAir").c_str());
-		logOutput << "TransportHover: " << transportHover << "\n";
-	}
-	catch(content_error) // If the modrules.tdf isnt found
-	{
-		logOutput << "Content error:" << "\n";
+	} catch(content_error) {
+		logOutput << "Mod uses default transportability rules" << "\n";
+		// If the modrules.tdf isnt found
 		// We already set the defaults so we should be able to ignore this
 		// Other optional mod rules MUST set their defaults...
 	}
-
 }
 
-CModInfo::~CModInfo(){}
+CModInfo::~CModInfo() {}
