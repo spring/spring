4365d9233f552f9b76131cd2bdc2fdab5847e334
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2007-09-03 17:38:26 +0000

    * Moved the default explosion aliases into explosion_alias.lua * Removed explosion_alias.tdf from springcontent
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@4302 37977431-3df6-0310-b722-df95706aa16b

--- installer/builddata/springcontent/gamedata/explosion_alias.lua
+++ installer/builddata/springcontent/gamedata/explosion_alias.lua
@@ -11,14 +11,65 @@
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+-- Specifies a list of alias names for spring C++ classes, allows the aliases
+-- to have easier names and the C++ names to be changed independent of mods.
+-- Projectile class names can change in the future, so mods should not change
+-- this file. It is distributed with spring.
+
+-- Syntax:  <alias> = <real name>
+
 
 local TDF = VFS.Include('gamedata/parse_tdf.lua')
 
 local aliases, err = TDF.Parse('gamedata/explosion_alias.tdf')
-if (aliases == nil) then
-  error('Error parsing explosion_alias.tdf: ' .. err)
-end
 
+--------------------------------------------------------------------------------
+
+
+if (aliases == nil) then
+  -- load the defaults
+  aliases = {
+    generators = {
+      std    = 'CStdExplosionGenerator',
+      custom = 'CCustomExplosionGenerator',
+    },
+    projectiles = {
+      beamlaser             = 'CBeamLaserProjectile',
+      bubble                = 'CBubbleProjectile',
+      dirt                  = 'CDirtProjectile',
+      emg                   = 'CEmgProjectile',
+      expl                  = 'CExplosiveProjectile',
+      explspike             = 'CExploSpikeProjectile',
+      fireball              = 'CFireBallProjectile',
+      fire                  = 'CFireProjectile',
+      flame                 = 'CFlameProjectile',
+      flare                 = 'CFlareProjectile',
+      geosquare             = 'CGeoSquareProjectile',
+      lighting              = 'CLightingProjectile',
+      missile               = 'CMissileProjectile',
+      muzzleflame           = 'CMuzzleFlame',
+      piece                 = 'CPieceProjectile',
+      shieldpart            = 'CShieldPartProjectile',
+      smoke                 = 'CSmokeProjectile',
+      smoke2                = 'CSmokeProjectile2',
+      smoketrail            = 'CSmokeTrailProjectile',
+      spherepart            = 'CSpherePartProjectile',
+      starburst             = 'CStarburstProjectile',
+      torpedo               = 'CTorpedoProjectile',
+      tracer                = 'CTracerProjectile',
+      wake                  = 'CWakeProjectile',
+      heatcloud             = 'CHeatCloudProjectile',
+      gfx                   = 'CGfxProjectile',
+      SimpleParticleSystem  = 'CSimpleParticleSystem',
+      BitmapMuzzleFlame     = 'CBitmapMuzzleFlame',
+      DelaySpawner          = 'CExpGenSpawner',
+      ExplosionSphere       = 'CSpherePartSpawner',
+      ExplosionSpike        = 'CExploSpikeProjectile',
+      SimpleParticleSpawner = 'CSphereParticleSpawner',
+      SimpleGroundFlash     = 'CSimpleGroundFlash',
+    },
+  }
+end
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
