spring/spring

commit 2618f2ccd339cc79a1ef88d0c20f316998b0ee37

* removed unused code from CGame * used sinf() instead of sin() for speed reasons
git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@4896 37977431-3df6-0310-b722-df95706aa16b

rts/Game/Game.cpp
@@ -2044,7 +2044,6 @@ bool CGame::Draw()
 		gu->timeOffset=0;
 		lastUpdate = SDL_GetTicks();
 	}
-	std::string tempstring;
 
 	ph->UpdateTextures();
 
@@ -2308,8 +2307,7 @@ bool CGame::Draw()
 	glEnable(GL_DEPTH_TEST );
 	glLoadIdentity();
 
-	Uint64 start;
-	start = SDL_GetTicks();
+	unsigned start = SDL_GetTicks();
 	gu->lastFrameTime = (float)(start - lastMoveUpdate)/1000.f;
 	lastMoveUpdate = start;
 
@@ -2511,12 +2509,7 @@ void CGame::SimFrame()
 	}
 //	CPathFinder::Instance()->Update();
 
-	Uint64 stopPhysics;
-	stopPhysics = SDL_GetTicks();
-
 	END_TIME_PROFILE("Sim time")
-
-	lastUpdate=stopPhysics;
 
 #ifdef DIRECT_CONTROL_ALLOWED
 
@@ -3270,7 +3263,6 @@ void CGame::UpdateUI()
 			disableTracker = true;
 		}
 
-//		CCameraController* camCtrl = camCtrl;
 		if (disableTracker && camCtrl->DisableTrackingByKey()) {
 			unitTracker.Disable();
 		}
@@ -3340,42 +3332,6 @@ void CGame::UpdateUI()
 		ignoreChar=0;
 	}
 }
-
-
-/*
-unsigned int crcTable[256];
-
-unsigned int Calculate_CRC(char *first,char *last)
-{  int length = ((int) (last - first));  unsigned int crc = 0xFFFFFFFF;
-  for(int i=0; i<length; i++)
-    {
-    crc = (crc>>8) & 0x00FFFFFF ^ crcTable[(crc^first[i]) & 0xFF];
-    }  return(crc);}
-
-
-
-void InitCRCTable()
-{
-  unsigned int crc;
-  for (int i=0; i<256; i++)
-    {
-    crc = i;
-    for(int j=8; j>0; j--)
-      {
-      if (crc&1)
-        {
-        crc = (crc >> 1) ^ 0xEDB88320;
-        }
-      else
-        {
-        crc >>= 1;
-        }
-     }
-     crcTable[i] = crc;
-   }
-  }
-
-  */
 
 void CGame::MakeMemDump(void)
 {

rts/Game/Camera.cpp
@@ -120,7 +120,6 @@ static bool CalculateInverse4x4(const double m[4][4], double inv[4][4])
 	return true;
 }
 
-
 void CCamera::Update(bool freeze)
 {
 	pos2 = pos;
@@ -128,11 +127,12 @@ void CCamera::Update(bool freeze)
 
 	right = forward.cross(up);
 	right.Normalize();
-
+	
 	up = right.cross(forward);
 	up.Normalize();
 
-	const float viewx = (float)tanf(float(gu->viewSizeX)/gu->viewSizeY * halfFov);
+	const float aspect = (float) gu->viewSizeX / (float) gu->viewSizeY;
+	const float viewx = aspect * tanHalfFov;
 	const float viewy = tanHalfFov;
 
 	if (gu->viewSizeY <= 0) {
@@ -142,13 +142,16 @@ void CCamera::Update(bool freeze)
 		lppScale = span / (float)gu->viewSizeY;
 	}
 
-	top    = (-forward * viewy) + up;
+	const float3 forwardy = (-forward * viewy);
+	top    = forwardy + up;
 	top.Normalize();
-	bottom = (-forward * viewy) - up;
+	bottom = forwardy - up;
 	bottom.Normalize();
-	rightside = (-forward * viewx) + right;
+	
+	const float3 forwardx = (-forward * viewx);
+	rightside = forwardx + right;
 	rightside.Normalize();
-	leftside  = (-forward * viewx) - right;
+	leftside  = forwardx - right;
 	leftside.Normalize();
 
 	if (!freeze) {
@@ -166,7 +169,6 @@ void CCamera::Update(bool freeze)
 
 	const float gndHeight = ground->GetHeight(pos.x, pos.z);
 	const float rangemod = 1.0f + max(0.0f, pos.y - gndHeight - 500.0f) * 0.0003f;
-	const float aspect = (GLfloat) gu->viewSizeX / (GLfloat) gu->viewSizeY;
 	const float zNear = (NEAR_PLANE * rangemod);
 	gu->viewRange = MAX_VIEW_RANGE * rangemod;
 
@@ -245,7 +247,6 @@ bool CCamera::InView(const float3& mins, const float3& maxs)
 	return false;
 }
 
-
 bool CCamera::InView(const float3 &p, float radius)
 {
 	const float3 t = (p - pos);
@@ -271,9 +272,9 @@ bool CCamera::InView(const float3 &p, float radius)
 
 void CCamera::UpdateForward()
 {
-	forward.z = (float)(cos(camera->rot.y) * cos(camera->rot.x));
-	forward.x = (float)(sin(camera->rot.y) * cos(camera->rot.x));
-	forward.y = (float)(sin(camera->rot.x));
+	forward.z = cosf(rot.y) * cosf(rot.x);
+	forward.x = sinf(rot.y) * cosf(rot.x);
+	forward.y = sinf(rot.x);
 	forward.Normalize();
 }
 
@@ -282,7 +283,7 @@ float3 CCamera::CalcPixelDir(int x, int y)
 {
 	float dx = float(x-gu->viewPosX-gu->viewSizeX/2)/gu->viewSizeY * tanHalfFov * 2;
 	float dy = float(y-gu->viewSizeY/2)/gu->viewSizeY * tanHalfFov * 2;
-	float3 dir = camera->forward-camera->up*dy+camera->right*dx;
+	float3 dir = forward - up * dy + right * dx;
 	dir.Normalize();
 	return dir;
 }
@@ -319,10 +320,8 @@ inline void CCamera::myGluPerspective(float aspect, float zNear, float zFar) {
 
 inline void CCamera::myGluLookAt(const float3& eye, const float3& center, const float3& up) {
 	float3 f = (center - eye).Normalize();
-	float3 u = float3(up).Normalize();
-	float3 s = f.cross(u);
-
-	u = s.cross(f);
+	float3 s = f.cross(up);
+	float3 u = s.cross(f);
 
 	modelview[ 0] =  s.x;
 	modelview[ 1] =  u.x;

commit 85f868058f812380feeec4d1880d0633dd4a6eb9

* fix the messed-up simtimes caused by r4896
git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@4897 37977431-3df6-0310-b722-df95706aa16b

rts/Game/Game.cpp
@@ -2511,6 +2511,8 @@ void CGame::SimFrame()
 
 	END_TIME_PROFILE("Sim time")
 
+	lastUpdate = SDL_GetTicks();
+
 #ifdef DIRECT_CONTROL_ALLOWED
 
 	for(int a=0;a<gs->activePlayers;++a){
@@ -3332,6 +3334,8 @@ void CGame::UpdateUI()
 		ignoreChar=0;
 	}
 }
+
+
 
 void CGame::MakeMemDump(void)
 {