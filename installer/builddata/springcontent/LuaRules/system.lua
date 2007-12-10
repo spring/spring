ae85c567630270b0ffd33b7b8388349fd86dc146
KDR_11k <KDR_11k@37977431-3df6-0310-b722-df95706aa16b>
2007-12-10 21:56:51 +0000

    *added the gadget handler to springcontent.sdz
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@4979 37977431-3df6-0310-b722-df95706aa16b

---
+++ installer/builddata/springcontent/LuaRules/system.lua
@@ -0,0 +1,108 @@
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--
+--  file:    system.lua
+--  brief:   defines the global entries placed into a gadget's table
+--  author:  Dave Rodgers
+--
+--  Copyright (C) 2007.
+--  Licensed under the terms of the GNU GPL, v2 or later.
+--
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if (System == nil) then
+
+  System = {
+    --
+    --  Custom Spring tables
+    --
+    Script = Script,
+    Spring = Spring,
+    Game = Game,
+    gl = gl,
+    GL = GL,
+    CMD = CMD,
+    CMDTYPE = CMDTYPE,
+    VFS = VFS,
+
+    UnitDefs     = UnitDefs,
+    UnitDefNames = UnitDefNames,
+    FeatureDefs  = FeatureDefs,
+    WeaponDefs   = WeaponDefs,
+
+    --
+    -- Custom Constants
+    --
+    COBSCALE = COBSCALE,
+
+    --
+    --  Synced Utilities
+    --
+    CallAsTeam = CallAsTeam,
+    SendToUnsynced = SendToUnsynced,
+
+    --
+    --  Unsynced Utilities
+    --
+    SYNCED  = SYNCED,
+    snext   = snext,
+    spairs  = spairs,
+    sipairs = sipairs,
+    
+    --
+    --  Standard libraries
+    --
+    io = io,
+    os = os,
+    math = math,
+    debug = debug,
+    table = table,
+    string = string,
+    package = package,
+    coroutine = coroutine,
+    
+    --  
+    --  Standard functions and variables
+    --
+    assert         = assert,
+    error          = error,
+
+    print          = print,
+    
+    next           = next,
+    pairs          = pairs,
+    ipairs         = ipairs,
+
+    tonumber       = tonumber,
+    tostring       = tostring,
+    type           = type,
+
+    collectgarbage = collectgarbage,
+    gcinfo         = gcinfo,
+
+    unpack         = unpack,
+    select         = select,
+    dofile         = dofile,
+    loadfile       = loadfile,
+    loadlib        = loadlib,
+    loadstring     = loadstring,
+    require        = require,
+    
+    getmetatable   = getmetatable,
+    setmetatable   = setmetatable,
+
+    rawequal       = rawequal,
+    rawget         = rawget,
+    rawset         = rawset,
+
+    getfenv        = getfenv,
+    setfenv        = setfenv,
+
+    pcall          = pcall,
+    xpcall         = xpcall,
+
+    _VERSION       = _VERSION
+  }
+
+end
