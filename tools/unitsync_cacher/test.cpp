828222ba3f296a6cf0df511e722ffcd430010cde
tnowell <tnowell@37977431-3df6-0310-b722-df95706aa16b>
2007-12-27 16:36:34 +0000

    - moved cache files to the lobby folder - data is cached to maps.js index.js and modname.version.js each holding their own respective data of the necessary detail - formatting corrections - added more data especially for mods
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5136 37977431-3df6-0310-b722-df95706aa16b

--- tools/unitsync_cacher/test.cpp
+++ tools/unitsync_cacher/test.cpp
@@ -151,6 +151,9 @@ DLL_EXPORT void			__stdcall SetSpringConfigFloat( const char* name, const float 
 
 static void DisplayOptions(int optionCount);
 static Array GetOptions(int optionCount);
+static void CacheIndex();
+
+static void CacheMaps();
 
 
 //http://techjude.blogspot.com/2006/11/neat-tostring-for-c.html
@@ -179,120 +182,7 @@ int main(int argc, char** argv)
   printf("MOD = %s\n", mod.c_str());
 */
 
-  Object json;
-
-  Init(false, 0);
-
-  string springversion = GetSpringVersion();
-  json.push_back(Pair("springversion",springversion));
-
-  printf("GetSpringVersion() = %s\n", GetSpringVersion());
-
-  // map names
-  printf("  MAPS\n");
-
-  
-  const int mapCount = GetMapCount();
-  json.push_back(Pair("mapcount",mapCount));
-
-  Object maps;
-  for (int i = 0; i < mapCount; i++) {
-    const string mapName = GetMapName(i);
-	maps.push_back(Pair(toString(i),mapName));
-    printf("    [map %3i]   %s\n", i, mapName.c_str());
-  }
-  json.push_back(Pair("mapIDs",maps));
-
-  
-
-  Object mods;
-  // mod names
-  printf("  MODS\n");
-  const int modCount = GetPrimaryModCount();
-
-  json.push_back(Pair("modcount",modCount));
-
-  UnInit();
-
-	for (int i = 0; i < modCount; i++) {
-		printf("initializing\n");
-
-		Init(false, 0);
-
-		printf("initialized\n");
-
-    
-		string modName      = GetPrimaryModName(i);
-		string modShortName = GetPrimaryModShortName(i);
-		string modVersion   = GetPrimaryModVersion(i);
-		string modMutator   = GetPrimaryModMutator(i);
-		string modArchive   = GetPrimaryModArchive(i);
-		string modGame      = GetPrimaryModGame(i);
-		string modShortGame = GetPrimaryModShortGame(i);
-		
-		printf("adding archives\n");
-		AddAllArchives(modArchive.c_str());
-		printf("archives added\n");
-
-		Object m;
-
-		m.push_back(Pair("name",		modName));
-		m.push_back(Pair("shortname",	modShortName));
-		m.push_back(Pair("version",		modVersion));
-		m.push_back(Pair("mutator",		modMutator));
-		m.push_back(Pair("archive",		modArchive));
-		m.push_back(Pair("game",		modGame));
-		m.push_back(Pair("shortgame",	modShortGame));
-
-		const int modOptCount = GetModOptionCount();
-		DisplayOptions(modOptCount);
-		m.push_back(Pair("luaoptions",GetOptions(modOptCount)));
-
-		while (true) {
-			int left = ProcessUnitsNoChecksum();
-			if (left <= 0) {
-				break;
-			}
-		}
-
-		printf("processed units\n");
-
-		const int unitCount = GetUnitCount();
-		
-		m.push_back(Pair("unitcount",	unitCount));
-
-		Object units;
-
-		for (int j = 0; j < unitCount; j++) {
-
-			
-			string unitName     = GetUnitName(j);
-			string unitFullName = GetFullUnitName(j);
-		
-			units.push_back(Pair(unitName,unitFullName));
-			
-			printf("    [unit %3i]   %-16s  <%s>\n", j,
-				   unitName.c_str(), unitFullName.c_str());
-		}
-
-		m.push_back(Pair("units",units));
-
-		mods.push_back(Pair(toString(i),m));
-
-
-		printf("    [mod %3i]   %-32s  <%s> %s %s %s\n", i,
-			   modName.c_str(), modArchive.c_str(),
-			   modShortName.c_str(), modVersion.c_str(), modMutator.c_str());
-
-		UnInit();
-		printf("uninitialized\n");
-	}
-
-	json.push_back(Pair("mods",mods));
-
-	ofstream o("cache.js");
-	write_formatted( json, o );
-	o.close();
+	CacheIndex();
 
   // load the mod archives
   /*AddAllArchives(mod.c_str());
@@ -456,6 +346,170 @@ static Array GetOptions(int optionCount)
 	return a;
 }
 
+static void CacheMaps(){
+	//
+	Object maps;
+	const int mapCount = GetMapCount();
+	maps.push_back(Pair("mapcount",mapCount));
+
+	
+	Object o2;
+	for (int i = 0; i < mapCount; i++) {
+		Object o;
+		const string mapName = GetMapName(i);
+		o.push_back(Pair("mapname",mapName));
+
+		o2.push_back(Pair(mapName,o));
+		printf("    [map %3i]   %s\n", i, mapName.c_str());
+	}
+
+	maps.push_back(Pair("mapdata",o2));
+	
+	ofstream o("lobby/maps.js");
+	write_formatted( maps, o );
+	o.close();
+}
+
+static void CacheIndex(){
+	//
+	Object json;
+
+	Init(false, 0);
+
+	string springversion = GetSpringVersion();
+	json.push_back(Pair("springversion",springversion));
+
+	printf("GetSpringVersion() = %s\n", GetSpringVersion());
+
+	// map names
+	printf("  MAPS\n");
+
+  
+	CacheMaps();
+
+  
+
+	Object mods;
+	// mod names
+	printf("  MODS\n");
+	const int modCount = GetPrimaryModCount();
+
+	json.push_back(Pair("modcount",modCount));
+
+	UnInit();
+
+	for (int i = 0; i < modCount; i++) {
+		printf("initializing\n");
+
+		Init(false, 0);
+
+		printf("initialized\n");
+
+    
+		string modName      = GetPrimaryModName(i);
+		string modShortName = GetPrimaryModShortName(i);
+		string modVersion   = GetPrimaryModVersion(i);
+		string modMutator   = GetPrimaryModMutator(i);
+		string modArchive   = GetPrimaryModArchive(i);
+		string modGame      = GetPrimaryModGame(i);
+		string modShortGame = GetPrimaryModShortGame(i);
+		
+		printf("adding archives\n");
+		AddAllArchives(modArchive.c_str());
+		printf("archives added\n");
+
+		Object m;
+
+		m.push_back(Pair("name",		modName));
+		m.push_back(Pair("shortname",	modShortName));
+		m.push_back(Pair("version",		modVersion));
+		m.push_back(Pair("mutator",		modMutator));
+		m.push_back(Pair("archive",		modArchive));
+		m.push_back(Pair("game",		modGame));
+		m.push_back(Pair("shortgame",	modShortGame));
+
+		m.push_back(Pair("crcchecksum",	(int)GetPrimaryModChecksum(i)));
+
+		while (true) {
+			int left = ProcessUnitsNoChecksum();
+			if (left <= 0) {
+				break;
+			}
+		}
+
+		printf("processed units\n");
+
+		
+		
+		Array sides;
+		int sidecount = GetSideCount();
+
+		const int unitCount = GetUnitCount();
+		
+		m.push_back(Pair("unitcount",	unitCount));
+
+		mods.push_back(Pair(modName,m));
+
+		for(int k = 0; k < sidecount; k++){
+			string sn = GetSideName(k);
+			sides.push_back(sn);
+		}
+		
+		const int modOptCount = GetModOptionCount();
+		Array options = GetOptions(modOptCount);
+		m.push_back(Pair("luaoptions",options));
+
+		
+
+		m.push_back(Pair("sides",sides));
+
+		
+
+		Object units;
+
+		for (int j = 0; j < unitCount; j++) {
+
+			
+			string unitName     = GetUnitName(j);
+			string unitFullName = GetFullUnitName(j);
+		
+			units.push_back(Pair(unitName,unitFullName));
+			
+			printf("    [unit %3i]   %-16s  <%s>\n", j,
+				   unitName.c_str(), unitFullName.c_str());
+		}
+
+		m.push_back(Pair("units",units));
+
+		
+		{
+			// keep 'o' out of scope
+			string f = "lobby/";
+			f += modName+".";
+			f += modVersion+".js";
+			ofstream o(f.c_str());
+			write_formatted( m, o );
+			o.close();
+		}
+
+
+		printf("    [mod %3i]   %-32s  <%s> %s %s %s\n", i,
+			   modName.c_str(), modArchive.c_str(),
+			   modShortName.c_str(), modVersion.c_str(), modMutator.c_str());
+
+		UnInit();
+		printf("uninitialized\n");
+	}
+
+	json.push_back(Pair("mods",mods));
+
+	{
+		// keep 'o' out of scope
+		ofstream o("lobby/index.js");
+		write_formatted( json, o );
+		o.close();
+	}
+}
 
 /******************************************************************************/
 /******************************************************************************/
