fdd3e7df0e0f5dbe59ebdfe8cfe7ca62c44c59f6
Auswaschbar <Auswaschbar@37977431-3df6-0310-b722-df95706aa16b>
2007-12-28 18:16:42 +0000

    * AIs can't set startpos outside their box anymore
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5147 37977431-3df6-0310-b722-df95706aa16b

--- rts/ExternalAI/AICallback.h
+++ rts/ExternalAI/AICallback.h
@@ -19,7 +19,7 @@ public:
 
 	void verify ();
 
-	void SendStartPos(bool ready, const float3& pos);
+	void SendStartPos(bool ready, float3 pos);
 	void SendTextMsg(const char* text, int zone);
 	void SetLastMsgPos(float3 pos);
 	void AddNotification(float3 pos, float3 color, float alpha);
