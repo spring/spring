26e9d6b141a4ffd54e9e6b0b4039791eec66b57b
Auswaschbar <Auswaschbar@37977431-3df6-0310-b722-df95706aa16b>
2008-01-02 18:16:53 +0000

    Added nice debugging tool: It reads a demofile and prints out the networc traffic in human readable form.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5219 37977431-3df6-0310-b722-df95706aa16b

---
+++ tools/DemoAnalyser/main.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <iostream>
+#include <limits>
+
+#include "../../rts/System/DemoReader.h"
+#include "System/Platform/FileSystem.h"
+#include "Game/GameSetupData.h"
+#include "BaseNetProtocol.h"
+
+CGameSetupData* gameSetup = 0;
+using namespace std;
+
+/*
+Usage:
+Start with the full! path to the demofile as the only argument
+
+Please note that not all NETMSG's are implemented, expand if needed.
+*/
+
+int main (int argc, char* argv[])
+{
+	FileSystemHandler::Cleanup();
+	FileSystemHandler::Initialize(false);
+
+	CDemoReader reader(string(argv[1]), 0.0f);
+	DemoFileHeader header = reader.GetFileHeader();
+	
+	unsigned unknownMessages = 0;
+	while (!reader.ReachedEnd())
+	{
+		const unsigned length= 40000;
+		unsigned char buffer[40000];
+		unsigned ret = 0;
+		ret = reader.GetData(buffer, length, 3.40282347e+38f);
+		if (ret == 0)
+			continue;
+		
+		switch ((unsigned char)buffer[0])
+		{
+			case NETMSG_ATTEMPTCONNECT:
+				cout << "ATTEMPTCONNECT: Playernum: " << (unsigned)buffer[1] << " Version: " << (unsigned)buffer[2] << endl;
+				break;
+			case NETMSG_PLAYERNAME:
+				cout << "PLAYERNAME: Playernum: " << (unsigned)buffer[2] << " Name: " << buffer+3 << endl;
+				break;
+			case NETMSG_SETPLAYERNUM:
+				cout << "SETPLAYERNUM: Playernum: " << (unsigned)buffer[1] << endl;
+				break;
+			case NETMSG_QUIT:
+				cout << "QUIT" << endl;
+				break;
+			case NETMSG_STARTPLAYING:
+				cout << "STARTPLAYING" << endl;
+				break;
+			case NETMSG_STARTPOS:
+				cout << "STARTPOS: Playernum: " << (unsigned)buffer[1] << " Team: " << (unsigned)buffer[2] << " Readyness: " << (unsigned)buffer[3] << endl;
+				break;
+			case NETMSG_SYSTEMMSG:
+				cout << "SYSTEMMSG: Player: " << (unsigned)buffer[2] << " Msg: " << (char*)(buffer+3) << endl;
+				break;
+			default:
+				unknownMessages++;
+		}
+	}
+	cout << endl << "There had been " << unknownMessages << " unknown NETMSGs" << endl;
+	FileSystemHandler::Cleanup();
+	return 0;
+}
