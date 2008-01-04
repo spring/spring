df62e700aba4f268fd42d30edea37395c7b9925d
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-01-04 21:01:18 +0000

    * Disabled the widget by default
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5266 37977431-3df6-0310-b722-df95706aa16b

--- game/LuaUI/Widgets/minimap_startbox.lua
+++ game/LuaUI/Widgets/minimap_startbox.lua
@@ -19,7 +19,7 @@ function widget:GetInfo()
     date      = "Mar 17, 2007",
     license   = "GNU GPL, v2 or later",
     layer     = 0,
-    enabled   = true  --  loaded by default?
+    enabled   = false  --  loaded by default?
   }
 end
 
