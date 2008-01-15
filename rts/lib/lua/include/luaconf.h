63fdfb67ef7f4b54e737f4bfb2e204156b560c29
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-01-15 16:43:35 +0000

    * Fixed LUAI_UACNUMBER and LUA_NUMBER_SCAN
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5331 37977431-3df6-0310-b722-df95706aa16b

--- rts/lib/lua/include/luaconf.h
+++ rts/lib/lua/include/luaconf.h
@@ -510,7 +510,8 @@
 @@ LUAI_UACNUMBER is the result of an 'usual argument conversion'
 @* over a number.
 */
-#define LUAI_UACNUMBER	double
+//SPRING#define LUAI_UACNUMBER	double
+#define LUAI_UACNUMBER	float
 
 
 /*
@@ -520,7 +521,8 @@
 @@ LUAI_MAXNUMBER2STR is maximum size of previous conversion.
 @@ lua_str2number converts a string to a number.
 */
-#define LUA_NUMBER_SCAN		"%lf"
+//SPRING#define LUA_NUMBER_SCAN		"%lf"
+#define LUA_NUMBER_SCAN		"%f"
 #define LUA_NUMBER_FMT		"%.14g"
 #define lua_number2str(s,n)	sprintf((s), LUA_NUMBER_FMT, (n))
 #define LUAI_MAXNUMBER2STR	32 /* 16 digits, sign, point, and \0 */
@@ -584,6 +586,7 @@ union luai_Cast { double l_d; long l_l; };
 #define lua_number2integer(i,d)	((i)=(lua_Integer)(d))
 
 #endif
+
 
 /* }================================================================== */
 
