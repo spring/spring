6cfb8ec2d0c2d5b56acb2e85c9252012bc19ee77
Auswaschbar <Auswaschbar@37977431-3df6-0310-b722-df95706aa16b>
2008-01-27 19:10:27 +0000

    * CGameServer uses simplified gamesetup-class * server detects when there are no players left and exits * fixed critical bug in TEAMMSG_JOIN_TEAM
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5409 37977431-3df6-0310-b722-df95706aa16b

--- tools/DedicatedServer/main.cpp
+++ tools/DedicatedServer/main.cpp
@@ -2,15 +2,17 @@
 #include "GameSetupData.h"
 #include "System/Platform/FileSystem.h"
 
-CGameSetupData* gameSetup = 0;
 
 int main(int argc, char *argv[])
 {
 	FileSystemHandler::Cleanup();
 	FileSystemHandler::Initialize(false);
 	
-	CGameServer* temp = new CGameServer(8452, "Altored_Divide.smf", "Balanced Annihilation V5.91", "Cmds 1000 res");
-	sleep(60);
+	CGameSetupData* gameSetup = 0;
+	
+	CGameServer* temp = new CGameServer(8452, "Altored_Divide.smf", "Balanced Annihilation V6.0", "Cmds 1000 res", gameSetup);
+	while (!temp->HasFinished())
+		sleep(1);
 	delete temp;
 	
 	FileSystemHandler::Cleanup();
