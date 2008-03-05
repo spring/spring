e159817a4301fd810e79e093066f9513d2ec463f
Auswaschbar <Auswaschbar@37977431-3df6-0310-b722-df95706aa16b>
2008-03-05 12:29:45 +0000

    * fixed the random number generator for producing randomness in a specific range
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5560 37977431-3df6-0310-b722-df95706aa16b

--- rts/System/UnsyncedRNG.h
+++ rts/System/UnsyncedRNG.h
@@ -7,7 +7,12 @@ class UnsyncedRNG
 public:
 	UnsyncedRNG();
 	void Seed(unsigned seed);
+	
+	/** @brief returns a random unsigned integer in the range [0, (UINT_MAX & 0x7FFF)) */
 	unsigned operator()();
+	
+	/** @brief returns a random integer in the range [0, (INT_MAX & 0x7FFF)) */
+	int RandInt();
 	
 	/** @brief returns a random number in the range [0, n) */
 	int operator()(int n);
