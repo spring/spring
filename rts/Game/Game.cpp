9461e940cad9a952d6c07d6513ce9051cb107c8d
kloot <kloot@37977431-3df6-0310-b722-df95706aa16b>
2008-03-09 20:48:09 +0000

    broadcast time(0) as the seed for the synced RNG
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5574 37977431-3df6-0310-b722-df95706aa16b

--- rts/Game/Game.cpp
+++ rts/Game/Game.cpp
@@ -1908,7 +1908,7 @@ bool CGame::Update()
 			chatting = false;
 			userWriting = false;
 			writingPos = 0;
-			net->SendRandSeed(gs->randSeed);
+			net->SendRandSeed(time(0));
 			net->SendStartPlaying(0);
 		}
 	}
