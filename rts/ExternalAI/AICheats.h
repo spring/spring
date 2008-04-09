8e464d02ab38f11e0d5a44ee1a36ea248ba58db2
kloot <kloot@37977431-3df6-0310-b722-df95706aa16b>
2008-04-09 22:26:14 +0000

    add the new callbacks to the AI cheat interface as well
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5691 37977431-3df6-0310-b722-df95706aa16b

--- rts/ExternalAI/AICheats.h
+++ rts/ExternalAI/AICheats.h
@@ -4,49 +4,49 @@
 #include "IAICheats.h"
 class CGlobalAI;
 
-class CAICheats :
-	public IAICheats
+class CAICheats: public IAICheats
 {
 	CGlobalAI* ai;
 public:
 	CAICheats(CGlobalAI* ai);
 	~CAICheats(void);
 
-	void SetMyHandicap(float handicap);			//same as the value in the game setup file (currently gives bonus on collected resources)
+	void SetMyHandicap(float handicap);
 
-	void GiveMeMetal(float amount);					//give selected amount of metal
-	void GiveMeEnergy(float amount);				//give selected amount of energy;
+	void GiveMeMetal(float amount);
+	void GiveMeEnergy(float amount);
 
-	int CreateUnit(const char* name,float3 pos);		//creates a unit with the selected name at pos
+	int CreateUnit(const char* name, float3 pos);
 
-	//the following commands works exactly like those in the standard interface except that they dont do any los checks
-	const UnitDef* GetUnitDef(int unitid);	//this returns the units unitdef struct from which you can read all the statistics of the unit, dont try to change any values in it, dont use this if you dont have to risk of changes in it
-	float3 GetUnitPos(int unitid);				//note that x and z are the horizontal axises while y represent height
-	int GetEnemyUnits(int *units);					//returns all known enemy units
-	int GetEnemyUnits(int *units,const float3& pos,float radius); //returns all known enemy units within radius from pos
+	const UnitDef* GetUnitDef(int unitid);
+	float3 GetUnitPos(int unitid);
+	int GetEnemyUnits(int* units);
+	int GetEnemyUnits(int* units, const float3& pos, float radius);
+	int GetNeutralUnits(int* units);
+	int GetNeutralUnits(int* units, const float3& pos, float radius);
 
-	int GetUnitTeam (int unitid);
-	int GetUnitAllyTeam (int unitid);
-	float GetUnitHealth (int unitid);			//the units current health
-	float GetUnitMaxHealth (int unitid);		//the units max health
-	float GetUnitPower(int unitid);				//sort of the measure of the units overall power
-	float GetUnitExperience (int unitid);	//how experienced the unit is (0.0f-1.0f)
-	bool IsUnitActivated (int unitid); 
-	bool UnitBeingBuilt (int unitid);			//returns true if the unit is currently being built
-	bool GetUnitResourceInfo (int unitid, UnitResourceInfo* resourceInfo);
+	int GetUnitTeam(int unitid);
+	int GetUnitAllyTeam(int unitid);
+	float GetUnitHealth(int unitid);
+	float GetUnitMaxHealth(int unitid);
+	float GetUnitPower(int unitid);
+	float GetUnitExperience(int unitid);
+	bool IsUnitActivated(int unitid);
+	bool UnitBeingBuilt(int unitid);
+	bool IsUnitNeutral(int unitid);
+	bool GetUnitResourceInfo(int unitid, UnitResourceInfo* resourceInfo);
 	const CCommandQueue* GetCurrentUnitCommands(int unitid);
 
-	int GetBuildingFacing(int unitid);		//returns building facing (0-3)
+	int GetBuildingFacing(int unitid);
 	bool IsUnitCloaked(int unitid);
 	bool IsUnitParalyzed(int unitid);
 
 	bool OnlyPassiveCheats();
 	void EnableCheatEvents(bool enable);
 
-	// future callback extensions
-	bool GetProperty(int unit, int property, void *dst);
-	bool GetValue(int id, void *dst);
-	int HandleCommand(int commandId, void *data); 
+	bool GetProperty(int unit, int property, void* dst);
+	bool GetValue(int id, void* dst);
+	int HandleCommand(int commandId, void* data);
 };
 
 #endif
