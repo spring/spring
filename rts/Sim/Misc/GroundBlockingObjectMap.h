37938f75a6b6019396f6a8979dfcc3d468333818
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2008-04-22 21:05:49 +0000

    * Remove extra qualifier, on which GCC would give an error.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5766 37977431-3df6-0310-b722-df95706aa16b

--- rts/Sim/Misc/GroundBlockingObjectMap.h
+++ rts/Sim/Misc/GroundBlockingObjectMap.h
@@ -1,7 +1,6 @@
 #ifndef GROUNDBLOCKINGOBJECTMAP_H
 #define GROUNDBLOCKINGOBJECTMAP_H
 
-#include <vector>
 #include "creg/creg.h"
 #include "float3.h"
 
@@ -19,7 +18,7 @@ public:
 	void MoveGroundBlockingObject(CSolidObject *object, float3 oldPos);
 	void OpenBlockingYard(CSolidObject *yard, unsigned char *blockingMap);
 	void CloseBlockingYard(CSolidObject *yard, unsigned char *blockingMap);
-	bool CGroundBlockingObjectMap::CanCloseYard(CSolidObject* object);
+	bool CanCloseYard(CSolidObject* object);
 	CSolidObject* GroundBlocked(float3 pos);
 	CSolidObject* GroundBlocked(int mapSquare);
 	///simple version of GroundBlocked without error checking
