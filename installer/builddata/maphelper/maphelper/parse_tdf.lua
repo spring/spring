3fb5a34f84c2e286df91614a0da734efb237d51f
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-06-06 21:53:00 +0000

    - improved (hopefully) version a parse_tdf.lua   - single pass lexical analysis   - up to 60% faster   - parses double quotes sections for values     (FLOZi noticed that the following used to fail:  <url = "http://...";>)   - cleaner code
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@5991 37977431-3df6-0310-b722-df95706aa16b

--- installer/builddata/maphelper/maphelper/parse_tdf.lua
+++ installer/builddata/maphelper/maphelper/parse_tdf.lua
@@ -5,19 +5,8 @@
 --  brief:   lua tdf parser
 --  author:  Dave Rodgers
 --
---  Copyright (C) 2007.
+--  Copyright (C) 2008.
 --  Licensed under the terms of the GNU GPL, v2 or later.
---
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
---
---  NOTES:
---
---    - Although this parser is faster then the C++ TDFParser, it still suffers
---      from a few serious slowdowns. Running global substitutions for dos2unix,
---      line comments, and block comments is probably not the best way to go.
---
---    - Add more complete error checking / handling
 --
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
@@ -26,176 +15,203 @@ if (TDFparser) then
   return TDFparser
 end
 
+
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
+
+local pos = 1
+
+local text = ''
+
+local tdfName = ''
 
 local keyFilter = string.lower
 
-local noDuplicates = false
-
-local debug = false
+local _ -- junk value
 
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-local function FindLineNum(pos, text)
-  local lineNum = 1
+local function GetLineNumber(x)
+  x = x and x or pos
+  local t = text  
   local p = 1
-  while (p ~= nil) do
-    local s, e = string.find(text, '^[^\n]*\n', p) 
-    if (s == nil) then
-      return lineNum
+  local n = 1
+  local l = ''
+  while (true) do
+    _, _, l, p = t:find('([^\n]*)\n()', p)
+    if ((not p) or (p > x)) then
+      break
     end
-    p = e + 1
-    if (p > pos) then
-      return lineNum
-    end
-    lineNum = lineNum + 1
+    n = n + 1
   end
-  return lineNum
+  return n, l
+end
+
+
+local function Crash(msg)
+  local num, line = GetLineNumber(pos)
+  line = line and ('  "' .. line:gsub('\r', '') .. '"') or ''
+  local name = (#tdfName > 0) and (tdfName .. ' ') or ''
+  msg = ('%s (%sline %i)%s'):format(msg, name, num, line)
+  error(msg)
 end
 
 
 --------------------------------------------------------------------------------
 
-local function DosToUnix(text)
-  return string.gsub(text, '\r', '')
-end
-
-
-local function ReplaceWithSpaces(text)
-  return string.gsub(text, '[^\n]', ' ')
-end
-
-
-local function StripLineComments(text)
-  return string.gsub(text, '//[^\n]*[\n]?', ReplaceWithSpaces)
-end
-
-
-local function StripBlockComments(text)
-  return string.gsub(text, '/%*.-%*/', ReplaceWithSpaces)
-end
-
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-local cleanText
-
-local stack = { {} }
-local current = stack[1]
-
-
---------------------------------------------------------------------------------
-
-local function ParseElement(text, pos)
-  if (debug) then
-    print('pos = ' .. tostring(pos))
-  end
-
-  local sectionPattern = '^%s*%[([^%]]+)%]%s*{'
-  local s, e, sec = string.find(text, sectionPattern, pos)
-  if (sec) then
-    sec = keyFilter(sec)
-    if (noDuplicates and current[sec]) then
-      return nil, 'duplicate entry: ' .. sec
-    end
-    local newSection = {}
-    table.insert(stack, newSection)
-    current[sec] = newSection
-    current = newSection
-    if (debug) then
-      print('  SECTION', sec, e + 1)
-      print(type(e))
-    end
-    return e + 1
-  end
-
-  local endSecPattern = '^(%s*})'
-  local s, e, endText = string.find(text, endSecPattern, pos)
-  if (s) then
-    local stackSize = #stack
-    if (stackSize <= 1) then
-      return nil, 'table underrun'
-    else
-      table.remove(stack)
-      current = stack[stackSize - 1]
-    end
-    if (debug) then
-      print('  ENDSECTION', endText, e + 1)
-    end
-    return e + 1
-  end
-
-  local entryPattern = '^%s*([^%s=]*)%s*=%s*([^;\n]*);'
-  local s, e, k, v = string.find(text, entryPattern, pos)
-  if (k and v) then
-    k = keyFilter(k)
-    if (noDuplicates and current[k]) then
-      return nil, 'duplicate entry: ' .. k
-    end
-
-    local _, _, v = string.find(v, '(.-)%s*$') -- remove trailing space
-    current[k] = v
-    if (debug) then
-      print('  PAIR:  ' .. k .. ' = ' .. v,
-            (e + 1) .. '/' .. FindLineNum(pos, cleanText))
-    end
-    return e + 1
-  end
-
-  if (string.find(text, '^%s*$', pos)) then
+local function CurrChar()
+  if (not pos) then
     return nil
   end
+  return text:sub(pos, pos)
+end
 
-  return nil, 'Bad TDF text at line ' .. FindLineNum(pos, cleanText)
+
+local function EatWhite()
+  if (not pos) then
+    return
+  end
+  repeat
+    local ppos = pos
+    local s, e, p -- start, end, pos
+    if (text:sub(pos, pos) ~= '/') then
+      -- whitespace
+      s, e, p = text:find('^%s*()', pos)
+      if (p) then pos = p end
+    else
+      -- line comments
+      s, e, p = text:find('^//[^\n]*()', pos)
+      if (p) then pos = p end
+      -- block comments
+      s, e, p = text:find('^/%*.-*/()', pos)
+      if (p) then pos = p end
+    end
+  until (ppos == pos)
 end
 
 
 --------------------------------------------------------------------------------
 
-local function Parse(text)
+local ParseKey
+local ParseValue
+local ParseSection
+local ParseElements
 
-  text = DosToUnix(text)
-  text = StripLineComments(text)
-  text = StripBlockComments(text)
 
-  cleanText = text
+ParseKey = function()
+  local s, e, k, p = text:find('^([^%s=]+)[ \t]*=[ \t]*()', pos)
+  if (not k) then
+    Crash('could not find key')
+  end
+  pos = p
+  return k
+end
 
-  stack = { {} }
-  current = stack[1]
 
-  local pos = 1
+ParseValue = function()
+  -- look for quoted string
+  local s, e, v, p = text:find('^"([^\n"]*)"[ \t]*;()', pos)
+  if (v) then
+    pos = p
+    return v
+  end
+  -- unquoted string (can not contain ';')
+  local s, e, v, p = text:find('^([^\n;]*);()', pos)
+  if (v) then
+    pos = p
+    return v
+  end
+  Crash('could not find value, missing ";" ?')
+end
 
-  while (pos) do
-    pos, err = ParseElement(text, pos)
-    if (err) then
-      return nil, err
-    end
-    if ((pos == nil) or (pos > #text)) then
-      if (#stack == 1) then
-        break;
-      else
-        return nil, 'missing } table terminations'
-      end
-    end
+
+ParseSection = function()
+  local s, e, h, p = text:find('^%[[ \t]*(%S*)[ \t]*%]()', pos)
+  if (not h) then
+    Crash('missing section')
+  end
+  pos = p
+
+  EatWhite()
+  if (CurrChar() ~= '{') then
+    Crash('missing section start brace')
+  end
+  pos = pos + 1
+
+  local t, c = ParseElements()
+  if (c ~= '}') then
+    Crash('missing section end brace')
   end
 
-  return stack[1]
+  return h, t
+end
+
+
+-- elements can be either sections, or key/value pairs
+
+ParseElements = function()
+  local t = {}
+
+  while (true) do
+    EatWhite()
+
+    local c = CurrChar()
+    if (c == '') then
+      return t, nil
+    elseif (c == '}') then
+      pos = pos + 1
+      return t, c
+    else
+      local k, v
+      if (c == '[') then
+        k, v = ParseSection()
+      else
+        k, v = ParseKey(), ParseValue()
+      end
+      t[keyFilter(k)] = v
+    end
+  end
+end
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local function Parse(newText)
+  text = newText
+  pos = 1
+
+  local success, data = pcall(function()
+    local d, char = ParseElements()
+    if (char ~= nil) then
+      Crash('extra junk')
+    end
+    return d
+  end)
+
+  if (success) then
+    return data
+  else
+    return nil, data
+  end
 end
 
 
 local function ParseFile(filename)
-  if (debug) then
-    print('TDF.Parse: ' .. tostring(filename))
-  end
-  local text, err = VFS.LoadFile(filename)
-  if (text == nil) then
+  local fileText, err = VFS.LoadFile(filename)
+  if (fileText == nil) then
     return nil, err
   end
-  return Parse(text)
+  tdfName = filename
+  return Parse(fileText)
+end
+
+
+local function ParseText(srcText)
+  tdfName = ''
+  return Parse(srcText)
 end
 
 
@@ -216,19 +232,13 @@ local function SetKeyFilter(filter)
 end
 
 
-local function AllowDuplicates(state)
-  if (type(state) == 'boolean') then
-    noDuplicates = not state
-  end
-end
-
-
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
 TDFparser = {
+  ParseText       = ParseText,
   Parse           = ParseFile,
-  ParseText       = Parse,
+  ParseFile       = ParseFile,
   GetKeyFilter    = GetKeyFilter,
   SetKeyFilter    = SetKeyFilter,
   AllowDuplicates = AllowDuplicates,
