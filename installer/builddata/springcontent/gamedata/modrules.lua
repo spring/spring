b04137e3e95c478d2bcac616db787276895dd0a0
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-06-10 01:13:17 +0000

    - allow sensors.tdf without modrules.tdf
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6019 37977431-3df6-0310-b722-df95706aa16b

--- installer/builddata/springcontent/gamedata/modrules.lua
+++ installer/builddata/springcontent/gamedata/modrules.lua
@@ -17,26 +17,42 @@ local TDF = VFS.Include('gamedata/parse_tdf.lua')
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-if (not VFS.FileExists('gamedata/modrules.tdf')) then
-  return false
+local modRules = {}
+
+
+local haveRules   = false
+local haveSensors = false
+
+
+if (VFS.FileExists('gamedata/modrules.tdf')) then
+	local err
+	modRules, err = TDF.Parse('gamedata/modrules.tdf')
+	if (modRules == nil) then
+		error('Error parsing modrules.tdf: ' .. err)
+	end
+	haveRules = true
 end
 
-local modrules, err = TDF.Parse('gamedata/modrules.tdf')
-if (modrules == nil) then
-  error('Error parsing modrules.tdf: ' .. err)
+
+if (VFS.FileExists('gamedata/sensors.tdf')) then
+	local sensors, err = TDF.Parse('gamedata/sensors.tdf')
+	if (sensors == nil)  then
+		error('Error parsing sensors.tdf: ' .. err)
+	end
+	modRules.sensors = sensors
+	haveSensors = true
 end
 
-local sensors, err2 = TDF.Parse('gamedata/sensors.tdf')
-if ((sensors == nil) and VFS.FileExists('gamedata/sensors.tdf'))  then
-	error('Error parsing sensors.tdf: ' .. err2)
+
+if (not (haveRules or haveSensors)) then
+	return false
 end
-modrules.sensors = sensors
 
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
-return modrules
+return modRules
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
