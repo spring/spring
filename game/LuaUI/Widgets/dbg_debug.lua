8cca5bbea86054a5685e7585ef300f8fcacd2816
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-06-15 13:55:55 +0000

    - use Spring.Echo() instead of print()
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6033 37977431-3df6-0310-b722-df95706aa16b

--- game/LuaUI/Widgets/dbg_debug.lua
+++ game/LuaUI/Widgets/dbg_debug.lua
@@ -32,6 +32,9 @@ end
 include("debug.lua")
 
 
+local Echo = Spring.Echo
+
+
 --  one-shots
 local printUpdate          = true
 local printDrawWorld       = true
@@ -39,11 +42,12 @@ local printDrawScreen      = true
 local printTweakDrawScreen = true
 local printIsAbove         = true
 local printTweakIsAbove    = true
+local printAddConsoleLine  = true
 
 
 function widget:TextCommand(command)
   if (command ~= "debug") then
-    print('DEBUG(TextCommand) '..command)
+    Echo('DEBUG(TextCommand) '..command)
     return false
   end
   printUpdate          = true
@@ -52,6 +56,7 @@ function widget:TextCommand(command)
   printTweakDrawScreen = true
   printIsAbove         = true
   printTweakIsAbove    = true
+  printAddConsoleLine  = true
   Debug()
   return true
 end
@@ -60,31 +65,31 @@ end
 --------------------------------------------------------------------------------
 
 function widget:Initialize()
-  print('DEBUG (Initialize)')
+  Echo('DEBUG (Initialize)')
   return
 end
 
 
 function widget:Shutdown()
-  print('DEBUG (Shutdown)')
+  Echo('DEBUG (Shutdown)')
   return
 end
 
 
 function widget:GamePreload()
-  print('DEBUG (GamePreload)')
+  Echo('DEBUG (GamePreload)')
   return
 end
 
 
 function widget:GameStart()
-  print('DEBUG (GameStart)')
+  Echo('DEBUG (GameStart)')
   return
 end
 
 
 function widget:GameOver()
-  print('DEBUG (GameOver)')
+  Echo('DEBUG (GameOver)')
   return
 end
 
@@ -92,26 +97,29 @@ end
 function widget:Update(deltaTime)
   if (printUpdate) then
     printUpdate = false
-    print('DEBUG (Update) '..deltaTime)
+    Echo('DEBUG (Update) '..deltaTime)
   end
   return
 end
 
 
 function widget:CommandNotify(id, params, options)
-  print('DEBUG (CommandNotify) '..id)
+  Echo('DEBUG (CommandNotify) '..id)
   return false
 end
 
 
 function widget:AddConsoleLine(msg, priority)
-  print('DEBUG (AddConsoleLine) '..msg)
+  if (printAddConsoleLine) then
+    Echo('DEBUG (AddConsoleLine) '..msg)
+    printAddConsoleLine = false
+  end
   return
 end
 
 
 function widget:ViewResize(vsx, vsy)
-  print('DEBUG (ViewResize) '..vsx..' '..vsy)
+  Echo('DEBUG (ViewResize) '..vsx..' '..vsy)
   return
 end
 
@@ -119,7 +127,7 @@ end
 function widget:DrawWorld()
   if (printDrawWorld) then
     printDrawWorld = false
-    print('DEBUG (DrawWorld)')
+    Echo('DEBUG (DrawWorld)')
   end
   return
 end
@@ -128,38 +136,38 @@ end
 function widget:DrawScreen()
   if (printDrawScreen) then
     printDrawScreen = false
-    print('DEBUG (DrawScreen)')
+    Echo('DEBUG (DrawScreen)')
   end
   return
 end
 
 
 function widget:KeyPress(key, mods, isRepeat, label, unicode)
-  print('DEBUG (KeyPress) ' .. key .. ' ' .. label .. ' ' .. tostring(unicode))
+  Echo('DEBUG (KeyPress) ' .. key .. ' ' .. label .. ' ' .. tostring(unicode))
   return false
 end
 
 
 function widget:KeyRelease(key, mods, label, unicode)
-  print('DEBUG (KeyReleased) ' .. key .. ' ' .. label .. ' ' .. tostring(unicode))
+  Echo('DEBUG (KeyReleased) ' .. key .. ' ' .. label .. ' ' .. tostring(unicode))
   return false
 end
 
 
 function widget:MousePress(x, y, button)
-  print('DEBUG (MousePress) '..x..' '..y..' '..button)
+  Echo('DEBUG (MousePress) '..x..' '..y..' '..button)
   return false
 end
 
 
 function widget:MouseMove(x, y, dx, dy, button)
-  print('DEBUG (MouseMove) '..x..' '..y..' '..dx..' '..dy..' '..button)
+  Echo('DEBUG (MouseMove) '..x..' '..y..' '..dx..' '..dy..' '..button)
   return false
 end
 
 
 function widget:MouseRelease(x, y, button)
-  print('DEBUG (MouseRelease) '..x..' '..y..' '..button)
+  Echo('DEBUG (MouseRelease) '..x..' '..y..' '..button)
   return -1
 end
 
@@ -167,87 +175,87 @@ end
 function widget:IsAbove(x, y)
   if (printIsAbove) then
     printIsAbove = false
-    print('DEBUG (IsAbove) '..x..' '..y)
+    Echo('DEBUG (IsAbove) '..x..' '..y)
   end
   return false
 end
 
 
 function widget:GetTooltip(x, y)
-  print('DEBUG (GetTooltip) '..x..' '..y)
+  Echo('DEBUG (GetTooltip) '..x..' '..y)
   return ""
 end
 
 
 function widget:GroupChanged(groupID)
-  print('DEBUG (GroupChanged) '..groupID)
+  Echo('DEBUG (GroupChanged) '..groupID)
   return
 end
 
 
 function widget:CommandsChanged()
-  print('DEBUG (CommandsChanged) ')
+  Echo('DEBUG (CommandsChanged) ')
   return
 end
 
 
 function widget:UnitCreated(unitID, unitDefID, unitTeam)
-  print('DEBUG (UnitCreated) '..unitID..' '..unitDefID..' '..unitTeam)
+  Echo('DEBUG (UnitCreated) '..unitID..' '..unitDefID..' '..unitTeam)
   return
 end
 
 
 function widget:UnitFinished(unitID, unitDefID, unitTeam)
-  print('DEBUG (UnitFinished) '..unitID..' '..unitDefID..' '..unitTeam)
+  Echo('DEBUG (UnitFinished) '..unitID..' '..unitDefID..' '..unitTeam)
   return
 end
 
 
 function widget:UnitFromFactory(unitID, unitDefID, unitTeam,
                                 factID, factDefID, userOrders)
-  print('DEBUG (UnitFromFactory) '
-        ..unitID..' '..unitDefID..' '..unitTeam..' '
-        ..factID..' '..factDefID..' '..tostring(userOrders))
+  Echo('DEBUG (UnitFromFactory) '
+       ..unitID..' '..unitDefID..' '..unitTeam..' '
+       ..factID..' '..factDefID..' '..tostring(userOrders))
   return
 end
 
 
 function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
-  print('DEBUG (UnitDestroyed) '..unitID..' '..unitDefID..' '..unitTeam)
+  Echo('DEBUG (UnitDestroyed) '..unitID..' '..unitDefID..' '..unitTeam)
   return
 end
 
 
 function widget:UnitTaken(unitID, unitDefID, unitTeam, newTeam)
-  print('DEBUG (UnitTaken) '
-        ..unitID..' '..unitDefID..' '..unitTeam..' '..newTeam)
+  Echo('DEBUG (UnitTaken) '
+       ..unitID..' '..unitDefID..' '..unitTeam..' '..newTeam)
   return
 end
 
 
 function widget:UnitGiven(unitID, unitDefID, unitTeam, oldTeam)
-  print('DEBUG (UnitGiven) '
-        ..unitID..' '..unitDefID..' '..unitTeam..' '..oldTeam)
+  Echo('DEBUG (UnitGiven) '
+       ..unitID..' '..unitDefID..' '..unitTeam..' '..oldTeam)
   return
 end
 
 
 function widget:UnitIdle(unitID, unitDefID, unitTeam)
-  print('DEBUG (UnitIdle) '..unitID..' '..unitDefID..' '..unitTeam)
+  Echo('DEBUG (UnitIdle) '..unitID..' '..unitDefID..' '..unitTeam)
   return
 end
 
 
 function widget:UnitDamaged(unitID, unitDefID, unitTeam, damage, paralyzer)
-  print('DEBUG (UnitDamaged) '
-        ..unitID..' '..unitDefID..' '..unitTeam..' '
-        ..damage..' '..tostring(paralyzer))
+  Echo('DEBUG (UnitDamaged) '
+       ..unitID..' '..unitDefID..' '..unitTeam..' '
+       ..damage..' '..tostring(paralyzer))
   return
 end
 
 
 function widget:UnitSeismicPing(x, y, z, strength)
-  print('DEBUG (UnitSeismicPing) '..x..' '..y..' '..z..' '..strength)
+  Echo('DEBUG (UnitSeismicPing) '..x..' '..y..' '..z..' '..strength)
   Spring.PlaySoundFile('LuaUI/Sounds/message_admin.wav', strength * 0.25, x, y, z)
   return
 end
@@ -255,32 +263,32 @@ end
 
 function widget:UnitLoaded(unitID, unitDefID, unitTeam,
                            transportID, transportTeam)
-  print('DEBUG (UnitLoaded) '
-        ..unitID..' '..unitDefID..' '..unitTeam..' '
-        ..transportID..' '..transportTeam)
+  Echo('DEBUG (UnitLoaded) '
+       ..unitID..' '..unitDefID..' '..unitTeam..' '
+       ..transportID..' '..transportTeam)
   return
 end
 
 
 function widget:UnitUnloaded(unitID, unitDefID, unitTeam,
                            transportID, transportTeam)
-  print('DEBUG (UnitUnloaded) '
-        ..unitID..' '..unitDefID..' '..unitTeam..' '
-        ..transportID..' '..transportTeam)
+  Echo('DEBUG (UnitUnloaded) '
+       ..unitID..' '..unitDefID..' '..unitTeam..' '
+       ..transportID..' '..transportTeam)
   return
 end
 
 
 function widget:UnitCloaked(unitID, unitDefID, unitTeam)
-  print('DEBUG (UnitCloaked) '
-        ..unitID..' '..unitDefID..' '..unitTeam)
+  Echo('DEBUG (UnitCloaked) '
+       ..unitID..' '..unitDefID..' '..unitTeam)
   return
 end
 
 
 function widget:UnitDecloaked(unitID, unitDefID, unitTeam)
-  print('DEBUG (UnitDecloaked) '
-        ..unitID..' '..unitDefID..' '..unitTeam)
+  Echo('DEBUG (UnitDecloaked) '
+       ..unitID..' '..unitDefID..' '..unitTeam)
   return
 end
 
@@ -288,38 +296,38 @@ end
 function widget:TweakDrawScreen()
   if (printTweakDrawScreen) then
     printTweakDrawScreen = false
-    print('DEBUG (TweakDrawScreen)')
+    Echo('DEBUG (TweakDrawScreen)')
   end
   return
 end
 
 
 function widget:TweakKeyPress(key, mods, isRepeat)
-  print('DEBUG (TweakKeyPress) '..key)
+  Echo('DEBUG (TweakKeyPress) '..key)
   return false
 end
 
 
 function widget:TweakKeyRelease(key, mods)
-  print('DEBUG (TweakKeyRelease) '..key)
+  Echo('DEBUG (TweakKeyRelease) '..key)
   return false
 end
 
 
 function widget:TweakMousePress(x, y, button)
-  print('DEBUG (TweakMousePress) '..x..' '..y..' '..button)
+  Echo('DEBUG (TweakMousePress) '..x..' '..y..' '..button)
   return false
 end
 
 
 function widget:TweakMouseMove(x, y, dx, dy, button)
-  print('DEBUG (TweakMouseMove) '..x..' '..y..' '..dx..' '..dy..' '..button)
+  Echo('DEBUG (TweakMouseMove) '..x..' '..y..' '..dx..' '..dy..' '..button)
   return false
 end
 
 
 function widget:TweakMouseRelease(x, y, button)
-  print('DEBUG (TweakMouseRelease) '..x..' '..y..' '..button)
+  Echo('DEBUG (TweakMouseRelease) '..x..' '..y..' '..button)
   return -1
 end
 
@@ -327,14 +335,14 @@ end
 function widget:TweakIsAbove(x, y)
   if (printTweakIsAbove) then
     printTweakIsAbove = false
-    print('DEBUG (TweakIsAbove) '..x..' '..y)
+    Echo('DEBUG (TweakIsAbove) '..x..' '..y)
   end
   return false
 end
 
 
 function widget:TweakGetTooltip(x, y)
-  print('DEBUG (TweakGetTooltip) '..x..' '..y)
+  Echo('DEBUG (TweakGetTooltip) '..x..' '..y)
   return ""
 end
 
