d99792f88239a8a17a5246f197ba82b866c9fcc0
trepan <trepan@37977431-3df6-0310-b722-df95706aa16b>
2008-07-01 21:19:50 +0000

    - reset idling immobile builders
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6097 37977431-3df6-0310-b722-df95706aa16b

--- game/LuaUI/Widgets/unit_immobile_buider.lua
+++ game/LuaUI/Widgets/unit_immobile_buider.lua
@@ -31,8 +31,10 @@ end
 local CMD_MOVE_STATE    = CMD.MOVE_STATE
 local CMD_PATROL        = CMD.PATROL
 local CMD_STOP          = CMD.STOP
+local spGetGameFrame    = Spring.GetGameFrame
 local spGetMyTeamID     = Spring.GetMyTeamID
 local spGetTeamUnits    = Spring.GetTeamUnits
+local spGetUnitCommands = Spring.GetUnitCommands
 local spGetUnitDefID    = Spring.GetUnitDefID
 local spGetUnitPosition = Spring.GetUnitPosition
 local spGiveOrderToUnit = Spring.GiveOrderToUnit
@@ -41,21 +43,37 @@ local spGiveOrderToUnit = Spring.GiveOrderToUnit
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 
+-- if this is >0, then commands are re-issued for immobile
+-- builders that have been idling for the number of game frames
+-- (in case a player accidentally STOPs them)
+
+local idleFrames = 30
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+local function IsImmobileBuilder(ud)
+  return (ud and ud.builder and not ud.canMove)
+end
+
+
 local function SetupUnit(unitID)
   -- set immobile builders (nanotowers?) to the ROAM movestate,
   -- and give them a PATROL order (does not matter where, afaict)
   local x, y, z = spGetUnitPosition(unitID)
-  spGiveOrderToUnit(unitID, CMD_STOP, {}, {})
-  spGiveOrderToUnit(unitID, CMD_MOVE_STATE, { 2 }, {})
-  spGiveOrderToUnit(unitID, CMD_PATROL, { x + 25, y, z - 25 }, {})
+  if (x) then
+    spGiveOrderToUnit(unitID, CMD_STOP, {}, {})
+    spGiveOrderToUnit(unitID, CMD_MOVE_STATE, { 2 }, {})
+    spGiveOrderToUnit(unitID, CMD_PATROL, { x + 25, y, z - 25 }, {})
+  end
 end
 
 
 function widget:Initialize()
   for _,unitID in ipairs(spGetTeamUnits(spGetMyTeamID())) do
     local unitDefID = spGetUnitDefID(unitID)
-    local ud = unitDefID and UnitDefs[unitDefID] or nil
-    if (ud and ud.builder and not ud.canMove) then
+    if (IsImmobileBuilder(UnitDefs[unitDefID])) then
       SetupUnit(unitID)
     end
   end
@@ -66,8 +84,7 @@ function widget:UnitCreated(unitID, unitDefID, unitTeam)
   if (unitTeam ~= spGetMyTeamID()) then
     return
   end
-  local ud = UnitDefs[unitDefID]
-  if (ud and ud.builder and not ud.canMove) then
+  if (IsImmobileBuilder(UnitDefs[unitDefID])) then
     SetupUnit(unitID)
   end
 end
@@ -75,6 +92,47 @@ end
 
 function widget:UnitGiven(unitID, unitDefID, unitTeam)
   widget:UnitCreated(unitID, unitDefID, unitTeam)
+end
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+if (idleFrames > 0) then
+
+  local idlers = {}
+
+
+  function widget:GameFrame(frame)
+    for unitID, f in pairs(idlers) do
+      local idler = idlers[k]
+      if ((frame - f) > idleFrames) then
+        local cmds = spGetUnitCommands(unitID)
+        if (cmds and (#cmds <= 0)) then
+          SetupUnit(unitID)
+        else
+          idlers[unitID] = nil
+        end
+      end
+    end  
+  end
+
+
+  function widget:UnitDestroyed(unitID, unitDefID, unitTeam)
+    idlers[unitID] = nil
+  end
+
+
+  function widget:UnitIdle(unitID, unitDefID, unitTeam)
+    if (unitTeam ~= spGetMyTeamID()) then
+      return
+    end
+    if (IsImmobileBuilder(UnitDefs[unitDefID])) then
+      idlers[unitID] = spGetGameFrame()
+    end
+  end
+
+
 end
 
 
