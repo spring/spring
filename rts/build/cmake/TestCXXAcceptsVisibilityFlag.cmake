6c58f165573209c12aa533aec7636ae479c8f130
Auswaschbar <Auswaschbar@37977431-3df6-0310-b722-df95706aa16b>
2008-09-18 19:31:26 +0000

    * CMake updates:   - fix compilewarning-spam about fisibility flag on mingw   - don't use -fpic for mingw (not needed)   - fix duplication of some compilerflags
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6437 37977431-3df6-0310-b722-df95706aa16b

--- rts/build/cmake/TestCXXAcceptsVisibilityFlag.cmake
+++ rts/build/cmake/TestCXXAcceptsVisibilityFlag.cmake
@@ -1,17 +1,30 @@
 # - Test whether the C++ compiler supports "-fvisibility=hidden"
 # Once done this will define
 #
-# VISIBILITY_HIDDEN - -fvisibility=hidden if supported, an empty string otherwise.
+# VISIBILITY_HIDDEN - -fvisibility=hidden   if supported, an empty string otherwise.
+# VISIBILITY_INLINES_HIDDEN - -fvisibility-inlines-hidden   likewise
 #
 # Copyright (C) 2008 Tobi Vollebregt
+# Copyright (C) 2008 Karl-Robert Ernst
 #
+# Note: gcc for windows supports these flags, but give lots of errors when compiling, so use only for linux builds
 
-IF(NOT DEFINED CMAKE_CXX_FLAGS)
-	INCLUDE(TestCXXAcceptsFlag)
-	CHECK_CXX_ACCEPTS_FLAG(-fvisibility=hidden VISIBILITY_HIDDEN)
-	IF(VISIBILITY_HIDDEN)
-		SET(VISIBILITY_HIDDEN -fvisibility=hidden)
-	ELSE(VISIBILITY_HIDDEN)
+INCLUDE(TestCXXAcceptsFlag)
+
+IF(NOT DEFINED VISIBILITY_HIDDEN)
+	CHECK_CXX_ACCEPTS_FLAG(-fvisibility=hidden HAS_VISIBILITY_HIDDEN)
+	IF(HAS_VISIBILITY_HIDDEN AND NOT MINGW)
+		SET(VISIBILITY_HIDDEN "-fvisibility=hidden")
+	ELSE(HAS_VISIBILITY_HIDDEN AND NOT MINGW)
 		SET(VISIBILITY_HIDDEN "")
-	ENDIF(VISIBILITY_HIDDEN )
-ENDIF(NOT DEFINED CMAKE_CXX_FLAGS) 
+	ENDIF(HAS_VISIBILITY_HIDDEN AND NOT MINGW)
+ENDIF(NOT DEFINED VISIBILITY_HIDDEN)
+	
+IF(NOT DEFINED VISIBILITY_INLINES_HIDDEN)
+	CHECK_CXX_ACCEPTS_FLAG(-fvisibility-inlines-hidden HAS_VISIBILITY_INLINES_HIDDEN)
+	IF(HAS_VISIBILITY_INLINES_HIDDEN AND NOT MINGW)
+		SET(VISIBILITY_INLINES_HIDDEN "-fvisibility-inlines-hidden")
+	ELSE(HAS_VISIBILITY_INLINES_HIDDEN AND NOT MINGW)
+		SET(VISIBILITY_INLINES_HIDDEN "")
+	ENDIF(HAS_VISIBILITY_INLINES_HIDDEN AND NOT MINGW)
+ENDIF(NOT DEFINED VISIBILITY_INLINES_HIDDEN)
