8d723dc97924e20ddb6856032ea5c9ee6736fe7a
imbaczek <imbaczek@37977431-3df6-0310-b722-df95706aa16b>
2008-10-05 21:27:37 +0000

    fix CColorMap memory leak
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6564 37977431-3df6-0310-b722-df95706aa16b

--- rts/Rendering/Textures/ColorMap.cpp
+++ rts/Rendering/Textures/ColorMap.cpp
@@ -24,7 +24,7 @@ CColorMap::CColorMap(std::vector<float> &vec)
 	map=0;
 
 	if(vec.size()<8) //needs at least two colors
-		throw content_error("To few colors in colormap.");
+		throw content_error("Too few colors in colormap.");
 
 	unsigned char *lmap = new unsigned char[vec.size() - vec.size()%4];
 	xsize = (vec.size() - vec.size()%4)/4;
@@ -72,23 +72,21 @@ CColorMap::CColorMap(const unsigned char *buf, int num)
 
 CColorMap::~CColorMap(void)
 {
-	if(map)
-		delete [] map;
+	delete [] map;
 }
 
 void CColorMap::DeleteColormaps()
 {
 	for(int i=0; i<colorMaps.size(); i++)
 	{
-		delete colorMaps.back();
-		colorMaps.pop_back();
+		delete colorMaps[i];
 	}
+	colorMaps.clear();
 }
 
 void CColorMap::LoadMap(const unsigned char *buf, int num)
 {
-	if(map)
-		delete [] map;
+	delete [] map;
 
 	map = new unsigned char[num];
 	memcpy(map, buf, num);
