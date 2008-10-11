7118fe1bc720e3e272443b88603ac5f3628d6896
hoijui <hoijui@37977431-3df6-0310-b722-df95706aa16b>
2008-10-11 12:37:53 +0000

    forgot these two files :/
    git-svn-id: https://spring.clan-sy.com/svn/spring/branches/caiinterface@6670 37977431-3df6-0310-b722-df95706aa16b

---
+++ rts/ExternalAI/Interface/SStaticGlobalData.h
@@ -0,0 +1,47 @@
+/*
+	Copyright (c) 2008 Robin Vobruba <hoijui.quaero@gmail.com>
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SSTATICGLOBALDATA_H
+#define	_SSTATICGLOBALDATA_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif	/* __cplusplus */
+
+// paths/dirs stored in this struct are guaranteed
+// to come with no '/' or '\\' at the end.
+// they come like this:	/home/username/.spring
+// NOT like this:		/home/username/.spring/
+struct SStaticGlobalData {
+	unsigned int maxTeams;
+	const char* springVersion;
+	const char* libDir;
+	unsigned int numDataDirs;
+	const char** dataDirs;
+};
+
+#ifdef	__cplusplus
+}		/* extern "C" */
+#endif	/* __cplusplus */
+
+#if defined	__cplusplus && !defined BUILDING_AI && !defined BUILDING_AI_INTERFACE
+SStaticGlobalData* createStaticGlobalData();
+void freeStaticGlobalData(SStaticGlobalData* staticGlobalData);
+#endif	/* defined	__cplusplus && !defined BUILDING_AI && !defined BUILDING_AI_INTERFACE */
+
+#endif	/* _SSTATICGLOBALDATA_H */
+
