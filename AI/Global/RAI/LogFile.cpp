c5c6f7c7b3c49e535ba1d7738106fa82e75bdc45
hoijui <hoijui@37977431-3df6-0310-b722-df95706aa16b>
2008-10-13 10:00:36 +0000

    - Renamed RAI dir from 'RAI-0.553' to 'RAI' - upgraded RAI from v0.553 to Reths new version v0.601 (he allowed me to do so) - adjusted CMake and Scons build systems for the change with RAI - fix: adjusted CMake AI build scrip to build RAI, NTai and JCAI also when building from an other location then the main dir (the dir containing rts) - adjusted the unitsync CMake build script to additionally use JAVA_INCLUDE_PATH2, as it is needed for jni_md.h to be found
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6692 37977431-3df6-0310-b722-df95706aa16b

---
+++ AI/Global/RAI/LogFile.cpp
@@ -0,0 +1,85 @@
+#include "LogFile.h"
+
+//#include <fstream>
+//#include <stdlib.h>
+//#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+cLogFile::cLogFile(string sFilename, bool bAppend)
+{
+	logFileName=RAIDirectory+sFilename;
+	if( bAppend )
+		logFile = fopen(logFileName.c_str(),"a");
+//		logFile = new ofstream();
+//		logFile->open( logFileName.c_str(), ios::app );
+	else
+		logFile = fopen(logFileName.c_str(),"w");
+//		ofstream oLog( logFileName.c_str() );
+//		oLog.close();
+
+/*
+		pFile = fopen(logFileName.c_str(),"wt");
+		fclose(pFile);
+*/
+}
+
+cLogFile::~cLogFile()
+{
+	fclose(logFile);
+//	logFile->close();
+//	delete logFile;
+}
+
+cLogFile& cLogFile::operator<<(float message)
+{
+	if( message - int(message) > 0.0 )
+	{
+		if( message < 1.0 && message > 0.0 )
+			fprintf(logFile, "%1.3f", message);
+		else
+			fprintf(logFile, "%1.2f", message);
+	}
+	else
+		fprintf(logFile, "%1.0f", message);
+	return *this;
+}
+/*
+cLogFile& cLogFile::operator<<(int message)
+{
+	fprintf(logFile, "%i", message);
+	return *this;
+}
+*/
+cLogFile& cLogFile::operator<<(string message)
+{
+	fprintf(logFile, "%s", message.c_str());
+	return *this;
+}
+
+/*
+void cLogFile::Write(string message)
+{
+	fprintf(logFile, "%s", message.c_str());
+//  fputs(message.c_str(),logFile);
+//  *logFile<<message.c_str();
+//	logFile->flush();
+}
+
+void cLogFile::Write(float message)
+{
+	if( message - int(message) > 0.0 )
+		fprintf(logFile, "%1.2f", message);
+	else
+		fprintf(logFile, "%1.0f", message);
+//	*logFile<<message;
+//	logFile->flush();
+}
+
+void cLogFile::Write(int message)
+{
+//	*logFile<<message;
+//	logFile->flush();
+}
+*/
