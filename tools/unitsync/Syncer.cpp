6dc4101191b4e0d47bddd30137d7baf0a6cf44c9
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2008-10-25 13:34:13 +0000

    Made unitsync better: * Doesn't leak exceptions anymore over C interface. * Doesn't just crash by design anymore after something goes wrong. * Added API GetNextError, can be used to get error queue (currently limited to 1 error) * Deprecated APIs:   - GetCurrentList   - AddClient   - RemoveClient   - GetClientDiff   - InstallClientDiff   - IsUnitDisabled   - IsUnitDisabledByClient * Removed all code supporting those functions. * These deprecated functions pop up MessageBox once on windows to nag lobby devs. * Use engine's logOutput, still writes to unitsync.log (overwrites, doesn't append) * ProcessUnitsNoChecksum is now identical to ProcessUnits. * ProcessUnits now returns 0 immediately (but should still be called in loop   until it returns 0, in case it's changed sometime later)
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6882 37977431-3df6-0310-b722-df95706aa16b

--- tools/unitsync/Syncer.cpp
+++ tools/unitsync/Syncer.cpp
@@ -1,9 +1,9 @@
-
 #include "StdAfx.h"
 #include "Syncer.h"
+#include "Exceptions.h"
 #include "FileSystem/FileHandler.h"
 #include "Lua/LuaParser.h"
-#include "unitsyncLogOutput.h"
+#include "LogOutput.h"
 #include <vector>
 #include <string>
 #include <algorithm>
@@ -11,55 +11,27 @@
 
 using namespace std;
 
-CSyncer::CSyncer(int id)
-: unitsLeft(-1)
-{
-	localId = id;
-}
-
-
-CSyncer::~CSyncer(void)
+CSyncer::CSyncer()
 {
 }
 
 
-crc_t CSyncer::CalculateCRC(const string& fileName)
+CSyncer::~CSyncer()
 {
-	CFileHandler file(fileName);
-	if (!file.FileExists()) {
-		return 0;
-	}
-
-	long size = file.FileSize();
-	char *data = new char[size];
-	file.Read(data, size);
-
-	//todo: do a less silly algorithm
-	crc_t cur = 0;
-	for (int i = 0; i < size; ++i) {
-		cur += data[i];
-	}
-
-	delete[] data;
-	return cur;
 }
 
 
-void CSyncer::LoadUnits(bool checksum)
+void CSyncer::LoadUnits()
 {
-	unitsLeft = 0;
-
 	LuaParser luaParser("gamedata/defs.lua",
 	                    SPRING_VFS_MOD_BASE, SPRING_VFS_ZIP);
 	if (!luaParser.Execute()) {
-		logOutput.Print("luaParser.Execute() failed");
-		return;
+		throw content_error("luaParser.Execute() failed: " + luaParser.GetErrorLog());
 	}
 
 	LuaTable rootTable = luaParser.GetRoot().SubTable("UnitDefs");
 	if (!rootTable.IsValid()) {
-		logOutput.Print("root unitdef table invalid");
-		return;
+		throw content_error("root unitdef table invalid");
 	}
 
 	vector<string> unitDefNames;
@@ -75,18 +47,6 @@ void CSyncer::LoadUnits(bool checksum)
 
 		u.fullName = udTable.GetString("name", udName);
 
-		if (checksum) {
-			const string fileName  = udTable.GetString("filename", "");
-			const string deadName  = udTable.GetString("corpse", udName + "_dead");
-			const string modelName = udTable.GetString("objectname", udName);
-
-			u.fbi    = CalculateCRC(fileName);
-			u.cob    = CalculateCRC("scripts/" + udName + ".cob");
-			u.model  = CalculateCRC("objects3d/" + modelName); // s3o ?
-			u.model += CalculateCRC("objects3d/" + modelName + ".3do");
-			u.model += CalculateCRC("objects3d/" +  deadName + ".3do");
-		}
-
 		units[udName] = u;
 	}
 
@@ -95,91 +55,14 @@ void CSyncer::LoadUnits(bool checksum)
 	for (mit = units.begin(); mit != units.end(); ++mit) {
 		unitIds.push_back(mit->first);
 	}
-
-	unitsLeft = count;
-
-	return;
 }
 
 
-int CSyncer::ProcessUnits(bool checksum)
+int CSyncer::ProcessUnits()
 {
-	if (unitsLeft < 0) {
-		LoadUnits(checksum);
-	}
+	LoadUnits();
 
-	if (unitsLeft <= 0) {
-		return 0;
-	}
-
-	unitsLeft--;
-
-	return unitsLeft;
-}
-
-
-string CSyncer::GetCurrentList()
-{
-	ostringstream s("");
-
-	for (map<string,Unit>::iterator i = units.begin(); i != units.end(); ++i) {
-		s << i->first << " ";
-		s << i->second.fbi << " ";
-		s << i->second.cob << " ";
-		s << i->second.model << " ";
-	}
-
-	return s.str();
-}
-
-
-void CSyncer::InstallClientDiff(const string& diff)
-{
-	istringstream i(diff);
-
-	int client;
-	string name;
-	bool wasRemove;
-	int count;
-
-	while (i >> client) {
-		i >> wasRemove;
-
-		//Handle remove if so
-		if (wasRemove) {
-			RemoveClient(client);
-			return;
-		}
-
-		// Alas, it was not..
-		i >> count;
-		for (int a = 0; a < count; ++a) {
-			i >> name;
-			map<string, DisabledUnit>::iterator found = disabledUnits.find(name);
-			if (found != disabledUnits.end()) {
-				disabledUnits[name].clients.insert(client);
-			}
-			else {
-				DisabledUnit mu;
-				mu.clients.insert(client);
-				disabledUnits[name] = mu;
-			}
-		}
-	}
-}
-
-
-void CSyncer::RemoveClient(int id)
-{
-	for (map<string, DisabledUnit>::iterator i = disabledUnits.begin(); i != disabledUnits.end(); ++i) {
-		set<int>::iterator clientId = i->second.clients.find(id);
-		if (clientId != i->second.clients.end()) {
-			i->second.clients.erase(clientId);
-
-			//could delete the unit from the map if the list is empty now
-			//the speed increase should be insignificant though I guess
-		}
-	}
+	return 0;
 }
 
 
@@ -200,29 +83,4 @@ string CSyncer::GetFullUnitName(int unit)
 {
 	const string& unitName = unitIds[unit];
 	return units[unitName].fullName;
-}
-
-
-bool CSyncer::IsUnitDisabled(int unit)
-{
-	string unitName = unitIds[unit];
-	map<string, DisabledUnit>::iterator found = disabledUnits.find(unitName);
-	if (found != disabledUnits.end())
-		return found->second.clients.size() > 0;
-	else
-		return false;
-}
-
-
-bool CSyncer::IsUnitDisabledByClient(int unit, int clientId)
-{
-	string unitName = unitIds[unit];
-
-	map<string, DisabledUnit>::iterator found = disabledUnits.find(unitName);
-	if (found == disabledUnits.end())
-		return false;
-
-	set<int> &clients = found->second.clients;
-	set<int>::iterator foundId = clients.find(clientId);
-	return foundId != clients.end();
 }
