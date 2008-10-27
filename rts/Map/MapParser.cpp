7a265e9499c5ae8de7721d8e5fc84e26e6369c07
tvo <tvo@37977431-3df6-0310-b722-df95706aa16b>
2008-10-27 20:06:21 +0000

    Made it possible to specify start positions in map with LUA .smd file.
    git-svn-id: https://spring.clan-sy.com/svn/spring/trunk@6924 37977431-3df6-0310-b722-df95706aa16b

--- rts/Map/MapParser.cpp
+++ rts/Map/MapParser.cpp
@@ -4,16 +4,21 @@
 #include <ctype.h>
 using namespace std;
 
-#include "mmgr.h"
-
 #include "MapParser.h"
 #include "Lua/LuaSyncedRead.h"
 #include "System/FileSystem/FileHandler.h"
+#include "LogOutput.h"
+
+#include "mmgr.h"
+
+
+static CLogSubsystem LOG_MAPPARSER("MapParser");
 
 
 string MapParser::GetMapConfigName(const string& mapName)
 {
 	if (mapName.length() < 3) {
+		logOutput.Print(LOG_MAPPARSER, "map name too short");
 		return "";
 	}
 
@@ -27,6 +32,7 @@ string MapParser::GetMapConfigName(const string& mapName)
 		       mapName.substr(0, mapName.find_last_of('.')) + ".smd";
 	}
 	else {
+		logOutput.Print(LOG_MAPPARSER, "unknown extension");
 		return "";
 	}
 }
@@ -34,6 +40,8 @@ string MapParser::GetMapConfigName(const string& mapName)
 
 MapParser::MapParser(const string& mapName) : parser(NULL)
 {
+	logOutput.Print(LOG_MAPPARSER, "MapParser::MapParser(mapName = \"%s\")", mapName.c_str());
+
 	const string mapConfig = GetMapConfigName(mapName);
 
 	parser = SAFE_NEW LuaParser("maphelper/mapinfo.lua", SPRING_VFS_MAP_BASE, SPRING_VFS_MAP_BASE);
@@ -51,6 +59,7 @@ MapParser::MapParser(const string& mapName) : parser(NULL)
 	parser->EndTable();
 #endif
 	if (!parser->Execute()) {
+		logOutput.Print(LOG_MAPPARSER, "parser->Execute() failed: " + parser->GetErrorLog());
 		// do nothing
 	}
 }
@@ -58,18 +67,21 @@ MapParser::MapParser(const string& mapName) : parser(NULL)
 
 MapParser::~MapParser()
 {
+	logOutput.Print(LOG_MAPPARSER, "MapParser::~MapParser");
 	delete parser;
 }
 
 
 bool MapParser::GetStartPos(int team, float3& pos) const
 {
-	if (!parser->IsValid()) {
+	if (!IsValid()) {
+		logOutput.Print(LOG_MAPPARSER, "GetStartPos: parser invalid");
 		return false;
 	}
 	const LuaTable teamsTable = parser->GetRoot().SubTable("teams");
-	const LuaTable posTable = teamsTable.SubTable(team).SubTable("startPos");
+	const LuaTable posTable = teamsTable.SubTable(team + 1).SubTable("startPos");
 	if (!posTable.IsValid()) {
+		logOutput.Print(LOG_MAPPARSER, "GetStartPos: posTable invalid for team %d", team);
 		return false;
 	}
 
