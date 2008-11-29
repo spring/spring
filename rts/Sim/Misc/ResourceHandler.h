0b498ddc176803841b279c6933f136137cffaea0
hoijui <hoijui@37977431-3df6-0310-b722-df95706aa16b>
2008-11-29 17:11:37 +0000

    (forgot to upload last time)
    git-svn-id: https://spring.clan-sy.com/svn/spring/branches/caiinterface@7106 37977431-3df6-0310-b722-df95706aa16b

---
+++ rts/Sim/Misc/ResourceHandler.h
@@ -0,0 +1,77 @@
+/*
+	Copyright (c) 2008 Robin Vobruba <hoijui.quaero@gmail.com>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _RESOURCEHANDLER_H
+#define _RESOURCEHANDLER_H
+
+#include <vector>
+#include <boost/noncopyable.hpp>
+#include "creg/creg.h"
+#include "Resource.h"
+
+class CResourceHandler : public boost::noncopyable
+{
+	CR_DECLARE(CResourceHandler);
+
+public:
+	static CResourceHandler* GetInstance();
+	static void FreeInstance();
+
+	/**
+	 * @brief	resource
+	 * @param	resourceIndex index to fetch
+	 * @return	the searched resource
+	 *
+	 * Accesses a CResource instance at a given index
+	 */
+	const CResource* GetResource(int resourceIndex) const;
+	/**
+	 * @brief	resource by name
+	 * @param	resourceName name of the resource to fetch
+	 * @return	the searched resource
+	 *
+	 * Accesses a CResource instance by name
+	 */
+	const CResource* GetResourceByName(const std::string& resourceName) const;
+	/**
+	 * @brief	resource index by name
+	 * @param	resourceName name of the resource to fetch
+	 * @return	index of the searched resource
+	 *
+	 * Accesses a resource by name
+	 */
+	int GetResourceIndex(const std::string& resourceName) const;
+
+	unsigned int GetNumResources() const;
+
+//	bool IsMetal(int resourceIndex) const;
+//	bool IsEnergy(int resourceIndex) const;
+	int GetMetalIndex() const;
+	int GetEnergyIndex() const;
+
+private:
+	static CResourceHandler* instance;
+
+	CResourceHandler();
+	~CResourceHandler();
+
+	std::vector<CResource> resources;
+};
+
+#define rh CResourceHandler::GetInstance()
+
+#endif // _RESOURCEHANDLER_H
