2e3351cdf16588d428fbf7836fc2d8ca11d8f38e
hoijui <hoijui@37977431-3df6-0310-b722-df95706aa16b>
2008-12-09 22:20:39 +0000

    parsing AI Interfaces and Skirmish AIs version info from files and submitting it to the installer generation script
    git-svn-id: https://spring.clan-sy.com/svn/spring/branches/caiinterface@7154 37977431-3df6-0310-b722-df95706aa16b

--- installer/make_test_installer.sh
+++ installer/make_test_installer.sh
@@ -18,6 +18,31 @@ while [ ! -d installer ]; do
         cd ..
 done
 
+
+# Acuire AI Interface and Skirmish AI Version information
+
+AI_VERSION_INFO=""
+
+cd AI/Interfaces
+for interfaceDir in `ls`; do
+	if [ -f ${interfaceDir}/VERSION ]; then
+		interfaceVersion=`cat ${interfaceDir}/VERSION`
+		AI_VERSION_INFO="$AI_VERSION_INFO -DAI_INT_VERS_${interfaceDir}=${interfaceVersion}"
+	fi
+done
+cd ../..
+
+cd AI/Skirmish
+for skirmishDir in `ls`; do
+	if [ -f ${skirmishDir}/VERSION ]; then
+		skirmishVersion=`cat ${skirmishDir}/VERSION`
+		AI_VERSION_INFO="$AI_VERSION_INFO -DSKIR_AI_VERS_${skirmishDir}=${skirmishVersion}"
+	fi
+done
+cd ../..
+
+echo AI_VERSION_INFO: $AI_VERSION_INFO
+
 # Got a revision argument?
 
 if [ -z "$1" ]; then
@@ -33,13 +58,13 @@ echo "Creating installers for revision $REVISION"
 
 if grep -o -E 'const char\* const.*\+' rts/Game/GameVersion.cpp >/dev/null; then
 	echo "Creating test installer"
-	makensis -V3 -DTEST_BUILD -DREVISION=$REVISION installer/spring.nsi
+	makensis -V3 $AI_VERSION_INFO -DTEST_BUILD -DREVISION=$REVISION installer/spring.nsi
 
 	#echo "Creating updating test installer"
 	#makensis -V3 -DSP_UPDATE -DTEST_BUILD -DREVISION=$REVISION installer/spring.nsi
 else
 	echo "Creating installer"
-	makensis -V3 installer/spring.nsi
+	makensis -V3 $AI_VERSION_INFO installer/spring.nsi
 
 	#echo "Creating updating installer"
 	#makensis -V3 -DSP_UPDATE installer/spring.nsi
