ea6ee70d1a91fedd18c254f0ab96c2216252eacf
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-01-03 01:59:34 +0100

    reworked make_test_installer.sh to make_installer.pl which works with git (by bibim)

---
+++ installer/make_installer.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl -w
+# Author: Yann Riou (aka bibim)
+
+use strict;
+use File::Basename;
+
+my $installerDir=$0;
+$installerDir=$ENV{PWD}."/".$installerDir unless($installerDir =~ /^\//);
+$installerDir=dirname($installerDir);
+
+chdir("$installerDir/..");
+die "Unable to find \"installer\" directory." unless(-d "installer");
+
+my $testBuildString="";
+my $tag=`git describe --candidate=0 2>/dev/null`;
+if($?) {
+  $testBuildString=" -DTEST_BUILD";
+  $tag=`git describe`;
+  die "Unable to run \"git describe\"." if($?);
+  $tag=~s/\-[^\-]*$//;
+  print "Creating test installer for revision $tag\n";
+}else{
+  chomp($tag);
+  print "Creating installer for release $tag\n";
+}
+system("makensis -V3$testBuildString -DREVISION=$tag installer/spring.nsi");
