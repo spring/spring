fa9c9e170cc4bccced5827ebce58bb29bcaebb62
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-01-10 15:12:30 +0100

    installer bugfix

--- installer/tasclient_download.sh
+++ installer/tasclient_download.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
+# Quit on error.
+set -e
+
 if [ ! -e installer ]; then
   echo "Error: This script needs to be run from the root directory of the archive"
   exit 1
 fi
 
 cd installer
-mkdir -p external
 rm -rf TASClient
 
 mkdir -p TASClient/SaveFiles
