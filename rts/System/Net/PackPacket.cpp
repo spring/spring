a8ea5e5540cfe09be2549b67a50d2b6789fdbfd3
Marek Baczynski <imbaczek@gmail.com>
2009-01-16 13:20:11 +0100

    netcode: fix PackPacket::operator<< for null characters (mantis #1269)
    full file newline diff because i'm lazy.

--- rts/System/Net/PackPacket.cpp
+++ rts/System/Net/PackPacket.cpp
@@ -1,4 +1,6 @@
+//#include <algorithm>
 #include "PackPacket.h"
+#include "LogOutput.h"
 
 namespace netcode
 {
@@ -14,9 +16,14 @@ PackPacket::PackPacket(const unsigned length, unsigned char msgID) : RawPacket(l
 
 PackPacket& PackPacket::operator<<(const std::string& text)
 {
-	unsigned size = text.size()+1;
-	assert(size + pos <= length);
-	strcpy((char*)(data+pos), text.c_str());
+	unsigned size = std::min(text.size()+1, length-pos);
+	if (size + pos > length) {
+		logOutput.Print("netcode warning: string data truncated in packet\n");
+		#ifdef DEBUG
+		abort();
+		#endif
+	}
+	memcpy((char*)(data+pos), text.data(), size);
 	pos += size;
 	return *this;
 }
