fcefb7d3f95bb1495da9e1f7a241e7e70fbbd78a
jk3064@arcor.de <jk3064@arcor.de>
2009-01-16 22:04:25 +0100

    lua: added a new (unsynced) callout Spring.SetWaterParams, it needs cheating enabled and allows you to edit the mapwater settings ingame

--- rts/Rendering/Env/BumpWater.cpp
+++ rts/Rendering/Env/BumpWater.cpp
@@ -199,7 +199,7 @@ CBumpWater::CBumpWater()
 	depthBits    = configHandler.Get("BumpWaterDepthBits", (gu->atiHacks)?16:24);
 	blurRefl     = !!configHandler.Get("BumpWaterBlurReflection", 0);
 	shoreWaves   = (!!configHandler.Get("BumpWaterShoreWaves", 1)) && mapInfo->water.shoreWaves;
-	endlessOcean = (!!configHandler.Get("BumpWaterEndlessOcean", 1)) && mapInfo->hasWaterPlane
+	endlessOcean = (!!configHandler.Get("BumpWaterEndlessOcean", 1)) && mapInfo->water.hasWaterPlane
 	               && ((readmap->minheight <= 0.0f) || (mapInfo->water.forceRendering));
 	dynWaves     = (!!configHandler.Get("BumpWaterDynamicWaves", 1)) && (mapInfo->water.numTiles>1);
 
