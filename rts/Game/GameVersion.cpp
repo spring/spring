5b7d8c23b11e936509e4bbc98a0f2908b10bd2fa
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-01-17 21:13:58 +0100

    version bump

--- rts/Game/GameVersion.cpp
+++ rts/Game/GameVersion.cpp
@@ -14,8 +14,8 @@
 namespace SpringVersion
 {
 	
-const char* const Major = "0.78+";
-const char* const Minor = "0";
+const char* const Major = "0.78";
+const char* const Minor = "2";
 const char* const Patchset = "0";
 const char* const Additional = "";
 
