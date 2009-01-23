5b1413fcb1c6e09ab83f399e6822e77df132d5d3
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-01-23 18:25:19 +0100

    forgot to include this file in minimap patch

---
+++ rts/Rendering/Colors.h
@@ -0,0 +1,14 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+namespace color4
+{
+const unsigned char red[4]    = {255,0,0,255};
+const unsigned char redA[4]   = {255,0,0,128};
+const unsigned char yellow[4] = {255,255,0,255};
+const unsigned char green[4]  = {0,255,0,25};
+const unsigned char white[4]  = {255,255,255,25};
+};
+
+#endif
+
