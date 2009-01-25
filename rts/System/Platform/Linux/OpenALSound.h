cd8c07c1167b18fbcae16d1eeec3bd3279d5ec90
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-01-25 20:08:10 +0100

    next step to doppler effect

--- rts/System/Platform/Linux/OpenALSound.h
+++ rts/System/Platform/Linux/OpenALSound.h
@@ -15,6 +15,7 @@ public:
 	void Update();
 	void PlaySample(int id, float volume);
 	void PlaySample(int id, const float3& p,float volume);
+	void PlaySample(int id, const float3& p, const float3& velocity, float volume);
 
 	void PlayStream(const std::string& path, float volume = 1.0f,
 					const float3& pos = ZeroVector, bool loop = false);
