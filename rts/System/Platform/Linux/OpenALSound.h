6aa01beb40a33b90e69f54f11058ce49a363d311
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-01-25 19:48:39 +0100

    whitespace + add initial doppler-effect support

--- rts/System/Platform/Linux/OpenALSound.h
+++ rts/System/Platform/Linux/OpenALSound.h
@@ -14,7 +14,7 @@ public:
 	ALuint GetWaveId(const std::string& path, bool hardFail);
 	void Update();
 	void PlaySample(int id, float volume);
-	void PlaySample(int id, const float3& p, float volume);
+	void PlaySample(int id, const float3& p,float volume);
 
 	void PlayStream(const std::string& path, float volume = 1.0f,
 					const float3& pos = ZeroVector, bool loop = false);
@@ -34,7 +34,7 @@ private:
 
 private:
 	ALuint LoadALBuffer(const std::string& path);
-	void PlaySample(int id, const float3 &p, float volume, bool relative);
+	void PlaySample(int id, const float3 &p, const float3& velocity, float volume, bool relative);
 
 	int maxSounds;
 	int cur;
