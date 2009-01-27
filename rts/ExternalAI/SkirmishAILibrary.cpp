96dcd14a84301f8319e637947ec911c82eb27d32
hoijui <hoijui.quaero@gmail.com>
2009-01-27 21:57:57 +0100

    ai: give a usefull message in the infolog when the interface library did something wrong

--- rts/ExternalAI/SkirmishAILibrary.cpp
+++ rts/ExternalAI/SkirmishAILibrary.cpp
@@ -24,7 +24,19 @@
 
 CSkirmishAILibrary::CSkirmishAILibrary(const SSAILibrary& ai,
 		const SkirmishAIKey& key)
-		: sSAI(ai), key(key) {}
+		: sSAI(ai), key(key) {
+
+	if (sSAI.handleEvent == NULL) {
+		logOutput.Print(
+				"ERROR: Fetched AI library %s-%s has no handleEvent function"
+				"available. It is therefore illegal and will not be used."
+				"This usually indicates a problem in the used AI Interface"
+				"library (%s-%s).",
+				key.GetShortName().c_str(), key.GetVersion().c_str(),
+				key.GetInterface().GetShortName().c_str(),
+				key.GetInterface().GetVersion().c_str());
+	}
+}
 
 CSkirmishAILibrary::~CSkirmishAILibrary() {}
 
