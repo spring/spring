5825ae35d6689226657ed605baf8f2e6030e73e6
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-02-22 14:05:50 +0100

    don't allow unsafe functions from within lua

--- rts/Game/UI/LuaUI.cpp
+++ rts/Game/UI/LuaUI.cpp
@@ -155,7 +155,7 @@ CLuaUI::CLuaUI()
 	LUA_OPEN_LIB(L, luaopen_math);
 	LUA_OPEN_LIB(L, luaopen_table);
 	LUA_OPEN_LIB(L, luaopen_string);
-	LUA_OPEN_LIB(L, luaopen_package);
+	//LUA_OPEN_LIB(L, luaopen_package);
 	LUA_OPEN_LIB(L, luaopen_debug);
 
 	// setup the lua IO access check functions
@@ -169,11 +169,16 @@ CLuaUI::CLuaUI()
 	lua_getglobal(L, "io");
 	lua_pushstring(L, "popen"); lua_pushnil(L); lua_rawset(L, -3);
 	lua_pop(L, 1);
-	lua_getglobal(L, "os");
-	lua_pushstring(L, "exit");      lua_pushnil(L); lua_rawset(L, -3);
-	lua_pushstring(L, "execute");   lua_pushnil(L); lua_rawset(L, -3);
-	lua_pushstring(L, "setlocale"); lua_pushnil(L); lua_rawset(L, -3);
-	lua_pop(L, 1);
+	lua_getglobal(L, "os"); {
+		lua_pushliteral(L, "exit");      lua_pushnil(L); lua_rawset(L, -3);
+		lua_pushliteral(L, "execute");   lua_pushnil(L); lua_rawset(L, -3);
+		lua_pushliteral(L, "remove");    lua_pushnil(L); lua_rawset(L, -3);
+		lua_pushliteral(L, "rename");    lua_pushnil(L); lua_rawset(L, -3);
+		lua_pushliteral(L, "tmpname");   lua_pushnil(L); lua_rawset(L, -3);
+		lua_pushliteral(L, "getenv");    lua_pushnil(L); lua_rawset(L, -3);
+		lua_pushliteral(L, "setlocale"); lua_pushnil(L); lua_rawset(L, -3);
+	}
+	lua_pop(L, 1); // os
 
 	lua_pushvalue(L, LUA_GLOBALSINDEX);
 
