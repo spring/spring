21d2a46e5c09c3607848c79d8217cc67a2e29e7d
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-03-10 23:51:19 +0100

    sound: fix LogSubsystem "Sound" beeing duplicated 4 times

---
+++ rts/System/Sound/ALShared.cpp
@@ -0,0 +1,16 @@
+#include "ALShared.h"
+
+#include <AL/al.h>
+
+const CLogSubsystem LOG_SOUND("Sound", true);
+
+bool CheckError(const char* msg)
+{
+	ALenum e = alGetError();
+	if (e != AL_NO_ERROR)
+	{
+		LogObject(LOG_SOUND) << msg << ": " << (char*)alGetString(e);
+		return false;
+	}
+	return true;
+}
