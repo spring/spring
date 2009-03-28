efaf58a5f6eb05020bb475485331c47757da9cb9
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-03-29 00:44:14 +0100

    server saves player statistics for inclusion in demo

--- rts/Game/PlayerBase.h
+++ rts/Game/PlayerBase.h
@@ -2,6 +2,7 @@
 #define PLAYERBASE_H
 
 #include <string>
+#include "Platform/byteorder.h"
 
 /**
 @brief Acts as a base class for the various player-representing classes
@@ -19,6 +20,28 @@ public:
 	std::string countryCode;
 	bool spectator;
 	bool isFromDemo;
+};
+
+struct PlayerStatistics
+{
+	/// how many pixels the mouse has traversed in total
+	int mousePixels;
+	int mouseClicks;
+	int keyPresses;
+
+	int numCommands;
+	/// total amount of units affected by commands
+	/// (divide by numCommands for average units/command)
+	int unitCommands;
+
+	/// Change structure from host endian to little endian or vice versa.
+	void swab() {
+		mousePixels = swabdword(mousePixels);
+		mouseClicks = swabdword(mouseClicks);
+		keyPresses = swabdword(keyPresses);
+		numCommands = swabdword(numCommands);
+		unitCommands = swabdword(unitCommands);
+	}
 };
 
 #endif
