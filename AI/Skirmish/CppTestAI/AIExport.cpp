c8511dfb27a7a72aeb9319942cb74b73ddc8d4bb
hoijui <hoijui.quaero@gmail.com>
2009-04-13 10:07:26 +0200

    ai: test C++ AI: initial upload

---
+++ AI/Skirmish/CppTestAI/AIExport.cpp
@@ -0,0 +1,46 @@
+/*
+	Copyright 2009 Robin Vobruba <hoijui.quaero@gmail.com>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	@author Robin Vobruba <hoijui.quaero@gmail.com>
+*/
+
+#include "AIExport.h"
+
+#include "Cpp/Interface/AISEvents.h"
+#include "CppTestAI.h"
+
+static std::map<int, cpptestai::CCppTestAI*> myAIs;
+
+EXPORT(int) init(int teamId, const struct SSkirmishAICallback* innerCallback) {
+
+	myAIs[teamId] = new cpptestai::CCppTestAI(teamId, innerCallback);
+
+	// signal: everything went OK
+	return 0;
+}
+
+EXPORT(int) release(int teamId) {
+
+	delete myAIs[teamId];
+	myAIs.erase(teamId);
+
+	// signal: everything went OK
+	return 0;
+}
+
+EXPORT(int) handleEvent(int teamId, int topic, const void* data) {
+	return myAIs[teamId]->HandleEvent(topic, data);
+}
