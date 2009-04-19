e4de4e4370e36cc4017a225c1c683469fa0d0de2
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-04-19 15:27:41 +0200

    sound: allow volume set for each channel seperately ("/soundchannelvolume <Channel> [0.0, +inf[") sound: available channels until now: Battle, UnitReply, General, UserInterface

--- rts/System/Sound/Sound.cpp
+++ rts/System/Sound/Sound.cpp
@@ -30,34 +30,34 @@ AudioChannel::AudioChannel() : volume(1.0f), enabled(true)
 {
 };
 
-void AudioChannel::PlaySample(size_t id, float volume)
+void AudioChannel::PlaySample(size_t id, float ovolume)
 {
 	if (enabled)
-		sound->PlaySample(id, ZeroVector, ZeroVector, volume, true);
+		sound->PlaySample(id, ZeroVector, ZeroVector, volume * ovolume, true);
 }
 
-void AudioChannel::PlaySample(size_t id, const float3& p, float volume)
+void AudioChannel::PlaySample(size_t id, const float3& p, float ovolume)
 {
 	if (enabled)
-		sound->PlaySample(id, p, ZeroVector, volume, false);
+		sound->PlaySample(id, p, ZeroVector, volume * ovolume, false);
 }
 
-void AudioChannel::PlaySample(size_t id, const float3& p, const float3& velocity, float volume)
+void AudioChannel::PlaySample(size_t id, const float3& p, const float3& velocity, float ovolume)
 {
 	if (enabled)
-		sound->PlaySample(id, p, velocity, volume, false);
+		sound->PlaySample(id, p, velocity, volume * ovolume, false);
 }
 
-void AudioChannel::PlaySample(size_t id, CUnit* u,float volume)
+void AudioChannel::PlaySample(size_t id, CUnit* u,float ovolume)
 {
 	if (enabled)
-		sound->PlaySample(id, u->pos, u->speed, volume, false);
+		sound->PlaySample(id, u->pos, u->speed, volume * ovolume, false);
 }
 
-void AudioChannel::PlaySample(size_t id, CWorldObject* p,float volume)
+void AudioChannel::PlaySample(size_t id, CWorldObject* p,float ovolume)
 {
 	if (enabled)
-		sound->PlaySample(id, p->pos, ZeroVector, volume, false);
+		sound->PlaySample(id, p->pos, ZeroVector, volume * ovolume, false);
 }
 
 
