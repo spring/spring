733e3c11bc39244393c56a4bec80c969401cac6f
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-05-02 16:46:31 +0200

    luacob: Moved SetUnitVal/GetUnitVal and global/team/allyteam/unit vars to CUnitScript

--- rts/Sim/Units/COB/CobInstance.h
+++ rts/Sim/Units/COB/CobInstance.h
@@ -5,44 +5,14 @@
 #include "UnitScript.h"
 
 
-#define PACKXZ(x,z) (((int)(x) << 16)+((int)(z) & 0xffff))
-#define UNPACKX(xz) ((signed short)((boost::uint32_t)(xz) >> 16))
-#define UNPACKZ(xz) ((signed short)((boost::uint32_t)(xz) & 0xffff))
-
-
 class CCobThread;
 class CCobFile;
 class CCobInstance;
 
 class CCobInstance : public CUnitScript
 {
-public:
-	static const int UNIT_VAR_COUNT   = 8;
-	static const int TEAM_VAR_COUNT   = 64;
-	static const int ALLY_VAR_COUNT   = 64;
-	static const int GLOBAL_VAR_COUNT = 4096;
-
-	static const int UNIT_VAR_START   = 1024;
-	static const int TEAM_VAR_START   = 2048;
-	static const int ALLY_VAR_START   = 3072;
-	static const int GLOBAL_VAR_START = 4096;
-
-	static const int UNIT_VAR_END   = UNIT_VAR_START   + UNIT_VAR_COUNT   - 1;
-	static const int TEAM_VAR_END   = TEAM_VAR_START   + TEAM_VAR_COUNT   - 1;
-	static const int ALLY_VAR_END   = ALLY_VAR_START   + ALLY_VAR_COUNT   - 1;
-	static const int GLOBAL_VAR_END = GLOBAL_VAR_START + GLOBAL_VAR_COUNT - 1;
-
-	static void InitVars(int numTeams, int numAllyTeams);
-
 protected:
 	CCobFile& script;
-	bool yardOpen;
-
-	static std::vector<int> teamVars[TEAM_VAR_COUNT];
-	static std::vector<int> allyVars[ALLY_VAR_COUNT];
-	static int globalVars[GLOBAL_VAR_COUNT];
-
-	int unitVars[UNIT_VAR_COUNT];
 
 	virtual int RealCall(int functionId, std::vector<int> &args, CBCobThreadFinish cb, void *p1, void *p2);
 	virtual void ShowScriptError(const std::string& msg);
@@ -67,8 +37,6 @@ public:
 
 	// used by CCobThread
 	void Signal(int signal);
-	int GetUnitVal(int val, int p1, int p2, int p3, int p4);
-	void SetUnitVal(int val, int param);
 	void PlayUnitSound(int snr, int attr);
 };
 
--- rts/Sim/Units/COB/UnitScript.h
+++ rts/Sim/Units/COB/UnitScript.h
@@ -12,6 +12,11 @@
 #include "Rendering/UnitModels/3DModel.h"
 
 
+#define PACKXZ(x,z) (((int)(x) << 16)+((int)(z) & 0xffff))
+#define UNPACKX(xz) ((signed short)((boost::uint32_t)(xz) >> 16))
+#define UNPACKZ(xz) ((signed short)((boost::uint32_t)(xz) & 0xffff))
+
+
 static const int COBSCALE = 65536;
 static const int COBSCALEHALF = COBSCALE / 2;
 static const float CORDDIV   = 1.0f / COBSCALE;
@@ -30,11 +35,38 @@ public:
 	enum AnimType {ATurn, ASpin, AMove};
 
 	struct IAnimListener {
-		virtual ~IAnimListener() = 0;
+		virtual ~IAnimListener() {}
 		virtual void AnimFinished(AnimType type, int piece, int axis) = 0;
 	};
 
+public:
+	static const int UNIT_VAR_COUNT   = 8;
+	static const int TEAM_VAR_COUNT   = 64;
+	static const int ALLY_VAR_COUNT   = 64;
+	static const int GLOBAL_VAR_COUNT = 4096;
+
+	static const int UNIT_VAR_START   = 1024;
+	static const int TEAM_VAR_START   = 2048;
+	static const int ALLY_VAR_START   = 3072;
+	static const int GLOBAL_VAR_START = 4096;
+
+	static const int UNIT_VAR_END   = UNIT_VAR_START   + UNIT_VAR_COUNT   - 1;
+	static const int TEAM_VAR_END   = TEAM_VAR_START   + TEAM_VAR_COUNT   - 1;
+	static const int ALLY_VAR_END   = ALLY_VAR_START   + ALLY_VAR_COUNT   - 1;
+	static const int GLOBAL_VAR_END = GLOBAL_VAR_START + GLOBAL_VAR_COUNT - 1;
+
+	static void InitVars(int numTeams, int numAllyTeams);
+
 protected:
+	bool busy;
+	bool yardOpen;
+
+	static std::vector<int> teamVars[TEAM_VAR_COUNT];
+	static std::vector<int> allyVars[ALLY_VAR_COUNT];
+	static int globalVars[GLOBAL_VAR_COUNT];
+
+	int unitVars[UNIT_VAR_COUNT];
+
 	struct AnimInfo {
 		AnimType type;
 		int axis;
@@ -52,6 +84,14 @@ protected:
 
 	void MapScriptToModelPieces(LocalModel* lmodel);
 	void UnblockAll(struct AnimInfo * anim);
+
+	int MoveToward(float &cur, float dest, float speed);
+	int TurnToward(float &cur, float dest, float speed);
+	int DoSpin(float &cur, float dest, float &speed, float accel, int divisor);
+
+	struct AnimInfo *FindAnim(AnimType anim, int piece, int axis);
+	void RemoveAnim(AnimType anim, int piece, int axis);
+	void AddAnim(AnimType type, int piece, int axis, int speed, int dest, int accel, bool interpolated = false);
 
 	// these _must_ be implemented by any type of UnitScript
 	virtual int RealCall(int functionId, std::vector<int> &args, CBCobThreadFinish cb, void *p1, void *p2) = 0;
@@ -108,8 +148,6 @@ public:
 	};
 
 public:
-	bool busy;
-
 	CUnitScript(CUnit* unit, const std::vector<int>& scriptIndex);
 	virtual ~CUnitScript();
 
@@ -134,9 +172,7 @@ public:
 
 	int Tick(int deltaTime);
 
-	int MoveToward(float &cur, float dest, float speed);
-	int TurnToward(float &cur, float dest, float speed);
-	int DoSpin(float &cur, float dest, float &speed, float accel, int divisor);
+	// animation, used by CCobThread
 	void Spin(int piece, int axis, int speed, int accel);
 	void StopSpin(int piece, int axis, int decel);
 	void Turn(int piece, int axis, int speed, int destination, bool interpolated = false);
@@ -144,21 +180,23 @@ public:
 	void MoveNow(int piece, int axis, int destination);
 	void TurnNow(int piece, int axis, int destination);
 
+	// for smoothing turn-now / move-now animations
+	void MoveSmooth(int piece, int axis, int destination, int delta, int deltaTime);
+	void TurnSmooth(int piece, int axis, int destination, int delta, int deltaTime);
+
+	int AddTurnListener(int piece, int axis, IAnimListener* listener);
+	int AddMoveListener(int piece, int axis, IAnimListener* listener);
+
+	// misc, used by CCobThread
+	// TODO: Lua binding (except GetUnitVal/SetUnitVal)
 	void SetVisibility(int piece, bool visible);
 	void EmitSfx(int type, int piece);
 	void AttachUnit(int piece, int unit);
 	void DropUnit(int unit);
-
-	struct AnimInfo *FindAnim(AnimType anim, int piece, int axis);
-	void RemoveAnim(AnimType anim, int piece, int axis);
-	void AddAnim(AnimType type, int piece, int axis, int speed, int dest, int accel, bool interpolated = false);
-	int AddTurnListener(int piece, int axis, IAnimListener *listener);
-	int AddMoveListener(int piece, int axis, IAnimListener *listener);
-
 	void Explode(int piece, int flags);
 	void ShowFlare(int piece);
-	void MoveSmooth(int piece, int axis, int destination, int delta, int deltaTime);
-	void TurnSmooth(int piece, int axis, int destination, int delta, int deltaTime);
+	int GetUnitVal(int val, int p1, int p2, int p3, int p4);
+	void SetUnitVal(int val, int param);
 
 	bool HasScriptFunction(int id);
 	bool FunctionExist(int id);
