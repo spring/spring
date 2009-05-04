8b7f866d6b88e88b8ff7a10bc7f2376b009531d1
-jk- <-jk->
2009-05-05 00:17:07 +0200

    VertexArray  * moved inlined functions into a seperate file  * added a new type 2dT (2d vertex pos + 2d texcoords)  * optimize vertex array strip length (try to render vertices in a bunch of 2048)  * added a callback function for DrawArray2dT (so you can colors between stripes etc.)

--- rts/Rendering/GL/VertexArray.cpp
+++ rts/Rendering/GL/VertexArray.cpp
@@ -7,42 +7,194 @@
 #include "mmgr.h"
 
 #include "VertexArray.h"
+#include "LogOutput.h"
 #include "myGL.h"
+
+unsigned int CVertexArray::maxVertices = 2048; //FIXME make this customizable some how
 
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
 
-CVertexArray::CVertexArray() {
+CVertexArray::CVertexArray()
+{
 	drawArray=new float[VA_INIT_VERTEXES]; // please don't change this, some files rely on specific initial sizes
-	stripArray=new int[VA_INIT_STRIPS];
+	stripArray=new unsigned int[VA_INIT_STRIPS];
 	Initialize();
 	drawArraySize=drawArray+VA_INIT_VERTEXES;
 	stripArraySize=stripArray+VA_INIT_STRIPS;
 }
 
-CVertexArray::~CVertexArray() {
-	delete [] drawArray;
-	delete [] stripArray;
+CVertexArray::~CVertexArray()
+{
+	delete[] drawArray;
+	delete[] stripArray;
 }
 
-void CVertexArray::Initialize() {
+
+//////////////////////////////////////////////////////////////////////
+// 
+//////////////////////////////////////////////////////////////////////
+
+void CVertexArray::Initialize()
+{
 	drawArrayPos=drawArray;
 	stripArrayPos=stripArray;
 }
 
-bool CVertexArray::IsReady() {
+bool CVertexArray::IsReady()
+{
 	return true;
 }
 
-void CVertexArray::EndStrip() {
-	if((char *)stripArrayPos>(char *)stripArraySize-4*sizeof(int))
+void CVertexArray::EndStrip()
+{
+	if((char *)stripArrayPos>(char *)stripArraySize-4*sizeof(unsigned int))
 		EnlargeStripArray();
 
 	*stripArrayPos++=((char *)drawArrayPos-(char *)drawArray);
 }
 
-void CVertexArray::DrawArray0(const int drawType,int stride) {
+
+//////////////////////////////////////////////////////////////////////
+// 
+//////////////////////////////////////////////////////////////////////
+
+
+static inline bool IsPrimitiveSplitable(GLenum mode)
+{
+	switch (mode) {
+		case GL_TRIANGLE_FAN:
+		case GL_POLYGON:
+		case GL_LINE_LOOP:
+			{ return false; }
+		default:
+			{ return true; }
+	}
+}
+
+
+static inline int GetPrimitiveRestartEach(GLenum mode)
+{
+	switch (mode) {
+		case GL_TRIANGLE_STRIP:
+			{ return 2; }
+		case GL_QUAD_STRIP:
+			{ return 2; }
+		case GL_TRIANGLES:
+			{ return 3; }
+		case GL_QUADS:
+			{ return 4; }
+		case GL_LINES:
+			{ return 2; }
+		default:
+			{ return 1; }
+	}
+}
+
+
+static inline int GetStripStartOffset(GLenum mode)
+{
+	switch (mode) {
+		case GL_TRIANGLES:
+		case GL_QUAD_STRIP:
+			{ return 2; }
+		case GL_LINE_STRIP:
+			{ return 1; }
+		default:
+			{ return 0; }
+	}
+}
+
+
+void CVertexArray::DrawArrays(const GLenum mode, const unsigned int stride)
+{
+	unsigned int length;
+	unsigned int newIndex,oldIndex=0;
+	const unsigned int* stripArrayPtr=stripArray;
+
+	if (!IsPrimitiveSplitable(mode)) {
+		//! we can't split this primitive types
+		while(stripArrayPtr<stripArrayPos) {
+			newIndex=(*stripArrayPtr++)/stride;
+			length=newIndex-oldIndex;
+			glDrawArrays(mode, oldIndex, length);
+			oldIndex=newIndex;
+		}
+	} else {
+		//! split the va in optimal strip sizes, to increase the performance
+		const int optVertCount = CVertexArray::maxVertices - (CVertexArray::maxVertices % GetPrimitiveRestartEach(mode));
+		const int stripOffset  = GetStripStartOffset(mode);
+
+		while(stripArrayPtr < stripArrayPos) {
+			newIndex=(*stripArrayPtr++) / stride;
+			length=newIndex - oldIndex;
+			if (length > 1.5f * optVertCount) {
+				int spliti = length / optVertCount;
+				do {
+					glDrawArrays(mode, oldIndex, optVertCount);
+					oldIndex += optVertCount - stripOffset;
+				} while(--spliti>0);
+
+				if (newIndex > oldIndex)
+					glDrawArrays(mode, oldIndex, newIndex - oldIndex);
+			} else {
+				glDrawArrays(mode, oldIndex, length);
+			}
+			oldIndex=newIndex;
+		}
+	}
+}
+
+
+void CVertexArray::DrawArraysCallback(const GLenum mode, const unsigned int stride, StripCallback callback, void* data)
+{
+	unsigned int length;
+	unsigned int newIndex,oldIndex=0;
+	const unsigned int* stripArrayPtr=stripArray;
+
+	if (!IsPrimitiveSplitable(mode)) {
+		//! we can't split those primitive types
+		while(stripArrayPtr<stripArrayPos) {
+			callback(data);
+			newIndex=(*stripArrayPtr++)/stride;
+			length=newIndex-oldIndex;
+			glDrawArrays(mode, oldIndex, length);
+			oldIndex=newIndex;
+		}
+	} else {
+		//! split the va in optimal strip sizes, to increase the performance
+		const int optVertCount = CVertexArray::maxVertices - (CVertexArray::maxVertices % GetPrimitiveRestartEach(mode));
+		const int stripOffset  = GetStripStartOffset(mode);
+
+		while(stripArrayPtr < stripArrayPos) {
+			callback(data);
+			newIndex=(*stripArrayPtr++) / stride;
+			length=newIndex - oldIndex;
+			if (length > 1.25f * optVertCount) {
+				int spliti = length / optVertCount;
+				do {
+					glDrawArrays(mode, oldIndex, optVertCount);
+					oldIndex += optVertCount - stripOffset;
+				} while(--spliti>0);
+
+				if (newIndex > oldIndex)
+					glDrawArrays(mode, oldIndex, newIndex - oldIndex);
+			} else {
+				glDrawArrays(mode, oldIndex, length);
+			}
+			oldIndex=newIndex;
+		}
+	}
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// 
+//////////////////////////////////////////////////////////////////////
+
+void CVertexArray::DrawArray0(const int drawType,unsigned int stride)
+{
 	CheckEndStrip();
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glVertexPointer(3,GL_FLOAT,stride,drawArray);
@@ -50,7 +202,9 @@ void CVertexArray::DrawArray0(const int drawType,int stride) {
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void CVertexArray::DrawArrayC(const int drawType,int stride) {
+
+void CVertexArray::DrawArrayC(const int drawType,unsigned int stride)
+{
 	CheckEndStrip();
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
@@ -61,7 +215,9 @@ void CVertexArray::DrawArrayC(const int drawType,int stride) {
 	glDisableClientState(GL_COLOR_ARRAY);
 }
 
-void CVertexArray::DrawArrayT(const int drawType,int stride) {
+
+void CVertexArray::DrawArrayT(const int drawType,unsigned int stride)
+{
 	CheckEndStrip();
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -72,7 +228,35 @@ void CVertexArray::DrawArrayT(const int drawType,int stride) {
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void CVertexArray::DrawArrayT2(const int drawType,int stride) {
+
+void CVertexArray::DrawArray2dT(const int drawType,unsigned int stride)
+{
+	CheckEndStrip();
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(2,GL_FLOAT,stride,drawArray);
+	glTexCoordPointer(2,GL_FLOAT,stride,drawArray+2);
+	DrawArrays(drawType, stride);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);
+}
+
+
+void CVertexArray::DrawArray2dT(const int drawType, StripCallback callback, void* data, unsigned int stride)
+{
+	CheckEndStrip();
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(2,GL_FLOAT,stride,drawArray);
+	glTexCoordPointer(2,GL_FLOAT,stride,drawArray+2);
+	DrawArraysCallback(drawType, stride, callback, data);
+	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+	glDisableClientState(GL_VERTEX_ARRAY);	
+}
+
+
+void CVertexArray::DrawArrayT2(const int drawType,unsigned int stride)
+{
 	CheckEndStrip();
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -92,7 +276,9 @@ void CVertexArray::DrawArrayT2(const int drawType,int stride) {
 	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
-void CVertexArray::DrawArrayTN(const int drawType, int stride) {
+
+void CVertexArray::DrawArrayTN(const int drawType, unsigned int stride)
+{
 	CheckEndStrip();
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -106,7 +292,9 @@ void CVertexArray::DrawArrayTN(const int drawType, int stride) {
 	glDisableClientState(GL_NORMAL_ARRAY);
 }
 
-void CVertexArray::DrawArrayTC(const int drawType, int stride) {
+
+void CVertexArray::DrawArrayTC(const int drawType, unsigned int stride)
+{
 	CheckEndStrip();
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
@@ -120,28 +308,37 @@ void CVertexArray::DrawArrayTC(const int drawType, int stride) {
 	glDisableClientState(GL_COLOR_ARRAY);
 }
 
-void CVertexArray::EnlargeDrawArray() {
-	int pos=drawArrayPos-drawArray;
-	int oldsize=drawArraySize-drawArray;
-	int newsize=oldsize*2;
+
+//////////////////////////////////////////////////////////////////////
+// 
+//////////////////////////////////////////////////////////////////////
+
+void CVertexArray::EnlargeDrawArray()
+{
+	unsigned int pos=drawArrayPos-drawArray;
+	unsigned int oldsize=drawArraySize-drawArray;
+	unsigned int newsize=oldsize*2;
+
 	float* tempArray=new float[newsize];
 	memcpy(tempArray,drawArray,oldsize*sizeof(float));
 
-	delete [] drawArray;
+	delete[] drawArray;
 	drawArray=tempArray;
 	drawArraySize=drawArray+newsize;
 	drawArrayPos=drawArray+pos;
 }
 
-void CVertexArray::EnlargeStripArray() {
-	int pos=stripArrayPos-stripArray;
-	int oldsize=stripArraySize-stripArray;
-	int newsize=oldsize*2;
 
-	int* tempArray=new int[newsize];
-	memcpy(tempArray,stripArray,oldsize*sizeof(int));
+void CVertexArray::EnlargeStripArray()
+{
+	unsigned int pos=stripArrayPos-stripArray;
+	unsigned int oldsize=stripArraySize-stripArray;
+	unsigned int newsize=oldsize*2;
 
-	delete [] stripArray;
+	unsigned int* tempArray=new unsigned int[newsize];
+	memcpy(tempArray,stripArray,oldsize*sizeof(unsigned int));
+
+	delete[] stripArray;
 	stripArray=tempArray;
 	stripArraySize=stripArray+newsize;
 	stripArrayPos=stripArray+pos;
