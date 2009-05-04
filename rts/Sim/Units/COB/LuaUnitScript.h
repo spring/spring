9fd9c0def7e7070911d79179452293d850ccc00c
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-05-04 22:51:51 +0200

    luacob: remove Piece prefix now it will come in new 'namespace'

--- rts/Sim/Units/COB/LuaUnitScript.h
+++ rts/Sim/Units/COB/LuaUnitScript.h
@@ -20,17 +20,17 @@ public:
 	static int GetUnitCOBValue(lua_State* L);
 	static int SetUnitCOBValue(lua_State* L);
 	static int SetPieceVisibility(lua_State* L);
-	static int PieceEmitSfx(lua_State* L);       // TODO: better names?
-	static int PieceAttachUnit(lua_State* L);
-	static int PieceDropUnit(lua_State* L);
-	static int PieceExplode(lua_State* L);
-	static int PieceShowFlare(lua_State* L);
+	static int EmitSfx(lua_State* L);       // TODO: better names?
+	static int AttachUnit(lua_State* L);
+	static int DropUnit(lua_State* L);
+	static int Explode(lua_State* L);
+	static int ShowFlare(lua_State* L);
 
 	// Lua COB replacement animation support funcs
-	static int PieceSpin(lua_State* L);
-	static int PieceStopSpin(lua_State* L);
-	static int PieceTurn(lua_State* L);
-	static int PieceMove(lua_State* L);
+	static int Spin(lua_State* L);
+	static int StopSpin(lua_State* L);
+	static int Turn(lua_State* L);
+	static int Move(lua_State* L);
 };
 
 #endif
