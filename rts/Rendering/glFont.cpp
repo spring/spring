224df3e81a7677142263666b8ce1ac05558ebe5d
-jk- <-jk->
2009-05-06 01:45:11 +0200

    missing pointer dereferences

--- rts/Rendering/glFont.cpp
+++ rts/Rendering/glFont.cpp
@@ -679,7 +679,7 @@ static inline bool SkipColorCodesAndNewLines(const std::string& text, T* c, floa
 
 static inline bool CompareColors(const float4* color1, const float4* color2)
 {
-	return (color1[0] == color2[0]) && (color1[1] == color2[1]) && (color1[2] == color2[2]) && (color1[3] == color2[3]);
+	return ((*color1)[0] == (*color2)[0]) && ((*color1)[1] == (*color2)[1]) && ((*color1)[2] == (*color2)[2]) && ((*color1)[3] == (*color2)[3]);
 }
 
 
