80ecca10d8d9440a8f2aa112d1a5437d7c197d6c
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-05-09 17:37:20 +0200

    sound: default MapDraw-sound now plays at its postion, but without distance attenuation

--- installer/builddata/springcontent/gamedata/sounds.lua
+++ installer/builddata/springcontent/gamedata/sounds.lua
@@ -3,15 +3,19 @@
 local Sounds = {
 	SoundItems = {
 		IncomingChat = {
+			--- always play on the front speaker(s)
 			file = "sounds/beep4.wav",
 			in3d = "false",
 		},
 		MultiSelect = {
+			--- always play on the front speaker(s)
 			file = "sounds/button9.wav",
 			in3d = "false",
 		},
 		MapPoint = {
+			--- respect where the point was set, but don't attuenuate in distace
 			file = "sounds/beep6.wav",
+			rolloff = 0,
 		},
 	},
 }
