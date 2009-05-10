8a9fe106c8f64fbc0c4b1d2965d980a5354295ad
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-05-10 20:44:53 +0200

    sound: fix rolloff factor beeing 10 times to low

--- rts/System/Sound/SoundSource.cpp
+++ rts/System/Sound/SoundSource.cpp
@@ -92,7 +92,7 @@ void SoundSource::Play(SoundItem* item, const float3& pos, float3 velocity, floa
 	{
 		alSourcei(id, AL_SOURCE_RELATIVE, AL_FALSE);
 		alSource3f(id, AL_POSITION, pos.x, pos.y, pos.z);
-		alSourcef(id, AL_ROLLOFF_FACTOR, item->rolloff * 0.04f);
+		alSourcef(id, AL_ROLLOFF_FACTOR, item->rolloff * 0.4f);
 	}
 	alSourcePlay(id);
 
