fdd3e9c059d6dd5a3b0251ff88c5702b83350d2c
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-05-10 12:52:19 +0200

    some documentation on sounds.lua

--- installer/builddata/springcontent/gamedata/sounds.lua
+++ installer/builddata/springcontent/gamedata/sounds.lua
@@ -14,9 +14,46 @@ local Sounds = {
 		},
 		MapPoint = {
 			--- respect where the point was set, but don't attuenuate in distace
+			--- also, when moving the camera, don't pitch it
 			file = "sounds/beep6.wav",
 			rolloff = 0,
 			dopplerscale = 0,
+		},
+		ExampleSound = {
+			--- some things you can do with this file
+
+			--- can be either ogg or wav
+			file = "somedir/subdir/soundfile.ogg",
+
+			--- loudness, > 1 is louder, < 1  is more quiet, you will most likely not set it to 0
+			gain = 1,
+
+			--- > 1 -> high pitched, < 1 lowered
+			pitch = 1,
+
+			--- how unit / camera speed affects the sound, to exagerate it, use values > 1
+			--- dopplerscale = 0 completely disables the effect
+			dopplerscale = 1,
+
+			--- when lots of sounds are played, sounds with lwoer priority are more likely to get cut off
+			--- priority > 0 will never be cut of (priorities can be negative)
+			priority = 0,
+
+			--- this sound will not be played more than 16 times at a time
+			maxconcurrent = 16,
+
+			--- cutoff distance
+			maxdist = 20000,
+
+			--- how fast it becomes more quiet in the distance (0 means aleays the same loudness regardless of dist)
+			rolloff = 1,
+
+			--- non-3d sounds do always came out of the front-speakers (or the center one)
+			--- 3d sounds are, well, in 3d
+			in3d = true,
+
+			--- you can loop it for X miliseconds
+			looptime = 0,
 		},
 	},
 }
