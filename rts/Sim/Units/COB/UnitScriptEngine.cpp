651739acfa0bf1cf07cf5fe439d01b15250c9cea
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-05-13 21:13:05 +0200

    touchups, disabled debugging code

--- rts/Sim/Units/COB/UnitScriptEngine.cpp
+++ rts/Sim/Units/COB/UnitScriptEngine.cpp
@@ -9,18 +9,21 @@
 #include "Platform/errorhandler.h"
 
 #ifndef _CONSOLE
-#include "TimeProfiler.h"
-#endif
-#ifdef _CONSOLE
-#define START_TIME_PROFILE(a) {}
-#define END_TIME_PROFILE(a) {}
-#define SCOPED_TIMER(a) {}
+#  include "TimeProfiler.h"
+#else
+#  define START_TIME_PROFILE(a) {}
+#  define END_TIME_PROFILE(a) {}
+#  define SCOPED_TIMER(a) {}
 #endif
 
 #include "mmgr.h"
 
 
 CUnitScriptEngine GUnitScriptEngine;
+
+
+/******************************************************************************/
+/******************************************************************************/
 
 
 CUnitScriptEngine::CUnitScriptEngine()
@@ -33,11 +36,11 @@ CUnitScriptEngine::~CUnitScriptEngine()
 }
 
 
-void CUnitScriptEngine::CheckForDuplicates(const char* name, CUnitScript *instance)
+void CUnitScriptEngine::CheckForDuplicates(const char* name, CUnitScript* instance)
 {
 	int found = 0;
 
-	for (std::list<CUnitScript *>::iterator i = animating.begin(); i != animating.end(); ++i) {
+	for (std::list<CUnitScript*>::iterator i = animating.begin(); i != animating.end(); ++i) {
 		if (*i == instance)
 			found++;
 	}
@@ -52,14 +55,14 @@ void CUnitScriptEngine::AddInstance(CUnitScript *instance)
 	animating.push_front(instance);
 
 	// Error checking
-	CheckForDuplicates(__FUNCTION__, instance); //TODO: disable
+	//CheckForDuplicates(__FUNCTION__, instance);
 }
 
 
 void CUnitScriptEngine::RemoveInstance(CUnitScript *instance)
 {
 	// Error checking
-	CheckForDuplicates(__FUNCTION__, instance); //TODO: disable
+	//CheckForDuplicates(__FUNCTION__, instance);
 
 	//This is slow. would be better if instance was a hashlist perhaps
 	animating.remove(instance);
@@ -71,11 +74,15 @@ void CUnitScriptEngine::Tick(int deltaTime)
 	SCOPED_TIMER("Scripts");
 
 	// Tick all instances that have registered themselves as animating
-	std::list<CUnitScript *>::iterator it = animating.begin();
-	std::list<CUnitScript *>::iterator curit;
+	std::list<CUnitScript*>::iterator it = animating.begin();
+	std::list<CUnitScript*>::iterator curit;
 	while (it != animating.end()) {
 		curit = it++;
 		if ((*curit)->Tick(deltaTime) == -1)
 			animating.erase(curit);
 	}
 }
+
+
+/******************************************************************************/
+/******************************************************************************/
