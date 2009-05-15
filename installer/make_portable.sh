56ea5e916ba4b7ae31dcfc7869c426534ac410b9
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-05-15 20:02:42 +0200

    make_portable.sh: add springlobby.conf for portable mode

--- installer/make_portable.sh
+++ installer/make_portable.sh
@@ -61,6 +61,7 @@ mkdir -p $dir_in_archive
 cp -r game/* $dir_in_archive
 cp installer/downloads/*.exe installer/downloads/TASServer.jar $dir_in_archive
 cp -r installer/Springlobby/SettingsDlls/* installer/Springlobby/SLArchive/* $dir_in_archive
+touch $dir_in_archive/springlobby.conf # make SL go into portable mode
 
 /usr/bin/zip -dc -dg -q -r -u -9 "$archive_filename" $dir_in_archive
 
