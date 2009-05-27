abab904a25970d35327dc120df1f545ad4fbf2ee
Kloot <blauw_scherm@hotmail.com>
2009-05-27 23:49:20 +0200

    * fix units taking no damage from water (acid, lava) (Mantis 1420)

--- rts/Sim/Units/Unit.cpp
+++ rts/Sim/Units/Unit.cpp
@@ -755,9 +755,13 @@ void CUnit::SlowUpdate()
 			}
 			UpdateResources();
 		}
+
+		// damage nano-frames too
+		DoWaterDamage();
 		return;
 	}
-	//below is stuff that shouldnt be run while being built
+
+	// below is stuff that should not be run while being built
 
 	lastSlowUpdate=gs->frameNum;
 
@@ -860,20 +864,7 @@ void CUnit::SlowUpdate()
 		}
 	}
 
-	if (uh->waterDamage) {
-		if (pos.IsInBounds()) {
-			bool inWater = (pos.y <= -3);
-			bool isFloating = (physicalState == CSolidObject::Floating);
-			bool onGround = (physicalState == CSolidObject::OnGround);
-			bool isWaterSquare = (readmap->mipHeightmap[1][int((pos.z / (SQUARE_SIZE * 2)) * gs->hmapx + (pos.x / (SQUARE_SIZE * 2)))] < -1);
-
-			// old: "floating or (on ground and height < -3 and mapheight < -1)"
-			// new: "height < -3 and (floating or on ground) and mapheight < -1"
-			if (inWater && (isFloating || onGround) && isWaterSquare) {
-				DoDamage(DamageArray() * uh->waterDamage, 0, ZeroVector, -1);
-			}
-		}
-	}
+	DoWaterDamage();
 
 	if (unitDef->canKamikaze) {
 		if (fireState >= 2) {
@@ -944,6 +935,24 @@ void CUnit::SetDirectionFromHeading(void)
 	}
 }
 
+
+
+void CUnit::DoWaterDamage()
+{
+	if (uh->waterDamage > 0.0f) {
+		if (pos.IsInBounds()) {
+			const int  px            = int(pos.x / (SQUARE_SIZE * 2));
+			const int  pz            = int(pos.z / (SQUARE_SIZE * 2));
+			const bool isFloating    = (physicalState == CSolidObject::Floating);
+			const bool onGround      = (physicalState == CSolidObject::OnGround);
+			const bool isWaterSquare = (readmap->mipHeightmap[1][pz * gs->hmapx + px] <= 0.0f);
+
+			if (inWater && isWaterSquare && (isFloating || onGround)) {
+				DoDamage(DamageArray() * uh->waterDamage, 0, ZeroVector, -1);
+			}
+		}
+	}
+}
 
 void CUnit::DoDamage(const DamageArray& damages, CUnit *attacker,const float3& impulse, int weaponId)
 {
@@ -1122,10 +1131,13 @@ void CUnit::DoDamage(const DamageArray& damages, CUnit *attacker,const float3& i
 #endif
 }
 
+
+
 void CUnit::Kill(float3& impulse) {
 	DamageArray da;
 	DoDamage(da * (health / da[armorType]), 0, impulse, -1);
 }
+
 
 
 void CUnit::UpdateDrawPos() {
