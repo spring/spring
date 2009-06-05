bad9e6e627f29fc6f30854365f628f17a86dce6e
-jk- <-jk->
2009-06-05 03:01:16 +0200

    GML fix of ThreadContainers

--- rts/Rendering/Env/DynWater.cpp
+++ rts/Rendering/Env/DynWater.cpp
@@ -453,7 +453,7 @@ void CDynWater::DrawReflection(CGame* game)
 
 	unitDrawer->Draw(true);
 	featureHandler->Draw();
-	unitDrawer->DrawCloakedUnits(false,true);
+	unitDrawer->DrawCloakedUnits(false);
 	featureHandler->DrawFadeFeatures(false,true);
 
 	ph->Draw(true);
@@ -512,7 +512,7 @@ void CDynWater::DrawRefraction(CGame* game)
 	drawReflection=true;
 	unitDrawer->Draw(false,true);
 	featureHandler->Draw();
-	unitDrawer->DrawCloakedUnits(true,true);
+	unitDrawer->DrawCloakedUnits(true);
 	featureHandler->DrawFadeFeatures(true,true); // FIXME: Make it fade out correctly without "noAdvShading"
 	drawReflection=false;
 	ph->Draw(false,true);
