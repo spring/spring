9129f9520b7e4b33a80a59ad1e42f1c8db249665
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-06-12 23:12:29 +0200

    server: don't forward spoofed giveaway / resign - messages (and print warning)
    Signed-off-by: Karl-Robert Ernst <k-r.ernst@my-mail.ch>

--- rts/Game/GameServer.cpp
+++ rts/Game/GameServer.cpp
@@ -849,7 +849,7 @@ void CGameServer::ProcessPacket(const unsigned playernum, boost::shared_ptr<cons
 		}
 		case NETMSG_TEAM:
 		{
-						//TODO update players[] and teams[] and send all to hostif
+			//TODO update players[] and teams[] and send all to hostif
 			const unsigned player = (unsigned)inbuf[1];
 			if (player != a)
 			{
@@ -869,6 +869,11 @@ void CGameServer::ProcessPacket(const unsigned playernum, boost::shared_ptr<cons
 				switch (action)
 				{
 					case TEAMMSG_GIVEAWAY: {
+						if (players[player].spectator)
+						{
+							Message(str( boost::format("Spectator %s tried to hack the game (spoofed TEAMMSG_GIVEAWAY)") %players[player].name), true);
+							break;
+						}
 						const unsigned toTeam = inbuf[3];
 						Broadcast(CBaseNetProtocol::Get().SendGiveAwayEverything(player, toTeam));
 						if (numPlayersInTeam <= 1)
@@ -881,6 +886,11 @@ void CGameServer::ProcessPacket(const unsigned playernum, boost::shared_ptr<cons
 						break;
 					}
 					case TEAMMSG_RESIGN: {
+						if (players[player].spectator)
+						{
+							Message(str(boost::format("Spectator %s tried to hack the game (spoofed TEAMMSG_RESIGN)") %players[player].name), true);
+							break;
+						}
 						Broadcast(CBaseNetProtocol::Get().SendResign(player));
 						players[player].team = 0;
 						players[player].spectator = true;
