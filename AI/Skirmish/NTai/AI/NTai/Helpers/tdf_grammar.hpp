b94c188b453de47b15268b841e1bd7eec8617759
hoijui <hoijui.quaero@gmail.com>
2009-06-19 09:40:43 +0200

    Revert "AI: NTai: separate the AIs custom TDF parser more from the engine internal one [Societal's patch]"
    This reverts commit 0939285e09caaedfff71382d8a3156e4d166cdf9.

--- AI/Skirmish/NTai/AI/NTai/Helpers/tdf_grammar.hpp
+++ AI/Skirmish/NTai/AI/NTai/Helpers/tdf_grammar.hpp
@@ -1,5 +1,5 @@
-#ifndef NTAI_TDF_GRAMMAR_HPP_INCLUDED
-#define NTAI_TDF_GRAMMAR_HPP_INCLUDED
+#ifndef TDF_GRAMMAR_HPP_INCLUDED
+#define TDF_GRAMMAR_HPP_INCLUDED 
 #include <map>
 #include <iostream>
 #include <string>
@@ -15,7 +15,7 @@
 #include <boost/spirit/iterator/position_iterator.hpp>
 #include <boost/spirit/phoenix/binders.hpp>
 #include <boost/spirit/error_handling/exceptions.hpp>
-//#include "NTAI_TdfParser.h"
+//#include "TdfParser.h"
 
 namespace ntai {
 	/**
@@ -144,5 +144,5 @@ namespace ntai {
 	};
 
 }
-#endif // NTAI_TDF_GRAMMAR_HPP_INCLUDED
+#endif
 
