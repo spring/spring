spring/spring

commit 644e42f46396a47e02befee0b42d57b4775f488f

lua: you can now change the team/allyteam of a feature in FeatureCreated

rts/Sim/Features/FeatureHandler.cpp
@@ -232,7 +232,7 @@ const FeatureDef* CFeatureHandler::CreateFeatureDef(const LuaTable& fdTable,
 	fd->maxHealth   = fdTable.GetFloat("damage", 0.0f);
 	fd->reclaimTime = fdTable.GetFloat("reclaimTime", (fd->metal + fd->energy)*6.f);
 
-	fd->smokeTime = fdTable.GetInt("smokeTime", 300);
+	fd->smokeTime = fdTable.GetInt("smokeTime", (fd->blocking) ? 300 : 0);
 
 	fd->drawType = fdTable.GetInt("drawType", DRAWTYPE_MODEL);
 	fd->modelname = fdTable.GetString("object", "");
@@ -383,9 +383,9 @@ void CFeatureHandler::LoadFeaturesFromMap(bool onlyCreateDefs)
 			}
 
 			const float ypos = ground->GetHeight2(mfi[a].pos.x, mfi[a].pos.z);
-			(new CFeature)->Initialize (float3(mfi[a].pos.x, ypos, mfi[a].pos.z),
+			(new CFeature)->Initialize(float3(mfi[a].pos.x, ypos, mfi[a].pos.z),
 			                                 def->second, (short int)mfi[a].rotation,
-			                                 0, -1, "");
+			                                 0, -1, 0, "");
 		}
 		delete[] mfi;
 	}
@@ -440,7 +440,7 @@ CFeature* CFeatureHandler::CreateWreckage(const float3& pos, const std::string& 
 
 	int i = iter;
 	do {
-		if (name.empty()) return NULL;
+		if (defname->empty()) return NULL;
 		fd = GetFeatureDef(*defname);
 		if (!fd) return NULL;
 		defname = &(fd->deathFeature);
@@ -454,12 +454,7 @@ CFeature* CFeatureHandler::CreateWreckage(const float3& pos, const std::string& 
 			fromUnit = "";
 
 		CFeature* f = new CFeature;
-		f->Initialize(pos, fd, (short int) rot, facing, team, fromUnit, speed);
-
-		// allow area-reclaiming wrecks of all units, including your own (they set allyteam = -1)
-		f->allyteam = allyteam;
-		if (emitSmoke && fd->blocking)
-			f->emitSmokeTime = fd->smokeTime;
+		f->Initialize(pos, fd, (short int) rot, facing, team, allyteam, fromUnit, speed, fd->smokeTime);
 
 		return f;
 	}
@@ -713,8 +708,8 @@ void CFeatureDrawer::DrawQuad(int x, int y)
 		CFeature* f = (*fi);
 		const FeatureDef* def = f->def;
 
-		if ((f->allyteam == -1 || f->allyteam == gu->myAllyTeam || gu->spectatingFullView ||
-			loshandler->InLos(f->pos, gu->myAllyTeam)) && def->drawType == DRAWTYPE_MODEL) {
+		if (def->drawType == DRAWTYPE_MODEL && (f->allyteam == -1 || f->allyteam == gu->myAllyTeam ||
+			gu->spectatingFullView || loshandler->InLos(f->pos, gu->myAllyTeam)) ) {
 
 			if (drawReflection) {
 				float3 zeroPos;

rts/Game/Game.cpp
@@ -2304,7 +2304,7 @@ void CGame::ActionReceived(const Action& action, int playernum)
 							float minposy = ground->GetHeight2(minposx, minposz);
 							const float3 upos(minposx, minposy, minposz);
 							CFeature* feature = new CFeature();
-							feature->Initialize(upos, featureDef, 0, 0, team, "");
+							feature->Initialize(upos, featureDef, 0, 0, team, teamHandler->AllyTeam(team), "");
 							--total;
 						}
 					}

rts/Lua/LuaSyncedCtrl.cpp
@@ -2186,7 +2186,7 @@ int LuaSyncedCtrl::CreateFeature(lua_State* L)
 	// use SetFeatureResurrect() to fill in the missing bits
 	inCreateFeature = true;
 	CFeature* feature = new CFeature();
-	feature->Initialize(pos, featureDef, heading, 0, team, "");
+	feature->Initialize(pos, featureDef, heading, 0, team, allyTeam, "");
 	inCreateFeature = false;
 
 	lua_pushnumber(L, feature->id);

rts/Sim/Features/Feature.cpp
@@ -137,7 +137,7 @@ void CFeature::ChangeTeam(int newTeam)
 
 
 void CFeature::Initialize(const float3& _pos, const FeatureDef* _def, short int _heading,
-	int facing, int _team, std::string fromUnit, const float3& speed)
+	int facing, int _team, int _allyteam, std::string fromUnit, const float3& speed, int _smokeTime)
 {
 	pos = _pos;
 	def = _def;
@@ -145,9 +145,9 @@ void CFeature::Initialize(const float3& _pos, const FeatureDef* _def, short int 
 	heading = _heading;
 	buildFacing = facing;
 	team = _team;
+	allyteam = _allyteam;
+	emitSmokeTime = _smokeTime;
 	createdFromUnit = fromUnit;
-
-	ChangeTeam(team);
 
 	pos.CheckInBounds();
 

rts/Sim/Features/Feature.h
@@ -30,7 +30,7 @@ public:
 
 	/** Pos of quad must not change after this. */
 	void Initialize(const float3& pos, const FeatureDef* def, short int heading, int facing,
-		int allyteam, std::string fromUnit, const float3& speed = ZeroVector);
+		int team, int allyteam, std::string fromUnit, const float3& speed = ZeroVector, int smokeTime = 0);
 	int GetBlockingMapID() const { return id + (10 * uh->MaxUnits()); }
 
 	/** Negative amount = reclaim

commit 3ead1aca6748510f309be6b242c22299d54bef42

Fix emitSmoke parameter of CreateWreckage.
This fixes dragon teeth and tank obstacles emitting smoke when built.

This was probably broken in 644e42f46396a47e02befee0b42d57b4775f488f.

rts/Sim/Features/FeatureHandler.cpp
@@ -454,7 +454,7 @@ CFeature* CFeatureHandler::CreateWreckage(const float3& pos, const std::string& 
 			fromUnit = "";
 
 		CFeature* f = new CFeature;
-		f->Initialize(pos, fd, (short int) rot, facing, team, allyteam, fromUnit, speed, fd->smokeTime);
+		f->Initialize(pos, fd, (short int) rot, facing, team, allyteam, fromUnit, speed, emitSmoke ? fd->smokeTime : 0);
 
 		return f;
 	}
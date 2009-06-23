3ead1aca6748510f309be6b242c22299d54bef42
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-06-23 22:18:45 +0200

    Fix emitSmoke parameter of CreateWreckage.
    This fixes dragon teeth and tank obstacles emitting smoke when built.
    
    This was probably broken in 644e42f46396a47e02befee0b42d57b4775f488f.

--- rts/Sim/Features/FeatureHandler.cpp
+++ rts/Sim/Features/FeatureHandler.cpp
@@ -454,7 +454,7 @@ CFeature* CFeatureHandler::CreateWreckage(const float3& pos, const std::string& 
 			fromUnit = "";
 
 		CFeature* f = new CFeature;
-		f->Initialize(pos, fd, (short int) rot, facing, team, allyteam, fromUnit, speed, fd->smokeTime);
+		f->Initialize(pos, fd, (short int) rot, facing, team, allyteam, fromUnit, speed, emitSmoke ? fd->smokeTime : 0);
 
 		return f;
 	}
