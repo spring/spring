1656f1cfabc02590d4878e6ac922b7dc65e1fe30
Marek Baczynski <imbaczek@gmail.com>
2009-06-25 13:31:55 +0200

    create infolog.txt in My Documents\My Games\Spring if cwd isn't writable
    also put ext.txt in the writable dir

--- rts/Rendering/GL/myGL.cpp
+++ rts/Rendering/GL/myGL.cpp
@@ -8,6 +8,7 @@
 #include "Rendering/glFont.h"
 #include "VertexArray.h"
 #include "VertexArrayRange.h"
+#include "FileSystem/FileSystem.h"
 #include "FileSystem/FileHandler.h"
 #include "Game/GameVersion.h"
 #include "Rendering/Textures/Bitmap.h"
@@ -133,7 +134,8 @@ void LoadExtensions()
 	for (unsigned int i=0; i<s.length(); i++)
 		if (s[i]==' ') s[i]='\n';
 
-	std::ofstream ofs("ext.txt");
+	std::string filename = FileSystemHandler::GetInstance().GetWriteDir() + "ext.txt";
+	std::ofstream ofs(filename.c_str());
 
 	if (!ofs.bad() && ofs.is_open())
 		ofs.write(s.c_str(), s.length());
