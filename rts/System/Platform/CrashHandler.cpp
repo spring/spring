3536ed3eee7a96fc187b9006749ec3cc14128848
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-07-02 01:04:02 +0200

    fix stacktracedumper on linux + use addr2line to translate adddresses into function names

--- rts/System/Platform/CrashHandler.cpp
+++ rts/System/Platform/CrashHandler.cpp
@@ -21,46 +21,72 @@ namespace CrashHandler {
 
 #include <string>
 #include <vector>
+#include <queue>
 #include <signal.h>
 #include <execinfo.h>
+#include <stdlib.h>
 
 #include "LogOutput.h"
 #include "errorhandler.h"
 #include "Game/GameVersion.h"
+#include "Platform/Misc.h"
 
 namespace CrashHandler {
 	
 	void HandleSignal(int signal)
 	{
-		logOutput.RemoveAllSubscribers();
-		LogObject log;
 		std::string error;
-		if (signal == SIGSEGV)
-			error = "Segmentation fault (SIGSEGV)";
-		else if (signal == SIGILL)
-			error = "Illegal instruction (SIGILL)";
-		else if (signal == SIGPIPE)
-			error = "Broken pipe (SIGPIPE)";
-		else if (signal == SIGIO)
-			error = "IO-Error (SIGIO)";
-		log << error << " in spring " << SpringVersion::GetFull() << "\nStacktrace:\n";
+		std::queue<std::string> addresses;
+		logOutput.RemoveAllSubscribers();
+		{
+			LogObject log;
+			if (signal == SIGSEGV)
+				error = "Segmentation fault (SIGSEGV)";
+			else if (signal == SIGILL)
+				error = "Illegal instruction (SIGILL)";
+			else if (signal == SIGPIPE)
+				error = "Broken pipe (SIGPIPE)";
+			else if (signal == SIGIO)
+				error = "IO-Error (SIGIO)";
+			log << error << " in spring " << SpringVersion::GetFull() << "\nStacktrace:\n";
 
-		std::vector<void*> buffer(128);
-		const int numptr = backtrace(&buffer[0], buffer.size()); // stack pointers
-		char ** strings = backtrace_symbols(&buffer[0], numptr); // give them meaningfull names
-		if (strings == NULL)
-		{
-			log << "Unable to create stacktrace\n";
-		}
-		else
-		{
-			for (int j = 0; j < numptr; j++)
+			std::vector<void*> buffer(128);
+			const int numptr = backtrace(&buffer[0], buffer.size()); // stack pointers
+			char ** strings = backtrace_symbols(&buffer[0], numptr); // give them meaningfull names
+			if (strings == NULL)
 			{
-				log << strings[j] << "\n";
+				log << "Unable to create stacktrace\n";
 			}
-			delete strings;
+			else
+			{
+				for (int j = 0; j < numptr; j++)
+				{
+					std::string addrline(strings[j]);
+					log << addrline << "\n";
+					size_t begin = addrline.find_first_of('[');
+					if (begin != std::string::npos)
+					{
+						size_t end = addrline.find_first_of(']');
+						if (end != std::string::npos)
+						{
+							addresses.push(addrline.substr(begin+1, end-begin-1));
+						}
+					}
+				}
+				delete strings;
+			}
 		}
 		logOutput.End();  // Stop writing to log.
+
+		std::ostringstream buf;
+		buf << "addr2line " << "--exe=\"" << Platform::GetBinaryFile() << "\"";
+		while (!addresses.empty())
+		{
+			buf << " " << addresses.front();
+			addresses.pop();
+		}
+		buf << " >> infolog.txt"; // pipe to infolog (which will be in CWD)
+		system(buf.str().c_str());
 		ErrorMessageBox(error, "Spring crashed", 0);
 	};
 	
