24baf0bcfc71a6d6b602ff7cfbba36445cf052d5
Auswaschbar <k-r.ernst@my-mail.ch>
2009-07-26 16:32:20 +0200

    TextElement

---
+++ rts/aGui/TextElement.cpp
@@ -0,0 +1,14 @@
+#include "TextElement.h"
+
+#include "Rendering/glFont.h"
+
+TextElement::TextElement(const std::string& _text, GuiElement* parent) : GuiElement(parent), text(_text)
+{
+}
+
+void TextElement::DrawSelf()
+{
+	font->SetTextColor(); //default
+	font->SetOutlineColor(0.0f, 0.0f, 0.0f, 1.0f);
+	font->glPrint(pos[0]+0.05, pos[1]+size[1]/2, 1.0, FONT_VCENTER | FONT_SHADOW | FONT_SCALE | FONT_NORM, text);
+}
