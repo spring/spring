cdf54d925da149f119961f209f97f12bba663588
Tobi Vollebregt <tobivollebregt@gmail.com>
2009-07-28 20:39:37 +0200

    factor COB #defines into separate header file

---
+++ rts/Sim/Units/COB/CobDefines.h
@@ -0,0 +1,96 @@
+#ifndef COBDEFINES_H
+#define COBDEFINES_H
+
+// Indices for set/get value
+#define ACTIVATION           1  // set or get
+#define STANDINGMOVEORDERS   2  // set or get
+#define STANDINGFIREORDERS   3  // set or get
+#define HEALTH               4  // get (0-100%)
+#define INBUILDSTANCE        5  // set or get
+#define BUSY                 6  // set or get (used by misc. special case missions like transport ships)
+#define PIECE_XZ             7  // get
+#define PIECE_Y              8  // get
+#define UNIT_XZ              9  // get
+#define UNIT_Y              10  // get
+#define UNIT_HEIGHT         11  // get
+#define XZ_ATAN             12  // get atan of packed x,z coords
+#define XZ_HYPOT            13  // get hypot of packed x,z coords
+#define ATAN                14  // get ordinary two-parameter atan
+#define HYPOT               15  // get ordinary two-parameter hypot
+#define GROUND_HEIGHT       16  // get land height, 0 if below water
+#define BUILD_PERCENT_LEFT  17  // get 0 = unit is built and ready, 1-100 = How much is left to build
+#define YARD_OPEN           18  // set or get (change which plots we occupy when building opens and closes)
+#define BUGGER_OFF          19  // set or get (ask other units to clear the area)
+#define ARMORED             20  // set or get
+
+/*#define WEAPON_AIM_ABORTED  21
+#define WEAPON_READY        22
+#define WEAPON_LAUNCH_NOW   23
+#define FINISHED_DYING      26
+#define ORIENTATION         27*/
+#define IN_WATER            28
+#define CURRENT_SPEED       29
+//#define MAGIC_DEATH         31
+#define VETERAN_LEVEL       32
+#define ON_ROAD             34
+
+#define MAX_ID                    70
+#define MY_ID                     71
+#define UNIT_TEAM                 72
+#define UNIT_BUILD_PERCENT_LEFT   73
+#define UNIT_ALLIED               74
+#define MAX_SPEED                 75
+#define CLOAKED                   76
+#define WANT_CLOAK                77
+#define GROUND_WATER_HEIGHT       78 // get land height, negative if below water
+#define UPRIGHT                   79 // set or get
+#define	POW                       80 // get
+#define PRINT                     81 // get, so multiple args can be passed
+#define HEADING                   82 // get
+#define TARGET_ID                 83 // get
+#define LAST_ATTACKER_ID          84 // get
+#define LOS_RADIUS                85 // set or get
+#define AIR_LOS_RADIUS            86 // set or get
+#define RADAR_RADIUS              87 // set or get
+#define JAMMER_RADIUS             88 // set or get
+#define SONAR_RADIUS              89 // set or get
+#define SONAR_JAM_RADIUS          90 // set or get
+#define SEISMIC_RADIUS            91 // set or get
+#define DO_SEISMIC_PING           92 // get
+#define CURRENT_FUEL              93 // set or get
+#define TRANSPORT_ID              94 // get
+#define SHIELD_POWER              95 // set or get
+#define STEALTH                   96 // set or get
+#define CRASHING                  97 // set or get, returns whether aircraft isCrashing state
+#define CHANGE_TARGET             98 // set, the value it's set to determines the affected weapon
+#define CEG_DAMAGE                99 // set
+#define COB_ID                   100 // get
+#define PLAY_SOUND               101 // get, so multiple args can be passed
+#define KILL_UNIT                102 // get KILL_UNIT(unitId, SelfDestruct=true, Reclaimed=false)
+#define ALPHA_THRESHOLD          103 // set or get
+#define SET_WEAPON_UNIT_TARGET   106 // get (fake set)
+#define SET_WEAPON_GROUND_TARGET 107 // get (fake set)
+#define SONAR_STEALTH            108 // set or get
+#define REVERSING                109 // get
+
+// NOTE: [LUA0 - LUA9] are defined in CobThread.cpp as [110 - 119]
+
+#define FLANK_B_MODE             120 // set or get
+#define FLANK_B_DIR              121 // set or get, set is through get for multiple args
+#define FLANK_B_MOBILITY_ADD     122 // set or get
+#define FLANK_B_MAX_DAMAGE       123 // set or get
+#define FLANK_B_MIN_DAMAGE       124 // set or get
+#define WEAPON_RELOADSTATE       125 // get (with fake set)
+#define WEAPON_RELOADTIME        126 // get (with fake set)
+#define WEAPON_ACCURACY          127 // get (with fake set)
+#define WEAPON_SPRAY             128 // get (with fake set)
+#define WEAPON_RANGE             129 // get (with fake set)
+#define WEAPON_PROJECTILE_SPEED  130 // get (with fake set)
+#define MIN                      131 // get
+#define MAX                      132 // get
+#define ABS                      133 // get
+#define GAME_FRAME               134 // get
+
+// NOTE: shared variables use codes [1024 - 5119]
+
+#endif
