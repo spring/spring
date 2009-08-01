e42ad11242222292fc91aff70e14bb265f29ef94
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-08-01 16:40:29 +0200

    make HorizontalLayout respect fixed width too

--- rts/aGui/HorizontalLayout.cpp
+++ rts/aGui/HorizontalLayout.cpp
@@ -21,20 +21,39 @@ void HorizontalLayout::DrawSelf()
 	if (borderWidth > 0)
 	{
 		glLineWidth(borderWidth);
-		glColor4f(1.f,1.f,1.f, 1.f);	
+		glColor4f(1.f,1.f,1.f, 1.f);
 		DrawBox(GL_LINE_LOOP);
 	}
 }
 
 void HorizontalLayout::GeometryChangeSelf()
 {
-	const float hspacePerObject = (size[0]-(float(children.size()-1)*spacing))/float(children.size());
-	size_t num = 0;
+	unsigned numFixed = 0;
+	float totalFixedSize = 0.0;
+	for (ChildList::const_iterator it = children.begin(); it != children.end(); ++it)
+	{
+		if ((*it)->SizeFixed())
+		{
+			numFixed++;
+			totalFixedSize += (*it)->GetSize()[0];
+		}
+	}
+
+	const float hspacePerObject = (size[0]-(float(children.size()+1)*spacing)-totalFixedSize)/float(children.size()-numFixed);
+	float startX = pos[0] + spacing;
 	for (ChildList::iterator i = children.begin(); i != children.end(); ++i)
 	{
-		(*i)->SetPos(pos[0] + num*hspacePerObject + (num+1)*spacing, pos[1] + spacing);
-		(*i)->SetSize(hspacePerObject - 2.0f * spacing, size[1] - 2.0f*spacing);
-		++num;
+		(*i)->SetPos(startX, pos[1] + spacing);
+		if ((*i)->SizeFixed())
+		{
+			(*i)->SetSize((*i)->GetSize()[0], size[1] - 2.0f*spacing, true);
+			startX += (*i)->GetSize()[0] + spacing;
+		}
+		else
+		{
+			(*i)->SetSize(hspacePerObject, size[1]- 2.0f*spacing);
+			startX += hspacePerObject + spacing;
+		}
 	}
 }
 
