5879cb977d2b3a548c3056205245b3b3b1231e93
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-08-02 23:47:39 +0200

    network handling error improved, so it only spams error message and doesn'T crash (can still timeout, tho)

--- rts/System/Net/Socket.cpp
+++ rts/System/Net/Socket.cpp
@@ -6,14 +6,29 @@
 
 #include "Socket.h"
 
-#ifndef _MSC_VER
-#include "StdAfx.h"
-#endif
+#include <boost/system/error_code.hpp>
+
+#include "LogOutput.h"
 
 namespace netcode
 {
+using namespace boost::system::errc;
 
 boost::asio::io_service netservice;
+
+bool CheckErrorCode(boost::system::error_code& err)
+{
+	// connection reset can happen when host didn't start up before the client wants to connect
+	if (!err || err.value() == connection_reset)
+	{
+		return false;
+	}
+	else
+	{
+		LogObject() << "Network error " << err.value() << ": " << err.message();
+		return true;
+	}
+}
 
 } // namespace netcode
 
