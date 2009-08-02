c28e1839a39662d29d70dd4cf192212e723e1c60
Marek Baczynski <imbaczek@gmail.com>
2009-08-02 03:37:48 +0200

    sim: units cut some corners now that used to be 45 degree turns.
    this results in more fluid and believable movement, even though it still happens on a grid.

--- rts/Sim/Path/IPath.h
+++ rts/Sim/Path/IPath.h
@@ -2,7 +2,7 @@
 #define IPATH_H
 
 #include "float3.h"
-#include <list>
+#include <vector>
 
 class IPath {
 public:
@@ -13,13 +13,15 @@ public:
 		Error
 	};
 
+	typedef std::vector<float3> path_list_type;
+
 	struct Path {
 		//Information about the requested path.
 		float3 desiredGoal;
 		float goalRadius;
 		//Information about the generated path.
 		float3 pathGoal;
-		std::list<float3> path;
+		path_list_type path;
 		float pathCost;
 	};
 };
