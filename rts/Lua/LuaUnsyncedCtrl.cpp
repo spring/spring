9716d7353ca8a4cea2d247b185d62649aa3a16cb
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-08-02 19:51:10 +0200

    fix: execvp() needs program name in as first argument

--- rts/Lua/LuaUnsyncedCtrl.cpp
+++ rts/Lua/LuaUnsyncedCtrl.cpp
@@ -1673,11 +1673,11 @@ int LuaUnsyncedCtrl::Restart(lua_State* L)
 		std::ofstream scriptfile((FileSystemHandler::GetInstance().GetWriteDir()+"/script.txt").c_str());
 		scriptfile << script;
 		scriptfile.close();
-		execlp(Platform::GetBinaryFile().c_str(), arguments.c_str(), (FileSystemHandler::GetInstance().GetWriteDir()+"/script.txt").c_str(), NULL);
+		execlp(Platform::GetBinaryFile().c_str(), Platform::GetBinaryFile().c_str(), arguments.c_str(), (FileSystemHandler::GetInstance().GetWriteDir()+"/script.txt").c_str(), NULL);
 	}
 	else
 	{
-		execlp(Platform::GetBinaryFile().c_str(), arguments.c_str(), NULL);
+		execlp(Platform::GetBinaryFile().c_str(), Platform::GetBinaryFile().c_str(), arguments.c_str(), NULL);
 	}
 	LogObject() << "Error in Restart: " << strerror(errno);
 	lua_pushboolean(L, false);
