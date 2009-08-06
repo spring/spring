997831518ec4965f32905981906aa72ef5ad6951
Karl-Robert Ernst <k-r.ernst@my-mail.ch>
2009-08-07 01:33:52 +0200

    fix shift+esp quit in the menu

--- rts/aGui/Picture.cpp
+++ rts/aGui/Picture.cpp
@@ -15,8 +15,17 @@ Picture::~Picture()
 void Picture::Load(const std::string& _file)
 {
 	file = _file;
-	texture = 0;
-	GeometryChangeSelf();
+	
+	CBitmap bmp;
+	if (bmp.Load(file))
+	{
+		texture = bmp.CreateTexture(false);
+	}
+	else
+	{
+		LogObject() << "Failed to load: " << file;
+		texture = 0;
+	}
 }
 
 void Picture::DrawSelf()
@@ -32,20 +41,6 @@ void Picture::DrawSelf()
 			glTexCoord2f(1,1);glVertex2f(1,0);
 		glEnd();
 		glDisable(GL_TEXTURE_2D);
-	}
-}
-
-void Picture::GeometryChangeSelf()
-{
-	CBitmap bmp;
-	if (bmp.Load(file))
-	{
-		texture = bmp.CreateTexture(false);
-	}
-	else
-	{
-		LogObject() << "Failed to load: " << file;
-		texture = 0;
 	}
 }
 
