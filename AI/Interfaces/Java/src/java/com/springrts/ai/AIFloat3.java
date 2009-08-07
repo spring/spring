c2ba85aa0a189f0d0219796e2e371b8f60566eb3
hoijui <hoijui.quaero@gmail.com>
2009-08-07 14:11:48 +0200

    Revert "AI: Java: remove equals and hashCode from AIFloat3, as it causes memory leak with JNA"
    This reverts commit 6720aaff8defbe1d3990682c2481c6767f2b36d8.

--- AI/Interfaces/Java/src/java/com/springrts/ai/AIFloat3.java
+++ AI/Interfaces/Java/src/java/com/springrts/ai/AIFloat3.java
@@ -88,4 +88,37 @@ public final class AIFloat3 extends Structure implements Structure.ByValue {
 		xyz[1] = y;
 		xyz[2] = z;
 	}
+
+	@Override
+	public int hashCode() {
+
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		result = prime * result + Float.floatToIntBits(z);
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj) {
+			return true;
+		} else if (!super.equals(obj)) {
+			return false;
+		} else if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		AIFloat3 other = (AIFloat3) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x)) {
+			return false;
+		} else if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y)) {
+			return false;
+		} else if (Float.floatToIntBits(z) != Float.floatToIntBits(other.z)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
