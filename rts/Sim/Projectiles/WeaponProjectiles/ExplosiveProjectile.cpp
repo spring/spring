95c0ba14087ad6145b90192874bc1a0d87b6c576
Marek Baczynski <imbaczek@gmail.com>
2009-08-09 01:25:39 +0200

    fix possible division by zero

--- rts/Sim/Projectiles/WeaponProjectiles/ExplosiveProjectile.cpp
+++ rts/Sim/Projectiles/WeaponProjectiles/ExplosiveProjectile.cpp
@@ -46,7 +46,10 @@ CExplosiveProjectile::CExplosiveProjectile(const float3& pos,
 		drawRadius = weaponDef->size;
 	}
 
-	invttl = 1.0f / ttl;
+	if (ttl <= 0)
+		invttl = 1;
+	else
+		invttl = 1.0f / ttl;
 
 #ifdef TRACE_SYNC
 	tracefile << "New explosive: ";
