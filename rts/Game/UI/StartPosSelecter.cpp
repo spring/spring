5dc48f4f0173c83b8cc9831d945b90340adb4166
-jk- <-jk->
2009-08-29 17:53:04 +0200

    fix Ready-button

--- rts/Game/UI/StartPosSelecter.cpp
+++ rts/Game/UI/StartPosSelecter.cpp
@@ -192,7 +192,7 @@ void CStartPosSelecter::Draw()
 	const float xSize = (readyBox.x2 - readyBox.x1);
 
 	const float fontScale = 0.9f * std::min(xSize/unitWidth, ySize/unitHeight);
-	const float yPos = readyBox.y1 + 0.5f * (readyBox.y1 + readyBox.y2);
+	const float yPos = 0.5f * (readyBox.y1 + readyBox.y2);
 	const float xPos = 0.5f * (readyBox.x1 + readyBox.x2);
 
 	font->Begin(); 
