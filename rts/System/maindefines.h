eb8300685edbe96977b620e1c444d9aa5c0d84ff
hoijui <hoijui.quaero@gmail.com>
2009-09-25 09:21:19 +0200

    define a cross-platform/-compiler compatible "%z" format replacement macro for printf() style functions "%z" being the proper way for size_t typed values, but support for it has not yet spread wide enough. (this is mainly to get rid of compiler warnings, it fixes nothing potentially fatal)

--- rts/System/maindefines.h
+++ rts/System/maindefines.h
@@ -35,6 +35,18 @@
 #define __arch32__
 #endif
 
+// define a cross-platform/-compiler compatible "%z" format replacement for
+// printf() style functions.
+// "%z" being the propper way for size_t typed values,
+// but support for it has not yet spread wide enough.
+#if defined __arch64__
+#define __SIZE_T_PRINTF_FORMAT__ "%lu"
+#else
+#define __SIZE_T_PRINTF_FORMAT__ "%u"
+#endif
+// a shorter form
+#define _STPF_ __SIZE_T_PRINTF_FORMAT__
+
 // Define an abreviation for the alignment attribute.
 // This should only be used in case of a known misalignment problem,
 // as it adds a performance hit, even though it is very very small.
