spring/spring

commit c395b5fdfc6220e6d29c1d5cb586d82724368ffd

always have gameSetup

rts/Sim/Units/UnitDefHandler.cpp
@@ -235,12 +235,9 @@ void CUnitDefHandler::ParseUnitDefTable(const LuaTable& udTable, const string& u
 
 	ud.isCommander = udTable.GetBool("commander", false);
 
-	if (ud.isCommander && gameSetup) {
+	if (ud.isCommander) {
 		ud.metalStorage  = udTable.GetFloat("metalStorage",  gameSetup->startMetal);
 		ud.energyStorage = udTable.GetFloat("energyStorage", gameSetup->startEnergy);
-	} else {
-		ud.metalStorage  = udTable.GetFloat("metalStorage",  0.0f);
-		ud.energyStorage = udTable.GetFloat("energyStorage", 0.0f);
 	}
 
 	ud.extractsMetal  = udTable.GetFloat("extractsMetal",  0.0f);

commit 6832af25dcc9d1f7864bd601f946735ca2703b87

Fixed epic bug introduced in c395b5fdfc6220e6d29c1d5cb586d82724368ffd
(Yes, non-commander units can have storage too...,
and definitely shouldn't have uninitialized amount of storage <_<)

rts/Sim/Units/UnitDefHandler.cpp
@@ -239,6 +239,10 @@ void CUnitDefHandler::ParseUnitDefTable(const LuaTable& udTable, const string& u
 		ud.metalStorage  = udTable.GetFloat("metalStorage",  gameSetup->startMetal);
 		ud.energyStorage = udTable.GetFloat("energyStorage", gameSetup->startEnergy);
 	}
+	else {
+		ud.metalStorage  = udTable.GetFloat("metalStorage",  0.0f);
+		ud.energyStorage = udTable.GetFloat("energyStorage", 0.0f);
+ 	}
 
 	ud.extractsMetal  = udTable.GetFloat("extractsMetal",  0.0f);
 	ud.windGenerator  = udTable.GetFloat("windGenerator",  0.0f);