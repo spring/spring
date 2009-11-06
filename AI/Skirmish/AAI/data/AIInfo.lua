2f395fc96cc64ef12808dbc25966b7e82b57af1e
hoijui <hoijui.quaero@gmail.com>
2009-11-06 20:10:44 +0100

    AI: add longer descriptions to AAI, KAIK and RAI (thanks to R-TEAM)

--- AI/Skirmish/AAI/data/AIInfo.lua
+++ AI/Skirmish/AAI/data/AIInfo.lua
@@ -1,17 +1,17 @@
---
---  Info Definition Table format
---
---
---  These keywords must be lowercase for LuaParser to read them.
---
---  key:      user defined or one of the SKIRMISH_AI_PROPERTY_* defines in
---            SSkirmishAILibrary.h
---  value:    the value of the property
---  desc:     the description (could be used as a tooltip)
---
---
+--[[
 --------------------------------------------------------------------------------
+
+	Info Definition Table format
+
+	These keywords must be lowercase for LuaParser to read them.
+
+	key:    user defined or one of the SKIRMISH_AI_PROPERTY_* defines in
+		    SSkirmishAILibrary.h
+	value:  the value of the property
+	desc:   the description (could be used as a tooltip)
+
 --------------------------------------------------------------------------------
+]]
 
 local infos = {
 	{
@@ -30,8 +30,16 @@ local infos = {
 	},
 	{
 		key    = 'description',
-		value  = 'Supports most TA based Mods plus a few others. Uses config files.',
-		desc   = 'tooltip.',
+		value  = [[
+Plays a nice and slow game, using ground, air & hovercrafts.
+Though water gets used too, it happens seldomly.
+Sometimes it uses smart moves, but needs many test games
+to get better through aquiring learning data.
+Realtive CPU usage: 1.0
+Scales well with growing unit numbers.
+Special: needs a (manually managed) config file per mod.
+Known to Support (if config available): BA, SA, XTA, S44]],
+		desc   = 'this should help noobs to find out whether this AI is what they want',
 	},
 	{
 		key    = 'url',
