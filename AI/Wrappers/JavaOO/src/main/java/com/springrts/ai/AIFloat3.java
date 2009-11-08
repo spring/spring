98fc80568b5da14ebb73c7152b148f633f29a0ae
hoijui <hoijui.quaero@gmail.com>
2009-11-08 09:27:39 +0100

    AI: Wrapper: Java OO: adjust AIFloat3 (more leight-weight & wider compatibility) * get rid of JNA stuff * now inherits from Vector3f * is not final anymore

--- AI/Wrappers/JavaOO/src/main/java/com/springrts/ai/AIFloat3.java
+++ AI/Wrappers/JavaOO/src/main/java/com/springrts/ai/AIFloat3.java
@@ -18,65 +18,53 @@
 package com.springrts.ai;
 
 
-import com.sun.jna.Structure;
-import java.awt.Color;
+import javax.vecmath.Tuple3d;
+import javax.vecmath.Tuple3f;
+import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
+import java.awt.Color;
 
 /**
- * Wrapper for file:
- * rts/ExternalAI/Interface/SAIFloat3.h
+ * Represents a position on the map.
  *
  * @author	hoijui.quaero@gmail.com
  * @version	0.1
  */
-public final class AIFloat3 extends Structure implements Structure.ByValue {
-
-	public float x, y, z;
+public class AIFloat3 extends Vector3f {
 
 	public AIFloat3() {
-
-		this.x = 0.0f;
-		this.y = 0.0f;
-		this.z = 0.0f;
-	}
-	public AIFloat3(final AIFloat3 float3) {
-
-		this.x = float3.x;
-		this.y = float3.y;
-		this.z = float3.z;
+		super(0.0f, 0.0f, 0.0f);
 	}
 	public AIFloat3(float x, float y, float z) {
-
-		this.x = x;
-		this.y = y;
-		this.z = z;
+		super(x, y, z);
 	}
-	public AIFloat3(final float[] xyz) {
-
-		this.x = xyz[0];
-		this.y = xyz[1];
-		this.z = xyz[2];
+	public AIFloat3(float[] xyz) {
+		super(xyz);
 	}
-	public AIFloat3(final Color color) {
+	public AIFloat3(AIFloat3 other) {
+		super(other);
+	}
+	public AIFloat3(Tuple3d tub3d) {
+		super(tub3d);
+	}
+	public AIFloat3(Tuple3f tub3f) {
+		super(tub3f);
+	}
+	public AIFloat3(Vector3d vec3d) {
+		super(vec3d);
+	}
+	public AIFloat3(Vector3f vec3f) {
+		super(vec3f);
+	}
+	public AIFloat3(Color color) {
 
 		this.x = color.getRed()   / 255.0F;
 		this.y = color.getGreen() / 255.0F;
 		this.z = color.getBlue()  / 255.0F;
 	}
-	public AIFloat3(final Vector3f vec3f) {
-
-		float[] tmpXyz = new float[3];
-		vec3f.get(tmpXyz);
-		this.x = tmpXyz[0];
-		this.y = tmpXyz[1];
-		this.z = tmpXyz[2];
-	}
 
 	public Color toColor() {
 		return new Color(x, y, z);
-	}
-	public Vector3f toVector3f() {
-		return new Vector3f(x, y, z);
 	}
 	@Override
 	public String toString() {
