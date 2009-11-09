c7ba89600fedc512f2375e45eb90f24b7784f208
hoijui <hoijui.quaero@gmail.com>
2009-11-09 13:00:23 +0100

    make some UnitDef comments DoxyGen compatible (no code logic changes)

--- rts/Sim/Units/UnitDef.h
+++ rts/Sim/Units/UnitDef.h
@@ -36,21 +36,21 @@ public:
 	std::string name;
 	std::string humanName;
 	std::string filename;
-	int id;					// unique id for this type of unit
+	int id;					///< unique id for this type of unit
 
 	CollisionVolume* collisionVolume;
 	const UnitDef* decoyDef;
 
 	int aihint;
-	int cobID;				// associated with the COB <GET COB_ID unitID> call
+	int cobID;				///< associated with the COB <GET COB_ID unitID> call
 
 	int techLevel;
 	std::string gaia;
 
 	float metalUpkeep;
 	float energyUpkeep;
-	float metalMake;		// metal will always be created
-	float makesMetal;		// metal will be created when unit is on and enough energy can be drained
+	float metalMake;		///< metal will always be created
+	float makesMetal;		///< metal will be created when unit is on and enough energy can be drained
 	float energyMake;
 	float metalCost;
 	float energyCost;
@@ -65,22 +65,28 @@ public:
 	bool extractSquare;
 	bool isMetalMaker;
 
-	float autoHeal;     // amount autohealed
-	float idleAutoHeal; // amount autohealed only during idling
-	int idleTime;       // time a unit needs to idle before its considered idling
+	float autoHeal;     ///< amount autohealed
+	float idleAutoHeal; ///< amount autohealed only during idling
+	int idleTime;       ///< time a unit needs to idle before its considered idling
 
 	float power;
 	float health;
 	unsigned int category;
 
-	float speed;        // maximum forward speed the unit can attain (elmos/sec)
-	float rSpeed;       // maximum reverse speed the unit can attain (elmos/sec)
+	float speed;        ///< maximum forward speed the unit can attain (elmos/sec)
+	float rSpeed;       ///< maximum reverse speed the unit can attain (elmos/sec)
 	float turnRate;
 	bool turnInPlace;
-	float turnInPlaceDistance; //!< units above this distance to goal will try to turn while keeping
-				   //!< some of their speed. 0 to disable
-	float turnInPlaceSpeedLimit; //!< units below this speed will turn in place regardless of their
-				   //!< turnInPlace setting
+	/**
+	 * units above this distance to goal will try to turn while keeping
+	 * some of their speed. 0 to disable
+	 */
+	float turnInPlaceDistance;
+	/**
+	 * units below this speed will turn in place regardless of their
+	 * turnInPlace setting
+	 */
+	float turnInPlaceSpeedLimit;
 
 	bool upright;
 	bool collide;
@@ -127,27 +133,30 @@ public:
 	float armoredMultiple;
 	int armorType;
 
-	int flankingBonusMode; // 0: no flanking bonus
-	                       // 1: global coords, mobile
-	                       // 2: unit coords, mobile
-	                       // 3: unit coords, locked
-	float3 flankingBonusDir; // units takes less damage when attacked from this dir (encourage flanking fire)
-	float  flankingBonusMax; // damage factor for the least protected direction
-	float  flankingBonusMin; // damage factor for the most protected direction
-	float  flankingBonusMobilityAdd; // how much the ability of the flanking bonus direction to move builds up each frame
+	/**
+	 * 0: no flanking bonus
+	 * 1: global coords, mobile
+	 * 2: unit coords, mobile
+	 * 3: unit coords, locked
+	 */
+	int flankingBonusMode;
+	float3 flankingBonusDir; ///< units takes less damage when attacked from this dir (encourage flanking fire)
+	float  flankingBonusMax; ///< damage factor for the least protected direction
+	float  flankingBonusMin; ///< damage factor for the most protected direction
+	float  flankingBonusMobilityAdd; ///< how much the ability of the flanking bonus direction to move builds up each frame
 
 	UnitModelDef modelDef;
 
-	std::string scriptName;		// the name of the unit's script, e.g. "armjeth.cob"
-	std::string scriptPath;		// the path of the unit's script, e.g. "scripts/armjeth.cob"
+	std::string scriptName;		///< the name of the unit's script, e.g. "armjeth.cob"
+	std::string scriptPath;		///< the path of the unit's script, e.g. "scripts/armjeth.cob"
 
-	float3 modelCenterOffset;	// offset from the unit model's default center point
+	float3 modelCenterOffset;	///< offset from the unit model's default center point
 
-	std::string collisionVolumeTypeStr;	// can be "Ell", "CylT" (where T is one of "XYZ"), or "Box"
-	float3 collisionVolumeScales;		// the collision volume's full axis lengths
-	float3 collisionVolumeOffsets;		// relative to the unit's center position
-	int collisionVolumeTest;			// 0: discrete, 1: continuous
-	bool usePieceCollisionVolumes;		// if true, collisions are checked per-piece
+	std::string collisionVolumeTypeStr;	///< can be "Ell", "CylT" (where T is one of "XYZ"), or "Box"
+	float3 collisionVolumeScales;		///< the collision volume's full axis lengths
+	float3 collisionVolumeOffsets;		///< relative to the unit's center position
+	int collisionVolumeTest;			///< 0: discrete, 1: continuous
+	bool usePieceCollisionVolumes;		///< if true, collisions are checked per-piece
 
 
 	struct UnitDefWeapon {
@@ -180,7 +189,7 @@ public:
 	std::string deathExplosion;
 	std::string selfDExplosion;
 
-	std::string TEDClassString;	// these might be changed later for something better
+	std::string TEDClassString;	///< these might be changed later for something better
 	std::string categoryString;
 
 	std::string buildPicName;
@@ -238,7 +247,7 @@ public:
 	bool canCrash;
 	bool hoverAttack;
 	bool airStrafe;
-	float dlHoverFactor; // < 0 means it can land, >= 0 indicates how much the unit will move during hovering on the spot
+	float dlHoverFactor; ///< < 0 means it can land, >= 0 indicates how much the unit will move during hovering on the spot
 	bool DontLand() const { return dlHoverFactor >= 0.0f; }
 	bool bankingAllowed;
 
@@ -250,40 +259,39 @@ public:
 	float crashDrag;
 
 	MoveData* movedata;
-//	unsigned char* yardmapLevels[6];
-	unsigned char* yardmaps[4];						// Iterations of the Ymap for building rotation
+	unsigned char* yardmaps[4];						///< Iterations of the Ymap for building rotation
 
-	int xsize;										// each size is 8 units
-	int zsize;										// each size is 8 units
+	int xsize;										///< each size is 8 units
+	int zsize;										///< each size is 8 units
 
 	int buildangle;
 
-	float loadingRadius;							// for transports
+	float loadingRadius;							///< for transports
 	float unloadSpread;
 	int transportCapacity;
 	int transportSize;
 	int minTransportSize;
 	bool isAirBase;
-	bool isFirePlatform;							// should the carried units still be able to shoot?
+	bool isFirePlatform;							///< should the carried units still be able to shoot?
 	float transportMass;
 	float minTransportMass;
 	bool holdSteady;
 	bool releaseHeld;
 	bool cantBeTransported;
 	bool transportByEnemy;
-	int transportUnloadMethod;						// 0 - land unload, 1 - flyover drop, 2 - land flood
-	float fallSpeed;								// dictates fall speed of all transported units
-	float unitFallSpeed;							// sets the transported units fbi, overrides fallSpeed
+	int transportUnloadMethod;						///< 0 - land unload, 1 - flyover drop, 2 - land flood
+	float fallSpeed;								///< dictates fall speed of all transported units
+	float unitFallSpeed;							///< sets the transported units fbi, overrides fallSpeed
 
-	bool canCloak;									// if the unit can cloak
-	bool startCloaked;								// if the units want to start out cloaked
-	float cloakCost;								// energy cost per second to stay cloaked when stationary
-	float cloakCostMoving;							// energy cost per second when moving
-	float decloakDistance;							// if enemy unit come within this range decloaking is forced
-	bool decloakSpherical;							// use a spherical test instead of a cylindrical test?
-	bool decloakOnFire;								// should the unit decloak upon firing
+	bool canCloak;									///< if the unit can cloak
+	bool startCloaked;								///< if the units want to start out cloaked
+	float cloakCost;								///< energy cost per second to stay cloaked when stationary
+	float cloakCostMoving;							///< energy cost per second when moving
+	float decloakDistance;							///< if enemy unit come within this range decloaking is forced
+	bool decloakSpherical;							///< use a spherical test instead of a cylindrical test?
+	bool decloakOnFire;								///< should the unit decloak upon firing
 
-	bool canKamikaze;								//self destruct if enemy come to close
+	bool canKamikaze;								///< self destructs if enemy come to close
 	float kamikazeDist;
 
 	bool targfac;
@@ -296,7 +304,7 @@ public:
 
 	bool canResurrect;
 	bool canCapture;
-	int highTrajectoryType;							// 0 (default) = only low, 1 = only high, 2 = choose
+	int highTrajectoryType;							///< 0 (default) = only low, 1 = only high, 2 = choose
 
 	unsigned int noChaseCategory;
 
@@ -330,9 +338,9 @@ public:
 	int flareSalvoSize;
 	int flareSalvoDelay;
 
-	bool smoothAnim;								// True if the unit should use interpolated animation
-	bool canLoopbackAttack;							// only matters for fighter aircraft
-	bool levelGround;								// only matters for buildings
+	bool smoothAnim;								///< True if the unit should use interpolated animation
+	bool canLoopbackAttack;							///< only matters for fighter aircraft
+	bool levelGround;								///< only matters for buildings
 
 	bool useBuildingGroundDecal;
 	int buildingDecalType;
@@ -340,19 +348,19 @@ public:
 	int buildingDecalSizeY;
 	float buildingDecalDecaySpeed;
 
-	bool showNanoFrame;								// Does the nano frame animation get shown during construction?
-	bool showNanoSpray;								// Does nano spray get shown at all?
-	float3 nanoColor;								// If nano spray is displayed what color is it?
+	bool showNanoFrame;								///< Does the nano frame animation get shown during construction?
+	bool showNanoSpray;								///< Does nano spray get shown at all?
+	float3 nanoColor;								///< If nano spray is displayed what color is it?
 
-	float maxFuel;									// max flight time in seconds before the aircraft needs to return to a air repair bay to refuel
-	float refuelTime;								// time to fully refuel unit
-	float minAirBasePower;							// min build power for airbases that this aircraft can land on
+	float maxFuel;									///< max flight time in seconds before the aircraft needs to return to a air repair bay to refuel
+	float refuelTime;								///< time to fully refuel unit
+	float minAirBasePower;							///< min build power for airbases that this aircraft can land on
 
-	std::vector<CExplosionGenerator*> sfxExplGens;	// list of explosion generators for use in scripts
-	std::string pieceTrailCEGTag;					// base tag (eg. "flame") of CEG attached to pieces of exploding units
-	int pieceTrailCEGRange;							// range of piece CEGs (0-based, range 8 ==> tags "flame0", ..., "flame7")
+	std::vector<CExplosionGenerator*> sfxExplGens;	///< list of explosion generators for use in scripts
+	std::string pieceTrailCEGTag;					///< base tag (eg. "flame") of CEG attached to pieces of exploding units
+	int pieceTrailCEGRange;							///< range of piece CEGs (0-based, range 8 ==> tags "flame0", ..., "flame7")
 
-	int maxThisUnit;								// number of units of this type allowed simultaneously in the game
+	int maxThisUnit;								///< number of units of this type allowed simultaneously in the game
 
 	std::map<std::string, std::string> customParams;
 
