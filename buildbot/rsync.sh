e269cbe8d667f67e55bddda018959a65b9ee3992
Rene Milk <koshi@springlobby.info>
2009-11-19 23:40:30 +0100

    fix rsync, add git remote in bbot setup to get proper tags

--- buildbot/rsync.sh
+++ buildbot/rsync.sh
@@ -1,30 +1,27 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 cd $(dirname $0)/..
 
 CONFIG=${1}
 BRANCH=${2}
-TMP_DIR=/tmp
-REMOTE_HOST=localhost
+TMP_BASE=/tmp/sl
+REMOTE_HOST=lh
 REMOTE_BASE=/home/buildbot/www
-REMOTE_PATH=${REMOTE_BASE}/${CONFIG}/${BRANCH}
+TMP_PATH=${TMP_BASE}/${CONFIG}/${BRANCH}
 REV=$(git describe --tags)
-LOCAL_BASE=../build-${CONFIG}
-BASE_ARCHIVE=${TMP_DIR}/${REV}_base.7z
-EXE_ARCHIVE=${TMP_DIR}/${REV}_spring.7z
-USYNC_ARCHIVE=${TMP_DIR}/${REV}_unitsync.7z
+LOCAL_BASE=build-${CONFIG}
+BASE_ARCHIVE=${TMP_PATH}/${REV}_base.7z
+EXE_ARCHIVE=${TMP_PATH}/${REV}_spring.7z
+USYNC_ARCHIVE=${TMP_PATH}/${REV}_unitsync.7z
+CMD="rsync -avz"
 
-7z a ${BASE_ARCHIVE} ${LOCAL_BASE}/base
-7z a ${EXE_ARCHIVE} ${LOCAL_BASE}/spring.exe
-7z a ${USYNC_ARCHIVE} ${LOCAL_BASE}/unitsync.dll
+7z u ${BASE_ARCHIVE} ${LOCAL_BASE}/base
+7z u ${EXE_ARCHIVE} ${LOCAL_BASE}/spring.exe
+7z u ${USYNC_ARCHIVE} ${LOCAL_BASE}/unitsync.dll
 
-rsync -lrvz ${BASE_ARCHIVE} ${REMOTE_HOST}:${REMOTE_PATH}/${BASE_ARCHIVE}
-rsync -lrvz ${EXE_ARCHIVE} ${REMOTE_HOST}:${REMOTE_PATH}/${EXE_ARCHIVE}
-rsync -lrvz ${USYNC_ARCHIVE} ${REMOTE_HOST}:${REMOTE_PATH}/${USYNC_ARCHIVE}
+$CMD ${TMP_BASE}/ ${REMOTE_HOST}:${REMOTE_BASE}/
 
-rm -f ${BASE_ARCHIVE}
-rm -f ${EXE_ARCHIVE}
-rm -f ${USYNC_ARCHIVE}
+#rm -rf ${TMP_BASE}/*
 
 
