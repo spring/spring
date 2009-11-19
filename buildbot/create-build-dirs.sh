c704726c7e73d20e100083f64d115370085433ad
Rene Milk <koshi@springlobby.info>
2009-11-19 23:00:14 +0100

    cmake builddir script fix

--- buildbot/create-build-dirs.sh
+++ buildbot/create-build-dirs.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 set -e
-cd $(dirname $0)/..
+cd $(dirname ${0})/..
 
-echo -n creating $1 ...
-(
-if [ ! -d $1 ] ; then
-	mkdir $1
+DIR=build-${1}
+
+echo -n creating ${DIR} ...
+
+if [ ! -d ${DIR} ] ; then
+	mkdir ${DIR}
 	echo done
 else
 	echo skipped
 fi
-)
-echo -n configuring $1 with $4 ...
-(
-cd $1
-cmake .. -DCMAKE_TOOLCHAIN_FILE:STRING=$2 -DMINGWLIBS:PATH=$3  $4
-)
+
+echo -n configuring ${DIR} with ${4} ...
+
+cd ${DIR}
+cmake .. -DCMAKE_TOOLCHAIN_FILE:STRING=${2} -DMINGWLIBS:PATH=${3}  ${4}
+
