c685e229b2b2b253853b19f7f1b47f435b171814
Hugh Perkins <hughperkins@gmail.com>
2009-11-22 19:12:19 +0800

    AI wrappers didn't compile on mingw32 with gcc 4.4.0, some strangeness with mingw32 c headers

--- AI/Wrappers/CUtils/SharedLibrary.c
+++ AI/Wrappers/CUtils/SharedLibrary.c
@@ -15,10 +15,11 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdio.h>
+
 #include "SharedLibrary.h"
 
 #include "maindefines.h"
-#include <stdio.h>
 
 const char* sharedLib_getLibExtension() {
 
