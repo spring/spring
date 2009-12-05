2374cf38a1e00c608310f447c7f451154548aa2b
tranchis <tranchis@gmail.com>
2009-12-05 04:30:22 -0700

    Added assembly to compile in Snow Leopard (64-bit). It should now compile in MacOS X, in any of both 32-bit and 64-bit flavors.

--- rts/System/Util.cpp
+++ rts/System/Util.cpp
@@ -33,14 +33,25 @@ namespace proc {
 			: "0" (*a)
 		);
 	#else
-		__asm__ __volatile__(
-			"pushl %%ebx\n\t"
-			"cpuid\n\t"
-			"movl %%ebx, %1\n\t"
-			"popl %%ebx"
-			: "=a" (*a), "=r" (*b), "=c" (*c), "=d" (*d)
-			: "0" (*a)
-		);
+                #ifdef __x86_64__
+                        __asm__ __volatile__(
+                                "pushq %%rbx\n\t"
+                                "cpuid\n\t"
+                                "movl %%ebx, %1\n\t"
+                                "popq %%rbx"
+                                : "=a" (*a), "=r" (*b), "=c" (*c), "=d" (*d)
+                                : "0" (*a)
+                        );
+                #else
+                        __asm__ __volatile__(
+                                "pushl %%ebx\n\t"
+                                "cpuid\n\t"
+                                "movl %%ebx, %1\n\t"
+                                "popl %%ebx"
+                                : "=a" (*a), "=r" (*b), "=c" (*c), "=d" (*d)
+                                : "0" (*a)
+                        );
+                #endif
 	#endif
 	}
 	#else
