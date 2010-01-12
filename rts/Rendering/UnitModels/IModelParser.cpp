52f5cbacfdca152e3b3df69aa62b9789b505760f
SpliFF <spliff@warriorhut.org>
2010-01-12 17:34:39 +1100

    fixed crash due to assuming pieces have only one mesh. added verbose logging.

--- rts/Rendering/UnitModels/IModelParser.cpp
+++ rts/Rendering/UnitModels/IModelParser.cpp
@@ -163,6 +163,8 @@ void C3DModelLoader::DeleteLocalModel(CUnit* unit)
 
 void C3DModelLoader::CreateLocalModel(CUnit* unit)
 {
+	logOutput.Print("C3DModelLoader::CreateLocalModel(CUnit* unit)");
+
 #if defined(USE_GML) && GML_ENABLE_SIM
 	GML_STDMUTEX_LOCK(model); // CreateLocalModel
 
@@ -172,29 +174,40 @@ void C3DModelLoader::CreateLocalModel(CUnit* unit)
 	unit->localmodel = CreateLocalModel(unit->model);
 	// FixLocalModel(unit);
 #endif
+
+	logOutput.Print("Finished C3DModelLoader::CreateLocalModel(CUnit* unit)");
 }
 
 
 LocalModel* C3DModelLoader::CreateLocalModel(S3DModel* model)
 {
+	logOutput.Print("CreateLocalModel(model '%s' type %d objects %d)", model->name.c_str(), model->type, model->numobjects);
+
 	LocalModel* lmodel = new LocalModel;
 	lmodel->type = model->type;
 	lmodel->pieces.reserve(model->numobjects);
 
 	for (unsigned int i = 0; i < model->numobjects; i++) {
+		logOutput.Print("Adding object %d", i);
 		lmodel->pieces.push_back(new LocalModelPiece);
 	}
 	lmodel->pieces[0]->parent = NULL;
 
 	int piecenum = 0;
 	CreateLocalModelPieces(model->rootobject, lmodel, &piecenum);
+
+	logOutput.Print("Finished CreateLocalModel(model)");
 	return lmodel;
 }
 
 
 void C3DModelLoader::CreateLocalModelPieces(S3DModelPiece* piece, LocalModel* lmodel, int* piecenum)
 {
+	logOutput.Print("C3DModelLoader::CreateLocalModelPieces (piece '%s' num %d)", piece->name.c_str(), *piecenum);
+
+
 	LocalModelPiece& lmp = *lmodel->pieces[*piecenum];
+
 	lmp.original  =  piece;
 	lmp.name      =  piece->name;
 	lmp.type      =  piece->type;
@@ -203,15 +216,22 @@ void C3DModelLoader::CreateLocalModelPieces(S3DModelPiece* piece, LocalModel* lm
 	lmp.updated   =  false;
 	lmp.pos       =  piece->offset;
 	lmp.rot       =  float3(0.0f, 0.0f, 0.0f);
+
+	logOutput.Print("Create CollisionVolume");
 	lmp.colvol    = new CollisionVolume(piece->colvol);
 
+	logOutput.Print("Setup childs (childs %d)", piece->childs.size());
 	lmp.childs.reserve(piece->childs.size());
 	for (unsigned int i = 0; i < piece->childs.size(); i++) {
 		(*piecenum)++;
+		logOutput.Print("Setup child piece (num %d)", *piecenum);
 		lmp.childs.push_back(lmodel->pieces[*piecenum]);
 		lmodel->pieces[*piecenum]->parent = &lmp;
 		CreateLocalModelPieces(piece->childs[i], lmodel, piecenum);
+		logOutput.Print("Finished child piece (num %d)", *piecenum);
 	}
+
+	logOutput.Print("Finished CreateLocalModelPieces (piece '%s')", piece->name.c_str());
 }
 
 
@@ -224,6 +244,8 @@ void C3DModelLoader::FixLocalModel(CUnit* unit)
 
 void C3DModelLoader::FixLocalModel(S3DModelPiece* model, LocalModel* lmodel, int* piecenum)
 {
+	logOutput.Print("C3DModelLoader::FixLocalModel");
+
 	lmodel->pieces[*piecenum]->displist = model->displist;
 
 	for (unsigned int i = 0; i < model->childs.size(); i++) {
@@ -235,6 +257,7 @@ void C3DModelLoader::FixLocalModel(S3DModelPiece* model, LocalModel* lmodel, int
 
 void C3DModelLoader::CreateListsNow(IModelParser* parser, S3DModelPiece* o)
 {
+	logOutput.Print("C3DModelLoader::CreateListsNow");
 	o->displist = glGenLists(1);
 	glNewList(o->displist, GL_COMPILE);
 	parser->Draw(o);
@@ -243,15 +266,21 @@ void C3DModelLoader::CreateListsNow(IModelParser* parser, S3DModelPiece* o)
 	for (std::vector<S3DModelPiece*>::iterator bs = o->childs.begin(); bs != o->childs.end(); bs++) {
 		CreateListsNow(parser, *bs);
 	}
+
+	logOutput.Print("Finished C3DModelLoader::CreateListsNow");
 }
 
 
 void C3DModelLoader::CreateLists(IModelParser* parser, S3DModelPiece* o) {
+	logOutput.Print("C3DModelLoader::CreateLists");
+
 #if defined(USE_GML) && GML_ENABLE_SIM
 	createLists.push_back(ModelParserPair(o, parser));
 #else
 	CreateListsNow(parser, o);
 #endif
+
+	logOutput.Print("Finished C3DModelLoader::CreateLists");
 }
 
 /******************************************************************************/
