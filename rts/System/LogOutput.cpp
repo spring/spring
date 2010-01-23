e1be8960e0432416de8655b53f7441b90f2b840e
hoijui <hoijui.quaero@gmail.com>
2010-01-23 11:22:02 +0100

    LogOutput: move doxygen doc from .cpp to .h

--- rts/System/LogOutput.cpp
+++ rts/System/LogOutput.cpp
@@ -126,14 +126,6 @@ void CLogOutput::SetFilename(const char* fname)
 	filename = fname;
 }
 
-
-/**
- * @brief initialize logOutput
- *
- * Only after calling this method, logOutput starts writing to disk.
- * The log file is written in the current directory so this may only be called
- * after the engine chdir'ed to the correct directory.
- */
 void CLogOutput::Initialize()
 {
 	if (initialized) return;
@@ -162,20 +154,6 @@ void CLogOutput::Initialize()
 	preInitLog().clear();
 }
 
-
-/**
- * @brief initialize the log subsystems
- *
- * This writes list of all available and all enabled subsystems to the log.
- *
- * Log subsystems can be enabled using the configuration key "LogSubsystems",
- * or the environment variable "SPRING_LOG_SUBSYSTEMS".
- *
- * Both specify a comma separated list of subsystems that should be enabled.
- * The lists from both sources are combined, there is no overriding.
- *
- * A subsystem that is by default enabled, can not be disabled.
- */
 void CLogOutput::InitializeSubsystems()
 {
 	{
@@ -235,16 +213,6 @@ void CLogOutput::InitializeSubsystems()
 }
 
 
-/**
- * @brief core log output method, used by all others
- *
- * Note that, when logOutput isn't initialized yet, the logging is done to the
- * global std::vector preInitLog(), and is only written to disk in the call to
- * Initialize().
- *
- * This method notifies all registered ILogSubscribers, calls OutputDebugString
- * (for MSVC builds) and prints the message to stdout and the file log.
- */
 void CLogOutput::Output(const CLogSubsystem& subsystem, const std::string& str)
 {
 	GML_STDMUTEX_LOCK(log); // Output
