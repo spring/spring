spring/spring

commit bdec14ebf32baf22bc24f42a65f93bd0e73a2385

Don't finish build command if build fails

rts/Game/UI/GuiHandler.cpp
@@ -47,6 +47,8 @@
 #include "Sim/Units/UnitLoader.h"
 #include "Sim/Weapons/WeaponDefHandler.h"
 #include "Sim/Weapons/Weapon.h"
+#include "Sound/AudioChannel.h"
+#include "Sound/Sound.h"
 #include "EventHandler.h"
 #include "FileSystem/SimpleParser.h"
 #include "LogOutput.h"
@@ -98,6 +100,8 @@ CGuiHandler::CGuiHandler():
 		glGetIntegerv(GL_STENCIL_BITS, &stencilBits);
 		useStencil = (stencilBits >= 1);
 	}
+
+	failedSound = sound->GetSoundId("FailedCommand", false);
 }
 
 
@@ -1121,6 +1125,10 @@ void CGuiHandler::MouseRelease(int x, int y, int button, float3& camerapos, floa
 	// not over a button, try to execute a command
 	Command c = GetCommand(x, y, button, false, camerapos, mousedir);
 
+	if (c.id == CMD_FAILED) { // indicates we should not finish the current command
+		Channels::UserInterface.PlaySample(failedSound, 5);
+		return;
+	}
 	// if cmd_stop is returned it indicates that no good command could be found
 	if (c.id != CMD_STOP) {
 		GiveCommand(c);
@@ -2129,6 +2137,15 @@ Command CGuiHandler::GetCommand(int mousex, int mousey, int buttonHint, bool pre
 
 			if(buildPos.empty()){
 				return defaultRet;
+			}
+
+			if(buildPos.size()==1) {
+				CFeature* feature; // TODO: Maybe also check out-of-range for immobile builder?
+				if(!uh->TestUnitBuildSquare(bi,feature,gu->myAllyTeam)) {
+					Command failedRet;
+					failedRet.id = CMD_FAILED;
+					return failedRet;
+				}
 			}
 
 			int a=0; // limit the number of max commands possible to send to avoid overflowing the network buffer

rts/Game/UI/GuiHandler.h
@@ -219,6 +219,7 @@ class CGuiHandler : public CInputReceiver {
 		std::map<std::string, unsigned int> textureMap; // filename, glTextureID
 
 		std::set<int> changedGroups;
+		int failedSound;
 };
 
 

rts/Sim/Units/CommandAI/Command.h
@@ -49,6 +49,7 @@
 #define CMD_AUTOREPAIRLEVEL      135
 #define CMD_LOOPBACKATTACK       140
 #define CMD_IDLEMODE             145
+#define CMD_FAILED               150
 
 #define CMDTYPE_ICON                        0  // expect 0 parameters in return
 #define CMDTYPE_ICON_MODE                   5  // expect 1 parameter in return (number selected mode)

commit e446cea4704e07e34d23cc256348080e5c97cd6c

Fixed placing building near other buildings often fails (mantis #1801)
this was caused by bdec14ebf32baf22bc24f42a65f93bd0e73a2385

rts/Game/UI/GuiHandler.cpp
@@ -2141,7 +2141,7 @@ Command CGuiHandler::GetCommand(int mousex, int mousey, int buttonHint, bool pre
 
 			if(buildPos.size()==1) {
 				CFeature* feature; // TODO: Maybe also check out-of-range for immobile builder?
-				if(!uh->TestUnitBuildSquare(bi,feature,gu->myAllyTeam)) {
+				if (!uh->TestUnitBuildSquare(buildPos[0], feature, gu->myAllyTeam)) {
 					Command failedRet;
 					failedRet.id = CMD_FAILED;
 					return failedRet;