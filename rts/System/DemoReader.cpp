spring/spring

commit 63deca5cb7f2493495391987ce1f6f093cbcf1a1

make DemoReader use fstream instead of CFileHandler

rts/System/DemoReader.cpp
@@ -8,35 +8,27 @@
 
 #include "Net/RawPacket.h"
 #include "Game/GameVersion.h"
-#include "FileSystem/FileHandler.h"
 
 /////////////////////////////////////
 // CDemoReader implementation
 
 CDemoReader::CDemoReader(const std::string& filename, float curTime)
 {
-	std::string firstTry = "demos/" + filename;
+	playbackDemo.open(filename.c_str(), std::ios::binary);
 
-	playbackDemo = new CFileHandler(firstTry);
-
-	if (!playbackDemo->FileExists()) {
-		delete playbackDemo;
-		playbackDemo = new CFileHandler(filename);
-	}
-
-	if (!playbackDemo->FileExists()) {
+	if (!playbackDemo.is_open()) {
 		// file not found -> exception
-		delete playbackDemo;
-		playbackDemo = NULL;
 		throw std::runtime_error(std::string("Demofile not found: ")+filename);
 	}
 
-	playbackDemo->Read((void*)&fileHeader, sizeof(fileHeader));
+	playbackDemo.read((char*)&fileHeader, sizeof(fileHeader));
 	fileHeader.swab();
 
 	if (memcmp(fileHeader.magic, DEMOFILE_MAGIC, sizeof(fileHeader.magic))
 		|| fileHeader.version != DEMOFILE_VERSION
 		|| fileHeader.headerSize != sizeof(fileHeader)
+		// || fileHeader.playerStatElemSize != sizeof(PlayerStatistics)
+		// || fileHeader.teamStatElemSize != sizeof(TeamStatistics)
 		// Don't compare spring version in debug mode: we don't want to make
 		// debugging SVN demos impossible (because VERSION_STRING is different
 		// each build.)
@@ -44,32 +36,21 @@ CDemoReader::CDemoReader(const std::string& filename, float curTime)
 		|| (SpringVersion::Get().find("+") == std::string::npos && strcmp(fileHeader.versionString, SpringVersion::Get().c_str()))
 #endif
 	) {
-		delete playbackDemo;
-		playbackDemo = NULL;
 		throw std::runtime_error(std::string("Demofile corrupt or created by a different version of Spring: ")+filename);
 	}
 
 	if (fileHeader.scriptSize != 0) {
 		char* buf = new char[fileHeader.scriptSize];
-		playbackDemo->Read(buf, fileHeader.scriptSize);
+		playbackDemo.read(buf, fileHeader.scriptSize);
 		setupScript = std::string(buf);
 		delete[] buf;
 	}
 
-	playbackDemo->Read((void*)&chunkHeader, sizeof(chunkHeader));
+	playbackDemo.read((char*)&chunkHeader, sizeof(chunkHeader));
 	chunkHeader.swab();
 
 	demoTimeOffset = curTime - chunkHeader.modGameTime - 0.1f;
 	nextDemoRead = curTime - 0.01f;
-
-	if (fileHeader.demoStreamSize != 0) {
-		bytesRemaining = fileHeader.demoStreamSize;
-	} else {
-		// Spring crashed while recording the demo: replay until EOF,
-		// but at most filesize bytes to block watching demo of running game.
-		bytesRemaining = playbackDemo->FileSize() - fileHeader.headerSize - fileHeader.scriptSize;
-	}
-	bytesRemaining -= sizeof(chunkHeader);
 }
 
 netcode::RawPacket* CDemoReader::GetData(float curTime)
@@ -80,13 +61,11 @@ netcode::RawPacket* CDemoReader::GetData(float curTime)
 	// when paused, modGameTime wont increase so no seperate check needed
 	if (nextDemoRead < curTime) {
 		netcode::RawPacket* buf = new netcode::RawPacket(chunkHeader.length);
-		playbackDemo->Read((void*)(buf->data), chunkHeader.length);
-		bytesRemaining -= chunkHeader.length;
+		playbackDemo.read((char*)(buf->data), chunkHeader.length);
 
-		playbackDemo->Read((void*)&chunkHeader, sizeof(chunkHeader));
+		playbackDemo.read((char*)&chunkHeader, sizeof(chunkHeader));
 		chunkHeader.swab();
 		nextDemoRead = chunkHeader.modGameTime + demoTimeOffset;
-		bytesRemaining -= sizeof(chunkHeader);
 
 		return buf;
 	} else {
@@ -96,7 +75,7 @@ netcode::RawPacket* CDemoReader::GetData(float curTime)
 
 bool CDemoReader::ReachedEnd() const
 {
-	if (bytesRemaining <= 0 || playbackDemo->Eof())
+	if (playbackDemo.eof())
 		return true;
 	else
 		return false;

rts/System/DemoReader.h
@@ -1,9 +1,10 @@
 #ifndef DEMO_READER
 #define DEMO_READER
 
+#include <fstream>
+
 #include "Demo.h"
 
-class CFileHandler;
 namespace netcode { class RawPacket; }
 
 /**
@@ -37,10 +38,9 @@ public:
 	};
 
 private:
-	CFileHandler* playbackDemo;
+	std::ifstream playbackDemo;
 	float demoTimeOffset;
 	float nextDemoRead;
-	int bytesRemaining;
 	DemoStreamChunkHeader chunkHeader;
 	std::string setupScript;	// the original, unaltered version from script
 };

tools/DemoAnalyser/CMakeLists.txt
@@ -1,26 +1,12 @@
-ADD_DEFINITIONS(-DDEDICATED -DNOT_USING_CREG)
+#ADD_DEFINITIONS(-DDEDICATED -DNOT_USING_CREG)
 
-INCLUDE_DIRECTORIES(../../rts/ ../../rts/lib/7zip ../../rts/lib/lua/include ../../rts/System)
+INCLUDE_DIRECTORIES(../../rts/ ../../rts/System)
 
-AUX_SOURCE_DIRECTORY(../../rts/System/FileSystem/ fsfiles)
-SET(build_files ${fsfiles}
-	../../rts/Game/GameSetup
-	../../rts/Game/PlayerBase
+SET(build_files
 	../../rts/Game/GameVersion
-	../../rts/System/Platform/Misc
-	../../rts/System/TdfParser
-	../../rts/System/ConfigHandler
 	../../rts/System/Net/RawPacket
-	../../rts/System/LogOutput
 	../../rts/System/DemoReader
-	../../rts/System/Demo
-	../../rts/System/UnsyncedRNG
-	../../rts/Sim/Misc/TeamBase
-	../../rts/Sim/Misc/AllyTeam
-	../../rts/Map/MapParser
-	../../rts/Lua/LuaParser
-	../../rts/Lua/LuaUtils
-	../../rts/Lua/LuaIO)
+	../../rts/System/Demo)
 IF (WIN32)
 	SET(build_files ${build_files}
 		../../rts/System/Platform/Win/WinVersion)
@@ -31,5 +17,5 @@ IF (MINGW)
 	# To enable console output/force a console window to open
 	SET_TARGET_PROPERTIES(demoanalyser PROPERTIES LINK_FLAGS "-Wl,-subsystem,console")
 ENDIF (MINGW)
-TARGET_LINK_LIBRARIES(demoanalyser ${SDL_LIBRARY} hpiutil2 7zip lua minizip ${Boost_REGEX_LIBRARY} ${Boost_THREAD_LIBRARY} ${Boost_SYSTEM_LIBRARY})
+TARGET_LINK_LIBRARIES(demoanalyser)
 

tools/DemoAnalyser/main.cpp
@@ -3,9 +3,6 @@
 #include <limits>
 
 #include "DemoReader.h"
-#include "FileSystem/FileSystem.h"
-#include "ConfigHandler.h"
-#include "Game/GameSetup.h"
 #include "BaseNetProtocol.h"
 #include "Net/RawPacket.h"
 
@@ -30,10 +27,6 @@ int main (int argc, char* argv[])
 		cout << "Missing parameter (full path to demo file)\n";
 		return 0;
 	}
-
-	ConfigHandler::Instantiate("");
-	FileSystemHandler::Cleanup();
-	FileSystemHandler::Initialize(false);
 
 	CDemoReader reader(string(argv[1]), 0.0f);
 	DemoFileHeader header = reader.GetFileHeader();
@@ -108,7 +101,6 @@ int main (int argc, char* argv[])
 		}
 		delete packet;
 	}
-	FileSystemHandler::Cleanup();
 
 	for (unsigned i = 0; i != trafficCounter.size(); ++i)
 	{

commit ed20ac2676189f314fc5aa6121519e11bc3764e2

Fixed demo replay for demos not properly closed (i.e. Spring crashed)
The bug was silently introduced in 63deca5cb7f2493495391987ce1f6f093cbcf1a1
and became active in 4776e64ee9fd18e2691f05499311d644433f3810, because
there fileHeader.demoStreamSize became enforcing.

rts/System/DemoReader.cpp
@@ -51,6 +51,20 @@ CDemoReader::CDemoReader(const std::string& filename, float curTime)
 
 	demoTimeOffset = curTime - chunkHeader.modGameTime - 0.1f;
 	nextDemoRead = curTime - 0.01f;
+
+	if (fileHeader.demoStreamSize != 0) {
+		bytesRemaining = fileHeader.demoStreamSize;
+	}
+	else {
+		// Spring crashed while recording the demo: replay until EOF,
+		// but at most filesize bytes to block watching demo of running game.
+		// For this we must determine the file size.
+		// (if this had still used CFileHandler that would have been easier ;-))
+		long curPos = playbackDemo.tellg();
+		playbackDemo.seekg(0, std::ios::end);
+ 		bytesRemaining = playbackDemo.tellg() - curPos;
+ 		playbackDemo.seekg(curPos);
+	}
 }
 
 netcode::RawPacket* CDemoReader::GetData(float curTime)
@@ -62,17 +76,16 @@ netcode::RawPacket* CDemoReader::GetData(float curTime)
 	if (nextDemoRead < curTime) {
 		netcode::RawPacket* buf = new netcode::RawPacket(chunkHeader.length);
 		playbackDemo.read((char*)(buf->data), chunkHeader.length);
+		bytesRemaining -= chunkHeader.length;
 
-		if (playbackDemo.tellg() >= sizeof(fileHeader) + fileHeader.scriptSize + fileHeader.demoStreamSize)
-		{
-			playbackDemo.seekg(0, std::ios::end); //seek to end, no more stuff to read
-		}
-		else
-		{ // read next chunk ehader
+		if (!ReachedEnd()) {
+			// read next chunk header
 			playbackDemo.read((char*)&chunkHeader, sizeof(chunkHeader));
 			chunkHeader.swab();
 			nextDemoRead = chunkHeader.modGameTime + demoTimeOffset;
+			bytesRemaining -= sizeof(chunkHeader);
 		}
+
 		return buf;
 	} else {
 		return 0;
@@ -81,7 +94,7 @@ netcode::RawPacket* CDemoReader::GetData(float curTime)
 
 bool CDemoReader::ReachedEnd() const
 {
-	if (playbackDemo.eof())
+	if (bytesRemaining <= 0 || playbackDemo.eof())
 		return true;
 	else
 		return false;
@@ -104,6 +117,11 @@ const std::vector< std::vector<TeamStatistics> >& CDemoReader::GetTeamStats() co
 
 void CDemoReader::LoadStats()
 {
+	// Stats are not available if Spring crashed while writing the demo.
+	if (fileHeader.demoStreamSize == 0) {
+		return;
+	}
+
 	const int curPos = playbackDemo.tellg();
 	playbackDemo.seekg(fileHeader.headerSize + fileHeader.scriptSize + fileHeader.demoStreamSize);
 

rts/System/DemoReader.h
@@ -51,9 +51,10 @@ private:
 	std::ifstream playbackDemo;
 	float demoTimeOffset;
 	float nextDemoRead;
+	int bytesRemaining;
 	DemoStreamChunkHeader chunkHeader;
 	std::string setupScript;	// the original, unaltered version from script
-	
+
 	std::vector<PlayerStatistics> playerStats;
 	std::vector< std::vector<TeamStatistics> > teamStats;
 };