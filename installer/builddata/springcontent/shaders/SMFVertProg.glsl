284cf38e8c1da06cc7681681f0da457bdc400916
kloot <kloot@kloot0.(none)>
2010-02-02 01:30:03 +0100

    * use mix()

--- installer/builddata/springcontent/shaders/SMFVertProg.glsl
+++ installer/builddata/springcontent/shaders/SMFVertProg.glsl
@@ -17,7 +17,7 @@ varying vec3 halfDir;
 #define SMF_DETAILTEX_RES 0.02
 
 void main() {
-	mat4 modelMatrix = cameraMatInv * gl_ModelViewMatrix;
+	mat4 glModelMatrix = cameraMatInv * gl_ModelViewMatrix;
 
 	viewDir = vec3(gl_ModelViewMatrixInverse * vec4(0.0, 0.0, 0.0, 1.0));
 	viewDir = normalize(viewDir - gl_Vertex.xyz);
@@ -25,22 +25,23 @@ void main() {
 
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 
-	// diffuse- and detail-map texture coors
+	// diffuse-tex coors
 	gl_TexCoord[0].s = float(int(gl_Vertex.x) - (texSquareX * SMF_TEXSQR_SIZE_INT)) / SMF_TEXSQR_SIZE_FLT;
 	gl_TexCoord[0].t = float(int(gl_Vertex.z) - (texSquareZ * SMF_TEXSQR_SIZE_INT)) / SMF_TEXSQR_SIZE_FLT;
 	gl_TexCoord[0].q = gl_Vertex.y;
 
-	// normal-,and specular-map texture coors; this is
+	// normal- and specular-tex coors; this is equal to
 	// (D / mapD) * (mapD / pwr2mapD) for D in {x, z}
 	gl_TexCoord[1].s = gl_Vertex.x / mapSizeX;
 	gl_TexCoord[1].t = gl_Vertex.z / mapSizeZ;
 
-	// shadow-map texture coors; shadowParams stores
+	// shadow-tex coors; shadowParams stores
 	// {x=xmid, y=ymid, z=sizeFactorA, w=sizeFactorB}
-	gl_TexCoord[2] = shadowMat * (modelMatrix * gl_Vertex);
+	gl_TexCoord[2] = shadowMat * (glModelMatrix * gl_Vertex);
 	gl_TexCoord[2].st *= (inversesqrt(abs(gl_TexCoord[2].st) + shadowParams.z) + shadowParams.w);
 	gl_TexCoord[2].st += shadowParams.xy;
 
+	// detail-tex coors
 	gl_TexCoord[3].st = gl_Vertex.xz * vec2(SMF_DETAILTEX_RES, SMF_DETAILTEX_RES);
 	gl_TexCoord[3].s += -floor(cameraPos.x * SMF_DETAILTEX_RES);
 	gl_TexCoord[3].t += -floor(cameraPos.z * SMF_DETAILTEX_RES);
