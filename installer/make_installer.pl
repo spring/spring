5c5db269ff99dee3c1270c23e89ae7747829beb0
hoijui <hoijui.quaero@gmail.com>
2010-02-10 09:38:08 +0100

    installer: instead of downloading SpringDownloader during install, integrate it into the installer

--- installer/make_installer.pl
+++ installer/make_installer.pl
@@ -61,6 +61,7 @@ if($?) {
 system("sh", "installer/springlobby_download.sh");
 chdir("$installerDir/downloads");
 system("wget", "-N", "http://springrts.com/dl/TASServer.jar");
-system("wget", "-N",  "http://www.springlobby.info/installer/springsettings.exe");
+system("wget", "-N", "http://www.springlobby.info/installer/springsettings.exe");
+system("wget", "-N", "http://files.caspring.org/caupdater/SpringDownloader.exe");
 chdir("$installerDir/..");
 system("makensis -V3$testBuildString -DREVISION=$tag $allVersStr installer/spring.nsi");
