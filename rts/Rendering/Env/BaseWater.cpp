1eb79ae8ecc0ab1aad577412a7b10ce9f36e8c8d
kloot <kloot@kloot0.(none)>
2010-02-13 14:18:00 +0100

    * centralize all the ATI detection cruft

--- rts/Rendering/Env/BaseWater.cpp
+++ rts/Rendering/Env/BaseWater.cpp
@@ -39,17 +39,12 @@ void CBaseWater::DeleteOldWater(CBaseWater *water) {
 CBaseWater* CBaseWater::GetWater(CBaseWater* old)
 {
 	CBaseWater* water = NULL;
-	int configValue = configHandler->Get("ReflectiveWater",1);
+	int configValue = configHandler->Get("ReflectiveWater", 1);
 
-	// ATI drivers since early 2009 leak memory in /water 1 and /water 3
-	// add some safeguards for people who can't be bothered to change their settings
-	std::string vendor = std::string((char*)glGetString(GL_VENDOR));
-	std::transform(vendor.begin(), vendor.end(), vendor.begin(), (int (*)(int))tolower);
-	bool isATI = (vendor.find("ati ") != std::string::npos);
-	bool isATIoverride = configHandler->Get("ATIWaterOverride", 0);
-
-	if (isATI) {
-		if (isATIoverride) {
+	if (gu->haveATI) {
+		// ATI drivers since early 2009 leak memory in /water 1 and /water 3
+		// add some safeguards for people who can't be bothered to change their settings
+		if (!!configHandler->Get("ATIWaterOverride", 0)) {
 			LogObject() << "ATI water safeguard override enabled!";
 		} else if (configValue == 1) {
 			LogObject() << "ATI reflective water disabled for stability reasons.";
