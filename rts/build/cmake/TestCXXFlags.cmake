0ff1ea40e7c0ae319d651f030cb76f7d7dc4a4b6
hoijui <hoijui.quaero@gmail.com>
2010-02-25 22:57:19 +0100

    put license header in our own build files

--- rts/build/cmake/TestCXXFlags.cmake
+++ rts/build/cmake/TestCXXFlags.cmake
@@ -1,11 +1,10 @@
+# This file is part of the Spring engine (GPL v2 or later), see LICENSE.html
+
 # - Test whether the C++ compiler supports "-fvisibility=hidden"
 # Once done this will define
 #
 # VISIBILITY_HIDDEN - -fvisibility=hidden   if supported, an empty string otherwise.
 # VISIBILITY_INLINES_HIDDEN - -fvisibility-inlines-hidden   likewise
-#
-# Copyright (C) 2008 Tobi Vollebregt
-# Copyright (C) 2008 Karl-Robert Ernst
 #
 # Note: gcc for windows supports these flags, but give lots of errors when compiling, so use only for linux builds
 
--- rts/build/scons/rts.py
+++ rts/build/scons/rts.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2006  Tobi Vollebregt
+# This file is part of the Spring engine (GPL v2 or later), see LICENSE.html
 
 import os, sys
 import platform
