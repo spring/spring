f31da0fbb3cd7c876041fe3cb04001073af03249
kloot <kloot@kloot0.(none)>
2010-03-06 19:50:32 +0100

    * repair tree shadows

--- installer/builddata/springcontent/shaders/ShadowGenVertProg.glsl
+++ installer/builddata/springcontent/shaders/ShadowGenVertProg.glsl
@@ -1,10 +1,27 @@
 uniform vec4 shadowParams;   // {x = xmid, y = ymid, z = p17, w = p18}
 
+#ifdef SHADOWGEN_PROGRAM_TREE_NEAR
+uniform vec3 cameraDirX;
+uniform vec3 cameraDirY;
+uniform vec4 treeOffset;
+
+#define MAX_TREE_HEIGHT 60.0
+#endif
+
+
 void main() {
 	vec2 p17 = vec2(shadowParams.z, shadowParams.z);
 	vec2 p18 = vec2(shadowParams.w, shadowParams.w);
 
-	vec4 vertexShadowPos = gl_ModelViewMatrix * gl_Vertex;
+	vec4 vertexPos = gl_Vertex;
+
+	#ifdef SHADOWGEN_PROGRAM_TREE_NEAR
+	vertexPos.xyz += treeOffset.xyz;
+	vertexPos.xyz += (cameraDirX * gl_Normal.x);
+	vertexPos.xyz += (cameraDirY * gl_Normal.y);
+	#endif
+
+	vec4 vertexShadowPos = gl_ModelViewMatrix * vertexPos;
 		vertexShadowPos.st *= (inversesqrt(abs(vertexShadowPos.st) + p17) + p18);
 		vertexShadowPos.st += shadowParams.xy;
 
@@ -15,12 +32,12 @@ void main() {
 	#endif
 
 	#ifdef SHADOWGEN_PROGRAM_MAP
+	// empty
 	#endif
 
 	#ifdef SHADOWGEN_PROGRAM_TREE_NEAR
-	// TODO
-	// gl_FrontColor.xyz = env11 * gl_Normal.z;
-	// gl_FrontColor.a = env12.w * gl_Vertex.y + env11.w;
+	gl_FrontColor.xyz = gl_Normal.z * vec3(1.0, 1.0, 1.0);
+	gl_FrontColor.a = gl_Vertex.y * (0.20 * (1.0 / MAX_TREE_HEIGHT)) + 0.85;
 	gl_TexCoord[0] = gl_MultiTexCoord0;
 	#endif
 
