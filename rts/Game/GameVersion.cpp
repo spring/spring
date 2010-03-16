884a107a017f03878c41c01a55640164ceed661f
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-03-16 21:16:31 +0100

    bumped version to 0.81.2.1

--- rts/Game/GameVersion.cpp
+++ rts/Game/GameVersion.cpp
@@ -16,7 +16,7 @@ namespace SpringVersion
 
 const char* const Major = "0.81";
 const char* const Minor = "2";
-const char* const Patchset = "0";
+const char* const Patchset = "1";
 const char* const Additional = "" // Build-Bot will write in here before compiling
 
 #if !defined GV_ADD_SPACE
