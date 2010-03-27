fe4bdef049a910e34fde9ad495d0d25fabc8508d
kloot <kloot@kloot0.(none)>
2010-03-27 14:02:36 +0100

    * setup OBJs for S3O-style texturing

--- rts/Rendering/UnitDrawer.cpp
+++ rts/Rendering/UnitDrawer.cpp
@@ -503,7 +503,7 @@ void CUnitDrawer::DrawOpaqueUnits(int modelType, const CUnit* excludeUnit, bool 
 	UnitSet::const_iterator unitSetIt;
 
 	for (unitBinIt = unitBin.begin(); unitBinIt != unitBin.end(); ++unitBinIt) {
-		if (modelType == MODELTYPE_S3O) {
+		if (modelType == MODELTYPE_S3O || modelType == MODELTYPE_OBJ) {
 			texturehandlerS3O->SetS3oTexture(unitBinIt->first);
 		}
 
@@ -775,17 +775,17 @@ inline void CUnitDrawer::DrawOpaqueUnitShadow(CUnit* unit) {
 	#ifdef UNIT_SHADOW_ALPHA_MASKING
 		#define S3O_TEX(model) \
 			texturehandlerS3O->GetS3oTex(model->textureType)
-		#define PUSH_SHADOW_TEXTURE_STATE(model)                    \
-			if (model->type == MODELTYPE_S3O) {                     \
-				glActiveTexture(GL_TEXTURE0);                       \
-				glEnable(GL_TEXTURE_2D);                            \
-				glBindTexture(GL_TEXTURE_2D, S3O_TEX(model)->tex2); \
+		#define PUSH_SHADOW_TEXTURE_STATE(model)                                  \
+			if (model->type == MODELTYPE_S3O || model->type == MODELTYPE_OBJ) {   \
+				glActiveTexture(GL_TEXTURE0);                                     \
+				glEnable(GL_TEXTURE_2D);                                          \
+				glBindTexture(GL_TEXTURE_2D, S3O_TEX(model)->tex2);               \
 			}
-		#define POP_SHADOW_TEXTURE_STATE(model)   \
-			if (model->type == MODELTYPE_S3O) {   \
-				glBindTexture(GL_TEXTURE_2D, 0);  \
-				glDisable(GL_TEXTURE_2D);         \
-				glActiveTexture(GL_TEXTURE0);     \
+		#define POP_SHADOW_TEXTURE_STATE(model)                                   \
+			if (model->type == MODELTYPE_S3O || model->type == MODELTYPE_OBJ) {   \
+				glBindTexture(GL_TEXTURE_2D, 0);                                  \
+				glDisable(GL_TEXTURE_2D);                                         \
+				glActiveTexture(GL_TEXTURE0);                                     \
 			}
 	#else
 		#define PUSH_SHADOW_TEXTURE_STATE(model)
@@ -1041,7 +1041,7 @@ void CUnitDrawer::DrawCloakedUnitsHelper(int modelType)
 
 		// cloaked units
 		for (UnitRenderBinIt it = unitBin.begin(); it != unitBin.end(); ++it) {
-			if (modelType == MODELTYPE_S3O) {
+			if (modelType == MODELTYPE_S3O || modelType == MODELTYPE_OBJ) {
 				texturehandlerS3O->SetS3oTexture(it->first);
 			}
 
@@ -1094,7 +1094,7 @@ void CUnitDrawer::DrawCloakedUnitsSet(const std::set<CUnit*>& cloakedUnits, int 
 			glTranslatef3(unit->pos);
 			glRotatef(unit->buildFacing * 90.0f, 0, 1, 0);
 
-			if (modelType == MODELTYPE_S3O && drawGhostBuildings) {
+			if ((modelType == MODELTYPE_S3O || modelType == MODELTYPE_OBJ) && drawGhostBuildings) {
 				// the units in liveGhostedBuildings[modelType] are not
 				// sorted by textureType, but we cannot merge them with
 				// cloakedModelRenderers[modelType] since they are not
@@ -1183,7 +1183,7 @@ void CUnitDrawer::DrawGhostedBuildings(int modelType)
 				glTranslatef3((*it)->pos);
 				glRotatef((*it)->facing * 90.0f, 0, 1, 0);
 
-				if (modelType == MODELTYPE_S3O) {
+				if (modelType == MODELTYPE_S3O || modelType == MODELTYPE_OBJ) {
 					texturehandlerS3O->SetS3oTexture((*it)->model->textureType);
 				}
 
@@ -1553,7 +1553,7 @@ void CUnitDrawer::DrawIndividual(CUnit* unit)
 		SetupForUnitDrawing();
 		opaqueModelRenderers[MDL_TYPE(unit)]->PushRenderState();
 
-		if (MDL_TYPE(unit) == MODELTYPE_S3O) {
+		if (MDL_TYPE(unit) == MODELTYPE_S3O || MDL_TYPE(unit) == MODELTYPE_OBJ) {
 			texturehandlerS3O->SetS3oTexture(TEX_TYPE(unit));
 		}
 
