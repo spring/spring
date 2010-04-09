494b63cbfbf07fbec0886210d71cfc32aa036aeb
zerver <mr@zerver.net>
2010-04-09 02:58:05 +0200

    Mix units during front move

--- rts/Game/SelectedUnitsAI.h
+++ rts/Game/SelectedUnitsAI.h
@@ -31,7 +31,7 @@ private:
 	void CalculateGroupData(int player, bool queueing);
 	void MakeFrontMove(Command* c,int player);
 	void CreateUnitOrder(std::multimap<float,int>& out,int player);
-	float3 MoveToPos(int unit, float3 nextCornerPos, float3 dir, Command* command);
+	float3 MoveToPos(int unit, float3 nextCornerPos, float3 dir, Command* command, std::vector<std::pair<int,Command> > *frontcmds, bool *newline);
 	void AddUnitSetMaxSpeedCommand(CUnit* unit, unsigned char options);
 	void AddGroupSetMaxSpeedCommand(CUnit* unit, unsigned char options);
 	void SelectAttack(const Command& cmd, int player);
