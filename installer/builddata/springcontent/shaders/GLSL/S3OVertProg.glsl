f672b6cd9140b0775a574679a0087fd5e74e5aa6
kloot <kloot@kloot0.(none)>
2010-04-25 20:01:40 +0200

    * initial fix for #1894

--- installer/builddata/springcontent/shaders/GLSL/S3OVertProg.glsl
+++ installer/builddata/springcontent/shaders/GLSL/S3OVertProg.glsl
@@ -1,16 +1,19 @@
-uniform vec3 cameraPos;     // world-space
-varying vec3 cameraDir;     // world-space
-varying vec3 vertexNormal;  // model-space
+uniform vec3 cameraPos;      // WS
+varying vec3 cameraDir;      // WS
+varying vec3 vertexNormal;   // OS
 
 varying float fogFactor;
 
-uniform mat4 cameraMatInv;  // unused
+varying mat3 normalMatrix;
+uniform mat4 cameraMat;
+uniform mat4 cameraMatInv;
 uniform mat4 shadowMat;
 uniform vec4 shadowParams;
 
 void main() {
 	// note: gl_ModelViewMatrix actually only contains the
 	// model matrix, view matrix is on the projection stack
+	// because of this, gl_NormalMatrix is not what we want
 	//
 	// todo: clip gl_Position against gl_ClipPlane[3] if advFade
 	//
@@ -30,12 +33,15 @@ void main() {
 	cameraDir = vertexPos.xyz - cameraPos;
 	vertexNormal = gl_Normal;
 
+	// M and V are both (always) orthonormal
+	normalMatrix = mat3(cameraMat * gl_ModelViewMatrix);
+
 	// diffuse- and shading-tex coors, shadow-tex coors
 	gl_TexCoord[0].st = gl_MultiTexCoord0.st;
 	gl_TexCoord[1] = vertexShadowPos;
 
 	gl_FogFragCoord = length(((gl_ProjectionMatrix * gl_ModelViewMatrix) * gl_Vertex).xyz);
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
 
 	fogFactor = (gl_Fog.end - gl_FogFragCoord) / (gl_Fog.end - gl_Fog.start);
 	fogFactor = clamp(fogFactor, 0.0, 1.0);
