887000702c9521e9decc991065c62382c1b45f45
kloot <kloot@kloot0.(none)>
2010-05-01 03:24:33 +0200

    * fix USE_MMGR compilation

--- rts/System/LuaLoadSaveHandler.cpp
+++ rts/System/LuaLoadSaveHandler.cpp
@@ -1,9 +1,11 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
+#include <string>
+#include <sstream>
+
 #include "StdAfx.h"
 #include "LuaLoadSaveHandler.h"
 
-#include <sstream>
 #include "lib/minizip/zip.h"
 
 #include "ExternalAI/EngineOutHandler.h"
