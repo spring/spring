07ab446c6beb7645de5c2fccbf77f6d9b5d75a8f
zerver <mr@zerver.net>
2010-05-23 20:23:54 +0300

    Fix dediserv compile error

--- rts/Sim/Misc/TeamHandler.cpp
+++ rts/Sim/Misc/TeamHandler.cpp
@@ -8,7 +8,9 @@
 #include "Game/GameSetup.h"
 #include "Lua/LuaGaia.h"
 #include "Sim/Misc/GlobalConstants.h"
+#ifndef DEDICATED
 #include "Sim/Misc/TeamHighlight.h"
+#endif
 #include "mmgr.h"
 #include "Util.h"
 #include "LogOutput.h"
@@ -98,6 +100,8 @@ void CTeamHandler::GameFrame(int frameNum)
 		for (int a = 0; a < ActiveTeams(); ++a) {
 			Team(a)->SlowUpdate();
 		}
+#ifndef DEDICATED
 		CTeamHighlight::Update();
+#endif
 	}
 }
