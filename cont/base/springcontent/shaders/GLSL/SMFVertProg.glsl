06b3285620303b97a984980e890cdba8b26bf877
-jk- <-jk->
2010-05-25 19:40:20 +0200

    SMF GLSL shader changes:  * fixed some float/int conflicts  * made it runable on OpenGL2.0 cards (32 varying components limit)  * some syntax cleanup c++ part of it:  * the normalmap is now stored in a 2 component 16F texture (z-coord is reconstructed in the shader)  * you can increase the normalmap precision to 32F with "GroundNormalTextureHighPrecision" (default off)  * fixed some duplicated texture generation
    note:
    I just had Blindside as a testmap for the debugging, and it looks quite differently from the screenshots in the forum thread, so I don't know if I broke it somewhere. So responses are welcome.

--- cont/base/springcontent/shaders/GLSL/SMFVertProg.glsl
+++ cont/base/springcontent/shaders/GLSL/SMFVertProg.glsl
@@ -9,66 +9,43 @@ uniform int texSquareZ;
 
 uniform vec3 cameraPos;
 uniform vec4 lightDir;      // mapInfo->light.sunDir
-varying vec3 cameraDir;     // WS
-varying vec3 viewDir;
 varying vec3 halfDir;
 
 varying float fogFactor;
-varying float vertexHeight;
+
+varying vec4 vertexPos;
+varying vec2 diffuseTexCoords;
+varying vec2 specularTexCoords;
+varying vec2 normalTexCoords;
 
 // defaults to SMF_DETAILTEX_RES per channel
 uniform vec4 splatTexScales;
 
 void main() {
-	viewDir = vec3(gl_ModelViewMatrixInverse * vec4(0.0, 0.0, 0.0, 1.0));
+	vec3 viewDir = vec3(gl_ModelViewMatrixInverse * vec4(0.0, 0.0, 0.0, 1.0));
 	viewDir = normalize(viewDir - gl_Vertex.xyz);
 	halfDir = normalize(lightDir.xyz + viewDir);
-	cameraDir = gl_Vertex.xyz - cameraPos;
+	vertexPos = gl_Vertex;
 
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+	gl_Position = gl_ModelViewMatrix * gl_Vertex;
+	float fogCoord = length(gl_Position.xyz);
+	gl_Position = gl_ProjectionMatrix * gl_Position;
 
-	// diffuse-tex coors
-	gl_TexCoord[0].s = float(int(gl_Vertex.x) - (texSquareX * SMF_TEXSQR_SIZE_INT)) / SMF_TEXSQR_SIZE_FLT;
-	gl_TexCoord[0].t = float(int(gl_Vertex.z) - (texSquareZ * SMF_TEXSQR_SIZE_INT)) / SMF_TEXSQR_SIZE_FLT;
-	gl_TexCoord[0].q = gl_Vertex.y;
-
-	// normal-tex coors
-	gl_TexCoord[1].s = gl_Vertex.x / mapSizePO2.x;
-	gl_TexCoord[1].t = gl_Vertex.z / mapSizePO2.y;
-
-	// specular-tex coors
-	gl_TexCoord[2].s = gl_Vertex.x / mapSize.x;
-	gl_TexCoord[2].t = gl_Vertex.z / mapSize.y;
-
-	// shadow-tex coors (map vertices are already in world-space)
-	gl_TexCoord[3] = gl_Vertex;
+	diffuseTexCoords  = vec2(ivec2(gl_Vertex.xz) - (ivec2(texSquareX,texSquareZ) * SMF_TEXSQR_SIZE_INT)) / SMF_TEXSQR_SIZE_FLT;
+	specularTexCoords = gl_Vertex.xz / mapSize.xy;
+	normalTexCoords   = gl_Vertex.xz / mapSizePO2.xy;
 
 	// detail-tex coors
 	#if (SMF_DETAIL_TEXTURE_SPLATTING == 0)
-	gl_TexCoord[4].st  = gl_Vertex.xz * vec2(SMF_DETAILTEX_RES, SMF_DETAILTEX_RES);
-	gl_TexCoord[4].s  += -floor(cameraPos.x * SMF_DETAILTEX_RES);
-	gl_TexCoord[4].t  += -floor(cameraPos.z * SMF_DETAILTEX_RES);
+		gl_TexCoord[0].st  = gl_Vertex.xz * vec2(SMF_DETAILTEX_RES);
 	#else
-	gl_TexCoord[4].st  = gl_Vertex.xz * vec2(splatTexScales.r, splatTexScales.r);
-	gl_TexCoord[4].s  += -floor(cameraPos.x * splatTexScales.r);
-	gl_TexCoord[4].t  += -floor(cameraPos.z * splatTexScales.r);
-	gl_TexCoord[5].st  = gl_Vertex.xz * vec2(splatTexScales.g, splatTexScales.g);
-	gl_TexCoord[5].s  += -floor(cameraPos.x * splatTexScales.g);
-	gl_TexCoord[5].t  += -floor(cameraPos.z * splatTexScales.g);
-	gl_TexCoord[6].st  = gl_Vertex.xz * vec2(splatTexScales.b, splatTexScales.b);
-	gl_TexCoord[6].s  += -floor(cameraPos.x * splatTexScales.b);
-	gl_TexCoord[6].t  += -floor(cameraPos.z * splatTexScales.b);
-	gl_TexCoord[7].st  = gl_Vertex.xz * vec2(splatTexScales.a, splatTexScales.a);
-	gl_TexCoord[7].s  += -floor(cameraPos.x * splatTexScales.a);
-	gl_TexCoord[7].t  += -floor(cameraPos.z * splatTexScales.a);
+		gl_TexCoord[0].st  = gl_Vertex.xz * vec2(splatTexScales.r);
+		gl_TexCoord[0].pq  = gl_Vertex.xz * vec2(splatTexScales.g);
+		gl_TexCoord[1].st  = gl_Vertex.xz * vec2(splatTexScales.b);
+		gl_TexCoord[1].pq  = gl_Vertex.xz * vec2(splatTexScales.a);
 	#endif
 
-	// gl_FogCoord is not initialized
-	gl_FogFragCoord = length((gl_ModelViewMatrix * gl_Vertex).xyz);
-
 	// emulate linear fog
-	fogFactor = (gl_Fog.end - gl_FogFragCoord) / (gl_Fog.end - gl_Fog.start);
+	fogFactor = (gl_Fog.end - fogCoord) / (gl_Fog.end - gl_Fog.start);
 	fogFactor = clamp(fogFactor, 0.0, 1.0);
-
-	vertexHeight = gl_Vertex.y;
 }
