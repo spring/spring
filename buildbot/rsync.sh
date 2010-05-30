34abc29feca756196cb92959960010a94809e5ca
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-05-30 15:26:11 +0200

    buildbot: ensure correct umask before creating world readable files

--- buildbot/rsync.sh
+++ buildbot/rsync.sh
@@ -18,6 +18,7 @@ EXE_ARCHIVE=${TMP_PATH}/${REV}_spring.7z
 USYNC_ARCHIVE=${TMP_PATH}/${REV}_unitsync.7z
 CMD="rsync -avz --chmod=D+rx,F+r"
 
+umask 022
 7z u ${BASE_ARCHIVE} ${LOCAL_BASE}/base
 7z u ${EXE_ARCHIVE} ${LOCAL_BASE}/spring.exe
 7z u ${USYNC_ARCHIVE} ${LOCAL_BASE}/unitsync.dll
