89bc4758395370d1db4833b5530ab7d25415e0c9
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-05-30 15:14:10 +0200

    buildbot: make rsync ensure dirs/files are world readable

--- buildbot/rsync.sh
+++ buildbot/rsync.sh
@@ -16,15 +16,16 @@ LOCAL_BASE=build-${CONFIG}
 BASE_ARCHIVE=${TMP_PATH}/${REV}_base.7z
 EXE_ARCHIVE=${TMP_PATH}/${REV}_spring.7z
 USYNC_ARCHIVE=${TMP_PATH}/${REV}_unitsync.7z
+CMD="rsync -avz --chmod=D+rx,F+r"
 
 7z u ${BASE_ARCHIVE} ${LOCAL_BASE}/base
 7z u ${EXE_ARCHIVE} ${LOCAL_BASE}/spring.exe
 7z u ${USYNC_ARCHIVE} ${LOCAL_BASE}/unitsync.dll
 
 if [ ${REMOTE_HOST} = localhost ] && [ -w ${REMOTE_BASE} ]; then
-	rsync -av ${TMP_BASE}/ ${REMOTE_BASE}/
+	${CMD} ${TMP_BASE}/ ${REMOTE_BASE}/
 else
-	rsync -avz ${TMP_BASE}/ ${REMOTE_HOST}:${REMOTE_BASE}/
+	${CMD} ${TMP_BASE}/ ${REMOTE_HOST}:${REMOTE_BASE}/
 fi
 
 #rm -rf ${TMP_BASE}/*
