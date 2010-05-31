ec4ba76562324d9399718de4eb92fb801998c07d
hoijui <hoijui.quaero@gmail.com>
2010-05-31 15:27:05 +0200

    adjust to master default GPL file header [minor]

--- tools/HeadlessSpring/lib/headlessstubs/glstub.c
+++ tools/HeadlessSpring/lib/headlessstubs/glstub.c
@@ -1,23 +1,4 @@
-// Copyright Hugh Perkins 2009
-// hughperkins@gmail.com http://manageddreams.com
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-//  more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program in the file licence.txt; if not, write to the
-// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-
-// 1307 USA
-// You can find the licence also on the web at:
-// http://www.opensource.org/licenses/gpl-license.php
-//
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "GL/gl.h"
 
@@ -315,3 +296,4 @@ GLAPI void APIENTRY glTexCoord2f(GLfloat s, GLfloat t ) {}
 GLAPI void APIENTRY glLineWidth(GLfloat width) {}
 GLAPI void APIENTRY glColor3f(GLfloat red, GLfloat green, GLfloat blue ) {}
 GLAPI void APIENTRY glPolygonOffset(GLfloat factor, GLfloat units) {}
+
