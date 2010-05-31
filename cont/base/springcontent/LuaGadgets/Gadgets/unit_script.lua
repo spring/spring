3dac42306aa08f8e985ac28cc960f0b4f07a6b0d
hoijui <hoijui.quaero@gmail.com>
2010-05-31 22:21:12 +0200

    fix merge fails [fix]

--- cont/base/springcontent/LuaGadgets/Gadgets/unit_script.lua
+++ cont/base/springcontent/LuaGadgets/Gadgets/unit_script.lua
@@ -348,16 +348,6 @@ function Spring.UnitScript.StartThread(fun, ...)
 end
 
 local function SetOnError(fun)
-<<<<<<< HEAD
-	local thread = activeUnit.threads[co_running()]
-	if thread then
-		thread.onerror = fun
-	end
-end
-
-function Spring.UnitScript.SetSignalMask(mask)
-=======
->>>>>>> spring/master
 	local thread = activeUnit.threads[co_running()]
 	if thread then
 		thread.onerror = fun
