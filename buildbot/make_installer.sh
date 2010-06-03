d1e3d49ecb39ca7bd4ce9f717825162ee7d44aa3
hoijui <hoijui.quaero@gmail.com>
2010-06-03 23:37:20 +0200

    do not define DIST_DIR on new build-bot [installer] [fix]

--- buildbot/make_installer.sh
+++ buildbot/make_installer.sh
@@ -16,7 +16,8 @@ REV=$(git describe --tags)
 echo "!define MINGWLIBS_DIR \"${MINGWLIBS_PATH}\"" > installer/custom_defines.nsi
 echo "!define BUILD_DIR \"${BUILDDIR}\"" >> installer/custom_defines.nsi
 
-./installer/make_installer.pl
+# The 0 means: do not define DIST_DIR
+./installer/make_installer.pl 0
 
 mv ./installer/spring*.exe ${TMP_PATH}
 
