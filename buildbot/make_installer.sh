44a27ee76bbf94d8a35cce373e5474d0f81010dc
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-06-06 22:40:13 +0200

    buildbot: a shot at symbol seperation
    Conflicts:
    
    	buildbot/make_installer.sh
    	buildbot/rsync.sh

--- buildbot/make_installer.sh
+++ buildbot/make_installer.sh
@@ -8,6 +8,7 @@ cd $(dirname $0)/..
 CONFIG=${1}
 BRANCH=${2}
 MINGWLIBS_PATH=${3}
+MINGW_HOST=i586-pc-mingw32-
 BUILDDIR=${PWD}/build-${CONFIG}
 TMP_BASE=/tmp/spring
 TMP_PATH=${TMP_BASE}/${CONFIG}/${BRANCH}
@@ -15,6 +16,14 @@ REV=$(git describe --tags)
 
 echo "!define MINGWLIBS_DIR \"${MINGWLIBS_PATH}\"" > installer/custom_defines.nsi
 echo "!define BUILD_DIR \"${BUILDDIR}\"" >> installer/custom_defines.nsi
+
+#strip symbols and archive them
+for tostripfile in spring.exe unitsync.dll ; do
+	debugfile=${tostripfile}.dbg
+	${MINGW_HOST}objcopy --only-keep-debug ${BUILDDIR}/${tostripfile} ${BUILDDIR}/${debugfile}
+	${MINGW_HOST}strip --strip-debug --strip-unneeded ${BUILDDIR}/${tostripfile}
+	${MINGW_HOST}objcopy --add-gnu-debuglink=${BUILDDIR}/${debugfile} ${BUILDDIR}/${tostripfile}
+done
 
 # The 0 means: do not define DIST_DIR
 ./installer/make_installer.pl 0
