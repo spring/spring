3a3dfbc816bab9887f35c7e70622fabf9eff6bab
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-06-07 22:23:30 +0200

    buildbot: create & upload debug symbols of everything

--- buildbot/rsync.sh
+++ buildbot/rsync.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Quit on error.
 set -e
@@ -14,21 +14,41 @@ TMP_PATH=${TMP_BASE}/${CONFIG}/${BRANCH}
 REV=$(git describe --tags)
 LOCAL_BASE=build-${CONFIG}
 BASE_ARCHIVE=${TMP_PATH}/${REV}_base.7z
-EXE_ARCHIVE=${TMP_PATH}/${REV}_spring.7z
-USYNC_ARCHIVE=${TMP_PATH}/${REV}_unitsync.7z
-DEBUG_ARCHIVE=${TMP_PATH}/${REV}_debug.7z
 CMD="rsync -avz --chmod=D+rx,F+r"
 
 umask 022
-7z u ${BASE_ARCHIVE} ${LOCAL_BASE}/base
-7z u ${EXE_ARCHIVE} ${LOCAL_BASE}/spring.exe
-7z u ${USYNC_ARCHIVE} ${LOCAL_BASE}/unitsync.dll
-7z u ${DEBUG_ARCHIVE} ${LOCAL_BASE}/*.dbg
 
+# Usage: zip <file> <name>
+function zip() {
+	file=$1
+	name=$2
+	if [ -f ${file} ]; then
+		debugfile=${file%.*}.dbg
+		archive=${TMP_PATH}/${REV}_${name}.7z
+		archive_debug=${TMP_PATH}/${REV}_${name}_dbg.7z
+		7z u ${archive} ${file}
+		[ ! -f ${debugfile} ] || 7z u ${archive_debug} ${debugfile}
+	fi
+}
+
+# Create one archive for base content and two archives for each exe/dll:
+# one containing the exe/dll, and one containing debug symbols.
+cd ${LOCAL_BASE}
+7z u ${BASE_ARCHIVE} base
+for tostripfile in spring.exe spring-gml.exe spring-hl.exe unitsync.dll; do
+	zip ${tostripfile} ${tostripfile%.*}
+done
+for tostripfile in $(find AI/Skirmish -name SkirmishAI.dll); do
+	zip ${tostripfile} $(basename $(dirname ${tostripfile}))
+done
+cd $OLDPWD
+
+# Rsync archives to a world-visible location.
 if [ ${REMOTE_HOST} = localhost ] && [ -w ${REMOTE_BASE} ]; then
 	${CMD} ${TMP_BASE}/ ${REMOTE_BASE}/
 else
 	${CMD} ${TMP_BASE}/ ${REMOTE_HOST}:${REMOTE_BASE}/
 fi
 
-#rm -rf ${TMP_BASE}/*
+# Clean up.
+rm -rf ${TMP_BASE}/*
