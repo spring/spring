7a3e19e3fda54ec1aa9c6dc0b2798931eed4c18b
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-06-10 22:09:15 +0200

    buildbot: factor out some common logic, made filenames consistent with version (except installer)

---
+++ buildbot/prepare.sh
@@ -0,0 +1,33 @@
+# This file is included by the other shell scripts to set up some variables.
+# Each of the scripts (including this) gets 2 arguments: <config> <branch>
+
+# It sets the following variables:
+# - CONFIG   : configuration (default, debug2, syncdebug, etc.)
+# - BRANCH   : branch (master, etc.)
+# - CONFIG_  : $CONFIG wrapped in [] or empty if CONFIG=default
+# - BRANCH_  : $BRANCH wrapped in {} or empty if BRANCH=master
+# - REV      : output of `git describe --tags'
+# - BUILDDIR : absolute path to the build directory
+
+# Quit on error.
+set -e
+cd $(dirname $0)/..
+
+CONFIG=${1:-default}
+BRANCH=${2:-master}
+REV=$(git describe --tags)
+BUILDDIR=${PWD}/build-${CONFIG}
+
+if [ ${CONFIG} == default ]; then
+   CONFIG_=''
+else
+   CONFIG_="[${CONFIG}]"
+fi
+
+if [ ${BRANCH} == master ]; then
+   BRANCH_=''
+else
+   BRANCH_="{${BRANCH}}"
+fi
+
+VERSION="${CONFIG_}${BRANCH_}${REV}"
