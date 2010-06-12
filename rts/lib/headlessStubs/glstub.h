c77370ac8bbfd38939773b66dde77404a2b35e5a
-jk- <-jk->
2010-06-12 18:53:06 +0200

    * finish headless under windows, too * pretend some gl extension, too reduce the cpu overhead * nv_dds uses now GLEW function linkings

--- rts/lib/headlessStubs/glstub.h
+++ rts/lib/headlessStubs/glstub.h
@@ -6,17 +6,7 @@
 #undef GL_GLEXT_LEGACY
 #define GL_GLEXT_PROTOTYPES
 #include <GL/gl.h>
-
-#ifdef    WIN32
-#include <stddef.h> // for ptrdiff_t
-// these are defined in glew.h under windows
-typedef char GLchar;
-typedef char GLcharARB;
-typedef unsigned int GLhandleARB;
-typedef ptrdiff_t GLsizeiptrARB;
-typedef ptrdiff_t GLsizeiptr;
-typedef ptrdiff_t GLintptr;
-#endif // WIN32
+#include <GL/glext.h> //gl.h may not include all extensions
 
 #endif // _GL_STUB_H_
 
