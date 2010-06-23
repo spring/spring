d3382d9c24bbeda6b7234dc3b6e79dbd73289672
kloot <kloot@kloot0.(none)>
2010-06-24 01:04:15 +0200

    * small header cleanup

--- rts/Sim/Features/Feature.cpp
+++ rts/Sim/Features/Feature.cpp
@@ -13,7 +13,6 @@
 #include "myMath.h"
 #include "Sim/Misc/DamageArray.h"
 #include "Sim/Misc/QuadField.h"
-#include "Rendering/FeatureDrawer.h"
 #include "Rendering/Env/BaseTreeDrawer.h"
 #include "Rendering/Models/3DModel.h"
 #include "Sim/Misc/CollisionVolume.h"
