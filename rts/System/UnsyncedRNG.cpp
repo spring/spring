c9f92bd292284a865e9351b31bd9bded34f88b38
kloot <kloot@kloot0.(none)>
2010-07-17 01:40:54 +0200

    * s/0x7fff/RANDINT_MAX

--- rts/System/UnsyncedRNG.cpp
+++ rts/System/UnsyncedRNG.cpp
@@ -2,6 +2,7 @@
 
 #include "StdAfx.h"
 #include "UnsyncedRNG.h"
+#include "Sim/Misc/GlobalConstants.h"
 
 #include <limits.h>
 
@@ -17,22 +18,22 @@ void UnsyncedRNG::Seed(unsigned seed)
 unsigned UnsyncedRNG::operator()()
 {
 	randSeed = (randSeed * 214013L + 2531011L);
-	return (randSeed >> 16) & 0x7FFF;
+	return (randSeed >> 16) & RANDINT_MAX;
 }
 
 int UnsyncedRNG::RandInt()
 {
 	randSeed = (randSeed * 214013L + 2531011L);
-	return (randSeed >> 16) & 0x7FFF;
+	return (randSeed >> 16) & RANDINT_MAX;
 }
 
 float UnsyncedRNG::RandFloat()
 {
 	randSeed = (randSeed * 214013L + 2531011L);
-	return float((randSeed >> 16) & 0x7FFF) / 0x7FFF;
+	return float((randSeed >> 16) & RANDINT_MAX) / RANDINT_MAX;
 }
 
 int UnsyncedRNG::operator()(int n)
 {
-	return RandInt() * n / ((INT_MAX & 0x7FFF) + 1); // the range of RandInt() is limited to (INT_MAX & 0x7FFF)
+	return RandInt() * n / ((INT_MAX & RANDINT_MAX) + 1); // the range of RandInt() is limited to (INT_MAX & 0x7FFF)
 }
