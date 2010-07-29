d27cb1db662f2360b0c1ab41bd6570c3c389cc78
zerver <mr@zerver.net>
2010-07-29 23:33:57 +0200

    Fix mantis #1842 (dark metal spots in F4 view, escpecially in water)

--- rts/Map/SMF/BFGroundDrawer.cpp
+++ rts/Map/SMF/BFGroundDrawer.cpp
@@ -1279,8 +1279,10 @@ void CBFGroundDrawer::SetupTextureUnits(bool drawReflection)
 		glActiveTexture(GL_TEXTURE2);
 		glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, map->GetShadingTexture());
+		glMultiTexCoord4f(GL_TEXTURE2_ARB, 1.0f, 1.0f, 1.0f, 1.0f); //fixes a nvidia bug with gltexgen
+		glTexEnvi(GL_TEXTURE_ENV, GL_COMBINE_RGB_ARB, GL_ADD_SIGNED_ARB);
+		glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_COMBINE_ARB);
 		SetTexGen(1.0f / (gs->pwr2mapx * SQUARE_SIZE), 1.0f / (gs->pwr2mapy * SQUARE_SIZE), 0, 0);
-		glMultiTexCoord4f(GL_TEXTURE2_ARB, 1,1,1,1); //fixes a nvidia bug with gltexgen
 
 		glActiveTexture(GL_TEXTURE3);
 		if (map->detailTex) {
