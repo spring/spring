e359f0ed32d9408ce0e99b504775ec9e7f206f1c
hoijui <hoijui.quaero@gmail.com>
2010-08-05 09:57:49 +0200

    cleanup (no logic changes)

--- rts/Game/UI/EndGameBox.h
+++ rts/Game/UI/EndGameBox.h
@@ -4,10 +4,10 @@
 #define __END_GAME_BOX_H__
 
 #include "InputReceiver.h"
-#include <list>
-#include <vector>
 #include "Rendering/GL/myGL.h"
 #include "Rendering/Textures/Bitmap.h"
+
+#include <vector>
 
 
 // msvc behaves really strange
@@ -18,19 +18,18 @@ namespace std {
 }
 #endif
 
-class CEndGameBox :
-	public CInputReceiver
+class CEndGameBox : public CInputReceiver
 {
 public:
-	CEndGameBox(void);
-	~CEndGameBox(void);
+	CEndGameBox();
+	~CEndGameBox();
 
 	virtual bool MousePress(int x, int y, int button);
-	virtual void MouseMove(int x, int y, int dx,int dy, int button);
+	virtual void MouseMove(int x, int y, int dx, int dy, int button);
 	virtual void MouseRelease(int x, int y, int button);
 	virtual void Draw();
 	virtual bool IsAbove(int x, int y);
-	virtual std::string GetTooltip(int x,int y);
+	virtual std::string GetTooltip(int x, int y);
 
 	static bool disabled;
 
@@ -51,20 +50,17 @@ protected:
 	int stat1;
 	int stat2;
 
-	struct TeamStat {
-	};
-
 	struct Stat {
-		Stat(std::string s) : name(s),max(1),maxdif(1){}
+		Stat(std::string s) : name(s), max(1), maxdif(1) {}
 
-		void AddStat(int team,float value){
-			if (value>max) {
-				max=value;
+		void AddStat(int team, float value) {
+			if (value > max) {
+				max = value;
 			}
 			if (team >= 0 && static_cast<size_t>(team) >= values.size()) {
-				values.resize(team+1);
+				values.resize(team + 1);
 			}
-			if (values[team].size()>0 && fabs(value-values[team].back())>maxdif) {
+			if (values[team].size() > 0 && fabs(value-values[team].back()) > maxdif) {
 				maxdif = fabs(value-values[team].back());
 			}
 
