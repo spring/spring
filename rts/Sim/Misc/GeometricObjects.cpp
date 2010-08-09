a74c4e4e8a2d07fbf02af486c8baa687620b377a
hoijui <hoijui.quaero@gmail.com>
2010-08-10 00:17:38 +0200

    cleanup (no code logic changes)

--- rts/Sim/Misc/GeometricObjects.cpp
+++ rts/Sim/Misc/GeometricObjects.cpp
@@ -25,13 +25,13 @@ CR_REG_METADATA_SUB(CGeometricObjects, GeoGroup, (CR_MEMBER(squares)));
 
 CGeometricObjects* geometricObjects;
 
-CGeometricObjects::CGeometricObjects(void)
+CGeometricObjects::CGeometricObjects()
+	: firstFreeGroup(1)
 {
-	firstFreeGroup = 1;
 }
 
 
-CGeometricObjects::~CGeometricObjects(void)
+CGeometricObjects::~CGeometricObjects()
 {
 	toBeDeleted.clear();
 	while (!geoGroups.empty()) {
@@ -41,30 +41,34 @@ CGeometricObjects::~CGeometricObjects(void)
 
 int CGeometricObjects::AddSpline(float3 b1, float3 b2, float3 b3, float3 b4, float width, int arrow, int lifeTime, int group)
 {
-	if (group == 0)
+	if (group == 0) {
 		group = firstFreeGroup++;
-
-	float3 old1,old2;
-	old1=CalcSpline( 0.00f,b1,b2,b3,b4);
-	old2=CalcSpline( 0.05f,b1,b2,b3,b4);
-	for(int a=0;a<20;++a){
-		float3 np=CalcSpline(a*0.05f+0.1f,b1,b2,b3,b4);
-		float3 dir1=(old2-old1).ANormalize();
-		float3 dir2=(np-old2).ANormalize();
-
-		if(arrow==1 && a==19){
-			CGeoSquareProjectile* gsp=new CGeoSquareProjectile(old1,old2,dir1,dir2,width,0);
-			geoGroups[group].squares.push_back(gsp);
-//			logOutput.Print("%f %f %f %f %f %f %f %f %f",old1.x,old1.y,old1.z,old2.x,old2.y,old2.z,np.x,np.y,np.z);
-		} else {
-			CGeoSquareProjectile* gsp=new CGeoSquareProjectile(old1,old2,dir1,dir2,width*0.5f,width*0.5f);
-			geoGroups[group].squares.push_back(gsp);
-		}
-		old1=old2;
-		old2=np;
 	}
-	if(lifeTime>0)
-		toBeDeleted.insert(std::pair<int,int>(gs->frameNum+lifeTime,group));
+
+	float3 old1, old2;
+	old1 = CalcSpline(0.00f, b1, b2, b3, b4);
+	old2 = CalcSpline(0.05f, b1, b2, b3, b4);
+	for (int a = 0; a < 20; ++a) {
+		const float3 np = CalcSpline(a*0.05f + 0.1f, b1, b2, b3, b4);
+		const float3 dir1 = (old2 - old1).ANormalize();
+		const float3 dir2 = (np - old2).ANormalize();
+
+		float w1, w2;
+		if ((arrow == 1) && (a == 19)) {
+			w1 = width;
+			w2 = 0;
+		} else {
+			w1 = width * 0.5f;
+			w2 = w1;
+		}
+		CGeoSquareProjectile* gsp = new CGeoSquareProjectile(old1, old2, dir1, dir2, w1, w2);
+		geoGroups[group].squares.push_back(gsp);
+		old1 = old2;
+		old2 = np;
+	}
+	if (lifeTime > 0) {
+		toBeDeleted.insert(std::pair<int, int>(gs->frameNum + lifeTime, group));
+	}
 
 	return group;
 }
@@ -72,12 +76,12 @@ int CGeometricObjects::AddSpline(float3 b1, float3 b2, float3 b3, float3 b4, flo
 
 void CGeometricObjects::DeleteGroup(int group)
 {
-	GeoGroup* gg=&geoGroups[group];
+	GeoGroup* gg = &geoGroups[group];
 
 	std::vector<CGeoSquareProjectile*>::iterator gi;
 
-	for(gi=gg->squares.begin();gi!=gg->squares.end();++gi){
-		(*gi)->deleteMe=true;
+	for (gi = gg->squares.begin(); gi != gg->squares.end(); ++gi) {
+		(*gi)->deleteMe = true;
 	}
 
 	geoGroups.erase(group);
@@ -86,24 +90,24 @@ void CGeometricObjects::DeleteGroup(int group)
 
 void CGeometricObjects::SetColor(int group, float r, float g, float b, float a)
 {
-	GeoGroup* gg=&geoGroups[group];
+	GeoGroup* gg = &geoGroups[group];
 
 	std::vector<CGeoSquareProjectile*>::iterator gi;
 
-	for(gi=gg->squares.begin();gi!=gg->squares.end();++gi){
-		(*gi)->r=r;
-		(*gi)->g=g;
-		(*gi)->b=b;
-		(*gi)->a=a;
+	for (gi = gg->squares.begin(); gi != gg->squares.end(); ++gi) {
+		(*gi)->r = r;
+		(*gi)->g = g;
+		(*gi)->b = b;
+		(*gi)->a = a;
 	}
 }
 
 
 float3 CGeometricObjects::CalcSpline(float i, const float3& p1, const float3& p2, const float3& p3, const float3& p4)
 {
-	float ni=1-i;
+	float ni = 1 - i;
 
-	float3 res=p1*ni*ni*ni+p2*3*i*ni*ni+p3*3*i*i*ni+p4*i*i*i;
+	float3 res = p1*ni*ni*ni + p2*3*i*ni*ni + p3*3*i*i*ni + p4*i*i*i;
 //	logOutput.Print("%f %f %f",res.x,res.y,res.z);
 	return res;
 }
@@ -111,32 +115,34 @@ float3 CGeometricObjects::CalcSpline(float i, const float3& p1, const float3& p2
 
 int CGeometricObjects::AddLine(float3 start, float3 end, float width, int arrow, int lifetime, int group)
 {
-	if(group==0)
-		group=firstFreeGroup++;
+	if (group == 0) {
+		group = firstFreeGroup++;
+	}
 
-	float3 dir=(end-start).ANormalize();
-	if(arrow){
-		CGeoSquareProjectile* gsp=new CGeoSquareProjectile(start,start*0.2f+end*0.8f,dir,dir,width*0.5f,width*0.5f);
+	float3 dir = (end - start).ANormalize();
+	if (arrow) {
+		CGeoSquareProjectile* gsp = new CGeoSquareProjectile(start, start*0.2f + end*0.8f, dir, dir, width*0.5f, width*0.5f);
 		geoGroups[group].squares.push_back(gsp);
 
-		gsp=new CGeoSquareProjectile(start*0.2f+end*0.8f,end,dir,dir,width,0);
+		gsp = new CGeoSquareProjectile(start*0.2f + end*0.8f, end, dir, dir, width, 0);
 		geoGroups[group].squares.push_back(gsp);
 
 	} else {
-		CGeoSquareProjectile* gsp=new CGeoSquareProjectile(start,end,dir,dir,width*0.5f,width*0.5f);
+		CGeoSquareProjectile* gsp = new CGeoSquareProjectile(start, end, dir, dir, width*0.5f, width*0.5f);
 		geoGroups[group].squares.push_back(gsp);
 	}
 
-	if(lifetime>0)
-		toBeDeleted.insert(std::pair<int,int>(gs->frameNum+lifetime,group));
+	if (lifetime > 0) {
+		toBeDeleted.insert(std::pair<int, int>(gs->frameNum + lifetime, group));
+	}
 
 	return group;
 }
 
 
-void CGeometricObjects::Update(void)
+void CGeometricObjects::Update()
 {
-	while(!toBeDeleted.empty() && toBeDeleted.begin()->first<=gs->frameNum){
+	while(!toBeDeleted.empty() && (toBeDeleted.begin()->first <= gs->frameNum)) {
 		DeleteGroup(toBeDeleted.begin()->second);
 		toBeDeleted.erase(toBeDeleted.begin());
 	}
@@ -144,14 +150,18 @@ void CGeometricObjects::Update(void)
 
 
 void CGeometricObjects::MarkSquare(int mapSquare) {
+
 	float3 startPos;
-	startPos.x = int(mapSquare * SQUARE_SIZE) % gs->mapx;
-	startPos.z = int(mapSquare * SQUARE_SIZE) / gs->mapx;
+	startPos.x = (int) (mapSquare * SQUARE_SIZE) % gs->mapx;
+	startPos.z = (int) (mapSquare * SQUARE_SIZE) / gs->mapx;
 	startPos.y = readmap->centerheightmap[mapSquare];
+
 	float3 endPos = startPos;
 	endPos.x += SQUARE_SIZE;
 	endPos.z += SQUARE_SIZE;
+
 	AddLine(startPos, endPos, 3, 0, 1000);
+
 	startPos.x += SQUARE_SIZE;
 	endPos.x -= SQUARE_SIZE;
 	AddLine(startPos, endPos, 3, 0, 1000);
--- rts/Sim/Projectiles/Unsynced/SmokeTrailProjectile.cpp
+++ rts/Sim/Projectiles/Unsynced/SmokeTrailProjectile.cpp
@@ -75,8 +75,8 @@ CSmokeTrailProjectile::CSmokeTrailProjectile(
 	drawCallbacker(drawCallback),
 	texture(texture)
 {
-	checkCol=false;
-	castShadow=true;
+	checkCol = false;
+	castShadow = true;
 
 	//if no custom texture is defined, use the default texture
 	//Note that this will crash anyway (no idea why) so never have a null texture!
@@ -88,19 +88,20 @@ CSmokeTrailProjectile::CSmokeTrailProjectile(
 		const float dist = pos1.distance(pos2);
 		dirpos1 = pos1 - dir1 * dist * 0.33f;
 		dirpos2 = pos2 + dir2 * dist * 0.33f;
-	} else if(dir1.dot(dir2)<0.98f){
+	} else if (dir1.dot(dir2) < 0.98f) {
 		const float dist = pos1.distance(pos2);
 		dirpos1 = pos1 - dir1 * dist * 0.33f;
 		dirpos2 = pos2 + dir2 * dist * 0.33f;
-		float3 mp=(pos1+pos2)/2;
-		midpos=CalcBeizer(0.5f,pos1,dirpos1,dirpos2,pos2);
-		middir=(dir1+dir2).ANormalize();
-		drawSegmented=true;
+		float3 mp = (pos1 + pos2) / 2;
+		midpos = CalcBeizer(0.5f, pos1, dirpos1, dirpos2, pos2);
+		middir = (dir1 + dir2).ANormalize();
+		drawSegmented = true;
 	}
 	SetRadius(pos1.distance(pos2));
 
-	if(pos.y-ground->GetApproximateHeight(pos.x,pos.z)>10)
-		useAirLos=true;
+	if ((pos.y - ground->GetApproximateHeight(pos.x, pos.z)) > 10) {
+		useAirLos = true;
+	}
 }
 
 CSmokeTrailProjectile::~CSmokeTrailProjectile()
@@ -110,9 +111,9 @@ CSmokeTrailProjectile::~CSmokeTrailProjectile()
 
 void CSmokeTrailProjectile::Draw()
 {
-	inArray=true;
-	float age=gs->frameNum+globalRendering->timeOffset-creationTime;
-	va->EnlargeArrays(8*4,0,VA_SIZE_TC);
+	inArray = true;
+	float age = gs->frameNum + globalRendering->timeOffset - creationTime;
+	va->EnlargeArrays(8*4, 0, VA_SIZE_TC);
 
 	if(drawTrail){
 		float3 dif(pos1-camera->pos2);
@@ -125,73 +126,75 @@ void CSmokeTrailProjectile::Draw()
 		odir2.ANormalize();
 
 		unsigned char col[4];
-		float a1=(1-float(age)/(lifeTime))*255;
-		if(lastSegment)
-			a1=0;
-		a1*=0.7f+fabs(dif.dot(dir1));
-		float alpha=std::min(255.f,std::max(0.f,a1));
-		col[0]=(unsigned char) (color*alpha);
-		col[1]=(unsigned char) (color*alpha);
-		col[2]=(unsigned char) (color*alpha);
-		col[3]=(unsigned char)alpha;
+		float a1=(1 - (float) age / lifeTime) * 255;
+		if (lastSegment) {
+			a1 = 0;
+		}
+		a1 *= 0.7f + fabs(dif.dot(dir1));
+		float alpha = std::min(255.f, std::max(0.f, a1));
+		col[0] = (unsigned char) (color * alpha);
+		col[1] = (unsigned char) (color * alpha);
+		col[2] = (unsigned char) (color * alpha);
+		col[3] = (unsigned char) alpha;
 
 		unsigned char col2[4];
-		float a2=(1-float(age+8)/(lifeTime))*255;
-		if(firstSegment)
-			a2=0;
-		a2*=0.7f+fabs(dif2.dot(dir2));
-		alpha=std::min(255.f,std::max(0.f,a2));
-		col2[0]=(unsigned char) (color*alpha);
-		col2[1]=(unsigned char) (color*alpha);
-		col2[2]=(unsigned char) (color*alpha);
-		col2[3]=(unsigned char)alpha;
+		float a2 = (1 - (float)(age + 8) / lifeTime) * 255;
+		if (firstSegment) {
+			a2 = 0;
+		}
+		a2 *= 0.7f + fabs(dif2.dot(dir2));
+		alpha = std::min(255.f, std::max(0.0f, a2));
+		col2[0] = (unsigned char) (color * alpha);
+		col2[1] = (unsigned char) (color * alpha);
+		col2[2] = (unsigned char) (color * alpha);
+		col2[3] = (unsigned char) alpha;
 
-		float size=1+(age*(1.0f/lifeTime))*orgSize;
-		float size2=1+((age+8)*(1.0f/lifeTime))*orgSize;
+		float size =  1 + ( age      * (1.0f / lifeTime)) * orgSize;
+		float size2 = 1 + ((age + 8) * (1.0f / lifeTime)) * orgSize;
 
-		if(drawSegmented){
+		if (drawSegmented) {
 			float3 dif3(midpos-camera->pos2);
 			dif3.ANormalize();
 			float3 odir3(dif3.cross(middir));
 			odir3.ANormalize();
-			float size3=0.2f+((age+4)*(1.0f/lifeTime))*orgSize;
+			float size3 = 0.2f + ((age + 4) * (1.0f / lifeTime)) * orgSize;
 
 			unsigned char col3[4];
-			float a2=(1-float(age+4)/(lifeTime))*255;
-			a2*=0.7f+fabs(dif3.dot(middir));
-			alpha=std::min(255.f,std::max(0.f,a2));
-			col3[0]=(unsigned char) (color*alpha);
-			col3[1]=(unsigned char) (color*alpha);
-			col3[2]=(unsigned char) (color*alpha);
-			col3[3]=(unsigned char)alpha;
+			float a2 = (1 - (float)(age + 4) / lifeTime) * 255;
+			a2 *= 0.7f + fabs(dif3.dot(middir));
+			alpha = std::min(255.f, std::max(0.f, a2));
+			col3[0] = (unsigned char) (color * alpha);
+			col3[1] = (unsigned char) (color * alpha);
+			col3[2] = (unsigned char) (color * alpha);
+			col3[3] = (unsigned char) alpha;
 
-			float midtexx = texture->xstart + (texture->xend - texture->xstart)*0.5f;
+			const float midtexx = texture->xstart + (texture->xend - texture->xstart)*0.5f;
 
-			va->AddVertexQTC(pos1-odir1*size,texture->xstart,texture->ystart,col);
-			va->AddVertexQTC(pos1+odir1*size,texture->xstart,texture->yend,col);
-			va->AddVertexQTC(midpos+odir3*size3,midtexx,texture->yend,col3);
-			va->AddVertexQTC(midpos-odir3*size3,midtexx,texture->ystart,col3);
+			va->AddVertexQTC(pos1 -   odir1 * size,  texture->xstart, texture->ystart, col);
+			va->AddVertexQTC(pos1 +   odir1 * size,  texture->xstart, texture->yend,   col);
+			va->AddVertexQTC(midpos + odir3 * size3, midtexx,         texture->yend,   col3);
+			va->AddVertexQTC(midpos - odir3 * size3, midtexx,         texture->ystart, col3);
 
-			va->AddVertexQTC(midpos-odir3*size3,midtexx,texture->ystart,col3);
-			va->AddVertexQTC(midpos+odir3*size3,midtexx,texture->yend,col3);
-			va->AddVertexQTC(pos2+odir2*size2,texture->xend,texture->yend,col2);
-			va->AddVertexQTC(pos2-odir2*size2,texture->xend,texture->ystart,col2);
+			va->AddVertexQTC(midpos - odir3 * size3, midtexx,         texture->ystart, col3);
+			va->AddVertexQTC(midpos + odir3 * size3, midtexx,         texture->yend,   col3);
+			va->AddVertexQTC(pos2 + odir2   * size2, texture->xend,   texture->yend,   col2);
+			va->AddVertexQTC(pos2 - odir2   * size2, texture->xend,   texture->ystart, col2);
 		} else {
-			va->AddVertexQTC(pos1-odir1*size,texture->xstart,texture->ystart,col);
-			va->AddVertexQTC(pos1+odir1*size,texture->xstart,texture->yend,col);
-			va->AddVertexQTC(pos2+odir2*size2,texture->xend,texture->yend,col2);
-			va->AddVertexQTC(pos2-odir2*size2,texture->xend,texture->ystart,col2);
+			va->AddVertexQTC(pos1 - odir1 * size,    texture->xstart, texture->ystart, col);
+			va->AddVertexQTC(pos1 + odir1 * size,    texture->xstart, texture->yend,   col);
+			va->AddVertexQTC(pos2 + odir2 * size2,   texture->xend,   texture->yend,   col2);
+			va->AddVertexQTC(pos2 - odir2 * size2,   texture->xend,   texture->ystart, col2);
 		}
 	} else {	//draw as particles
 		unsigned char col[4];
-		for(int a=0;a<8;++a){
-			float a1=1-float(age+a)/lifeTime;
-			float alpha=std::min(255.f,std::max(0.f,a1*255));
-			col[0]=(unsigned char) (color*alpha);
-			col[1]=(unsigned char) (color*alpha);
-			col[2]=(unsigned char) (color*alpha);
-			col[3]=(unsigned char)alpha;
-			float size=((0.2f+(age+a)*(1.0f/lifeTime))*orgSize)*1.2f;
+		for (int a = 0; a < 8; ++a) {
+			float a1 = 1 - (float)(age + a) / lifeTime;
+			float alpha = std::min(255.f, std::max(0.f, a1 * 255));
+			col[0] = (unsigned char) (color * alpha);
+			col[1] = (unsigned char) (color * alpha);
+			col[2] = (unsigned char) (color * alpha);
+			col[3] = (unsigned char) alpha;
+			float size = ((0.2f + (age + a) * (1.0f / lifeTime)) * orgSize) * 1.2f;
 
 			const float3 pos = CalcBeizer(a / 8.0f, pos1, dirpos1, dirpos2, pos2);
 
@@ -205,16 +208,19 @@ void CSmokeTrailProjectile::Draw()
 	}
 #if defined(USE_GML) && GML_ENABLE_SIM
 	CProjectile * callbacker = *(CProjectile * volatile *)&drawCallbacker;
-	if(callbacker)
+	if (callbacker) {
 		callbacker->DrawCallback();
+	}
 #else
-	if(drawCallbacker)
+	if (drawCallbacker) {
 		drawCallbacker->DrawCallback();
+	}
 #endif
 }
 
 void CSmokeTrailProjectile::Update()
 {
-	if(gs->frameNum>=creationTime+lifeTime)
-		deleteMe=true;
+	if (gs->frameNum>=creationTime + lifeTime) {
+		deleteMe = true;
+	}
 }
