30de0ce44e17c0dcf376d1977c449d01956faa33
-jk- <-jk->
2010-08-11 00:38:34 +0200

    always print crash-errors to infolog and simplify ErrorMessageBox()

--- rts/System/Platform/errorhandler.h
+++ rts/System/Platform/errorhandler.h
@@ -10,9 +10,9 @@
 
 #include <string>
 
-#define MBF_OK		1
-#define MBF_INFO	2
-#define MBF_EXCL	4
+#define MBF_OK    1
+#define MBF_INFO  2
+#define MBF_EXCL  4
 
 #if defined __GNUC__ && (__GNUC__ >= 4)
 	#define NO_RETURN __attribute__ ((noreturn))
@@ -32,15 +32,7 @@
  *                 - MB_EXCL : Warning
  *                 - MB_INFO : Info
  */
-void ErrorMessageBox(const char* msg, const char* caption, unsigned int flags) NO_RETURN;
+void ErrorMessageBox(const std::string& msg, const std::string& caption, unsigned int flags = MBF_OK) NO_RETURN;
 
-/**
- * Will pop up an error message window and exit (C++ version).
- * @see ErrorMessageBox(const char* msg, const char* caption, unsigned int flags)
- */
-inline void ErrorMessageBox(const std::string& msg, const std::string& caption, unsigned int flags = MBF_OK)
-{
-	ErrorMessageBox(msg.c_str(), caption.c_str(), flags);
-};
 
 #endif // ERRORHANDLER_H
