635c0d561cc370894613ac028f7a151d900b04a0
Tobi Vollebregt <tobivollebregt@gmail.com>
2010-08-13 14:11:03 +0200

    bump version to 0.82.2

--- rts/Game/GameVersion.cpp
+++ rts/Game/GameVersion.cpp
@@ -18,7 +18,7 @@ namespace SpringVersion
 {
 
 const char* const Major = "0.82";
-const char* const Minor = "1";
+const char* const Minor = "2";
 const char* const Patchset = "0";
 const char* const Additional = "" // Build-Bot will write in here before compiling
 
