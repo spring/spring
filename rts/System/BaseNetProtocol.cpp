f969489a92895982bf3f9b1ff3190612c38c2b9e
Andrea Piras <braindamage@springlobby.info>
2010-08-16 00:39:09 +0200

    fixed linking, function placed wrongly in ifdef

--- rts/System/BaseNetProtocol.cpp
+++ rts/System/BaseNetProtocol.cpp
@@ -393,6 +393,16 @@ PacketType CBaseNetProtocol::SendUnRegisterNetMsg( uchar myPlayerNum, NETMSG msg
 }
 
 
+PacketType CBaseNetProtocol::SendCreateNewPlayer( uchar playerNum, bool spectator, uchar teamNum, std::string playerName )
+{
+	unsigned size = 1 + 1 + 1 + 1 + 2+playerName.size()+1;
+	PackPacket* packet = new PackPacket( size, NETMSG_CREATE_NEWPLAYER);
+	*packet << static_cast<boost::uint16_t>(size) << playerNum << (uchar)spectator << teamNum << playerName;
+	return PacketType(packet);
+
+}
+
+
 #ifdef SYNCDEBUG
 PacketType CBaseNetProtocol::SendSdCheckrequest(int frameNum)
 {
@@ -422,14 +432,6 @@ PacketType CBaseNetProtocol::SendSdBlockrequest(unsigned short begin, unsigned s
 	
 }
 
-PacketType CBaseNetProtocol::SendCreateNewPlayer( uchar playerNum, bool spectator, uchar teamNum, std::string playerName )
-{
-	unsigned size = 1 + 1 + 1 + 1 + 2+playerName.size()+1;
-	PackPacket* packet = new PackPacket( size, NETMSG_CREATE_NEWPLAYER);
-	*packet << static_cast<boost::uint16_t>(size) << playerNum << (uchar)spectator << teamNum << playerName;
-	return PacketType(packet);
-
-}
 
 PacketType CBaseNetProtocol::SendSdBlockresponse(uchar myPlayerNum, std::vector<unsigned> checksums)
 {
