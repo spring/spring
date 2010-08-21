ac3eb410574ae7f7a5585f247542ad3c7e2d52c6
-jk- <-jk->
2010-08-21 08:34:43 +0200

    moved spring's exception catcher from Main.cpp into errorhandler.h [experimental] * so other threads can use it easily, too * also added a new spring custom exception: opengl_error * made the ErrorMessageBox `thread-safe` (it could still need a lot mutexes)  * it exits the current boost::thread and adds a new activeController, which exits the game and then shows the error (in the mainthread!)

--- rts/System/Platform/errorhandler.h
+++ rts/System/Platform/errorhandler.h
@@ -9,6 +9,8 @@
 #define ERRORHANDLER_H
 
 #include <string>
+#include <boost/system/system_error.hpp>
+#include "Exceptions.h"
 
 #define MBF_OK    1
 #define MBF_INFO  2
@@ -32,7 +34,40 @@
  *                 - MB_EXCL : Warning
  *                 - MB_INFO : Info
  */
-void ErrorMessageBox(const std::string& msg, const std::string& caption, unsigned int flags = MBF_OK) NO_RETURN;
+void ErrorMessageBox(const std::string msg, const std::string caption, unsigned int flags = MBF_OK) NO_RETURN;
 
+
+/**
+ * Spring's common error handler.
+ */
+#ifdef NO_CATCH_EXCEPTIONS
+	#define CATCH_SPRING_ERRORS \
+		catch (const content_error& e) {                                                        \
+			ErrorMessageBox(e.what(), "Incorrect/Missing content:", MBF_OK | MBF_EXCL);     \
+		}                                                                                       \
+		catch (const opengl_error& e) {                                                         \
+			ErrorMessageBox(e.what(), "OpenGL content:", MBF_OK | MBF_EXCL);                \
+		}
+#else
+	#define CATCH_SPRING_ERRORS \
+		catch (const content_error& e) {                                                        \
+			ErrorMessageBox(e.what(), "Incorrect/Missing content:", MBF_OK | MBF_EXCL);     \
+		}                                                                                       \
+		catch (const opengl_error& e) {                                                         \
+			ErrorMessageBox(e.what(), "OpenGL content:", MBF_OK | MBF_EXCL);                \
+		}                                                                                       \
+		catch (const boost::system::system_error& e) {                                          \
+			std::stringstream ss;                                                           \
+			ss << e.code().value() << ": " << e.what();                                     \
+			std::string tmp = ss.str();                                                     \
+			ErrorMessageBox(tmp, "Fatal Error", MBF_OK | MBF_EXCL);                         \
+		}                                                                                       \
+		catch (const std::exception& e) {                                                       \
+			ErrorMessageBox(e.what(), "Fatal Error", MBF_OK | MBF_EXCL);                    \
+		}                                                                                       \
+		catch (const char* e) {                                                                 \
+			ErrorMessageBox(e, "Fatal Error", MBF_OK | MBF_EXCL);                           \
+		}
+#endif
 
 #endif // ERRORHANDLER_H
