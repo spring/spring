1517ec64fa400ae2d6bb6c5e1cd3f43bfe7ae688
-jk- <-jk->
2010-08-22 16:49:42 +0200

    New LoadingScreen #2.5 * new offscreen OpenGL context creation helper class * new COffscreenGLThread class

---
+++ rts/System/OffscreenGLContext.h
@@ -0,0 +1,72 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#ifndef _OFFSCREENGLCONTEXT_H
+#define _OFFSCREENGLCONTEXT_H
+
+#ifdef WIN32
+	#include "System/Platform/Win/win32.h"
+	#include <wingdi.h>
+#elif __APPLE__
+	#include <AGL/agl.h>
+#else
+	#include <GL/glx.h>
+
+	#include <X11/Xlib.h>
+	#undef KeyPress
+	#undef KeyRelease
+	#undef GrayScale
+#endif
+
+class COffscreenGLContext
+{
+public:
+	//! Note: the functions are sorted in the way they should be called
+	COffscreenGLContext();
+	void WorkerThreadPost();
+	void WorkerThreadFree(); //! must run in the same thread as the offscreen GL context!
+
+private:
+
+#ifdef WIN32
+	HDC hdc;
+	HGLRC offscreenRC;
+#elif __APPLE__
+	AGLPixelFmtID pxlfmt;
+	AGLContext workerCtx;
+	AGLDrawable drawable;
+#else
+	Display* display;
+	GLXPbuffer pbuf;
+	GLXContext workerCtx;
+#endif
+};
+
+/******************************************************************************/
+
+#include <boost/thread/thread.hpp>
+#include <boost/function.hpp>
+
+
+/**
+ * @brief COffscreenGLThread
+ * Runs a boost::bind in an additional thread with an offscreen OpenGL context.
+ * (Don't try render to the 'screen' a.k.a. default framebuffer in that thread, the results will be undetermistic)
+ */
+class COffscreenGLThread
+{
+public:
+	COffscreenGLThread(boost::function<void()> f);
+	~COffscreenGLThread();
+
+	bool IsFinished(boost::posix_time::time_duration wait = boost::posix_time::milliseconds(200));
+	void Join();
+
+private:
+	void WrapFunc(boost::function<void()> f);
+
+	boost::thread* thread;
+	COffscreenGLContext glOffscreenCtx;
+};
+
+
+#endif // _OFFSCREENGLCONTEXT_H
