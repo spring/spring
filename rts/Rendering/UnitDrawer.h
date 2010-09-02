921ff494b25df547eddc60841c4c5a812e991c82
hoijui <hoijui.quaero@gmail.com>
2010-09-02 20:35:55 +0200

    * fix perma-visible icons (again)
    Conflicts:
    
    	rts/Rendering/UnitDrawer.h

--- rts/Rendering/UnitDrawer.h
+++ rts/Rendering/UnitDrawer.h
@@ -169,7 +169,9 @@ private:
 	inline void DrawUnitModel(CUnit*);
 	void DrawUnitNow(CUnit*);
 	void DrawUnitStats(CUnit*);
-	void UpdateDrawPos(CUnit*);
+
+	void UpdateUnitIconState(CUnit*);
+	void UpdateUnitDrawPos(CUnit*);
 
 	void SetBasicTeamColour(int team, float alpha = 1.0f) const;
 	void SetupBasicS3OTexture0(void) const;
