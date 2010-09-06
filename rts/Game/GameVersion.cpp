a5f7ebe57e852e98e81b6cdbf7d626356bb626cd
hoijui <hoijui.quaero@gmail.com>
2010-09-06 13:40:54 +0200

    bump version to 0.82.5.1

--- rts/Game/GameVersion.cpp
+++ rts/Game/GameVersion.cpp
@@ -17,9 +17,9 @@
 namespace SpringVersion
 {
 
-const char* const Major = "0.82+";
+const char* const Major = "0.82";
 const char* const Minor = "5";
-const char* const Patchset = "0";
+const char* const Patchset = "1";
 const char* const Additional = "" // Build-Bot will write in here before compiling
 
 #if !defined GV_ADD_SPACE
