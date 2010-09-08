32c040732320d5fed2691b0640d44cb4c9427f29
hoijui <hoijui.quaero@gmail.com>
2010-09-08 20:31:43 +0200

    cleanup white-space & includes (no logic changed)

--- rts/Game/GameHelper.h
+++ rts/Game/GameHelper.h
@@ -6,19 +6,19 @@
 #include <list>
 #include <map>
 #include <vector>
-#include "Object.h"
+
+#include "Sim/Misc/DamageArray.h"
+#include "System/float3.h"
+#include "System/MemPool.h"
+
 class CGame;
 class CUnit;
 class CWeapon;
 class CSolidObject;
 class CFeature;
 class CMobileCAI;
-
 struct UnitDef;
-#include "Sim/Misc/DamageArray.h"
-#include "MemPool.h"
-#include "Sim/Units/UnitDef.h"
-
+struct BuildInfo;
 class CExplosionGenerator;
 
 class CGameHelper
@@ -26,10 +26,11 @@ class CGameHelper
 public:
 	CGameHelper();
 	~CGameHelper();
+
 	bool TestAllyCone(const float3& from, const float3& dir, float length, float spread, int allyteam, CUnit* owner);
 	bool TestNeutralCone(const float3& from, const float3& dir, float length, float spread, CUnit* owner);
-	bool TestTrajectoryAllyCone(const float3 &from, const float3& flatdir, float length, float linear, float quadratic, float spread, float baseSize, int allyteam, CUnit* owner);
-	bool TestTrajectoryNeutralCone(const float3 &from, const float3& flatdir, float length, float linear, float quadratic, float spread, float baseSize, CUnit* owner);
+	bool TestTrajectoryAllyCone(const float3& from, const float3& flatdir, float length, float linear, float quadratic, float spread, float baseSize, int allyteam, CUnit* owner);
+	bool TestTrajectoryNeutralCone(const float3& from, const float3& flatdir, float length, float linear, float quadratic, float spread, float baseSize, CUnit* owner);
 
 	void GetEnemyUnits(const float3& pos, float searchRadius, int searchAllyteam, std::vector<int>& found);
 	void GetEnemyUnitsNoLosTest(const float3& pos, float searchRadius, int searchAllyteam, std::vector<int>& found);
@@ -41,28 +42,25 @@ public:
 	CUnit* GetClosestEnemyAircraft(const float3& pos, float searchRadius, int searchAllyteam);
 
 	void GenerateWeaponTargets(const CWeapon* attacker, CUnit* lastTarget, std::multimap<float, CUnit*>& targets);
-	float TraceRay(const float3& start,const float3& dir,float length,float power,const CUnit* owner,const CUnit*& hit,int collisionFlags=0,const CFeature** hitfeature=0);
-	float GuiTraceRay(const float3& start,const float3& dir,float length,const CUnit*& hit,bool useRadar,const CUnit* exclude=NULL);
-	float GuiTraceRayFeature(const float3& start, const float3& dir, float length,const CFeature*& feature);
+	float TraceRay(const float3& start, const float3& dir, float length, float power, const CUnit* owner, const CUnit*& hit, int collisionFlags = 0, const CFeature** hitFeature = NULL);
+	float GuiTraceRay(const float3& start, const float3& dir, float length, const CUnit*& hit, bool useRadar, const CUnit* exclude = NULL);
+	float GuiTraceRayFeature(const float3& start, const float3& dir, float length, const CFeature*& feature);
 
-	void DoExplosionDamage(CUnit*, const float3&, float, float, bool, CUnit*, float, const DamageArray&, int);
-	void DoExplosionDamage(CFeature*, const float3&, float, CUnit*, const DamageArray&);
-	void Explosion(float3 pos, const DamageArray& damages,float radius, float edgeEffectiveness, float explosionSpeed,
-		       CUnit* owner,bool damageGround,float gfxMod,bool ignoreOwner,bool impactOnly,
-		       CExplosionGenerator *explosionGraphics,CUnit *hit, const float3 &impactDir, int weaponId,
-		       CFeature* hitfeature=0);
+	void DoExplosionDamage(CUnit* unit, const float3& expPos, float expRad, float expSpeed, bool ignoreOwner, CUnit* owner, float edgeEffectiveness, const DamageArray& damages, int weaponId);
+	void DoExplosionDamage(CFeature* feature, const float3& expPos, float expRad, CUnit* owner, const DamageArray& damages);
+	void Explosion(float3 pos, const DamageArray& damages, float radius, float edgeEffectiveness, float explosionSpeed, CUnit* owner, bool damageGround, float gfxMod, bool ignoreOwner, bool impactOnly, CExplosionGenerator* explosionGraphics, CUnit* hit, const float3& impactDir, int weaponId, CFeature* hitfeature = NULL);
 
-	float TraceRayTeam(const float3& start,const float3& dir,float length, CUnit*& hit,bool useRadar,CUnit* exclude,int allyteam);
-	void BuggerOff(float3 pos, float radius, bool spherical = true, CUnit* exclude = 0);
+	float TraceRayTeam(const float3& start, const float3& dir, float length, CUnit*& hit, bool useRadar, CUnit* exclude, int allyteam);
+	void BuggerOff(float3 pos, float radius, bool spherical = true, CUnit* exclude = NULL);
 	float3 Pos2BuildPos(const BuildInfo& buildInfo);
 	float3 Pos2BuildPos(const float3& pos, const UnitDef* ud);
 	/**
 	 * @param minDist messured in 1/(SQUARE_SIZE * 2) = 1/16 of full map resolution.
 	 */
 	float3 ClosestBuildSite(int team, const UnitDef* unitDef, float3 pos, float searchRadius, int minDist, int facing = 0);
-	void Update(void);
+	void Update();
 
-	bool LineFeatureCol(const float3& start, const float3& dir,float length);
+	bool LineFeatureCol(const float3& start, const float3& dir, float length);
 
 	//get the position of a unit + eventuall error due to lack of los
 	float3 GetUnitErrorPos(const CUnit* unit, int allyteam);
@@ -72,10 +70,14 @@ protected:
 
 	struct WaitingDamage{
 #if !defined(SYNCIFY) && !defined(USE_MMGR)
-		inline void* operator new(size_t size){return mempool.Alloc(size);};
-		inline void operator delete(void* p,size_t size){mempool.Free(p,size);};
+		inline void* operator new(size_t size) {
+			return mempool.Alloc(size);
+		};
+		inline void operator delete(void* p, size_t size) {
+			mempool.Free(p, size);
+		};
 #endif
-		WaitingDamage(int attacker,int target,const DamageArray& damage,const float3& impulse, const int weaponId)
+		WaitingDamage(int attacker, int target, const DamageArray& damage, const float3& impulse, const int weaponId)
 			:	target(target),
 				attacker(attacker),
 				weaponId(weaponId),
@@ -90,7 +92,12 @@ protected:
 		float3 impulse;
 	};
 
-	std::list<WaitingDamage*> waitingDamages[128];		//probably a symptom of some other problems but im getting paranoid about putting whole classes into high trafic stl containers instead of pointers to them
+	/**
+	 * probably a symptom of some other problems,
+	 * but im getting paranoid about putting whole classes
+	 * into high trafic STL containers instead of pointers to them
+	 */
+	std::list<WaitingDamage*> waitingDamages[128];
 
 private:
 	bool TestConeHelper(const float3& from, const float3& dir, float length, float spread, const CUnit* u);
