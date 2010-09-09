f317f460299109573977296c4ba2f9192dc54cc1
zerver <mr@zerver.net>
2010-09-09 18:17:00 +0200

    Make main a real boost thread to enable interruption

--- rts/System/Platform/Threading.cpp
+++ rts/System/Platform/Threading.cpp
@@ -4,13 +4,27 @@
 
 namespace Threading {
 	static bool haveMainThreadID = false;
+	static boost::thread *mainThread = NULL;
 	static boost::thread::id mainThreadID;
+	static std::runtime_error *threadError = NULL;
 
-	void SetMainThreadID() {
+	void SetMainThread(boost::thread *mt) {
 		if (!haveMainThreadID) {
 			haveMainThreadID = true;
+			mainThread = mt;
 			mainThreadID = boost::this_thread::get_id();
 		}
+	}
+
+	boost::thread *GetMainThread() {
+		return mainThread;
+	}
+
+	void SetThreadError(const std::string &s) {
+		threadError = new std::runtime_error(s);
+	}
+	std::runtime_error GetThreadError() {
+		return threadError ? *threadError : std::runtime_error("Unknown error");
 	}
 
 	bool IsMainThread() {
