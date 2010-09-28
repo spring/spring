6dc9736d01f4c3dabe10667313ce5bb8122e3ff4
sirmaverick <sirmaverick>
2010-09-28 16:20:50 +0200

    rts/System/EventClient.cpp: typo 2

--- rts/System/EventClient.cpp
+++ rts/System/EventClient.cpp
@@ -30,7 +30,7 @@ void CEventClient::Load(CArchiveBase* archive) {}
 void CEventClient::GamePreload() {}
 void CEventClient::GameStart() {}
 void CEventClient::GameOver(std::vector<unsigned char> winningAllyTeams) {}
-void CEventClient::GamePaused(int playerID, bool pausedD) {}
+void CEventClient::GamePaused(int playerID, bool paused) {}
 void CEventClient::TeamDied(int teamID) {}
 void CEventClient::TeamChanged(int teamID) {}
 void CEventClient::PlayerChanged(int playerID) {}
