ba003a1ec51912521da88c88a15c67a311587faa
hoijui <hoijui.quaero@gmail.com>
2010-10-06 20:24:31 +0200

    add TimeUtil [AI] Allows to get a high precision time in milli-seconds, platform independently, with `timeUtil_getCurrentTimeMillis()`. Also emulated the following POSIX function on windows: `gettimeofday(struct timeval* tv, struct timezone* tz)`

---
+++ AI/Wrappers/CUtils/TimeUtil.h
@@ -0,0 +1,38 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#ifndef TIME_UTIL_H
+#define TIME_UTIL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+#if       defined WIN32
+struct timezone {
+	int  tz_minuteswest; ///< minutes W of Greenwich
+	int  tz_dsttime;     ///< type of dst correction
+};
+
+/// @link http://linux.die.net/man/2/gettimeofday
+int gettimeofday(struct timeval* tv, struct timezone* tz);
+#else  // defined WIN32
+// gettimeofday() is POSIX standard -> already declared
+#endif // defined WIN32
+
+
+/**
+ * Returns the current time in milli-seconds.
+ * On POSIX, this uses gettimeofday(),
+ * on Windows, GetSystemTimeAsFileTime(),
+ * which is what Boost uses when HIGH_PRECISION_CLOCK is defined.
+ * This function does not depend on any libraries (like Boost or SDL).
+ */
+unsigned long timeUtil_getCurrentTimeMillis();
+
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // TIME_UTIL_H
