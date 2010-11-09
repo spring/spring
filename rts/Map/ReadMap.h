93ea0cbe282930c63df722327a0a80af1d63d5be
hoijui <hoijui.quaero@gmail.com>
2010-11-09 14:59:50 +0100

    cleanup * get rid of some warnings (init order) * keep comments <= 80 chars * initialize `CTeam.teamNum` to `-1`

--- rts/Map/ReadMap.h
+++ rts/Map/ReadMap.h
@@ -1,14 +1,14 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#ifndef READMAP_H
-#define READMAP_H
+#ifndef READ_MAP_H
+#define READ_MAP_H
 
-#ifdef BITMAP_NO_OPENGL
+#ifdef     BITMAP_NO_OPENGL
 // FIXME: this is hacky. this class should not depend on OpenGL stuff
 typedef unsigned int GLuint;
-#else
+#else  // !BITMAP_NO_OPENGL
 #include "Rendering/GL/myGL.h"
-#endif
+#endif //  BITMAP_NO_OPENGL
 #include "creg/creg_cond.h"
 #include "float3.h"
 #include "Sim/Misc/GlobalConstants.h"
@@ -27,7 +27,8 @@ class CBaseGroundDrawer;
 struct MapFeatureInfo
 {
 	float3 pos;
-	int featureType;	///< index to one of the strings above
+	/// index to one of the strings above
+	int featureType;
 	float rotation;
 };
 
@@ -42,7 +43,8 @@ struct MapBitmapInfo
 };
 
 struct HeightmapUpdate {
-	HeightmapUpdate(int x, int xx, int y, int yy) : x1(x), x2(xx), y1(y), y2(yy) {}
+	HeightmapUpdate(int x, int xx, int y, int yy)
+			: x1(x), x2(xx), y1(y), y2(yy) {}
 
 	int x1, x2;
 	int y1, y2;
@@ -52,48 +54,77 @@ class CReadMap
 {
 public:
 	CR_DECLARE(CReadMap);
-	void Serialize(creg::ISerializer& s); //! creg serialize callback
+	/// creg serialize callback
+	void Serialize(creg::ISerializer& s);
 
 	virtual ~CReadMap();
 	static CReadMap* LoadMap(const std::string& mapname);
 
 protected:
 	CReadMap();
-	void Initialize(); //! called by implementations of CReadMap
+	/// called by implementations of CReadMap
+	void Initialize();
 
 	std::vector<HeightmapUpdate> heightmapUpdates;
 	virtual void UpdateHeightmapUnsynced(int x1, int y1, int x2, int y2) = 0;
 
-	//! calculates derived heightmap information such as normals, centerheightmap and slopemap
+	/**
+	 * calculates derived heightmap information
+	 * such as normals, centerheightmap and slopemap
+	 */
 	void UpdateHeightmapSynced(int x1, int y1, int x2, int y2);
 	void CalcHeightmapChecksum();
 public:
-	virtual const float* GetHeightmap() const = 0; //! returns a float[(mapx+1)*(mapy+1)]
+	/// Returns a float[(mapx + 1) * (mapy + 1)]
+	virtual const float* GetHeightmap() const = 0;
 
 	virtual void NewGroundDrawer() = 0;
 	virtual CBaseGroundDrawer* GetGroundDrawer() { return 0; }
 
 	virtual GLuint GetGrassShadingTexture() const { return 0; }
-	virtual GLuint GetShadingTexture() const = 0; //! a texture with RGB for shading and A for height (0 := above water; 1-255 := under water = 255+height*10)
+	/**
+	 * a texture with RGB for shading and A for height
+	 * (0 := above water; 1-255 := under water = 255+height*10)
+	 */
+	virtual GLuint GetShadingTexture() const = 0;
 
-	//! if you modify the heightmap, call HeightmapUpdated
+	/// if you modify the heightmap, call HeightmapUpdated
 	virtual void SetHeight(const int& idx, const float& h) = 0;
 	virtual void AddHeight(const int& idx, const float& a) = 0;
 	void HeightmapUpdated(const int& x1, const int& y1, const int& x2, const int& y2);
 
-	float* orgheightmap;                   //! size: (mapx+1)*(mapy+1) (per vertex)
-	float* centerheightmap;                //! size: (mapx)*(mapy)     (per face)
-	static const int numHeightMipMaps = 7; //! number of heightmap mipmaps, including full resolution
-	float* mipHeightmap[numHeightMipMaps]; //! array of pointers to heightmap in different resolutions, mipHeightmap[0] is full resolution, mipHeightmap[n+1] is half resolution of mipHeightmap[n]
-	float* slopemap;                       //! size: (mapx/2)*(mapy/2) (1.0 - interpolate(centernomal[i]).y)
-	float3* facenormals;                   //! size: 2*mapx*mapy (contains 2 normals per quad -> triangle strip)
-	float3* centernormals;                 //! size: mapx*mapy (contains interpolated 1 normal per quad, same as (facenormal0+facenormal1).Normalize())
+	/// size: (mapx+1)*(mapy+1) (per vertex)
+	float* orgheightmap;
+	/// size: (mapx)*(mapy)     (per face)
+	float* centerheightmap;
+	/// number of heightmap mipmaps, including full resolution
+	static const int numHeightMipMaps = 7;
+	/**
+	 * array of pointers to heightmap in different resolutions,
+	 * mipHeightmap[0] is full resolution,
+	 * mipHeightmap[n+1] is half resolution of mipHeightmap[n]
+	 */
+	float* mipHeightmap[numHeightMipMaps];
+	/// size: (mapx/2)*(mapy/2) (1.0 - interpolate(centernomal[i]).y)
+	float* slopemap;
+	/// size: 2*mapx*mapy (contains 2 normals per quad -> triangle strip)
+	float3* facenormals;
+	/**
+	 * size: mapx*mapy (contains interpolated 1 normal per quad, same as
+	 * (facenormal0+facenormal1).Normalize())
+	 */
+	float3* centernormals;
 
-	std::vector<float3> vertexNormals;     //! size: (mapx + 1) * (mapy + 1), contains one vertex normal per heightmap pixel
+	/**
+	 * size: (mapx + 1) * (mapy + 1),
+	 * contains one vertex normal per heightmap pixel
+	 */
+	std::vector<float3> vertexNormals;
 
 	unsigned char* typemap;
 
-	CMetalMap *metalMap;   //! Metal-density/height-map
+	/// Metal-density/height-map
+	CMetalMap* metalMap;
 
 	int width, height;
 	float minheight, maxheight;
@@ -103,40 +134,47 @@ public:
 
 public:
 	void UpdateDraw();
-	virtual void Update() {};
-	virtual void Explosion(float x,float y,float strength) {};
+	virtual void Update() {}
+	virtual void Explosion(float x, float y, float strength) {}
 
-	static inline unsigned char EncodeHeight(const float h) { return std::max(0, 255+(int)(10.0f*h)); }
+	static inline unsigned char EncodeHeight(const float h) {
+		return std::max(0, 255 + (int)(10.0f * h));
+	}
 
-	virtual void DrawMinimap() const = 0; //! draw the minimap in a quad (with extends: (0,0)-(1,1))
+	/// Draws the minimap in a quad (with extends: (0,0)-(1,1))
+	virtual void DrawMinimap() const = 0;
 
-	//! Feature creation
+	/// Feature creation
 	virtual int GetNumFeatures() = 0;
 	virtual int GetNumFeatureTypes() = 0;
-	virtual void GetFeatureInfo(MapFeatureInfo* f) = 0; //! returns MapFeatureInfo[GetNumFeatures()]
-	virtual const char *GetFeatureTypeName(int typeID) = 0;
+	/// Returns MapFeatureInfo[GetNumFeatures()]
+	virtual void GetFeatureInfo(MapFeatureInfo* f) = 0;
+	virtual const char* GetFeatureTypeName(int typeID) = 0;
 
-	//! Infomaps (such as metal map, grass map, ...), handling them with a string as type seems flexible...
-	//! Some map types:
-	//!   "metal"  -  metalmap
-	//!   "grass"  -  grassmap
+	/**
+	 * Infomaps (such as metal map, grass map, ...),
+	 * handling them with a string as type seems flexible...
+	 * Some map types:
+	 *   "metal"  -  metalmap
+	 *   "grass"  -  grassmap
+	 */
 	virtual unsigned char* GetInfoMap(const std::string& name, MapBitmapInfo* bm) = 0;
-	virtual void FreeInfoMap(const std::string& name, unsigned char *data) = 0;
+	virtual void FreeInfoMap(const std::string& name, unsigned char* data) = 0;
 
-	//! Determine visibility for a rectangular grid
+	/// Determine visibility for a rectangular grid
 	struct IQuadDrawer
 	{
 		virtual ~IQuadDrawer();
 		virtual void DrawQuad (int x,int y) = 0;
 	};
-	virtual void GridVisibility(CCamera *cam, int quadSize, float maxdist, IQuadDrawer *cb, int extraSize=0) = 0;
+	virtual void GridVisibility(CCamera* cam, int quadSize, float maxdist, IQuadDrawer* cb, int extraSize = 0) = 0;
 };
 
 extern CReadMap* readmap;
 
-//! Converts a map-square into a float3-position.
+/// Converts a map-square into a float3-position.
 inline float3 SquareToFloat3(int xSquare, int zSquare) {
-	return float3(((xSquare))*SQUARE_SIZE, readmap->centerheightmap[(xSquare) + (zSquare) * gs->mapx], ((zSquare))*SQUARE_SIZE);
-};
+	return float3(xSquare * SQUARE_SIZE, readmap->centerheightmap[(zSquare * gs->mapx) + xSquare], zSquare * SQUARE_SIZE);
+}
 
-#endif /* READMAP_H */
+#endif /* READ_MAP_H */
