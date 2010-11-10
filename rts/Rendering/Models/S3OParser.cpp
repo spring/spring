6623cbae4463be78c7aba00995fb9b675178ddf7
kloot <kloot@kloot0.(none)>
2010-11-11 00:07:58 +0100

    -

--- rts/Rendering/Models/S3OParser.cpp
+++ rts/Rendering/Models/S3OParser.cpp
@@ -1,7 +1,6 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "StdAfx.h"
-#include <cassert>
 #include <cctype>
 #include <stdexcept>
 #include "mmgr.h"
