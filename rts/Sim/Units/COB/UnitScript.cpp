spring/spring

commit e1ec8a17efb3d2971c2fd8ae8b393740335e3abe

Tweak previous commit

rts/Sim/Units/COB/UnitScript.cpp
@@ -1406,6 +1406,8 @@ void CUnitScript::SetUnitVal(int val, int param)
 		case INBUILDSTANCE: {
 			//logOutput.Print("buildstance %d", param);
 			unit->inBuildStance = (param != 0);
+			if(unit->inBuildStance)
+				unit->commandAI->SlowUpdate(); // start building immediately once build stance reached
 			break;
 		}
 		case BUSY: {

rts/Sim/Units/CommandAI/BuilderCAI.cpp
@@ -430,14 +430,15 @@ void CBuilderCAI::SlowUpdate()
 							buildRetries++;
 							owner->moveType->KeepPointingTo(build.pos, builder->buildDistance * 0.7f + radius, false);
 
-							if (builder->StartBuild(build, f) || (buildRetries > 20)) {
+							bool waitstance = false;
+							if (builder->StartBuild(build, f, waitstance) || (buildRetries > 20)) {
 								building = true;
 							}
 							else if (f) {
 								inCommand = false;
 								ReclaimFeature(f);
 							}
-							else {
+							else if (!waitstance) {
 								const float fpSqRadius = (ud->xsize * ud->xsize + ud->zsize * ud->zsize);
 								const float fpRadius = (math::sqrt(fpSqRadius) * 0.5f) * SQUARE_SIZE;
 

rts/Sim/Units/UnitTypes/Builder.cpp
@@ -527,7 +527,7 @@ void CBuilder::StopBuild(bool callScript)
 }
 
 
-bool CBuilder::StartBuild(BuildInfo& buildInfo, CFeature*& feature)
+bool CBuilder::StartBuild(BuildInfo& buildInfo, CFeature*& feature, bool& waitstance)
 {
 	StopBuild(false);
 
@@ -572,8 +572,10 @@ bool CBuilder::StartBuild(BuildInfo& buildInfo, CFeature*& feature)
 	const UnitDef* unitDef = buildInfo.def;
 	SetBuildStanceToward(buildInfo.pos);
 
-	if (!inBuildStance)
+	if (!inBuildStance) {
+		waitstance = true;
 		return false;
+	}
 
 	CUnit* b = unitLoader->LoadUnit(buildInfo.def, buildInfo.pos, team,
 	                               true, buildInfo.buildFacing, this);

rts/Sim/Units/UnitTypes/Builder.h
@@ -44,7 +44,7 @@ public:
 	void DependentDied(CObject* o);
 	virtual void DeleteDeathDependence(CObject* o, DependenceType dep);
 
-	bool StartBuild(BuildInfo& buildInfo, CFeature*& feature);
+	bool StartBuild(BuildInfo& buildInfo, CFeature*& feature, bool& waitstance);
 	float CalculateBuildTerraformCost(BuildInfo& buildInfo);
 	void StopBuild(bool callScript = true);
 	void SetRepairTarget(CUnit* target);

commit abbbd1b2223b352b40df978f8a7ccc2b941b0eac

fix infinite recursion (#2239) caused by e1ec8a17efb3d2971c2fd8ae8b393740335e3abe
you CANNOT call a CommandAI SlowUpdate member from any random place in the code!

rts/Sim/Units/COB/UnitScript.cpp
@@ -1404,10 +1404,7 @@ void CUnitScript::SetUnitVal(int val, int param)
 			break;
 		}
 		case INBUILDSTANCE: {
-			//logOutput.Print("buildstance %d", param);
 			unit->inBuildStance = (param != 0);
-			if(unit->inBuildStance)
-				unit->commandAI->SlowUpdate(); // start building immediately once build stance reached
 			break;
 		}
 		case BUSY: {