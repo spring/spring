bcbd731bc519481ccb8ac3a8948c380ec4365886
-jk- <-jk->
2010-11-29 16:49:24 +0100

    format

--- rts/System/float3.cpp
+++ rts/System/float3.cpp
@@ -14,12 +14,13 @@ CR_REG_METADATA(float2, (CR_MEMBER(x), CR_MEMBER(y)));
 CR_BIND(float3, );
 CR_REG_METADATA(float3, (CR_MEMBER(x), CR_MEMBER(y), CR_MEMBER(z)));
 
-float float3::maxxpos = 2048.0f; /**< Maximum x position is 2048 */
-float float3::maxzpos = 2048.0f; /**< Maximum z position is 2048 */
+//! gets initialized later when the map is loaded
+float float3::maxxpos = 2048.0f;
+float float3::maxzpos = 2048.0f;
 
 #ifdef _MSC_VER
-const float float3::CMP_EPS = 1e-4f;
-const float float3::NORMALIZE_EPS = 1e-12f;
+	const float float3::CMP_EPS = 1e-4f;
+	const float float3::NORMALIZE_EPS = 1e-12f;
 #endif
 
 bool float3::IsInBounds() const
