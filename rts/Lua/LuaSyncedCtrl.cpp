8be9b218ab9a5521ef8358e00c13865abda13d85
kloot <kloot@kloot0.(none)>
2010-12-23 22:02:21 +0100

    do not send collision events to unsynced clients

--- rts/Lua/LuaSyncedCtrl.cpp
+++ rts/Lua/LuaSyncedCtrl.cpp
@@ -1364,13 +1364,14 @@ int LuaSyncedCtrl::SetUnitArmored(lua_State* L)
 	if (lua_isboolean(L, 2)) {
 		unit->armoredState = lua_toboolean(L, 2);
 	}
-	const float armoredMultiple = luaL_optfloat(L, 3, unit->armoredMultiple);
-	unit->armoredMultiple = max(0.0001f, armoredMultiple); //armored multiple of 0 will crash spring
+
+	// armored multiple of 0 will crash spring
+	unit->armoredMultiple = std::max(0.0001f, luaL_optfloat(L, 3, unit->armoredMultiple));
+
 	if (lua_toboolean(L, 2)) {
 		unit->curArmorMultiple = unit->armoredMultiple;
-	}
-	else {
-		unit->curArmorMultiple = 1;
+	} else {
+		unit->curArmorMultiple = 1.0f;
 	}
 	return 0;
 }
