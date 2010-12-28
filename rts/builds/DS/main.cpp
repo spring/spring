172334e22fe5908f771f72235a2c5ab388fcba19
hoijui <hoijui.quaero@gmail.com>
2010-12-28 16:25:01 +0100

    dedicated-server compile error [fix]

--- rts/builds/DS/main.cpp
+++ rts/builds/DS/main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char *argv[])
 		}
 
 		data.SetSetup(gameSetup->gameSetupText);
-		server = new CGameServer(settings.hostport, &data, gameSetup);
+		server = new CGameServer(settings.hostIP, settings.hostPort, &data, gameSetup);
 
 		while (!server->HasFinished()) // check if still running
 #ifdef _WIN32
