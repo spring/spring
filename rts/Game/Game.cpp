e1dc199ad4e69323ef2bfad67db8dfaf434be432
kloot <kloot@kloot0.(none)>
2011-01-03 15:43:30 +0100

    MT fix #2

--- rts/Game/Game.cpp
+++ rts/Game/Game.cpp
@@ -1048,6 +1048,8 @@ bool CGame::DrawWorld()
 
 	if (globalRendering->drawWater && !mapInfo->map.voidWater) {
 		SCOPED_TIMER("Water");
+		GML_STDMUTEX_LOCK(water);
+
 		water->OcclusionQuery();
 		if (water->drawSolid) {
 			water->UpdateWater(this);
@@ -1084,6 +1086,8 @@ bool CGame::DrawWorld()
 	//! draw water
 	if (globalRendering->drawWater && !mapInfo->map.voidWater) {
 		SCOPED_TIMER("Water");
+		GML_STDMUTEX_LOCK(water);
+
 		if (!water->drawSolid) {
 			water->UpdateWater(this);
 			water->Draw();
