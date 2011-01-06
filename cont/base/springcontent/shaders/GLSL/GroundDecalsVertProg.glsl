74ff12d34f764fc129423be3771c397d7cc86720
kloot <kloot@kloot0.(none)>
2011-01-06 01:51:35 +0100

    fix decal alpha-fading

--- cont/base/springcontent/shaders/GLSL/GroundDecalsVertProg.glsl
+++ cont/base/springcontent/shaders/GLSL/GroundDecalsVertProg.glsl
@@ -5,6 +5,7 @@ void main() {
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_TexCoord[0].st = gl_MultiTexCoord0.st;
 	gl_TexCoord[1].st = gl_Vertex.xz * mapSizePO2;
+	gl_FrontColor = gl_Color;
 	gl_FogFragCoord = dot(gl_ModelViewProjectionMatrix[2].xyz, gl_Vertex.xyz);
 
 	vertexPos = gl_Vertex;
