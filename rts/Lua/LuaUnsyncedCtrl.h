a71af179c58a21c88e109bee488bdc6e4edf4097
kloot <kloot@kloot0.(none)>
2011-01-14 22:25:12 +0100

    rename TrackPosition ==> TrackingState

--- rts/Lua/LuaUnsyncedCtrl.h
+++ rts/Lua/LuaUnsyncedCtrl.h
@@ -65,8 +65,8 @@ class LuaUnsyncedCtrl {
 		static int AddModelLight(lua_State* L);
 		static int UpdateMapLight(lua_State* L);
 		static int UpdateModelLight(lua_State* L);
-		static int SetMapLightTrackPosition(lua_State* L);
-		static int SetModelLightTrackPosition(lua_State* L);
+		static int SetMapLightTrackingState(lua_State* L);
+		static int SetModelLightTrackingState(lua_State* L);
 
 		static int SetUnitNoDraw(lua_State* L);
 		static int SetUnitNoMinimap(lua_State* L);
