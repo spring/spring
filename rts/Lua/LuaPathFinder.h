c042829405e17a5f8fd0eb52c3dba9bab939fb69
kloot <kloot@kloot0.(none)>
2011-02-10 18:45:15 +0100

    LuaPF: add array versions of {Get,Set}PathNodeCost

--- rts/Lua/LuaPathFinder.h
+++ rts/Lua/LuaPathFinder.h
@@ -3,19 +3,20 @@
 #ifndef LUA_PATH_FINDER_H
 #define LUA_PATH_FINDER_H
 
-#include <vector>
-
-
 struct lua_State;
 
 class LuaPathFinder {
-	public:
-		static bool PushEntries(lua_State* L);
+public:
+	static bool PushEntries(lua_State* L);
 
-	private:
-		static int RequestPath(lua_State* L);
-		static int SetPathNodeCost(lua_State* L);
-		static int GetPathNodeCost(lua_State* L);
+private:
+	static int RequestPath(lua_State* L);
+	static int InitPathNodeCostsArray(lua_State* L);
+	static int FreePathNodeCostsArray(lua_State* L);
+	static int SetPathNodeCosts(lua_State* L);
+	static int GetPathNodeCosts(lua_State* L);
+	static int SetPathNodeCost(lua_State* L);
+	static int GetPathNodeCost(lua_State* L);
 };
 
 
