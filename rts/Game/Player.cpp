spring/spring

commit 581c9e50591249a91fba56be35e7dd0b2b365946

if a unit can dgun, only its command-fire weapons will be fired upon dgun command (patch by aegis) [fix] Before, all weapons of a unit were fired upon a dgun attack command. Aegis tested this patch with all the mods he had, and it seems to work fine.

rts/Sim/Units/Unit.cpp
@@ -836,10 +836,16 @@ void CUnit::SlowUpdate()
 		if(!dontFire){
 			for(vector<CWeapon*>::iterator wi=weapons.begin();wi!=weapons.end();++wi){
 				CWeapon* w=*wi;
-				if(userTarget && !w->haveUserTarget && (haveDGunRequest || !unitDef->canDGun || !w->weaponDef->manualfire))
-					w->AttackUnit(userTarget,true);
-				else if(userAttackGround && !w->haveUserTarget && (haveDGunRequest || !unitDef->canDGun || !w->weaponDef->manualfire))
-					w->AttackGround(userAttackPos,true);
+				
+				if (!w->haveUserTarget) {
+					if (haveDGunRequest == (unitDef->canDGun && w->weaponDef->manualfire)) { // == ((!haveDGunRequest && !isDGun) || (haveDGunRequest && isDGun))
+						if (userTarget) {
+							w->AttackUnit(userTarget, true);
+						} else if (userAttackGround)
+							w->AttackGround(userAttackPos, true);
+						}
+					}
+				}
 
 				w->SlowUpdate();
 
@@ -1395,7 +1401,7 @@ bool CUnit::AttackUnit(CUnit *unit,bool dgun)
 	for (wi = weapons.begin(); wi != weapons.end(); ++wi) {
 		(*wi)->haveUserTarget = false;
 		(*wi)->targetType = Target_None;
-		if (dgun || !unitDef->canDGun || !(*wi)->weaponDef->manualfire)
+		if (haveDGunRequest == (unitDef->canDGun && (*wi)->weaponDef->manualfire)) // == ((!haveDGunRequest && !isDGun) || (haveDGunRequest && isDGun))
 			if ((*wi)->AttackUnit(unit, true))
 				r = true;
 	}
@@ -1415,7 +1421,7 @@ bool CUnit::AttackGround(const float3 &pos, bool dgun)
 	std::vector<CWeapon*>::iterator wi;
 	for (wi = weapons.begin(); wi != weapons.end(); ++wi) {
 		(*wi)->haveUserTarget = false;
-		if (dgun || !unitDef->canDGun || !(*wi)->weaponDef->manualfire)
+		if (haveDGunRequest == (unitDef->canDGun && (*wi)->weaponDef->manualfire)) // == ((!haveDGunRequest && !isDGun) || (haveDGunRequest && isDGun))
 			if ((*wi)->AttackGround(pos, true))
 				r = true;
 	}

commit cd6930f8f5f4681ae28f055a460c506c565ddc62

fix crash when exiting while in FPS mode fix units unable to fire non-dgun weapons while in FPS mode (caused by 581c9e50591249a91fba56be35e7dd0b2b365946)

rts/Game/Player.cpp
@@ -16,7 +16,6 @@
 #include "Sim/Misc/TeamHandler.h"
 #include "Sim/Misc/GlobalSynced.h"
 #include "Sim/Units/Unit.h"
-#include "Sim/Units/UnitHandler.h"
 #include "Sim/Weapons/Weapon.h"
 #include "Sim/Units/Scripts/CobInstance.h"
 #include "System/myMath.h"
@@ -147,40 +146,40 @@ void CPlayer::GameFrame(int frameNum)
 
 	const int piece = unit->script->AimFromWeapon(0);
 	const float3 relPos = unit->script->GetPiecePos(piece);
-	float3 pos = unit->pos +
+	const float3 pos = unit->pos +
 		unit->frontdir * relPos.z +
 		unit->updir    * relPos.y +
-		unit->rightdir * relPos.x;
-	pos += UpVector * 7;
+		unit->rightdir * relPos.x +
+		UpVector       * 7.0f;
 
 	dccs.oldDCpos = pos;
 
-	CUnit* hit;
+	CUnit* hit = NULL;
 	float dist = helper->TraceRayTeam(pos, dc->viewDir, unit->maxRange, hit, 1, unit, teamHandler->AllyTeam(team));
-	dc->target = hit;
 
 	if (hit) {
+		dc->target = hit;
 		dc->targetDist = dist;
 		dc->targetPos = hit->pos;
 		if (!dc->mouse2) {
-			unit->AttackUnit(hit, true);
+			unit->AttackUnit(hit, true, true);
 		}
 	} else {
 		if (dist > unit->maxRange * 0.95f) {
 			dist = unit->maxRange * 0.95f;
 		}
 
+		dc->target = NULL;
 		dc->targetDist = dist;
 		dc->targetPos = pos + dc->viewDir * dc->targetDist;
 
 		if (!dc->mouse2) {
-			unit->AttackGround(dc->targetPos, true);
+			unit->AttackGround(dc->targetPos, true, true);
+
 			for (std::vector<CWeapon*>::iterator wi = unit->weapons.begin(); wi != unit->weapons.end(); ++wi) {
-				float d = dc->targetDist;
-				if (d > (*wi)->range * 0.95f) {
-					d = (*wi)->range * 0.95f;
-				}
+				const float d = std::min(dc->targetDist, (*wi)->range * 0.95f);
 				const float3 p = pos + dc->viewDir * d;
+
 				(*wi)->AttackGround(p, true);
 			}
 		}
@@ -189,13 +188,14 @@ void CPlayer::GameFrame(int frameNum)
 
 void CPlayer::StopControllingUnit()
 {
-	if (!dccs.playerControlledUnit) {
+	if (dccs.playerControlledUnit == NULL || mouse == NULL) {
 		return;
 	}
 
 	CUnit* unit = dccs.playerControlledUnit;
 	unit->directControl = NULL;
-	unit->AttackUnit(NULL, true);
+	unit->AttackUnit(NULL, true, true);
+
 	if (gu->directControl == dccs.playerControlledUnit) {
 		assert(playerHandler->Player(gu->myPlayerNum) == this);
 		gu->directControl = NULL;

rts/Sim/Units/Unit.cpp
@@ -72,8 +72,6 @@
 
 CLogSubsystem LOG_UNIT("unit");
 
-CR_BIND_DERIVED(CUnit, CSolidObject, );
-
 // See end of source for member bindings
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
@@ -90,6 +88,7 @@ float CUnit::expGrade       = 0.0f;
 
 CUnit::CUnit() : CSolidObject(),
 	unitDef(NULL),
+	unitDefID(-1),
 	frontdir(0.0f, 0.0f, 1.0f),
 	rightdir(-1.0f, 0.0f, 0.0f),
 	updir(0.0f, 1.0f, 0.0f),
@@ -295,8 +294,7 @@ CUnit::~CUnit()
 	// Remove us from our group, if we were in one
 	SetGroup(NULL);
 
-	std::vector<CWeapon*>::iterator wi;
-	for (wi = weapons.begin(); wi != weapons.end(); ++wi) {
+	for (std::vector<CWeapon*>::const_iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
 		delete *wi;
 	}
 
@@ -337,7 +335,7 @@ void CUnit::PreInit(const UnitDef* uDef, int uTeam, int facing, const float3& po
 	allyteam = teamHandler->AllyTeam(uTeam);
 
 	unitDef = uDef;
-	unitDefName = unitDef->name;
+	unitDefID = unitDef->id;
 
 	pos = position;
 	pos.CheckInBounds();
@@ -360,8 +358,8 @@ void CUnit::PreInit(const UnitDef* uDef, int uTeam, int facing, const float3& po
 	posErrorVector.y *= 0.2f;
 
 #ifdef TRACE_SYNC
-	tracefile << "[" << __FUNCTION__ << "] new unit: " << unitDefName << " ";
-	tracefile << pos.x << " " << pos.y << " " << pos.z << " " << id << "\n";
+	tracefile << "[" << __FUNCTION__ << "] id: " << id << ", name: " << unitDef->name << " ";
+	tracefile << "pos: <" << pos.x << ", " << pos.y << ", " << pos.z << ">\n";
 #endif
 
 	ASSERT_SYNCED_FLOAT3(pos);
@@ -588,6 +586,7 @@ void CUnit::ForcedSpin(const float3& newDir)
 }
 
 
+/*
 void CUnit::SetFront(const SyncedFloat3& newDir)
 {
 	frontdir = newDir;
@@ -612,7 +611,6 @@ void CUnit::SetUp(const SyncedFloat3& newDir)
 	UpdateMidPos();
 }
 
-
 void CUnit::SetRight(const SyncedFloat3& newDir)
 {
 	rightdir = newDir;
@@ -623,6 +621,24 @@ void CUnit::SetRight(const SyncedFloat3& newDir)
 	frontdir.Normalize();
 	heading = GetHeadingFromVector(frontdir.x, frontdir.z);
 	UpdateMidPos();
+}
+*/
+
+void CUnit::SetDirectionFromHeading()
+{
+	if (GetTransporter() == NULL) {
+		frontdir = GetVectorFromHeading(heading);
+
+		if (upright || !unitDef->canmove) {
+			updir = UpVector;
+			rightdir = frontdir.cross(updir);
+		} else  {
+			updir = ground->GetNormal(pos.x, pos.z);
+			rightdir = frontdir.cross(updir);
+			rightdir.Normalize();
+			frontdir = updir.cross(rightdir);
+		}
+	}
 }
 
 
@@ -732,8 +748,7 @@ void CUnit::Update()
 			moveType->padStatus = 0;
 		}
 		// paralyzed weapons shouldn't reload
-		std::vector<CWeapon*>::iterator wi;
-		for (wi = weapons.begin(); wi != weapons.end(); ++wi) {
+		for (std::vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
 			++(*wi)->reloadStatus;
 		}
 		return;
@@ -742,8 +757,7 @@ void CUnit::Update()
 	restTime++;
 
 	if (!dontUseWeapons) {
-		std::vector<CWeapon*>::iterator wi;
-		for (wi = weapons.begin(); wi != weapons.end(); ++wi) {
+		for (std::vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
 			(*wi)->Update();
 		}
 	}
@@ -1039,9 +1053,9 @@ void CUnit::SlowUpdateWeapons() {
 	if (!dontFire) {
 		for (vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
 			CWeapon* w = *wi;
-			
+
 			if (!w->haveUserTarget) {
-				if (haveDGunRequest == (unitDef->canDGun && w->weaponDef->manualfire)) { // == ((!haveDGunRequest && !isDGun) || (haveDGunRequest && isDGun))
+				if ((haveDGunRequest == (unitDef->canDGun && w->weaponDef->manualfire))) {
 					if (userTarget) {
 						w->AttackUnit(userTarget, true);
 					} else if (userAttackGround) {
@@ -1054,24 +1068,6 @@ void CUnit::SlowUpdateWeapons() {
 
 			if (w->targetType == Target_None && fireState > FIRESTATE_HOLDFIRE && lastAttacker && (lastAttack + 200 > gs->frameNum))
 				w->AttackUnit(lastAttacker, false);
-		}
-	}
-}
-
-
-void CUnit::SetDirectionFromHeading()
-{
-	if (GetTransporter() == NULL) {
-		frontdir = GetVectorFromHeading(heading);
-
-		if (upright || !unitDef->canmove) {
-			updir = UpVector;
-			rightdir = frontdir.cross(updir);
-		} else  {
-			updir = ground->GetNormal(pos.x, pos.z);
-			rightdir = frontdir.cross(updir);
-			rightdir.Normalize();
-			frontdir = updir.cross(rightdir);
 		}
 	}
 }
@@ -1540,48 +1536,56 @@ void CUnit::ChangeTeamReset()
 }
 
 
-bool CUnit::AttackUnit(CUnit *unit,bool dgun)
+
+bool CUnit::AttackUnit(CUnit* targetUnit, bool wantDGun, bool fpsMode)
 {
-	bool r = false;
-	haveDGunRequest = dgun;
+	bool ret = false;
+
+	haveDGunRequest = wantDGun;
 	userAttackGround = false;
 	commandShotCount = 0;
-	SetUserTarget(unit);
 
-	std::vector<CWeapon*>::iterator wi;
-	for (wi = weapons.begin(); wi != weapons.end(); ++wi) {
-		(*wi)->haveUserTarget = false;
-		(*wi)->targetType = Target_None;
-		if (haveDGunRequest == (unitDef->canDGun && (*wi)->weaponDef->manualfire)) // == ((!haveDGunRequest && !isDGun) || (haveDGunRequest && isDGun))
-			if ((*wi)->AttackUnit(unit, true))
-				r = true;
-	}
-	return r;
-}
-
-
-bool CUnit::AttackGround(const float3& pos, bool dgun)
-{
-	bool r = false;
-
-	haveDGunRequest = dgun;
-	SetUserTarget(0);
-	userAttackPos = pos;
-	userAttackGround = true;
-	commandShotCount = 0;
+	SetUserTarget(targetUnit);
 
 	for (std::vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
-		(*wi)->haveUserTarget = false;
+		CWeapon* w = *wi;
 
-		if (haveDGunRequest == (unitDef->canDGun && (*wi)->weaponDef->manualfire)) { // == ((!haveDGunRequest && !isDGun) || (haveDGunRequest && isDGun))
-			if ((*wi)->AttackGround(pos, true)) {
-				r = true;
+		w->targetType = Target_None;
+
+		if ((wantDGun == (unitDef->canDGun && w->weaponDef->manualfire)) || fpsMode) {
+			if (w->AttackUnit(targetUnit, true)) {
+				ret = true;
 			}
 		}
 	}
 
-	return r;
+	return ret;
 }
+
+bool CUnit::AttackGround(const float3& pos, bool wantDGun, bool fpsMode)
+{
+	bool ret = false;
+
+	haveDGunRequest = wantDGun;
+	userAttackPos = pos;
+	userAttackGround = true;
+	commandShotCount = 0;
+
+	SetUserTarget(NULL);
+
+	for (std::vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
+		CWeapon* w = *wi;
+
+		if ((wantDGun == (unitDef->canDGun && w->weaponDef->manualfire)) || fpsMode) {
+			if (w->AttackGround(pos, true)) {
+				ret = true;
+			}
+		}
+	}
+
+	return ret;
+}
+
 
 
 void CUnit::SetLastAttacker(CUnit* attacker)
@@ -1598,6 +1602,20 @@ void CUnit::SetLastAttacker(CUnit* attacker)
 		AddDeathDependence(attacker);
 }
 
+void CUnit::SetUserTarget(CUnit* target)
+{
+	if (userTarget)
+		DeleteDeathDependence(userTarget, DEPENDENCE_TARGET);
+
+	userTarget = target;
+	for (vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
+		(*wi)->haveUserTarget = false; // should be (target != NULL)?
+	}
+
+	if (target) {
+		AddDeathDependence(target);
+	}
+}
 
 void CUnit::DependentDied(CObject* o)
 {
@@ -1609,22 +1627,6 @@ void CUnit::DependentDied(CObject* o)
 	incomingMissiles.remove((CMissileProjectile*) o);
 
 	CSolidObject::DependentDied(o);
-}
-
-
-void CUnit::SetUserTarget(CUnit* target)
-{
-	if (userTarget)
-		DeleteDeathDependence(userTarget, DEPENDENCE_TARGET);
-
-	userTarget=target;
-	for(vector<CWeapon*>::iterator wi=weapons.begin();wi!=weapons.end();++wi) {
-		(*wi)->haveUserTarget = false;
-	}
-
-	if (target) {
-		AddDeathDependence(target);
-	}
 }
 
 
@@ -2104,7 +2106,7 @@ void CUnit::ReleaseTempHoldFire()
 void CUnit::PostLoad()
 {
 	//HACK:Initializing after load
-	unitDef = unitDefHandler->GetUnitDefByName(unitDefName);
+	unitDef = unitDefHandler->GetUnitDefByID(unitDefID);
 
 	curYardMap = (unitDef->yardmaps[buildFacing].empty())? NULL: &unitDef->yardmaps[buildFacing][0];
 
@@ -2232,10 +2234,12 @@ void CUnit::DeleteDeathDependence(CObject* o, DependenceType dep) {
 }
 
 
+
+CR_BIND_DERIVED(CUnit, CSolidObject, );
 // Member bindings
 CR_REG_METADATA(CUnit, (
-	//CR_MEMBER(unitDef),
-	CR_MEMBER(unitDefName),
+	// CR_MEMBER(unitDef),
+	CR_MEMBER(unitDefID),
 	CR_MEMBER(collisionVolume),
 	CR_MEMBER(frontdir),
 	CR_MEMBER(rightdir),
@@ -2411,4 +2415,4 @@ CR_REG_METADATA(CUnit, (
 //	CR_MEMBER(expGrade),
 
 	CR_POSTLOAD(PostLoad)
-	));
+));

rts/Sim/Units/Unit.h
@@ -72,8 +72,9 @@ public:
 	virtual void PreInit(const UnitDef* def, int team, int facing, const float3& position, bool build);
 	virtual void PostInit(const CUnit* builder);
 
-	bool AttackGround(const float3& pos, bool dgun);
-	bool AttackUnit(CUnit* unit, bool dgun);
+	virtual void SlowUpdate();
+	virtual void SlowUpdateWeapons();
+	virtual void Update();
 
 	virtual void DoDamage(const DamageArray& damages, CUnit* attacker,
 	                      const float3& impulse, int weaponId = -1);
@@ -81,9 +82,12 @@ public:
 	virtual void Kill(const float3& impulse);
 	virtual void FinishedBuilding();
 
+	bool AttackGround(const float3& pos, bool wantDGun, bool fpsMode = false);
+	bool AttackUnit(CUnit* unit, bool wantDGun, bool fpsMode = false);
+
 	int GetBlockingMapID() const { return id; }
 
-	void ChangeLos(int l, int airlos);
+	void ChangeLos(int losRad, int airRad);
 	void ChangeSensorRadius(int* valuePtr, int newValue);
 	/// negative amount=reclaim, return= true -> build power was successfully applied
 	bool AddBuildPower(float amount, CUnit* builder);
@@ -94,20 +98,9 @@ public:
 
 	void ForcedMove(const float3& newPos);
 	void ForcedSpin(const float3& newDir);
-	void SetFront(const SyncedFloat3& newDir);
-	void SetUp(const SyncedFloat3& newDir);
-	void SetRight(const SyncedFloat3& newDir);
+	void SetDirectionFromHeading();
 	void EnableScriptMoveType();
 	void DisableScriptMoveType();
-
-	///// order unit to move to position
-	//void SetGoal(float3 pos);
-
-	virtual void SlowUpdate();
-	virtual void SlowUpdateWeapons();
-	virtual void Update();
-
-	void SetDirectionFromHeading();
 
 	void ApplyTransformMatrix() const;
 	CMatrix44f GetTransformMatrix(const bool synced = false, const bool error = false) const;
@@ -119,8 +112,18 @@ public:
 	}
 
 	void DependentDied(CObject* o);
-	enum DependenceType { DEPENDENCE_ATTACKER, DEPENDENCE_BUILD, DEPENDENCE_BUILDER, DEPENDENCE_CAPTURE, DEPENDENCE_RECLAIM, 
-						DEPENDENCE_RESURRECT, DEPENDENCE_TARGET, DEPENDENCE_TERRAFORM, DEPENDENCE_TRANSPORTEE, DEPENDENCE_TRANSPORTER };
+	enum DependenceType {
+		DEPENDENCE_ATTACKER,
+		DEPENDENCE_BUILD,
+		DEPENDENCE_BUILDER,
+		DEPENDENCE_CAPTURE,
+		DEPENDENCE_RECLAIM, 
+		DEPENDENCE_RESURRECT,
+		DEPENDENCE_TARGET,
+		DEPENDENCE_TERRAFORM,
+		DEPENDENCE_TRANSPORTEE,
+		DEPENDENCE_TRANSPORTER
+	};
 	virtual void DeleteDeathDependence(CObject* o, DependenceType dep);
 
 	void SetUserTarget(CUnit* target);
@@ -155,7 +158,7 @@ public:
 	void SlowUpdateCloak(bool);
 	void ScriptDecloak(bool);
 
-	enum ChangeType{
+	enum ChangeType {
 		ChangeGiven,
 		ChangeCaptured
 	};
@@ -163,7 +166,7 @@ public:
 	virtual void StopAttackingAllyTeam(int ally);
 
 	const UnitDef* unitDef;
-	std::string unitDefName;
+	int unitDefID;
 
 	/**
 	 * @brief mod controlled parameters
@@ -239,8 +242,11 @@ public:
 	bool stunned;
 	/// tells weapons that support it to try to use a high trajectory
 	bool useHighTrajectory;
-	/// used by landed aircrafts for now
+
+	/// used by landed gunships to block weapon updates
 	bool dontUseWeapons;
+	/// temp variable that can be set when building etc to stop units to turn away to fire
+	bool dontFire;
 
 	/// the script has finished exectuting the killed function and the unit can be deleted
 	bool deathScriptFinished;
@@ -367,8 +373,6 @@ public:
 
 	int fireState;
 	int moveState;
-	/// temp variable that can be set when building etc to stop units to turn away to fire
-	bool dontFire;
 
 	/// if the unit is in it's 'on'-state
 	bool activated;