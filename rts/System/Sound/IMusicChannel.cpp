0123dfa16a9696d07b8c707b1a31dbb4c8f170af
-jk- <-jk->
2011-03-04 21:08:38 +0100

    merge MusicChannel & EffectChannel classes

--- rts/System/Sound/IMusicChannel.cpp
+++
@@ -1,9 +0,0 @@
-/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
-
-#include "IMusicChannel.h"
-#include "SoundChannels.h"
-
-namespace Channels
-{
-	MusicChannelImpl BGMusic;
-}
