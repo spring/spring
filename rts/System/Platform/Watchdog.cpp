f09dae87776c579a512b2b609fbcb80efd2da13b
-jk- <-jk->
2011-03-08 18:04:20 +0100

    deactivate Watchdog when gdb is running (gdb locks the stack)

--- rts/System/Platform/Watchdog.cpp
+++ rts/System/Platform/Watchdog.cpp
@@ -4,6 +4,10 @@
 #include "Watchdog.h"
 
 #include "lib/gml/gml.h"
+#ifndef _WIN32
+	#include <fstream>
+	#include <iostream>
+#endif
 #include <boost/thread.hpp>
 #include <boost/bind.hpp>
 #include <boost/function.hpp>
@@ -12,6 +16,7 @@
 #include "Game/GameVersion.h"
 #include "System/ConfigHandler.h"
 #include "System/LogOutput.h"
+#include "System/maindefines.h"
 #include "System/myTime.h"
 #include "System/Platform/CrashHandler.h"
 #include "System/Platform/Threading.h"
@@ -46,9 +51,9 @@ namespace Watchdog
 					if (spring_istime(th_info.timer) && (curtime - th_info.timer) > hangTimeout) {
 						if(!hangDetected) {
 							logOutput.Print("Hang detection triggered for Spring %s.", SpringVersion::GetFull().c_str());
-#ifdef USE_GML
+						#ifdef USE_GML
 							logOutput.Print("MT with %d threads.", gmlThreadCount);
-#endif
+						#endif
 						}
 						logOutput.Print("(in thread: %s)", th_info.name.c_str());
 						hangDetected = true;
@@ -83,11 +88,11 @@ namespace Watchdog
 	}
 
 
-	/*void DeregisterThread()
+	void DeregisterThread(std::string name)
 	{
 		boost::unique_lock<boost::shared_mutex> lock(mutex);
 		//FIXME ADD CODE
-	}*/
+	}
 
 
 	void ClearTimer(bool disable, Threading::NativeThreadId* _threadId)
@@ -128,10 +133,26 @@ namespace Watchdog
 
 	void Install()
 	{
+	#ifndef _WIN32
+		//! disable if gdb is running
+		char buf[1024];
+		SNPRINTF(buf, sizeof(buf), "/proc/%d/cmdline", getppid());
+		std::ifstream f(buf);
+		if (f.good()) {
+			f.read(buf,sizeof(buf));
+			f.close();
+			std::string str(buf);
+			if (str.find("gdb") != std::string::npos) {
+				logOutput.Print("[Watchdog] disabled (gdb detected)");
+				return;
+			}
+		}
+	#endif
 		int hangTimeoutSecs = configHandler->Get("HangTimeout", 0);
 
 		//! HangTimeout = -1 to force disable hang detection
 		if (hangTimeoutSecs < 0)
+			logOutput.Print("[Watchdog] disabled");
 			return;
 		if (hangTimeoutSecs == 0)
 			hangTimeoutSecs = 10;
