be0b5f0dd093d0ef702374253dd518dd0cac6dd7
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-03-08 22:14:30 +0100

    buildbot: let cmake find binaries on PATH to allow ccache usage

--- buildbot/slave/win32.cmake
+++ buildbot/slave/win32.cmake
@@ -1,15 +1,9 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # which compilers to use for C and C++
-SET(CMAKE_C_COMPILER /usr/bin/i586-mingw32msvc-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/i586-mingw32msvc-g++)
-SET(WINDRES /usr/bin/i586-mingw32msvc-windres)
+SET(CMAKE_C_COMPILER i586-mingw32msvc-gcc)
+SET(CMAKE_CXX_COMPILER i586-mingw32msvc-g++)
+SET(WINDRES i586-mingw32msvc-windres)
 
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH /usr/lib/gcc/i586-mingw32msvc)
-
-# the spring mingw32 dependencies
-SET(MINGWLIBS "~/mingwlibs/")
-
-# the path that make install will use to put the final binaries
-SET(CMAKE_INSTALL_PREFIX /share/sources/spring-cross/win32/final)
