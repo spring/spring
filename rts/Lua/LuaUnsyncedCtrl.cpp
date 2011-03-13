8846b0cd40214c64d2130cb7c5022b5e39d02898
-jk- <-jk->
2011-03-13 04:17:36 +0100

    fix windows compile

--- rts/Lua/LuaUnsyncedCtrl.cpp
+++ rts/Lua/LuaUnsyncedCtrl.cpp
@@ -75,6 +75,7 @@ using namespace std;
 
 // MinGW defines this for a WINAPI function
 #undef SendMessage
+#undef CreateDirectory
 
 const int CMD_INDEX_OFFSET = 1; // starting index for command descriptions
 
