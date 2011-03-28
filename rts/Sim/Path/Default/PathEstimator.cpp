39fa4125a0ba558f8720836cc19a86f76b36b315
zerver <mr@zerver.net>
2011-03-28 22:01:26 +0200

    -

--- rts/Sim/Path/Default/PathEstimator.cpp
+++ rts/Sim/Path/Default/PathEstimator.cpp
@@ -1,6 +1,9 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "StdAfx.h"
+
+#include "Rendering/GL/myGL.h"
+
 #include "PathEstimator.h"
 
 #include <fstream>
