spring/spring

commit 8f47d5f525753f4f8804c8e65c0f2fed14a94c5a

[GMT] prevent units from being pushed onto impassable squares

rts/Sim/MoveTypes/GroundMoveType.cpp
@@ -14,7 +14,6 @@
 #include "Map/ReadMap.h"
 #include "MoveMath/MoveMath.h"
 #include "Rendering/GroundDecalHandler.h"
-#include "Rendering/Models/3DModel.h"
 #include "Sim/Features/Feature.h"
 #include "Sim/Features/FeatureHandler.h"
 #include "Sim/Misc/GeometricObjects.h"
@@ -28,14 +27,14 @@
 #include "Sim/Units/CommandAI/MobileCAI.h"
 #include "Sim/Weapons/WeaponDefHandler.h"
 #include "Sim/Weapons/Weapon.h"
-#include "System/Sync/SyncTracer.h"
 #include "System/GlobalUnsynced.h"
 #include "System/EventHandler.h"
 #include "System/LogOutput.h"
-#include "Sound/IEffectChannel.h"
 #include "System/FastMath.h"
 #include "System/myMath.h"
 #include "System/Vec2.h"
+#include "System/Sound/IEffectChannel.h"
+#include "System/Sync/SyncTracer.h"
 
 #define MIN_WAYPOINT_DISTANCE (SQUARE_SIZE << 1)
 #define DEBUG_OUTPUT 0
@@ -1366,8 +1365,9 @@ void CGroundMoveType::HandleObjectCollisions()
 				continue;
 			}
 
-			const UnitDef*  collideeUD = collidee->unitDef;
-			const MoveData* collideeMD = collidee->mobility;
+			const UnitDef*   collideeUD = collidee->unitDef;
+			const MoveData*  collideeMD = collidee->mobility;
+			const CMoveMath* collideeMM = (collideeMD != NULL)? collideeMD->moveMath: NULL;
 
 			const float3& collideeCurPos = collidee->pos;
 			const float3& collideeOldPos = collidee->moveType->oldPos;
@@ -1387,8 +1387,10 @@ void CGroundMoveType::HandleObjectCollisions()
 			if ((separationVector.SqLength() - separationMinDist) > 0.01f) { continue; }
 			if (collidee->usingScriptMoveType) { pushCollidee = false; }
 			if (collideeUD->pushResistant) { pushCollidee = false; }
+
 			if (!teamHandler->Ally(collider->allyteam, collidee->allyteam)) { pushCollider = false; pushCollidee = false; }
 			if (!teamHandler->Ally(collidee->allyteam, collider->allyteam)) { pushCollider = false; pushCollidee = false; }
+
 			if (colliderMM->IsNonBlocking(*colliderMD, collidee)) { continue; }
 			if (!collideeMobile && (colliderMM->IsBlocked(*colliderMD, colliderCurPos) & CMoveMath::BLOCK_STRUCTURE) == 0) { continue; }
 
@@ -1418,8 +1420,17 @@ void CGroundMoveType::HandleObjectCollisions()
 				SWAP_MASS_SCALES(&colliderMassScale, &collideeMassScale);
 			}
 
-			if (pushCollider) { collider->pos += (colResponseVec * colliderMassScale); } else { collider->pos = colliderOldPos; }
-			if (pushCollidee) { collidee->pos -= (colResponseVec * collideeMassScale); } else { collidee->pos = collideeOldPos; }
+			const float3 colliderNewPos = collider->pos + (colResponseVec * colliderMassScale);
+			const float3 collideeNewPos = collidee->pos - (colResponseVec * collideeMassScale);
+
+			// try to prevent both parties from being pushed onto non-traversable squares
+			if (                  (colliderMM->IsBlocked(*colliderMD, colliderNewPos) & CMoveMath::BLOCK_STRUCTURE) != 0) { pushCollider = false; }
+			if (collideeMobile && (collideeMM->IsBlocked(*collideeMD, collideeNewPos) & CMoveMath::BLOCK_STRUCTURE) != 0) { pushCollidee = false; }
+			if (                  colliderMM->SpeedMod(*colliderMD, colliderNewPos) <= 0.01f) { pushCollider = false; }
+			if (collideeMobile && collideeMM->SpeedMod(*collideeMD, collideeNewPos) <= 0.01f) { pushCollidee = false; }
+
+			if (pushCollider) { collider->pos = colliderNewPos; } else { collider->pos = colliderOldPos; }
+			if (pushCollidee) { collidee->pos = collideeNewPos; } else { collidee->pos = collideeOldPos; }
 
 			collider->UpdateMidPos();
 			collidee->UpdateMidPos();

rts/Sim/MoveTypes/MoveMath/GroundMoveMath.cpp
@@ -8,47 +8,45 @@
 
 CR_BIND_DERIVED(CGroundMoveMath, CMoveMath, );
 
-using namespace std;
-
-float CGroundMoveMath::waterCost=0;
+float CGroundMoveMath::waterCost = 0.0f;
 
 /*
 Calculate speed-multiplier for given height and slope data.
 */
 float CGroundMoveMath::SpeedMod(const MoveData& moveData, float height, float slope) const
 {
-	//Too slope?
-	if(slope > moveData.maxSlope)
-		return 0.0f;
-	//Too depth?
-	if(-height > moveData.depth)
-		return 0.0f;
-	//Slope-mod
-	float mod = 1 / (1 + slope * moveData.slopeMod);
-	//Under-water-mod
-	if(height < 0) {
-		mod /= 1 - max(-1.0f, (height * moveData.depthMod));
-		mod*=waterCost;
+	float speedMod = 0.0f;
+
+	// too steep or too deep?
+	if (slope > moveData.maxSlope)
+		return speedMod;
+	if (-height > moveData.depth)
+		return speedMod;
+
+	// slope-mod
+	speedMod = 1.0f / (1.0f + slope * moveData.slopeMod);
+
+	// depth-mod
+	if (height < 0.0f) {
+		speedMod /= (1.0f - std::max(-1.0f, (height * moveData.depthMod)));
+		speedMod *= waterCost;
 	}
-	return mod;
+
+	return speedMod;
 }
 
-float CGroundMoveMath::SpeedMod(const MoveData& moveData, float height, float slope,float moveSlope) const
+float CGroundMoveMath::SpeedMod(const MoveData& moveData, float height, float slope, float moveSlope) const
 {
-	//Too slope?
-/*	if(slope*moveSlope > moveData.maxSlope)
-		return 0.0f;
-	//Too depth?
-	if(-height > moveData.depth && moveSlope<0)
-		return 0.0f;
-*/	//Slope-mod
-	float mod = 1 / (1 + max(0.0f,slope*moveSlope) * moveData.slopeMod);
-	//Under-water-mod
-	if(height < 0) {
-		mod /= 1 - max(-0.9f, height * moveData.depthMod);
+	float speedMod = 1.0f / (1.0f + std::max(0.0f, slope * moveSlope) * moveData.slopeMod);
+
+	if (height < 0.0) {
+		speedMod /= (1.0f - std::max(-0.9f, height * moveData.depthMod));
 	}
-	return mod;
+
+	return speedMod;
 }
+
+
 
 /*
 Gives the ground-level of given square.
@@ -58,10 +56,7 @@ float CGroundMoveMath::yLevel(int xSquare, int zSquare) const
 	return readmap->centerheightmap[xSquare + zSquare * gs->mapx];
 }
 
-
 float CGroundMoveMath::yLevel(const float3& pos) const
 {
-	return ground->GetHeightReal(pos.x, pos.z) + 10;
+	return (ground->GetHeightReal(pos.x, pos.z) + 10.0f);
 }
-
-

rts/Sim/MoveTypes/MoveMath/GroundMoveMath.h
@@ -11,11 +11,9 @@
 class CGroundMoveMath :	public CMoveMath {
 	CR_DECLARE(CGroundMoveMath);
 public:
-	//SpeedMod returns a speed-multiplier for given position or data.
 	float SpeedMod(const MoveData& moveData, float height, float slope) const;
-	float SpeedMod(const MoveData& moveData, float height, float slope,float moveSlope) const;
+	float SpeedMod(const MoveData& moveData, float height, float slope, float moveSlope) const;
 
-	//Gives the y-coordinate the unit will "stand on".
 	float yLevel(int xSquare, int zSquare) const;
 	float yLevel(const float3& pos) const;
 

rts/Sim/MoveTypes/MoveMath/HoverMoveMath.cpp
@@ -8,8 +8,7 @@
 
 CR_BIND_DERIVED(CHoverMoveMath, CMoveMath, );
 
-const float HOVERING_HEIGHT = 5;
-bool CHoverMoveMath::noWaterMove;
+bool CHoverMoveMath::noWaterMove = false;
 
 
 /*
@@ -17,42 +16,40 @@ Calculate speed-multiplier for given height and slope data.
 */
 float CHoverMoveMath::SpeedMod(const MoveData& moveData, float height, float slope) const
 {
-	//On water?
-	if(height < 0){
-		if(noWaterMove)
-			return 0.0f;
-		return 1.0f;
+	// no speed-penalty if on water (unless noWaterMove)
+	if (height < 0.0f) {
+		return (noWaterMove? 0.0f: 1.0f);
 	}
-	//Too slope?
-	if(slope > moveData.maxSlope)
+
+	if (slope > moveData.maxSlope)
 		return 0.0f;
-	//Slope-mod
-	return 1 / (1 + slope * moveData.slopeMod);
+
+	return (1.0f / (1.0f + slope * moveData.slopeMod));
 }
 
-float CHoverMoveMath::SpeedMod(const MoveData& moveData, float height, float slope,float moveSlope) const
+float CHoverMoveMath::SpeedMod(const MoveData& moveData, float height, float slope, float moveSlope) const
 {
-	//On water?
-	if(height < 0)
+	// no speed-penalty if on water
+	if (height < 0.0f)
 		return 1.0f;
-	//Too slope?
-	if(slope*moveSlope > moveData.maxSlope)
+
+	if ((slope * moveSlope) > moveData.maxSlope)
 		return 0.0f;
-	//Slope-mod
-	return 1 / (1 + std::max(0.0f,slope*moveSlope) * moveData.slopeMod);
+
+	return (1.0f / (1.0f + std::max(0.0f, slope * moveSlope) * moveData.slopeMod));
 }
+
+
 
 /*
 Gives a position slightly over ground and water level.
 */
 float CHoverMoveMath::yLevel(int xSquare, int zSquare) const
 {
-	return ground->GetHeightAboveWater(xSquare*SQUARE_SIZE, zSquare*SQUARE_SIZE) + 10;
+	return (ground->GetHeightAboveWater(xSquare * SQUARE_SIZE, zSquare * SQUARE_SIZE) + 10.0f);
 }
-
 
 float CHoverMoveMath::yLevel(const float3& pos) const
 {
-	return ground->GetHeightAboveWater(pos.x, pos.z) + 10;
+	return (ground->GetHeightAboveWater(pos.x, pos.z) + 10.0f);
 }
-

rts/Sim/MoveTypes/MoveMath/HoverMoveMath.h
@@ -12,11 +12,9 @@
 class CHoverMoveMath : public CMoveMath {
 	CR_DECLARE(CHoverMoveMath);
 public:
-	//SpeedMod returns a speed-multiplier for given position or data.
 	float SpeedMod(const MoveData& moveData, float height, float slope) const;
-	float SpeedMod(const MoveData& moveData, float height, float slope,float moveSlope) const;
+	float SpeedMod(const MoveData& moveData, float height, float slope, float moveSlope) const;
 
-	//Gives the y-coordinate the unit will "stand on".
 	float yLevel(int xSquare, int zSquare) const;
 	float yLevel(const float3& pos) const;
 

rts/Sim/MoveTypes/MoveMath/MoveMath.cpp
@@ -24,13 +24,16 @@ float CMoveMath::yLevel(const float3& pos) const
 /* Converts a point-request into a square-positional request. */
 float CMoveMath::SpeedMod(const MoveData& moveData, const float3& pos) const
 {
-	return SpeedMod(moveData, (pos.x / SQUARE_SIZE), (pos.z / SQUARE_SIZE));
+	//! casts are required so we call MM::SpeedMod(int, int) and not an overloaded *MM::SpeedMod(float, float)
+	return SpeedMod(moveData, int(pos.x / SQUARE_SIZE), int(pos.z / SQUARE_SIZE));
 }
 
 float CMoveMath::SpeedMod(const MoveData& moveData, const float3& pos, const float3& moveDir) const
 {
-	return SpeedMod(moveData, (pos.x / SQUARE_SIZE), (pos.z / SQUARE_SIZE), moveDir);
+	//! casts are required so we call MM::SpeedMod(int, int) and not an overloaded *MM::SpeedMod(float, float)
+	return SpeedMod(moveData, int(pos.x / SQUARE_SIZE), int(pos.z / SQUARE_SIZE), moveDir);
 }
+
 
 
 /* calculate the local speed-modifier for this movedata */

rts/Sim/MoveTypes/MoveMath/MoveMath.h
@@ -11,7 +11,6 @@ class CMoveMath {
 	CR_DECLARE(CMoveMath);
 public:
 	// Block-check-options
-	// Note: options are hierarchical, with CHECK_STRUCTURE as the first check.
 	const static int BLOCK_MOVING = 1;
 	const static int BLOCK_MOBILE = 2;
 	const static int BLOCK_MOBILE_BUSY = 4;
@@ -19,10 +18,10 @@ public:
 
 	// returns a speed-multiplier for given position or data
 	float SpeedMod(const MoveData& moveData, const float3& pos) const;
-	float SpeedMod(const MoveData& moveData, int xSquare, int zSquare) const;
-	virtual float SpeedMod(const MoveData& moveData, float height, float slope) const = 0;
 	float SpeedMod(const MoveData& moveData, const float3& pos, const float3& moveDir) const;
-	float SpeedMod(const MoveData& moveData, int xSquare, int zSquare,const float3& moveDir) const;
+	float SpeedMod(const MoveData& moveData, int xSquare, int zSquare) const;
+	float SpeedMod(const MoveData& moveData, int xSquare, int zSquare, const float3& moveDir) const;
+	virtual float SpeedMod(const MoveData& moveData, float height, float slope) const = 0;
 	virtual float SpeedMod(const MoveData& moveData, float height, float slope, float moveSlope) const = 0;
 
 	// tells whether a position is blocked (inaccessable for a given object's movedata)

rts/Sim/MoveTypes/MoveMath/ShipMoveMath.cpp
@@ -13,31 +13,16 @@ Calculate speed-multiplier for given height and slope data.
 */
 float CShipMoveMath::SpeedMod(const MoveData& moveData, float height, float slope) const
 {
-	//Too ground?
-	if(-height < moveData.depth)
+	if (-height < moveData.depth)
 		return 0.0f;
+
 	return 1.0f;
 }
 
 float CShipMoveMath::SpeedMod(const MoveData& moveData, float height, float slope, float moveSlope) const
 {
-	if(-height < moveData.depth && moveSlope>0)
+	if (-height < moveData.depth && moveSlope > 0.0f)
 		return 0.0f;
+
 	return 1.0f;
 }
-
-
-/*
-Ships are always in water level.
-*/
-float CShipMoveMath::yLevel(int xSquare, int zSquare) const
-{
-	return 0.0f;
-}
-
-
-float CShipMoveMath::yLevel(const float3& pos) const
-{
-	return 0.0f;
-}
-

commit 2d6bd9cb5a9ceb9c728dd34a1ab2925d4b0759e0

Fix mantis #2361 [Some units continuously jumping up and down after give all] Appears to be caused by 8f47d5f525753f4f8804c8e65c0f2fed14a94c5a

rts/Sim/MoveTypes/GroundMoveType.cpp
@@ -1325,7 +1325,8 @@ void CGroundMoveType::HandleObjectCollisions()
 				FOOTPRINT_RADIUS(collideeMD->xsize, collideeMD->zsize):
 				FOOTPRINT_RADIUS(collideeUD->xsize, collideeUD->zsize);
 
-			bool pushCollider = true;
+			bool colliderMobile = (collider->mobility != NULL);
+			bool pushCollider = colliderMobile;
 			bool pushCollidee = (collideeMobile || collideeUD->canfly);
 
 			const float3 separationVector = colliderCurPos - collideeCurPos;
@@ -1394,8 +1395,8 @@ void CGroundMoveType::HandleObjectCollisions()
 			if (                  colliderMM->GetPosSpeedMod(*colliderMD, colliderNewPos) <= 0.01f) { colliderMassScale = 0.0f; }
 			if (collideeMobile && collideeMM->GetPosSpeedMod(*collideeMD, collideeNewPos) <= 0.01f) { collideeMassScale = 0.0f; }
 
-			if (pushCollider) { collider->pos += (colResponseVec * colliderMassScale); } else { collider->pos = colliderOldPos; }
-			if (pushCollidee) { collidee->pos -= (colResponseVec * collideeMassScale); } else { collidee->pos = collideeOldPos; }
+			if (pushCollider) { collider->pos += (colResponseVec * colliderMassScale); } else if (colliderMobile) { collider->pos = colliderOldPos; }
+			if (pushCollidee) { collidee->pos -= (colResponseVec * collideeMassScale); } else if (collideeMobile) { collidee->pos = collideeOldPos; }
 
 			collider->UpdateMidPos();
 			collidee->UpdateMidPos();