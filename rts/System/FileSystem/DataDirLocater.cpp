f2c192f2b495dda335d066fadbcc17e640135852
hoijui <hoijui.quaero@gmail.com>
2011-04-29 10:04:25 +0200

    cleanup DataDirLocater (no logic changes)

--- rts/System/FileSystem/DataDirLocater.cpp
+++ rts/System/FileSystem/DataDirLocater.cpp
@@ -27,12 +27,15 @@
 #include "maindefines.h" // for sPS, cPS, cPD
 #include "Platform/Misc.h"
 
-DataDir::DataDir(const std::string& p) : path(p), writable(false)
+DataDir::DataDir(const std::string& path)
+	: path(path)
+	, writable(false)
 {
-	FileSystemHandler::EnsurePathSepAtEnd(path);
+	FileSystemHandler::EnsurePathSepAtEnd(this->path);
 }
 
-DataDirLocater::DataDirLocater() : writedir(NULL)
+DataDirLocater::DataDirLocater()
+	: writeDir(NULL)
 {
 }
 
@@ -72,14 +75,15 @@ std::string DataDirLocater::SubstEnvVars(const std::string& in) const
 	return out.str();
 }
 
-void DataDirLocater::AddDirs(const std::string& in)
+void DataDirLocater::AddDirs(const std::string& dirs)
 {
-	size_t prev_colon = 0, colon;
-	while ((colon = in.find(cPD, prev_colon)) != std::string::npos) { // cPD (depending on OS): ';' or ':'
-		AddDir(in.substr(prev_colon, colon - prev_colon));
+	size_t prev_colon = 0;
+	size_t colon;
+	while ((colon = dirs.find(cPD, prev_colon)) != std::string::npos) { // cPD (depending on OS): ';' or ':'
+		AddDir(dirs.substr(prev_colon, colon - prev_colon));
 		prev_colon = colon + 1;
 	}
-	AddDir(in.substr(prev_colon));
+	AddDir(dirs.substr(prev_colon));
 }
 
 void DataDirLocater::AddDir(const std::string& dir)
@@ -91,7 +95,7 @@ void DataDirLocater::AddDir(const std::string& dir)
 		bool alreadyAdded = false;
 
 		std::vector<DataDir>::const_iterator ddi;
-		for (ddi = datadirs.begin(); ddi != datadirs.end(); ++ddi) {
+		for (ddi = dataDirs.begin(); ddi != dataDirs.end(); ++ddi) {
 			if (newDataDir.path == ddi->path) {
 				alreadyAdded = true;
 				break;
@@ -99,7 +103,7 @@ void DataDirLocater::AddDir(const std::string& dir)
 		}
 
 		if (!alreadyAdded) {
-			datadirs.push_back(newDataDir);
+			dataDirs.push_back(newDataDir);
 #ifdef DEBUG
 			logOutput.Print("Adding %s to directories", newDataDir.path.c_str());
 		} else {
@@ -109,39 +113,41 @@ void DataDirLocater::AddDir(const std::string& dir)
 	}
 }
 
-bool DataDirLocater::DeterminePermissions(DataDir* d)
+bool DataDirLocater::DeterminePermissions(DataDir* dataDir)
 {
 #ifndef _WIN32
-	if (d->path.c_str()[0] != '/' || d->path.find("..") != std::string::npos)
+	if ((dataDir->path.c_str()[0] != '/') || (dataDir->path.find("..") != std::string::npos))
 #else
-	if (d->path.find("..") != std::string::npos)
+	if (dataDir->path.find("..") != std::string::npos)
 #endif
 	{
-		throw content_error(std::string("Error: datadir specified with relative path: \"")+d->path+"\"");
+		throw content_error(std::string("Error: datadir specified with relative path: \"") + dataDir->path + "\"");
 	}
 	// Figure out whether we have read/write permissions
-	// First check read access, if we got that check write access too
+	// First check read access, if we got that, check write access too
 	// (no support for write-only directories)
-	// Note: we check for executable bit otherwise we can't browse the directory
-	// Note: we fail to test whether the path actually is a directory
-	// Note: modifying permissions while or after this function runs has undefined behaviour
-	if (FileSystemHandler::GetInstance().DirExists(d->path))
+	// We check for the executable bit, because otherwise we can not browse the
+	// directory.
+	// FIXME: We fail to test whether the path actually is a directory
+	// Modifying the permissions while or after this function runs has undefined
+	// behaviour.
+	if (FileSystemHandler::GetInstance().DirExists(dataDir->path))
 	{
-		if (!writedir && FileSystemHandler::GetInstance().DirIsWritable(d->path))
+		if (!writeDir && FileSystemHandler::GetInstance().DirIsWritable(dataDir->path))
 		{
-			d->writable = true;
-			writedir = &*d;
+			dataDir->writable = true;
+			writeDir = dataDir;
 		}
 		return true;
 	}
-	else if (!writedir) // if there is already a rw data directory, do not create new folder for read-only locations
+	else if (!writeDir) // if there is already a rw data directory, do not create new folder for read-only locations
 	{
-		if (filesystem.CreateDirectory(d->path))
+		if (filesystem.CreateDirectory(dataDir->path))
 		{
-			// it didn't exist before, now it does and we just created it with rw access,
-			// so we just assume we still have read-write acces...
-			d->writable = true;
-			writedir = d;
+			// it did not exist before, now it does and we just created it with
+			// rw access, so we just assume we still have read-write access ...
+			dataDir->writable = true;
+			writeDir = dataDir;
 			return true;
 		}
 	}
@@ -152,19 +158,19 @@ void DataDirLocater::DeterminePermissions()
 {
 	std::vector<DataDir> newDatadirs;
 	std::string previous; // used to filter out consecutive duplicates
-	// (I didn't bother filtering out non-consecutive duplicates because then
+	// (I did not bother filtering out non-consecutive duplicates because then
 	//  there is the question which of the multiple instances to purge.)
 
-	writedir = NULL;
+	writeDir = NULL;
 
-	for (std::vector<DataDir>::iterator d = datadirs.begin(); d != datadirs.end(); ++d) {
-		if (d->path != previous && DeterminePermissions(&*d)) {
+	for (std::vector<DataDir>::iterator d = dataDirs.begin(); d != dataDirs.end(); ++d) {
+		if ((d->path != previous) && DeterminePermissions(&*d)) {
 			newDatadirs.push_back(*d);
 			previous = d->path;
 		}
 	}
 
-	datadirs = newDatadirs;
+	dataDirs = newDatadirs;
 }
 
 void DataDirLocater::AddCwdOrParentDir(const std::string& curWorkDir, bool forceAdd)
@@ -262,8 +268,8 @@ void DataDirLocater::LocateDataDirs()
 	}
 #endif // defined(WIN32), defined(MACOSX_BUNDLE), else
 
-	// Construct the list of datadirs from various sources.
-	datadirs.clear();
+	// Construct the list of dataDirs from various sources.
+	dataDirs.clear();
 	// The first dir added will be the writeable data dir.
 
 	if (isolationMode) {
@@ -311,10 +317,10 @@ void DataDirLocater::LocateDataDirs()
 #endif
 	}
 
-	// Figure out permissions of all datadirs
+	// Figure out permissions of all dataDirs
 	DeterminePermissions();
 
-	if (!writedir) {
+	if (!writeDir) {
 		// bail out
 		const std::string errstr = "Not a single writable data directory found!\n\n"
 				"Configure a writable data directory using either:\n"
@@ -331,7 +337,7 @@ void DataDirLocater::LocateDataDirs()
 
 	// for now, chdir to the data directory as a safety measure:
 	// Not only safety anymore, it's just easier if other code can safely assume that
-	// writedir == current working directory
+	// writeDir == current working directory
 	FileSystemHandler::GetInstance().Chdir(GetWriteDir()->path.c_str());
 
 	// Initialize the log. Only after this moment log will be written to file.
@@ -339,7 +345,7 @@ void DataDirLocater::LocateDataDirs()
 	// Logging MAY NOT start before the chdir, otherwise the logfile ends up
 	// in the wrong directory.
 	// Update: now it actually may start before, log has preInitLog.
-	for (std::vector<DataDir>::const_iterator d = datadirs.begin(); d != datadirs.end(); ++d) {
+	for (std::vector<DataDir>::const_iterator d = dataDirs.begin(); d != dataDirs.end(); ++d) {
 		if (d->writable) {
 			logOutput.Print("Using read-write data directory: %s", d->path.c_str());
 
