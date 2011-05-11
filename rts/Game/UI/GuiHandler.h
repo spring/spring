c821e70227762e9469f507d5c69b9da3e6f0a74c
hoijui <hoijui.quaero@gmail.com>
2011-05-11 08:40:58 +0200

    GuiHandler: cleanup parameters

--- rts/Game/UI/GuiHandler.h
+++ rts/Game/UI/GuiHandler.h
@@ -29,18 +29,17 @@ public:
 	void DrawMapStuff(bool onMinimap);
 	void DrawCentroidCursor();
 
-	bool AboveGui(int x,int y);
+	bool AboveGui(int x, int y);
 	bool KeyPressed(unsigned short key, bool isRepeat);
 	bool KeyReleased(unsigned short key);
 	bool MousePress(int x, int y, int button);
-	void MouseRelease(int x, int y, int button) {MouseRelease(x,y,button, ::camera->pos, ::mouse->dir);}
-	void MouseRelease(int x, int y, int button, float3& cameraPos, float3& mouseDir);
+	void MouseRelease(int x, int y, int button, float3& cameraPos = ::camera->pos, float3& mouseDir = ::mouse->dir);
 	bool IsAbove(int x, int y);
 	std::string GetTooltip(int x, int y);
 	std::string GetBuildTooltip() const;
 
 	Command GetOrderPreview();
-	Command GetCommand(int mouseX, int mouseY, int buttonHint, bool preview, float3& cameraPos=::camera->pos, float3& mouseDir=::mouse->dir);
+	Command GetCommand(int mouseX, int mouseY, int buttonHint, bool preview, float3& cameraPos = ::camera->pos, float3& mouseDir = ::mouse->dir);
 	std::vector<BuildInfo> GetBuildPos(const BuildInfo& startInfo,
 		const BuildInfo& endInfo, float3& cameraPos, float3& mouseDir); // startInfo.def has to be endInfo.def
 
@@ -52,7 +51,7 @@ public:
 	int GetActivePage() const { return activePage; }
 
 	void RunLayoutCommand(const std::string& command);
-	void RunCustomCommands(const std::vector<std::string>& cmds, bool rmb);
+	void RunCustomCommands(const std::vector<std::string>& cmds, bool rightMouseButton);
 
 	bool GetInvertQueueKey() const { return invertQueueKey; }
 	void SetInvertQueueKey(bool value) { invertQueueKey = value; }
@@ -63,11 +62,11 @@ public:
 
 	bool GetOutlineFonts() const { return outlineFonts; }
 
-	int  GetDefaultCommand(int x, int y, float3& cameraPos=::camera->pos, float3& mouseDir=::mouse->dir) const;
+	int  GetDefaultCommand(int x, int y, float3& cameraPos = ::camera->pos, float3& mouseDir = ::mouse->dir) const;
 
-	bool SetActiveCommand(int cmdIndex, bool rmb);
+	bool SetActiveCommand(int cmdIndex, bool rightMouseButton);
 	bool SetActiveCommand(int cmdIndex,
-		int button, bool lmb, bool rmb,
+		int button, bool leftMouseButton, bool rightMouseButton,
 		bool alt, bool ctrl, bool meta, bool shift);
 	bool SetActiveCommand(const Action& action, const CKeySet& ks, int actionIndex);
 
@@ -77,7 +76,7 @@ public:
 	void SetBuildFacing(unsigned int facing);
 	void SetBuildSpacing(int spacing);
 
-	void PushLayoutCommand(const std::string&, bool luaCmd = true);
+	void PushLayoutCommand(const std::string& cmd, bool luaCmd = true);
 	void RunLayoutCommands();
 
 public:
@@ -92,7 +91,7 @@ private:
 	bool LayoutCustomIcons(bool useSelectionPage);
 	void ResizeIconArray(unsigned int size);
 	void AppendPrevAndNext(std::vector<CommandDescription>& cmds);
-	void ConvertCommands(std::vector<CommandDescription>&);
+	void ConvertCommands(std::vector<CommandDescription>& cmds);
 
 	int  FindInCommandPage();
 	void RevertToCmdDesc(const CommandDescription& cmdDesc, bool defaultCommand, bool samePage);
@@ -134,7 +133,7 @@ private:
 	void DrawStencilRange(const float3& pos, float radius);
 
 
-	int  IconAtPos(int x,int y);
+	int  IconAtPos(int x, int y);
 	void SetCursorIcon() const;
 
 	void LoadDefaults();
@@ -223,8 +222,6 @@ private:
 	bool hasLuaUILayoutCommands;
 };
 
-
 extern CGuiHandler* guihandler;
-
 
 #endif /* GUI_HANDLER_H */
