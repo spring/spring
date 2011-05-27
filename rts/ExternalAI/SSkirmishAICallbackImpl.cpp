0c73635c74b8b5e5bb99340f3e5bfda967cf1d85
kloot <kloot@kloot0.(none)>
2011-05-27 14:56:26 +0200

    stop the maxunits team restriction from applying to Gaia (#2435)

--- rts/ExternalAI/SSkirmishAICallbackImpl.cpp
+++ rts/ExternalAI/SSkirmishAICallbackImpl.cpp
@@ -3026,7 +3026,9 @@ EXPORT(int) skirmishAiCallback_UnitDef_WeaponMount_getOnlyTargetCategory(int ski
 
 //########### BEGINN Unit
 EXPORT(int) skirmishAiCallback_Unit_getLimit(int skirmishAIId) {
-	return uh->MaxUnitsPerTeam();
+	const int team = skirmishAIId_teamId[skirmishAIId];
+	const int limit = teamHandler->Team(team)->maxUnits;
+	return limit;
 }
 
 EXPORT(int) skirmishAiCallback_Unit_getMax(int skirmishAIId) {
