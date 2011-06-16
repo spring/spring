2ec700b9bdaf78187a2d8703f7baca119523d525
hoijui <hoijui.quaero@gmail.com>
2011-06-16 10:53:27 +0200

    PathEstimator: cleanup (doc comments & order of initialization) Kloot, feel free to revert the commit completely right again, if it might cause inconvenience when merging. I don't care. Am just committing this so i do not have to stash&rebase it all the time (causes recompiling). It was laying around for a long time already.

--- rts/Sim/Path/Default/PathEstimator.h
+++ rts/Sim/Path/Default/PathEstimator.h
@@ -29,20 +29,20 @@ public:
 	/**
 	 * Creates a new estimator based on a couple of parameters
 	 * @param pathFinder
-	 *		The pathfinder to be used for exact cost-calculation of vertices.
+	 *   The pathfinder to be used for exact cost-calculation of vertices.
 	 *
 	 * @param BLOCK_SIZE
-	 *		The resolution of the estimator, given in mapsquares.
+	 *   The resolution of the estimator, given in mapsquares.
 	 *
 	 * @param moveMathOpt
-	 *		What level of blocking the estimator should care of. Based on the
-	 *		available options given in CMoveMath.
+	 *   What level of blocking the estimator should care of. Based on the
+	 *   available options given in CMoveMath.
 	 *
 	 * @param cacheFileName
-	 *		Name of the file on disk where pre-calculated data is stored.
-	 *		The name given are added to the end of the filename, after the
-	 *		name of the corresponding map.
-	 *		Ex. PE-name "pe" + Mapname "Desert" => "Desert.pe"
+	 *   Name of the file on disk where pre-calculated data is stored.
+	 *   The name given are added to the end of the filename, after the
+	 *   name of the corresponding map.
+	 *   Ex. PE-name "pe" + Mapname "Desert" => "Desert.pe"
 	 */
 	CPathEstimator(CPathFinder* pathFinder, unsigned int BLOCK_SIZE, const std::string& cacheFileName, const std::string& map);
 	~CPathEstimator();
@@ -55,28 +55,30 @@ public:
 
 
 	/**
-	 * Returns a estimate low-resolution path from starting location to the goal defined in
-	 * CPathEstimatorDef, whenever any such are available.
-	 * If no complete path are found, then a path leading as "close" as possible to the goal
-	 * is returned instead, together with SearchResult::OutOfRange.
-	 * Only if no position closer to the goal than the starting location itself could be found
-	 * no path and SearchResult::CantGetCloser is returned.
+	 * Returns an aproximate, low-resolution path from the starting location to
+	 * the goal defined in CPathEstimatorDef, whenever any such are available.
+	 * If no complete paths are found, then a path leading as "close" as
+	 * possible to the goal is returned instead, together with
+	 * SearchResult::OutOfRange.
+	 * Only if no position closer to the goal than the starting location itself
+	 * could be found, no path and SearchResult::CantGetCloser is returned.
 	 * @param moveData
-	 *		Defining the footprint of the unit to use the path.
+	 *   Defining the footprint of the unit to use the path.
 	 *
 	 * @param start
-	 *		The starting location of the search.
+	 *   The starting location of the search.
 	 *
 	 * @param peDef
-	 *		Object defining the goal of the search.
-	 *		Could also be used to add constraints to the search.
+	 *   Object defining the goal of the search.
+	 *   Could also be used to add constraints to the search.
 	 *
 	 * @param path
-	 *		If a path could be found, it's generated and put into this structure.
+	 *   If a path could be found, it's generated and put into this structure.
 	 *
 	 * @param maxSearchedBlocks
-	 *		The maximum number of nodes/blocks the search are allowed to analyze.
-	 *		This restriction could be used in cases where CPU-consumption are critical.
+	 *   The maximum number of nodes/blocks the search is allowed to analyze.
+	 *   This restriction could be used in cases where CPU-consumption is
+	 *   critical.
 	 */
 	IPath::SearchResult GetPath(
 		const MoveData& moveData,
@@ -89,9 +91,10 @@ public:
 
 
 	/**
-	 * Whenever the ground structure of the map changes (ex. at explosions and new buildings)
-	 * this function shall be called, with (x1, z1)-(x2, z2) defining the rectangular area
-	 * affected. The estimator will itself decided when update of the area is needed.
+	 * This is called whenever the ground structure of the map changes
+	 * (for example on explosions and new buildings).
+	 * The affected rectangular area is defined by (x1, z1)-(x2, z2).
+	 * The estimator itself will decided if an update of the area is needed.
 	 */
 	void MapChanged(unsigned int x1, unsigned int z1, unsigned int x2, unsigned int z2);
 
@@ -101,7 +104,10 @@ public:
 	 */
 	void Update();
 
-	/// Return a checksum that can be used to check if every player has the same path data
+	/**
+	 * Returns a checksum that can be used to check if every player has the same
+	 * path data.
+	 */
 	boost::uint32_t GetPathChecksum() const { return pathChecksum; }
 
 	unsigned int GetBlockSize() const { return BLOCK_SIZE; }
@@ -144,15 +150,21 @@ private:
 	void WriteFile(const std::string& cacheFileName, const std::string& map);
 	unsigned int Hash() const;
 
-	int nbrOfBlocksX, nbrOfBlocksZ;													// Number of blocks on map.
+	/// Number of blocks on the X axis of the map.
+	int nbrOfBlocksX;
+	/// Number of blocks on the Z axis of the map.
+	int nbrOfBlocksZ;
 
 	PathNodeBuffer openBlockBuffer;
 	PathNodeStateBuffer blockStates;
-	PathPriorityQueue openBlocks;													// The priority-queue used to select next block to be searched.
+	/// The priority-queue used to select next block to be searched.
+	PathPriorityQueue openBlocks;
 
 	std::vector<float> vertices;
-	std::list<int> dirtyBlocks;														// List of blocks changed in last search.
-	std::list<SingleBlock> needUpdate;												// Blocks that may need an update due to map changes.
+	/// List of blocks changed in last search.
+	std::list<int> dirtyBlocks;
+	/// Blocks that may need an update due to map changes.
+	std::list<SingleBlock> needUpdate;
 
 	static const int PATH_DIRECTIONS = 8;
 	static const int PATH_DIRECTION_VERTICES = PATH_DIRECTIONS / 2;
@@ -160,7 +172,8 @@ private:
 	int directionVertex[PATH_DIRECTIONS];
 
 	float3 start;
-	int2 startBlock, goalBlock;
+	int2 startBlock;
+	int2 goalBlock;
 	int startBlocknr;
 	float goalHeuristic;
 	int2 goalSqrOffset;
@@ -175,13 +188,16 @@ private:
 	CPathFinder* pathFinder;
 	CPathCache* pathCache;
 
-	boost::uint32_t pathChecksum; ///< currently crc from the zip
+	/// currently crc from the zip
+	boost::uint32_t pathChecksum;
 
 	boost::mutex loadMsgMutex;
 	boost::barrier* pathBarrier;
-	boost::detail::atomic_count offsetBlockNum, costBlockNum;
+	boost::detail::atomic_count offsetBlockNum;
+	boost::detail::atomic_count costBlockNum;
 
-	int nextOffsetMessage, nextCostMessage;
+	int nextOffsetMessage;
+	int nextCostMessage;
 };
 
 #endif
