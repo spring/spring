936c783bf110996e94806487376ec89fd316b1a8
abma <spring@abma.de>
2011-06-18 22:54:18 +0200

    (cppcheck.sh) cleanup a bit * don't print "include not found" as gcc would already find this * add include dirs * also scan *.c * add -j2

--- buildbot/slave/cppcheck.sh
+++ buildbot/slave/cppcheck.sh
@@ -3,4 +3,8 @@ set +e
 
 # TODO: add suppressions instead of excluding rts/lib completely
 
-find rts -name \*.cpp | grep -v '^rts/lib' | cppcheck --enable=all --quiet --file-list=/dev/stdin $@
+INCLUDE="-I rts -I rts/System"
+
+find rts AI/Wrappers AI/Interfaces -name *\.cpp -or -name *\.c \
+|grep -v '^rts/lib' \
+|cppcheck --enable=style,information,unusedFunction --quiet -j2 ${INCLUDE} --file-list=/dev/stdin $@
