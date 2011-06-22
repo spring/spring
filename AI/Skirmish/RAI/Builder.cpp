82a562e4e7656393db963e37835d5ed7c3674292
abma <spring@abma.de>
2011-06-22 02:30:18 +0200

    (RAI) more compile warnings: warning: passing NULL to non-pointer argument 2 of ‘void* memset(void*, int, size_t)’ [-Wconversion-null]

--- AI/Skirmish/RAI/Builder.cpp
+++ AI/Skirmish/RAI/Builder.cpp
@@ -58,7 +58,7 @@ cBuilder::cBuilder(IAICallback* callback, cRAI* global)
 	ConMetalRate=0;
 	ConEnergyStorage=0;
 	ConMetalStorage=0;
-	memset(BQ, NULL, BUILD_QUARRY_SIZE);
+	memset(BQ, 0, BUILD_QUARRY_SIZE);
 	BuilderMetalDebug = 0;
 	BuilderEnergyDebug = 0;
 	ECostLimit = 0;
