bb0211f99b76da6bcf98599a338dddb808125150
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-26 16:28:55 +0200

    split ConfigSources into a separate file

--- rts/System/ConfigHandler.cpp
+++ rts/System/ConfigHandler.cpp
@@ -1,8 +1,23 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
+/*
+ * Stack of maps
+ *
+ * - Maps earlier in the chain override later ones
+ * - Last one contains the static defaults
+ * - First one is what is now called the "overlay"
+ * - A value identical to the value later in the chain is not stored
+ *
+ * The sources are (later sources override earlier sources):
+ * - Static defaults (might still differ per user/PC)
+ * - Sources from ConfigLocater::GetDefaultLocations()
+ * - Overlay (i.e. config values from start script)
+ */
+
 #include "StdAfx.h"
 #include "Util.h"
 #include "ConfigHandler.h"
+#include "ConfigSource.h"
 
 #ifdef WIN32
 	#include <io.h>
@@ -16,7 +31,6 @@
 #include <boost/thread/mutex.hpp>
 
 #include "Platform/ConfigLocater.h"
-#include "Platform/ScopedFileLock.h"
 #include "LogOutput.h"
 
 /******************************************************************************/
@@ -29,54 +43,6 @@ using std::vector;
 typedef map<string, string> StringMap;
 
 ConfigHandler* configHandler = NULL;
-
-/******************************************************************************/
-
-/**
- * @brief One configuration source (either a file or the overlay).
- */
-class ConfigSource
-{
-public:
-	virtual ~ConfigSource() {}
-
-	virtual bool IsSet(const string& key) const;
-	virtual string GetString(const string& key) const;
-	virtual void SetString(const string& key, const string& value);
-	virtual void Delete(const string& key);
-
-	const StringMap& GetData() const { return data; }
-
-protected:
-	StringMap data;
-};
-
-class OverlayConfigSource : public ConfigSource
-{
-};
-
-class FileConfigSource : public ConfigSource
-{
-public:
-	FileConfigSource(const string& filename);
-
-	void SetString(const string& key, const string& value);
-	void Delete(const string& key);
-
-	string GetFilename() const { return filename; }
-
-private:
-	void DeleteInternal(const string& key);
-	void ReadModifyWrite(boost::function<void ()> modify);
-
-	string filename;
-	StringMap comments;
-
-	// helper functions
-	void Read(FILE* file);
-	void Write(FILE* file);
-	char* Strip(char* begin, char* end);
-};
 
 /******************************************************************************/
 
@@ -107,159 +73,6 @@ private:
 	boost::mutex observerMutex;
 	StringMap changedValues;
 };
-
-/******************************************************************************/
-
-bool ConfigSource::IsSet(const string& key) const
-{
-	return data.find(key) != data.end();
-}
-
-string ConfigSource::GetString(const string& key) const
-{
-	StringMap::const_iterator pos = data.find(key);
-	if (pos == data.end()) {
-		throw std::runtime_error("ConfigSource: Error: Key does not exist: " + key);
-	}
-	return pos->second;
-}
-
-void ConfigSource::SetString(const string& key, const string& value)
-{
-	data[key] = value;
-}
-
-void ConfigSource::Delete(const string& key)
-{
-	data.erase(key);
-}
-
-/******************************************************************************/
-
-FileConfigSource::FileConfigSource(const string& filename) : filename(filename)
-{
-	FILE* file;
-
-	if ((file = fopen(filename.c_str(), "r"))) {
-		ScopedFileLock scoped_lock(fileno(file), false);
-		Read(file);
-	}
-	else {
-		if (!(file = fopen(filename.c_str(), "a")))
-			throw std::runtime_error("FileConfigSource: Error: Could not write to config file \"" + filename + "\"");
-	}
-	fclose(file);
-}
-
-void FileConfigSource::SetString(const string& key, const string& value)
-{
-	ReadModifyWrite(boost::bind(&ConfigSource::SetString, this, key, value));
-}
-
-void FileConfigSource::DeleteInternal(const string& key)
-{
-	ConfigSource::Delete(key);
-	comments.erase(key);
-}
-
-void FileConfigSource::Delete(const string& key)
-{
-	ReadModifyWrite(boost::bind(&FileConfigSource::DeleteInternal, this, key));
-}
-
-void FileConfigSource::ReadModifyWrite(boost::function<void ()> modify) {
-	FILE* file = fopen(filename.c_str(), "r+");
-
-	if (file) {
-		ScopedFileLock scoped_lock(fileno(file), true);
-		Read(file);
-		modify();
-		Write(file);
-	}
-	else {
-		modify();
-	}
-
-	// must be outside above 'if (file)' block because of the lock.
-	if (file) {
-		fclose(file);
-	}
-}
-
-/**
- * @brief strip whitespace
- *
- * Strips whitespace off the string [begin, end] by setting the first
- * non-whitespace character from the end to 0 and returning a pointer
- * to the first non-whitespace character from the beginning.
- *
- * Precondition: end must point to the last character of the string,
- * i.e. the one before the terminating '\0'.
- */
-char* FileConfigSource::Strip(char* begin, char* end) {
-	while (isspace(*begin)) ++begin;
-	while (end >= begin && isspace(*end)) --end;
-	*(end + 1) = '\0';
-	return begin;
-}
-
-/**
- * @brief Rewind file and re-read it.
- */
-void FileConfigSource::Read(FILE* file)
-{
-	std::ostringstream commentBuffer;
-	char line[500];
-	rewind(file);
-	while (fgets(line, sizeof(line), file)) {
-		char* line_stripped = Strip(line, strchr(line, '\0'));
-		if (*line_stripped == '\0' || *line_stripped == '#') {
-			// an empty line or a comment line
-			// note: trailing whitespace has been removed by Strip
-			commentBuffer << line << "\n";
-		}
-		else {
-			char* eq = strchr(line_stripped, '=');
-			if (eq) {
-				// a "key=value" line
-				char* key = Strip(line_stripped, eq - 1);
-				char* value = Strip(eq + 1, strchr(eq + 1, '\0') - 1);
-				data[key] = value;
-				comments[key] = commentBuffer.str();
-				// reset the ostringstream
-				commentBuffer.clear();
-				commentBuffer.str("");
-			}
-			else {
-				// neither a comment nor an empty line nor a key=value line
-				logOutput.Print("ConfigSource: Error: Can not parse line: %s\n", line);
-			}
-		}
-	}
-}
-
-/**
- * @brief Truncate file and write data to it.
- */
-void FileConfigSource::Write(FILE* file)
-{
-	rewind(file);
-#ifdef WIN32
-	int err = _chsize(fileno(file), 0);
-#else
-	int err = ftruncate(fileno(file), 0);
-#endif
-	if (err != 0) {
-		logOutput.Print("FileConfigSource: Error: Failed truncating config file.");
-	}
-	for(StringMap::const_iterator iter = data.begin(); iter != data.end(); ++iter) {
-		StringMap::const_iterator comment = comments.find(iter->first);
-		if (comment != comments.end()) {
-			fputs(comment->second.c_str(), file);
-		}
-		fprintf(file, "%s = %s\n", iter->first.c_str(), iter->second.c_str());
-	}
-}
 
 /******************************************************************************/
 
---
+++ rts/System/ConfigSource.cpp
@@ -0,0 +1,176 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#include "StdAfx.h"
+#include "ConfigSource.h"
+
+#ifdef WIN32
+	#include <io.h>
+#endif
+#include <string.h>
+#include <stdexcept>
+#include <boost/bind.hpp>
+
+#include "Platform/ScopedFileLock.h"
+#include "LogOutput.h"
+
+/******************************************************************************/
+
+using std::map;
+using std::string;
+
+typedef map<string, string> StringMap;
+
+/******************************************************************************/
+
+bool ConfigSource::IsSet(const string& key) const
+{
+	return data.find(key) != data.end();
+}
+
+string ConfigSource::GetString(const string& key) const
+{
+	StringMap::const_iterator pos = data.find(key);
+	if (pos == data.end()) {
+		throw std::runtime_error("ConfigSource: Error: Key does not exist: " + key);
+	}
+	return pos->second;
+}
+
+void ConfigSource::SetString(const string& key, const string& value)
+{
+	data[key] = value;
+}
+
+void ConfigSource::Delete(const string& key)
+{
+	data.erase(key);
+}
+
+/******************************************************************************/
+
+FileConfigSource::FileConfigSource(const string& filename) : filename(filename)
+{
+	FILE* file;
+
+	if ((file = fopen(filename.c_str(), "r"))) {
+		ScopedFileLock scoped_lock(fileno(file), false);
+		Read(file);
+	}
+	else {
+		if (!(file = fopen(filename.c_str(), "a")))
+			throw std::runtime_error("FileConfigSource: Error: Could not write to config file \"" + filename + "\"");
+	}
+	fclose(file);
+}
+
+void FileConfigSource::SetString(const string& key, const string& value)
+{
+	ReadModifyWrite(boost::bind(&ConfigSource::SetString, this, key, value));
+}
+
+void FileConfigSource::DeleteInternal(const string& key)
+{
+	ConfigSource::Delete(key);
+	comments.erase(key);
+}
+
+void FileConfigSource::Delete(const string& key)
+{
+	ReadModifyWrite(boost::bind(&FileConfigSource::DeleteInternal, this, key));
+}
+
+void FileConfigSource::ReadModifyWrite(boost::function<void ()> modify) {
+	FILE* file = fopen(filename.c_str(), "r+");
+
+	if (file) {
+		ScopedFileLock scoped_lock(fileno(file), true);
+		Read(file);
+		modify();
+		Write(file);
+	}
+	else {
+		modify();
+	}
+
+	// must be outside above 'if (file)' block because of the lock.
+	if (file) {
+		fclose(file);
+	}
+}
+
+/**
+ * @brief strip whitespace
+ *
+ * Strips whitespace off the string [begin, end] by setting the first
+ * non-whitespace character from the end to 0 and returning a pointer
+ * to the first non-whitespace character from the beginning.
+ *
+ * Precondition: end must point to the last character of the string,
+ * i.e. the one before the terminating '\0'.
+ */
+char* FileConfigSource::Strip(char* begin, char* end) {
+	while (isspace(*begin)) ++begin;
+	while (end >= begin && isspace(*end)) --end;
+	*(end + 1) = '\0';
+	return begin;
+}
+
+/**
+ * @brief Rewind file and re-read it.
+ */
+void FileConfigSource::Read(FILE* file)
+{
+	std::ostringstream commentBuffer;
+	char line[500];
+	rewind(file);
+	while (fgets(line, sizeof(line), file)) {
+		char* line_stripped = Strip(line, strchr(line, '\0'));
+		if (*line_stripped == '\0' || *line_stripped == '#') {
+			// an empty line or a comment line
+			// note: trailing whitespace has been removed by Strip
+			commentBuffer << line << "\n";
+		}
+		else {
+			char* eq = strchr(line_stripped, '=');
+			if (eq) {
+				// a "key=value" line
+				char* key = Strip(line_stripped, eq - 1);
+				char* value = Strip(eq + 1, strchr(eq + 1, '\0') - 1);
+				data[key] = value;
+				comments[key] = commentBuffer.str();
+				// reset the ostringstream
+				commentBuffer.clear();
+				commentBuffer.str("");
+			}
+			else {
+				// neither a comment nor an empty line nor a key=value line
+				logOutput.Print("ConfigSource: Error: Can not parse line: %s\n", line);
+			}
+		}
+	}
+}
+
+/**
+ * @brief Truncate file and write data to it.
+ */
+void FileConfigSource::Write(FILE* file)
+{
+	rewind(file);
+#ifdef WIN32
+	int err = _chsize(fileno(file), 0);
+#else
+	int err = ftruncate(fileno(file), 0);
+#endif
+	if (err != 0) {
+		logOutput.Print("FileConfigSource: Error: Failed truncating config file.");
+	}
+	for(StringMap::const_iterator iter = data.begin(); iter != data.end(); ++iter) {
+		StringMap::const_iterator comment = comments.find(iter->first);
+		if (comment != comments.end()) {
+			fputs(comment->second.c_str(), file);
+		}
+		fprintf(file, "%s = %s\n", iter->first.c_str(), iter->second.c_str());
+	}
+}
+
+/******************************************************************************/
