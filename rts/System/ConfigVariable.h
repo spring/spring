518d6bb620920a74180d527fc68e9995a9033ee8
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-06-28 00:47:58 +0200

    add location of declaration to ConfigVariable

--- rts/System/ConfigVariable.h
+++ rts/System/ConfigVariable.h
@@ -16,8 +16,8 @@ class ConfigVariable
 {
 public:
 	template<typename T>
-	ConfigVariable(const std::string& key, const T& value, const std::string& comment = ""):
-			next(head), key(key), defaultValue(ToString(value)), comment(comment), hasDefaultValue(true)
+	ConfigVariable(const std::string& key, const T& value, const std::string& comment, const char* declFile, int declLine) :
+		next(head), key(key), defaultValue(ToString(value)), comment(comment), hasDefaultValue(true), declarationFile(declFile), declarationLine(declLine)
 	{
 		head = this;
 	}
@@ -31,27 +31,28 @@ private:
 		return buffer.str();
 	}
 
+public:
 	static ConfigVariable* head;
 
-private:
-	ConfigVariable* next;
-	std::string key;
-	std::string defaultValue;
-	std::string comment;
-	bool hasDefaultValue;
+public:
+	const ConfigVariable* next;
+	const std::string key;
+	const std::string defaultValue;
+	const std::string comment;
+	const bool hasDefaultValue;
 
-	friend class ConfigHandler;
+	const char* const declarationFile;
+	const int declarationLine;
 };
 
 // note: explicit specialization has to be at the namespace scope
-template<>
-inline ConfigVariable::ConfigVariable<cfg::_NoDefault>(const std::string& key, const cfg::_NoDefault& value, const std::string& comment):
-		next(head), key(key), comment(comment), hasDefaultValue(false)
+template<> inline ConfigVariable::ConfigVariable<cfg::_NoDefault>(const std::string& key, const cfg::_NoDefault& value, const std::string& comment, const char* declFile, int declLine) :
+	next(head), key(key), comment(comment), hasDefaultValue(false), declarationFile(declFile), declarationLine(declLine)
 {
 	head = this;
 }
 
 #define CONFIG(name, defVal) \
-	static ConfigVariable cfg##name(#name, defVal)
+	static ConfigVariable cfg##name(#name, defVal, "", __FILE__, __LINE__)
 
 #endif // CONFIG_VALUE_H
