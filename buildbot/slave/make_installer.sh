131b4891375665717889e55861a6e8f1232dc124
abma <spring@abma.de>
2011-06-30 01:20:51 +0200

    (buildbot) run "make install" and create portable archive (excluding AI/, ArchiveMover, headless, dedicated) *<version>_portable.7z should be directly runable *AIInterface + ArchiveMover is also stripped *moved all non-rsync-code to make_installer.sh *added bw-limit to rsync (about 25% lower than max seen speed)

--- buildbot/slave/make_installer.sh
+++ buildbot/slave/make_installer.sh
@@ -2,15 +2,34 @@
 set -e
 . buildbot/slave/prepare.sh
 
+
+DEST=${TMP_BASE}/${CONFIG}/inst
+INSTALLDIR=${DEST}/${BUILDDIR}
+
+#Ultra settings
+SEVENZIP="7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on"
+
 MINGWLIBS_PATH=${1}
 MINGW_HOST=i586-mingw32msvc-
 
-echo "!define MINGWLIBS_DIR \"${MINGWLIBS_PATH}\"" > installer/custom_defines.nsi
-echo "!define BUILD_DIR \"${BUILDDIR}\"" >> installer/custom_defines.nsi
+make install DESTDIR=${DEST}
+
+function zip() {
+	file=$1
+	name=$2
+	if [ -f ${file} ]; then
+		debugfile=${file%.*}.dbg
+		archive="${TMP_PATH}/${VERSION}_${name}.7z"
+		archive_debug="${TMP_PATH}/${VERSION}_${name}_dbg.7z"
+		${SEVENZIP} "${archive}" ${file}
+		[ ! -f ${debugfile} ] || ${SEVENZIP} "${archive_debug}" ${debugfile}
+	fi
+}
 
 #strip symbols and archive them
-cd ${BUILDDIR}
-for tostripfile in spring.exe spring-dedicated.exe spring-multithreaded.exe spring-headless.exe unitsync.dll $(find AI/Skirmish -name SkirmishAI.dll); do
+cd ${INSTALLDIR}
+EXECUTABLES="spring.exe spring-dedicated.exe spring-multithreaded.exe spring-headless.exe unitsync.dll ArchiveMover.exe $(find AI/Skirmish -name SkirmishAI.dll) $(find AI/Interfaces -name AIInterface.dll)"
+for tostripfile in ${EXECUTABLES}; do
 	if [ -f ${tostripfile} ]; then
 		# dont strip binaries that we processed earlier
 		if ! ${MINGW_HOST}objdump -h ${tostripfile} | grep -q .gnu_debuglink; then
@@ -24,10 +43,36 @@ for tostripfile in spring.exe spring-dedicated.exe spring-multithreaded.exe spri
 		fi
 	fi
 done
+
+mkdir -p ${TMP_PATH}
+
+#create portable spring
+touch ${INSTALLDIR}/springsettings.cfg
+${SEVENZIP} ${TMP_PATH}/${VERSION}_portable.7z ${INSTALLDIR}/* -x!AI -x!spring-dedicated.exe -x!spring-headless.exe -x!ArchiveMover.exe -x!*.dbg
+
+#create archives
+for tocompress in ${EXECUTABLES}; do
+	zip ${tocompress} $(basename ${tocompress})
+done
+
+#create 7z's for AI's and Interface
+for TYPE in $(find ${INSTALLDIR}/AI -maxdepth 1 -mindepth 1 -type d) ; do
+	echo ${TYPE}
+	type=$(basename ${TYPE})
+	echo "Packing ${type}"
+	for DIR in $(find ${TYPE} -maxdepth 1 -mindepth 1 -type d); do
+		NAME=$(basename ${DIR})
+		${SEVENZIP} ${TMP_PATH}/$(basename ${VERSION}_${type}-${NAME}.7z) ${DIR} -xr!*.dbg
+	done
+done
+
 cd $OLDPWD
+
+echo "!define MINGWLIBS_DIR \"${MINGWLIBS_PATH}\"" > installer/custom_defines.nsi
+echo "!define BUILD_DIR \"${BUILDDIR}\"" >> installer/custom_defines.nsi
 
 # The 0 means: do not define DIST_DIR
 ./installer/make_installer.pl 0
 
-mkdir -p ${TMP_PATH}
 mv ./installer/spring*.exe ${TMP_PATH}
+
