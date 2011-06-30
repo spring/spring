d4665bbe8b902dcd4e66d7d6a3ac74549867530d
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-07-01 00:57:40 +0200

    Fluent interface for declaring config vars

--- rts/System/Config/ConfigVariable.h
+++ rts/System/Config/ConfigVariable.h
@@ -3,32 +3,128 @@
 #ifndef CONFIG_VALUE_H
 #define CONFIG_VALUE_H
 
-namespace cfg
+#include <assert.h>
+#include <boost/utility.hpp>
+#include <sstream>
+
+/**
+ * @brief Wraps a value and detects whether it has been assigned to.
+ */
+template<typename T>
+class Nullable : public boost::noncopyable
 {
-	class _NoDefault {};
-	const _NoDefault NoDefault = _NoDefault();
+public:
+	Nullable() : isSet(false) {}
+	void operator=(const T& x) { value = x; isSet = true; }
+	const T& Get() const { return value; }
+	bool IsSet() const { return isSet; }
+
+private:
+	T value;
+	bool isSet;
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, const Nullable<T>& o)
+{
+	if (o.IsSet()) {
+		stream << o.Get();
+	}
+	return stream;
 }
 
 /**
- * @brief Configuration value declaration
+ * @brief Data members for config variable that don't depend on type.
+ */
+class ConfigVariableData : public boost::noncopyable
+{
+public:
+	virtual ~ConfigVariableData() {}
+	virtual std::string GetDefaultValue() const = 0;
+	virtual bool HasDefaultValue() const = 0;
+
+public:
+	std::string key;
+	const char* declarationFile;
+	int declarationLine;
+	Nullable<std::string> description;
+};
+
+/**
+ * @brief Data members for config variable that do depend on type.
+ */
+template<typename T>
+class ConfigVariableTypedData : public ConfigVariableData
+{
+public:
+	std::string GetDefaultValue() const
+	{
+		std::ostringstream buf;
+		buf << defaultValue;
+		return buf.str();
+	}
+
+	bool HasDefaultValue() const
+	{
+		return defaultValue.IsSet();
+	}
+
+public:
+	Nullable<T> defaultValue;
+	Nullable<T> minimumValue;
+	Nullable<T> maximumValue;
+};
+
+/**
+ * @brief Fluent interface to construct ConfigVariables
+ * Uses method chaining.
+ */
+template<typename T>
+class ConfigVariableBuilder : public boost::noncopyable
+{
+public:
+	ConfigVariableBuilder(ConfigVariableTypedData<T>& data) : data(&data) {}
+	const ConfigVariableData* GetData() const { return data; }
+
+#define MAKE_CHAIN_METHOD(property, type) \
+	ConfigVariableBuilder& property(type const& x) { \
+		data->property = x; \
+		return *this; \
+	}
+
+	MAKE_CHAIN_METHOD(key, std::string);
+	MAKE_CHAIN_METHOD(declarationFile, const char*);
+	MAKE_CHAIN_METHOD(declarationLine, int);
+	MAKE_CHAIN_METHOD(description, std::string);
+	MAKE_CHAIN_METHOD(defaultValue, T);
+	MAKE_CHAIN_METHOD(minimumValue, T);
+	MAKE_CHAIN_METHOD(maximumValue, T);
+
+#undef MAKE_CHAIN_METHOD
+
+private:
+	ConfigVariableTypedData<T>* data;
+};
+
+/**
+ * @brief Configuration variable meta data
  */
 class ConfigVariable
 {
 public:
+	/// @brief Implicit conversion from ConfigVariableBuilder<T>.
 	template<typename T>
-	ConfigVariable(const std::string& key, const T& value, const std::string& comment, const char* declFile, int declLine) :
-		next(head), key(key), defaultValue(ToString(value)), comment(comment), hasDefaultValue(true), declarationFile(declFile), declarationLine(declLine)
+	ConfigVariable(const ConfigVariableBuilder<T>& builder) :
+		next(NULL), data(builder.GetData())
 	{
-		head = this;
 	}
 
-private:
-	template<typename T>
-	static std::string ToString(const T& value)
+	/// @brief Copy constructor
+	ConfigVariable(const ConfigVariable& other) :
+		next(head), data(other.data)
 	{
-		std::ostringstream buffer;
-		buffer << value;
-		return buffer.str();
+		assert(other.next == NULL);
+		head = this;
 	}
 
 public:
@@ -36,23 +132,16 @@ public:
 
 public:
 	const ConfigVariable* next;
-	const std::string key;
-	const std::string defaultValue;
-	const std::string comment;
-	const bool hasDefaultValue;
+	const ConfigVariableData* data;
 
-	const char* const declarationFile;
-	const int declarationLine;
+private:
+	/// @brief No assignment allowed
+	ConfigVariable& operator=(const ConfigVariable&);
 };
 
-// note: explicit specialization has to be at the namespace scope
-template<> inline ConfigVariable::ConfigVariable<cfg::_NoDefault>(const std::string& key, const cfg::_NoDefault& value, const std::string& comment, const char* declFile, int declLine) :
-	next(head), key(key), comment(comment), hasDefaultValue(false), declarationFile(declFile), declarationLine(declLine)
-{
-	head = this;
-}
-
-#define CONFIG(name, defVal) \
-	static ConfigVariable cfg##name(#name, defVal, "", __FILE__, __LINE__)
+#define CONFIG(T, name) \
+	static ConfigVariableTypedData<T> cfgdata##name; \
+	static const ConfigVariable cfg##name = ConfigVariableBuilder<T>(cfgdata##name) \
+		.key(#name).declarationFile(__FILE__).declarationLine(__LINE__)
 
 #endif // CONFIG_VALUE_H
