261a667fbb48d8716e52148d54b9ef689d8793e5
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-07-01 00:57:40 +0200

    Fluent interface for declaring config vars

--- rts/System/Config/DefaultConfigSource.cpp
+++ rts/System/Config/DefaultConfigSource.cpp
@@ -9,23 +9,25 @@ using std::map;
 using std::string;
 
 #define LOG_VAR(var) \
-	LogObject() << (var)->declarationFile << ":" << (var)->declarationLine << ": "
+	LogObject() << (var)->data->declarationFile << ":" << (var)->data->declarationLine << ": "
 
 DefaultConfigSource::DefaultConfigSource()
 {
 	for (const ConfigVariable* var = ConfigVariable::head; var != NULL; var = var->next) {
-		LOG_VAR(var) << "ConfigVariable var(\"" << var->key << "\", \"" << var->defaultValue << "\")\n";
+		LOG_VAR(var) << "ConfigVariable var(\"" << var->data->key << "\", \"" << var->data->GetDefaultValue() << "\")\n";
 
-		map<string, const ConfigVariable*>::const_iterator pos = vars.find(var->key);
+		map<string, const ConfigVariable*>::const_iterator pos = vars.find(var->data->key);
 
 		if (pos != vars.end()) {
-			LOG_VAR(var) << "Duplicate config variable declaration \"" << var->key << "\"\n";
+			LOG_VAR(var) << "Duplicate config variable declaration \"" << var->data->key << "\"\n";
 			LOG_VAR(pos->second) << "  Previously declared here\n";
 		}
 		else {
-			vars[var->key] = var;
+			vars[var->data->key] = var;
 		}
 
-		data[var->key] = var->defaultValue;
+		if (var->data->HasDefaultValue()) {
+			data[var->data->key] = var->data->GetDefaultValue();
+		}
 	}
 }
