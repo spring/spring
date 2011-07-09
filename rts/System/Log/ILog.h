59554149355694024a8277f3a56c79c7d8822ddd
hoijui <hoijui.quaero@gmail.com>
2011-07-09 12:13:48 +0200

    log DefaultFilter refactor: allow to set per section minimum log level

--- rts/System/Log/ILog.h
+++ rts/System/Log/ILog.h
@@ -45,18 +45,13 @@ extern "C" {
 // Connection to the non-pre-processor part of the frontend
 
 /**
- * Allows the sink to manage a minimal log level too.
+ * Allows the global filter to manage a minimal log level too,
+ * optionally per section.
  * This will only ever be called for levels higher then the minimal level set
- * during compile-time (_LOG_LEVEL).
+ * during compile-time (_LOG_LEVEL_MIN), and if the section is not already
+ * disabled during compile-time.
  */
-extern bool log_frontend_isLevelEnabled(int level);
-
-/**
- * Allows the sink to manage a minimal log section too.
- * This will only ever be called if the section is not already disabled
- * during compile-time.
- */
-extern bool log_frontend_isSectionEnabled(const char* section);
+extern bool log_frontend_isEnabled(const char* section, int level);
 
 
 // format string error checking
@@ -71,8 +66,7 @@ extern bool log_frontend_isSectionEnabled(const char* section);
  * The main connection to the backend/sink.
  * This will receive all log messages that are not disabled at compile-time
  * already, so it has to check internally, whether the criteria for logging
- * are really met, so it will have to call log_frontend_isLevelEnabled() and
- * log_frontend_isSectionEnabled() internally.
+ * are really met, so it will have to call log_frontend_isEnabled() internally.
  */
 extern void log_frontend_record(const char* section, int level, const char* fmt,
 		...) FORMAT_STRING(3);
@@ -82,74 +76,25 @@ extern void log_frontend_record(const char* section, int level, const char* fmt,
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-// Level
+// Level & Section
 
-/**
- * Only stuff from in this doc-group should ever be used by clients of this API.
- *
- * @group logging_api
- * @{
- */
-
-#define LOG_LEVEL_ALL       0
-
-#define LOG_LEVEL_DEBUG    20
-#define LOG_LEVEL_INFO     30
-#define LOG_LEVEL_WARNING  40
-#define LOG_LEVEL_ERROR    50
-#define LOG_LEVEL_FATAL    60
-
-#define LOG_LEVEL_NONE    255
-
-/** @} */ // group logging_api
-
-
-#define _LOG_LEVEL_DEFAULT LOG_LEVEL_ALL
-
-/**
- * Minimal log level (compile-time).
- * This is the compile-time version of this setting;
- * there might be a more restrictive one at runtime.
- * Initialize to the default (may be overriden with -DLOG_LEVEL=X).
- */
-#ifndef _LOG_LEVEL
-	#define _LOG_LEVEL _LOG_LEVEL_DEFAULT
-#endif
-
+#include "Level.h"
 
 #define _LOG_IS_ENABLED_LEVEL_STATIC(level) \
-	(LOG_LEVE##level >= _LOG_LEVEL)
-
-#define _LOG_IS_ENABLED_LEVEL_RUNTIME(level) \
-	log_frontend_isLevelEnabled(LOG_LEVE##level)
+	(LOG_LEVE##level >= _LOG_LEVEL_MIN)
 
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-// Section
-
-#define LOG_SECTION_DEFAULT NULL
-
-/**
- * The current log section.
- * Initialize to the default.
- * You might set a custom section by defining LOG_SECTION before including
- * this header, or by redefining LOG_SECTION (#undef & #define).
- */
-#ifndef LOG_SECTION
-	#define LOG_SECTION LOG_SECTION_DEFAULT
-#endif
+#include "Section.h"
 
 // enable all log sections at compile-time
 #define _LOG_IS_ENABLED_SECTION_STATIC(section) \
 	true
 #define _LOG_IS_ENABLED_SECTION_DEFINED_STATIC(section) \
-	_LOG_IS_ENABLED_SECTION_STATIC(LOG_SECTION)
+	_LOG_IS_ENABLED_SECTION_STATIC(LOG_SECTION_CURRENT)
 
-#define _LOG_IS_ENABLED_SECTION_RUNTIME(section) \
-	log_frontend_isSectionEnabled(section)
-#define _LOG_IS_ENABLED_SECTION_DEFINED_RUNTIME(section) \
-	_LOG_IS_ENABLED_SECTION_RUNTIME(LOG_SECTION)
+
+#define _LOG_IS_ENABLED_RUNTIME(section, level) \
+	log_frontend_isEnabled(section, LOG_LEVE##level)
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -223,21 +168,24 @@ extern void log_frontend_record(const char* section, int level, const char* fmt,
 
 /// Uses the section defined in LOG_SECTION
 #define _LOG_SECTION_DEFINED(level, fmt, ...) \
-	_LOG_SECTION(LOG_SECTION, level, fmt, ##__VA_ARGS__)
+	_LOG_SECTION(LOG_SECTION_CURRENT, level, fmt, ##__VA_ARGS__)
 
 /// Entry point for frontend-internal processing
 #define _LOG(level, fmt, ...) \
 	_LOG_SECTION_DEFINED(level, fmt, ##__VA_ARGS__)
 
 
-#define _LOG_IS_ENABLED_STATIC(level) \
+#define _LOG_IS_ENABLED_STATIC_S(section, level) \
 	(  _LOG_IS_ENABLED_LEVEL_STATIC(level) \
-	&& _LOG_IS_ENABLED_SECTION_DEFINED_STATIC())
+	&& _LOG_IS_ENABLED_SECTION_STATIC(section))
+#define _LOG_IS_ENABLED_STATIC(level) \
+	_LOG_IS_ENABLED_STATIC_S(LOG_SECTION_CURRENT, level)
 
-#define _LOG_IS_ENABLED(level) \
+#define _LOG_IS_ENABLED_S(section, level) \
 	(  _LOG_IS_ENABLED_STATIC(level) \
-	&& _LOG_IS_ENABLED_LEVEL_RUNTIME(level) \
-	&& _LOG_IS_ENABLED_SECTION_DEFINED_RUNTIME())
+	&& _LOG_IS_ENABLED_RUNTIME(section, level))
+#define _LOG_IS_ENABLED(level) \
+	_LOG_IS_ENABLED_S(LOG_SECTION_CURRENT, level)
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -263,10 +211,20 @@ extern void log_frontend_record(const char* section, int level, const char* fmt,
  *   LOG(L_DEBUG, "the stats: %i", statistics);
  * #endif
  * </code>
+ * @see LOG_IS_ENABLED_STATIC_S
  * @see LOG_IS_ENABLED
  */
 #define LOG_IS_ENABLED_STATIC(level) \
 	_LOG_IS_ENABLED_STATIC(level)
+
+/**
+ * Returns whether logging for a specific section and the supplied level is
+ * enabled at compile-time.
+ * @see LOG_IS_ENABLED_STATIC
+ * @see LOG_IS_ENABLED_S
+ */
+#define LOG_IS_ENABLED_STATIC_S(section, level) \
+	_LOG_IS_ENABLED_STATIC_S(section, level)
 
 /**
  * Returns whether logging for the current section and the supplied level is
@@ -278,10 +236,21 @@ extern void log_frontend_record(const char* section, int level, const char* fmt,
  *   LOG(L_DEBUG, "the stats: %i", statistics);
  * }
  * </code>
+ * @see LOG_IS_ENABLED_S
  * @see LOG_IS_ENABLED_STATIC
  */
 #define LOG_IS_ENABLED(level) \
 	_LOG_IS_ENABLED(level)
+
+/**
+ * Returns whether logging for a specific section and the supplied level is
+ * enabled at run-time.
+ * @see LOG_IS_ENABLED
+ * @see LOG_IS_ENABLED_STATIC_S
+ */
+#define LOG_IS_ENABLED_S(section, level) \
+	_LOG_IS_ENABLED_S(section, level)
+
 
 /**
  * Registers a log message with level INFO.
