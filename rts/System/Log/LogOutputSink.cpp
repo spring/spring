ba16cff5e0f8f1e52592baa0634bb9a71354ab90
hoijui <hoijui.quaero@gmail.com>
2011-07-09 12:16:53 +0200

    print the log section as prefix, except the default section (examples: "[unitsync] ...", "Error: [GML] ...")

--- rts/System/Log/LogOutputSink.cpp
+++ rts/System/Log/LogOutputSink.cpp
@@ -24,15 +24,23 @@ extern "C" {
 void log_sink_record(const char* section, int level, const char* fmt,
 		va_list arguments)
 {
+	std::string fmtPrefix;
+	// HACK this stuff should be done later, closer to the point where it is written to a file or the console
 	if (level != LOG_LEVEL_INFO) {
-		// HACK this should be done later, closer to the point where it is written to a file or the console
 		const char* levelStr = log_levelToString(level);
-		const std::string prefixedFmt = std::string(levelStr) + ": " + fmt;
-		logOutput.Printv(logOutput.GetDefaultLogSubsystem(),
-				prefixedFmt.c_str(), arguments);
-	} else {
-		logOutput.Printv(logOutput.GetDefaultLogSubsystem(), fmt, arguments);
+		fmtPrefix += std::string(levelStr) + ": ";
 	}
+	if (section != LOG_SECTION_DEFAULT) {
+		section = log_prepareSection(section);
+		fmtPrefix += std::string("[") + section + "] ";
+	}
+
+	if (!fmtPrefix.empty()) {
+		fmtPrefix += fmt;
+		fmt = fmtPrefix.c_str();
+	}
+
+	logOutput.Printv(logOutput.GetDefaultLogSubsystem(), fmt, arguments);
 }
 
 /** @} */ // group logging_sink_logOutput
