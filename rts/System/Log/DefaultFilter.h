29b8c103dac7a0419d668db797f91fb9896acfe5
hoijui <hoijui.quaero@gmail.com>
2011-07-14 23:33:28 +0200

    log: make use of std::string in DefaultFilter (thanks Tobi)

--- rts/System/Log/DefaultFilter.h
+++ rts/System/Log/DefaultFilter.h
@@ -84,13 +84,14 @@ const char* log_filter_section_getRegisteredIndex(int index);
 
 #ifdef __cplusplus
 #include <set>
+#include <string>
 
 /**
  * Returns the registered sections.
  * This is simply to be more C++ friendly.
  * @see #log_filter_section_getRegistered
  */
-std::set<const char*> log_filter_section_getRegisteredSet();
+const std::set<std::string>& log_filter_section_getRegisteredSet();
 #endif
 
 /** @} */ // group logging_filter_defaultFilter_control
