740ae6e943c8622fc4bdb1fc123bd17304c348c8
hoijui <hoijui.quaero@gmail.com>
2011-07-15 12:53:45 +0200

    make FileFilter.cpp compile again with CMake [fix]

--- rts/System/FileSystem/FileFilter.cpp
+++ rts/System/FileSystem/FileFilter.cpp
@@ -1,5 +1,7 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
-#include "StdAfx.h"
+
+#include "System/StdAfx.h"
+
 #include "FileFilter.h"
 
 #include "System/mmgr.h"
