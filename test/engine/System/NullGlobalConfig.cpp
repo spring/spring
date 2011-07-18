b030de33975d4386257f70b0c92f21528c138a6a
hoijui <hoijui.quaero@gmail.com>
2011-07-18 22:23:17 +0200

    testing: initial commit; includes tests for UDPListener::TryBindSocket [CMake] See test/README.md for usage instructions.

---
+++ test/engine/System/NullGlobalConfig.cpp
@@ -0,0 +1,41 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#include "System/GlobalConfig.h"
+
+#include <cstddef>
+
+GlobalConfig* globalConfig = NULL;
+
+GlobalConfig::GlobalConfig() {
+
+	initialNetworkTimeout = 30;
+	networkTimeout = 120;
+	reconnectTimeout = 15;
+	mtu = 1400;
+	teamHighlight = 1;
+
+	linkOutgoingBandwidth = 64 * 1024;
+	linkIncomingSustainedBandwidth = 2;
+	linkIncomingPeakBandwidth = 32;
+	linkIncomingMaxPacketRate = 64;
+	linkIncomingMaxWaitingPackets = 512;
+	if ((linkIncomingSustainedBandwidth > 0) && (linkIncomingPeakBandwidth < linkIncomingSustainedBandwidth)) {
+		linkIncomingPeakBandwidth = linkIncomingSustainedBandwidth;
+	}
+	if ((linkIncomingPeakBandwidth > 0) && (linkIncomingSustainedBandwidth <= 0)) {
+		linkIncomingSustainedBandwidth = linkIncomingPeakBandwidth;
+	}
+	if ((linkIncomingMaxPacketRate > 0) && (linkIncomingSustainedBandwidth <= 0)) {
+		linkIncomingSustainedBandwidth = linkIncomingPeakBandwidth = 1024 * 1024;
+	}
+}
+
+void GlobalConfig::Instantiate() {
+	Deallocate();
+	globalConfig = new GlobalConfig();
+}
+
+void GlobalConfig::Deallocate() {
+	delete globalConfig;
+	globalConfig = NULL;
+}
