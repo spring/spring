6bcfae370cf847cb2b591b8483ce44e91e0a73d2
hoijui <hoijui.quaero@gmail.com>
2011-07-24 13:45:12 +0200

    cleanup CREG (no logic changes)

--- rts/System/creg/ISerializer.h
+++ rts/System/creg/ISerializer.h
@@ -12,24 +12,25 @@ namespace creg
 	{
 	public:
 		virtual ~ISerializer();
-		virtual bool IsWriting () = 0;
+		virtual bool IsWriting() = 0;
 
 		/// Serialize a memory buffer
-		virtual void Serialize (void *data, int byteSize) = 0;
+		virtual void Serialize(void* data, int byteSize) = 0;
 
 		/// Serialize integer value - char, short or int
-		virtual void SerializeInt (void *data, int byteSize) = 0;
+		virtual void SerializeInt(void* data, int byteSize) = 0;
 
 		/// Serialize a pointer to an instance of a creg registered class/struct
-		virtual void SerializeObjectPtr (void **ptr, Class *objectClass) = 0;
+		virtual void SerializeObjectPtr(void** ptr, Class* objectClass) = 0;
 		
 		/** Serialize an instance of an object, could be a structure embedded into another object
 		 * The big difference with SerializeObjectPtr, is that the caller of this function
-		 * controls the allocation of the object instead of the creg serializer itself */
-		virtual void SerializeObjectInstance (void *inst, Class *objectClass) = 0;
+		 * controls the allocation of the object instead of the creg serializer itself
+		 */
+		virtual void SerializeObjectInstance(void* inst, Class* objectClass) = 0;
 
 		// Add a callback that will be called after all serialization is done
-		virtual void AddPostLoadCallback (void (*cb)(void *userdata), void *userdata) = 0;
+		virtual void AddPostLoadCallback(void (*cb)(void* userdata), void* userdata) = 0;
 	};
 
 }
--- rts/System/creg/STL_Deque.h
+++ rts/System/creg/STL_Deque.h
@@ -11,12 +11,12 @@
 
 namespace creg
 {
-	// Deque type (uses vector implementation)
+	/// Deque type (uses vector implementation)
 	template<typename T>
-	struct DeduceType < std::deque <T> > {
-		boost::shared_ptr<IType> Get () {
+	struct DeduceType< std::deque <T> > {
+		boost::shared_ptr<IType> Get() {
 			DeduceType<T> elemtype;
-			return boost::shared_ptr<IType>(new DynamicArrayType < std::deque<T> > (elemtype.Get()));
+			return boost::shared_ptr<IType>(new DynamicArrayType< std::deque<T> >(elemtype.Get()));
 		}
 	};
 };
