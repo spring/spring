d6755ce4574aeee196708be2fd3884310bd63c56
Kaylewt <kloot@kloot0.(none)>
2011-08-04 23:19:51 +0200

    Lua: no asserts for internal math calls

--- rts/lib/lua/src/lauxlib.cpp
+++ rts/lib/lua/src/lauxlib.cpp
@@ -180,13 +180,21 @@ LUALIB_API lua_Number luaL_checknumber (lua_State *L, int narg) {
   if (d == 0 && !lua_isnumber(L, narg))  /* avoid extra test when d is not 0 */
     tag_error(L, narg, LUA_TNUMBER);
 
-  //Spring
-  // this is used by luaL_optnumber, luaL_optfloat (via luaL_optnumber),
-  // and luaL_checkfloat, so the asserts should cover 90% of all cases
-  // in which non-numbers can infect the engine -- lua_tofloat asserts
-  // take care of the rest
+  // SPRING
+  //   this is used by luaL_optnumber, luaL_optfloat (via luaL_optnumber),
+  //   and luaL_checkfloat, so the asserts should cover 90% of all cases
+  //   in which non-numbers can infect the engine -- lua_tofloat asserts
+  //   take care of the rest
   assert(!math::isinf(d));
   assert(!math::isnan(d));
+
+  return d;
+}
+
+LUALIB_API lua_Number luaL_checknumber_noassert (lua_State *L, int narg) {
+  lua_Number d = lua_tonumber(L, narg);
+  if (d == 0 && !lua_isnumber(L, narg))  /* avoid extra test when d is not 0 */
+    tag_error(L, narg, LUA_TNUMBER);
 
   return d;
 }
