38fb6709df6a053db4d16aeb6398e1f560f79a39
Tobi Vollebregt <tobivollebregt@gmail.com>
2011-08-07 10:38:25 +0200

    buildbot/cppcheck: increase timeout, pass -j2 only once

--- buildbot/slave/cppcheck.sh
+++ buildbot/slave/cppcheck.sh
@@ -3,4 +3,4 @@ set +e
 
 find rts AI/Wrappers AI/Interfaces -name *\.cpp -or -name *\.c \
 |grep -v '^rts/lib' \
-|cppcheck --force --enable=style,information,unusedFunction --quiet -j2 -I rts --file-list=/dev/stdin --suppressions-list=buildbot/slave/cppcheck.supress $@
+|cppcheck --force --enable=style,information,unusedFunction --quiet -I rts --file-list=/dev/stdin --suppressions-list=buildbot/slave/cppcheck.supress $@
