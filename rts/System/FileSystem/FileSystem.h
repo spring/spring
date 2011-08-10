54d579d499c6d24c72be5123374e5d62694227ed
hoijui <hoijui.quaero@gmail.com>
2011-08-10 16:13:23 +0200

    FileSystem use common indentation [minor]

--- rts/System/FileSystem/FileSystem.h
+++ rts/System/FileSystem/FileSystem.h
@@ -23,88 +23,88 @@
  */
 class FileSystem : public FileSystemAbstraction
 {
-	public:
+public:
 
-		/**
-		 * @brief remove a file
-		 *
-		 * Operates on the current working directory.
-		 */
-		static bool Remove(std::string file);
+	/**
+	 * @brief remove a file
+	 *
+	 * Operates on the current working directory.
+	 */
+	static bool Remove(std::string file);
 
-		/// @name meta-data
-		///@{
-		/// Returns true if the file exists, and is not a directory
-		static bool FileExists(std::string path);
-		/**
-		 * @brief get filesize
-		 *
-		 * @return the filesize or 0 if the file doesn't exist.
-		 */
-		static size_t GetFileSize(std::string path);
-		///@}
+	/// @name meta-data
+	///@{
+	/// Returns true if the file exists, and is not a directory
+	static bool FileExists(std::string path);
+	/**
+	 * @brief get filesize
+	 *
+	 * @return the filesize or 0 if the file doesn't exist.
+	 */
+	static size_t GetFileSize(std::string path);
+	///@}
 
-		/// @name directory
-		///@{
-		/**
-		 * @brief creates a directory recursively
-		 *
-		 * Works like mkdir -p, ie. attempts to create parent directories too.
-		 * Operates on the current working directory.
-		 * @return true if the postcondition of this function is that dir exists
-		 *   in the write directory.
-		 */
-		static bool CreateDirectory(std::string dir);
-		///@}
+	/// @name directory
+	///@{
+	/**
+	 * @brief creates a directory recursively
+	 *
+	 * Works like mkdir -p, ie. attempts to create parent directories too.
+	 * Operates on the current working directory.
+	 * @return true if the postcondition of this function is that dir exists
+	 *   in the write directory.
+	 */
+	static bool CreateDirectory(std::string dir);
+	///@}
 
-		/// @name convenience
-		///@{
-		/**
-		 * @brief Returns the directory part of a path
-		 * "/home/user/.spring/test.txt" -> "/home/user/.spring/"
-		 * "test.txt" -> ""
-		 */
-		static std::string GetDirectory(const std::string& path);
-		/**
-		 * @brief Returns the filename part of a path
-		 * "/home/user/.spring/test.txt" -> "test.txt"
-		 */
-		static std::string GetFilename(const std::string& path);
-		/**
-		 * @brief Returns the basename part of a path
-		 * This is equvalent to the filename without extension.
-		 * "/home/user/.spring/test.txt" -> "test"
-		 */
-		static std::string GetBasename(const std::string& path);
-		/**
-		 * @brief Returns the extension of the filename part of the path
-		 * "/home/user/.spring/test.txt" -> "txt"
-		 * "/home/user/.spring/test.txt..." -> "txt"
-		 * "/home/user/.spring/test.txt. . ." -> "txt"
-		 */
-		static std::string GetExtension(const std::string& path);
-		/**
-		 * @brief Converts a glob expression to a regex
-		 * @param glob string containing glob
-		 * @return string containing regex
-		 */
-		static std::string ConvertGlobToRegex(const std::string& glob);
-		/**
-		 * Converts all slashes and backslashes in path to the
-		 * native_path_separator.
-		 */
-		static std::string& FixSlashes(std::string& path);
-		/**
-		 * @brief converts backslashes in path to forward slashes
-		 */
-		static std::string& ForwardSlashes(std::string& path);
-		///@}
+	/// @name convenience
+	///@{
+	/**
+	 * @brief Returns the directory part of a path
+	 * "/home/user/.spring/test.txt" -> "/home/user/.spring/"
+	 * "test.txt" -> ""
+	 */
+	static std::string GetDirectory(const std::string& path);
+	/**
+	 * @brief Returns the filename part of a path
+	 * "/home/user/.spring/test.txt" -> "test.txt"
+	 */
+	static std::string GetFilename(const std::string& path);
+	/**
+	 * @brief Returns the basename part of a path
+	 * This is equvalent to the filename without extension.
+	 * "/home/user/.spring/test.txt" -> "test"
+	 */
+	static std::string GetBasename(const std::string& path);
+	/**
+	 * @brief Returns the extension of the filename part of the path
+	 * "/home/user/.spring/test.txt" -> "txt"
+	 * "/home/user/.spring/test.txt..." -> "txt"
+	 * "/home/user/.spring/test.txt. . ." -> "txt"
+	 */
+	static std::string GetExtension(const std::string& path);
+	/**
+	 * @brief Converts a glob expression to a regex
+	 * @param glob string containing glob
+	 * @return string containing regex
+	 */
+	static std::string ConvertGlobToRegex(const std::string& glob);
+	/**
+	 * Converts all slashes and backslashes in path to the
+	 * native_path_separator.
+	 */
+	static std::string& FixSlashes(std::string& path);
+	/**
+	 * @brief converts backslashes in path to forward slashes
+	 */
+	static std::string& ForwardSlashes(std::string& path);
+	///@}
 
-		/**
-		 * @brief does a little checking of a filename
-		 */
-		static bool CheckFile(const std::string& file);
-//		bool CheckDir(const std::string& dir) const;
+	/**
+	 * @brief does a little checking of a filename
+	 */
+	static bool CheckFile(const std::string& file);
+//	static bool CheckDir(const std::string& dir) const;
 };
 
 #endif // !FILE_SYSTEM_H
