spring/spring

commit 5ee7220183336f729afd3d92368b103990b4f146

refactor file-system * FileSystem and FileSystemHandler do not depend on data-dir or VFS anymore * made some more functions static * moved a lot of code to different files; no logic changes

tools/unitsync/unitsync.cpp
@@ -24,10 +24,12 @@
 #include "System/FileSystem/IArchive.h"
 #include "System/FileSystem/ArchiveLoader.h"
 #include "System/FileSystem/ArchiveScanner.h"
+#include "System/FileSystem/DataDirsAccess.h"
+#include "System/FileSystem/DataDirLocater.h"
 #include "System/FileSystem/FileHandler.h"
 #include "System/FileSystem/VFSHandler.h"
 #include "System/FileSystem/FileSystem.h"
-#include "System/FileSystem/FileSystemHandler.h"
+#include "System/FileSystem/FileSystemInitializer.h"
 #include "System/Config/ConfigHandler.h"
 #include "System/Exceptions.h"
 #include "System/Log/ILog.h"
@@ -293,7 +295,7 @@ static void _UnInit()
 
 	lpClose();
 
-	FileSystemHandler::Cleanup();
+	FileSystemInitializer::Cleanup();
 
 	if (syncer) {
 		SafeDelete(syncer);
@@ -313,7 +315,7 @@ EXPORT(int) Init(bool isServer, int id)
 		if (!configHandler) {
 			ConfigHandler::Instantiate(); // use the default config file
 		}
-		FileSystemHandler::Initialize(false);
+		FileSystemInitializer::Initialize();
 
 		if (!logOutputInitialised) {
 			logOutput.Initialize();
@@ -359,7 +361,7 @@ EXPORT(const char*) GetWritableDataDirectory()
 {
 	try {
 		CheckInit();
-		return GetStr(FileSystemHandler::GetInstance().GetWriteDir());
+		return GetStr(dataDirLocater.GetWriteDirPath());
 	}
 	UNITSYNC_CATCH_BLOCKS;
 	return NULL;
@@ -371,7 +373,7 @@ EXPORT(int) GetDataDirectoryCount()
 
 	try {
 		CheckInit();
-		count = (int) FileSystemHandler::GetInstance().GetDataDirectories().size();
+		count = (int) dataDirLocater.GetDataDirs().size();
 	}
 	UNITSYNC_CATCH_BLOCKS;
 
@@ -382,9 +384,10 @@ EXPORT(const char*) GetDataDirectory(int index)
 {
 	try {
 		CheckInit();
-		const std::vector<std::string> datadirs = FileSystemHandler::GetInstance().GetDataDirectories();
-		if (index > datadirs.size())
+		const std::vector<std::string> datadirs = dataDirLocater.GetDataDirPaths();
+		if (index > datadirs.size()) {
 			return NULL;
+		}
 		return GetStr(datadirs[index]);
 	}
 	UNITSYNC_CATCH_BLOCKS;
@@ -1706,7 +1709,7 @@ EXPORT(int) GetSkirmishAICount() {
 		skirmishAIDataDirs.clear();
 
 		std::vector<std::string> dataDirs_tmp =
-				filesystem.FindDirsInDirectSubDirs(SKIRMISH_AI_DATA_DIR);
+				dataDirsAccess.FindDirsInDirectSubDirs(SKIRMISH_AI_DATA_DIR);
 
 		// filter out dirs not containing an AIInfo.lua file
 		std::vector<std::string>::const_iterator i;

rts/ExternalAI/AICallback.cpp
@@ -54,7 +54,9 @@
 #include "System/LogOutput.h"
 #include "System/NetProtocol.h"
 #include "System/FileSystem/FileHandler.h"
+#include "System/FileSystem/DataDirsAccess.h"
 #include "System/FileSystem/FileSystem.h"
+#include "System/FileSystem/FileQueryFlags.h"
 #include "System/FileSystem/FileSystemHandler.h"
 #include "System/Platform/errorhandler.h"
 
@@ -1410,12 +1412,12 @@ bool CAICallback::GetValue(int id, void *data)
 			return true;
 		}case AIVAL_LOCATE_FILE_R:{
 			std::string f((char*) data);
-			f = filesystem.LocateFile(f);
+			f = dataDirsAccess.LocateFile(f);
 			strcpy((char*) data, f.c_str());
 			return FileSystemHandler::IsReadableFile(f);
 		}case AIVAL_LOCATE_FILE_W:{
 			std::string f((char*) data);
-			std::string f_abs = filesystem.LocateFile(f, FileSystem::WRITE | FileSystem::CREATE_DIRS);
+			std::string f_abs = dataDirsAccess.LocateFile(f, FileQueryFlags::WRITE | FileQueryFlags::CREATE_DIRS);
 			if (!FileSystemHandler::IsAbsolutePath(f_abs)) {
 				return false;
 			} else {

rts/ExternalAI/AILibraryManager.cpp
@@ -14,7 +14,8 @@
 #include "System/Platform/errorhandler.h"
 #include "System/Platform/SharedLib.h"
 #include "System/FileSystem/FileHandler.h"
-#include "System/FileSystem/FileSystem.h"
+#include "System/FileSystem/DataDirsAccess.h"
+#include "System/FileSystem/FileQueryFlags.h"
 #include "System/FileSystem/FileSystemHandler.h"
 #include "Sim/Misc/GlobalConstants.h"
 #include "Sim/Misc/Team.h"
@@ -45,7 +46,7 @@ void CAILibraryManager::GatherInterfaceLibrariesInfos() {
 	// we are looking for:
 	// {AI_INTERFACES_DATA_DIR}/{*}/{*}/InterfaceInfo.lua
 	T_dirs aiInterfaceDataDirs =
-			filesystem.FindDirsInDirectSubDirs(AI_INTERFACES_DATA_DIR);
+			dataDirsAccess.FindDirsInDirectSubDirs(AI_INTERFACES_DATA_DIR);
 	typedef std::map<const AIInterfaceKey, std::set<std::string> > T_dupInt;
 	T_dupInt duplicateInterfaceInfoCheck;
 	for (T_dirs::iterator dir = aiInterfaceDataDirs.begin();
@@ -113,7 +114,7 @@ void CAILibraryManager::GatherSkirmishAIsLibrariesInfos() {
 	// we are looking for:
 	// {SKIRMISH_AI_DATA_DIR}/{*}/{*}/AIInfo.lua
 	// {SKIRMISH_AI_DATA_DIR}/{*}/{*}/AIOptions.lua
-	T_dirs skirmishAIDataDirs = filesystem.FindDirsInDirectSubDirs(SKIRMISH_AI_DATA_DIR);
+	T_dirs skirmishAIDataDirs = dataDirsAccess.FindDirsInDirectSubDirs(SKIRMISH_AI_DATA_DIR);
 	T_dupSkirm duplicateSkirmishAIInfoCheck;
 	for (T_dirs::iterator dir = skirmishAIDataDirs.begin();
 			dir != skirmishAIDataDirs.end(); ++dir) {

rts/ExternalAI/SAIInterfaceCallbackImpl.cpp
@@ -18,8 +18,9 @@ using std::sprintf;
 #include "ExternalAI/Interface/AISCommands.h"         // for ABI version
 #include "ExternalAI/Interface/SSkirmishAILibrary.h"  // for ABI version
 #include "ExternalAI/Interface/SAIInterfaceLibrary.h" // for ABI version and AI_INTERFACE_PROPERTY_*
-#include "System/FileSystem/FileSystem.h"
-#include "System/FileSystem/FileSystemHandler.h"
+#include "System/FileSystem/DataDirsAccess.h"
+#include "System/FileSystem/FileQueryFlags.h"
+#include "System/FileSystem/DataDirLocater.h"
 #include "System/Log/ILog.h"
 
 #include <vector>
@@ -174,14 +175,12 @@ EXPORT(char) aiInterfaceCallback_DataDirs_getPathSeparator(int UNUSED_interfaceI
 }
 EXPORT(int) aiInterfaceCallback_DataDirs_Roots_getSize(int UNUSED_interfaceId) {
 
-	const std::vector<std::string> &dds =
-			FileSystemHandler::GetInstance().GetDataDirectories();
+	const std::vector<std::string>& dds = dataDirLocater.GetDataDirPaths();
 	return dds.size();
 }
 EXPORT(bool) aiInterfaceCallback_DataDirs_Roots_getDir(int UNUSED_interfaceId, char* path, int path_sizeMax, int dirIndex) {
 
-	const std::vector<std::string> &dds =
-			FileSystemHandler::GetInstance().GetDataDirectories();
+	const std::vector<std::string>& dds = dataDirLocater.GetDataDirPaths();
 	size_t numDataDirs = dds.size();
 	if (dirIndex >= 0 && (size_t)dirIndex < numDataDirs) {
 		STRCPYS(path, path_sizeMax, dds[dirIndex].c_str());
@@ -196,9 +195,9 @@ EXPORT(bool) aiInterfaceCallback_DataDirs_Roots_locatePath(int UNUSED_interfaceI
 
 	int locateFlags = 0;
 	if (writeable) {
-		locateFlags = locateFlags | FileSystem::WRITE;
+		locateFlags = locateFlags | FileQueryFlags::WRITE;
 		if (create) {
-			locateFlags = locateFlags | FileSystem::CREATE_DIRS;
+			locateFlags = locateFlags | FileQueryFlags::CREATE_DIRS;
 		}
 	}
 	std::string locatedPath = "";
@@ -207,9 +206,9 @@ EXPORT(bool) aiInterfaceCallback_DataDirs_Roots_locatePath(int UNUSED_interfaceI
 	STRCPYS(tmpRelPath, tmpRelPath_size, relPath);
 	std::string tmpRelPathStr = tmpRelPath;
 	if (dir) {
-		locatedPath = filesystem.LocateDir(tmpRelPathStr, locateFlags);
+		locatedPath = dataDirsAccess.LocateDir(tmpRelPathStr, locateFlags);
 	} else {
-		locatedPath = filesystem.LocateFile(tmpRelPathStr, locateFlags);
+		locatedPath = dataDirsAccess.LocateFile(tmpRelPathStr, locateFlags);
 	}
 	exists = (locatedPath != relPath);
 	STRCPYS(path, path_sizeMax, locatedPath.c_str());

rts/ExternalAI/SkirmishAIWrapper.cpp
@@ -4,7 +4,8 @@
 #include "SkirmishAIWrapper.h"
 
 #include "System/StdAfx.h"
-#include "System/FileSystem/FileSystem.h"
+#include "System/FileSystem/DataDirsAccess.h"
+#include "System/FileSystem/FileQueryFlags.h"
 #include "System/FileSystem/FileSystemHandler.h"
 #include "System/Log/ILog.h"
 #include "System/mmgr.h"
@@ -245,8 +246,8 @@ static std::string createTempFileName(const char* action, int teamId,
 	char* tmpFileName = new char[tmpFileName_size];
 	SNPRINTF(tmpFileName, tmpFileName_size, "%s-team%i_id%i.tmp", action,
 			teamId, skirmishAIId);
-	std::string tmpFile = filesystem.LocateFile(tmpFileName,
-			FileSystem::WRITE | FileSystem::CREATE_DIRS);
+	std::string tmpFile = dataDirsAccess.LocateFile(tmpFileName,
+			FileQueryFlags::WRITE | FileQueryFlags::CREATE_DIRS);
 	delete[] tmpFileName;
 	return tmpFile;
 }

rts/Game/UI/SelectionKeyHandler.cpp
@@ -23,7 +23,7 @@
 #include "Sim/Units/UnitTypes/Building.h"
 #include "System/Log/ILog.h"
 #include "System/myMath.h"
-#include "System/FileSystem/FileSystem.h"
+#include "System/FileSystem/DataDirsAccess.h"
 #include <boost/cstdint.hpp>
 
 CSelectionKeyHandler* selectionKeys;
@@ -39,7 +39,7 @@ CSelectionKeyHandler::~CSelectionKeyHandler()
 
 void CSelectionKeyHandler::LoadSelectionKeys()
 {
-	std::ifstream ifs(filesystem.LocateFile("selectkeys.txt").c_str());
+	std::ifstream ifs(dataDirsAccess.LocateFile("selectkeys.txt").c_str());
 
 	selectNumber = 0;
 

rts/Lua/LuaIO.cpp
@@ -20,6 +20,7 @@ using std::string;
 #include "LuaHandle.h"
 #endif // !defined UNITSYNC && !defined DEDICATED && !defined BUILDING_AI
 #include "LuaInclude.h"
+#include "System/FileSystem/DataDirsAccess.h"
 #include "System/FileSystem/FileSystem.h"
 #include "System/Util.h"
 
@@ -66,7 +67,7 @@ bool LuaIO::SafeExecPath(const string& path)
 
 bool LuaIO::SafeReadPath(const string& path)
 {
-	return filesystem.InReadDir(path);
+	return dataDirsAccess.InReadDir(path);
 }
 
 
@@ -87,7 +88,7 @@ bool LuaIO::SafeWritePath(const string& path)
 		if (ext == exeFiles[i])
 			return false;
 	}
-	return filesystem.InWriteDir(path, prefix);
+	return dataDirsAccess.InWriteDir(path, prefix);
 }
 
 

rts/Lua/LuaUnsyncedCtrl.cpp
@@ -60,7 +60,7 @@
 #include "System/Sound/ISound.h"
 #include "System/Sound/SoundChannels.h"
 #include "System/FileSystem/FileHandler.h"
-#include "System/FileSystem/FileSystemHandler.h"
+#include "System/FileSystem/DataDirLocater.h"
 #include "System/FileSystem/FileSystem.h"
 #include "System/Platform/Watchdog.h"
 #include "System/Platform/WindowManagerHelper.h"
@@ -2163,7 +2163,7 @@ int LuaUnsyncedCtrl::Restart(lua_State* L)
 	}
 
 	// script.txt, if content for it is given by Lua code
-	const std::string scriptFullName = FileSystemHandler::GetInstance().GetWriteDir() + "script.txt";
+	const std::string scriptFullName = dataDirLocater.GetWriteDirPath() + "script.txt";
 	if (!script.empty()) {
 		std::ofstream scriptfile(scriptFullName.c_str());
 		scriptfile << script;

commit fa4b4c86f88e094ab07bca66a6b7613de1784a0e

make unitsync work again (broken in 5ee7220183336f729afd3d92368b103990b4f146) [fix]

tools/unitsync/unitsync.cpp
@@ -288,13 +288,11 @@ EXPORT(const char*) GetSpringVersionPatchset()
 
 static void internal_deleteMapInfos();
 
-static void _UnInit()
+static void _Cleanup()
 {
 	internal_deleteMapInfos();
 
 	lpClose();
-
-	FileSystemInitializer::Cleanup();
 
 	if (syncer) {
 		SafeDelete(syncer);
@@ -322,7 +320,7 @@ EXPORT(int) Init(bool isServer, int id)
 		}
 		LOG("loaded, %s", SpringVersion::GetFull().c_str());
 
-		_UnInit();
+		_Cleanup();
 
 		std::vector<std::string> filesToCheck;
 		filesToCheck.push_back("base/springcontent.sdz");
@@ -349,7 +347,8 @@ EXPORT(int) Init(bool isServer, int id)
 EXPORT(void) UnInit()
 {
 	try {
-		_UnInit();
+		_Cleanup();
+		FileSystemInitializer::Cleanup();
 		ConfigHandler::Deallocate();
 	}
 	UNITSYNC_CATCH_BLOCKS;