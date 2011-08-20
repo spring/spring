c682dd05e002be6b041b7cf9f5ba3feab493f92b
abma <spring@abma.de>
2011-08-20 23:15:36 +0200

    (buildbot) add upload script for macos buildslave

---
+++ buildbot/slave/rsync-mac.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+. buildbot/slave/prepare.sh
+
+#TODO: this is the same as in rsync.sh
+
+REMOTE_HOST=springrts.com
+REMOTE_USER=buildbot
+REMOTE_BASE=/home/buildbot/www/mac
+RSYNC="rsync -avz --chmod=D+rx,F+r --bwlimit 50"
+REMOTE_RSYNC="nice -19 ionice -c3 rsync" #prevent QQ about rsync killing server
+#Ultra settings
+
+umask 022
+
+#cleanup installed files before rsyncing
+rm -rf ${TMP_BASE}/inst/
+
+# Rsync archives to a world-visible location.
+if [ ${REMOTE_HOST} = localhost ] && [ -w ${REMOTE_BASE} ]; then
+	${RSYNC} ${TMP_BASE}/ ${REMOTE_BASE}/
+else
+	${RSYNC} --rsync-path="${REMOTE_RSYNC}" ${TMP_BASE}/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_BASE}/
+fi
+
+# Clean up.
+rm -rf ${TMP_BASE}/*
