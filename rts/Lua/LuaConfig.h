ceb40cb981acd1064140c77a7cfd2c0efb96f187
zerver <mr@zerver.net>
2011-08-27 19:48:20 +0200

    [MT] Use same mechanism for delayed xcalls and recvfromsynced

--- rts/Lua/LuaConfig.h
+++ rts/Lua/LuaConfig.h
@@ -8,8 +8,10 @@
 #define LUA_MUTEX 1
 #define LUA_BATCH 2
 #define LUA_STATE 4
-#if (defined(USE_GML) && GML_ENABLE_SIM) || defined(USE_LUA_MT)
+#if (defined(USE_GML) && GML_ENABLE_SIM)
 #define LUA_MT_OPT ( LUA_MUTEX | LUA_BATCH | LUA_STATE ) // optimizations for faster parallel execution of separate lua_States
+#elif defined(USE_LUA_MT)
+#define LUA_MT_OPT ( LUA_BATCH | LUA_STATE )
 #else
 #define LUA_MT_OPT 0
 #endif
