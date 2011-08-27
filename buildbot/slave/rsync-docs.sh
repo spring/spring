e95fb800771af6668da6998fca99259537a9c0ae
abma <spring@abma.de>
2011-08-27 13:26:31 +0200

    (buildslave) don't be so verbose on rsync

--- buildbot/slave/rsync-docs.sh
+++ buildbot/slave/rsync-docs.sh
@@ -8,7 +8,7 @@ umask 022
 #FIXME: we should somehow delete the source-directory, as it will grow over time
 
 REMOTE_PATH=OmoikaneWeb:/mnt/data/WWW/spring.osocial.se/
-RSYNC="rsync -avz --delete-after"
+RSYNC="rsync -az --delete-after"
 
 ${RSYNC} ${SOURCEDIR}/doc/doxygen/html/ ${REMOTE_PATH}
 
