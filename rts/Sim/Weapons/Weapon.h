963669f162949b5fbac0bcf84ddc43846c832a67
Kaylewt <kloot@kloot0.(none)>
2011-09-10 02:49:15 +0200

    refactor CWeapon::TryTarget into something half-readable; fix assertion failure (#2647)

--- rts/Sim/Weapons/Weapon.h
+++ rts/Sim/Weapons/Weapon.h
@@ -33,6 +33,7 @@ public:
 	void DependentDied(CObject* o);
 
 	bool HaveFreeLineOfFire(const float3& pos, const float3& dir, float length, const CUnit* target) const;
+	bool AdjustTargetVectorLength(CUnit*, float3&, float3&, float3&) const;
 	virtual bool TryTarget(const float3& pos, bool userTarget,CUnit* unit);
 	bool TryTarget(CUnit* unit, bool userTarget);
 	bool TryTargetRotate(CUnit* unit, bool userTarget);
