a9742d5ae2cba8ca7538f1ea2f5d51c9d13c5e44
Kaylewt <kloot@kloot0.(none)>
2011-09-19 17:29:05 +0200

    -

--- rts/Sim/Objects/SolidObject.h
+++ rts/Sim/Objects/SolidObject.h
@@ -30,7 +30,7 @@ public:
 	virtual bool AddBuildPower(float amount, CUnit* builder);
 
 	virtual void DoDamage(const DamageArray& damages, CUnit* attacker, const float3& impulse) {}
-	virtual void Kill(const float3& impulse) {}
+	virtual void Kill(const float3& impulse, bool crushed) {}
 	virtual int GetBlockingMapID() const { return -1; }
 
 	/**
@@ -54,6 +54,8 @@ public:
 	bool floatOnWater;							///< if the object will float on water (TODO: "float density;" would be more dynamic.)
 	bool immobile;								///< Immobile objects can not be moved. (Except perhaps along y-axis, to make them stay on ground.)
 	bool blockHeightChanges;					///< map height cannot change under this object
+	bool crushKilled;							///< true if this object died by being crushed
+
 	int xsize;									///< The x-size of this object, according to its footprint.
 	int zsize;									///< The z-size of this object, according to its footprint.
 	float height;								///< The height of this object.
@@ -61,6 +63,7 @@ public:
 	// Current dynamic properties
 	SyncedSshort heading;						///< Contains the same information as frontdir, but in a short signed integer.
 	PhysicalState physicalState;				///< The current state of the object within the gameworld. I.e Flying or OnGround.
+
 	bool isMoving;								///< = velocity.length() > 0.0
 	bool isUnderWater;
 	bool isMarkedOnBlockingMap;					///< true if this object is currently marked on the GroundBlockingMap
