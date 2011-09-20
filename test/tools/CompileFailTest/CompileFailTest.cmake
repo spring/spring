180a81aa80a9e843bb51fef158a7b759664e8b50
jK <-jk->
2011-09-20 18:42:58 +0200

    document CompileFailTest.cmake

--- test/tools/CompileFailTest/CompileFailTest.cmake
+++ test/tools/CompileFailTest/CompileFailTest.cmake
@@ -1,4 +1,13 @@
 
+# How does it work?
+# 1. We run the `ctest` program and ask it run "${CMAKE_CURRENT_SOURCE_DIR}/tools/CompileFailTest/CMakeLists.txt"
+#    with SOURCE=${sourcefile}.
+# 2. CMake will then create a corresponding Makefile to compile ${sourcefile}.
+# 3. `ctest` will then run this Makefile, the compilation should fail now (that's what we want to test)
+# 4. with Set_Tests_Properties("${testname}" PROPERTIES WILL_FAIL ON)
+#    we told CTest (not the program! the current CMakeFile build target!) that we want the test named "${testname}"
+#    to fail, so it gets marked as "Passed" in summary
+
 # @brief A macro for compile fail test (to test illegal code)
 # based on Boost's BoostTesting.cmake
 # URL: http://svn.boost.org/svn/boost/branches/CMake/Boost_1_35_0/tools/build/CMake/BoostTesting.cmake
@@ -18,7 +27,7 @@ macro(spring_test_compile_fail testname sourcefile def_flag)
 		"${CMAKE_CURRENT_BINARY_DIR}/tools/CompileFailTest"
 		--build-generator "${CMAKE_GENERATOR}"
 		--build-makeprogram "${CMAKE_MAKE_PROGRAM}"
-		--build-project test
+		--build-project CompileFailTest
 		--build-options -DSOURCE=${sourcefile} -DINCLUDES=${TEST_INCLUDES} -DDEF_FLAG="${def_flag}"
 	)
 	Set_Tests_Properties("${testname}" PROPERTIES WILL_FAIL ON)
