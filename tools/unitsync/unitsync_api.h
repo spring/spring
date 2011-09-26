9187c1ba7bd27826655205ab13ef3581041a60c1
abma <spring@abma.de>
2011-09-26 21:36:16 +0200

    Revert "(unitsync) add SetMinLogLevel(int level) to api"
    This reverts commit f89666e6203cf3ce9952d1dc1144b608f7ca2665.

--- tools/unitsync/unitsync_api.h
+++ tools/unitsync/unitsync_api.h
@@ -1370,12 +1370,6 @@ EXPORT(void        ) SetSpringConfigInt(const char* name, const int value);
  */
 EXPORT(void        ) SetSpringConfigFloat(const char* name, const float value);
 
-/**
- * @brief set minimum log level for unitsync
- * @param minimum log level, @see log_filter_global_setMinLevel
- */
-EXPORT(void        ) SetMinLogLevel(int level);
-
 
 // from LuaParserAPI.cpp:
 
