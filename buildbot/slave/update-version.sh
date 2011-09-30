cb469f9351ffd76d4e9722082ba1e14b0c523055
hoijui <hoijui.quaero@gmail.com>
2011-09-30 11:59:28 +0200

    generate version from git-describe output, and write it to a generated header [CMake]
    This is the main pre-requisite for the new branching, tagging
    and versioning scheme described here:
    http://springrts.com/phpbb/viewtopic.php?p=493086#p493086
    
    We now have more precise version info (exact to a commit)
    on all builds, not just buildbot ones.

--- buildbot/slave/update-version.sh
+++ buildbot/slave/update-version.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-set -e
-. buildbot/slave/prepare.sh
 
-FILE=rts/Game/GameVersion.cpp
+# DEPRECATED CMake now sets the full version info on each new commit by itsself
 
-echo "setting version string '${VERSION_}' in ${FILE}"
-
-ESCAPED_VERSION=`echo "${VERSION_}" | sed 's@/@\\\\/@g'`
-sed -i -r '/\badditional\s+=/ s/"[^"]*"/"'"${ESCAPED_VERSION}"'"/; s/#define GV_ADD_SPACE ""/#define GV_ADD_SPACE " "/' ${FILE}
