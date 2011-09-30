ac08046950ee780283ca9d2a8dcaea4ff2445d60
hoijui <hoijui.quaero@gmail.com>
2011-09-30 11:35:08 +0200

    adjust make_source_package.sh to new branching/tagging/versioning scheme

--- installer/make_source_package.sh
+++ installer/make_source_package.sh
@@ -18,46 +18,60 @@ while [ ! -d installer ]; do
 	if [ "${PWD}" = "/" ]; then
 		echo "Error: Could not find installer directory." >&2
 		echo "Make sure to run this script from a directory below your checkout directory." >&2
-		exit 1
+		exit 2
 	fi
 	cd ..
 done
 
+
+branch=$(git rev-parse --abbrev-ref HEAD)
+describe=$(git describe --tags --candidates 999 --match "*.*.*")
+
 set +e # turn of quit on error
-git describe --candidates 0 --tags &> /dev/null
-if [ $? -ne "0" ]; then
-	RELEASE_SOURCE=false
-	echo "Making test-packages"
-else
-	RELEASE_SOURCE=true
-	echo "Making release-packages"
-fi
+# Check if current HEAD has a version tag
+git describe --tags --candidates 0 --match "*.*.*" &> /dev/null
+onVersionTag=$(if [ $? -eq "0" ]; then echo "true"; else echo "false"; fi)
 set -e # turn it on again
 
-if [ ${RELEASE_SOURCE} ]; then
-	version_string=$(git describe --tags)
-	branch=${version_string}
-else
-	version_string=$(git describe --tags | sed s/\-[^\-]*$//)
-	branch="develop"
+isRelease=false
+if [ "${branch}" = "master" ]; then
+	if ${onVersionTag}; then
+		isRelease=true
+	else
+		echo "Error: On branch master but not on a version tag." >&2
+		echo "This indicates a tagging, branching or push error." >&2
+		exit 3
+	fi
 fi
-echo "Using ${branch} (${version_string}) as source"
 
-dir="spring_${version_string}"
+if ${isRelease}; then
+	echo "Making release-packages"
+	versionString=${describe}
+else
+	echo "Making test-packages"
+	# Insert the branch name as the patch-set part.
+	# (double-quotation is required because of the sub-shell)
+	versionString=$(echo "${describe}" | sed s/\\\([0-9]\\\+[+\-]\\\)\-/\\\1.${branch}-/)
+fi
+
+echo "Using branch ${branch} (${versionString}) as source"
+
+
+dir="spring_${versionString}"
 
 # Each one of these that is set, is built when running this script.
 # Linux archives
 # * linux (LF) line endings
 # * GPL compatible
-lzma="spring_${version_string}_src.tar.lzma"
-#tbz="spring_${version_string}_src.tar.bz2"
-tgz="spring_${version_string}_src.tar.gz"
+lzma="spring_${versionString}_src.tar.lzma"
+#tbz="spring_${versionString}_src.tar.bz2"
+tgz="spring_${versionString}_src.tar.gz"
 
 # Windows archives
 # * windows (CRLF) line endings (bugged, see TODO below)
 # * contain everything from the GIT repository
-#zip="spring_${version_string}_src.zip"
-#seven_zip="spring_${version_string}_src.7z"
+#zip="spring_${versionString}_src.zip"
+#seven_zip="spring_${versionString}_src.7z"
 
 # This is the list of files/directories that go in the source package.
 # (directories are included recursively)
