b234eaec3677501b3b62e617eab2341891c6f6c2
jK <jk3064@arcor.de>
2011-10-16 12:38:25 +0200

    fix style in headers * we do "float* foo;" in spring * don't use tabs to align comments

--- rts/Map/SMF/Patch.h
+++ rts/Map/SMF/Patch.h
@@ -1,12 +1,11 @@
-//#include "BFGroundDrawer.h"
 
-//#include "Vec2.h"
-
-class CVertexArray;
-class CSMFGroundDrawer;
 #ifndef PATCH_H
 #define PATCH_H
 
+#include "Rendering/GL/myGL.h"
+
+class CVertexArray;
+class CSMFGroundDrawer;
 
 //--UNCOMMENT WHICH TRIANGLE PUSHING METHOD SHOULD BE USED!-----------------------
 
@@ -24,11 +23,11 @@ class CSMFGroundDrawer;
 //
 struct TriTreeNode
 {
-	TriTreeNode *LeftChild;
-	TriTreeNode *RightChild;
-	TriTreeNode *BaseNeighbor;
-	TriTreeNode *LeftNeighbor;
-	TriTreeNode *RightNeighbor;
+	TriTreeNode* LeftChild;
+	TriTreeNode* RightChild;
+	TriTreeNode* BaseNeighbor;
+	TriTreeNode* LeftNeighbor;
+	TriTreeNode* RightNeighbor;
 };
 
 //
@@ -38,12 +37,12 @@ struct TriTreeNode
 class Patch
 {
 protected:
-	const float *m_HeightMap; // Pointer to height map to use
+	const float* m_HeightMap; // Pointer to height map to use
 
 	float m_VarianceLeft[1 << (VARIANCE_DEPTH)]; // Left variance tree
 	float m_VarianceRight[1 << (VARIANCE_DEPTH)]; // Right variance tree
 
-	float *m_CurrentVariance; // Which varience we are currently using. [Only valid during the Tessellate and ComputeVariance passes]
+	float* m_CurrentVariance; // Which varience we are currently using. [Only valid during the Tessellate and ComputeVariance passes]
 
 	unsigned char m_isVisible; // Is this patch visible in the current frame?
 
@@ -54,11 +53,11 @@ protected:
 	int ptrsf;
 
 	CSMFGroundDrawer* m_parent;
-	CVertexArray *m_Array;
+	CVertexArray* m_Array;
 
 public:
 	float superfloat[128000]; //Why yes, this IS a mind bogglingly wasteful thing to do: TODO: remove this for both the Displaylist and the VBO implementations (only really needed for vertexarrays)
-	const float * heightData;
+	const float* heightData;
 	unsigned int lend;
 	unsigned int rend;
 	// Some encapsulation functions & extras
@@ -70,19 +69,21 @@ public:
 	unsigned char m_VarianceDirty; // Does the Varience Tree need to be recalculated for this Patch?
 
 #ifdef ROAM_DL
-		GLuint triList;
+	GLuint triList;
 #endif
+
 #ifdef ROAM_VBO
 	//roamvbo
 	int superint[129*129*4];
 	GLuint vertexBuffer;
 	GLuint vertexIndexBuffer;
 #endif
-	TriTreeNode *GetBaseLeft()
+
+	TriTreeNode* GetBaseLeft()
 	{
 		return &m_BaseLeft;
 	}
-	TriTreeNode *GetBaseRight()
+	TriTreeNode* GetBaseRight()
 	{
 		return &m_BaseRight;
 	}
@@ -98,23 +99,23 @@ public:
 
 	// The static half of the Patch Class
 	virtual void Init(int heightX, int heightY, int worldX, int worldY,
-			const float *hMap, int mx, float maxH, float minH);
+			const float* hMap, int mx, float maxH, float minH);
 	virtual void Reset();
 	virtual void Tessellate(float cx, float cy, float cz, int viewradius);
 	
-	virtual int Render(CSMFGroundDrawer * parent, int n, bool waterdrawn);
+	virtual int Render(CSMFGroundDrawer* parent, int n, bool waterdrawn);
 	virtual void ComputeVariance();
 
 	// The recursive half of the Patch Class
-	virtual void Split(TriTreeNode *tri);
-	virtual void RecursTessellate(TriTreeNode *tri, int leftX, int leftY,
+	virtual void Split(TriTreeNode* tri);
+	virtual void RecursTessellate(TriTreeNode* tri, int leftX, int leftY,
 			int rightX, int rightY, int apexX, int apexY, int node);
-	virtual void RecursRender(TriTreeNode *tri, int leftX, int leftY, int rightX,
+	virtual void RecursRender(TriTreeNode* tri, int leftX, int leftY, int rightX,
 		int rightY, int apexX, int apexY, int n,bool dir, int maxdepth, bool waterdrawn);
 	virtual unsigned char RecursComputeVariance(int leftX, int leftY,
 		float leftZ, int rightX, int rightY, float rightZ,
 		int apexX, int apexY, float apexZ, int node);
-	virtual void DrawTriArray(CSMFGroundDrawer * parent);
+	virtual void DrawTriArray(CSMFGroundDrawer* parent);
 };
 
 #endif
