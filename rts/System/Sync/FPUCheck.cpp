9a46f038fb8e99336eccbfca1c7250c7c791b700
jK <-jk->
2011-11-09 08:01:29 +0100

    print correct valid SSE/FPU states on sync errors (depends on SNAN)

--- rts/System/Sync/FPUCheck.cpp
+++ rts/System/Sync/FPUCheck.cpp
@@ -59,22 +59,30 @@ void good_fpu_control_registers(const char* text)
 	// We are paranoid.
 	// We don't trust the enumeration constants from streflop / (g)libc.
 
+	// accepted/syncsafe FPU states:
+#if defined(__SUPPORT_SNAN__) && !defined(USE_GML)	// -fsignaling-nans
+	static const int sse_a = (0x1937 & 0xFF80);
+	static const int sse_b = (0x1925 & 0xFF80);
+	static const int x87_a = (0x0072 & 0x1F3F);
+	static const int x87_b = 0x003F;
+#else
+	static const int sse_a = 0x1D00;
+	static const int sse_b = 0x1F80;
+	static const int x87_a = 0x003A;
+	static const int x87_b = 0x003F;
+#endif
+
 #if defined(STREFLOP_SSE)
 	// struct
 	streflop::fpenv_t fenv;
 	streflop::fegetenv(&fenv);
 
-	#if defined(__SUPPORT_SNAN__) && !defined(USE_GML)	// -fsignaling-nans
-	bool ret = ((fenv.sse_mode & 0xFF80) == (0x1937 & 0xFF80) || (fenv.sse_mode & 0xFF80) == (0x1925 & 0xFF80)) &&
-	           ((fenv.x87_mode & 0x1F3F) == (0x0072 & 0x1F3F) || (fenv.x87_mode & 0x1F3F) == 0x003F);
-	#else
-	bool ret = ((fenv.sse_mode & 0xFF80) == 0x1D00 || (fenv.sse_mode & 0xFF80) == 0x1F80) &&
-	           ((fenv.x87_mode & 0x1F3F) == 0x003A || (fenv.x87_mode & 0x1F3F) == 0x003F);
-	#endif
+	bool ret = ((fenv.sse_mode & 0xFF80) == sse_a || (fenv.sse_mode & 0xFF80) == sse_b) &&
+	           ((fenv.x87_mode & 0x1F3F) == x87_a || (fenv.x87_mode & 0x1F3F) == x87_b);
 
 	if (!ret) {
-		LOG_L(L_WARNING, "[%s] Sync warning: (env.sse_mode) MXCSR 0x%04X instead of 0x1D00 or 0x1F80 (\"%s\")", __FUNCTION__, fenv.sse_mode, text);
-		LOG_L(L_WARNING, "[%s] Sync warning: (env.x87_mode) FPUCW 0x%04X instead of 0x003A or 0x003F (\"%s\")", __FUNCTION__, fenv.x87_mode, text);
+		LOG_L(L_WARNING, "[%s] Sync warning: (env.sse_mode) MXCSR 0x%04X instead of 0x%04X or 0x%04X (\"%s\")", __FUNCTION__, fenv.sse_mode, sse_a, sse_b, text);
+		LOG_L(L_WARNING, "[%s] Sync warning: (env.x87_mode) FPUCW 0x%04X instead of 0x%04X or 0x%04X (\"%s\")", __FUNCTION__, fenv.x87_mode, x87_a, x87_b, text);
 
 		// Set single precision floating point math.
 		streflop_init<streflop::Simple>();
@@ -88,14 +96,10 @@ void good_fpu_control_registers(const char* text)
 	streflop::fpenv_t fenv;
 	streflop::fegetenv(&fenv);
 
-	#if defined(__SUPPORT_SNAN__) && !defined(USE_GML)
-	bool ret = (fenv & 0x1F3F) == 0x0072 || (fenv & 0x1F3F) == 0x003F;
-	#else
-	bool ret = (fenv & 0x1F3F) == 0x003A || (fenv & 0x1F3F) == 0x003F;
-	#endif
+	bool ret = (fenv & 0x1F3F) == x87_a || (fenv & 0x1F3F) == x87_b;
 
 	if (!ret) {
-		LOG_L(L_WARNING, "[%s] Sync warning: FPUCW 0x%04X instead of 0x003A or 0x003F (\"%s\")", __FUNCTION__, fenv, text);
+		LOG_L(L_WARNING, "[%s] Sync warning: FPUCW 0x%04X instead of 0x%04X or 0x%04X (\"%s\")", __FUNCTION__, fenv, x87_a, x87_b, text);
 
 		// Set single precision floating point math.
 		streflop_init<streflop::Simple>();
