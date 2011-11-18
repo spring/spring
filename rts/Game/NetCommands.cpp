5cc1acf2d5efc30ce6bfcd19a48b608090f52293
Kaylewt <rt@rt0.(none)>
2011-11-18 15:07:54 +0100

    re-apply 5bd294108092d0bf5c8d1fe3f081d55f0c04040c with some improvements

--- rts/Game/NetCommands.cpp
+++ rts/Game/NetCommands.cpp
@@ -362,8 +362,10 @@ void CGame::ClientReadNet()
 				SimFrame();
 				// both NETMSG_SYNCRESPONSE and NETMSG_NEWFRAME are used for ping calculation by server
 #ifdef SYNCCHECK
-				net->Send(CBaseNetProtocol::Get().SendSyncResponse(gs->frameNum, CSyncChecker::GetChecksum()));
-				if ((gs->frameNum & 4095) == 0) {// reset checksum every ~2.5 minute gametime
+				net->Send(CBaseNetProtocol::Get().SendSyncResponse(gu->myPlayerNum, gs->frameNum, CSyncChecker::GetChecksum()));
+
+				if ((gs->frameNum & 4095) == 0) {
+					// reset checksum every 4096 frames =~ 2.5 minutes
 					CSyncChecker::NewFrame();
 				}
 #endif
@@ -374,6 +376,36 @@ void CGame::ClientReadNet()
 				}
 				break;
 			}
+
+			case NETMSG_SYNCRESPONSE: {
+#if (defined(SYNCCHECK) && defined(DEBUG))
+				// NOTE:
+				//     this packet is also sent during live games,
+				//     during which we should just ignore it (the
+				//     server does sync-checking for us)
+				netcode::UnpackPacket pckt(packet, 1);
+
+				unsigned char playerNum; pckt >> playerNum;
+				          int  frameNum; pckt >> frameNum;
+				unsigned  int  checkSum; pckt >> checkSum;
+
+				const unsigned int ourCheckSum = CSyncChecker::GetChecksum();
+				const char* fmtStr =
+					"[DESYNC_WARNING] checksum %x from player %d (%s)"
+					" does not match our checksum %x for frame-number %d";
+				const CPlayer* player = playerHandler->Player(playerNum);
+
+				// check if our checksum for this frame matches what
+				// player <playerNum> sent to the server at the same
+				// frame in the original game (in case of a demo)
+				if (playerNum == gu->myPlayerNum) { return; }
+				if (gs->frameNum != frameNum) { return; }
+				if (checkSum == ourCheckSum) { return; }
+
+				LOG_L(L_ERROR, fmtStr, checkSum, playerNum, player->name.c_str(), ourCheckSum, gs->frameNum);
+#endif
+			} break;
+
 
 			case NETMSG_COMMAND: {
 				try {
