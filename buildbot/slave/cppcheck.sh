e63ee480a4b529ad59dd04149e8a868bea71d7fb
hoijui <hoijui.quaero@gmail.com>
2011-12-03 13:25:57 +0100

    cppcheck script: prevent too early expansion of regexes [fix]

--- buildbot/slave/cppcheck.sh
+++ buildbot/slave/cppcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set +e
 
-find rts AI/ -name *\.cpp -or -name *\.c \
+find rts AI/ -name "*\.cpp" -or -name "*\.c" \
 |grep -v '^rts/lib' \
 |cppcheck --force --enable=style,information,unusedFunction --quiet -I rts --file-list=/dev/stdin --suppressions-list=buildbot/slave/cppcheck.supress $@
