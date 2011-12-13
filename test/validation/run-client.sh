54fd11a28bff8e27af639739087fb63bca6095b8
abma <spring@abma.de>
2011-12-13 01:51:24 +0100

    (validation) prepare connecting a spring-client (currently not enabled)

---
+++ test/validation/run-client.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+set -e #abort on error
+
+if [ $# -ne 1 ]; then
+	echo "runs spring as client, waits until  ~/.spring/cache/paths/*.pe[1|2].zip exists"
+	echo "Usage: $0 /path/to/spring"
+	exit 1
+fi
+
+CACHEDIR=~/.spring/cache/paths
+HEADLESS=$1
+MAXWAIT=60
+
+for (( i=0; $i<$MAXWAIT; i++ ));
+do
+	if [ -s $CACHEDIR/*.pe.zip ] && [ -s $CACHEDIR/*.pe2.zip ];
+	then
+		# wait additional seconds to let the other process write the files
+		sleep 3
+		LOG=$(mktemp)
+		echo "Starting $HEADLESS client"
+		$HEADLESS connect.txt >$LOG 2>&1
+		# dump log file at exit, to not mix client + server output
+		# FIXME: this merges stdout + stderr
+		cat $LOG
+		exit $?
+	fi
+	# don't use 100% cpu in polling
+	sleep 1
+
+done
+
+echo "cache file didn't show up within MAXWAIT=$MAXWAIT seconds"
+exit 1
+
--- test/validation/run.sh
+++ test/validation/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e #abort on error
+set -e # abort on error
 
 if [ $# -le 0 ]; then
 	echo "Usage: $0 /path/to/spring testScript [parameters]"
@@ -21,18 +21,24 @@ GDBCMDS=$(mktemp)
 	echo quit
 )>$GDBCMDS
 
-#limit to 1GB RAM
+# limit to 1GB RAM
 ulimit -v 1000000
-#max 15 min cpu time
+# max 15 min cpu time
 ulimit -t 900
+
+# start up the client in background
+#$(pwd)/$(dirname $0)/run-client.sh $1 &
 
 set +e #temp disable abort on error
 gdb -batch -return-child-result -x $GDBCMDS
-#store exit code
+# store exit code
 EXIT=$?
 set -e
 
-#cleanup
+# cleanup
 rm -f $GDBCMDS
+
+# wait for client to exit
+#wait
 exit $EXIT
 
