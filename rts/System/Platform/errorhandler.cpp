29908f0a53436d8d6dd7ef14a6564779bdfba369
abma <spring@abma.de>
2012-01-29 08:32:17 +0100

    fix macosx the 4th + cleanup

--- rts/System/Platform/errorhandler.cpp
+++ rts/System/Platform/errorhandler.cpp
@@ -27,8 +27,13 @@
 		#ifdef WIN32
 			#include <windows.h>
 		#else
+			#ifdef __APPLE__
+			//FIXME: move to Mac/ + implement me
+			static void X_MessageBox(const char*msg, const char* caption, unsigned int flags){}
+			#else
 			// from X_MessageBox.cpp:
 			void X_MessageBox(const char* msg, const char* caption, unsigned int flags);
+			#endif
 		#endif
 	#endif // ifndef HEADLESS
 #endif // ifndef DEDICATED
@@ -57,8 +62,7 @@ static void ExitMessage(const std::string& msg, const std::string& caption, unsi
 		MessageBox(GetActiveWindow(), msg.c_str(), caption.c_str(), winFlags);
 
 	#else  // ifdef WIN32
-		//! X implementation
-		// TODO: write Mac OS X specific message box
+		// X implementation
 		X_MessageBox(msg.c_str(), caption.c_str(), flags);
 
 	#endif
@@ -91,11 +95,9 @@ void ErrorMessageBox(const std::string& msg, const std::string& caption, unsigne
 	ExitMessage(msg, caption, flags, false);
 
 #else
-//#ifdef USE_GML
 	//! SpringApp::Shutdown is extremely likely to deadlock or end up waiting indefinitely if any 
 	//! MT thread has crashed or deviated from its normal execution path by throwing an exception
 	boost::thread* forcedExitThread = new boost::thread(boost::bind(&ForcedExit, msg, caption, flags));
-//#endif
 
 	//! Non-MainThread. Inform main one and then interrupt it.
 	if (!Threading::IsMainThread()) {
@@ -113,11 +115,9 @@ void ErrorMessageBox(const std::string& msg, const std::string& caption, unsigne
 	//! (else they would still run while the error messagebox is shown)
 	SpringApp::Shutdown();
 
-//#ifdef USE_GML
 	shutdownSucceeded = true;
 	forcedExitThread->join();
 	delete forcedExitThread;
-//#endif
 
 	ExitMessage(msg, caption, flags, false);
 #endif // defined(DEDICATED)
