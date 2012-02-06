3dbaf3392172b7a202e5080748e710880219184b
abma <spring@abma.de>
2012-02-06 20:49:21 +0100

    refactor lua socket example

--- cont/examples/Widgets/luasocket.lua
+++ cont/examples/Widgets/luasocket.lua
@@ -36,22 +36,7 @@ local function dumpConfig()
 
 end
 
-function widget:Initialize()
-	dumpConfig()
-	client=socket.tcp()
-	client:settimeout(0)
-	--Spring.Echo(socket.dns.toip("localhost"))
-	--FIXME dns-request seems to block
-	res, err = client:connect(host, port)
-	if not res and not res=="timeout" then
-		Spring.Echo("Error in connect: "..err)
-		return
-	end
-	set = newset()
-	set:insert(client)
-end
-
-function newset()
+local function newset()
     local reverse = {}
     local set = {}
     return setmetatable(set, {__index = {
@@ -75,36 +60,74 @@ function newset()
     }})
 end
 
+
+-- initiates a connection to host:port, returns true on success
+local function SocketConnect(host, port)
+	client=socket.tcp()
+	client:settimeout(0)
+	res, err = client:connect(host, port)
+	if not res and not res=="timeout" then
+		Spring.Echo("Error in connect: "..err)
+		return false
+	end
+	set = newset()
+	set:insert(client)
+	return true
+end
+
+function widget:Initialize()
+	dumpConfig()
+	--Spring.Echo(socket.dns.toip("localhost"))
+	--FIXME dns-request seems to block
+	SocketConnect(host, port)
+end
+
+-- called when data was received through a connection
+local function SocketDataReceived(sock, str)
+	Spring.Echo(str)
+end
+
+local headersent
+-- called when data can be written to a socket
+local function SocketWriteAble(sock)
+	if headersent==nil then
+		-- socket is writeable
+		headersent=1
+		Spring.Echo("sending http request")
+		sock:send("GET " .. file .. " HTTP/1.0\r\nHost: " .. host ..  " \r\n\r\n")
+	end
+end
+
+-- called when a connection is closed
+local function SocketClosed(sock)
+	Spring.Echo("closed connection")
+end
+
 function widget:DrawScreen(n)
 	if set==nil or #set<=0 then
 		return
 	end
 	-- get sockets ready for read
-	local readable, writeable, error = socket.select(set, set, 0)
-	if error~=nil then
+	local readable, writeable, err = socket.select(set, set, 0)
+	if err~=nil then
 		-- some error happened in select
-		if error=="timeout" then
+		if err=="timeout" then
 			-- nothing to do, return
 			return
 		end
 		Spring.Echo("Error in select: " .. error)
 	end
-	for __, input in ipairs(readable) do
+	for _, input in ipairs(readable) do
 		local s, status, partial = input:receive('*a') --try to read all data
 		if status == "timeout" or status == nil then
-			Spring.Echo(s or partial)
+			SocketDataReceived(input, s or partial)
 		elseif status == "closed" then
-			Spring.Echo("closed connection")
+			SocketClosed(input)
 			input:close()
 			set:remove(input)
 		end
 	end
 	for __, output in ipairs(writeable) do
-		if headersent==nil then
-			-- socket is writeable
-			headersent=1
-			Spring.Echo("sending http request")
-			output:send("GET " .. file .. " HTTP/1.0\r\nHost: " .. host ..  " \r\n\r\n")
-		end
+		SocketWriteAble(output)
 	end
 end
