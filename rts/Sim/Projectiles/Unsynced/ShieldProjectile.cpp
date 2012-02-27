11beac897e627f3938a47c4c195a211083cc3d5a
rt <rt@rt-0.(none)>
2012-02-27 22:46:28 +0100

    give shields their 85.0 transparency-levels back

--- rts/Sim/Projectiles/Unsynced/ShieldProjectile.cpp
+++ rts/Sim/Projectiles/Unsynced/ShieldProjectile.cpp
@@ -9,11 +9,15 @@
 #include "Sim/Units/Unit.h"
 #include "Sim/Weapons/PlasmaRepulser.h"
 #include "Sim/Weapons/WeaponDef.h"
+#include "System/myMath.h"
 
 CR_BIND_DERIVED(ShieldProjectile, CProjectile, (NULL));
 CR_BIND_DERIVED(ShieldSegmentProjectile, CProjectile, (NULL, NULL, ZeroVector, 0, 0));
 
 
+
+#define NUM_SEGMENTS_X 8
+#define NUM_SEGMENTS_Y 4
 
 ShieldProjectile::ShieldProjectile(
 	const CPlasmaRepulser* shield_
@@ -42,9 +46,9 @@ ShieldProjectile::ShieldProjectile(
 	if ((allowDrawing = wd->visibleShield)) {
 		shieldTexture = wd->visuals.texture1;
 
-		// 4*8 segments, deleted by ProjectileHandler
-		for (int y = 0; y < 16; y += 4) {
-			for (int x = 0; x < 32; x += 4) {
+		// Y*X segments, deleted by ProjectileHandler
+		for (int y = 0; y < (NUM_SEGMENTS_Y * 4); y += 4) {
+			for (int x = 0; x < (NUM_SEGMENTS_X * 4); x += 4) {
 				shieldSegments.push_back(new ShieldSegmentProjectile(this, wd, u->pos, x, y));
 			}
 		}
@@ -89,6 +93,9 @@ void ShieldProjectile::Draw() {
 
 
 
+#define NUM_VERTICES_X 5
+#define NUM_VERTICES_Y 5
+
 ShieldSegmentProjectile::ShieldSegmentProjectile(
 	const ShieldProjectile* shieldProjectile_,
 	const WeaponDef* shieldWeaponDef,
@@ -126,16 +133,19 @@ ShieldSegmentProjectile::ShieldSegmentProjectile(
 		const float xscale = (texture->xend - texture->xstart) * 0.25f, xmid = (texture->xstart + texture->xend) * 0.5f;
 		const float yscale = (texture->yend - texture->ystart) * 0.25f, ymid = (texture->ystart + texture->yend) * 0.5f;
 
-		// 5*5 vertices
-		for (int y = 0; y < 5; ++y) {
-			const float yp = (y + ypart) / 16.0f * PI - PI / 2;
+		// N*N vertices, (N-1)*(N-1) quads (a segment is not planar)
+		for (int y = 0; y < NUM_VERTICES_Y; ++y) {
+			const float yp = (y + ypart) / float(NUM_SEGMENTS_Y * 4) * PI - PI / 2;
 
-			for (int x = 0; x < 5; ++x) {
-				const float xp = (x + xpart) / 32.0f * 2 * PI;
+			for (int x = 0; x < NUM_VERTICES_X; ++x) {
+				const float xp = (x + xpart) / float(NUM_SEGMENTS_X * 4) * 2 * PI;
+				const unsigned int vIdx = y * NUM_VERTICES_X + x;
 
-				vertices[y * 5 + x] = float3(sin(xp) * cos(yp), sin(yp), cos(xp) * cos(yp));
-				texCoors[y * 5 + x].x = (vertices[y * 5 + x].x * (2 - fabs(vertices[y * 5 + x].y))) * xscale + xmid;
-				texCoors[y * 5 + x].y = (vertices[y * 5 + x].z * (2 - fabs(vertices[y * 5 + x].y))) * yscale + ymid;
+				vertices[vIdx].x = sin(xp) * cos(yp);
+				vertices[vIdx].y = sin(yp);
+				vertices[vIdx].z = cos(xp) * cos(yp);
+				texCoors[vIdx].x = (vertices[vIdx].x * (2 - fabs(vertices[vIdx].y))) * xscale + xmid;
+				texCoors[vIdx].y = (vertices[vIdx].z * (2 - fabs(vertices[vIdx].y))) * yscale + ymid;
 			}
 		}
 
@@ -158,7 +168,7 @@ ShieldSegmentProjectile::~ShieldSegmentProjectile()
 
 void ShieldSegmentProjectile::Draw() {
 	// use the "middle" vertex for z-ordering
-	pos = segmentPos + vertices[12] * segmentSize;
+	pos = segmentPos + vertices[(NUM_VERTICES_X * NUM_VERTICES_Y) >> 1] * segmentSize;
 
 	if (shieldProjectile == NULL)
 		return;
@@ -167,6 +177,27 @@ void ShieldSegmentProjectile::Draw() {
 
 	const CPlasmaRepulser* shield = shieldProjectile->GetShield();
 	const WeaponDef* shieldDef = shield->weaponDef;
+
+	// lerp between badColor and goodColor based on shield's current power
+	const float colorMix = std::min(1.0f, shield->curPower / std::max(1.0f, shieldDef->shieldPower));
+
+	segmentPos = shield->weaponPos;
+	segmentColor = mix(shieldDef->shieldBadColor, shieldDef->shieldGoodColor, colorMix);
+	segmentAlpha = shieldDef->shieldAlpha;
+
+	if (shield->hitFrames > 0 && shieldDef->visibleShieldHitFrames > 0) {
+		// when a shield is hit, increase segment's opacity to
+		// min(1, def->alpha + k * def->alpha) with k in [1, 0]
+		segmentAlpha += (shield->hitFrames / float(shieldDef->visibleShieldHitFrames));
+		segmentAlpha *= shieldDef->shieldAlpha;
+		segmentAlpha = std::min(segmentAlpha, 1.0f);
+	}
+
+	if (segmentAlpha <= 0.0f)
+		return;
+	if (!shield->isEnabled)
+		return;
+
 	const unsigned char segmentColorRGBA[4] = {
 		segmentColor.x * segmentAlpha * 255,
 		segmentColor.y * segmentAlpha * 255,
@@ -174,34 +205,19 @@ void ShieldSegmentProjectile::Draw() {
 		                 segmentAlpha * 255,
 	};
 
-	float rawSegmentAlpha = 1.0f;
-
-	if (shield->hitFrames > 0 && shieldDef->visibleShieldHitFrames > 0) {
-		rawSegmentAlpha += (shield->hitFrames / float(shieldDef->visibleShieldHitFrames));
-		rawSegmentAlpha = std::min(1.0f, rawSegmentAlpha * shieldDef->shieldAlpha);
-	}
-
-	if (rawSegmentAlpha > 0.0f) {
-		const float colorMix = std::min(1.0f, shield->curPower / std::max(1.0f, shieldDef->shieldPower));
-		const float3 color = (shieldDef->shieldGoodColor * colorMix) + (shieldDef->shieldBadColor * (1.0f - colorMix));
-
-		segmentPos = shield->weaponPos;
-		segmentColor = color;
-		segmentAlpha = shield->isEnabled? rawSegmentAlpha: 0.0f;
-	}
-
-	if (segmentAlpha <= 0.0f)
-		return;
-
 	inArray = true;
 	va->EnlargeArrays(4 * 4 * 4, 0, VA_SIZE_TC);
 
-	for (int y = 0; y < 4; ++y) {
-		for (int x = 0; x < 4; ++x) {
-			va->AddVertexQTC(segmentPos + vertices[(y    ) * 5 + x    ] * segmentSize, texCoors[(y    ) * 5 + x    ].x, texCoors[(y    ) * 5 + x    ].y, segmentColorRGBA);
-			va->AddVertexQTC(segmentPos + vertices[(y    ) * 5 + x + 1] * segmentSize, texCoors[(y    ) * 5 + x + 1].x, texCoors[(y    ) * 5 + x + 1].y, segmentColorRGBA);
-			va->AddVertexQTC(segmentPos + vertices[(y + 1) * 5 + x + 1] * segmentSize, texCoors[(y + 1) * 5 + x + 1].x, texCoors[(y + 1) * 5 + x + 1].y, segmentColorRGBA);
-			va->AddVertexQTC(segmentPos + vertices[(y + 1) * 5 + x    ] * segmentSize, texCoors[(y + 1) * 5 + x    ].x, texCoors[(y + 1) * 5 + x    ].y, segmentColorRGBA);
+	// draw all quads
+	for (int y = 0; y < (NUM_VERTICES_Y - 1); ++y) {
+		for (int x = 0; x < (NUM_VERTICES_X - 1); ++x) {
+			const int idxTL = (y    ) * NUM_VERTICES_X + x, idxTR = (y    ) * NUM_VERTICES_X + x + 1;
+			const int idxBL = (y + 1) * NUM_VERTICES_X + x, idxBR = (y + 1) * NUM_VERTICES_X + x + 1;
+
+			va->AddVertexQTC(segmentPos + vertices[idxTL] * segmentSize, texCoors[idxTL].x, texCoors[idxTL].y, segmentColorRGBA);
+			va->AddVertexQTC(segmentPos + vertices[idxTR] * segmentSize, texCoors[idxTR].x, texCoors[idxTR].y, segmentColorRGBA);
+			va->AddVertexQTC(segmentPos + vertices[idxBR] * segmentSize, texCoors[idxBR].x, texCoors[idxBR].y, segmentColorRGBA);
+			va->AddVertexQTC(segmentPos + vertices[idxBL] * segmentSize, texCoors[idxBL].x, texCoors[idxBL].y, segmentColorRGBA);
 		}
 	}
 }
