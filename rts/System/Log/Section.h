c1f5202eb9ab9de3e7a376d3e380f2525fbb7995
jK <-jk->
2012-03-18 15:16:16 +0100

    fix LOG_SECTION_EQUAL(std::string("").c_str(), LOG_SECTION_DEFAULT)

--- rts/System/Log/Section.h
+++ rts/System/Log/Section.h
@@ -3,6 +3,8 @@
 #ifndef LOG_SECTION_H
 #define LOG_SECTION_H
 
+#include <string.h>
+
 /*
  * see ILog.h for documentation
  */
@@ -11,35 +13,18 @@
 
 
 /**
- * As all sections are required to be defined as compile-time constants,
- * we may use an address comparison, if the compiler merges constants.
- * MinGW does not merge constants, therefore we have to use !defined(_WIN32).
+ * Helpers
  */
-#if       defined(__GNUC__) && !defined(_WIN32) && !defined(DEBUG)
-	/**
-	 * GCC does merged constants on -O2+:
-	 * -fmerge-constants
-	 *   Attempt to merge identical constants (string constants and floating
-	 *   point constants) across compilation units. 
-	 *   This option is the default for optimized compilation if the assembler
-	 *   and linker support it. Use -fno-merge-constants to inhibit this
-	 *   behavior. 
-	 *   Enabled at levels -O, -O2, -O3, -Os.
-	 */
-	#define LOG_SECTION_EQUAL(section1, section2) \
-		(section1 == section2)
-	#define LOG_SECTION_COMPARE(section1, section2) \
-		(section1 < section2)
-#else  // defined(__GNUC__) && !defined(_WIN32) && !defined(DEBUG)
-	#include <string.h>
-	#define LOG_SECTION_EQUAL(section1, section2) \
-		(((void*)section1 == (void*)section2) \
-		|| (((void*)section1 != NULL) && ((void*)section2 != NULL) \
-			&& (strcmp(section1, section2) == 0)))
-	#define LOG_SECTION_COMPARE(section1, section2) \
-		(((void*)section1 == NULL) \
-			|| (((void*)section2 != NULL) && (strcmp(section1, section2) > 0)))
-#endif // defined(__GNUC__) && !defined(_WIN32) && !defined(DEBUG)
+// first do a fasth-path and check if both c-strings share the same memory location
+// if not check if both c-strings have the same content
+#define LOG_SECTION_EQUAL(section1, section2) \
+	(((void*)section1 == (void*)section2) \
+	|| (((void*)section1 != NULL) && ((void*)section2 != NULL) \
+		&& (strcmp(section1, section2) == 0)))
+
+#define LOG_SECTION_COMPARE(section1, section2) \
+	(((void*)section1 == NULL) \
+		|| (((void*)section2 != NULL) && (strcmp(section1, section2) > 0)))
 
 #define LOG_SECTION_IS_DEFAULT(section) \
 	LOG_SECTION_EQUAL(section, LOG_SECTION_DEFAULT)
