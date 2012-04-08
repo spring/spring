dff37fc9e4f869dd51d2cedf79f404b2593783c3
rt <rt@rt-0.(none)>
2012-04-08 19:14:23 +0200

    QTPFS: make F2 functional

--- rts/Sim/Units/UnitDef.cpp
+++ rts/Sim/Units/UnitDef.cpp
@@ -867,9 +867,9 @@ void UnitDef::CreateYardMap(std::string yardMapStr)
 	const unsigned int hzsize = zsize >> 1;
 
 	std::vector<YardmapStatus> yardMap(hxsize * hzsize, YARDMAP_BLOCKED);
+	std::string foundUnknownChars;
 
 	unsigned int idx = 0;
-	std::string foundUnknownChars;
 
 	for (unsigned int n = 0; n < yardMapStr.size(); n++) {
 		const unsigned char c = yardMapStr[n];
@@ -902,10 +902,10 @@ void UnitDef::CreateYardMap(std::string yardMapStr)
 
 	// print warnings
 	if (idx > yardMap.size())
-		LOG_L(L_WARNING, "%s: Given yardmap/blockmap contains %i char(s) too many!", name.c_str(), idx - yardMap.size());
+		LOG_L(L_WARNING, "%s: Given yardmap/blockmap contains %i excess char(s)!", name.c_str(), idx - yardMap.size());
 
 	if (idx > 0 && idx < yardMap.size())
-		LOG_L(L_WARNING, "%s: Given yardmap/blockmap contains %i char(s) too less!", name.c_str(), yardMap.size() - idx);
+		LOG_L(L_WARNING, "%s: Given yardmap/blockmap requires %i extra char(s)!", name.c_str(), yardMap.size() - idx);
 
 	if (!foundUnknownChars.empty())
 		LOG_L(L_WARNING, "%s: Unknown char(s) in yardmap/blockmap \"%s\"!", name.c_str(), foundUnknownChars.c_str());
