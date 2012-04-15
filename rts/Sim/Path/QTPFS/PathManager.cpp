cdfe4fa4ce472bdc2f3694486c47d927e7b69e7f
rt <rt@rt-0.(none)>
2012-04-16 01:15:27 +0200

    QTPFS: decrease MAX_UPDATE_DELAY (and rename it LAYERS_PER_UPDATE)

--- rts/Sim/Path/QTPFS/PathManager.cpp
+++ rts/Sim/Path/QTPFS/PathManager.cpp
@@ -472,10 +472,10 @@ void QTPFS::PathManager::Update() {
 	SCOPED_TIMER("PathManager::Update");
 
 	// NOTE:
-	//     for a mod with N move-types, a unit will be waiting
-	//     (N / MAX_UPDATE_DELAY) sim-frames before its request
+	//     for a mod with N move-types, any unit will be waiting
+	//     (N / LAYERS_PER_UPDATE) sim-frames before its request
 	//     executes at a minimum
-	static const unsigned int numPathTypeUpdates = std::max(1U, static_cast<unsigned int>(nodeLayers.size() / MAX_UPDATE_DELAY));
+	static const unsigned int numPathTypeUpdates = std::max(1U, static_cast<unsigned int>(nodeLayers.size() / LAYERS_PER_UPDATE));
 	static unsigned int minPathTypeUpdate = 0;
 	static unsigned int maxPathTypeUpdate = numPathTypeUpdates;
 
