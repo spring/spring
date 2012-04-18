5702c044574abb331f32771528358e302bfb1f95
jK <-jk->
2012-04-18 17:59:09 +0200

    add CMake code to search & link system installed LuaJIT * it globaly replaced the linked Lua and so won't sync (when everything is finished only LuaUI should use LuaJIT)

---
+++ rts/build/cmake/FindLuaJIT.cmake
@@ -0,0 +1,44 @@
+# Find the LuaJIT includes and library
+#
+# LUAJIT_INCLUDE_DIR - where to find lua.h
+# LUAJIT_LIBRARIES - List of fully qualified libraries to link against
+# LUAJIT_FOUND - Set to TRUE if found
+
+# Copyright (c) 2007, Pau Garcia i Quiles, <pgquiles@elpauer.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# modified for LuaJIT @ 2012, jK
+
+IF(LUAJIT_INCLUDE_DIR AND LUAJIT_LIBRARY)
+	SET(LUAJIT_FIND_QUIETLY TRUE)
+ENDIF(LUAJIT_INCLUDE_DIR AND LUAJIT_LIBRARY)
+
+FIND_PATH(LUAJIT_INCLUDE_DIR lua.h)
+
+# the "lua" causes that system installed lua library is still preferred to Spring's internal synced Lua
+SET(LUAJIT_NAMES luajit luajit luajit-5.1 lua)
+FIND_LIBRARY(LUAJIT_LIBRARY NAMES ${LUAJIT_NAMES})
+
+IF(LUAJIT_INCLUDE_DIR AND LUAJIT_LIBRARY)
+	SET(LUAJIT_FOUND TRUE)
+	INCLUDE(CheckLibraryExists)
+	CHECK_LIBRARY_EXISTS(${LUAJIT_LIBRARY} lua_close "" LUAJIT_NEED_PREFIX)
+	CHECK_LIBRARY_EXISTS(${LUAJIT_LIBRARY} luaL_argerror "" LUAJIT_NEED_PREFIX_)
+ELSE(LUAJIT_INCLUDE_DIR AND LUAJIT_LIBRARY)
+	SET(LUAJIT_FOUND FALSE)
+ENDIF (LUAJIT_INCLUDE_DIR AND LUAJIT_LIBRARY)
+
+IF(LUAJIT_FOUND)
+	IF (NOT LUAJIT_FIND_QUIETLY)
+		MESSAGE(STATUS "Found Lua library: ${LUAJIT_LIBRARY}")
+		MESSAGE(STATUS "Found Lua headers: ${LUAJIT_INCLUDE_DIR}")
+	ENDIF (NOT LUAJIT_FIND_QUIETLY)
+ELSE(LUAJIT_FOUND)
+	IF(LUAJIT_FIND_REQUIRED)
+		MESSAGE(FATAL_ERROR "Could NOT find Lua")
+	ENDIF(LUAJIT_FIND_REQUIRED)
+ENDIF(LUAJIT_FOUND)
+
+MARK_AS_ADVANCED(LUAJIT_INCLUDE_DIR LUAJIT_LIBRARY)
