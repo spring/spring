1f473c6959e4fdbfe754e1642853dad278cbed08
jK <-jk->
2012-05-25 01:12:57 +0200

    improve the buildslave job to display the linking of specific libraries

---
+++ buildbot/slave/static-test-if-shared-linked.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+BINARY=${1:-build/default/spring}
+LIBRARY=${2}
+
+# check if the binary links the library
+ldd ${BINARY} | grep -iq ${LIBRARY}
+
+# grep returns 0 if found and 1 if not
+RESULT=$?
+
+# invert the return argument (it should give an error when the library is linked)
+if (( RESULT == 1 )); then
+	exit 0
+elif (( RESULT == 0 )); then
+	exit 1
+fi
+exit $?
