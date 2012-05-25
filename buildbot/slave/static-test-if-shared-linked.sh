45957a6c3655e9daa292c0164dfe320d5c3625bd
jK <-jk->
2012-05-25 05:25:54 +0200

    static-linux buildslave: convert failures to warnings

--- buildbot/slave/static-test-if-shared-linked.sh
+++ buildbot/slave/static-test-if-shared-linked.sh
@@ -11,8 +11,10 @@ RESULT=$?
 
 # invert the return argument (it should give an error when the library is linked)
 if (( RESULT == 1 )); then
+	echo "${BINARY} static linked to ${LIBRARY}"
 	exit 0
 elif (( RESULT == 0 )); then
+	echo "${BINARY} links to ${LIBRARY}"
 	exit 1
 fi
 exit $?
