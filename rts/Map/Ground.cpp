spring/spring

commit 0e28867bc8169874cc79c2eb9fe6f588d3f1c519

Fix bug in 4efde89 (problem to build near some map edges, especially in windowed mode and with tilted camera)

rts/Map/Ground.cpp
@@ -256,10 +256,10 @@ float CGround::LineGroundCol(float3 from, float3 to, bool synced) const
 
 	// Claming is done cause LineGroundSquareCol() operates on the 2 triangles faces each heightmap
 	// square is formed of.
-	const float ffsx = Clamp(from.x / SQUARE_SIZE, 0.0f, (float)gs->mapxm1);
-	const float ffsz = Clamp(from.z / SQUARE_SIZE, 0.0f, (float)gs->mapym1);
-	const float ttsx = Clamp(to.x / SQUARE_SIZE, 0.0f, (float)gs->mapxm1);
-	const float ttsz = Clamp(to.z / SQUARE_SIZE, 0.0f, (float)gs->mapym1);
+	const float ffsx = Clamp(from.x / SQUARE_SIZE, 0.0f, (float)gs->mapx);
+	const float ffsz = Clamp(from.z / SQUARE_SIZE, 0.0f, (float)gs->mapy);
+	const float ttsx = Clamp(to.x / SQUARE_SIZE, 0.0f, (float)gs->mapx);
+	const float ttsz = Clamp(to.z / SQUARE_SIZE, 0.0f, (float)gs->mapy);
 	const int fsx = ffsx; // a>=0: int(a):=floor(a)
 	const int fsz = ffsz;
 	const int tsx = ttsx;

commit 7bd4209c8b5cab56e4ed2e1422943ec41c5daf9c

Remove assertion caused by 0e28867

rts/Map/Ground.cpp
@@ -77,7 +77,7 @@ static inline float LineGroundSquareCol(
 	const int& ys)
 {
 	const bool inMap = (xs >= 0) && (ys >= 0) && (xs <= gs->mapxm1) && (ys <= gs->mapym1);
-	assert(inMap);
+//	assert(inMap);
 	if (!inMap)
 		return -1.0f;
 