spring/spring

commit 86494bc2240eb27bb713ed97f434d7f5ee5cc5e5

Attempt to fix mantis #3073 (Lag after midgame join)

rts/System/Net/UDPConnection.cpp
@@ -732,11 +732,14 @@ void UDPConnection::SendIfNecessary(bool flushed)
 					nak = 0; // 1 request is enough, unless high loss
 			}
 
+			bool first = true;
 			while (true) {
-				bool canResend = maxResend > 0 && ((buf.GetSize() + resIter->second->GetSize()) <= mtu);
+				bool canResend = maxResend > 0 && ((buf.GetSize() + (((netLossFactor == MIN_LOSS_FACTOR) || (rev == 0)) ? resIter->second->GetSize() : 
+					((rev == 1) ? resRevIter->second->GetSize() : resMidIter->second->GetSize()))) <= mtu);
 				bool canSendNew = !newChunks.empty() && ((buf.GetSize() + newChunks[0]->GetSize()) <= mtu);
 				if (!canResend && !canSendNew) {
-					todo = false;
+					if (first)
+						todo = false;
 					break;
 				}
 				resend = !resend; // alternate between send and resend to make sure none is starved
@@ -770,10 +773,12 @@ void UDPConnection::SendIfNecessary(bool flushed)
 					}
 					++resentChunks;
 					--maxResend;
+					first = false;
 				} else if (!resend && canSendNew) {
 					buf.chunks.push_back(newChunks[0]);
 					unackedChunks.push_back(newChunks[0]);
 					newChunks.pop_front();
+					first = false;
 				}
 			}
 

commit d61ecc37f94a2919cd8016033c43a609ee3d9792

test if UDP connection problems are caused by 86494bc2240eb

rts/System/Net/UDPConnection.cpp
@@ -734,13 +734,14 @@ void UDPConnection::SendIfNecessary(bool flushed)
 
 			bool first = true;
 			while (true) {
-				bool canResend = maxResend > 0 &&
-					((buf.GetSize() +
-					(((netLossFactor == MIN_LOSS_FACTOR) || (rev == 0)) ? resIter->second->GetSize() : ((rev == 1) ? resRevIter->second->GetSize() : resMidIter->second->GetSize())) // resend chunk size
-					) <= mtu);
+				const size_t resendChunkSize = (((netLossFactor == MIN_LOSS_FACTOR) || (rev == 0)) ? resIter->second->GetSize() : ((rev == 1) ? resRevIter->second->GetSize() : resMidIter->second->GetSize())); // resend chunk size
+				bool canResend  = maxResend > 0      && ((buf.GetSize() + resendChunkSize) <= mtu);
 				bool canSendNew = !newChunks.empty() && ((buf.GetSize() + newChunks[0]->GetSize()) <= mtu);
+
+				canResend = true; //FIXME DEBUG
+
 				if (!canResend && !canSendNew) {
-					if (first)
+					//FIXME if (first)
 						todo = false;
 					break;
 				}