57eb4c210a35c903b97e129e5d16da6ad22996d0
jK <-jk->
2012-06-21 18:01:46 +0200

    check if -mno-avx is available (seems to be 64bit only)

--- rts/build/cmake/TestCXXFlags.cmake
+++ rts/build/cmake/TestCXXFlags.cmake
@@ -41,14 +41,22 @@ EndIf (NOT DEFINED VISIBILITY_INLINES_HIDDEN)
 
 If    (NOT DEFINED SSE_FLAGS)
 	CHECK_CXX_ACCEPTS_FLAG("-msse -mfpmath=sse" HAS_SSE_FLAGS)
+	CHECK_CXX_ACCEPTS_FLAG("-mno-avx" HAS_AVX_FLAGS)
+	CHECK_CXX_ACCEPTS_FLAG("-mno-avx2" HAS_AVX2_FLAGS)
 	If    (HAS_SSE_FLAGS)
 		# activate SSE1 only
-		Set(SSE_FLAGS "-mmmx -msse -mfpmath=sse")
+		Set(SSE_FLAGS "-msse -mfpmath=sse")
+		#Set(SSE_FLAGS "${SSE_FLAGS} -mmmx")
 
 		# disable rest
-		#Set(SSE_FLAGS "${SSE_FLAGS} -mno-3dnow") tests showed it might sync 
+		#Set(SSE_FLAGS "${SSE_FLAGS} -mno-3dnow") tests showed it might sync
 		Set(SSE_FLAGS "${SSE_FLAGS} -mno-sse2 -mno-sse3 -mno-ssse3 -mno-sse4.1 -mno-sse4.2 -mno-sse4 -mno-sse4a")
-		Set(SSE_FLAGS "${SSE_FLAGS} -mno-avx -mno-avx2 -mno-fma -mno-fma4 -mno-xop -mno-lwp")
+		If    (HAS_AVX_FLAGS)
+			Set(SSE_FLAGS "${SSE_FLAGS} -mno-avx -mno-fma -mno-fma4 -mno-xop -mno-lwp")
+		EndIf (HAS_AVX_FLAGS)
+		If    (HAS_AVX2_FLAGS)
+			Set(SSE_FLAGS "${SSE_FLAGS} -mno-avx2")
+		EndIf (HAS_AVX2_FLAGS)
 	Else  (HAS_SSE_FLAGS)
 		Set(SSE_FLAGS "-DDEDICATED_NOSSE")
 		Message(WARNING "SSE1 support is missing, online play is highly discouraged with this build")
