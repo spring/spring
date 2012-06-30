0fcf96f405b793cdd49fe0394e95c96935f13d5a
abma <spring@abma.de>
2012-06-30 16:19:09 +0200

    validationtests: remove speed adjustment

--- test/validation/LuaUI/Widgets/test.lua
+++ test/validation/LuaUI/Widgets/test.lua
@@ -11,11 +11,7 @@ return {
 end
 
 local maxframes = 27000 -- run spring 15 minutes (ingame time)
-local minfps = 10 -- slow down gamespeed if below this value
-local maxfps = 30 -- increase gamespeed if fps higher as this value
-local initialspeed = 30 -- speed at the beginning
-local os_clock = os.clock
-local lastcheck = math.floor(os_clock())
+local initialspeed = 120 -- speed at the beginning
 
 function widget:GameFrame(n)
 	if n==0 then -- set gamespeed at start of game
@@ -23,18 +19,9 @@ function widget:GameFrame(n)
 			"setminspeed " .. initialspeed,
 			"setminspeed 1")
 	end
-	local now = math.floor(os_clock()) --try to run it at frame rate >10 and <30 by adjusting game speed
 	if n==maxframes then
 		Spring.Echo("Tests run long enough, quitting...")
 		Spring.SendCommands("quit")
-	elseif (n>100) and (now > lastcheck) then -- adjust fps only once a second
-		lastcheck = now
-		fps = Spring.GetFPS()
-		if (fps<minfps) then
-			Spring.SendCommands("slowdown")
-		elseif (fps>maxfps) then
-			Spring.SendCommands("speedup")
-		end
 	end
 end
 
