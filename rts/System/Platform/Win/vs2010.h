225944df9b4d1a07e60941bfb669807266372851
zerver <mr@zerver.net>
2012-07-01 18:33:41 +0200

    Reduce chances for compile error

--- rts/System/Platform/Win/vs2010.h
+++ rts/System/Platform/Win/vs2010.h
@@ -1,4 +1,4 @@
-#if _MSC_VER >= 1600
+#if defined(_MSC_VER) && _MSC_VER >= 1600
 // FIXME ugly hacks for MSVC 2010 due to the _CMATH_ preprocessor flag workaround
 // Unsigned int is intentional to avoid ambiguous call
 extern float ldexpf(float x, unsigned int y);
