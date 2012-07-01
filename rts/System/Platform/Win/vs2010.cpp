03cbae543d30ddbb29027b072e7a0a299b18bee5
zerver <mr@zerver.net>
2012-07-01 18:33:41 +0200

    Reduce chances for compile error

--- rts/System/Platform/Win/vs2010.cpp
+++ rts/System/Platform/Win/vs2010.cpp
@@ -1,4 +1,4 @@
-#if _MSC_VER >= 1600
+#if defined(_MSC_VER) && _MSC_VER >= 1600
 #include "lib/streflop/streflop_cond.h"
 // FIXME ugly hacks for MSVC 2010 due to the _CMATH_ preprocessor flag workaround
 // Unsigned int is intentional to avoid ambiguous call
