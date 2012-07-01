b178a50484606920e19cae15b901dbae24aed3e9
abma <spring@abma.de>
2012-07-01 16:17:54 +0200

    fix symbolic link

--- buildbot/slave/make_installer.sh
+++ buildbot/slave/make_installer.sh
@@ -87,7 +87,7 @@ mv ./installer/spring*.exe ${TMP_PATH}
 # create relative symbolic links to current files for rsyncing
 cd ${TMP_PATH}/..
 ln -sfv ${REV}/*.exe spring_testing.exe
-ln -sfv ${REV}/spring_${VERSION}_portable.7z spring_testing-portable.7z
+ln -sfv ${REV}/spring_${REV}_portable.7z spring_testing-portable.7z
 ln -sfv ${REV}/spring_${VERSION}_minimal-portable.7z spring_testing_minimal-portable.7z
 ln -sfv ${REV}/spring_${VERSION}_minimal-portable+dedicated.zip spring_testing_minimal-portable+dedicated.zip
 
