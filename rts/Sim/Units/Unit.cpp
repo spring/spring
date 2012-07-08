spring/spring

commit 1e79d4201fe73076411eacc88b6f1d2dd1b510ac

3154 (commandfire weapons jammed on user targets)

rts/Sim/Units/Unit.cpp
@@ -1002,11 +1002,16 @@ void CUnit::SlowUpdateWeapons() {
 		for (vector<CWeapon*>::iterator wi = weapons.begin(); wi != weapons.end(); ++wi) {
 			CWeapon* w = *wi;
 
-			// NOTE:
-			//     w->haveUserTarget can only be true if ::AttackUnit
-			//     was called with a non-NULL target-unit AND the CAI
-			//     did not auto-select it
-			if (!w->haveUserTarget) {
+			w->SlowUpdate();
+
+			if (w->haveUserTarget) {
+				// do not interfere with user targets
+				w->AttackUnit(attackTarget, true);
+			} else {
+				// NOTE:
+				//     w->haveUserTarget can only be true if ::AttackUnit
+				//     was called with a non-NULL target-unit AND the CAI
+				//     did not auto-select it
 				if ((haveManualFireRequest == (unitDef->canManualFire && w->weaponDef->manualfire))) {
 					if (attackTarget != NULL) {
 						w->AttackUnit(attackTarget, false);
@@ -1014,21 +1019,19 @@ void CUnit::SlowUpdateWeapons() {
 						w->AttackGround(attackPos, true);
 					}
 				}
+
+				if (lastAttacker == NULL)
+					continue;
+				if ((lastAttack + 200) <= gs->frameNum)
+					continue;
+				if (w->targetType != Target_None)
+					continue;
+				if (fireState == FIRESTATE_HOLDFIRE)
+					continue;
+
+				// return fire at our last attacker if allowed
+				w->AttackUnit(lastAttacker, false);
 			}
-
-			w->SlowUpdate();
-
-			if (lastAttacker == NULL)
-				continue;
-			if ((lastAttack + 200) <= gs->frameNum)
-				continue;
-			if (w->targetType != Target_None)
-				continue;
-			if (fireState == FIRESTATE_HOLDFIRE)
-				continue;
-
-			// return fire at our last attacker if allowed
-			w->AttackUnit(lastAttacker, false);
 		}
 	}
 }

rts/Sim/Weapons/Weapon.cpp
@@ -519,8 +519,9 @@ bool CWeapon::AttackGround(float3 pos, bool userTarget)
 
 bool CWeapon::AttackUnit(CUnit* unit, bool userTarget)
 {
-	if ((!userTarget && weaponDef->noAutoTarget))
+	if ((!userTarget && weaponDef->noAutoTarget)) {
 		return false;
+	}
 	if (weaponDef->interceptor)
 		return false;
 
@@ -587,8 +588,12 @@ void CWeapon::HoldFire()
 		DeleteDeathDependence(targetUnit, DEPENDENCE_TARGETUNIT);
 		targetUnit = NULL;
 	}
+
 	targetType = Target_None;
-	haveUserTarget = false;
+	// don't set this to false, otherwise a subsequent
+	// call to AttackUnit from Unit::SlowUpdateWeapons
+	// will abort the attack for noAutoTarget weapons
+	// haveUserTarget = false;
 }
 
 

commit a94fd456c9a06a966b2735ee1ccd323fce686d04

fix attack-ground bug reported by knorke; unintentionally caused by 1e79d4201f

rts/Sim/Units/Unit.cpp
@@ -1004,34 +1004,32 @@ void CUnit::SlowUpdateWeapons() {
 
 			w->SlowUpdate();
 
-			if (w->haveUserTarget) {
-				// do not interfere with user targets
-				w->AttackUnit(attackTarget, true);
-			} else {
-				// NOTE:
-				//     w->haveUserTarget can only be true if ::AttackUnit
-				//     was called with a non-NULL target-unit AND the CAI
-				//     did not auto-select it
-				if ((haveManualFireRequest == (unitDef->canManualFire && w->weaponDef->manualfire))) {
-					if (attackTarget != NULL) {
-						w->AttackUnit(attackTarget, false);
-					} else if (userAttackGround) {
-						w->AttackGround(attackPos, true);
-					}
+			// NOTE:
+			//     pass w->haveUserTarget so we do not interfere with
+			//     user targets; w->haveUserTarget can only be true if
+			//     either 1) ::AttackUnit was called with a (non-NULL)
+			//     target-unit which the CAI did *not* auto-select, or
+			//     2) ::AttackGround was called with any user-selected
+			//     position
+			if ((haveManualFireRequest == (unitDef->canManualFire && w->weaponDef->manualfire))) {
+				if (attackTarget != NULL) {
+					w->AttackUnit(attackTarget, w->haveUserTarget);
+				} else if (userAttackGround) {
+					w->AttackGround(attackPos, w->haveUserTarget);
 				}
-
-				if (lastAttacker == NULL)
-					continue;
-				if ((lastAttack + 200) <= gs->frameNum)
-					continue;
-				if (w->targetType != Target_None)
-					continue;
-				if (fireState == FIRESTATE_HOLDFIRE)
-					continue;
-
-				// return fire at our last attacker if allowed
-				w->AttackUnit(lastAttacker, false);
 			}
+
+			if (lastAttacker == NULL)
+				continue;
+			if ((lastAttack + 200) <= gs->frameNum)
+				continue;
+			if (w->targetType != Target_None)
+				continue;
+			if (fireState == FIRESTATE_HOLDFIRE)
+				continue;
+
+			// return fire at our last attacker if allowed
+			w->AttackUnit(lastAttacker, false);
 		}
 	}
 }