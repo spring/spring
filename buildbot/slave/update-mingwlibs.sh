c149ea5ac2106de2c4e4af3fa3e4eb649188fa06
abma <spring@abma.de>
2012-07-15 19:02:35 +0200

    remove builddir when mingwlibs are updated

--- buildbot/slave/update-mingwlibs.sh
+++ buildbot/slave/update-mingwlibs.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 set -e
-if [ $# != 1 ]; then
-	echo Usage: $0 /path/to/mingwlibs
+
+if [ $# != 3 ]; then
+	echo Usage: $0 config branch /path/to/mingwlibs
 	exit 1
 fi
 
+. buildbot/slave/prepare.sh
+
 cd $1
-git fetch
-git clean -f -d
-git reset --hard origin/master
+if [ -n "$(git fetch)" ]; then
+	# remote contains new commits, delete builddir
+	rm -rf "$BUILDDIR"
+	git clean -f -d
+	git reset --hard origin/master
+fi
 
