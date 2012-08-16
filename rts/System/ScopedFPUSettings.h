1d279e3049ef4a3e1b6fd0bb471e065591ec6ef3
zerver <mr@zerver.net>
2012-08-16 23:41:11 +0200

    Fix ScopedDisableFpuExceptions for MT

--- rts/System/ScopedFPUSettings.h
+++ rts/System/ScopedFPUSettings.h
@@ -2,16 +2,19 @@
 #define SCOPED_FPU_SETTINGS
 
 // dedicated is compiled w/o streflop!
-#if defined(__SUPPORT_SNAN__) && !defined(USE_GML) && !defined(DEDICATED)
+#if defined(__SUPPORT_SNAN__) && !defined(DEDICATED)
 
 class ScopedDisableFpuExceptions {
 public:
 	ScopedDisableFpuExceptions() {
-		streflop::fegetenv(&fenv);
-		streflop::feclearexcept(streflop::FPU_Exceptions(streflop::FE_INVALID | streflop::FE_DIVBYZERO | streflop::FE_OVERFLOW));
+		if (!GML::Enabled() || Threading::IsSimThread()) {
+			streflop::fegetenv(&fenv);
+			streflop::feclearexcept(streflop::FPU_Exceptions(streflop::FE_INVALID | streflop::FE_DIVBYZERO | streflop::FE_OVERFLOW));
+		}
 	}
 	~ScopedDisableFpuExceptions() {
-		streflop::fesetenv(&fenv);
+		if (!GML::Enabled() || Threading::IsSimThread())
+			streflop::fesetenv(&fenv);
 	}
 private:
 	streflop::fpenv_t fenv;
