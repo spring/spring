af436be6bd31c68ebbf737543e89f8225b20f516
jK <-jk->
2012-08-19 17:16:15 +0200

    cmake is a pain ...

--- buildbot/slave/linuxStatic.cmake
+++ buildbot/slave/linuxStatic.cmake
@@ -1,10 +1,12 @@
 
-SET(PREFER_STATIC_LIBS TRUE)
+## Note, the `CACHE ...` prefix is needed
+## else CMakeLists.txt overrides our given values!
 
-SET(BUILD_spring-headless  FALSE)
-SET(BUILD_spring-dedicated FALSE)
+SET(PREFER_STATIC_LIBS TRUE CACHE BOOL "")
+
+SET(BUILD_spring-headless  FALSE CACHE BOOL "")
+SET(BUILD_spring-dedicated FALSE CACHE BOOL "")
 SET(AI_TYPES "NONE" CACHE STRING "")
-
 
 SET(CMAKE_INSTALL_PREFIX "./" CACHE STRING "")
 SET(BINDIR  "./" CACHE STRING "")
@@ -12,6 +14,7 @@ SET(LIBDIR  "./" CACHE STRING "")
 SET(DATADIR "./" CACHE STRING "")
 SET(MANDIR  "share/man" CACHE STRING "")
 SET(DOCDIR  "share/doc/spring" CACHE STRING "")
+
 #SET(APPLICATIONS_DIR "share/applications" CACHE STRING "")
 #SET(PIXMAPS_DIR      "share/pixmaps" CACHE STRING "")
 #SET(MIME_DIR         "share/mime" CACHE STRING "")
