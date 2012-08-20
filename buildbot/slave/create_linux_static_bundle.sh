ce98847d315b4ef87599f5e458aa1075c11b3b04
jK <-jk->
2012-08-20 23:43:54 +0200

    first uploaded linux static bundle :)

--- buildbot/slave/create_linux_static_bundle.sh
+++ buildbot/slave/create_linux_static_bundle.sh
@@ -76,7 +76,6 @@ done
 
 # create relative symbolic links to current files for rsyncing
 cd ${TMP_PATH}/..
-ln -sfv ${REV}/spring-${FILEPREFIX} spring_testing-${FILEPREFIX}    spring_testing_minimal-portable-${FILEPREFIX}.7z
 ln -sfv ${REV}/spring_${VERSION}_minimal-portable-${FILEPREFIX}.7z  spring_testing_minimal-portable-${FILEPREFIX}.7z
 #ln -sfv ${REV}/spring_${VERSION}_minimal-portable-${FILEPREFIX}.zip spring_testing_minimal-portable-${FILEPREFIX}.zip
 
