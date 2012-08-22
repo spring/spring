c9537a0288e258c2b7823a62574532ccdfe72789
abma <spring@abma.de>
2012-08-22 09:06:25 +0200

    Revert "Fix ScopedDisableFpuExceptions for MT"
    This reverts commit abfd867ae4291f5bdac32371af37121acbcd85b3.

--- rts/System/ScopedFPUSettings.h
+++ rts/System/ScopedFPUSettings.h
@@ -2,19 +2,16 @@
 #define SCOPED_FPU_SETTINGS
 
 // dedicated is compiled w/o streflop!
-#if defined(__SUPPORT_SNAN__) && !defined(DEDICATED)
+#if defined(__SUPPORT_SNAN__) && !defined(USE_GML) && !defined(DEDICATED)
 
 class ScopedDisableFpuExceptions {
 public:
 	ScopedDisableFpuExceptions() {
-		if (!GML::Enabled() || Threading::IsSimThread()) {
-			streflop::fegetenv(&fenv);
-			streflop::feclearexcept(streflop::FPU_Exceptions(streflop::FE_INVALID | streflop::FE_DIVBYZERO | streflop::FE_OVERFLOW));
-		}
+		streflop::fegetenv(&fenv);
+		streflop::feclearexcept(streflop::FPU_Exceptions(streflop::FE_INVALID | streflop::FE_DIVBYZERO | streflop::FE_OVERFLOW));
 	}
 	~ScopedDisableFpuExceptions() {
-		if (!GML::Enabled() || Threading::IsSimThread())
-			streflop::fesetenv(&fenv);
+		streflop::fesetenv(&fenv);
 	}
 private:
 	streflop::fpenv_t fenv;
