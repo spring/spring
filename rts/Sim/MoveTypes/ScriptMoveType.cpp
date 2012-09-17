589611486ef6b1bd91ea47f5c7f2d3f10548236f
rt <rt@rt-0.(none)>
2012-09-17 16:04:40 +0200

    ScriptMoveType: call Move3D in SetPosition

--- rts/Sim/MoveTypes/ScriptMoveType.cpp
+++ rts/Sim/MoveTypes/ScriptMoveType.cpp
@@ -196,18 +196,17 @@ void CScriptMoveType::CheckLimits()
 }
 
 
-void CScriptMoveType::SetPhysics(const float3& pos,
-                                 const float3& vel,
-                                 const float3& rot)
+void CScriptMoveType::SetPhysics(const float3& _pos,
+                                 const float3& _vel,
+                                 const float3& _rot)
 {
-	owner->pos = pos;
-	owner->speed = vel;
-
-	SetRotation(rot);
+	SetPosition(_pos);
+	SetVelocity(_vel);
+	SetRotation(_rot);
 }
 
 
-void CScriptMoveType::SetPosition(const float3& pos) { owner->pos = pos; }
+void CScriptMoveType::SetPosition(const float3& pos) { owner->Move3D(pos, false); }
 void CScriptMoveType::SetVelocity(const float3& _vel) { owner->speed = (vel = _vel); }
 
 
