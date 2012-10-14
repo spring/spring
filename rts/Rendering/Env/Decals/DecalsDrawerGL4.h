6d2f3870a2c5150734baf486e1c6a4a0a2bda0ec
jK <-jk->
2012-10-14 17:56:38 +0200

    add new GL4 based decal drawer (WIP)

---
+++ rts/Rendering/Env/Decals/DecalsDrawerGL4.h
@@ -0,0 +1,141 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#ifndef SHADER_GROUND_DECAL_DRAWER_H
+#define SHADER_GROUND_DECAL_DRAWER_H
+
+#include <list>
+#include <vector>
+
+#include "Rendering/Env/IGroundDecalDrawer.h"
+#include "Rendering/GL/myGL.h"
+#include "Rendering/GL/VBO.h"
+#include "System/EventClient.h"
+#include "Sim/Projectiles/ExplosionListener.h"
+#include "System/float3.h"
+#include "System/float4.h"
+#include "System/Vec2.h"
+
+
+namespace Shader {
+	struct IProgramObject;
+}
+
+
+class CDecalsDrawerGL4: public IGroundDecalDrawer, public CEventClient, public IExplosionListener
+{
+public:
+	CDecalsDrawerGL4();
+	virtual ~CDecalsDrawerGL4();
+
+	virtual void Draw();
+	virtual void Update() {}
+
+	virtual void ForceRemoveSolidObject(CSolidObject* object) {/*FIXME*/}
+	virtual void RemoveSolidObject(CSolidObject* object, GhostSolidObject* gb) {/*FIXME*/}
+	virtual void GhostDestroyed(GhostSolidObject* gb) {/*FIXME*/}
+	virtual void GhostCreated(CSolidObject* object, GhostSolidObject* gb) {/*FIXME*/}
+
+	virtual void ExplosionOccurred(const CExplosionEvent&);
+
+private:
+	void AddExplosion(float3 pos, float damage, float radius, bool addScar);
+
+public:
+	bool WantsEvent(const std::string& eventName) {
+		return
+			   (eventName == "UnitCreated")
+			|| (eventName == "UnitDestroyed")
+			|| (eventName == "ViewResize")
+		;
+		/*return
+			(eventName == "UnitMoved") ||
+			(eventName == "SunChanged");*/
+	}
+	bool GetFullRead() const { return true; }
+	int GetReadAllyTeam() const { return AllAccessTeam; }
+
+	void UnitCreated(const CUnit* unit, const CUnit* builder);
+	void UnitDestroyed(const CUnit* unit, const CUnit* attacker);
+
+	void ViewResize();
+
+	//void SunChanged(const float3& sunDir);
+	//void UnitMoved(const CUnit*);
+
+private:
+	struct Decal {
+		Decal()
+			: pos(ZeroVector)
+			, size(0.0f, 0.0f)
+			, rot(0.0f)
+			, alpha(1.0f)
+			, texOffsets(0.0f, 0.0f, 0.0f, 0.0f)
+		{}
+
+		float3 pos;
+		float2 size;
+		float rot;
+		float alpha;
+		float4 texOffsets;
+	};
+
+	struct BuildingGroundDecal { //FIXME
+		BuildingGroundDecal()
+			: owner(NULL)
+			, gbOwner(NULL)
+			, size(0,0)
+			, facing(-1)
+			, pos(ZeroVector)
+			, radius(0.0f)
+			, alpha(1.0f)
+			, alphaFalloff(1.0f)
+		{}
+
+		const CUnit* owner;
+		const GhostSolidObject* gbOwner;
+		int2 size;
+		int facing;
+		float3 pos;
+		float radius;
+		float alpha;
+		float alphaFalloff;
+	};
+
+private:
+	void LoadScarTexture();
+	void LoadShaders();
+	void GenerateAtlasTexture();
+
+	void CreateBoundingBoxVBOs();
+	void CreateStructureVBOs();
+
+	void UpdateVisibilityVBO();
+	void UpdateDecalsVBO();
+
+	void DrawDecals();
+	//void DrawTracks();
+	//void DrawBuildingDecals();
+
+private:
+	VBO vboVertices;
+	VBO vboIndices;
+	VBO uboDecalsStructures;
+	VBO uboGroundLighting;
+	VBO vboVisibilityFeeback;
+
+	std::vector<Decal*> decals; //FIXME use mt-safe container!
+	size_t lastUpdate;
+
+	size_t maxDecals;
+
+	GLuint tbo;
+	GLuint scarTex;
+	GLuint depthTex;
+	GLuint atlasTex;
+	//GLuint decalShader;
+	Shader::IProgramObject* decalShader;
+};
+
+extern CDecalsDrawerGL4* shaderGroundDecals;
+
+#endif // SHADER_GROUND_DECAL_DRAWER_H 
--- rts/Rendering/Env/IGroundDecalDrawer.cpp
+++ rts/Rendering/Env/IGroundDecalDrawer.cpp
@@ -2,7 +2,9 @@
 
 #include "IGroundDecalDrawer.h"
 #include "Rendering/Env/Decals/GroundDecalHandler.h"
+#include "Rendering/Env/Decals/DecalsDrawerGL4.h"
 #include "System/Config/ConfigHandler.h"
+#include "System/Log/ILog.h"
 
 
 CONFIG(int, GroundDecals).defaultValue(1);
@@ -10,6 +12,7 @@ CONFIG(int, GroundDecals).defaultValue(1);
 bool IGroundDecalDrawer::drawDecals = 0;
 unsigned int IGroundDecalDrawer::decalLevel = 0;
 
+static IGroundDecalDrawer* singleton = NULL;
 
 IGroundDecalDrawer::IGroundDecalDrawer()
 {
@@ -19,6 +22,29 @@ IGroundDecalDrawer::IGroundDecalDrawer()
 
 IGroundDecalDrawer* IGroundDecalDrawer::GetInstance()
 {
-	static CGroundDecalHandler groundDecl; //FIXME
-	return &groundDecl;
+	if (!singleton) {
+#if 0
+		try {
+			singleton = new CDecalsDrawerGL4();
+			LOG_L(L_INFO, "Loaded DecalsDrawer: %s", "GL4");
+		} catch(const unsupported_error& ex) {
+			LOG_L(L_ERROR, "IGroundDecalDrawer loading failed: %s", ex.what());
+#endif
+			delete singleton;
+			singleton = new CGroundDecalHandler();
+			LOG_L(L_INFO, "Loaded DecalsDrawer: %s", "Legacy");
+
+#if 0
+		}
+#endif
+	}
+
+	return singleton;
+}
+
+
+void IGroundDecalDrawer::FreeInstance()
+{
+	delete singleton;
+	singleton = NULL;
 }
