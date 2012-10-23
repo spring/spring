b1c7fbf86cabddd6986b7b4e6117d79d0728576f
jK <-jk->
2012-10-23 15:10:11 +0200

    add fog to GL4 decal renderer & fix normal calculation

--- cont/base/springcontent/shaders/GLSL/DecalsFragGL4.glsl
+++ cont/base/springcontent/shaders/GLSL/DecalsFragGL4.glsl
@@ -23,6 +23,7 @@ layout(binding=4) uniform sampler2D depthTex;
 layout(binding=5) uniform sampler2D normalMap;
 layout(binding=6) uniform sampler2D normalMap2;
 
+uniform vec3 camPos;
 uniform vec2 invScreenSize;
 uniform vec2 invMapSize;
 uniform vec2 invMapSizePO2; // 1.0f / (pwr2map{x,z} * SQUARE_SIZE)  needed for infotex & shadingtex
@@ -33,6 +34,10 @@ layout(std140, binding=1) uniform SGroundLighting
 	uniform vec3 diffuseColor;
 	uniform vec3 specularColor;
 	uniform vec3 dir;
+
+	uniform vec3 fogColor;
+	uniform float fogEnd;
+	uniform float fogScale;
 } groundLighting;
 
 #ifdef HAVE_SHADOWS
@@ -41,27 +46,11 @@ uniform float shadowDensity;
 layout(binding=2) uniform sampler2DShadow shadowTex;
 #endif
 
-/*flat in int instanceID;
-layout(binding=7) uniform samplerBuffer decalInfo;
-
-SScar GetDecalInfo() {
-	vec4 info1 = texelFetch(decalInfo, instanceID*3 + 0);
-	vec4 info2 = texelFetch(decalInfo, instanceID*3 + 1);
-	vec4 info3 = texelFetch(decalInfo, instanceID*3 + 2);
-	SDecal decal;
-	decal.pos       = info1.xyz;
-	decal.rot       = info1.w;
-	decal.texcoords = info2.xy;
-	decal.size      = info2.zw;
-	decal.alpha     = info3.x;
-	return decal;
-}*/
-
 flat in SDecal decal;
 flat in mat2 rot;
 
 vec3 ReconstructWorldPos() {
-	vec2 screenCoord = gl_FragCoord.st * invScreenSize;
+	vec2 screenCoord = (gl_FragCoord.st + 0.5) * invScreenSize;
 	float z = texture2D(depthTex, screenCoord).x;
 
 	vec4 ppos;
@@ -72,20 +61,29 @@ vec3 ReconstructWorldPos() {
 }
 
 vec3 GetDiffuseLighting(vec3 worldPos, vec2 ntx) {
-	vec3 normal = texture2D(normalMap, ntx).grr;
-	normal.y = sqrt(1.0 - clamp(dot(normal.xz, normal.xz), 0.0, 1.0));
+	//vec3 normal = texture2D(normalMap, ntx).rgg;
+	vec3 normal = texture2D(normalMap, ntx).rgb;
+	normal = (normal * 2.0) - 1.0;
+	//normal.y = sqrt(1.0 - clamp(dot(normal.xz, normal.xz), 0.0, 1.0));
 
-	vec3 normal2 = texture2D(normalMap2, ntx * 1.789).grr;
-	normal2.y = sqrt(1.0 - clamp(dot(normal2.xz, normal2.xz), 0.0, 1.0));
-	normal = normalize(normal * 3.0 + normal2);
+	vec3 normal2 = texture2D(normalMap2, ntx * 5.789).rgb;
+	normal2 = (normal2 * 2.0) - 1.0;
+	//normal2.y = sqrt(1.0 - clamp(dot(normal2.xz, normal2.xz), 0.0, 1.0));
+	normal = normalize(normal * 1.0 + normal2);
+
+	normal = normal;
+	normal.xy = rot * normal.xy;
 
 	vec3 groundNormal;
 	groundNormal.xz = texture2D(groundNormalsTex, worldPos.xz * invMapSize).ra;
 	groundNormal.y  = sqrt(1.0 - dot(groundNormal.xz, groundNormal.xz));
-	groundNormal = normalize(groundNormal + normal * 3.0);
-	float normalSunCos = max(0.0, dot(groundNormal, groundLighting.dir));
 
-	//vec3 diffuse = groundLighting.diffuseColor * pow(normalSunCos, 8.0f);
+	vec3 front = normalize(cross(groundNormal, vec3(-1.,0.,0.)));
+	vec3 right = cross(groundNormal, front);
+	groundNormal = groundNormal * normal.z + front * normal.y + right * normal.x;
+	groundNormal = normalize(groundNormal);
+
+	float normalSunCos = max(0.0, dot(groundNormal, groundLighting.dir));
 	vec3 diffuse = groundLighting.diffuseColor * normalSunCos;
 	return diffuse;
 }
@@ -103,23 +101,27 @@ float GetShadowOcclusion(vec3 worldPos) {
 
 
 void main() {
-	//SDecal decal = GetDecalInfo();
+	gl_FragColor = vec4(0.0);
+
+	// PROJECTION
 	vec3 worldPos = ReconstructWorldPos();
 	vec3 relPos   = worldPos - decal.pos;
 
 	vec2 ntx; // range: -1 .. 0 .. +1
-	ntx.x = dot(relPos.xz, rot * vec2(1.0,0.0)) / decal.size.x;
-	ntx.y = dot(relPos.xz, rot * vec2(0.0,1.0)) / decal.size.y;
+	ntx.x = dot(relPos.xz, rot[0]) / decal.size.x;
+	ntx.y = dot(relPos.xz, rot[1]) / decal.size.y;
 
 	bool outside = any(greaterThan(abs(ntx), vec2(1.0)));
 	if (outside) {
-		gl_FragColor = vec4(0.0);
-		return;
+		// yeah, this branch is worth it and discard too (we neither rely on early-z/stencil, so it doesn't matter)
+		discard;
 	}
 
 	ntx = (ntx + 1.0) * 0.5; // range: 0..+1
 	vec2 tx = mix(decal.texcoords.st, decal.texcoords.pq, ntx);
 
+
+	// TEXTURING
 	vec4 albedo = texture2D(decalTex, tx);
 	albedo.a *= 1.0 - abs(relPos.y) / decal.size.x; // make transparent when terrain is higher or lower than the decal's pos
 	albedo.a *= decal.alpha;
@@ -135,6 +137,8 @@ void main() {
 	gl_FragColor.rgb *= texture2D(infoTex, worldPos.xz * invMapSizePO2).rgb;
 #endif
 
-	// check if pixel is in decal dist
-	gl_FragColor.a = gl_FragColor.a * (1.0 - float(outside));
+	// FOG
+	float fogFactor = (groundLighting.fogEnd - length(worldPos - camPos)) * groundLighting.fogScale;
+	fogFactor = clamp(fogFactor, 0.0, 1.0);
+	gl_FragColor.rgb = mix(groundLighting.fogColor, gl_FragColor.rgb, fogFactor);
 }
