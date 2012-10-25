5f1fc86bbfb6e7decc9e5a44d4eab95cff6b4686
abma <spring@abma.de>
2012-10-26 01:58:09 +0200

    validation test: log runtime duration

--- test/validation/LuaUI/Widgets/test.lua
+++ test/validation/LuaUI/Widgets/test.lua
@@ -12,15 +12,19 @@ end
 
 local maxframes = 27000 -- run spring 15 minutes (ingame time)
 local initialspeed = 120 -- speed at the beginning
+local timer
 
 function widget:GameFrame(n)
 	if n==0 then -- set gamespeed at start of game
 		Spring.SendCommands("setmaxspeed " .. 1000,
 			"setminspeed " .. initialspeed,
 			"setminspeed 1")
+		timer = Spring.GetTimer()
 	end
 	if n==maxframes then
+		local time = Spring.DiffTimers(Spring.GetTimer(), timer)
 		Spring.Echo("Tests run long enough, quitting...")
+		Spring.Echo(string.format('Game over, realtime: %i seconds, gametime: %i seconds', time, Spring.GetGameSeconds()))
 		Spring.SendCommands("quit")
 	end
 end
