c207eb474fd691ad4283c33cea9378ff7549b84e
jK <-jk->
2012-11-07 16:16:42 +0100

    fix FIXME

--- rts/System/StartScriptGen.h
+++ rts/System/StartScriptGen.h
@@ -1,14 +1,16 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-class StartScriptGen {
+#include <string>
 
-public:
+namespace StartScriptGen {
+
 	/**
 	* creates an empty script.txt with game & map, no error checking is done!
 	* @param game name of the game
 	* @param map name of the map
 	*/
-	static std::string CreateMinimalSetup(const std::string game, const std::string map); //FIXME: should be non-static
+	std::string CreateMinimalSetup(const std::string& game, const std::string& map);
+
 	/**
 	* creates an empty script.txt with game & map, only few error checking is done!
 	* @param game name of the game
@@ -16,7 +18,5 @@ public:
 	* @param ai ai to use (Lua/Skirmish), if not found "empty" player is used
 	* @param playername name to use ingame
 	*/
-	static std::string CreateDefaultSetup(const std::string& map, const std::string& game, const std::string& ai, const std::string& playername); //FIXME: should be non-static
-
+	std::string CreateDefaultSetup(const std::string& map, const std::string& game, const std::string& ai, const std::string& playername);
 };
-
