1956ccd515b57143a09c5e0ba11e0631b6408ca1
abma <spring@abma.de>
2012-11-19 19:34:13 +0100

    update 7z to v9.20

--- rts/System/FileSystem/Archives/SevenZipArchive.cpp
+++ rts/System/FileSystem/Archives/SevenZipArchive.cpp
@@ -9,18 +9,76 @@
 
 extern "C" {
 #include "lib/7z/Types.h"
-#include "lib/7z/Archive/7z/7zAlloc.h"
-#include "lib/7z/Archive/7z/7zExtract.h"
+#include "lib/7z/7zAlloc.h"
 #include "lib/7z/7zCrc.h"
 }
 
 #include "System/Util.h"
 #include "System/Log/ILog.h"
 
+static Byte kUtf8Limits[5] = { 0xC0, 0xE0, 0xF0, 0xF8, 0xFC };
+static Bool Utf16_To_Utf8(char *dest, size_t *destLen, const UInt16 *src, size_t srcLen)
+{
+	size_t destPos = 0, srcPos = 0;
+	for (;;) {
+		unsigned numAdds;
+		UInt32 value;
+		if (srcPos == srcLen) {
+			*destLen = destPos;
+			return True;
+		}
+		value = src[srcPos++];
+		if (value < 0x80) {
+			if (dest)
+				dest[destPos] = (char)value;
+			destPos++;
+			continue;
+		}
+		if (value >= 0xD800 && value < 0xE000) {
+			UInt32 c2;
+			if (value >= 0xDC00 || srcPos == srcLen)
+				break;
+			c2 = src[srcPos++];
+			if (c2 < 0xDC00 || c2 >= 0xE000)
+				break;
+			value = (((value - 0xD800) << 10) | (c2 - 0xDC00)) + 0x10000;
+		}
+		for (numAdds = 1; numAdds < 5; numAdds++)
+			if (value < (((UInt32)1) << (numAdds * 5 + 6)))
+				break;
+		if (dest)
+			dest[destPos] = (char)(kUtf8Limits[numAdds - 1] + (value >> (6 * numAdds)));
+		destPos++;
+		do {
+			numAdds--;
+			if (dest)
+				dest[destPos] = (char)(0x80 + ((value >> (6 * numAdds)) & 0x3F));
+			destPos++;
+		} while (numAdds != 0);
+	}
+	*destLen = destPos;
+	return False;
+}
 
 CSevenZipArchiveFactory::CSevenZipArchiveFactory()
 	: IArchiveFactory("sd7")
 {
+}
+
+int CSevenZipArchive::GetFileName(const CSzArEx* db, int i)
+{
+	size_t len = SzArEx_GetFileNameUtf16(db, i, NULL);
+
+	if (len > tempBufSize) {
+		SzFree(NULL, tempBuf);
+		tempBufSize = len;
+		tempBuf = (UInt16 *)SzAlloc(NULL, tempBufSize * sizeof(tempBuf[0]));
+		if (tempBuf == 0) {
+			return SZ_ERROR_MEM;
+		}
+	}
+	tempBuf[len-1] = 0;
+	return SzArEx_GetFileNameUtf16(db, i, tempBuf);
 }
 
 IArchive* CSevenZipArchiveFactory::DoCreateArchive(const std::string& filePath) const
@@ -28,18 +86,38 @@ IArchive* CSevenZipArchiveFactory::DoCreateArchive(const std::string& filePath) 
 	return new CSevenZipArchive(filePath);
 }
 
-
-CSevenZipArchive::CSevenZipArchive(const std::string& name) :
-	CBufferedArchive(name, false),
-	isOpen(false)
+const char* CSevenZipArchive::GetErrorStr(int err)
 {
-	blockIndex = 0xFFFFFFFF;
-	outBuffer = NULL;
-	outBufferSize = 0;
+	switch(err) {
+	case SZ_OK:
+		return "OK";
+	case SZ_ERROR_FAIL:
+		return "Extracting failed";
+	case SZ_ERROR_CRC:
+		return "CRC error (archive corrupted?)";
+	case SZ_ERROR_INPUT_EOF:
+		return "Unexpected end of file (truncated?)";
+	case SZ_ERROR_MEM:
+		return "Out of memory";
+	case SZ_ERROR_UNSUPPORTED:
+		return "Unsupported archive";
+	case SZ_ERROR_NO_ARCHIVE:
+		return "Archive not found";
+	}
+	return "Unknown error";
+}
 
+
+CSevenZipArchive::CSevenZipArchive(const std::string& name):
+	CBufferedArchive(name, false),
+	blockIndex(0xFFFFFFFF),
+	outBuffer(NULL),
+	outBufferSize(0),
+	tempBuf(NULL),
+	tempBufSize(0)
+{
 	allocImp.Alloc = SzAlloc;
 	allocImp.Free = SzFree;
-
 	allocTempImp.Alloc = SzAllocTemp;
 	allocTempImp.Free = SzFreeTemp;
 
@@ -66,52 +144,39 @@ CSevenZipArchive::CSevenZipArchive(const std::string& name) :
 		isOpen = true;
 	} else {
 		isOpen = false;
-		std::string error;
-		switch (res) {
-			case SZ_ERROR_FAIL:
-				error = "Extracting failed";
-				break;
-			case SZ_ERROR_CRC:
-				error = "CRC error (archive corrupted?)";
-				break;
-			case SZ_ERROR_INPUT_EOF:
-				error = "Unexpected end of file (truncated?)";
-				break;
-			case SZ_ERROR_MEM:
-				error = "Out of memory";
-				break;
-			case SZ_ERROR_UNSUPPORTED:
-				error = "Unsupported archive";
-				break;
-			case SZ_ERROR_NO_ARCHIVE:
-				error = "Archive not found";
-				break;
-			default:
-				error = "Unknown error";
-				break;
-		}
-		LOG_L(L_ERROR, "Error opening %s: %s", name.c_str(), error.c_str());
+		LOG_L(L_ERROR, "Error opening %s: %s", name.c_str(), GetErrorStr(res));
 		return;
 	}
 
 	// In 7zip talk, folders are pack-units (solid blocks),
 	// not related to file-system folders.
 	UInt64* folderUnpackSizes = new UInt64[db.db.NumFolders];
-	for (int fi = 0; fi < db.db.NumFolders; fi++) {
+	for (unsigned int fi = 0; fi < db.db.NumFolders; fi++) {
 		folderUnpackSizes[fi] = SzFolder_GetUnpackSize(db.db.Folders + fi);
 	}
 
 	// Get contents of archive and store name->int mapping
 	for (unsigned int i = 0; i < db.db.NumFiles; ++i) {
 		CSzFileItem* f = db.db.Files + i;
-		if ((f->Size >= 0) && !f->IsDir) {
-			std::string fileName = f->Name;
+		if (!f->IsDir) {
+			int written = GetFileName(&db, i);
+			if (written<=0) {
+				LOG_L(L_ERROR, "Error getting filename in Archive: %s %d, file skipped in %s", GetErrorStr(res), res, name.c_str());
+				continue;
+			}
+
+			char buf[1024];
+			size_t dstlen = sizeof(buf);
+
+			Utf16_To_Utf8(buf, &dstlen, tempBuf, written);
 
 			FileData fd;
-			fd.origName = fileName;
+
+			fd.origName=buf;
 			fd.fp = i;
 			fd.size = f->Size;
-			fd.crc = (f->Size > 0) ? f->FileCRC : 0;
+			fd.crc = (f->Size > 0) ? f->Crc: 0;
+
 			const UInt32 folderIndex = db.FileIndexToFolderIndexMap[i];
 			if (folderIndex == ((UInt32)-1)) {
 				// file has no folder assigned
@@ -121,7 +186,7 @@ CSevenZipArchive::CSevenZipArchive(const std::string& name) :
 				fd.unpackedSize = folderUnpackSizes[folderIndex];
 				fd.packedSize   = db.db.PackSizes[folderIndex];
 			}
-
+			std::string fileName = fd.origName;
 			StringToLowerInPlace(fileName);
 			fileData.push_back(fd);
 			lcNameIndex[fileName] = fileData.size()-1;
@@ -130,6 +195,7 @@ CSevenZipArchive::CSevenZipArchive(const std::string& name) :
 
 	delete [] folderUnpackSizes;
 }
+
 
 CSevenZipArchive::~CSevenZipArchive()
 {
@@ -140,6 +206,9 @@ CSevenZipArchive::~CSevenZipArchive()
 		File_Close(&archiveStream.file);
 	}
 	SzArEx_Free(&db, &allocImp);
+	SzFree(NULL, tempBuf);
+	tempBuf = NULL;
+	tempBufSize = 0;
 }
 
 bool CSevenZipArchive::IsOpen()
@@ -161,7 +230,7 @@ bool CSevenZipArchive::GetFileImpl(unsigned int fid, std::vector<boost::uint8_t>
 	size_t outSizeProcessed;
 	SRes res;
 
-	res = SzAr_Extract(&db, &lookStream.s, fileData[fid].fp, &blockIndex, &outBuffer, &outBufferSize, &offset, &outSizeProcessed, &allocImp, &allocTempImp);
+	res = SzArEx_Extract(&db, &lookStream.s, fileData[fid].fp, &blockIndex, &outBuffer, &outBufferSize, &offset, &outSizeProcessed, &allocImp, &allocTempImp);
 	if (res == SZ_OK) {
 		buffer.resize(outSizeProcessed);
 		memcpy(&buffer[0], (char*)outBuffer+offset, outSizeProcessed);
