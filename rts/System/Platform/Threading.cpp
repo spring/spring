d9297d8e1360f22f4fede19eccb0e6dbf2ae87ec
jK <-jk->
2012-11-23 17:17:44 +0100

    Linux: only set SCHED_BATCH for mainthread

--- rts/System/Platform/Threading.cpp
+++ rts/System/Platform/Threading.cpp
@@ -35,7 +35,7 @@ namespace Threading {
 	boost::uint32_t SetAffinity(boost::uint32_t cores_bitmask, bool hard)
 	{
 		if (cores_bitmask == 0) {
-			return 0xFFFFFF;
+			return ~0;
 		}
 
 	#if defined(__APPLE__)
@@ -92,7 +92,7 @@ namespace Threading {
 
 	void SetAffinityHelper(const char *threadName, uint32_t affinity) {
 		const uint32_t cpuMask  = Threading::SetAffinity(affinity);
-		if (cpuMask == 0xFFFFFF) {
+		if (cpuMask == ~0) {
 			LOG("[Threading] %s thread CPU affinity not set", threadName);
 		}
 		else if (cpuMask != affinity) {
@@ -120,15 +120,15 @@ namespace Threading {
 
 	void SetThreadScheduler()
 	{
-		if (!GML::Enabled()) {
-		#if defined(__APPLE__)
-			// no-op
+	#if defined(__APPLE__)
+		// no-op
 
-		#elif defined(WIN32)
-			//TODO add MMCSS (http://msdn.microsoft.com/en-us/library/ms684247.aspx)
-			//Note: only available with mingw64!!!
+	#elif defined(WIN32)
+		//TODO add MMCSS (http://msdn.microsoft.com/en-us/library/ms684247.aspx)
+		//Note: only available with mingw64!!!
 
-		#else
+	#else
+		if (!GML::Enabled()) { // with GML mainthread yields a lot, so SCHED_BATCH with its longer wakup times is counter-productive then
 			if (GetAvailableCores() > 1) {
 				// Change os scheduler for this process.
 				// This way the kernel knows that we are a CPU-intensive task
@@ -137,14 +137,13 @@ namespace Threading {
 				//Note:
 				// It _may_ be possible that this has negative impact in case
 				// threads are waiting for mutexes (-> less yields).
-				sched_param sp;
-				sched_getparam(0, &sp);
-				sp.sched_priority += 2;
-				sp.sched_priority = Clamp(sp.sched_priority, sched_get_priority_min(SCHED_BATCH), sched_get_priority_max(SCHED_BATCH));
-				sched_setscheduler(0, SCHED_BATCH, &sp);
+				int policy;
+				struct sched_param param;
+				pthread_getschedparam(Threading::GetCurrentThread(), &policy, &param);
+				pthread_setschedparam(Threading::GetCurrentThread(), SCHED_BATCH, &param);
 			}
-		#endif
 		}
+	#endif
 	}
 
 
