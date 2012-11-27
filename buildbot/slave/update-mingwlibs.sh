e7a4e86b947536b6d16bf7bae8d560851b7f4dc8
jK <-jk->
2012-11-27 20:12:59 +0100

    revert to /slave/mingwlibs

--- buildbot/slave/update-mingwlibs.sh
+++ buildbot/slave/update-mingwlibs.sh
@@ -9,12 +9,12 @@ fi
 
 . buildbot/slave/prepare.sh
 
-if [ ! -d ~/$1 ]; then
+if [ ! -d $1 ]; then
 	echo "clone mingwlibs git-repo"
-	git clone git://github.com/spring/mingwlibs.git ~/$1
+	git clone git://github.com/spring/mingwlibs.git $1
 fi
 
-cd ~/$1
+cd $1
 
 GITOUTPUT=$(git fetch 2>&1 |cat)
 echo $GITOUTPUT
