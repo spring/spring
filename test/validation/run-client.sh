950aedf600600494dd0d1e1899d36ce37d47b372
abma <spring@abma.de>
2012-11-29 21:05:42 +0100

    validation test: dump ~/.spring/cache/QTPFS

--- test/validation/run-client.sh
+++ test/validation/run-client.sh
@@ -16,8 +16,12 @@ for (( i=0; $i<$MAXWAIT; i++ ));
 do
 	if [ -s ~/.spring/infolog.txt ] && [ -n "$(grep "Finalizing" ~/.spring/infolog.txt)" ];
 	then
-		# flushing path-caches to disk
-		sync
+
+		if [ -d ~/.spring/cache/QTPFS ]; then
+			ls -lR ~/.spring/cache/QTPFS
+			find ~/.spring/cache/QTPFS -type f |xargs md5sum
+		fi
+
 		LOG=$(mktemp)
 		echo "Starting $HEADLESS client"
 		set +e
