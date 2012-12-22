7d99f79848267dc81f9ef1cb781d6312e17ad411
abma <spring@abma.de>
2012-12-22 23:49:50 +0100

    validation test: add additional step for analyzing core dumps

---
+++ buildbot/slave/tests-analyze.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+set -e
+. buildbot/slave/prepare.sh
+
+TESTDIR=${TMP_BASE}/tests
+SPRING=${TESTDIR}/usr/local/bin/spring-headless
+
+if ! [ -x $SPRING ]; then
+	echo Usage: $0 /path/to/spring
+	exit 1
+fi
+
+EXITCODE=0
+
+for i in $(find ${TESTDIR} -maxdepth 1 -type f -name "core.*") ; do
+
+	SPRING=$1
+	COREFILE=$i
+
+	echo Core file found, creating backtrace
+	GDBCMDS=$(mktemp)
+	(
+		echo file $SPRING
+		echo core-file $COREFILE
+		echo info program
+		echo bt full
+		echo quit
+	)>$GDBCMDS
+	gdb -batch -x $GDBCMDS
+	cat $GDBCMDS
+	# cleanup
+	rm -f $GDBCMDS
+	rm -f $COREFILE
+	EXITCODE=1
+done
+
+exit $EXITCODE
