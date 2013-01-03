e4ca4f3487a6db4004fe1d9c3afb49464c48d9bb
abma <spring@abma.de>
2013-01-03 11:10:50 +0100

    buildbot: fix error when dir doesn't exist

--- buildbot/slave/tests-cleanup.sh
+++ buildbot/slave/tests-cleanup.sh
@@ -7,7 +7,9 @@ set -e
 TESTDIR=${TMP_BASE}/tests
 
 #FIXME/HACK reset directory permissions
-chmod 755 ${TESTDIR}/usr/local/share/games/spring
+if [ -d ${TESTDIR}/usr/local/share/games/spring ]; then
+	chmod 755 ${TESTDIR}/usr/local/share/games/spring
+fi
 
 #cleanup
 rm -rf ${TESTDIR}
