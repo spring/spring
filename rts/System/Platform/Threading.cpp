77d035b758edc8f6a10024b5a83740cca468bb01
zerver <mr@zerver.net>
2013-01-23 02:33:55 +0200

    +1

--- rts/System/Platform/Threading.cpp
+++ rts/System/Platform/Threading.cpp
@@ -1,6 +1,7 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "lib/gml/gml_base.h"
+#include "lib/streflop/streflop_cond.h"
 #include "Threading.h"
 #include "System/bitops.h"
 #include "System/OpenMP_cond.h"
