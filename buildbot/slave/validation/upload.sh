f3a1fb8c43e0bc0054ec65e6f03eaa7250d98dc3
abma <spring@abma.de>
2013-01-23 20:45:49 +0100

    validation test: fix upload

--- buildbot/slave/validation/upload.sh
+++ buildbot/slave/validation/upload.sh
@@ -9,6 +9,4 @@ REMOTE_BASE=/home/buildbot/www
 RSYNC="rsync -vzr--chmod=D+rx,F+r --bwlimit 300"
 REMOTE_RSYNC="nice -19 ionice -c3 rsync" #prevent QQ about rsync killing server
 
-unmask 022
-
 ${RSYNC} ${TMP_BASE}/tests/.spring/ ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_BASE}/${CONFIG}/${BRANCH}/${REV}/validation/
