eabbcccd14553dac077099243c0375ce6d398aef
jK <-jk->
2013-02-11 09:08:12 +0100

    fix GLSL grass w/o shadows + add infotex support ARB grass is still broken, but ARB terrain shadows seem to be broken, too. So I would prefer a removal of both after a release.

--- cont/base/springcontent/shaders/GLSL/GrassFragProg.glsl
+++ cont/base/springcontent/shaders/GLSL/GrassFragProg.glsl
@@ -1,13 +1,24 @@
 
 uniform sampler2D shadingTex;
-uniform sampler2DShadow shadowMap;
 uniform sampler2D grassShadingTex;
 uniform sampler2D bladeTex;
 
+#ifdef HAVE_SHADOW
+uniform sampler2DShadow shadowMap;
+#endif
+
+#ifdef HAVE_INFOTEX
+uniform sampler2D infoMap;
+#endif
+
 void main() {
-	#ifdef GRASS_SHADOW_GEN
+	#ifdef SHADOW_GEN
 	{
-		gl_FragColor = vec4(1.0);
+		#ifdef DISTANCE_FAR
+			gl_FragColor = texture2D(bladeTex, gl_TexCoord[3].st);
+		#else
+			gl_FragColor = vec4(1.0);
+		#endif
 		return;
 	}
 	#endif
@@ -19,8 +30,15 @@ void main() {
 	gl_FragColor.rgb *= texture2D(grassShadingTex, gl_TexCoord[2].st).rgb;
 	gl_FragColor *= texture2D(bladeTex, gl_TexCoord[3].st) * gl_Color;
 
+#ifdef HAVE_SHADOW
 	float shadowCoeff = shadow2DProj(shadowMap, gl_TexCoord[1]).a;
 	gl_FragColor.rgb *= mix(gl_TextureEnvColor[1].rgb, vec3(1.0), shadowCoeff);
+#endif
+
+#ifdef HAVE_INFOTEX
+	gl_FragColor.rgb += texture2D(infoMap, gl_TexCoord[0].st).rgb;
+	gl_FragColor.rgb -= vec3(0.5, 0.5, 0.5);
+#endif
 
 	gl_FragColor.rgb = mix(gl_Fog.color.rgb, gl_FragColor.rgb, gl_FogFragCoord);
 }
