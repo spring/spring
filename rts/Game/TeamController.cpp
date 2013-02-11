e0ec1856f577078e5ed5017641afeca2d180c97d
jK <-jk->
2013-02-12 00:01:06 +0100

    fix first 2 CREG classes found with the test

---
+++ rts/Game/TeamController.cpp
@@ -0,0 +1,10 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#include "TeamController.h" // for swabDWord
+
+CR_BIND(TeamController, );
+
+CR_REG_METADATA(TeamController, (
+	CR_MEMBER(team),
+	CR_MEMBER(name)
+));
