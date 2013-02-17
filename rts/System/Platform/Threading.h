30b2a603d0c36d8da1ec1231c205933ebb5e5e07
rt <rt@rt-0.(none)>
2013-02-17 22:27:04 +0100

    addendum to 54852c10e7 (obstacle-avoidance now knows about pushResistant)
    do not name a function "UpdateSim" when 1) it updates the active game
    controller which can be anything and 2) calls to it are conditional on
    !GML::SimThreadRunning()

--- rts/System/Platform/Threading.h
+++ rts/System/Platform/Threading.h
@@ -12,10 +12,11 @@
 #ifdef __APPLE__
 	#include <libkern/OSAtomic.h> // OSAtomicIncrement64
 #endif
-#include "Game/GameController.h"
+
 #include "System/Platform/Win/win32.h"
 #include <boost/cstdint.hpp>
 
+class CGameController;
 
 namespace Threading {
 	/**
@@ -36,13 +37,13 @@ namespace Threading {
 	/**
 	 * Sets the affinity of the current thread
 	 *
-	 * Interpret <cores_bitmask> as a bit-mask indicating on which of the available system CPU's (which
-	 * are numbered logically from 1 to N) we want to run.
-	 * Note: that this approach will fail when N > 32.
+	 * Interpret <cores_bitmask> as a bit-mask indicating on which of the
+	 * available system CPU's (which are numbered logically from 1 to N) we
+	 * want to run. Note that this approach will fail when N > 32.
 	 */
 	void DetectCores();
 	boost::uint32_t SetAffinity(boost::uint32_t cores_bitmask, bool hard = true);
-	void SetAffinityHelper(const char *threadName, boost::uint32_t affinity);
+	void SetAffinityHelper(const char* threadName, boost::uint32_t affinity);
 	int GetAvailableCores();
 	boost::uint32_t GetAvailableCoresMask();
 
@@ -73,7 +74,7 @@ namespace Threading {
 	 */
 	void SetSimThread(bool set);
 	bool IsSimThread();
-	bool UpdateSim(CGameController *ac);
+	bool UpdateGameController(CGameController* ac);
 	void SetBatchThread(bool set);
 	bool IsBatchThread();
 
