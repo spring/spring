e1cc41dde342e0d5367c04b221d42d48523af9d7
abma <spring@abma.de>
2013-03-16 04:55:03 +0100

    use ninja for validation test

--- buildbot/slave/validation/tests-prepare.sh
+++ buildbot/slave/validation/tests-prepare.sh
@@ -18,7 +18,7 @@ fi
 
 #install
 cd ${BUILDDIR}
-make install-spring-headless install-pr-downloader demotool unitsyncTest lua2php DESTDIR=${TESTDIR}
+DESTDIR=${TESTDIR} ninja install-spring-headless install-pr-downloader demotool unitsyncTest lua2php
 
 # HACK/FIXME force spring to detect install dir as read-only
 chmod 555 ${TESTDIR}/usr/local/share/games/spring
