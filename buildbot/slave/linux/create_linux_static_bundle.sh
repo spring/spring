0771829731ca2aea11bb75b43a083b47f2a7a8bd
abma <spring@abma.de>
2013-03-16 04:26:48 +0100

    use ninja for linux static builds

--- buildbot/slave/linux/create_linux_static_bundle.sh
+++ buildbot/slave/linux/create_linux_static_bundle.sh
@@ -15,7 +15,7 @@ SEVENZIP_NONSOLID="nice -19 ionice -c3 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m 
 ZIP="zip -r9"
 
 cd ${BUILDDIR}
-make install DESTDIR=${DEST}
+DESTDIR=${DEST} ninja install
 cd ${INSTALLDIR}
 
 
