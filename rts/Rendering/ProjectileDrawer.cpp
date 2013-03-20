a981d48a29c7740ec83ed8fe9b7b812c6f835985
rt <rt@rt-0.(none)>
2013-03-20 15:42:50 +0100

    dynamic_cast --> static_cast

--- rts/Rendering/ProjectileDrawer.cpp
+++ rts/Rendering/ProjectileDrawer.cpp
@@ -748,7 +748,7 @@ bool CProjectileDrawer::DrawProjectileModel(const CProjectile* p, bool shadowPas
 
 	if (p->weapon) {
 		// weapon-projectile
-		const CWeaponProjectile* wp = dynamic_cast<const CWeaponProjectile*>(p);
+		const CWeaponProjectile* wp = static_cast<const CWeaponProjectile*>(p);
 
 		#define SET_TRANSFORM_VECTORS(dir)           \
 			float3 rightdir, updir;                  \
@@ -818,7 +818,7 @@ bool CProjectileDrawer::DrawProjectileModel(const CProjectile* p, bool shadowPas
 		#undef TRANSFORM_DRAW
 	} else {
 		// piece-projectile
-		const CPieceProjectile* pp = dynamic_cast<const CPieceProjectile*>(p);
+		const CPieceProjectile* pp = static_cast<const CPieceProjectile*>(p);
 
 		if (!shadowPass) {
 			unitDrawer->SetTeamColour(pp->GetTeamID());
