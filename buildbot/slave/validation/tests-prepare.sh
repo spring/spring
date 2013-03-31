2eb48178cca2d5a141bd4d32cf02e5fb0afbfe61
abma <spring@abma.de>
2013-03-31 04:01:54 +0200

    validation test: relocate config, too

--- buildbot/slave/validation/tests-prepare.sh
+++ buildbot/slave/validation/tests-prepare.sh
@@ -63,7 +63,8 @@ cp -v ${SOURCEDIR}/test/validation/LuaUI/Config/ZK_data.lua ${CONTENT_DIR}/LuaUI
 cp -v ${SOURCEDIR}/cont/springrc-template-headless.txt ${TESTDIR}/.springrc
 
 #set data directory to test directory
-echo "SpringData = ${CONTENT_DIR}:${TESTDIR}/usr/local/share/games/spring" >> ${TESTDIR}/.springrc
+mkdir -p ${TESTDIR}/.config/spring/
+echo "SpringData = ${CONTENT_DIR}:${TESTDIR}/usr/local/share/games/spring" >> ${TESTDIR}/.config/spring/springsettings.cfg
 
 makescript "$GAME1" "$MAP" AAI 0.9
 makescript "$GAME1" "$MAP" E323AI 3.25.0
