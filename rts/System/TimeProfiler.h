dcff9881845d8ba443461d0883bcca88f923e7b9
jK <-jk->
2013-04-17 18:48:46 +0200

    TimeProfiler: use spring_time instead of SDL

--- rts/System/TimeProfiler.h
+++ rts/System/TimeProfiler.h
@@ -3,12 +3,14 @@
 #ifndef TIME_PROFILER_H
 #define TIME_PROFILER_H
 
-#include <string>
-#include <map>
+#include "System/Misc/SpringTime.h"
+#include "System/float3.h"
+
 #include <boost/noncopyable.hpp>
 #include <cstring>
+#include <string>
+#include <map>
 
-#include "System/float3.h"
 
 // disable this if you want minimal profiling
 // (sim time is still measured because of game slowdown)
@@ -18,11 +20,15 @@
 class BasicTimer : public boost::noncopyable
 {
 public:
-	BasicTimer(const char* const name);
+	BasicTimer(const std::string& myname)
+		: name(myname)
+		, starttime(spring_gettime())
+	{
+	}
 
 protected:
 	const std::string name;
-	const unsigned starttime;
+	const spring_time starttime;
 };
 
 
@@ -35,27 +41,23 @@ protected:
 class ScopedTimer : public BasicTimer
 {
 public:
-	ScopedTimer(const char* const name, bool autoShow = false): BasicTimer(name) {
-		autoShowGraph = autoShow;
-	}
-	/**
-	 * @brief destroy and add time to profiler
-	 */
+	ScopedTimer(const std::string& name, bool autoShow = false);
 	~ScopedTimer();
 
 private:
-	bool autoShowGraph;
+	const bool autoShowGraph;
+	const unsigned hash;
 };
 
 
+
+/**
+ * @brief print passed time to infolog
+ */
 class ScopedOnceTimer : public BasicTimer
 {
 public:
-	ScopedOnceTimer(const char* const name): BasicTimer(name) {}
-	ScopedOnceTimer(const std::string& name): BasicTimer(name.c_str()) {}
-	/**
-	 * @brief destroy and print passed time to infolog
-	 */
+	ScopedOnceTimer(const std::string& name): BasicTimer(name) {}
 	~ScopedOnceTimer();
 };
 
@@ -65,13 +67,13 @@ class CTimeProfiler
 {
 public:
 	struct TimeRecord {
-		TimeRecord() : total(0), current(0), percent(0), color(0,0,0), showGraph(false), peak(0), newpeak(false) { 
+		TimeRecord() : total(0), current(0), percent(0), color(0,0,0), showGraph(false), peak(0), newpeak(false) {
 			memset(frames, 0, sizeof(frames));
 		}
-		unsigned total;
-		unsigned current;
+		spring_time total;
+		spring_time current;
 		static const unsigned frames_size = 128;
-		unsigned frames[frames_size];
+		spring_time frames[frames_size];
 		float percent;
 		float3 color;
 		bool showGraph;
@@ -83,7 +85,7 @@ public:
 	~CTimeProfiler();
 
 	float GetPercent(const char *name);
-	void AddTime(const std::string& name, unsigned time, bool showGraph = false);
+	void AddTime(const std::string& name, spring_time time, bool showGraph = false);
 	void Update();
 
 	void PrintProfilingInfo() const;
@@ -91,7 +93,7 @@ public:
 	std::map<std::string,TimeRecord> profile;
 
 private:
-	unsigned lastBigUpdate;
+	spring_time lastBigUpdate;
 	/// increases each update, from 0 to (frames_size-1)
 	unsigned currentPosition;
 };
