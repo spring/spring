30c138c0185735805a276846e10b3abfaa9cc639
jK <-jk->
2013-04-17 19:30:14 +0200

    fix #3706: catch exceptions in dedi server, too

--- rts/builds/dedicated/main.cpp
+++ rts/builds/dedicated/main.cpp
@@ -134,123 +134,116 @@ void zzz(unsigned int secs) {
 
 int main(int argc, char* argv[])
 {
-#ifdef _WIN32
 	try {
-#endif
-	CLogOutput::LogSystemInfo();
+		CLogOutput::LogSystemInfo();
 
-	std::string scriptName;
-	std::string scriptText;
+		std::string scriptName;
+		std::string scriptText;
 
-	ParseCmdLine(argc, argv, &scriptName);
+		ParseCmdLine(argc, argv, &scriptName);
 
-	FileSystemInitializer::Initialize();
+		FileSystemInitializer::Initialize();
 
-	// Initialize crash reporting
-	CrashHandler::Install();
+		// Initialize crash reporting
+		CrashHandler::Install();
 
-	SDL_Init(SDL_INIT_TIMER);
-	LOG("report any errors to Mantis or the forums.");
-	LOG("loading script from file: %s", scriptName.c_str());
+		SDL_Init(SDL_INIT_TIMER);
+		LOG("report any errors to Mantis or the forums.");
+		LOG("loading script from file: %s", scriptName.c_str());
 
-	logOutput.Initialize();
+		logOutput.Initialize();
 
-	CGameServer* server = NULL;
-	CGameSetup* gameSetup = NULL;
-	ClientSetup settings;
-	CFileHandler fh(scriptName);
+		CGameServer* server = NULL;
+		CGameSetup* gameSetup = NULL;
+		ClientSetup settings;
+		CFileHandler fh(scriptName);
 
-	if (!fh.FileExists())
-		throw content_error("script does not exist in given location: " + scriptName);
+		if (!fh.FileExists())
+			throw content_error("script does not exist in given location: " + scriptName);
 
-	if (!fh.LoadStringData(scriptText))
-		throw content_error("script cannot be read: " + scriptName);
+		if (!fh.LoadStringData(scriptText))
+			throw content_error("script cannot be read: " + scriptName);
 
-	settings.Init(scriptText);
-	gameSetup = new CGameSetup(); // to store the gamedata inside
+		settings.Init(scriptText);
+		gameSetup = new CGameSetup(); // to store the gamedata inside
 
-	if (!gameSetup->Init(scriptText)) {
-		// read the script provided by cmdline
-		LOG_L(L_ERROR, "failed to load script %s", scriptName.c_str());
-		return 1;
-	}
-
-	// Create the server, it will run in a separate thread
-	GameData data;
-	UnsyncedRNG rng;
-
-	rng.Seed(gameSetup->gameSetupText.length());
-	rng.Seed(scriptName.length());
-	data.SetRandomSeed(rng.RandInt());
-
-	//  Use script provided hashes if they exist
-	if (gameSetup->mapHash != 0) {
-		data.SetMapChecksum(gameSetup->mapHash);
-		gameSetup->LoadStartPositions(false); // reduced mode
-	} else {
-		data.SetMapChecksum(archiveScanner->GetArchiveCompleteChecksum(gameSetup->mapName));
-
-		CFileHandler f("maps/" + gameSetup->mapName);
-		if (!f.FileExists()) {
-			vfsHandler->AddArchiveWithDeps(gameSetup->mapName, false);
-		}
-		gameSetup->LoadStartPositions(); // full mode
-	}
-
-	if (gameSetup->modHash != 0) {
-		data.SetModChecksum(gameSetup->modHash);
-	} else {
-		const std::string& modArchive = archiveScanner->ArchiveFromName(gameSetup->modName);
-		const unsigned int modCheckSum = archiveScanner->GetArchiveCompleteChecksum(modArchive);
-		data.SetModChecksum(modCheckSum);
-	}
-
-	LOG("starting server...");
-
-	data.SetSetup(gameSetup->gameSetupText);
-	server = new CGameServer(settings.hostIP, settings.hostPort, &data, gameSetup);
-
-	while (!server->HasGameID()) {
-		// wait until gameID has been generated or
-		// a timeout occurs (if no clients connect)
-		if (server->HasFinished()) {
-			break;
+		if (!gameSetup->Init(scriptText)) {
+			// read the script provided by cmdline
+			LOG_L(L_ERROR, "failed to load script %s", scriptName.c_str());
+			return 1;
 		}
 
-		zzz(1);
-	}
+		// Create the server, it will run in a separate thread
+		GameData data;
+		UnsyncedRNG rng;
 
-	while (!server->HasFinished()) {
-		static bool printData = true;
+		rng.Seed(gameSetup->gameSetupText.length());
+		rng.Seed(scriptName.length());
+		data.SetRandomSeed(rng.RandInt());
 
-		if (printData) {
-			printData = false;
+		//  Use script provided hashes if they exist
+		if (gameSetup->mapHash != 0) {
+			data.SetMapChecksum(gameSetup->mapHash);
+			gameSetup->LoadStartPositions(false); // reduced mode
+		} else {
+			data.SetMapChecksum(archiveScanner->GetArchiveCompleteChecksum(gameSetup->mapName));
 
-			const boost::scoped_ptr<CDemoRecorder>& demoRec = server->GetDemoRecorder();
-			const boost::uint8_t* gameID = (demoRec->GetFileHeader()).gameID;
-
-			LOG("recording demo: %s", (demoRec->GetName()).c_str());
-			LOG("using mod: %s", (gameSetup->modName).c_str());
-			LOG("using map: %s", (gameSetup->mapName).c_str());
-			LOG("GameID: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", gameID[0], gameID[1], gameID[2], gameID[3], gameID[4], gameID[5], gameID[6], gameID[7], gameID[8], gameID[9], gameID[10], gameID[11], gameID[12], gameID[13], gameID[14], gameID[15]);
+			CFileHandler f("maps/" + gameSetup->mapName);
+			if (!f.FileExists()) {
+				vfsHandler->AddArchiveWithDeps(gameSetup->mapName, false);
+			}
+			gameSetup->LoadStartPositions(); // full mode
 		}
 
-		// wait 1 second between checks
-		zzz(1);
+		if (gameSetup->modHash != 0) {
+			data.SetModChecksum(gameSetup->modHash);
+		} else {
+			const std::string& modArchive = archiveScanner->ArchiveFromName(gameSetup->modName);
+			const unsigned int modCheckSum = archiveScanner->GetArchiveCompleteChecksum(modArchive);
+			data.SetModChecksum(modCheckSum);
+		}
+
+		LOG("starting server...");
+
+		data.SetSetup(gameSetup->gameSetupText);
+		server = new CGameServer(settings.hostIP, settings.hostPort, &data, gameSetup);
+
+		while (!server->HasGameID()) {
+			// wait until gameID has been generated or
+			// a timeout occurs (if no clients connect)
+			if (server->HasFinished()) {
+				break;
+			}
+
+			zzz(1);
+		}
+
+		while (!server->HasFinished()) {
+			static bool printData = true;
+
+			if (printData) {
+				printData = false;
+
+				const boost::scoped_ptr<CDemoRecorder>& demoRec = server->GetDemoRecorder();
+				const boost::uint8_t* gameID = (demoRec->GetFileHeader()).gameID;
+
+				LOG("recording demo: %s", (demoRec->GetName()).c_str());
+				LOG("using mod: %s", (gameSetup->modName).c_str());
+				LOG("using map: %s", (gameSetup->mapName).c_str());
+				LOG("GameID: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x", gameID[0], gameID[1], gameID[2], gameID[3], gameID[4], gameID[5], gameID[6], gameID[7], gameID[8], gameID[9], gameID[10], gameID[11], gameID[12], gameID[13], gameID[14], gameID[15]);
+			}
+
+			// wait 1 second between checks
+			zzz(1);
+		}
+
+		delete server;
+
+		FileSystemInitializer::Cleanup();
+		GlobalConfig::Deallocate();
+		ConfigHandler::Deallocate();
 	}
-
-	delete server;
-
-	FileSystemInitializer::Cleanup();
-	GlobalConfig::Deallocate();
-	ConfigHandler::Deallocate();
-
-#ifdef _WIN32
-} catch (const std::exception& err) {
-	LOG_L(L_ERROR, "Dedicated server failed to start: %s", err.what());
-	return 1;
-}
-#endif
+	NO_CATCH_EXCEPTIONS
 
 	return GetExitCode();
 }
