spring/spring

commit 2d493dbe6ce444afcd384db3c635875c91a7ac79

misc #2:
make sure ALL LuaSynced* weapon API functions are 1-based;
use a #define for the choice between 0- and 1-based indices
and update unit_script.lua since it does not need to correct
them for SetUnit*State anymore (note: the same should be done
for piece indices)

NEXT TIME *THINK* ABOUT A CHANGE LIKE THIS LONG AND HARD BEFORE YOU MAKE IT!

reduce target-lost CAI timeout from 8 to 2 seconds

unhardcode some magic numbers in HoverAirMoveType

rts/Lua/LuaSyncedCtrl.cpp
@@ -1333,22 +1333,26 @@ int LuaSyncedCtrl::SetUnitWeaponState(lua_State* L)
 		return 0;
 	}
 
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
+
 	CWeapon* weapon = unit->weapons[weaponNum];
 
-	if (lua_istable(L,3)) {
-		const int table = 3;
-		for (lua_pushnil(L); lua_next(L, table) != 0; lua_pop(L, 1)) {
+	if (lua_istable(L, 3)) {
+		// {key1 = value1, ...}
+		for (lua_pushnil(L); lua_next(L, 3) != 0; lua_pop(L, 1)) {
 			if (lua_israwstring(L, -2) && lua_isnumber(L, -1)) {
 				SetSingleUnitWeaponState(L, weapon, -2);
 			}
 		}
-	}
-	else if (lua_israwstring(L, 3) && lua_isnumber(L, 4)) {
-		SetSingleUnitWeaponState(L, weapon, 3);
+	} else {
+		// key, value
+		if (lua_israwstring(L, 3) && lua_isnumber(L, 4)) {
+			SetSingleUnitWeaponState(L, weapon, 3);
+		}
 	}
 
 	return 0;
@@ -1718,14 +1722,16 @@ int LuaSyncedCtrl::SetUnitShieldState(lua_State* L)
 	}
 
 	int args = lua_gettop(L);
-	int arg = 2;
+	int arg = 2 + (lua_isnumber(L, 2) && args > 2);
 
 	CPlasmaRepulser* shield = static_cast<CPlasmaRepulser*>(unit->shieldWeapon);
-	if (lua_isnumber(L, 2) && args > 2) {
-		arg++;
-		const int idx = luaL_optint(L, 2, -1);
-		if (idx >= 0 && idx < unit->weapons.size())
+
+	if (arg == 3) {
+		const size_t idx = luaL_optint(L, 2, -1) - LUA_WEAPON_BASE_INDEX;
+
+		if (idx < unit->weapons.size()) {
 			shield = dynamic_cast<CPlasmaRepulser*>(unit->weapons[idx]);
+		}
 	}
 
 	if (shield == NULL) {
@@ -3629,7 +3635,7 @@ int LuaSyncedCtrl::UnitWeaponFire(lua_State* L)
 
 	if (unit == NULL)
 		return 0;
-	if (static_cast<uint32_t>(luaL_checkint(L, 2)-1) >= unit->weapons.size())
+	if (static_cast<uint32_t>(luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX) >= unit->weapons.size())
 		return 0;
 
 	unit->weapons[luaL_checkint(L, 2)-1]->Fire();
@@ -3641,7 +3647,7 @@ int LuaSyncedCtrl::UnitWeaponHoldFire(lua_State* L)
 
 	if (unit == NULL)
 		return 0;
-	if (static_cast<uint32_t>(luaL_checkint(L, 2)-1) >= unit->weapons.size())
+	if (static_cast<uint32_t>(luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX) >= unit->weapons.size())
 		return 0;
 
 	unit->weapons[luaL_checkint(L, 2)-1]->HoldFire();

cont/base/springcontent/LuaGadgets/Gadgets/unit_script.lua
@@ -126,7 +126,6 @@ local sp_WaitForTurn = Spring.UnitScript.WaitForTurn
 local sp_SetPieceVisibility = Spring.UnitScript.SetPieceVisibility
 local sp_SetDeathScriptFinished = Spring.UnitScript.SetDeathScriptFinished
 
-
 local UNITSCRIPT_DIR = (UNITSCRIPT_DIR or "scripts/"):lower()
 local VFSMODE = VFS.ZIP_ONLY
 if (Spring.IsDevLuaEnabled()) then
@@ -571,12 +570,19 @@ local function Wrap_AimWeapon(unitID, callins)
 
 	-- SetUnitShieldState wants true or false, while
 	-- SetUnitWeaponState wants 1.0 or 0.0, niiice =)
+	--
+	-- NOTE:
+	--   the LuaSynced* API functions all EXPECT 1-based arguments
+	--   the LuaUnitScript::*Weapon* callins all SUPPLY 1-based arguments
+	--
+	--   therefore on the Lua side all weapon indices are ASSUMED to be
+	--   1-based and if LuaConfig::LUA_WEAPON_BASE_INDEX is changed to 0
+	--   no Lua code should need to be updated
 	local function AimWeaponThread(weaponNum, heading, pitch)
 		local bAimReady = AimWeapon(weaponNum, heading, pitch) or false
 		local fAimReady = (bAimReady and 1.0) or 0.0
 
-		-- SetUnitWeaponState counts weapons from 0
-		return sp_SetUnitWeaponState(unitID, weaponNum - 1, "aimReady", fAimReady)
+		return sp_SetUnitWeaponState(unitID, weaponNum, "aimReady", fAimReady)
 	end
 
 	callins["AimWeapon"] = function(weaponNum, heading, pitch)
@@ -593,8 +599,8 @@ local function Wrap_AimShield(unitID, callins)
 	-- SetUnitWeaponState wants 1 or 0, niiice =)
 	local function AimShieldThread(weaponNum)
 		local enabled = AimShield(weaponNum) and true or false
-		-- SetUnitShieldState counts weapons from 0
-		return sp_SetUnitShieldState(unitID, weaponNum - 1, enabled)
+
+		return sp_SetUnitShieldState(unitID, weaponNum, enabled)
 	end
 
 	callins["AimShield"] = function(weaponNum)

rts/Lua/LuaConfig.h
@@ -5,9 +5,14 @@
 
 #include "lib/gml/gmlcnf.h"
 
+// set to 0 to switch to 0-based weapon
+// indices in LuaSynced** API functions
+#define LUA_WEAPON_BASE_INDEX 1
+
 #define LUA_MUTEX 1
 #define LUA_BATCH 2
 #define LUA_STATE 4
+
 #if (defined(USE_GML) && GML_ENABLE_SIM)
 #define LUA_MT_OPT ( LUA_MUTEX | LUA_BATCH | LUA_STATE ) // optimizations for faster parallel execution of separate lua_States
 #elif defined(USE_LUA_MT)

rts/Lua/LuaSyncedRead.cpp
@@ -3087,13 +3087,13 @@ int LuaSyncedRead::GetUnitShieldState(lua_State* L)
 		return 0;
 	}
 
-	CPlasmaRepulser* shield = NULL;
-	const int idx = luaL_optint(L, 2, -1);
+	const CPlasmaRepulser* shield = NULL;
+	const size_t idx = luaL_optint(L, 2, -1) - LUA_WEAPON_BASE_INDEX;
 
-	if (idx < 0 || idx >= unit->weapons.size()) {
-		shield = static_cast<CPlasmaRepulser*>(unit->shieldWeapon);
+	if (idx >= unit->weapons.size()) {
+		shield = static_cast<const CPlasmaRepulser*>(unit->shieldWeapon);
 	} else {
-		shield = dynamic_cast<CPlasmaRepulser*>(unit->weapons[idx]);
+		shield = dynamic_cast<const CPlasmaRepulser*>(unit->weapons[idx]);
 	}
 
 	if (shield == NULL) {
@@ -3162,12 +3162,16 @@ int LuaSyncedRead::GetUnitWeaponState(lua_State* L)
 	if (unit == NULL) {
 		return 0;
 	}
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
+
 	const CWeapon* weapon = unit->weapons[weaponNum];
 	const string key = luaL_optstring(L, 3, "");
+
 	if (key.empty()) { // backwards compatible
 		lua_pushboolean(L, weapon->angleGood);
 		lua_pushboolean(L, weapon->reloadStatus <= gs->frameNum);
@@ -3216,8 +3220,10 @@ int LuaSyncedRead::GetUnitWeaponVectors(lua_State* L)
 	if (unit == NULL) {
 		return 0;
 	}
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
 
@@ -3250,8 +3256,10 @@ int LuaSyncedRead::GetUnitWeaponTryTarget(lua_State* L)
 	if (unit == NULL) {
 		return 0;
 	}
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
 
@@ -3279,8 +3287,10 @@ int LuaSyncedRead::GetUnitWeaponTestTarget(lua_State* L)
 	if (unit == NULL) {
 		return 0;
 	}
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
 
@@ -3308,8 +3318,10 @@ int LuaSyncedRead::GetUnitWeaponTestRange(lua_State* L)
 	if (unit == NULL) {
 		return 0;
 	}
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
 
@@ -3337,8 +3349,10 @@ int LuaSyncedRead::GetUnitWeaponHaveFreeLineOfFire(lua_State* L)
 	if (unit == NULL) {
 		return 0;
 	}
-	const int weaponNum = luaL_checkint(L, 2) -1;
-	if ((weaponNum < 0) || ((size_t)weaponNum >= unit->weapons.size())) {
+
+	const size_t weaponNum = luaL_checkint(L, 2) - LUA_WEAPON_BASE_INDEX;
+
+	if (weaponNum >= unit->weapons.size()) {
 		return 0;
 	}
 

rts/Lua/LuaUnitDefs.cpp
@@ -405,11 +405,9 @@ static int WeaponsTable(lua_State* L, const void* data)
 	const vector<UnitDefWeapon>& weapons =
 		*((const vector<UnitDefWeapon>*)data);
 
-	const int weaponCount = (int)weapons.size();
-
 	lua_newtable(L);
 
-	for (int i = 0; i < weaponCount; i++) {
+	for (size_t i = 0; i < weapons.size(); i++) {
 		const UnitDefWeapon& udw = weapons[i];
 		const WeaponDef* weapon = udw.def;
 		lua_pushnumber(L, i + 1);

rts/Sim/MoveTypes/GroundMoveType.cpp
@@ -2254,8 +2254,7 @@ bool CGroundMoveType::WantReverse(const float3& waypointDir2D) const
 	const float waypointDist  = waypointDif.Length();                                             // in elmos
 	const float waypointFETA  = (waypointDist / maxSpeed);                                        // in frames (simplistic)
 	const float waypointRETA  = (waypointDist / maxReverseSpeed);                                 // in frames (simplistic)
-	const float waypointDirDP = waypointDir2D.dot(owner->frontdir);
-	const float waypointAngle = Clamp(waypointDirDP, -1.0f, 1.0f);                                // prevent NaN's
+	const float waypointAngle = Clamp(waypointDir2D.dot(owner->frontdir), -1.0f, 1.0f);           // clamp to prevent NaN's
 	const float turnAngleDeg  = math::acosf(waypointAngle) * (180.0f / PI);                       // in degrees
 	const float turnAngleSpr  = (turnAngleDeg / 360.0f) * SPRING_CIRCLE_DIVS;                     // in "headings"
 	const float revAngleSpr   = SHORTINT_MAXVALUE - turnAngleSpr;                                 // 180 deg - angle

rts/Sim/MoveTypes/HoverAirMoveType.cpp
@@ -339,7 +339,7 @@ void CHoverAirMoveType::UpdateHovering()
 	// behind us when we have more waypoints to get to
 	// *** this behavior interferes with the loading procedure of transports ***
 	const bool b0 = (aircraftState != AIRCRAFT_LANDING && owner->commandAI->HasMoreMoveCommands());
-	const bool b1 = (goalDistance < 120.0f && goalDistance > 1.0f);
+	const bool b1 = (goalDistance < brakeDistance && goalDistance > 1.0f);
 
 	if (b0 && b1 && dynamic_cast<CTransportUnit*>(owner) == NULL) {
 		deltaDir = owner->frontdir;
@@ -488,15 +488,16 @@ void CHoverAirMoveType::UpdateFlying()
 	// if we are close to our goal and not in attack mode,
 	// we should go slow enough to be able to brake in time
 	const float goalDist = goalVec.Length() + 0.1f;
-	const float approachSpeed =
+	const float goalSpeed =
 		(flyState != FLY_ATTACKING && goalDist < brakeDistance)?
 		((goalDist / (speed.Length2D() + 0.01f)) * decRate):
 		maxSpeed;
 
-	// do not set wantedSpeed if approachSpeed exceeds goal-distance
-	// because this can lead to overshooting oscillations (jitter)
-	if (approachSpeed < goalDist)
-		wantedSpeed = (goalVec / goalDist) * approachSpeed;
+	// do not set wantedSpeed if goal is behind us because
+	// this can lead to overshooting oscillations (jitter)
+	// --> wait for turn first
+	if (goalVec.dot(owner->frontdir) >= 0.0f)
+		wantedSpeed = (goalVec / goalDist) * std::min(goalSpeed, maxSpeed);
 
 	UpdateAirPhysics();
 
@@ -505,7 +506,7 @@ void CHoverAirMoveType::UpdateFlying()
 		goalVec = circlingPos - pos;
 	} else {
 		const bool b0 = (flyState != FLY_LANDING && (owner->commandAI->HasMoreMoveCommands()));
-		const bool b1 = (goalDist < 120.0f && goalDist > 1.0);
+		const bool b1 = (goalDist < brakeDistance && goalDist > 1.0f);
 
 		if (b0 && b1) {
 			goalVec = owner->frontdir;

rts/Sim/Units/CommandAI/CommandAI.cpp
@@ -38,7 +38,7 @@
 // seconds (!) before eg. aircraft would stop tracking a
 // target that cloaked after flying over it --> obviously
 // unreasonable
-static const int TARGET_LOST_TIMER = 15;
+static const int TARGET_LOST_TIMER = 4;
 static const float COMMAND_CANCEL_DIST = 17.0f;
 
 CR_BIND(CCommandQueue, );

commit 2c0d51aa5b99cabc04b6a5ed2b3b69719f2a9204

fix SetUnitShieldState syntax broken by 2d493dbe

rts/Lua/LuaSyncedCtrl.cpp
@@ -1722,16 +1722,18 @@ int LuaSyncedCtrl::SetUnitShieldState(lua_State* L)
 	}
 
 	int args = lua_gettop(L);
-	int arg = 2 + (lua_isnumber(L, 2) && args > 2);
+	int arg = 2;
 
 	CPlasmaRepulser* shield = static_cast<CPlasmaRepulser*>(unit->shieldWeapon);
 
-	if (arg == 3) {
+	if (lua_isnumber(L, 2) && args > 2) {
 		const size_t idx = luaL_optint(L, 2, -1) - LUA_WEAPON_BASE_INDEX;
 
 		if (idx < unit->weapons.size()) {
 			shield = dynamic_cast<CPlasmaRepulser*>(unit->weapons[idx]);
 		}
+
+		arg++;
 	}
 
 	if (shield == NULL) {