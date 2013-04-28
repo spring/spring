508091639cc51dbe37f0b08f1dcd46b802858123
jK <-jk->
2013-04-28 19:24:51 +0200

    try to fix unitsync compile (that works fine locally :<)

--- rts/lib/lua/include/LuaUser.h
+++ rts/lib/lua/include/LuaUser.h
@@ -3,19 +3,21 @@
 #ifndef SPRING_LUA_USER_H
 #define SPRING_LUA_USER_H
 
-extern void LuaCreateMutex(lua_State * L);
-extern void LuaDestroyMutex(lua_State * L);
-extern void LuaLinkMutex(lua_State* L_parent, lua_State* L_child);
-extern void LuaMutexLock(lua_State * L);
-extern void LuaMutexUnlock(lua_State * L);
-extern void LuaMutexYield(lua_State * L);
+#if !defined(BUILDING_AI) || !defined(DEDICATED) || !defined(UNITSYNC)
+	extern void LuaCreateMutex(lua_State * L);
+	extern void LuaDestroyMutex(lua_State * L);
+	extern void LuaLinkMutex(lua_State* L_parent, lua_State* L_child);
+	extern void LuaMutexLock(lua_State * L);
+	extern void LuaMutexUnlock(lua_State * L);
+	extern void LuaMutexYield(lua_State * L);
 
 
-struct SLuaInfo {
-	int allocedBytes;
-	int numStates;
-};
-extern void* spring_lua_alloc(void* ud, void* ptr, size_t osize, size_t nsize);
-extern void spring_lua_alloc_get_stats(SLuaInfo* info);
+	struct SLuaInfo {
+		int allocedBytes;
+		int numStates;
+	};
+	extern void* spring_lua_alloc(void* ud, void* ptr, size_t osize, size_t nsize);
+	extern void spring_lua_alloc_get_stats(SLuaInfo* info);
+#endif
 
 #endif // SPRING_LUA_USER_H
