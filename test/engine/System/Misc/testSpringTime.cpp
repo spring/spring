1b1ec0b66b1fb2e30634f1d8021d75839e38f590
jK <-jk->
2013-05-04 22:24:25 +0200

    extend FileSyste, UT further

--- test/engine/System/Misc/testSpringTime.cpp
+++ test/engine/System/Misc/testSpringTime.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#define BOOST_TEST_MODULE Matrix44f
+#define BOOST_TEST_MODULE SpringTime
 #include <boost/test/unit_test.hpp>
 
 #include "System/TimeProfiler.h"
