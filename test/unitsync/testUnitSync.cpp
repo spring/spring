042cc4b6888aef059dcd3759475b65d878fda329
jK <-jk->
2013-05-07 14:21:09 +0200

    unitsync UT: move unitsync methods into a namespace

--- test/unitsync/testUnitSync.cpp
+++ test/unitsync/testUnitSync.cpp
@@ -2,7 +2,6 @@
 
 //TODO rewrite most of this file atm it's more a verbose debug tool than a UnitTest
 
-#include "../tools/unitsync/unitsync_api.h"
 #include "ExternalAI/Interface/SSkirmishAILibrary.h"
 #include "System/Option.h"
 #include "System/Log/ILog.h"
@@ -13,17 +12,15 @@
 #include <string>
 #include <vector>
 
+namespace us {
+	#include "../tools/unitsync/unitsync_api.h"
+};
+
 #define BOOST_TEST_MODULE UnitSync
 #include <boost/test/unit_test.hpp>
 
 
 using std::string;
-
-/******************************************************************************/
-/******************************************************************************/
-
-static void DisplayOptions(int optionCount);
-static bool TestLuaParser();
 
 
 /******************************************************************************/
@@ -31,45 +28,45 @@ static bool TestLuaParser();
 
 static void PrintMapInfo(const string& mapName)
 {
-	const int map_count = GetMapCount();
+	const int map_count = us::GetMapCount();
 	int mapidx = -1;
 	for (int i = 0; i < map_count; i++){
-		if (GetMapName(i) == mapName) {
+		if (us::GetMapName(i) == mapName) {
 			mapidx = i;
 		}
 	}
 	if (mapidx<0) {
-		printf("Error: Map not found\n");
+		LOG("Error: Map not found");
 		return;
 	}
-	printf("    author:    '%s'\n", GetMapAuthor(mapidx));
-	printf("    desc:      '%s'\n", GetMapDescription(mapidx));
-	printf("    gravity:   %i\n",   GetMapGravity(mapidx));
-	printf("    tidal:     %i\n",   GetMapTidalStrength(mapidx));
-	const int rescount = GetMapResourceCount(mapidx);
+	LOG("    author:    '%s'", us::GetMapAuthor(mapidx));
+	LOG("    desc:      '%s'", us::GetMapDescription(mapidx));
+	LOG("    gravity:   %i",   us::GetMapGravity(mapidx));
+	LOG("    tidal:     %i",   us::GetMapTidalStrength(mapidx));
+	const int rescount = us::GetMapResourceCount(mapidx);
 	for (int i = 0; i < rescount; i++) {
-		const char* resName = GetMapResourceName(mapidx, i);
-		printf("    max%s:  %f\n", resName, GetMapResourceMax(mapidx, i));
-		printf("    mex%sRad:    %i\n", resName, GetMapResourceExtractorRadius(mapidx, i));
+		const char* resName = us::GetMapResourceName(mapidx, i);
+		LOG("    max%s:  %f", resName, us::GetMapResourceMax(mapidx, i));
+		LOG("    mex%sRad:    %i", resName, us::GetMapResourceExtractorRadius(mapidx, i));
 	}
-	printf("    minWind:   %i\n",   GetMapWindMin(mapidx));
-	printf("    maxWind:   %i\n",   GetMapWindMax(mapidx));
-	printf("    width:     %i\n",   GetMapWidth(mapidx));
-	printf("    height:    %i\n",   GetMapHeight(mapidx));
-	const int poscount = GetMapPosCount(mapidx);
+	LOG("    minWind:   %i",   us::GetMapWindMin(mapidx));
+	LOG("    maxWind:   %i",   us::GetMapWindMax(mapidx));
+	LOG("    width:     %i",   us::GetMapWidth(mapidx));
+	LOG("    height:    %i",   us::GetMapHeight(mapidx));
+	const int poscount = us::GetMapPosCount(mapidx);
 	for (int p = 0; p < poscount; p++) {
-		const int x = GetMapPosX(mapidx, p);
-		const int z = GetMapPosZ(mapidx, p);
-		printf("    pos %i:     <%5i, %5i>\n", p, x, z);
+		const int x = us::GetMapPosX(mapidx, p);
+		const int z = us::GetMapPosZ(mapidx, p);
+		LOG("    pos %i:     <%5i, %5i>", p, x, z);
 	}
 
 	const char* infomaps[] = { "height", "grass", "metal", "type", NULL };
 	int width, height;
 	for (int i = 0; infomaps[i]; ++i) {
-		if (GetInfoMapSize(mapName.c_str(), infomaps[i], &width, &height)) {
-			printf("    %smap: %i x %i\n", infomaps[i], width, height);
+		if (us::GetInfoMapSize(mapName.c_str(), infomaps[i], &width, &height)) {
+			LOG("    %smap: %i x %i", infomaps[i], width, height);
 			/*void* data = malloc(width*height);
-			 *	GetInfoMap(mapName.c_str(), infomaps[i], data, 1);
+			 *	us::GetInfoMap(mapName.c_str(), infomaps[i], data, 1);
 			 *	FILE* f = fopen(infomaps[i], "w");
 			 *	if (f == NULL) {
 			 *	perror(infomaps[i]);
@@ -89,43 +86,43 @@ static void PrintMapInfo(const string& mapName)
 static void DisplayOptions(int optionCount)
 {
 	for (int i = 0; i < optionCount; i++) {
-		printf("    Option #%i\n", i);
-		printf("      key  = '%s'\n", GetOptionKey(i));
-		printf("      name = '%s'\n", GetOptionName(i));
-		printf("      desc = '%s'\n", GetOptionDesc(i));
-		printf("      type = %i\n", GetOptionType(i));
+		LOG("    Option #%i", i);
+		LOG("      key  = '%s'", us::GetOptionKey(i));
+		LOG("      name = '%s'", us::GetOptionName(i));
+		LOG("      desc = '%s'", us::GetOptionDesc(i));
+		LOG("      type = %i", us::GetOptionType(i));
 
-		const int type = GetOptionType(i);
+		const int type = us::GetOptionType(i);
 
 		if (type == opt_error) {
-			printf("      BAD OPTION\n");
+			LOG("      BAD OPTION");
 		}
 		else if (type == opt_bool) {
-			printf("      BOOL: def = %s\n",
-			GetOptionBoolDef(i) ? "true" : "false");
+			LOG("      BOOL: def = %s",
+			us::GetOptionBoolDef(i) ? "true" : "false");
 		}
 		else if (type == opt_string) {
-			printf("      STRING: def = '%s', maxlen = %i\n",
-			GetOptionStringDef(i),
-			GetOptionStringMaxLen(i));
+			LOG("      STRING: def = '%s', maxlen = %i",
+			us::GetOptionStringDef(i),
+			us::GetOptionStringMaxLen(i));
 		}
 		else if (type == opt_number) {
-			printf("      NUMBER: def = %f, min = %f, max = %f, step = %f\n",
-			GetOptionNumberDef(i),
-			GetOptionNumberMin(i),
-			GetOptionNumberMax(i),
-			GetOptionNumberStep(i));
+			LOG("      NUMBER: def = %f, min = %f, max = %f, step = %f",
+			us::GetOptionNumberDef(i),
+			us::GetOptionNumberMin(i),
+			us::GetOptionNumberMax(i),
+			us::GetOptionNumberStep(i));
 		}
 		else if (type == opt_list) {
-			printf("      LIST: def = '%s'\n",
-			GetOptionListDef(i));
+			LOG("      LIST: def = '%s'",
+			us::GetOptionListDef(i));
 
-			const int listCount = GetOptionListCount(i);
+			const int listCount = us::GetOptionListCount(i);
 			for (int li = 0; li < listCount; li++) {
-				printf("      %3i: key  = '%s'\n", li,
-				GetOptionListItemKey(i, li));
-				printf("           name = '%s'\n", GetOptionListItemName(i, li));
-				printf("           desc = '%s'\n", GetOptionListItemDesc(i, li));
+				LOG("      %3i: key  = '%s'", li,
+				us::GetOptionListItemKey(i, li));
+				LOG("           name = '%s'", us::GetOptionListItemName(i, li));
+				LOG("           desc = '%s'", us::GetOptionListItemDesc(i, li));
 			}
 		}
 	}
@@ -153,75 +150,75 @@ static bool TestLuaParser()
 	"  [12] = { 'one', 'success1', three = { 'success2', 'crap' }, four = { 'success3' }},\n"
 	"  string = 'string',\n"
 	"  number = 12345678,\n"
-	"}\n";
+	"}";
 
-	if (!lpOpenSource(source.c_str(), "r")) {
-		printf("LuaParser API test failed  --  should not happen here\n");
+	if (!us::lpOpenSource(source.c_str(), "r")) {
+		LOG("LuaParser API test failed  --  should not happen here");
 		return false;
 	}
 
-	lpAddTableStr("Test", 1);
-	lpAddIntKeyFloatVal(1, 111.1f);
-	lpAddIntKeyFloatVal(2, 222.2f);
-	lpAddIntKeyFloatVal(3, 333.3f);
-	lpAddTableStr("Sub", 1);
-	lpAddStrKeyStrVal("test", "value1");
-	lpEndTable();
-	lpAddTableInt(4, 1);
-	lpAddStrKeyStrVal("test", "value2");
-	lpEndTable();
-	lpAddTableInt(5, 1);
-	lpAddStrKeyStrVal("test", "value3");
-	lpEndTable();
-	lpAddIntKeyStrVal(6, "hello");
-	lpAddIntKeyStrVal(7, "world");
-	lpEndTable();
+	us::lpAddTableStr("Test", 1);
+	us::lpAddIntKeyFloatVal(1, 111.1f);
+	us::lpAddIntKeyFloatVal(2, 222.2f);
+	us::lpAddIntKeyFloatVal(3, 333.3f);
+	us::lpAddTableStr("Sub", 1);
+	us::lpAddStrKeyStrVal("test", "value1");
+	us::lpEndTable();
+	us::lpAddTableInt(4, 1);
+	us::lpAddStrKeyStrVal("test", "value2");
+	us::lpEndTable();
+	us::lpAddTableInt(5, 1);
+	us::lpAddStrKeyStrVal("test", "value3");
+	us::lpEndTable();
+	us::lpAddIntKeyStrVal(6, "hello");
+	us::lpAddIntKeyStrVal(7, "world");
+	us::lpEndTable();
 
-	if (!lpExecute()) {
-		printf("LuaParser API test failed: %s\n", lpErrorLog());
+	if (!us::lpExecute()) {
+		LOG("LuaParser API test failed: %s", us::lpErrorLog());
 		return false;
 	}
 
-	const int intCount = lpGetIntKeyListCount();
+	const int intCount = us::lpGetIntKeyListCount();
 	for (int i = 0; i < intCount; i++) {
-		const char* str = lpGetIntKeyStrVal(i, "");
-		const float num = lpGetIntKeyFloatVal(i, 666.666f);
-		printf("  int-key = %i, val = '%s'  (%f)\n", i, str, num);
+		const char* str = us::lpGetIntKeyStrVal(i, "");
+		const float num = us::lpGetIntKeyFloatVal(i, 666.666f);
+		LOG("  int-key = %i, val = '%s'  (%f)", i, str, num);
 	}
 
-	const int strCount = lpGetStrKeyListCount();
+	const int strCount = us::lpGetStrKeyListCount();
 	for (int i = 0; i < strCount; i++) {
-		const string key = lpGetStrKeyListEntry(i);
-		const char* str  = lpGetStrKeyStrVal(key.c_str(), "");
-		const float num  = lpGetStrKeyFloatVal(key.c_str(), 666.666f);
-		printf("  str-key = '%s', val = '%s'  (%f)\n", key.c_str(), str, num);
+		const string key = us::lpGetStrKeyListEntry(i);
+		const char* str  = us::lpGetStrKeyStrVal(key.c_str(), "");
+		const float num  = us::lpGetStrKeyFloatVal(key.c_str(), 666.666f);
+		LOG("  str-key = '%s', val = '%s'  (%f)", key.c_str(), str, num);
 	}
 
-	lpRootTable();
-	lpSubTableInt(12);
-	printf("SubTable test1: '%s'\n", lpGetIntKeyStrVal(2, "FAILURE"));
-	lpSubTableStr("three");
-	printf("SubTable test2: '%s'\n", lpGetIntKeyStrVal(1, "FAILURE"));
-	lpPopTable();
-	lpSubTableStr("four");
-	printf("SubTable test3: '%s'\n", lpGetIntKeyStrVal(1, "FAILURE"));
-	lpPopTable();
-	lpPopTable();
-	lpPopTable();
-	lpPopTable();
-	lpPopTable();
-	lpPopTable();
-	lpPopTable();
-	lpSubTableInt(12);
-	printf("SubTable test1: '%s'\n", lpGetIntKeyStrVal(2, "FAILURE"));
-	lpPopTable();
-	lpRootTable();
-	lpSubTableExpr("[12].four");
-	printf("SubTable sub-expr: '%s'\n", lpGetIntKeyStrVal(1, "FAILURE"));
-	lpRootTableExpr("[12].four");
-	printf("SubTable root-expr: '%s'\n", lpGetIntKeyStrVal(1, "FAILURE"));
-	lpRootTableExpr("[12].four");
-	printf("SubTable root-expr: '%s'\n", lpGetIntKeyStrVal(1, "FAILURE"));
+	us::lpRootTable();
+	us::lpSubTableInt(12);
+	LOG("SubTable test1: '%s'", us::lpGetIntKeyStrVal(2, "FAILURE"));
+	us::lpSubTableStr("three");
+	LOG("SubTable test2: '%s'", us::lpGetIntKeyStrVal(1, "FAILURE"));
+	us::lpPopTable();
+	us::lpSubTableStr("four");
+	LOG("SubTable test3: '%s'", us::lpGetIntKeyStrVal(1, "FAILURE"));
+	us::lpPopTable();
+	us::lpPopTable();
+	us::lpPopTable();
+	us::lpPopTable();
+	us::lpPopTable();
+	us::lpPopTable();
+	us::lpPopTable();
+	us::lpSubTableInt(12);
+	LOG("SubTable test1: '%s'", us::lpGetIntKeyStrVal(2, "FAILURE"));
+	us::lpPopTable();
+	us::lpRootTable();
+	us::lpSubTableExpr("[12].four");
+	LOG("SubTable sub-expr: '%s'", us::lpGetIntKeyStrVal(1, "FAILURE"));
+	us::lpRootTableExpr("[12].four");
+	LOG("SubTable root-expr: '%s'", us::lpGetIntKeyStrVal(1, "FAILURE"));
+	us::lpRootTableExpr("[12].four");
+	LOG("SubTable root-expr: '%s'", us::lpGetIntKeyStrVal(1, "FAILURE"));
 
 	return true;
 }
@@ -233,18 +230,18 @@ static bool TestLuaParser()
 
 BOOST_AUTO_TEST_CASE( UnitSync )
 {
-	Init(false, 0);
+	us::Init(false, 0);
 
-	int i = GetMapCount() - 1, j = GetPrimaryModCount() - 1;
-	while (i > 0 && !GetMapName(i)) --i;
-	while (j > 0 && !GetPrimaryModName(j)) --j;
+	int i = us::GetMapCount() - 1, j = us::GetPrimaryModCount() - 1;
+	while (i > 0 && !us::GetMapName(i)) --i;
+	while (j > 0 && !us::GetPrimaryModName(j)) --j;
 
-	const string map = GetMapName(i);
-	const string mod = GetPrimaryModName(j);
+	const string map = us::GetMapName(i);
+	const string mod = us::GetPrimaryModName(j);
 	LOG("MAP = %s", map.c_str());
 	LOG("MOD = %s", mod.c_str());
 
-	LOG("GetSpringVersion() = %s", GetSpringVersion());
+	LOG("GetSpringVersion() = %s", us::GetSpringVersion());
 
 	// test the lua parser interface
 	BOOST_CHECK(TestLuaParser());
@@ -252,18 +249,18 @@ BOOST_AUTO_TEST_CASE( UnitSync )
 	// map names
 	LOG("  MAPS");
 	std::vector<string> mapNames;
-	const int mapCount = GetMapCount();
+	const int mapCount = us::GetMapCount();
 	for (int i = 0; i < mapCount; i++) {
-		const string mapName = GetMapName(i);
+		const string mapName = us::GetMapName(i);
 		mapNames.push_back(mapName);
-		printf("    [map %3i]   %s\n", i, mapName.c_str());
+		LOG("    [map %3i]   %s", i, mapName.c_str());
 	}
 
 	// map archives
-	printf("  MAP ARCHIVES  (for %s)\n", map.c_str());
-	const int mapArcCount = GetMapArchiveCount(map.c_str());
+	LOG("  MAP ARCHIVES  (for %s)", map.c_str());
+	const int mapArcCount = us::GetMapArchiveCount(map.c_str());
 	for (int a = 0; a < mapArcCount; a++) {
-		printf("      arc %i: %s\n", a, GetMapArchiveName(a));
+		LOG("      arc %i: %s", a, us::GetMapArchiveName(a));
 	}
 
 	// map info
@@ -276,99 +273,99 @@ BOOST_AUTO_TEST_CASE( UnitSync )
 	}
 
 	// mod names
-	printf("  GAMES\n");
-	const int modCount = GetPrimaryModCount();
+	LOG("  GAMES");
+	const int modCount = us::GetPrimaryModCount();
 	for (int i = 0; i < modCount; i++) {
-		const string modArchive = GetPrimaryModArchive(i);
-		const int infoCount = GetPrimaryModInfoCount(i);
+		const string modArchive = us::GetPrimaryModArchive(i);
+		const int infoCount = us::GetPrimaryModInfoCount(i);
 		for (int j=0; j < infoCount; j++) {
-			const char* key = GetInfoKey(j);
+			const char* key = us::GetInfoKey(j);
 			string skey="";
 			string svalue="";
 			if (key!=NULL)
 				skey=key;
-			const char* value = GetInfoValueString(j);
+			const char* value = us::GetInfoValueString(j);
 			if (value!=NULL)
 				svalue=value;
-			printf("    [%s]: %s = %s\n", modArchive.c_str(), skey.c_str(), svalue.c_str());
+			LOG("    [%s]: %s = %s", modArchive.c_str(), skey.c_str(), svalue.c_str());
 		}
 	}
 
 	// load the mod archives
-	AddAllArchives(mod.c_str());
+	us::AddAllArchives(mod.c_str());
 
 	// unit names
 	while (true) {
-		const int left = ProcessUnits();
-		//printf("unitsLeft = %i\n", left);
+		const int left = us::ProcessUnits();
+		//LOG("unitsLeft = %i", left);
 		if (left <= 0) {
 			break;
 		}
 	}
-	printf("  UNITS\n");
-	const int unitCount = GetUnitCount();
+	LOG("  UNITS");
+	const int unitCount = us::GetUnitCount();
 	for (int i = 0; i < unitCount; i++) {
-		const string unitName     = GetUnitName(i);
-		const string unitFullName = GetFullUnitName(i);
-		printf("    [unit %3i]   %-16s  <%s>\n", i,
+		const string unitName     = us::GetUnitName(i);
+		const string unitFullName = us::GetFullUnitName(i);
+		LOG("    [unit %3i]   %-16s  <%s>", i,
 		unitName.c_str(), unitFullName.c_str());
 	}
-	printf("  SIDES\n");
-	const int sideCount = GetSideCount();
+	LOG("  SIDES");
+	const int sideCount = us::GetSideCount();
 	for (int i = 0; i < sideCount; i++) {
-		const string sideName  = GetSideName(i);
-		const string startUnit = GetSideStartUnit(i);
-		printf("    side %i = '%s' <%s>\n",
+		const string sideName  = us::GetSideName(i);
+		const string startUnit = us::GetSideStartUnit(i);
+		LOG("    side %i = '%s' <%s>",
 		i, sideName.c_str(), startUnit.c_str());
 	}
 
 	// available Skirmish AIs
-	printf("  SkirmishAI\n");
-	const int skirmishAICount = GetSkirmishAICount();
+	LOG("  SkirmishAI");
+	const int skirmishAICount = us::GetSkirmishAICount();
 	for (int i = 0; i < skirmishAICount; i++) {
-		const int skirmishAIInfoCount = GetSkirmishAIInfoCount(i);
-		printf("    %i:\n", i);
+		const int skirmishAIInfoCount = us::GetSkirmishAIInfoCount(i);
+		LOG("    %i:", i);
 		for (int j = 0; j < skirmishAIInfoCount; j++) {
-			const string key = GetInfoKey(j);
+			const string key = us::GetInfoKey(j);
 			if ((key == SKIRMISH_AI_PROPERTY_SHORT_NAME) || (key == SKIRMISH_AI_PROPERTY_VERSION)) {
-				const string value = GetInfoValueString(j);
-				printf("        %s = %s\n", key.c_str(), value.c_str());
+				const string value = us::GetInfoValueString(j);
+				LOG("        %s = %s", key.c_str(), value.c_str());
 			}
 		}
 	}
 
 	// MapOptions
-	printf("  MapOptions\n");
-	const int mapOptCount = GetMapOptionCount(map.c_str());
+	LOG("  MapOptions");
+	const int mapOptCount = us::GetMapOptionCount(map.c_str());
 	DisplayOptions(mapOptCount);
 
 	// ModOptions
-	printf("  ModOptions\n");
-	const int modOptCount = GetModOptionCount();
+	LOG("  ModOptions");
+	const int modOptCount = us::GetModOptionCount();
 	DisplayOptions(modOptCount);
 
 	// ModValidMaps
-	printf("  ModValidMaps\n");
-	const int modValidMapCount = GetModValidMapCount();
+	LOG("  ModValidMaps");
+	const int modValidMapCount = us::GetModValidMapCount();
 	if (modValidMapCount == 0) {
-		printf("    * ALL MAPS *\n");
+		LOG("    * ALL MAPS *");
 	}
 	else {
 		for (int i = 0; i < modValidMapCount; i++) {
-			printf("    %i: %s\n", i, GetModValidMap(i));
+			LOG("    %i: %s", i, us::GetModValidMap(i));
 		}
 	}
 
-	InitDirListVFS(NULL, NULL, NULL);
+	us::InitDirListVFS(NULL, NULL, NULL);
 	char buf[512];
-	for (int i = 0; (i = FindFilesVFS(i, buf, sizeof(buf))); /* noop */) {
-		printf("FOUND FILE:  %s\n", buf);
+	for (int i = 0; (i = us::FindFilesVFS(i, buf, sizeof(buf))); /* noop */) {
+		LOG("FOUND FILE:  %s", buf);
 	}
 
-	InitSubDirsVFS(NULL, NULL, NULL);
-	for (int i = 0; (i = FindFilesVFS(i, buf, sizeof(buf))); /* noop */) {
-		printf("FOUND DIR:  %s\n", buf);
+	us::InitSubDirsVFS(NULL, NULL, NULL);
+	for (int i = 0; (i = us::FindFilesVFS(i, buf, sizeof(buf))); /* noop */) {
+		LOG("FOUND DIR:  %s", buf);
 	}
 
-	UnInit();
+	us::UnInit();
 }
