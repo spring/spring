spring/spring

commit 2fafeb7c3166349e14c7d1efbcfc65e3bec5a9f5

restructure files into rts/Net/ & rts/Game/Players/ move all netcode & server/client related code (from Game/ & System) to rts/Net/* move some player structures from Game/ to Game/Players/

rts/Game/Game.cpp
@@ -21,16 +21,11 @@
 #include "CommandMessage.h"
 #include "ConsoleHistory.h"
 #include "GameHelper.h"
-#include "GameServer.h"
 #include "GameVersion.h"
 #include "GameSetup.h"
 #include "GlobalUnsynced.h"
 #include "LoadScreen.h"
 #include "SelectedUnitsHandler.h"
-#include "Player.h"
-#include "PlayerHandler.h"
-#include "PlayerRoster.h"
-#include "PlayerRosterDrawer.h"
 #include "WaitCommandsAI.h"
 #include "WordCompletion.h"
 #include "IVideoCapturing.h"
@@ -40,6 +35,11 @@
 #include "SyncedGameCommands.h"
 #include "UnsyncedActionExecutor.h"
 #include "UnsyncedGameCommands.h"
+#include "Game/GUI/PlayerRoster.h"
+#include "Game/GUI/PlayerRosterDrawer.h"
+#include "Game/Players/Player.h"
+#include "Game/Players/PlayerHandler.h"
+#include "Net/GameServer.h"
 #include "Game/UI/UnitTracker.h"
 #include "ExternalAI/EngineOutHandler.h"
 #include "ExternalAI/IAILibraryManager.h"
@@ -135,7 +135,7 @@
 #include "System/Sync/FPUCheck.h"
 #include "System/GlobalConfig.h"
 #include "System/myMath.h"
-#include "System/NetProtocol.h"
+#include "Net/Protocol/NetProtocol.h"
 #include "System/SpringApp.h"
 #include "System/Util.h"
 #include "System/Input/KeyInput.h"

rts/Net/NetCommands.cpp
@@ -1,34 +1,35 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#include "Game.h"
-#include "CameraHandler.h"
+#include "Game/Game.h"
 #include "GameServer.h"
-#include "CommandMessage.h"
-#include "GameSetup.h"
-#include "GlobalUnsynced.h"
-#include "SelectedUnitsHandler.h"
-#include "Player.h"
-#include "PlayerHandler.h"
-#include "ChatMessage.h"
-#include "System/TimeProfiler.h"
-#include "WordCompletion.h"
-#include "IVideoCapturing.h"
-#include "InMapDraw.h"
+
 #ifdef _WIN32
 #  include "winerror.h" // TODO someone on windows (MinGW? VS?) please check if this is required
 #endif
 #include "ExternalAI/EngineOutHandler.h"
 #include "ExternalAI/SkirmishAIHandler.h"
+#include "Game/CameraHandler.h"
+#include "Game/CommandMessage.h"
+#include "Game/GameSetup.h"
+#include "Game/GlobalUnsynced.h"
+#include "Game/SelectedUnitsHandler.h"
+#include "Game/ChatMessage.h"
+#include "Game/WordCompletion.h"
+#include "Game/IVideoCapturing.h"
+#include "Game/InMapDraw.h"
+#include "Game/Players/Player.h"
+#include "Game/Players/PlayerHandler.h"
+#include "Game/UI/GameSetupDrawer.h"
+#include "Game/UI/MouseHandler.h"
 #include "Lua/LuaRules.h"
-#include "UI/GameSetupDrawer.h"
-#include "UI/MouseHandler.h"
 #include "Rendering/GlobalRendering.h"
 #include "Sim/Misc/TeamHandler.h"
 #include "Sim/Path/IPathManager.h"
 #include "System/EventHandler.h"
 #include "System/Log/ILog.h"
 #include "System/myMath.h"
-#include "System/NetProtocol.h"
+#include "Net/Protocol/NetProtocol.h"
+#include "System/TimeProfiler.h"
 #include "System/LoadSave/DemoRecorder.h"
 #include "System/Net/UnpackPacket.h"
 #include "System/Sound/ISound.h"
@@ -57,22 +58,6 @@ void CGame::AddTraffic(int playerID, int packetCode, int length)
 
 void CGame::ClientReadNet()
 {
-	if (gu->gameTime - lastCpuUsageTime >= 1) {
-		lastCpuUsageTime = gu->gameTime;
-
-		if (playing) {
-			float simCpuUsage = profiler.GetPercent("SimFrame");
-
-			if (!GML::SimEnabled() || !GML::MultiThreadSim()) // take the minimum drawframes into account, too
-				simCpuUsage += (profiler.GetPercent("GameController::Draw") / std::max(1.0f, globalRendering->FPS)) * gu->minFPS;
-
-			net->Send(CBaseNetProtocol::Get().SendCPUUsage(simCpuUsage));
-		} else {
-			// the CPU-load percentage is undefined prior to SimFrame()
-			net->Send(CBaseNetProtocol::Get().SendCPUUsage(0.0f));
-		}
-	}
-
 	boost::shared_ptr<const netcode::RawPacket> packet;
 
 	// compute new msgProcTimeLeft to "smooth" out SimFrame() calls

commit 667d8303e225d56571481619c0f4b45b03a8c6ec

fix server speedcontrol logic broken by 2fafeb7c + ff55956c

rts/Game/Game.cpp
@@ -962,6 +962,7 @@ bool CGame::Update()
 	}
 
 	ENTER_SYNCED_CODE();
+	SendClientProcUsage();
 	ClientReadNet(); // this can issue new SimFrame()s
 
 	if (!gameOver) {

rts/Game/Game.h
@@ -128,6 +128,7 @@ private:
 
 	void ReColorTeams();
 
+	void SendClientProcUsage();
 	void ClientReadNet();
 	void SimFrame();
 	void StartPlaying();

rts/Net/NetCommands.cpp
@@ -55,6 +55,25 @@ void CGame::AddTraffic(int playerID, int packetCode, int length)
 	}
 }
 
+void CGame::SendClientProcUsage()
+{
+	static spring_time lastProcUsageUpdateTime = spring_gettime();
+
+	if ((spring_gettime() - lastProcUsageUpdateTime).toSecsf() >= 1.0f) {
+		lastProcUsageUpdateTime = spring_gettime();
+
+		if (playing) {
+			const float simProcUsage = (profiler.GetPercent("SimFrame"));
+			const float drawProcUsage = (profiler.GetPercent("GameController::Draw") / std::max(1.0f, globalRendering->FPS)) * gu->minFPS;
+
+			// take the minimum drawframes into account, too
+			net->Send(CBaseNetProtocol::Get().SendCPUUsage(simProcUsage + drawProcUsage * (!GML::SimEnabled() || !GML::MultiThreadSim())));
+		} else {
+			// the CPU-load percentage is undefined prior to SimFrame()
+			net->Send(CBaseNetProtocol::Get().SendCPUUsage(0.0f));
+		}
+	}
+}
 
 void CGame::ClientReadNet()
 {
@@ -67,10 +86,8 @@ void CGame::ClientReadNet()
 		if (skipping) {
 			msgProcTimeLeft = 0.01f;
 		} else {
-			if (msgProcTimeLeft > 1.0f)
-				msgProcTimeLeft -= 1.0f;
-
-			msgProcTimeLeft += consumeSpeed * (spring_tomsecs(currentFrame - lastframe) / 1000.0f);
+			msgProcTimeLeft -= ((msgProcTimeLeft > 1.0f) * 1.0f);
+			msgProcTimeLeft += (consumeSpeed * (spring_tomsecs(currentFrame - lastframe) / 1000.0f));
 		}
 
 		lastframe = currentFrame;