81230b101a7131046aae62fb150182466a0931d7
jK <-jk->
2013-05-21 18:50:20 +0200

    fix 0360de80

--- rts/Game/ChatMessage.cpp
+++ rts/Game/ChatMessage.cpp
@@ -33,15 +33,18 @@ ChatMessage::ChatMessage(boost::shared_ptr<const netcode::RawPacket> data)
 const netcode::RawPacket* ChatMessage::Pack() const
 {
 	unsigned size = (4 * sizeof(unsigned char)) + (msg.size() + 1);
-	boost::uint8_t csize = std::min(size, UINT8_MAX);
-	if (size > UINT16_MAX) {
-		msg.resize(size - csize);
-	}
+	boost::uint8_t csize = (size > UINT8_MAX) ? size : UINT8_MAX;
 
 	PackPacket* buffer = new PackPacket(size, NETMSG_CHAT);
 	*buffer << csize;
 	*buffer << (unsigned char)fromPlayer;
 	*buffer << (unsigned char)destination;
-	*buffer << msg;
+	if (size > UINT16_MAX) {
+		std::string msg_(msg);
+		msg_.resize(size - csize);
+		*buffer << msg_;
+	} else {
+		*buffer << msg;
+	}
 	return buffer;
 }
