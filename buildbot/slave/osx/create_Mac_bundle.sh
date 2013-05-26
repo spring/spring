92adf7b35e0cc2bc1aa273310ce5190dd56d229f
daftalx <daft@gmx.fr>
2013-05-26 13:41:53 +0200

    Update create_Mac_bundle.sh
    * thorough verbo-isation for faster debugging (than mentally)
    * use same sub-directory trick in lib dependency scanning (2nd part)

--- buildbot/slave/osx/create_Mac_bundle.sh
+++ buildbot/slave/osx/create_Mac_bundle.sh
@@ -33,64 +33,71 @@ MACPORTS_BASE=`which port | sed s/[/]bin[/]port//`
 STRIP="strip -r -u"
 
 mkdir -p ${BUNDLE_BASE}
+echo "-- changing directory to ${BUNDLE_BASE}"
 cd ${BUNDLE_BASE}
 
 # insert the version number in the Info.plist file
-echo "-- installing header file"
+echo "-- creating executable header file (Info.plist)"
 cat ${SOURCEDIR}/installer/Mac/Info.plist | sed s/###VERSION###/${VERSION}/ > Info.plist
 
+echo "-- creating folders MacOS and Resources"
 mkdir MacOS Resources
 
-echo "-- installing executables"
+echo "-- installing executables into folder: MacOS"
 mv ${INSTALLDIR}/bin/* MacOS/
 
-echo "-- installing application icon"
+echo "-- installing application icon into folder: Resources"
 cp ${SOURCEDIR}/installer/Mac/spring.icns Resources/
 
-echo "-- installing spring libs"
+echo "-- moving library folder into folder: lib"
 mv ${INSTALLDIR}/lib .
 
-echo "-- installing game content"
-mv ${INSTALLDIR}/share .
-
-# move spring libraries to the executable folder (avoids spring relative path issues)
+echo "-- moving unitsync library to folder: MacOS (due to spring's relative pathing)"
 mv lib/libunitsync.dylib MacOS/
 
 # not needed (yet) for the server lib, uncomment if necessary
 #mv lib/libspringserver.dylib MacOS/
 
-echo "-- installing 3rd-party libraries (provided by MacPorts)"
+echo "-- moving game content into folder: share"
+mv ${INSTALLDIR}/share .
+
+echo "-- copying 3rd-party libraries into folder: lib (provided by MacPorts)"
 for executable in `ls MacOS`
 do
+	echo "--- copying libraries for ${executable}"
+	
 	# for each MacPorts's dylib required by the executable
 	for dylib in `otool -L MacOS/${executable} | grep ${MACPORTS_BASE} | egrep -o lib.*dylib`
 	do
 		# dylib =~ "lib/lib*.dylib"
 
-		echo "---- installing ${dylib}"
+		echo "---- copying ${MACPORTS_BASE}/${dylib} to folder: lib"
 		cp ${MACPORTS_BASE}/${dylib} lib
 
 		# strip down lib's subdirectories (if any)
 		dylib=lib/`echo ${dylib} | egrep -o [^/]*$`
 
-		# take write permissions on the bundled lib
+		echo "---- taking write permissions on ${dylib}"
 		chmod u+w ${dylib}
 
 		# change the bundled lib's id to relative pathing mode (not necessary but cleaner)
 		install_name_tool -id @loader_path/../${dylib} ${dylib}
 
+		echo "---- telling ${executable} how to find ${dylib}"
 		# point the executable to the bundled lib in relative pathing mode
 		install_name_tool -change ${MACPORTS_BASE}/${dylib} @loader_path/../${dylib} MacOS/${executable}
 
 		# the bundled lib is autonomous => it can be stripped
+		echo "---- stripping down ${dylib}"
 		${STRIP} ${dylib}
 	done
 
-	# finally, strip the executable
+	echo "--- stripping down ${executable}"
 	${STRIP} MacOS/${executable}
 done
 
 # continue with recursive dependencies
+echo "-- copying other required 3rd-party libraries into folder: lib"
 end=0
 while [ $end -eq 0 ]
 do
@@ -101,29 +108,36 @@ do
 	for dylib in `ls lib`
 	do
 		# check if all dependent MacPorts libs are also bundled
+		echo "--- looking for missing dependencies of lib/${dylib}"
 		for requiredlib in `otool -L lib/${dylib} | grep ${MACPORTS_BASE} | egrep -o lib.*dylib`
 		do
-			#echo "MacPorts lib required: ${requiredlib}"
-			if [ ! -f ${requiredlib} ]
+			# strip down lib's subdirectories (if any)
+			locallib=lib/`echo ${requiredlib} | egrep -o [^/]*$`
+
+			echo "---- scanning dependency: ${locallib}"
+
+			if [ ! -f ${locallib} ]
 			then
 				# loop
 				end=0
 
-				echo "---- installing ${requiredlib}"
+				echo "---- copying ${MACPORTS_BASE}/${requiredlib} to folder: lib"
 				cp ${MACPORTS_BASE}/${requiredlib} lib
 
-				# take write permissions on the bundled lib
-				chmod u+w ${requiredlib}
+				echo "---- taking write permissions on ${locallib}"
+				chmod u+w ${locallib}
 
 				# change the bundled lib's id to relative pathing mode (not necessary but cleaner)
-				install_name_tool -id @loader_path/../${requiredlib} ${requiredlib}
+				install_name_tool -id @loader_path/../${locallib} ${locallib}
 
 				# the bundled lib is autonomous => it can be stripped
-				${STRIP} ${requiredlib}
+				echo "---- stripping down ${locallib}"
+				${STRIP} ${locallib}
 			fi
 
 			# point the parent lib to the bundled lib in relative pathing mode
-			install_name_tool -change ${MACPORTS_BASE}/${requiredlib} @loader_path/../${requiredlib} lib/${dylib}
+			echo "---- telling lib/${dylib} how to find ${locallib}"
+			install_name_tool -change ${MACPORTS_BASE}/${requiredlib} @loader_path/../${locallib} lib/${dylib}
 		done
 	done
 done
