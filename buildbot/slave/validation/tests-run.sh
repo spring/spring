28f8019f0a7d22680bcf7d86fd4b77c377fa0f73
abma <spring@abma.de>
2013-07-17 03:32:24 +0200

    compress files in writeable dir only once

--- buildbot/slave/validation/tests-run.sh
+++ buildbot/slave/validation/tests-run.sh
@@ -12,10 +12,13 @@ if [ ! -n "$1" ]; then
 	exit 1
 fi
 
+# don't abort on error
+set +e
 #run test
 HOME=${TESTDIR} ${SOURCEDIR}/test/validation/run.sh ${TESTDIR}/usr/local/bin/spring-headless $@
 
 # zip output for upload
 mkdir -p ${TMP_PATH}/validation/
 ${SEVENZIP} ${TMP_PATH}/validation/$VERSION_$(date +"%Y-%m-%d_%H-%M-%S").7z ${TMP_BASE}/tests/.config/spring/
+rm -rf ${TMP_BASE}/tests/.config/spring/
 
