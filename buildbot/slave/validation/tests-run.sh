6b7205523a52f018081f8f68f8897ff09f512e77
abma <spring@abma.de>
2013-07-17 03:19:19 +0200

    always upload output of validation run

--- buildbot/slave/validation/tests-run.sh
+++ buildbot/slave/validation/tests-run.sh
@@ -4,6 +4,7 @@ set -e
 . buildbot/slave/prepare.sh
 
 TESTDIR=${TMP_BASE}/tests
+SEVENZIP="7z a"
 
 
 if [ ! -n "$1" ]; then
@@ -13,4 +14,8 @@ fi
 
 #run test
 HOME=${TESTDIR} ${SOURCEDIR}/test/validation/run.sh ${TESTDIR}/usr/local/bin/spring-headless $@
+
+# zip output for upload
+mkdir -p ${TMP_PATH}/validation/
+${SEVENZIP} ${TMP_PATH}/validation/$VERSION_$(date +"%Y-%m-%d_%H-%M-%S").7z ${TMP_BASE}/tests/.config/spring/
 
