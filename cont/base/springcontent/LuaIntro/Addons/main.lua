4066c8dc1d92016ec4963b398095b9a5ecc342af
jK <-jk->
2013-07-18 15:19:07 +0200

    LuaIntro: split default loadbar & music addon

--- cont/base/springcontent/LuaIntro/Addons/main.lua
+++ cont/base/springcontent/LuaIntro/Addons/main.lua
@@ -2,9 +2,9 @@
 if addon.InGetInfo then
 	return {
 		name    = "Main",
-		desc    = "(music and other stuff)",
+		desc    = "displays a simplae loadbar",
 		author  = "jK",
-		date    = "2012",
+		date    = "2012,2013",
 		license = "GPL2",
 		layer   = 0,
 		depend  = {"LoadProgress"},
@@ -14,8 +14,6 @@ end
 
 ------------------------------------------
 
-require "savetable.lua"
-
 local lastLoadMessage = ""
 
 function addon.LoadProgress(message, replaceLastLine)
@@ -24,25 +22,10 @@ end
 
 ------------------------------------------
 
-Spring.SetSoundStreamVolume(0)
-local musicfiles = VFS.DirList(LUA_DIRNAME .. "Assets/music", "*.ogg")
-if (#musicfiles > 0) then
-	Spring.PlaySoundStream(musicfiles[ math.random(#musicfiles) ], 1)
-	Spring.SetSoundStreamVolume(0)
-end
-
-
 local font = gl.LoadFont("FreeSansBold.otf", 50, 20, 1.95)
 
 function addon.DrawLoadScreen()
 	local loadProgress = SG.GetLoadProgress()
-
-	-- fade in & out music with progress
-	if (loadProgress < 0.9) then
-		Spring.SetSoundStreamVolume(loadProgress)
-	else
-		Spring.SetSoundStreamVolume(0.9 + ((0.9 - loadProgress) * 9))
-	end
 
 	local vsx, vsy = gl.GetViewSizes()
 
@@ -195,7 +178,5 @@ end
 
 
 function addon.Shutdown()
-	Spring.StopSoundStream()
-	Spring.SetSoundStreamVolume(1)
 	gl.DeleteFont(font)
 end
