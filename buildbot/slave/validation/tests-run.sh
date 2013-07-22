8c94bdd1a07adf3b64453431e4d2045ab8ccedaf
abma <spring@abma.de>
2013-07-22 07:45:36 +0200

    validation test: run prepare & analyze for each test

--- buildbot/slave/validation/tests-run.sh
+++ buildbot/slave/validation/tests-run.sh
@@ -17,8 +17,4 @@ set +e
 #run test
 HOME=${TESTDIR} ${SOURCEDIR}/test/validation/run.sh ${TESTDIR}/usr/local/bin/spring-headless $@
 
-# zip output for upload
-mkdir -p ${TMP_PATH}/validation/
-${SEVENZIP} ${TMP_PATH}/validation/$VERSION_$(date +"%Y-%m-%d_%H-%M-%S")-dbg.7z ${TMP_BASE}/tests/.config/spring/
-rm -rf ${TMP_BASE}/tests/.config/spring/
 
