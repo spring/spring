e279a12ce6f664e0b0380844f4bc11997957528a
abma <spring@abma.de>
2013-07-22 09:27:40 +0200

    validation test: fix tests-run.sh

--- buildbot/slave/validation/tests-run.sh
+++ buildbot/slave/validation/tests-run.sh
@@ -6,6 +6,11 @@ set -e
 TESTDIR=${TMP_BASE}/tests
 SEVENZIP="7z a"
 
+GAME=$2
+MAP=$3
+AI=$4
+AIVER=$5
+echo "Running test: GAME=$GAME MAP=$MAP AI=$AI AIVER=$AIVER"
 
 if [ ! -n "$1" ]; then
 	echo "Error in $0: no test script given"
@@ -15,6 +20,6 @@ fi
 # don't abort on error
 set +e
 #run test
-HOME=${TESTDIR} ${SOURCEDIR}/test/validation/run.sh ${TESTDIR}/usr/local/bin/spring-headless $@
+HOME=${TESTDIR} ${SOURCEDIR}/test/validation/run.sh ${TESTDIR}/usr/local/bin/spring-headless script.txt
 
 
