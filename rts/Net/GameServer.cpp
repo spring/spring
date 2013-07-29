29e76eb2533f01288338a84cc54801bafef41ee8
jK <-jk->
2013-07-29 19:33:19 +0200

    try to fix mingw64 "%hc" warning

--- rts/Net/GameServer.cpp
+++ rts/Net/GameServer.cpp
@@ -1858,8 +1858,8 @@ void CGameServer::GenerateAndSendGameID()
 	#ifdef __MINGW32__
 		// workaround missing C99 support in a msvc lib with %02hhx
 		generatedGameID = (sscanf(setup->gameID.c_str(),
-		      "%02hc%02hc%02hc%02hc%02hc%02hc%02hc%02hc"
-		      "%02hc%02hc%02hc%02hc%02hc%02hc%02hc%02hc",
+		      "%02hs%02hs%02hs%02hs%02hs%02hs%02hs%02hs"
+		      "%02hs%02hs%02hs%02hs%02hs%02hs%02hs%02hs",
 		      &p[ 0], &p[ 1], &p[ 2], &p[ 3], &p[ 4], &p[ 5], &p[ 6], &p[ 7],
 		      &p[ 8], &p[ 9], &p[10], &p[11], &p[12], &p[13], &p[14], &p[15]) == 16);
 	#else
