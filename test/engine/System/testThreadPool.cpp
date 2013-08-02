a10915286954a91f7fa886b76cc4f30e22db8b4b
abma <spring@abma.de>
2013-08-02 14:37:33 +0200

    testThreadPool: print stats & add additional check

--- test/engine/System/testThreadPool.cpp
+++ test/engine/System/testThreadPool.cpp
@@ -11,19 +11,30 @@
 BOOST_AUTO_TEST_CASE( ThreadPoolTest )
 {
 	std::vector<int> nums(1000);
-
+	std::vector<int> runs(NUM_THREADS);
 	ThreadPool::SetThreadCount(NUM_THREADS);
 	BOOST_CHECK(ThreadPool::GetNumThreads() == NUM_THREADS);
 	for_mt(0, nums.size(), [&](const int y) {
-		BOOST_CHECK(ThreadPool::GetThreadNum() >= 0);
-		BOOST_CHECK(ThreadPool::GetThreadNum() < NUM_THREADS);
+		const int threadnum = ThreadPool::GetThreadNum();
+		BOOST_CHECK(threadnum >= 0);
+		BOOST_CHECK(threadnum < NUM_THREADS);
 		for(int i=0; i<=y; i++) {
 			nums[y] = i;
 		}
+
+		runs[threadnum]++;
 	});
 
 	for(int i=0; i<nums.size(); i++) {
 		BOOST_CHECK(nums[i] == i);
 	}
+
+	int rounds = 0;
+	for(int i=0; i<runs.size(); i++) {
+		printf("Thread %d executed %d times\n", i, runs[i]);
+		rounds += runs[i];
+	}
+	BOOST_CHECK(rounds = nums.size());
 }
+
 
