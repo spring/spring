18afe0557c56e3e92a04ea09b4427fb808aa81a4
jK <-jk->
2013-08-03 03:22:11 +0200

    threadpool ut: make the others safe too

--- test/engine/System/testThreadPool.cpp
+++ test/engine/System/testThreadPool.cpp
@@ -8,10 +8,9 @@
 
 #define NUM_THREADS 10
 
-static boost::mutex m;
-
 // !!! BOOST.TEST IS NOT THREADSAFE !!! (facepalms)
 #define SAFE_BOOST_CHECK( P )           do { boost::lock_guard<boost::mutex> _(m); BOOST_CHECK( ( P ) );               } while( 0 );
+static boost::mutex m;
 
 
 BOOST_AUTO_TEST_CASE( testThreadPool1 )
@@ -24,8 +23,8 @@ BOOST_AUTO_TEST_CASE( testThreadPool1 )
 	BOOST_CHECK(ThreadPool::GetNumThreads() == NUM_THREADS);
 	for_mt(0, RUNS, 2, [&](const int i) {
 		const int threadnum = ThreadPool::GetThreadNum();
-			SAFE_BOOST_CHECK(threadnum >= 0);
 		SAFE_BOOST_CHECK(threadnum < NUM_THREADS);
+		SAFE_BOOST_CHECK(threadnum >= 0);
 		runs[threadnum]++;
 		nums[i] = 1;
 		++cnt;
@@ -51,8 +50,8 @@ BOOST_AUTO_TEST_CASE( testThreadPool2 )
 	BOOST_CHECK(ThreadPool::GetNumThreads() == NUM_THREADS);
 	parallel([&]{
 		const int threadnum = ThreadPool::GetThreadNum();
-		BOOST_CHECK(threadnum >= 0);
-		BOOST_CHECK(threadnum < NUM_THREADS);
+		SAFE_BOOST_CHECK(threadnum >= 0);
+		SAFE_BOOST_CHECK(threadnum < NUM_THREADS);
 		runs[threadnum]++;
 	});
 
