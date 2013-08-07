cde72bf53318b8d51180a2f6a0e4b983d94a4eeb
abma <spring@abma.de>
2013-08-08 01:08:05 +0200

    Revert "change loadprogress.lua to hardcoded checkpoints:"
    This reverts commit 5961aaa30bbfa64bfc58e5efa9a5fab24864207b.

--- cont/base/springcontent/LuaIntro/Addons/loadprogress.lua
+++ cont/base/springcontent/LuaIntro/Addons/loadprogress.lua
@@ -13,59 +13,31 @@ end
 
 ------------------------------------------
 
-local checkpoints = {
-		"Parsing Map Information",
-		"Loading SMF",
-		"Loading Radar Icons",
-		"Loading GameData Definitions",
-		"Loading Sound Definitions",
-		"Creating Smooth Height Mesh",
-		"Creating QuadField & CEGs",
-		"Creating Unit Textures",
-		"Creating Sky",
-		"Loading Weapon Definitions",
-		"Loading Unit Definitions",
-		"Loading Feature Definitions",
-		"Initializing Map Features",
-		"Creating ShadowHandler & DecalHandler",
-		"Creating GroundDrawer",
-		"Loading Tile Files",
-		"Reading Tile Map",
-		"Loading Square Textures",
-		"Creating TreeDrawer",
-		"Creating ProjectileDrawer & UnitDrawer",
-		"Creating Projectile Textures",
-		"Creating Water",
-		"Loading LuaRules",
-		"Loading LuaGaia",
-		"Loading LuaUI",
-		"Initializing PathCache",
-		"Finalizing",
-	}
+require "savetable.lua"
 
-local function endswith(s, send)
-	return #s >= #send and s:find(send, #s-#send+1, true) and true or false
+local startTime = -1
+local cachedLoadTimes = VFS.FileExists("loadprogress_cached.lua") and VFS.Include("loadprogress_cached.lua") or { }
+local cachedTotalTime = (cachedLoadTimes[Game.mapName] or -1) * 0.97 --*0.97 cause else last rendered frame would show 99%
+
+
+local function mix(x,y,a)
+	return x * (1-a) + y * a
 end
 
-local pos = 0 -- position in checkpoints
-local progress = 1 -- loading progress: 0..1
-
-function addon.LoadProgress(str)
-	for i = pos + 1,#checkpoints do
-		if endswith(str, checkpoints[i]) then -- checkpoint found, update progress
-			if i > pos + 1 then
-				Spring.Log("loadprogress.lua", LOG.WARNING, "skipped checkpoint" .. checkpoints[i-1])
-			end
-			-- Spring.Log("loadprogress.lua", LOG.WARNING, "checkpoint " .. checkpoints[i])
-			pos = i
-			break
-		end
-	end
-	-- update progress for SG.GetLoadProgress()
-	progress = pos/#checkpoints
-end
 
 function SG.GetLoadProgress()
-	return progress
+	if (startTime < 0) then startTime = os.clock() end
+
+	if cachedTotalTime > 0 then
+		return math.max(0.0, math.min(1.0, (os.clock() - startTime) / cachedTotalTime))
+	else
+		return -1
+	end
+end
+
+
+function addon.Shutdown()
+	cachedLoadTimes[Game.mapName] = os.clock() - startTime
+	table.save(cachedLoadTimes, "loadprogress_cached.lua")
 end
 
