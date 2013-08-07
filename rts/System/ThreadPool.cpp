637026ee6926fc3899d81afd009e71c0c204a98a
jK <-jk->
2013-08-07 11:34:48 +0200

    improve threadpool: * make shared gl ctx creation safe & add fallback * disable shred gl ctx for now (not used atm) * use lockfree taskqueues (reduces latency even more)

--- rts/System/ThreadPool.cpp
+++ rts/System/ThreadPool.cpp
@@ -12,20 +12,20 @@
 #include <utility>
 #include <boost/optional.hpp>
 
-#ifdef UNITSYNC
-typedef boost::thread thread_class;
-#else
-typedef COffscreenGLThread thread_class;
-#endif
-
 static std::deque<std::shared_ptr<ITaskGroup>> taskGroups;
-static std::deque<thread_class> thread_group;
+static std::deque<void*> thread_group;
 
-static boost::mutex taskMutex;
+static boost::shared_mutex taskMutex;
 static boost::condition_variable newTasks;
 
+#ifndef UNITSYNC
+static bool hasOGLthreads = false; // disable for now (not used atm)
+#else
+static bool hasOGLthreads = false;
+#endif
 static __thread int threadnum(0); //FIXME __thread is gcc only, thread_local is c++11 but doesn't work in <4.8, and is also slower
 static __thread bool exitThread(false);
+
 
 static struct do_once {
 	do_once() {
@@ -67,44 +67,55 @@ int GetNumThreads()
 }
 
 
-static void DoTask(boost::unique_lock<boost::mutex>& lk)
+static void DoTask(boost::shared_lock<boost::shared_mutex>& lk)
 {
-	if (lk.try_lock()) { //FIXME make it lock-free?
-		for(auto it = taskGroups.begin(); it != taskGroups.end();) {
-			auto& tg = **it;
-			auto p = tg.GetTask();
+	if (lk.try_lock()) {
+		bool foundEmpty = false;
+
+		for(auto tg: taskGroups) {
+			auto p = tg->GetTask();
+
 			if (p) {
-				lk.unlock();
 				SCOPED_MT_TIMER("::ThreadWorkers (accumulated)");
 				try {
 					(*p)();
 				} catch(...) {
 					assert(false);
-					tg.PushException(std::current_exception());
+					tg->PushException(std::current_exception());
 				}
+				lk.unlock();
 				return;
 			}
-			if (tg.IsEmpty()) {
-				it = taskGroups.erase(it);
-			} else {
-				++it;
+
+			if (tg->IsEmpty()) {
+				foundEmpty = true;
 			}
 		}
 		lk.unlock();
+
+		if (foundEmpty) {
+			//FIXME this could be made lock-free too, but is it worth it?
+			boost::unique_lock<boost::shared_mutex> ulk(taskMutex, boost::defer_lock);
+			while (!ulk.try_lock()) {}
+			for(auto it = taskGroups.begin(); it != taskGroups.end();) {
+				if ((*it)->IsEmpty()) {
+					it = taskGroups.erase(it);
+				} else {
+					++it;
+				}
+			}
+			ulk.unlock();
+		}
 	}
 }
 
 
-static void DoTask(std::shared_ptr<ITaskGroup> tg, boost::unique_lock<boost::mutex>& lk)
+static void DoTask(std::shared_ptr<ITaskGroup> tg)
 {
-	if (lk.try_lock()) { //FIXME make it lock-free?
-		auto p = tg->GetTask();
-		lk.unlock();
-		if (p) {
-			SCOPED_MT_TIMER("::ThreadWorkers (accumulated)");
-			(*p)();
-			return;
-		}
+	auto p = tg->GetTask();
+	if (p) {
+		SCOPED_MT_TIMER("::ThreadWorkers (accumulated)");
+		(*p)();
 	}
 }
 
@@ -113,7 +124,7 @@ static void WorkerLoop(int id)
 {
 	SetThreadNum(id);
 	Threading::SetThreadName(IntToString(id, "worker%i"));
-	boost::unique_lock<boost::mutex> lk(taskMutex, boost::defer_lock);
+	boost::shared_lock<boost::shared_mutex> lk(taskMutex, boost::defer_lock);
 	boost::mutex m;
 	boost::unique_lock<boost::mutex> lk2(m);
 
@@ -132,10 +143,8 @@ static void WorkerLoop(int id)
 
 void WaitForFinished(std::shared_ptr<ITaskGroup> taskgroup)
 {
-	boost::unique_lock<boost::mutex> lk(taskMutex, boost::defer_lock);
-
 	while (!taskgroup->IsEmpty()) {
-		DoTask(taskgroup, lk);
+		DoTask(taskgroup);
 	}
 
 	while (!taskgroup->IsFinished()) { }
@@ -147,7 +156,7 @@ void WaitForFinished(std::shared_ptr<ITaskGroup> taskgroup)
 
 void PushTaskGroup(std::shared_ptr<ITaskGroup> taskgroup)
 {
-	boost::unique_lock<boost::mutex> lk(taskMutex, boost::defer_lock);
+	boost::unique_lock<boost::shared_mutex> lk(taskMutex, boost::defer_lock);
 	while (!lk.try_lock()) {}
 	taskGroups.emplace_back(taskgroup);
 	lk.unlock();
@@ -166,8 +175,24 @@ void SetThreadCount(int num)
 	int curThreads = GetNumThreads();
 
 	if (curThreads < num) {
-		for (int i = curThreads; i<num; ++i) {
-			thread_group.emplace_back(boost::bind(&WorkerLoop, i));
+#ifndef UNITSYNC
+		if (hasOGLthreads) {
+			try {
+				for (int i = curThreads; i<num; ++i) {
+					thread_group.push_back(new COffscreenGLThread(boost::bind(&WorkerLoop, i)));
+				}
+			} catch (const opengl_error& gle) {
+				// shared gl context creation failed :<
+				SetThreadCount(0);
+				hasOGLthreads = false;
+				curThreads = 0;
+			}
+		}
+#endif
+		if (!hasOGLthreads) {
+			for (int i = curThreads; i<num; ++i) {
+				thread_group.push_back(new boost::thread(boost::bind(&WorkerLoop, i)));
+			}
 		}
 	} else {
 		for (int i = curThreads; i>num && i>1; --i) {
@@ -176,8 +201,18 @@ void SetThreadCount(int num)
 			auto taskgroup = std::make_shared<ParallelTaskGroup<const std::function<void()>>>();
 			taskgroup->enqueue_unique(GetNumThreads() - 1, []{ exitThread = true; });
 			ThreadPool::PushTaskGroup(taskgroup);
-
-			thread_group.back().join();
+#ifndef UNITSYNC
+			if (hasOGLthreads) {
+				auto th = reinterpret_cast<COffscreenGLThread*>(thread_group.back());
+				th->join();
+				delete th;
+			} else
+#endif
+			{
+				auto th = reinterpret_cast<boost::thread*>(thread_group.back());
+				th->join();
+				delete th;
+			}
 			thread_group.pop_back();
 		}
 		if (num == 0) assert(thread_group.empty());
