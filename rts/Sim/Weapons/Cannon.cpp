16ebc12b9ebd7ad556edeff103837ce53a660029
abma <spring@abma.de>
2013-08-08 23:48:53 +0200

    revert part of 858d517f8e007a4fd191b469e6b7c1556ef37819
    fix #3913
    
    thanks FLOZi!
    
    (fuuu... cppcheck!)

--- rts/Sim/Weapons/Cannon.cpp
+++ rts/Sim/Weapons/Cannon.cpp
@@ -63,6 +63,7 @@ void CCannon::UpdateRange(float val)
 {
 	range = val;
 	// initialize range factor
+	rangeFactor = 1.f;
 	rangeFactor = range / GetRange2D(0);
 	// do not extend range if the modder specified speed too low
 	// for the projectile to reach specified range
