da8ef9e3768c9b289a5b4d8960dac0e6933a7d45
abma <spring@abma.de>
2013-08-13 03:24:29 +0200

    compile warnings--

--- rts/Sim/Misc/RadarHandler.cpp
+++ rts/Sim/Misc/RadarHandler.cpp
@@ -49,9 +49,9 @@ CRadarHandler::CRadarHandler(bool circularRadar)
   circularRadar(circularRadar),
   xsize(std::max(1, gs->mapx >> radarMipLevel)),
   zsize(std::max(1, gs->mapy >> radarMipLevel)),
+  radarAlgo(int2(xsize, zsize), -1000, 20, readmap->GetMIPHeightMapSynced(radarMipLevel)),
   baseRadarErrorSize(96.0f),
-  baseRadarErrorMult(2.0f),
-  radarAlgo(int2(xsize, zsize), -1000, 20, readmap->GetMIPHeightMapSynced(radarMipLevel))
+  baseRadarErrorMult(2.0f)
 {
 	commonJammerMap.SetSize(xsize, zsize, false);
 	commonSonarJammerMap.SetSize(xsize, zsize, false);
