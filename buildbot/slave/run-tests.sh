999538f9e9f4590782228401af6c151692c7b436
jK <-jk->
2013-08-14 12:12:35 +0200

    ulimit: detect #cores and increase cputime depending on it

--- buildbot/slave/run-tests.sh
+++ buildbot/slave/run-tests.sh
@@ -3,7 +3,9 @@
 set -e
 . buildbot/slave/prepare.sh
 
-ulimit -t 600 -m 500000 -v 4000000
+NUM_CORES=1
+which nproc &> /dev/null && NUM_CORES=`nproc`
+ulimit -t $((NUM_CORES * 100)) -m 500000 -v 4000000
 
 cd ${BUILDDIR}
 export SPRING_DATADIR=${BUILDDIR}
