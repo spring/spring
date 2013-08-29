a6604706be02eabbebed721883f64d6bd134f7c6
jK <-jk->
2013-08-29 17:57:36 +0200

    SpringTime UT: compare best clock with spring used one and fail if it is 3x as worse

--- test/engine/System/Misc/testSpringTime.cpp
+++ test/engine/System/Misc/testSpringTime.cpp
@@ -13,31 +13,6 @@
 
 static const int testRuns = 1000000;
 
-template<class Clock>
-struct TestProcessor {
-	static inline void Run()
-	{
-		ScopedOnceTimer timer(Clock::GetName());
-		int64_t lastTick = Clock::Get();
-		int64_t maxTick = 0;
-		int64_t minTick = 1e9;
-		float avgTick = 0;
-
-		for (int i=0; i < testRuns; ++i) {
-			int64_t curTick = Clock::Get();
-			int64_t tick = curTick - lastTick;
-			maxTick = std::max<int64_t>(tick, maxTick);
-			minTick = std::min<int64_t>(tick, minTick);
-			avgTick = float(i * avgTick + tick) / (i + 1);
-			lastTick = curTick;
-		}
-
-		float maxMsTick = maxTick * Clock::ToMs();
-		float minMsTick = std::max<int64_t>(minTick, 1LL) * Clock::ToMs();
-		float avgMsTick = std::max<int64_t>(avgTick, 1.0f) * Clock::ToMs();
-		LOG("[%17s] maxTick: %3.6fms minTick: %3.6fms avgTick: %3.6fms", Clock::GetName().c_str(), maxMsTick, minMsTick, avgMsTick);
-	}
-};
 
 #include <SDL_timer.h>
 struct SDLClock {
@@ -109,6 +84,43 @@ struct PosixClock {
 #endif
 
 
+struct SpringClock {
+	static inline float ToMs() { return 1.0f / 1e6; }
+	static inline std::string GetName() { return "SpringTime"; }
+	static inline int64_t Get() {
+		return spring_time::gettime().toNanoSecs();
+	}
+};
+
+
+template<class Clock>
+struct TestProcessor {
+	static inline float Run()
+	{
+		ScopedOnceTimer timer(Clock::GetName());
+		int64_t lastTick = Clock::Get();
+		int64_t maxTick = 0;
+		int64_t minTick = 1e9;
+		float avgTick = 0;
+
+		for (int i=0; i < testRuns; ++i) {
+			int64_t curTick = Clock::Get();
+			int64_t tick = curTick - lastTick;
+			maxTick = std::max<int64_t>(tick, maxTick);
+			minTick = std::min<int64_t>(tick, minTick);
+			avgTick = float(i * avgTick + tick) / (i + 1);
+			lastTick = curTick;
+		}
+
+		float maxMsTick = maxTick * Clock::ToMs();
+		float minMsTick = std::max<int64_t>(minTick, 1LL) * Clock::ToMs();
+		float avgMsTick = std::max<int64_t>(avgTick, 1.0f) * Clock::ToMs();
+		LOG("[%17s] maxTick: %3.6fms minTick: %3.6fms avgTick: %3.6fms", Clock::GetName().c_str(), maxMsTick, minMsTick, avgMsTick);
+		return avgMsTick;
+	}
+};
+
+
 BOOST_AUTO_TEST_CASE( ClockQualityCheck )
 {
 	BOOST_CHECK(boost::chrono::high_resolution_clock::is_steady);
@@ -116,24 +128,35 @@ BOOST_AUTO_TEST_CASE( ClockQualityCheck )
 	BOOST_WARN(std::chrono::high_resolution_clock::is_steady);
 #endif
 
-	TestProcessor<SDLClock>::Run();
-	TestProcessor<BoostChronoClock>::Run();
-	TestProcessor<BoostChronoMicroClock>::Run();
+	float bestAvg = 1e9;
+
+	bestAvg = std::min(bestAvg, TestProcessor<SDLClock>::Run());
+	bestAvg = std::min(bestAvg, TestProcessor<BoostChronoClock>::Run());
+	bestAvg = std::min(bestAvg, TestProcessor<BoostChronoMicroClock>::Run());
 #ifdef Boost_TIMER_FOUND
-	TestProcessor<BoostTimerClock>::Run();
+	bestAvg = std::min(bestAvg, TestProcessor<BoostTimerClock>::Run());
 #endif
 #if defined(__USE_GNU) && !defined(WIN32)
-	TestProcessor<PosixClock>::Run();
+	bestAvg = std::min(bestAvg, TestProcessor<PosixClock>::Run());
 #endif
 #if __cplusplus > 199711L
-	TestProcessor<Cpp11ChronoClock>::Run();
+	bestAvg = std::min(bestAvg, TestProcessor<Cpp11ChronoClock>::Run());
 #endif
+	float springAvg = TestProcessor<SpringClock>::Run();
+
+	bestAvg = std::min(bestAvg, springAvg);
+	BOOST_CHECK( std::abs(springAvg - bestAvg) < 3.f * bestAvg );
 }
 
 
 void sleep_posix()  { boost::this_thread::sleep(boost::posix_time::milliseconds(1)); }
 #ifdef BOOST_THREAD_USES_CHRONO
-void sleep_chrono() { boost::this_thread::sleep_for(boost::chrono::nanoseconds( 1 )); }
+void sleep_boost() { boost::this_thread::sleep_for(boost::chrono::nanoseconds( 1 )); }
+#endif
+void yield_boost() { boost::this_thread::yield(); }
+#if __cplusplus > 199711L
+#include <thread>
+void yield_chrono() { std::this_thread::yield(); }
 #endif
 
 void BenchmarkSleepFnc(const std::string& name, void (*sleep)() )
@@ -157,6 +180,10 @@ BOOST_AUTO_TEST_CASE( ThreadSleepTime )
 {
 	BenchmarkSleepFnc("sleep_posix",  &sleep_posix);
 #ifdef BOOST_THREAD_USES_CHRONO
-	BenchmarkSleepFnc("sleep_chrono", &sleep_chrono);
+	BenchmarkSleepFnc("sleep_boost", &sleep_boost);
+#endif
+	BenchmarkSleepFnc("yield_boost", &yield_boost);
+#if __cplusplus > 199711L
+	BenchmarkSleepFnc("yield_chrono", &yield_chrono);
 #endif
 }
