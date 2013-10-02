204a2a314d1addc5d9a7f508148c9ff043933d62
jK <-jk->
2013-10-02 13:34:59 +0200

    don't save WindowPosX/WindowPosY when window is maximized/minimized

--- rts/Rendering/GlobalRendering.h
+++ rts/Rendering/GlobalRendering.h
@@ -53,6 +53,12 @@ public:
 	float FPS;
 
 	/// the window state (0=normal,1=maximized,2=minimized)
+	/// note don't change values without updating MyX11SetWindowState()!!!
+	enum {
+		WINSTATE_DEFAULT   = 0,
+		WINSTATE_MAXIMIZED = 1,
+		WINSTATE_MINIMIZED = 2
+	};
 	int winState;
 
 	/// the screen size in pixels
