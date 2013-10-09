0b56b0f241f64328b23d2aea4b856ceffd2d3b32
jk3064 <jk3064@arcor.de>
2013-10-09 12:14:46 +0200

    Update LuaMathExtra.cpp
    fix compile

--- rts/Lua/LuaMathExtra.cpp
+++ rts/Lua/LuaMathExtra.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#include "streflop_cond.h"
+#include "lib/streflop/streflop_cond.h"
 #include "LuaMathExtra.h"
 #include "LuaInclude.h"
 #include "LuaUtils.h"
