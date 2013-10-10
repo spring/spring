8038083ab12533ee9d02672d5127d51858cb4ea8
DeadnightWarrior <ufpdnw666@yahoo.com>
2013-10-10 09:43:26 +0200

    Resolve conflict with dev branch

--- rts/Lua/LuaMathExtra.cpp
+++ rts/Lua/LuaMathExtra.cpp
@@ -6,7 +6,7 @@
 #include "LuaInclude.h"
 #include "LuaUtils.h"
 
-static const lua_Number pow10[] = {1.0f, 10.0f, 100.0f, 1000.0f, 10000.0f, 100000.0f, 1000000.0f, 10000000.0f};
+static const lua_Number POWERS_OF_TEN[] = {1.0f, 10.0f, 100.0f, 1000.0f, 10000.0f, 100000.0f, 1000000.0f, 10000000.0f};
 
 /******************************************************************************/
 /******************************************************************************/
@@ -27,77 +27,78 @@ bool LuaMathExtra::PushEntries(lua_State* L)
 /******************************************************************************/
 /******************************************************************************/
 
-int LuaMathExtra::hypot (lua_State *L) {
-  lua_pushnumber(L, math::hypot(luaL_checknumber_noassert(L, 1), luaL_checknumber_noassert(L, 2)));
-  return 1;
+int LuaMathExtra::hypot(lua_State* L) {
+	lua_pushnumber(L, math::hypot(luaL_checknumber_noassert(L, 1), luaL_checknumber_noassert(L, 2)));
+	return 1;
 }
 
-int LuaMathExtra::diag (lua_State *L) {
-  int n = lua_gettop(L);  /* number of arguments */
-  lua_Number res = 0.0f;
-  int i;
-  for (i=1; i<=n; i++) {
-    lua_Number d = luaL_checknumber_noassert(L, i);
-    res += d*d;
-  }
-  lua_pushnumber(L, math::sqrt(res));
-  return 1;
+int LuaMathExtra::diag(lua_State* L) {
+	lua_Number res = 0.0f;
+
+	for (int i = lua_gettop(L); i >= 1; i--) {
+		res += Square(luaL_checknumber_noassert(L, i));
+	}
+
+	lua_pushnumber(L, math::sqrt(res));
+	return 1;
 }
 
-int LuaMathExtra::clamp (lua_State *L) {
-  lua_Number clamp = luaL_checknumber_noassert(L, 1);
-  lua_Number lbound = luaL_checknumber_noassert(L, 2);
-  lua_Number ubound = luaL_checknumber_noassert(L, 3);
-  if (clamp < lbound)
-    clamp = lbound;
-  else if (clamp > ubound)
-    clamp = ubound;
-  lua_pushnumber(L, clamp);
-  return 1;
+int LuaMathExtra::clamp(lua_State* L) {
+	const lua_Number lbound = luaL_checknumber_noassert(L, 2);
+	const lua_Number ubound = luaL_checknumber_noassert(L, 3);
+
+	lua_pushnumber(L, Clamp(luaL_checknumber_noassert(L, 1), lbound, ubound));
+	return 1;
 }
 
-int LuaMathExtra::sgn (lua_State *L) {
-  lua_Number x = luaL_checknumber_noassert(L, 1);
-  if (x != 0.0f)
-    lua_pushnumber(L, Sign(x));
-  else
-    lua_pushnumber(L, 0.0f);
-  return 1;
+int LuaMathExtra::sgn(lua_State* L) {
+	const lua_Number x = luaL_checknumber_noassert(L, 1);
+
+	if (x != 0.0f) {
+		// engine's version returns -1 for sgn(0)
+		lua_pushnumber(L, Sign(x));
+	} else {
+		lua_pushnumber(L, 0.0f);
+	}
+
+	return 1;
 }
 
-int LuaMathExtra::mix (lua_State *L) {
-  lua_Number x = luaL_checknumber_noassert(L, 1);
-  lua_Number y = luaL_checknumber_noassert(L, 2);
-  lua_Number a = luaL_checknumber_noassert(L, 3);
-  lua_pushnumber(L, x+(y-x)*a);
-  return 1;
+int LuaMathExtra::mix(lua_State* L) {
+	const lua_Number x = luaL_checknumber_noassert(L, 1);
+	const lua_Number y = luaL_checknumber_noassert(L, 2);
+	const lua_Number a = luaL_checknumber_noassert(L, 3);
+
+	lua_pushnumber(L, Blend(x, y, a));
+	return 1;
 }
 
-int LuaMathExtra::round (lua_State *L) {
-  lua_Number x = luaL_checknumber_noassert(L, 1);
-  int ops = lua_gettop(L);  /* number of arguments */
-  if (ops>1) {
-    int n = luaL_checkint(L, 2);
-    if (n>0){
-      if (n<=7){ // LUA in Spring uses 32-bit floats, therefore max accuracy is 7 decimal digits
-        lua_Number flx = math::floor(x);
-        lua_Number t = x - flx;
-        lua_Number rounding = pow10[n];
-        lua_pushnumber(L, flx + math::floor(t*rounding + 0.5f)/rounding);
-      }
-      else
-        lua_pushnumber(L, x);
-      return 1;
-    }
-  }
-  lua_pushnumber(L, math::floor(x+0.5f));
-  return 1;
+int LuaMathExtra::round(lua_State* L) {
+	const lua_Number x = luaL_checknumber_noassert(L, 1);
+
+	if (lua_gettop(L) > 1) {
+		// round number to <n> decimals
+		// Spring's Lua interpreter uses 32-bit floats,
+		// therefore max. accuracy is ~7 decimal digits
+		const int i = std::min(7, int(sizeof(POWERS_OF_TEN) / sizeof(float)) - 1);
+		const int n = Clamp(luaL_checkint(L, 2), 0, i);
+
+		const lua_Number xinteg = math::floor(x);
+		const lua_Number xfract = x - xinteg;
+
+		lua_pushnumber(L, xinteg + math::floor((xfract * POWERS_OF_TEN[n]) + 0.5f) / POWERS_OF_TEN[n]);
+	} else {
+		lua_pushnumber(L, math::floor(x + 0.5f));
+	}
+
+	return 1;
 }
 
-int LuaMathExtra::erf (lua_State *L) {
-  lua_pushnumber(L, math::erf(luaL_checknumber_noassert(L, 1)));
-  return 1;
+int LuaMathExtra::erf(lua_State* L) {
+	lua_pushnumber(L, math::erf(luaL_checknumber_noassert(L, 1)));
+	return 1;
 }
 
 /******************************************************************************/
 /******************************************************************************/
+
--- rts/Lua/LuaMathExtra.h
+++ rts/Lua/LuaMathExtra.h
@@ -20,3 +20,4 @@ class LuaMathExtra {
 };
 
 #endif /* LUA_MATH_EXTRA_H */
+
