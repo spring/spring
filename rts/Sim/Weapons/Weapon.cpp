acf76eeb570b2d0d906228a9d7a17cfe02fc5c25
rt <rt@rt-0.(none)>
2013-11-13 13:58:38 +0100

    revert previous

--- rts/Sim/Weapons/Weapon.cpp
+++ rts/Sim/Weapons/Weapon.cpp
@@ -919,12 +919,8 @@ void CWeapon::DependentDied(CObject* o)
 bool CWeapon::TargetUnitOrPositionUnderWater(const float3& targetPos, const CUnit* targetUnit, float offset)
 {
 	// test if a target position or unit is strictly underwater
-	//
-	// previously this called targetUnit->IsUnderWater whose
-	// return value depends on the unit's base-position, but
-	// targetting cares more about its aim-position
 	if (targetUnit != NULL) {
-		return (targetUnit->aimPos.y < 0.0f);
+		return (targetUnit->IsUnderWater());
 	} else {
 		// consistent with CSolidObject::IsUnderWater (LT)
 		return ((targetPos.y + offset) < 0.0f);
@@ -934,12 +930,8 @@ bool CWeapon::TargetUnitOrPositionUnderWater(const float3& targetPos, const CUni
 bool CWeapon::TargetUnitOrPositionInWater(const float3& targetPos, const CUnit* targetUnit, float offset)
 {
 	// test if a target position or unit is in water (including underwater)
-	//
-	// previously this called targetUnit->IsInWater whose
-	// return value depends on the unit's base-position,
-	// but targetting cares more about its aim-position
 	if (targetUnit != NULL) {
-		return (targetUnit->aimPos.y <= 0.0f);
+		return (targetUnit->IsInWater());
 	} else {
 		// consistent with CSolidObject::IsInWater (LE)
 		return ((targetPos.y + offset) <= 0.0f);
