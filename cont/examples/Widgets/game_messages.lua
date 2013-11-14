b027668d8c2362584cf87f23105bc70cead7cd1a
abma <spring@abma.de>
2013-11-14 01:33:21 +0100

    update game_messages.lua, thanks zwzsg!
    http://springrts.com/phpbb/viewtopic.php?p=550119#p550119

--- cont/examples/Widgets/game_messages.lua
+++ cont/examples/Widgets/game_messages.lua
@@ -1,12 +1,12 @@
 function widget:GetInfo()
   return {
     name      = "TeamDiedMessages",
-    desc      = "Prints a message when a Team died",
-    author    = "abma",
+    desc      = "Prints a message when a team die",
+    author    = "abma & zwzsg",
     date      = "Sep, 2013",
     license   = "GNU GPL, v2 or later",
     layer     = 5,
-    enabled   = true  --  loaded by default?
+    enabled   = true
   }
 end
 
@@ -30,21 +30,20 @@ function widget:Initialize()
 		Spring.Log(sec, LOG.ERROR, 'Missing "messages" section in ' .. msgsfile )
 	end
 
-
 	for label, msgs in pairs(tdfMsgs.messages) do
 		if ((type(label) == 'string') and (type(msgs)  == 'table')) then
-				local msgArray = {}
-				for _, msg in pairs(msgs) do
-					if (type(msg) == 'string') then
-						table.insert(msgArray, msg)
-					end
+			local msgArray = {}
+			for _, msg in pairs(msgs) do
+				if (type(msg) == 'string') then
+					table.insert(msgArray, msg)
 				end
 			end
-		luaMsgs[label:lower()] = msgArray -- lower case keys
+			luaMsgs[label:lower()] = msgArray -- lower case keys
+		end
 	end
 end
 
-local function tr(msg)
+local function Translate(msg)
 	msg = msg:lower()
 	if luaMsgs and type(luaMsgs[msg]) == "table" then
 		local msgs = luaMsgs[msg]
@@ -52,17 +51,27 @@ local function tr(msg)
 	end
 	Spring.Log(sec, LOG.DEBUG, string.format("Missing translation for %s", msg))
 	return msg
-
 end
 
 function widget:TeamDied(teamID)
-	local _, leadPlayerID = Spring.GetTeamInfo(teamID)
-	if leadPlayerID then
-		local name = Spring.GetPlayerInfo(leadPlayerID)
-		msg = string.format(tr("Team %i (lead by %s) is no more"), teamID, name)
-	else
-		msg = string.format(tr("Team %i is no more"), teamID)
+	local playerNames
+	local _, leaderPlayerId, isDead, isAiTeam = Spring.GetTeamInfo(teamID)
+	if isAiTeam then
+		local skirmishAIID, aiName, hostingPlayerID, shortName, version, options = Spring.GetAIInfo(teamID)
+		if aiName~="UNKNOWN" and aiName:lower():sub(1,3)~="bot" and aiName:len()>1 then
+			playerNames=aiName
+		elseif shortName~="UNKNOWN" then
+			playerNames=shortName
+		elseif hostingPlayerID~=Spring.GetMyPlayerID() then
+			local ownerName=Spring.GetPlayerInfo(hostingPlayerID)
+			if ownerName then
+				playerNames=ownerName.."'s bot"
+			end
+		end
 	end
+	for _,playerId in ipairs(Spring.GetPlayerList(teamID,true)) do
+		playerNames=(playerNames and playerNames..", " or "")..Spring.GetPlayerInfo(playerId)
+	end
+	msg = string.format(Translate("Team%i(%s) is no more"), teamID, playerNames or "")
 	Spring.Echo(msg)
 end
-
