f53fb8305d9d91b468d45363e722e0a09cd4f56b
N0U <petushkov.egor@gmail.com>
2013-11-18 22:55:43 -0800

    Insted of loading signle chars, CFontTexture loads an entire block. Some blocks are predefined, other blocks assumed to contain 128 chars

--- rts/Rendering/Fonts/CFontTexture.h
+++ rts/Rendering/Fonts/CFontTexture.h
@@ -15,6 +15,70 @@ class texture_size_exception : public std::exception
 {
 };
 
+struct IGlyphRect { //FIXME use SRect
+	IGlyphRect():
+		x(0),y(0),
+		w(0),h(0) {
+	};
+
+	IGlyphRect(int _x,int _y,int _w,int _h):
+		x(_x),
+		y(_y),
+		w(_w),
+		h(_h) {
+	};
+
+	int x0() const {
+		return x;
+	};
+	int x1() const {
+		return x+w;
+	};
+	int y0() const {
+		return y;
+	};
+	int y1() const {
+		return y+h;
+	};
+
+	int x,y;
+	int w,h;
+};
+
+struct GlyphInfo {
+	GlyphInfo():
+		size(),
+		texCord(0,0,1,1),
+		advance(1),
+		height(1),
+		descender(0),
+		index(0) {
+	};
+
+	IGlyphRect size;
+	IGlyphRect texCord;
+	int advance, height, descender;
+	char32_t index;
+};
+
+class LanguageBlock
+{
+public:
+	LanguageBlock(char32_t start,char32_t end):
+		start(start),
+		end(end)
+	{
+		glyphs=new GlyphInfo[end-start];// [start,end)
+	}
+
+	~LanguageBlock(){ delete[] glyphs; };
+
+	inline bool OnThisPlane(char32_t ch) const {return start<=ch && ch<end;}
+	GlyphInfo& Get(char32_t ch){return glyphs[ch-start];}
+private:
+	char32_t start,end;
+	GlyphInfo* glyphs;
+};
 
 /**
 This class just store glyphs and load new glyphs if requred
@@ -35,61 +99,15 @@ public:
 	int GetFontDescender() const;
 
 	int GetTexture() const;
-
-	struct IGlyphRect { //FIXME use SRect
-		IGlyphRect():
-			x(0),y(0),
-			w(0),h(0) {
-		};
-
-		IGlyphRect(int _x,int _y,int _w,int _h):
-			x(_x),
-			y(_y),
-			w(_w),
-			h(_h) {
-		};
-
-		int x0() const {
-			return x;
-		};
-		int x1() const {
-			return x+w;
-		};
-		int y0() const {
-			return y;
-		};
-		int y1() const {
-			return y+h;
-		};
-
-		int x,y;
-		int w,h;
-	};
-
-	struct GlyphInfo {
-		GlyphInfo():
-			size(),
-			texCord(0,0,1,1),
-			advance(1),
-			height(1),
-			descender(0),
-			index(0) {
-		};
-
-		IGlyphRect size;
-		IGlyphRect texCord;
-		int advance, height, descender;
-		char32_t index;
-	};
 	// Get or load a glyph
-	const CFontTexture::GlyphInfo& GetGlyph(char32_t ch);
+	const GlyphInfo& GetGlyph(char32_t ch);
 
 protected:
 	const FT_Face& GetFace() const;
 
 protected:
 	int GetKerning(char32_t lchar, char32_t rchar);
-	int GetKerning(const CFontTexture::GlyphInfo& lgl,const CFontTexture::GlyphInfo& rgl);
+	int GetKerning(const GlyphInfo& lgl,const GlyphInfo& rgl);
 
 private:
 	int outlineSize;
@@ -107,7 +125,7 @@ private:
 	void ResizeTexture(int w,int h);
 	//! Copy glyph pixels on the texture
 	void Update(const unsigned char* pixels,int x,int y,int w,int h);
-
+	//! Fill given rect by null bytes
 	void Clear(int x,int y,int w,int h);
 
 private:
@@ -116,8 +134,10 @@ private:
 	unsigned char* faceDataBuffer;
 
 private:
-	std::map<char32_t,GlyphInfo> glyphs;    //!UTF32 -> GlyphInfo
-	CFontTexture::GlyphInfo& LoadGlyph(char32_t ch);
+	std::map<char32_t,LanguageBlock*> blocks;    //!UTF32 block start -> LanguageBlock
+	//! Load all chars in block's range
+	LanguageBlock* LoadBlock(char32_t start,char32_t end);
+	void LoadGlyph(LanguageBlock* block,char32_t ch);
 
 private:
 	struct Row {
@@ -138,7 +158,7 @@ private:
 	Row* AddRow(int glyphWidth, int glyphHeight);
 	//! Try to find a place where can be placed a glyph with given size
 	//! If it is imposible, resize the texture and try it again
-	CFontTexture::IGlyphRect AllocateGlyphRect(int glyphWidth, int glyphHeight);
+	IGlyphRect AllocateGlyphRect(int glyphWidth, int glyphHeight);
 };
 
 #endif // CFONTTEXTURE_H
---
+++ rts/Rendering/Fonts/LanguageBlocksDefs.h
@@ -0,0 +1,49 @@
+#ifndef LANGUAGEBLOCKSDEFS_H_INCLUDED
+#define LANGUAGEBLOCKSDEFS_H_INCLUDED
+
+//! Predefined blocks
+//! It contains only the most widly used blocks (Latin and Cyrilic) and blocks betwen them
+const char32_t blocks[]={
+				0x0000,	//Controls and Basic Latin
+				0x0080,	//Controls and Latin-1 Supplement
+				0x0100,	//Latin Extended-A
+				0x0180,	//Latin Extended-B
+				0x0250,	//IPA Extensions
+				0x02B0, //Spacing Modifier Letters
+				0x0300,	//Combining Diacritical Marks
+				0x0370,	//Greek and Coptic
+				0x0400,	//Cyrillic
+				0x0500,	//Cyrillic Supplement
+				0x0530, //Cyrillic Supplement end
+			};
+
+const unsigned int defBlocksAmount=(sizeof(blocks)/sizeof(char32_t))-1;
+//! Any other blocks assumed to be 128 size
+const unsigned int undefBlocksStart=blocks[defBlocksAmount];
+const unsigned int undefBlocksSize=128;
+
+char32_t GetUndefLanguageBlock(char32_t ch,char32_t& end)
+{
+	char32_t dif=ch-undefBlocksStart;
+	//(dif/128)*128
+	char32_t start=undefBlocksStart+(dif>>7)*undefBlocksSize;
+	end=start+undefBlocksSize;
+	return start;
+}
+
+char32_t GetLanguageBlock(char32_t ch,char32_t& end)
+{
+	if(ch>=undefBlocksStart)
+		return GetUndefLanguageBlock(ch,end);
+
+	for(int i=0;i<defBlocksAmount;i++)
+	{
+		if(blocks[i]<=ch && ch<blocks[i+1])
+		{
+			end=blocks[i+1];
+			return blocks[i];
+		}
+	}
+}
+
+#endif // LANGUAGEBLOCKSDEFS_H_INCLUDED
