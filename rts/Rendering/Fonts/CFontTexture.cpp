c4fa5c9eff66eb6acd6caf3fc3fff8d84a83fba8
N0U <petushkov.egor@gmail.com>
2013-11-22 21:55:12 -0800

    Merge jK & Clecn up & utf-8 support in chat

--- rts/Rendering/Fonts/CFontTexture.cpp
+++ rts/Rendering/Fonts/CFontTexture.cpp
@@ -1,3 +1,5 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
 #include "CFontTexture.h"
 #include "Rendering/GlobalRendering.h"
 
@@ -48,6 +50,14 @@ static const char* GetFTError(FT_Error e) {
 #endif // HEADLESS
 
 
+static inline uint32_t GetKerningHash(char32_t lchar, char32_t rchar)
+{
+	if (lchar < 128 && rchar < 128) {
+		return (lchar << 7) | rchar; // 14bit used
+	}
+	return (lchar << 16) | rchar; // 32bit used
+}
+
 #ifndef   HEADLESS
 class FtLibraryHandler
 {
@@ -96,11 +106,15 @@ CFontTexture::CFontTexture(const std::string& fontfile, int size, int _outlinesi
 {
 	if (size <= 0)
 		size = 14;
+
+	static const int FT_INTERNAL_DPI = 64;
+	normScale = 1.0f / (size * FT_INTERNAL_DPI);
+
 #ifndef   HEADLESS
 	std::string fontPath(fontfile);
 	CFileHandler* f = new CFileHandler(fontPath);
 	if (!f->FileExists()) {
-		//! check in 'fonts/', too
+		// check in 'fonts/', too
 		if (fontPath.substr(0,6) != "fonts/") {
 			delete f;
 			fontPath = "fonts/" + fontPath;
@@ -128,7 +142,7 @@ CFontTexture::CFontTexture(const std::string& fontfile, int size, int _outlinesi
 		throw content_error(msg);
 	}
 
-	//! set render size
+	// set render size
 	error = FT_Set_Pixel_Sizes(face, 0, size);
 	if (error) {
 		FT_Done_Face(face);
@@ -138,33 +152,44 @@ CFontTexture::CFontTexture(const std::string& fontfile, int size, int _outlinesi
 		throw content_error(msg);
 	}
 
-	//! select unicode charmap
+	// select unicode charmap
 	error = FT_Select_Charmap(face, FT_ENCODING_UNICODE);
-	if(error) {
-		//Support unicode or GTFO
+	if (error) {
 		FT_Done_Face(face);
 		delete[] faceDataBuffer;
 		std::string msg = fontfile + ": FT_Select_Charmap failed: ";
 		msg += GetFTError(error);
 		throw content_error(msg);
 	}
-	fontDescender = FT_MulFix(face->descender, face->size->metrics.y_scale);
-	lineHeight = FT_MulFix(face->height, face->size->metrics.y_scale);
 
-	if(lineHeight<=0)
+	fontDescender = normScale * FT_MulFix(face->descender, face->size->metrics.y_scale);
+	//lineHeight = FT_MulFix(face->height, face->size->metrics.y_scale); // bad results
+	lineHeight = face->height / face->units_per_EM;
+	if (lineHeight <= 0)
 		lineHeight = 1.25 * (face->bbox.yMax - face->bbox.yMin);
 
-	//! Create initial small texture
-	CreateTexture(32,32); //FIXME
+	// Create initial small texture
+	ResizeTexture(32,32);
+
+	// precache ASCII kernings
+	memset(kerningPrecached, 0, 128*128*sizeof(float));
+	for (char32_t i=32; i<128; ++i) {
+		const auto& lgl = GetGlyph(i);
+		const float advance = lgl.advance;
+		for (char32_t j=32; j<128; ++j) {
+			const auto& rgl = GetGlyph(j);
+			const auto hash = GetKerningHash(i, j);
+			FT_Vector kerning;
+			FT_Get_Kerning(face, lgl.index, rgl.index, FT_KERNING_DEFAULT, &kerning);
+			kerningPrecached[hash] = advance + normScale * kerning.x;
+		}
+	}
 #endif
 }
 
 CFontTexture::~CFontTexture()
 {
 #ifndef   HEADLESS
-	for(auto it=blocks.begin();it!=blocks.end();++it)
-		delete it->second;
-
 	FT_Done_Face(face);
 	delete[] faceDataBuffer;
 	glDeleteTextures(1, (const GLuint*)&texture);
@@ -191,12 +216,12 @@ float CFontTexture::GetOutlineWeight() const
 	return outlineWeight;
 }
 
-int CFontTexture::GetLineHeightA() const
+float CFontTexture::GetLineHeight() const
 {
 	return lineHeight;
 }
 
-int CFontTexture::GetFontDescender() const
+float CFontTexture::GetDescender() const
 {
 	return fontDescender;
 }
@@ -211,172 +236,137 @@ int CFontTexture::GetTexture() const
 	return texture;
 }
 
+
 const GlyphInfo& CFontTexture::GetGlyph(char32_t ch)
 {
 #ifndef HEADLESS
-	char32_t end;
-	//Get block start pos
-	char32_t start=GetLanguageBlock(ch,end);
-	auto it = blocks.find(start);
-	if (it ==  blocks.end())
-	{
-		//Load an entire block
-		return LoadBlock(start,end)->Get(ch); //FIXME can throw texture_size_exception
-	}
-	return it->second->Get(ch);
+	const auto it = glyphs.find(ch);
+	if (it != glyphs.end())
+		return it->second;
+
+	// Get block start pos
+	char32_t start, end;
+	start = GetLanguageBlock(ch, end);
+
+	// Load an entire block
+	LoadBlock(start, end); ////FIXME can throw texture_size_exception
+	return GetGlyph(ch);
 #else
 	static GlyphInfo g = GlyphInfo();
 	return g;
 #endif
 }
 
-int CFontTexture::GetKerning(char32_t lchar, char32_t rchar)
+
+float CFontTexture::GetKerning(const GlyphInfo& lgl, const GlyphInfo& rgl)
 {
 #ifndef HEADLESS
-	//FIXME cache!
-	const GlyphInfo& left = GetGlyph(lchar);
-	// if(FT_HAS_KERNING(face))
-	{
-		const GlyphInfo& right = GetGlyph(rchar);
-		FT_Vector kerning;
-		FT_Get_Kerning(face, left.index, right.index, FT_KERNING_DEFAULT, &kerning);
-		return left.advance + kerning.x;
+	// first check caches
+	uint32_t hash = GetKerningHash(lgl.utf16, rgl.utf16);
+	if (hash < 128*128) {
+		return kerningPrecached[hash];
 	}
-//   else
-//       return left.advance; // This font does not contain kerning
+	const auto it = kerningDynamic.find(hash);
+	if (it != kerningDynamic.end()) {
+		return it->second;
+	}
+
+	// load & cache
+	FT_Vector kerning;
+	FT_Get_Kerning(face, lgl.index, rgl.index, FT_KERNING_DEFAULT, &kerning);
+	kerningDynamic[hash] = lgl.advance + normScale * kerning.x;
+	return kerningDynamic[hash];
 #else
 	return 0;
 #endif
 }
 
-int CFontTexture::GetKerning(const GlyphInfo& lgl, const GlyphInfo& rgl)
+
+void CFontTexture::LoadBlock(char32_t start, char32_t end)
 {
-#ifndef HEADLESS
-	//FIXME cache!
-	// if(FT_HAS_KERNING(face))
-	{
-		FT_Vector kerning;
-		FT_Get_Kerning(face, lgl.index, rgl.index, FT_KERNING_DEFAULT, &kerning);
-		return lgl.advance + kerning.x;
-	}
-	//  else
-	//      return lgl.advance;
-#else
-	return 0;
-#endif
+	for(char32_t i=start; i<end; ++i)
+		LoadGlyph(i);
 }
 
-LanguageBlock* CFontTexture::LoadBlock(char32_t start,char32_t end)
+void CFontTexture::LoadGlyph(char32_t ch)
 {
 #ifndef   HEADLESS
-	LanguageBlock* block=blocks.insert(std::pair<char32_t,LanguageBlock*>(start,new LanguageBlock(start,end))).first->second;
-	for(char32_t i=start;i<end;i++)
-		LoadGlyph(block,i);
-
-	return block;
-#endif
-}
-
-void CFontTexture::LoadGlyph(LanguageBlock* block,char32_t ch)
-{
-#ifndef   HEADLESS
-	// This code mainly base on SFML code
-	// I actually do not have a clear idea how it works
-	// I just pray
-
-	FT_Error error;
-	auto& glyph = block->Get(ch);
-	glyph=GlyphInfo();// Make empty glyph in case of error
+	// map unicode to font internal index
 	FT_UInt index = FT_Get_Char_Index(face, ch);
-	glyph.index = index;
 
-	error = FT_Load_Glyph(face, index, FT_LOAD_RENDER|FT_LOAD_FORCE_AUTOHINT);
+	// check for duplicated glyphs
+	for (auto& it: glyphs) {
+		if (it.second.index == index) {
+			auto& glyph = glyphs[ch];
+			glyph = it.second;
+			glyph.utf16 = ch;
+			return;
+		}
+	}
+
+	auto& glyph = glyphs[ch];
+	glyph.index = index;
+	glyph.utf16 = ch;
+
+	// load glyph
+	FT_Error error = FT_Load_Glyph(face, index, FT_LOAD_RENDER|FT_LOAD_FORCE_AUTOHINT);
 	if (error) {
 		LOG_L(L_ERROR, "Couldn't load glyph %d", ch);
 	}
 
 	FT_GlyphSlot slot = face->glyph;
 
-	const float xbearing = slot->metrics.horiBearingX;
-	const float ybearing = slot->metrics.horiBearingY;
+	const float xbearing = slot->metrics.horiBearingX * normScale;
+	const float ybearing = slot->metrics.horiBearingY * normScale;
 
-	glyph.advance   = slot->advance.x;
-	glyph.height    = slot->metrics.height;
+	glyph.size.x = xbearing;
+	glyph.size.y = ybearing - fontDescender;
+	glyph.size.w =  slot->metrics.width * normScale;
+	glyph.size.h = -slot->metrics.height * normScale;
+
+	glyph.advance   = slot->advance.x * normScale;
+	glyph.height    = slot->metrics.height * normScale;
 	glyph.descender = ybearing - glyph.height;
 
-	//! Get glyph bound box
-	glyph.size = IGlyphRect(xbearing,                    // x0
-				ybearing - fontDescender,    // y0
-				 slot->metrics.width,        // w
-				-slot->metrics.height);      // h
+	const int width  = slot->bitmap.width;
+	const int height = slot->bitmap.rows;
 
-	int width  = slot->bitmap.width;
-	int height = slot->bitmap.rows;
-	static const int padding = 1;
-	if (width>0 && height>0) {
-		//FIXME
-		/*glyph.size.x-=padding;
-		glyph.size.y-=padding;
-		glyph.size.w+=2*padding;
-		glyph.size.h+=2*padding;
-		*/
-		unsigned char* pixels_buffer = new unsigned char[width * height];
-		glyph.texCord = AllocateGlyphRect(width+2*padding,height+2*padding);
-
-		const unsigned char* source_pixels = slot->bitmap.buffer;
-
-		/*  if(slot->bitmap.pixel_mode == FT_PIXEL_MODE_MONO)
-		  {
-		      // Pixels are 1 bit monochrome values
-		      for (int y = 0; y < height; ++y)
-		      {
-		          for (int x = 0; x < width; ++x)
-		          {
-		              // The color channels remain white, just fill the alpha channel
-		              int index = x + y * width;
-		              pixels_buffer[index] = ((source_pixels[x / 8]) & (1 << (7 - (x % 8)))) ? 255 : 0;
-		          }
-		          source_pixels += slot->bitmap.pitch;
-		      }
-		  }
-		  else
-		  {*/
-		// Pixels are 8 bits gray levels
-		for(int y = 0; y < height; y++) {
-			//!Flip y cords
-			//const unsigned char* src = source_pixels + (height - 1 - y) * slot->bitmap.pitch;
-			const unsigned char* src = source_pixels + y * slot->bitmap.pitch;
-			unsigned char* dst = pixels_buffer + y * width;
-			memcpy(dst,src,width);
-		}
-		//  }
-		Clear(glyph.texCord.x,glyph.texCord.y,glyph.texCord.w,glyph.texCord.h);
-		glyph.texCord.x += padding;
-		glyph.texCord.y += padding;
-		glyph.texCord.w -= 2 * padding;
-		glyph.texCord.h -= 2 * padding;
-
-		Update(pixels_buffer, glyph.texCord.x, glyph.texCord.y,
-		       width, height);
-
-		delete[] pixels_buffer;
-	} else {
-		LOG_L(L_ERROR, "invalid glyph size");
+	if (width<=0 || height<=0) {
+		return;
 	}
 
+	if (slot->bitmap.pixel_mode != FT_PIXEL_MODE_GRAY) {
+		LOG_L(L_ERROR, "invalid pixeldata mode");
+		return;
+	}
+
+	      unsigned char* dst_pixels = new unsigned char[width * height]; // alpha-only 8bit texture
+	const unsigned char* src_pixels = slot->bitmap.buffer;
+
+	// Pixels are 8 bits gray levels
+	for(int y = 0; y < height; y++) {
+		const unsigned char* src = src_pixels + y * slot->bitmap.pitch;
+		      unsigned char* dst = dst_pixels + y * width;
+		memcpy(dst, src, width);
+	}
+
+	glyph.texCord = AllocateGlyphRect(width, height);
+	Update(dst_pixels, glyph.texCord.x, glyph.texCord.y, width, height);
+
+	delete[] dst_pixels;
 #endif
 }
 
 CFontTexture::Row* CFontTexture::FindRow(int glyphWidth, int glyphHeight)
 {
 	for(auto& row: imageRows) {
-		float ratio=(float)row.height/(float)glyphHeight;
+		float ratio = (float)row.height/(float)glyphHeight;
 		// Ignore too small or too big raws
 		if (ratio < 1.0f || ratio > 1.3f)
 			continue;
 
-		// Check if there is enought space in this row
-		if (texWidth - row.wight < glyphWidth)
+		// Check if there is enough space in this row
+		if (texWidth - row.width < glyphWidth)
 			continue;
 
 		return &row;
@@ -387,11 +377,11 @@ CFontTexture::Row* CFontTexture::FindRow(int glyphWidth, int glyphHeight)
 CFontTexture::Row* CFontTexture::AddRow(int glyphWidth, int glyphHeight)
 {
 	int rowHeight = glyphHeight + (2*glyphHeight)/10;
-	while(nextRowPos+rowHeight >= texHeight) {
+	while (nextRowPos+rowHeight >= texHeight) {
 		// Resize texture
-		ResizeTexture(texWidth*2,texHeight*2); //FIXME
+		ResizeTexture(texWidth*2, texHeight*2); //FIXME
 	}
-	Row newrow(nextRowPos,rowHeight);
+	Row newrow(nextRowPos, rowHeight);
 	nextRowPos += rowHeight;
 	imageRows.push_back(newrow);
 	return &imageRows.back();
@@ -400,62 +390,65 @@ CFontTexture::Row* CFontTexture::AddRow(int glyphWidth, int glyphHeight)
 IGlyphRect CFontTexture::AllocateGlyphRect(int glyphWidth,int glyphHeight)
 {
 #ifndef   HEADLESS
-	Row* row = FindRow(glyphWidth,glyphHeight);
-	if(!row)
-		row = AddRow(glyphWidth,glyphHeight);
+	//FIXME add padding
+	Row* row = FindRow(glyphWidth, glyphHeight);
+	if (!row)
+		row = AddRow(glyphWidth, glyphHeight);
 
-	IGlyphRect rect = IGlyphRect(row->wight,row->position,glyphWidth,glyphHeight);
-	row->wight += glyphWidth;
+	IGlyphRect rect = IGlyphRect(row->width, row->position, glyphWidth, glyphHeight);
+	row->width += glyphWidth;
 	return rect;
 #else
 	return IGlyphRect();
 #endif
 }
 
-void CFontTexture::CreateTexture(int w,int h)
+void CFontTexture::ResizeTexture(int w, int h)
 {
 #ifndef   HEADLESS
-	if(w>2048 || h>2048)
+	if (w>2048 || h>2048)
 		throw texture_size_exception();
 
-	if(!texture)
+	glPushAttrib(GL_PIXEL_MODE_BIT);
+
+	unsigned char* pixels = NULL;
+	if (!texture) {
 		glGenTextures(1, &texture);
+	} else {
+		pixels = new unsigned char[texWidth * texHeight];
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glGetTexImage(GL_TEXTURE_2D, 0, GL_ALPHA, GL_UNSIGNED_BYTE, pixels);
+	}
 
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	if (GLEW_ARB_texture_border_clamp) {
-			static const GLfloat borderColor[4] = {1.0f,1.0f,1.0f,0.0f};
-			glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		static const GLfloat borderColor[4] = {1.0f,1.0f,1.0f,0.0f};
+		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 	} else {
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 	}
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, w, h, 0, GL_ALPHA, GL_UNSIGNED_BYTE, 0);
 
-	texWidth=w;
-	texHeight=h;
-#endif
-}
+	unsigned char empty[w*h];
+	memset(empty, 0, w*h);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, w, h, 0, GL_ALPHA, GL_UNSIGNED_BYTE, empty);
 
-void CFontTexture::ResizeTexture(int w,int h)
-{
-#ifndef   HEADLESS
-	glPushAttrib(GL_PIXEL_MODE_BIT);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	if (pixels != NULL) {
+		// copy back old data
+		glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, texWidth, texHeight, GL_ALPHA, GL_UNSIGNED_BYTE, pixels);
+	}
 
-	unsigned char* pixels = new unsigned char[texWidth * texHeight];
-	glBindTexture(GL_TEXTURE_2D, texture);
-	glGetTexImage(GL_TEXTURE_2D, 0, GL_ALPHA, GL_UNSIGNED_BYTE, pixels);
-
-	int oldsw=texWidth;
-	int oldsh=texHeight;
-	CreateTexture(w,h);
-
-	Update(pixels,0,0,oldsw,oldsh);
+	texWidth  = w;
+	texHeight = h;
 	delete[] pixels;
+	glPopAttrib();
 #endif
 }
 
@@ -463,18 +456,9 @@ void CFontTexture::Update(const unsigned char* pixels,int x,int y,int w,int h)
 {
 #ifndef HEADLESS
 	//FIXME readd shadow blur
-	glEnable(GL_TEXTURE_2D);
 	glPushAttrib(GL_PIXEL_MODE_BIT);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexSubImage2D(GL_TEXTURE_2D, 0, x, y, (unsigned int)w, (unsigned int)h, GL_ALPHA, GL_UNSIGNED_BYTE, pixels);
 	glPopAttrib();
 #endif
-}
-
-void CFontTexture::Clear(int x,int y,int w,int h)
-{
-	unsigned char* wipe_buf=new unsigned char[w*h];
-	memset(wipe_buf,0,w*h);
-	Update(wipe_buf,x,y,w,h);
-	delete[] wipe_buf;
 }
