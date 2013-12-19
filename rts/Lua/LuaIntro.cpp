spring/spring

commit 81eb12c99633f560966606fe5727995459f622ee

add Spring.GetLogSections() & Spring.SetLogSectionFilterLevel() so Lua's can runtime add their sections (needed for Spring.Log)

rts/Lua/LuaUnsyncedCtrl.cpp
@@ -53,6 +53,7 @@
 #include "System/Config/ConfigHandler.h"
 #include "System/EventHandler.h"
 #include "System/GlobalConfig.h"
+#include "System/Log/DefaultFilter.h"
 #include "System/Log/ILog.h"
 #include "Net/Protocol/NetProtocol.h"
 #include "System/Net/PackPacket.h"
@@ -233,6 +234,8 @@ bool LuaUnsyncedCtrl::PushEntries(lua_State* L)
 	REGISTER_LUA_CFUNC(SetSunDirection);
 
 	REGISTER_LUA_CFUNC(SendSkirmishAIMessage);
+
+	REGISTER_LUA_CFUNC(SetLogSectionFilterLevel);
 
 	REGISTER_LUA_CFUNC(ClearWatchDogTimer);
 
@@ -2925,6 +2928,43 @@ int LuaUnsyncedCtrl::SendSkirmishAIMessage(lua_State* L) {
 	}
 
 	return 2;
+}
+
+/******************************************************************************/
+/******************************************************************************/
+
+int LuaUnsyncedCtrl::SetLogSectionFilterLevel(lua_State* L) {
+	const char* section = luaL_checkstring(L, 1);
+
+	int loglevel = LOG_LEVEL_INFO;
+	if (lua_israwnumber(L, 2)) {
+		loglevel = lua_tonumber(L, 2);
+	} else {
+		std::string loglvlstr = lua_tostring(L, 2);
+		StringToLowerInPlace(loglvlstr);
+		if (loglvlstr == "debug") {
+			loglevel = LOG_LEVEL_DEBUG;
+		}
+		else if (loglvlstr == "info") {
+			loglevel = LOG_LEVEL_INFO;
+		}
+		else if (loglvlstr == "warning") {
+			loglevel = LOG_LEVEL_WARNING;
+		}
+		else if (loglvlstr == "error") {
+			loglevel = LOG_LEVEL_ERROR;
+		}
+		else if (loglvlstr == "fatal") {
+			loglevel = LOG_LEVEL_FATAL;
+		}
+		else {
+			return luaL_error(L, "Incorrect arguments to Spring.SetLogSectionFilterLevel(logsection, loglevel)");
+		}
+	}
+
+	//LOG();
+	log_filter_section_setMinLevel(section, loglevel);
+	return 0;
 }
 
 /******************************************************************************/

commit b2b4740bfd85121e92a7fa90dc5f3ead222d78aa

fix #4053
caused by 81eb12c99633f560966606fe5727995459f622ee which allows pointers to
GARBAGE-COLLECTED Lua memory to be inserted into the log system (and because
LuaIntro/main.lua calls Spring.SetLogSectionFilterLevel this happens in every
game)

the callout itself will not work in any case, see Section.h

rts/Lua/LuaIntro.cpp
@@ -4,7 +4,6 @@
 #include "LuaIntro.h"
 
 #include "LuaInclude.h"
-
 #include "LuaUnsyncedCtrl.h"
 #include "LuaCallInCheck.h"
 #include "LuaConstGL.h"

rts/Lua/LuaUnsyncedCtrl.cpp
@@ -2776,36 +2776,35 @@ int LuaUnsyncedCtrl::SendSkirmishAIMessage(lua_State* L) {
 /******************************************************************************/
 
 int LuaUnsyncedCtrl::SetLogSectionFilterLevel(lua_State* L) {
-	const char* section = luaL_checkstring(L, 1);
+	int logLevel = LOG_LEVEL_INFO;
 
-	int loglevel = LOG_LEVEL_INFO;
 	if (lua_israwnumber(L, 2)) {
-		loglevel = lua_tonumber(L, 2);
-	} else {
-		std::string loglvlstr = lua_tostring(L, 2);
-		StringToLowerInPlace(loglvlstr);
+		logLevel = lua_tonumber(L, 2);
+	}
+	if (lua_isstring(L, 2)) {
+		const std::string& loglvlstr = StringToLower(lua_tostring(L, 2));
+
 		if (loglvlstr == "debug") {
-			loglevel = LOG_LEVEL_DEBUG;
+			logLevel = LOG_LEVEL_DEBUG;
 		}
 		else if (loglvlstr == "info") {
-			loglevel = LOG_LEVEL_INFO;
+			logLevel = LOG_LEVEL_INFO;
 		}
 		else if (loglvlstr == "warning") {
-			loglevel = LOG_LEVEL_WARNING;
+			logLevel = LOG_LEVEL_WARNING;
 		}
 		else if (loglvlstr == "error") {
-			loglevel = LOG_LEVEL_ERROR;
+			logLevel = LOG_LEVEL_ERROR;
 		}
 		else if (loglvlstr == "fatal") {
-			loglevel = LOG_LEVEL_FATAL;
+			logLevel = LOG_LEVEL_FATAL;
 		}
 		else {
 			return luaL_error(L, "Incorrect arguments to Spring.SetLogSectionFilterLevel(logsection, loglevel)");
 		}
 	}
 
-	//LOG();
-	log_filter_section_setMinLevel(section, loglevel);
+	log_filter_section_setMinLevel(luaL_checkstring(L, 1), logLevel);
 	return 0;
 }
 

rts/Lua/LuaUnsyncedRead.cpp
@@ -2533,12 +2533,13 @@ int LuaUnsyncedRead::GetConfigParams(lua_State* L)
 /******************************************************************************/
 
 int LuaUnsyncedRead::GetLogSections(lua_State* L) {
-	const int numLogSections = log_filter_section_getRegistered();
+	const int numLogSections = log_filter_section_getNumRegisteredSections();
 
 	lua_createtable(L, 0, numLogSections);
 	for (int i = 0; i < numLogSections; ++i) {
 		const char* sectionName = log_filter_section_getRegisteredIndex(i);
 		const int logLevel = log_filter_section_getMinLevel(sectionName);
+
 		lua_pushstring(L, sectionName);
 		lua_pushnumber(L, logLevel);
 		lua_rawset(L, -3);

rts/System/Input/InputHandler.cpp
@@ -1,6 +1,7 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "InputHandler.h"
+#include "System/TimeProfiler.h"
 
 InputHandler input;
 
@@ -12,6 +13,19 @@ void InputHandler::PushEvent(const SDL_Event& ev)
 {
 	sig(ev);
 }
+
+void InputHandler::PushEvents()
+{
+	SCOPED_TIMER("InputHandler::PushEvents");
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+		// SDL_PollEvent may modify FPU flags
+		streflop::streflop_init<streflop::Simple>();
+		PushEvent(event);
+	}
+}
+
 
 boost::signals2::connection InputHandler::AddHandler(SignalType::slot_function_type handler)
 {

rts/System/Input/InputHandler.h
@@ -17,6 +17,7 @@ public:
 	InputHandler();
 
 	void PushEvent(const SDL_Event& ev);
+	void PushEvents();
 
 	boost::signals2::connection AddHandler(SignalType::slot_function_type);
 

rts/System/Log/Backend.cpp
@@ -39,9 +39,9 @@ void log_backend_registerSink(log_sink_ptr sink) {
 // LogSinkHandler::RemoveSink --> log_backend_unregisterSink
 // 
 void log_backend_unregisterSink(log_sink_ptr sink) {
-
 	std::vector<log_sink_ptr>& sinks = log_formatter_getSinks();
 	std::vector<log_sink_ptr>::iterator si;
+
 	for (si = sinks.begin(); si != sinks.end(); ++si) {
 		if (*si == sink) {
 			sinks.erase(si);
@@ -56,9 +56,9 @@ void log_backend_registerCleanup(log_cleanup_ptr cleanupFunc) {
 }
 
 void log_backend_unregisterCleanup(log_cleanup_ptr cleanupFunc) {
-
 	std::vector<log_cleanup_ptr>& cleanupFuncs = log_formatter_getCleanupFuncs();
 	std::vector<log_cleanup_ptr>::iterator si;
+
 	for (si = cleanupFuncs.begin(); si != cleanupFuncs.end(); ++si) {
 		if (*si == cleanupFunc) {
 			cleanupFuncs.erase(si);
@@ -74,24 +74,25 @@ void log_backend_unregisterCleanup(log_cleanup_ptr cleanupFunc) {
 ///@{
 
 /// Eventually formats and routes the record to all sinks
-void log_backend_record(const char* section, int level, const char* fmt,
-		va_list arguments)
+void log_backend_record(const char* section, int level, const char* fmt, va_list arguments)
 {
 	const std::vector<log_sink_ptr>& sinks = log_formatter_getSinks();
+
 	if (sinks.empty()) {
-		// no sinks are registered
 		static bool warned = false;
+
 		if (!warned) {
-			fprintf(stderr,
-					"\nWARNING: A log message was recorded, but no sink is registered."
-					"\n         (there will be no further warnings)\n\n");
 			warned = true;
+
+			fprintf(stderr,
+				"\nWARNING: A log message was recorded, but no sink is registered."
+				"\n         (there will be no further warnings)\n\n");
 		}
 	} else {
 		// format the record
 		const char* record = log_formatter_format(section, level, fmt, arguments);
 
-		// sink the record
+		// sink the record with each registered sink
 		for (auto si = sinks.begin(); si != sinks.end(); ++si) {
 			(*si)(section, level, record);
 		}
@@ -102,7 +103,6 @@ void log_backend_record(const char* section, int level, const char* fmt,
 
 /// Passes on a cleanup request to all sinks
 void log_backend_cleanup() {
-
 	const std::vector<log_cleanup_ptr>& cleanupFuncs = log_formatter_getCleanupFuncs();
 
 	for (auto si = cleanupFuncs.begin(); si != cleanupFuncs.end(); ++si) {

rts/System/Log/DefaultFilter.cpp
@@ -22,25 +22,24 @@ extern "C" {
 bool log_frontend_isEnabled(const char* section, int level);
 
 
-extern void log_backend_record(const char* section, int level,
-		const char* fmt, va_list arguments);
-
+extern void log_backend_record(const char* section, int level, const char* fmt, va_list arguments);
 extern void log_backend_cleanup();
 
 
 struct log_filter_section_compare {
-	inline bool operator()(const char* const& section1,
-			const char* const& section2) const
+	inline bool operator()(const char* const& section1, const char* const& section2) const
 	{
 		return LOG_SECTION_COMPARE(section1, section2);
 	}
 };
 
+
+
 namespace {
 	typedef std::set<const char*, log_filter_section_compare> secSet_t;
 	typedef std::map<const char*, int, log_filter_section_compare> secIntMap_t;
 
-	int minLevel = LOG_LEVEL_ALL;
+	int minLogLevel = LOG_LEVEL_ALL;
 
 	secIntMap_t& log_filter_getSectionMinLevels() {
 		static secIntMap_t sectionMinLevels;
@@ -51,7 +50,18 @@ namespace {
 		static secSet_t sections;
 		return sections;
 	}
+
+	void log_filter_printSectionMinLevels(const char* func) {
+		printf("[%s][caller=%s]\n", __FUNCTION__, func);
+
+		const auto& secLevels = log_filter_getSectionMinLevels();
+
+		for (auto it = secLevels.begin(); it != secLevels.end(); ++it) {
+			printf("\tsectionName=\"%s\" minLevel=%d\n", it->first, it->second);
+		}
+	}
 }
+
 
 
 static inline int log_filter_section_getDefaultMinLevel(const char* section) {
@@ -71,37 +81,32 @@ static inline int log_filter_section_getDefaultMinLevel(const char* section) {
 
 static inline void log_filter_checkCompileTimeMinLevel(int level) {
 
-	if (level < _LOG_LEVEL_MIN) {
-		// to prevent an endless recursion
-		if (_LOG_LEVEL_MIN <= LOG_LEVEL_WARNING) {
-			LOG_L(L_WARNING,
-					"Tried to set minimum log level %i, but it was set to %i"
-					" at compile-time -> effective min-level is %i.",
-					level, _LOG_LEVEL_MIN, _LOG_LEVEL_MIN);
-		}
-	}
+	if (level >= _LOG_LEVEL_MIN)
+		return;
+
+	// to prevent an endless recursion
+	if (_LOG_LEVEL_MIN > LOG_LEVEL_WARNING)
+		return;
+
+	LOG_L(L_WARNING,
+		"Tried to set minimum log level %i, but it was set to"
+		" %i at compile-time -> effective min-level is %i.",
+		level, _LOG_LEVEL_MIN, _LOG_LEVEL_MIN
+	);
 }
 
 
-int log_filter_global_getMinLevel() {
-	return minLevel;
-}
-
-void log_filter_global_setMinLevel(int level) {
-
-	log_filter_checkCompileTimeMinLevel(level);
-
-	minLevel = level;
-}
+int log_filter_global_getMinLevel() { return minLogLevel; }
+void log_filter_global_setMinLevel(int level) { log_filter_checkCompileTimeMinLevel(level); minLogLevel = level; }
 
 
-int log_filter_section_getMinLevel(const char* section) {
-
+int log_filter_section_getMinLevel(const char* section)
+{
 	int level = -1;
 
 	const secIntMap_t& sectionMinLevels = log_filter_getSectionMinLevels();
-	const secIntMap_t::const_iterator sectionMinLevel
-			= sectionMinLevels.find(section);
+	const secIntMap_t::const_iterator sectionMinLevel = sectionMinLevels.find(section);
+
 	if (sectionMinLevel == sectionMinLevels.end()) {
 		level = log_filter_section_getDefaultMinLevel(section);
 	} else {
@@ -111,11 +116,33 @@ int log_filter_section_getMinLevel(const char* section) {
 	return level;
 }
 
-void log_filter_section_setMinLevel(const char* section, int level) {
-
+// called by LogOutput for each ENABLED section
+// also by LuaUnsyncedCtrl::SetLogSectionFilterLevel
+void log_filter_section_setMinLevel(const char* section, int level)
+{
 	log_filter_checkCompileTimeMinLevel(level);
 
+	secSet_t& registeredSections = log_filter_getRegisteredSections();
 	secIntMap_t& sectionMinLevels = log_filter_getSectionMinLevels();
+
+	// NOTE:
+	//   <section> might not be in the registered set if called from Lua
+	//
+	//   if we inserted the raw pointer passed from Lua (lua_to*string)
+	//   which lives on the heap into the min-level map it could become
+	//   dangling because of Lua garbage collection but this is not even
+	//   allowed by Section.h (!)
+	const auto it = registeredSections.find(section);
+
+	if (it == registeredSections.end()) {
+		LOG_L(L_WARNING, "[%s] section \"%s\" is not registered", __FUNCTION__, section);
+		return;
+	}
+
+	// take the pointer from the registered set
+	// (same string but will not become garbage)
+	section = *it;
+
 	if (level == log_filter_section_getDefaultMinLevel(section)) {
 		sectionMinLevels.erase(section);
 	} else {
@@ -141,35 +168,34 @@ void log_enable_and_disable(const bool enable)
 	log_filter_global_setMinLevel(newLevel);
 }
 
-int log_filter_section_getRegistered() {
-	return log_filter_getRegisteredSections().size();
+int log_filter_section_getNumRegisteredSections() {
+	return (log_filter_getRegisteredSections().size());
 }
 
 const char* log_filter_section_getRegisteredIndex(int index) {
-
-	const char* section = NULL;
-
 	const secSet_t& registeredSections = log_filter_getRegisteredSections();
-	if ((index >= 0) && (index < (int)registeredSections.size())) {
-		secSet_t::const_iterator si = registeredSections.begin();
-		for (int curIndex = 0; curIndex < index; ++curIndex) {
-			si = ++si;
-		}
-		section = *si;
+
+	if (index < 0)
+		return NULL;
+	if (index >= static_cast<int>(registeredSections.size()))
+		return NULL;
+
+	secSet_t::const_iterator si = registeredSections.begin();
+
+	for (int curIndex = 0; curIndex < index; ++curIndex) {
+		++si;
 	}
 
-	return section;
+	return (*si);
 }
 
-static void log_filter_record(const char* section, int level, const char* fmt,
-		va_list arguments)
+static void log_filter_record(const char* section, int level, const char* fmt, va_list arguments)
 {
 	assert(level > LOG_LEVEL_ALL);
 	assert(level < LOG_LEVEL_NONE);
 
-	if (!log_frontend_isEnabled(section, level)) {
+	if (!log_frontend_isEnabled(section, level))
 		return;
-	}
 
 	// format (and later store) the log record
 	log_backend_record(section, level, fmt, arguments);
@@ -183,24 +209,27 @@ static void log_filter_record(const char* section, int level, const char* fmt,
 ///@{
 
 bool log_frontend_isEnabled(const char* section, int level) {
+	if (level < log_filter_global_getMinLevel())
+		return false;
+	if (level < log_filter_section_getMinLevel(section))
+		return false;
 
-	return ((level >= log_filter_global_getMinLevel())
-			&& (level >= log_filter_section_getMinLevel(section)));
+	return true;
 }
 
+// see the LOG_REGISTER_SECTION_RAW macro in ILog.h
 void log_frontend_registerSection(const char* section) {
-
 	if (!LOG_SECTION_IS_DEFAULT(section)) {
 		secSet_t& registeredSections = log_filter_getRegisteredSections();
 		secSet_t::const_iterator si = registeredSections.find(section);
+
 		if (si == registeredSections.end()) {
 			registeredSections.insert(section);
 		}
 	}
 }
 
-void log_frontend_record(const char* section, int level, const char* fmt,
-		...)
+void log_frontend_record(const char* section, int level, const char* fmt, ...)
 {
 	assert(level > LOG_LEVEL_ALL);
 	assert(level < LOG_LEVEL_NONE);
@@ -222,15 +251,15 @@ void log_frontend_cleanup() {
 } // extern "C"
 #endif
 
+
+
 std::set<const char*> log_filter_section_getRegisteredSet() {
 
 	std::set<const char*> outSet;
 
 	const secSet_t& registeredSections = log_filter_getRegisteredSections();
-	secSet_t::const_iterator si;
-	for (si = registeredSections.begin(); si != registeredSections.end();
-			++si)
-	{
+
+	for (auto si = registeredSections.begin(); si != registeredSections.end(); ++si) {
 		outSet.insert(*si);
 	}
 

rts/System/Log/DefaultFilter.h
@@ -9,6 +9,8 @@
  * what is logged and what not.
  */
 
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -17,6 +19,8 @@ extern "C" {
  * @name logging_filter_defaultFilter_control
  * @{
  */
+
+
 
 /**
  * Sets the minimum level to log for all sections, including the default one.
@@ -38,6 +42,8 @@ void log_filter_global_setMinLevel(int level);
  * @see #log_filter_section_getMinLevel
  */
 int log_filter_global_getMinLevel();
+
+
 
 /**
  * Sets whether log messages for a certain section are logged or not.
@@ -66,15 +72,16 @@ void log_filter_section_setMinLevel(const char* section, int level);
  */
 int log_filter_section_getMinLevel(const char* section);
 
+
+
 /**
  * Returns the number of currently registered sections.
  * @see #log_filter_section_getRegisteredIndex
  */
-int log_filter_section_getRegistered();
+int log_filter_section_getNumRegisteredSections();
 
 /**
  * Returns a registered section.
- * @see #log_filter_section_getRegistered
  */
 const char* log_filter_section_getRegisteredIndex(int index);
 
@@ -91,10 +98,10 @@ void log_enable_and_disable(const bool enable);
 #include <set>
 
 
+
 /**
  * Returns the registered sections.
  * This is simply to be more C++ friendly.
- * @see #log_filter_section_getRegistered
  */
 std::set<const char*> log_filter_section_getRegisteredSet();
 #endif

rts/System/Log/DefaultFormatter.cpp
@@ -29,19 +29,22 @@ extern "C" {
 static const int SECTION_SIZE_MIN = 10;
 static const int SECTION_SIZE_MAX = 20;
 
+
 // *******************************************************************************************
 // Helpers
 static inline void ResizeBuffer(char** buffer, size_t* bufferSize, const bool copy = false)
 {
-	*bufferSize <<= 2; // `2` to increase it faster
 	char* old = *buffer;
+
+	*bufferSize <<= 2; // `2` to increase it faster
 	*buffer = new char[*bufferSize];
+
 	if (copy) {
-		memcpy(*buffer, old, *bufferSize >> 2);
+		memcpy(*buffer, old, (*bufferSize) >> 2);
 	}
+
 	delete[] old;
 }
-
 
 static inline void PrintfAppend(char** buffer, size_t* bufferSize, const char* fmt, va_list arguments)
 {
@@ -49,20 +52,23 @@ static inline void PrintfAppend(char** buffer, size_t* bufferSize, const char* f
 	size_t bufferPos = strlen(*buffer);
 
 	do {
-		size_t freeBufferSize = (*bufferSize) - bufferPos;
+		const size_t freeBufferSize = (*bufferSize) - bufferPos;
 		char* bufAppendPos = &((*buffer)[bufferPos]);
 
 		// printf will move the internal pointer of va_list.
 		// So we need to make a copy, if want to run it again.
 		va_list arguments_;
 		va_copy(arguments_, arguments); 
-		int writtenChars = VSNPRINTF(bufAppendPos, freeBufferSize, fmt, arguments_);
+		const int writtenChars = VSNPRINTF(bufAppendPos, freeBufferSize, fmt, arguments_);
 		va_end(arguments_);
+
 		// since writtenChars excludes the null terminator (if any was written),
 		// writtenChars >= freeBufferSize always means buffer was too small
 		// NOTE: earlier glibc versions and MSVC will return -1 when buffer is too small
-		const bool bufferTooSmall = writtenChars >= freeBufferSize || writtenChars < 0;
-		if (!bufferTooSmall) break;
+		// const bool bufferTooSmall = writtenChars >= freeBufferSize || writtenChars < 0;
+
+		if (writtenChars >= 0 && writtenChars < freeBufferSize)
+			break;
 
 		ResizeBuffer(buffer, bufferSize, true);
 	} while (true);
@@ -71,38 +77,43 @@ static inline void PrintfAppend(char** buffer, size_t* bufferSize, const char* f
 
 // *******************************************************************************************
 
-static void log_formatter_createPrefix_xorgStyle(char** buffer,
-		size_t* bufferSize, const char* section, int level)
-{
-	section = log_util_prepareSection(section);
-
+static void log_formatter_createPrefix_xorgStyle(
+	char** buffer,
+	size_t* bufferSize,
+	const char* section,
+	int level
+) {
+	const char* prepSection = log_util_prepareSection(section);
 	const char levelChar = log_util_levelToChar(level);
 
 	SNPRINTF(*buffer, *bufferSize, "(%c%c) %*.*s - ", levelChar, levelChar,
-			SECTION_SIZE_MIN, SECTION_SIZE_MAX, section);
+			SECTION_SIZE_MIN, SECTION_SIZE_MAX, prepSection);
 }
 
-
-static void log_formatter_createPrefix_testing(char** buffer,
-		size_t* bufferSize, const char* section, int level)
-{
-	section = log_util_prepareSection(section);
-
+static void log_formatter_createPrefix_testing(
+	char** buffer,
+	size_t* bufferSize,
+	const char* section,
+	int level
+) {
+	const char* prepSection = log_util_prepareSection(section);
 	const char* levelStr = log_util_levelToString(level);
 
-	SNPRINTF(*buffer, *bufferSize, "%s %s: ", levelStr, section);
+	SNPRINTF(*buffer, *bufferSize, "%s %s: ", levelStr, prepSection);
 }
 
-
-static void log_formatter_createPrefix_default(char** buffer,
-		size_t* bufferSize, const char* section, int level)
-{
+static void log_formatter_createPrefix_default(
+	char** buffer,
+	size_t* bufferSize,
+	const char* section,
+	int level
+) {
 	(*buffer)[0] = '\0';
 
 	if (!LOG_SECTION_IS_DEFAULT(section)) {
-		section = log_util_prepareSection(section);
+		const char* prepSection = log_util_prepareSection(section);
 		STRCAT_T(*buffer, *bufferSize, "[");
-		STRCAT_T(*buffer, *bufferSize, section);
+		STRCAT_T(*buffer, *bufferSize, prepSection);
 		STRCAT_T(*buffer, *bufferSize, "] ");
 	}
 	if (level != LOG_LEVEL_INFO) {
@@ -113,16 +124,18 @@ static void log_formatter_createPrefix_default(char** buffer,
 }
 
 
-static inline void log_formatter_createPrefix(char** buffer, size_t* bufferSize,
-		const char* section, int level)
-{
+static inline void log_formatter_createPrefix(
+	char** buffer,
+	size_t* bufferSize,
+	const char* section,
+	int level
+) {
 	//log_formatter_createPrefix_xorgStyle(buffer, bufferSize, section, level);
 	//log_formatter_createPrefix_testing(buffer, bufferSize, section, level);
 	log_formatter_createPrefix_default(buffer, bufferSize, section, level);
 
 	// check if the buffer was large enough, if not resize it and try again
-	const bool bufferTooSmall = ((strlen(*buffer) + 1) >= *bufferSize);
-	if (bufferTooSmall) {
+	if ((strlen(*buffer) + 1) >= *bufferSize) {
 		ResizeBuffer(buffer, bufferSize);
 		log_formatter_createPrefix(buffer, bufferSize, section, level); // recursive
 	}
@@ -143,13 +156,18 @@ static inline void log_formatter_createPrefix(char** buffer, size_t* bufferSize,
 char* log_formatter_format(const char* section, int level, const char* fmt, va_list arguments)
 {
 	size_t bufferSize = 256;
+
 	char* mem = new char[bufferSize];
 	char** buffer = &mem;
 
+	memset(&mem[0], 0, bufferSize);
+
+	#if 1
 	log_formatter_createPrefix(buffer, &bufferSize, section, level);
 	PrintfAppend(buffer, &bufferSize, fmt, arguments);
+	#endif
 
-	return *buffer;
+	return mem;
 }
 
 ///@}

rts/System/Log/ILog.h
@@ -85,17 +85,18 @@ extern void log_frontend_record(const char* section, int level, const char* fmt,
 extern void log_frontend_cleanup();
 
 
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 // Level & Section
 
 #include "Level.h"
+#include "Section.h"
 
 #define _LOG_IS_ENABLED_LEVEL_STATIC(level) \
 	(LOG_LEVE##level >= _LOG_LEVEL_MIN)
 
 
-#include "Section.h"
 
 // enable all log sections at compile-time
 #define _LOG_IS_ENABLED_SECTION_STATIC(section) \
@@ -114,12 +115,9 @@ extern void log_frontend_cleanup();
  * Pre-processor trickery, useful to create unique identifiers.
  * see http://stackoverflow.com/questions/461062/c-anonymous-variables
  */
-#define _CONCAT_SUB(start, end) \
-	start##end
-#define _CONCAT(start, end) \
-	_CONCAT_SUB(start, end)
-#define _UNIQUE_IDENT(prefix) \
-	_CONCAT(prefix##__, _CONCAT(_CONCAT(__COUNTER__, __), __LINE__))
+#define _CONCAT_SUB(start, end)   start##end
+#define _CONCAT(start, end)   _CONCAT_SUB(start, end)
+#define _UNIQUE_IDENT(prefix)   _CONCAT(prefix##__, _CONCAT(_CONCAT(__COUNTER__, __), __LINE__))
 
 // Register a section (only the first time the code is run)
 #if       defined(__cplusplus)
@@ -162,6 +160,7 @@ extern void log_frontend_cleanup();
 #endif // defined(__cplusplus)
 
 
+
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 // Log pre-processing
@@ -175,79 +174,75 @@ extern void log_frontend_cleanup();
 /*
  * This is where we might add all sorts of additional info,
  * like the LOG_SECTION, __FILE__, __LINE__ or a stack-trace.
- * In theory, we could also use this to compleetly disable logging at
+ * In theory, we could also use this to completely disable logging at
  * compile-time already.
  */
 
 /// Redirect to runtime processing
-#define _LOG_RECORD(section, level, fmt, ...) \
-	log_frontend_record(section, LOG_LEVE##level, fmt, ##__VA_ARGS__)
+#define _LOG_RECORD(section, level, fmt, ...)   log_frontend_record(section, LOG_LEVE##level, fmt, ##__VA_ARGS__)
 
 // per level compile-time filters
 #if _LOG_IS_ENABLED_LEVEL_STATIC(L_DEBUG)
-	#define _LOG_FILTER_L_DEBUG(section, fmt, ...) \
-		_LOG_RECORD(section, L_DEBUG, fmt, ##__VA_ARGS__)
+	#define _LOG_FILTER_L_DEBUG(section, fmt, ...)   _LOG_RECORD(section, L_DEBUG, fmt, ##__VA_ARGS__)
 #else
 	#define _LOG_FILTER_L_DEBUG(section, fmt, ...)
 #endif
+
 #if _LOG_IS_ENABLED_LEVEL_STATIC(L_INFO)
-	#define _LOG_FILTER_L_INFO(section, fmt, ...) \
-		_LOG_RECORD(section, L_INFO, fmt, ##__VA_ARGS__)
+	#define _LOG_FILTER_L_INFO(section, fmt, ...)   _LOG_RECORD(section, L_INFO, fmt, ##__VA_ARGS__)
 #else
 	#define _LOG_FILTER_L_INFO(section, fmt,...)
 	#warning log messages of level INFO are not compiled into the binary
 #endif
+
 #if _LOG_IS_ENABLED_LEVEL_STATIC(L_WARNING)
-	#define _LOG_FILTER_L_WARNING(section, fmt, ...) \
-		_LOG_RECORD(section, L_WARNING, fmt, ##__VA_ARGS__)
+	#define _LOG_FILTER_L_WARNING(section, fmt, ...)   _LOG_RECORD(section, L_WARNING, fmt, ##__VA_ARGS__)
 #else
 	#define _LOG_FILTER_L_WARNING(section, fmt, ...)
 	#warning log messages of level WARNING are not compiled into the binary
 #endif
+
 #if _LOG_IS_ENABLED_LEVEL_STATIC(L_ERROR)
-	#define _LOG_FILTER_L_ERROR(section, fmt, ...) \
-		_LOG_RECORD(section, L_ERROR, fmt, ##__VA_ARGS__)
+	#define _LOG_FILTER_L_ERROR(section, fmt, ...)   _LOG_RECORD(section, L_ERROR, fmt, ##__VA_ARGS__)
 #else
 	#define _LOG_FILTER_L_ERROR(section, fmt, ##__VA_ARGS__)
 	#warning log messages of level ERROR are not compiled into the binary
 #endif
+
 #if _LOG_IS_ENABLED_LEVEL_STATIC(L_FATAL)
-	#define _LOG_FILTER_L_FATAL(section, fmt, ...) \
-		_LOG_RECORD(section, L_FATAL, fmt, ##__VA_ARGS__)
+	#define _LOG_FILTER_L_FATAL(section, fmt, ...)   _LOG_RECORD(section, L_FATAL, fmt, ##__VA_ARGS__)
 #else
 	#define _LOG_FILTER_L_FATAL(section, fmt, ...)
 	#warning log messages of level FATAL are not compiled into the binary
 #endif
 
 /// Registers the section and connects to the filter macro
-#define _LOG_SECTION(section, level, fmt, ...) \
-	_LOG_FILTER_##level(section, fmt, ##__VA_ARGS__)
+#define _LOG_SECTION(section, level, fmt, ...)   _LOG_FILTER_##level(section, fmt, ##__VA_ARGS__)
 
 /// Uses the section defined in LOG_SECTION
-#define _LOG_SECTION_DEFINED(level, fmt, ...) \
-	_LOG_SECTION(LOG_SECTION_CURRENT, level, fmt, ##__VA_ARGS__)
+#define _LOG_SECTION_DEFINED(level, fmt, ...)   _LOG_SECTION(LOG_SECTION_CURRENT, level, fmt, ##__VA_ARGS__)
 
 /// Entry point for frontend-internal processing
-#define _LOG(level, fmt, ...) \
-	_LOG_SECTION_DEFINED(level, fmt, ##__VA_ARGS__)
+#define _LOG(level, fmt, ...)   _LOG_SECTION_DEFINED(level, fmt, ##__VA_ARGS__)
 
 
 #define _LOG_IS_ENABLED_STATIC_S(section, level) \
 	(  _LOG_IS_ENABLED_LEVEL_STATIC(level) \
 	&& _LOG_IS_ENABLED_SECTION_STATIC(section))
-#define _LOG_IS_ENABLED_STATIC(level) \
-	_LOG_IS_ENABLED_STATIC_S(LOG_SECTION_CURRENT, level)
 
 #define _LOG_IS_ENABLED_S(section, level) \
 	(  _LOG_IS_ENABLED_STATIC_S(section, level) \
 	&& _LOG_IS_ENABLED_RUNTIME(section, level))
-#define _LOG_IS_ENABLED(level) \
-	_LOG_IS_ENABLED_S(LOG_SECTION_CURRENT, level)
+
+#define _LOG_IS_ENABLED_STATIC(level)   _LOG_IS_ENABLED_STATIC_S(LOG_SECTION_CURRENT, level)
+#define _LOG_IS_ENABLED(level)   _LOG_IS_ENABLED_S(LOG_SECTION_CURRENT, level)
 
 
 /// Redirect to runtime processing
 #define _LOG_CLEANUP() \
 	log_frontend_cleanup()
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////

rts/System/Log/Section.h
@@ -1,13 +1,13 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#ifndef LOG_SECTION_H
-#define LOG_SECTION_H
-
-#include <string.h>
-
 /*
  * see ILog.h for documentation
  */
+
+#ifndef LOG_SECTION_H
+#define LOG_SECTION_H
+
+#include <cstring>
 
 #define LOG_SECTION_DEFAULT ""
 
@@ -15,19 +15,27 @@
 /**
  * Helpers
  */
-// first do a fasth-path and check if both c-strings share the same memory location
+// first check if both c-strings share the same memory location
 // if not check if both c-strings have the same content
-#define LOG_SECTION_EQUAL(section1, section2) \
-	(((void*)section1 == (void*)section2) \
-	|| (((void*)section1 != NULL) && ((void*)section2 != NULL) \
-		&& (strcmp(section1, section2) == 0)))
+static int LOG_SECTION_EQUAL(const char* s1, const char* s2) {
+	if (s1 == s2)
+		return 1;
+	if (s1 == NULL || s2 == NULL)
+		return 0;
 
-#define LOG_SECTION_COMPARE(section1, section2) \
-	(((void*)section1 == NULL) \
-		|| (((void*)section2 != NULL) && (strcmp(section1, section2) > 0)))
+	return (strcmp(s1, s2) == 0);
+}
+static int LOG_SECTION_COMPARE(const char* s1, const char* s2) {
+	if (s1 == NULL)
+		return 1;
+	if (s2 == NULL)
+		return 0;
 
-#define LOG_SECTION_IS_DEFAULT(section) \
-	LOG_SECTION_EQUAL(section, LOG_SECTION_DEFAULT)
+	return (strcmp(s1, s2) > 0);
+}
+static int LOG_SECTION_IS_DEFAULT(const char* s) {
+	return (LOG_SECTION_EQUAL(s, LOG_SECTION_DEFAULT));
+}
 
 
 /**

rts/System/SpringApp.cpp
@@ -1029,16 +1029,7 @@ int SpringApp::Run()
 
 	while (!gu->globalQuit) {
 		ResetScreenSaverTimeout();
-
-		{
-			SCOPED_TIMER("InputHandler::PushEvents");
-			SDL_Event event;
-
-			while (SDL_PollEvent(&event)) {
-				streflop::streflop_init<streflop::Simple>(); // SDL_PollEvent may modify FPU flags
-				input.PushEvent(event);
-			}
-		}
+		input.PushEvents();
 
 		if (!Update())
 			break;