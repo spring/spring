spring/spring

commit 2b5598a122e5d70a161a1b22232e477fc6d95f85

fix a NaN in Camera.cpp with top down camera -> the NaN causes an endless loop in later TraceRay codes

rts/Game/Camera.cpp
@@ -61,12 +61,10 @@ void CCamera::CopyState(const CCamera* cam) {
 	lppScale  = cam->lppScale;
 }
 
-
-void CCamera::Update(bool resetUp)
+void CCamera::Update()
 {
-	if (resetUp) {
+	if (forward.cross(UpVector) != ZeroVector)
 		up = UpVector;
-	}
 
 	right = forward.cross(up);
 	right.UnsafeANormalize();

rts/Game/Camera.h
@@ -19,7 +19,7 @@ public:
 	void UpdateForward();
 	bool InView(const float3& p, float radius = 0) const;
 	bool InView(const float3& mins, const float3& maxs) const;
-	void Update(bool resetUp = true);
+	void Update();
 
 	void GetFrustumSides(float miny, float maxy, float scale, bool negSide = false);
 	void GetFrustumSide(

rts/Rendering/Env/CubeMapHandler.cpp
@@ -203,7 +203,7 @@ void CubeMapHandler::CreateReflectionFace(unsigned int glType, const float3& cam
 	if (camera->forward.y == -1.0f) { camera->up = -FwdVector; }
 
 	camera->SetPos().y = ground->GetHeightAboveWater(camera->GetPos().x, camera->GetPos().z, false) + 50.0f;
-	camera->Update(false);
+	camera->Update();
 
 	sky->Draw();
 

commit c194773bfef64f1841f3a1c54aa74758ef4e292d

fix cubemap reflections (#4163); broken by 2b5598a1

rts/Game/Camera.cpp
@@ -64,15 +64,9 @@ void CCamera::CopyState(const CCamera* cam) {
 	lppScale  = cam->lppScale;
 }
 
-void CCamera::Update()
+void CCamera::Update(bool terrainReflectionPass)
 {
-	if (forward.cross(UpVector) != ZeroVector)
-		up = UpVector;
-
-	right = forward.cross(up);
-	right.UnsafeANormalize();
-	up = right.cross(forward);
-	up.UnsafeANormalize();
+	UpdateRightAndUp(terrainReflectionPass);
 
 	const float aspect = globalRendering->aspectRatio;
 	const float viewx = math::tan(aspect * halfFov);
@@ -208,6 +202,22 @@ void CCamera::UpdateForward()
 	forward.x = math::sin(rot.y) * math::cos(rot.x);
 	forward.y = math::sin(rot.x);
 	forward.Normalize();
+}
+
+void CCamera::UpdateRightAndUp(bool terrainReflectionPass)
+{
+	// terrain (not water) cubemap reflection passes set forward
+	// to {+/-}UpVector which would cause vector degeneracy when
+	// calculating right and up
+	if (std::fabs(forward.y * UpVector.y) >= 0.99f) {
+		up = FwdVector * Sign(forward.y);
+	} else {
+		// in the terrain reflection pass everything is upside-down!
+		up = UpVector * -Sign(int(terrainReflectionPass));
+	}
+
+	right = (forward.cross(up)).UnsafeANormalize();
+	up = (right.cross(forward)).UnsafeANormalize();
 }
 
 

rts/Game/Camera.h
@@ -26,11 +26,14 @@ public:
 
 	float3 CalcPixelDir(int x,int y) const;
 	float3 CalcWindowCoordinates(const float3& objPos) const;
+
 	void CopyState(const CCamera*);
 	void UpdateForward();
+	void UpdateRightAndUp(bool terrainReflectionPass);
+
 	bool InView(const float3& p, float radius = 0) const;
 	bool InView(const float3& mins, const float3& maxs) const;
-	void Update();
+	void Update(bool terrainReflectionPass = false);
 
 	void GetFrustumSides(float miny, float maxy, float scale, bool negSide = false);
 	void GetFrustumSide(
@@ -64,8 +67,7 @@ public:
 	float GetLPPScale() const { return lppScale; }
 
 	/// @param fov in degree
-	float3& SetPos() { return pos; }
-	void SetPos(float3 p) { pos = p; }
+	void SetPos(const float3& p) { pos = p; }
 	void SetFov(float fov);
 
 	float GetMoveDistance(float* time, float* speed, int idx) const;

rts/Game/Camera/OrbitController.cpp
@@ -157,8 +157,8 @@ float3 COrbitController::GetDir() const
 
 void COrbitController::Orbit()
 {
-	camera->SetPos() = cen + GetOrbitPos();
-	camera->SetPos().y = std::max(camera->GetPos().y, ground->GetHeightReal(camera->GetPos().x, camera->GetPos().z, false));
+	camera->SetPos(cen + GetOrbitPos());
+	camera->SetPos((camera->GetPos() + XZVector) + (UpVector * std::max(camera->GetPos().y, ground->GetHeightReal(camera->GetPos().x, camera->GetPos().z, false))));
 	camera->forward = (cen - camera->GetPos()).ANormalize();
 	camera->up = UpVector;
 }
@@ -166,11 +166,11 @@ void COrbitController::Orbit()
 void COrbitController::Pan(int rdx, int rdy)
 {
 	// horizontal pan
-	camera->SetPos() += (camera->right * -rdx * panSpeedFact);
+	camera->SetPos(camera->GetPos() + (camera->right * -rdx * panSpeedFact));
 	cen += (camera->right * -rdx * panSpeedFact);
 
 	// vertical pan
-	camera->SetPos() += (camera->up * rdy * panSpeedFact);
+	camera->SetPos(camera->GetPos() + (camera->up * rdy * panSpeedFact));
 	cen += (camera->up * rdy * panSpeedFact);
 
 
@@ -179,7 +179,7 @@ void COrbitController::Pan(int rdx, int rdy)
 	const float cenGH = ground->GetHeightReal(cen.x, cen.z, false);
 
 	if (camera->GetPos().y < camGH) {
-		camera->SetPos().y = camGH;
+		camera->SetPos((camera->GetPos() * XZVector) + (UpVector * camGH));
 	}
 
 	if (cen.y < cenGH) {
@@ -227,9 +227,7 @@ void COrbitController::SetPos(const float3& newPos)
 	cen.z += dz;
 	cen.y = ground->GetHeightReal(cen.x, cen.z, false);
 
-	camera->SetPos().x += dx;
-	camera->SetPos().z += dz;
-
+	camera->SetPos(camera->GetPos() + float3(dx, 0.0f, dz));
 	Init(camera->GetPos(), cen);
 }
 

rts/Rendering/Env/AdvWater.cpp
@@ -307,7 +307,7 @@ void CAdvWater::UpdateWater(CGame* game)
 	new (realCam) CCamera(*camera); // anti-crash workaround for multithreading
 
 	camera->forward.y *= -1.0f;
-	camera->SetPos().y *= -1.0f;
+	camera->SetPos(camera->GetPos() * float3(1.0f, -1.0f, 1.0f));
 	camera->Update();
 
 	reflectFBO.Bind();

rts/Rendering/Env/BumpWater.cpp
@@ -1221,7 +1221,7 @@ void CBumpWater::DrawReflection(CGame* game)
 	new (realCam) CCamera(*camera); // anti-crash workaround for multithreading
 
 	camera->forward.y *= -1.0f;
-	camera->SetPos().y *= -1.0f;
+	camera->SetPos(camera->GetPos() * float3(1.0f, -1.0f, 1.0f));
 	camera->Update();
 
 	game->SetDrawMode(CGame::gameReflectionDraw);

rts/Rendering/Env/CubeMapHandler.cpp
@@ -194,15 +194,11 @@ void CubeMapHandler::CreateReflectionFace(unsigned int glType, const float3& cam
 
 	game->SetDrawMode(CGame::gameReflectionDraw);
 
-	camera->SetFov(90.0f);
 	camera->forward = camDir;
-	camera->up = -UpVector;
-
-	if (camera->forward.y ==  1.0f) { camera->up =  FwdVector; }
-	if (camera->forward.y == -1.0f) { camera->up = -FwdVector; }
-
-	camera->SetPos().y = ground->GetHeightAboveWater(camera->GetPos().x, camera->GetPos().z, false) + 50.0f;
-	camera->Update();
+	camera->SetFov(90.0f);
+	camera->SetPos((camera->GetPos()) * XZVector + UpVector * (ground->GetHeightAboveWater(camera->GetPos().x, camera->GetPos().z, false) + 50.0f));
+	// calculate temporary new coor-system and matrices
+	camera->Update(true);
 
 	sky->Draw();
 

rts/Rendering/Env/DynWater.cpp
@@ -452,7 +452,7 @@ void CDynWater::DrawReflection(CGame* game)
 	new (realCam) CCamera(*camera); // anti-crash workaround for multithreading
 
 	camera->forward.y *= -1.0f;
-	camera->SetPos().y *= -1.0f;
+	camera->SetPos(camera->GetPos() * float3(1.0f, -1.0f, 1.0f));
 	camera->Update();
 
 	reflectRight = camera->right;