9066f4aa01ca398b8466036aa64da5934d32ddea
jK <-jk->
2014-02-06 16:17:53 +0100

    cleanup in ArchiveScanner (nlc)

--- rts/System/FileSystem/ArchiveScanner.h
+++ rts/System/FileSystem/ArchiveScanner.h
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <vector>
+#include <list>
 #include <map>
 #include "System/Info.h"
 
@@ -178,7 +179,7 @@ private:
 
 private:
 	void ScanDirs(const std::vector<std::string>& dirs, bool checksum = false);
-	void Scan(const std::string& curPath, bool doChecksum);
+	void Scan(const std::string& curPath, std::list<std::string>* foundArchives);
 
 	/// scan mapinfo / modinfo lua files
 	bool ScanArchiveLua(IArchive* ar, const std::string& fileName, ArchiveInfo& ai, std::string& err);
