4e6abb9d703b1d54fd1f839a4115e4e2274d7aa9
jK <-jk->
2014-02-18 17:26:16 +0100

    new & default (!) LogLevel: L_NOTICE, same as L_INFO but it always gets outputed even when a logSection is set

--- rts/System/Log/LogUtil.c
+++ rts/System/Log/LogUtil.c
@@ -10,6 +10,7 @@ const char* log_util_levelToString(int level) {
 	switch (level) {
 		case LOG_LEVEL_DEBUG:   return "Debug";
 		case LOG_LEVEL_INFO:    return "Info";
+		case LOG_LEVEL_NOTICE:  return "Notice";
 		case LOG_LEVEL_WARNING: return "Warning";
 		case LOG_LEVEL_ERROR:   return "Error";
 		case LOG_LEVEL_FATAL:   return "Fatal";
@@ -22,11 +23,23 @@ char log_util_levelToChar(int level) {
 	switch (level) {
 		case LOG_LEVEL_DEBUG:   return 'D';
 		case LOG_LEVEL_INFO:    return 'I';
+		case LOG_LEVEL_NOTICE:  return 'N';
 		case LOG_LEVEL_WARNING: return 'W';
 		case LOG_LEVEL_ERROR:   return 'E';
 		case LOG_LEVEL_FATAL:   return 'F';
 		default:                return '?';
 	}
+}
+
+int log_util_getNearestLevel(int level) {
+
+	if (level >= LOG_LEVEL_FATAL)   return LOG_LEVEL_FATAL;
+	if (level >= LOG_LEVEL_ERROR)   return LOG_LEVEL_ERROR;
+	if (level >= LOG_LEVEL_WARNING) return LOG_LEVEL_WARNING;
+	if (level >= LOG_LEVEL_NOTICE)  return LOG_LEVEL_NOTICE;
+	if (level >= LOG_LEVEL_INFO)    return LOG_LEVEL_INFO;
+	if (level >= LOG_LEVEL_DEBUG)   return LOG_LEVEL_DEBUG;
+	return DEFAULT_LOG_LEVEL;
 }
 
 const char* log_util_prepareSection(const char* section) {
