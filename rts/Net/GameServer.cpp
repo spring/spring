f4420470da30ff5f5f301bcdcca374b478560a4a
jK <-jk->
2014-03-24 21:34:04 +0100

    fix 100% cpu usage in netcode thread

--- rts/Net/GameServer.cpp
+++ rts/Net/GameServer.cpp
@@ -2503,8 +2503,9 @@ void CGameServer::UpdateLoop()
 		Threading::SetThreadName("netcode");
 		Threading::SetAffinity(~0);
 
+		//FIXME use async callback funcs in boost udp sockets and so get rid of any latency & remove netcode thread
 		while (!quitServer) {
-			spring_sleep(spring_msecs(1));
+			spring_sleep(spring_msecs(5));
 
 			if (UDPNet)
 				UDPNet->Update();
@@ -2513,6 +2514,7 @@ void CGameServer::UpdateLoop()
 			ServerReadNet();
 			Update();
 		}
+
 
 		if (hostif)
 			hostif->SendQuit();
@@ -2781,7 +2783,7 @@ void CGameServer::InternalSpeedChange(float newSpeed)
 
 void CGameServer::UserSpeedChange(float newSpeed, int player)
 {
-	newSpeed = std::min(maxUserSpeed, std::max(newSpeed, minUserSpeed));
+	newSpeed = Clamp(newSpeed, minUserSpeed, maxUserSpeed);
 
 	if (userSpeedFactor != newSpeed) {
 		if (internalSpeed > newSpeed || internalSpeed == userSpeedFactor) // insta-raise speed when not slowed down
