b5133af82c013bc267d2354aeca669e4ec72e909
abma <spring@abma.de>
2014-05-04 12:02:57 +0200

    unused config vars--

--- rts/Rendering/UnitDrawer.cpp
+++ rts/Rendering/UnitDrawer.cpp
@@ -64,8 +64,6 @@ CUnitDrawer* unitDrawer;
 CONFIG(int, UnitLodDist).defaultValue(1000);
 CONFIG(int, UnitIconDist).defaultValue(200);
 CONFIG(float, UnitTransparency).defaultValue(0.7f);
-CONFIG(bool, MultiThreadDrawUnit).defaultValue(true);
-CONFIG(bool, MultiThreadDrawUnitShadow).defaultValue(true);
 
 CONFIG(int, MaxDynamicModelLights)
 	.defaultValue(1)
