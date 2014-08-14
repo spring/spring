9052bf05bcabe9dbaaa03329b41129fbc1ad2fe1
abma <spring@abma.de>
2014-08-14 02:36:31 +0200

    add missing file

---
+++ rts/System/Sound/Null/SoundChannels.cpp
@@ -0,0 +1,12 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#include "NullAudioChannel.h"
+
+namespace Channels
+{
+	NullAudioChannel BGMusic;
+	NullAudioChannel General;
+	NullAudioChannel Battle;
+	NullAudioChannel UnitReply;
+	NullAudioChannel UserInterface;
+}
