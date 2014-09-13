spring/spring

commit 534d1887ab3b3e5aa7286e72d2c122b6c98a2a1f

fix a delete vs. delete[] bug

rts/System/Log/DefaultFilter.cpp
@@ -274,7 +274,7 @@ std::set<const char*> log_filter_section_getRegisteredSet()
 
 const char* log_filter_section_getSectionCString(const char* section_cstr_tmp)
 {
-	static std::unordered_map<std::string, std::unique_ptr<const char>> cache;
+	static std::unordered_map<std::string, std::unique_ptr<const char[]>> cache;
 
 	const auto str = std::string(section_cstr_tmp);
 	const auto it = cache.find(str);

commit 7e117ee0d026dcc057d9ee8fe6e249e49c849900

probably fix linux-static build error caused by 534d188
see http://stackoverflow.com/questions/14873577/from-unique-ptrchar-to-unique-ptrconst-char

rts/System/Log/DefaultFilter.cpp
@@ -287,6 +287,6 @@ const char* log_filter_section_getSectionCString(const char* section_cstr_tmp)
 	strcpy(&section_cstr[0], section_cstr_tmp);
 	section_cstr[str.size()] = '\0';
 
-	cache[str].reset(section_cstr);
+	cache[str].reset(const_cast<const char*>(section_cstr));
 	return section_cstr;
 }