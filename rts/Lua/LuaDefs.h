2c68a3a6876d5071877a7b2d44a5540c09b5b472
jK <-jk->
2014-09-24 13:39:56 +0200

    another attempt to fix "GetDataType" clang warning

--- rts/Lua/LuaDefs.h
+++ rts/Lua/LuaDefs.h
@@ -46,11 +46,11 @@ namespace {
 		assert(valid_type);
 		return ERROR_TYPE;
 	}
-	static DataType GetDataType(unsigned)    { return INT_TYPE; }
-	static DataType GetDataType(int)         { return INT_TYPE; }
-	static DataType GetDataType(bool)        { return BOOL_TYPE; }
-	static DataType GetDataType(float)       { return FLOAT_TYPE; }
-	static DataType GetDataType(const std::string&) { return STRING_TYPE; }
+	template<> DataType GetDataType(unsigned)           { return INT_TYPE; }
+	template<> DataType GetDataType(int)                { return INT_TYPE; }
+	template<> DataType GetDataType(bool)               { return BOOL_TYPE; }
+	template<> DataType GetDataType(float)              { return FLOAT_TYPE; }
+	template<> DataType GetDataType(const std::string&) { return STRING_TYPE; }
 }
 
 #define ADDRESS(name) ((const char *)&name)
