8a3b61d9707e93ba6bfc058047b103f94014ad1b
hoijui <hoijui.quaero@gmail.com>
2014-10-10 11:35:20 +0200

    Java-AIInterface: add new and improve doc comments & formatting [minor]

--- AI/Interfaces/Java/src/main/native/JavaBridge.c
+++ AI/Interfaces/Java/src/main/native/JavaBridge.c
@@ -37,7 +37,15 @@ static struct Properties* jvmCfgProps = NULL;
 static size_t  skirmishAIId_sizeMax = 0;
 static size_t* skirmishAIId_skirmishAiImpl;
 
+/**
+ * Marks the actual storage capacity limit for AI implementations.
+ * There can not be more then this many implementations in use at any time.
+ */
 static size_t   skirmishAiImpl_sizeMax = 0;
+/**
+ * No more implementations can be found at indices equal or greater to this one,
+ * though there can also be free indices lower then this one.
+ */
 static size_t   skirmishAiImpl_size = 0;
 static char**   skirmishAiImpl_className;
 static jobject* skirmishAiImpl_instance;
@@ -1162,7 +1170,7 @@ bool java_initSkirmishAIClass(
 	}
 	// sai is now either the instantiated one, or a free one
 
-	// instantiate AI (if needed)
+	// instantiate AI (if not already instantiated)
 	assert(sai < skirmishAiImpl_size);
 	if (skirmishAiImpl_className[sai] == NULL) {
 		sai = firstFree;
@@ -1206,13 +1214,14 @@ bool java_releaseSkirmishAIClass(const char* className) {
 	size_t sai;
 	for (sai = 0; sai < skirmishAiImpl_size; ++sai) {
 		if ((skirmishAiImpl_className[sai] != NULL) &&
-				(strcmp(skirmishAiImpl_className[sai], className) == 0)) {
+				(strcmp(skirmishAiImpl_className[sai], className) == 0))
+		{
 			break;
 		}
 	}
 	// sai is now either the instantiated one, or a free one
 
-	// release AI (if needed)
+	// release AI (if its instance was found)
 	assert(sai < skirmishAiImpl_size);
 	if (skirmishAiImpl_className[sai] != NULL) {
 		java_establishJavaEnv();
