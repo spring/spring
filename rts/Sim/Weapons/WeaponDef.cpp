d919c62b106e30b6d98399dc3535aef4469634fe
GoogleFrog <googl3frog@gmail.com>
2015-01-08 00:42:28 +1100

    Added fire tolerance tag to weapondefs. Based on https://github.com/spring/spring/commit/226dba6089d1471f5bd61e47f5e21771dfa2d0bc Note that this does not fix burst weapons but that case is much less frequent.

--- rts/Sim/Weapons/WeaponDef.cpp
+++ rts/Sim/Weapons/WeaponDef.cpp
@@ -91,6 +91,7 @@ WEAPONTAG(float, cylinderTargeting).fallbackName("cylinderTargetting").defaultVa
 WEAPONTAG(bool, turret).defaultValue(false).description("Does the unit aim within an arc (up-to and including full 360° turret traverse) or always aim along the owner's heading?");
 WEAPONTAG(bool, fixedLauncher).defaultValue(false);
 WEAPONTAG(float, maxAngle).externalName("tolerance").defaultValue(3000.0f).scaleValue(180.0f / COBSCALEHALF);
+WEAPONTAG(float, maxFireAngle).externalName("firetolerance").defaultValue(16384.0f).scaleValue(180.0f / COBSCALEHALF);
 WEAPONTAG(int, highTrajectory).defaultValue(2);
 WEAPONTAG(float, trajectoryHeight).defaultValue(0.0f);
 WEAPONTAG(bool, tracks).defaultValue(false);
