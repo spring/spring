fdfc3993fae1daf02be86c1a8090e862c1eb7759
jK <-jk->
2015-01-11 15:44:31 +0100

    fix #4494: unitshapes needs an alpha channel in the screen buffer

--- rts/System/SpringApp.cpp
+++ rts/System/SpringApp.cpp
@@ -327,7 +327,7 @@ bool SpringApp::CreateSDLWindow(const char* title)
 	SDL_GL_SetAttribute(SDL_GL_RED_SIZE,   8);
 	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,  8);
-	//SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24);
 	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
