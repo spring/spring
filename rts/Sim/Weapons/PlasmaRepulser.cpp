a091bd2eb6332f1acfc1400607f894d2cecead34
GoogleFrog <googl3frog@gmail.com>
2015-03-18 23:08:06 +1100

    Fixed allyTeam check in PlasmaRepulser.

--- rts/Sim/Weapons/PlasmaRepulser.cpp
+++ rts/Sim/Weapons/PlasmaRepulser.cpp
@@ -208,7 +208,7 @@ void CPlasmaRepulser::NewProjectile(CWeaponProjectile* p)
 	// due to isShield being a separate tag (in 91.0)
 	assert(weaponDef->isShield);
 
-	if (weaponDef->smartShield && teamHandler->IsValidAllyTeam(p->GetAllyteamID()) && teamHandler->AlliedTeams(p->GetAllyteamID(), owner->team)) {
+	if (weaponDef->smartShield && teamHandler->IsValidAllyTeam(p->GetAllyteamID()) && teamHandler->Ally(p->GetAllyteamID(), owner->allyteam)) {
 		return;
 	}
 
