spring/spring

commit d35221e3e6542c1afa122451cdf89038cf966dbb

minor

rts/Sim/Misc/QuadField.cpp
@@ -136,22 +136,18 @@ std::vector<int> CQuadField::GetQuads(float3 pos, const float radius)
 	pos.ClampInBounds();
 	std::vector<int> ret;
 
+	const int2 min = WorldPosToQuadField(pos - radius);
+	const int2 max = WorldPosToQuadField(pos + radius) + int2(1,1);
+
+	if (max.y < min.y || max.x < min.x)
+		return ret;
+
 	// qsx and qsz are always equal
 	const float maxSqLength = (radius + quadSizeX * 0.72f) * (radius + quadSizeZ * 0.72f);
 
-	const int maxx = std::min(int(pos.x + radius) / quadSizeX + 1, numQuadsX - 1);
-	const int maxz = std::min(int(pos.z + radius) / quadSizeZ + 1, numQuadsZ - 1);
-
-	const int minx = std::max(int(pos.x - radius) / quadSizeX, 0);
-	const int minz = std::max(int(pos.z - radius) / quadSizeZ, 0);
-
-	if (maxz < minz || maxx < minx) {
-		return ret;
-	}
-
-	ret.reserve((maxz - minz) * (maxx - minx));
-	for (int z = minz; z <= maxz; ++z) {
-		for (int x = minx; x <= maxx; ++x) {
+	ret.reserve((max.y - min.y) * (max.x - min.x));
+	for (int z = min.y; z <= max.y; ++z) {
+		for (int x = min.x; x <= max.x; ++x) {
 			assert(x < numQuadsX);
 			assert(z < numQuadsZ);
 			const float3 quadPos = float3(x * quadSizeX + quadSizeX * 0.5f, 0, z * quadSizeZ + quadSizeZ * 0.5f);
@@ -165,24 +161,21 @@ std::vector<int> CQuadField::GetQuads(float3 pos, const float radius)
 }
 
 
-std::vector<int> CQuadField::GetQuadsRectangle(const float3 pos1, const float3 pos2)
+std::vector<int> CQuadField::GetQuadsRectangle(const float3 mins, const float3 maxs)
 {
-	pos1.AssertNaNs();
-	pos2.AssertNaNs();
+	mins.AssertNaNs();
+	maxs.AssertNaNs();
 	std::vector<int> ret;
 
-	const int maxx = Clamp(int(pos2.x) / quadSizeX + 1, 0, numQuadsX - 1);
-	const int maxz = Clamp(int(pos2.z) / quadSizeZ + 1, 0, numQuadsZ - 1);
+	const int2 min = WorldPosToQuadField(mins);
+	const int2 max = WorldPosToQuadField(maxs) + int2(1,1);
 
-	const int minx = Clamp(int(pos1.x) / quadSizeX, 0, numQuadsX - 1);
-	const int minz = Clamp(int(pos1.z) / quadSizeZ, 0, numQuadsZ - 1);
-
-	if (maxz < minz || maxx < minx)
+	if (max.y < min.y || max.x < min.x)
 		return ret;
 
-	ret.reserve((maxz - minz) * (maxx - minx));
-	for (int z = minz; z <= maxz; ++z) {
-		for (int x = minx; x <= maxx; ++x) {
+	ret.reserve((max.y - min.y) * (max.x - min.x));
+	for (int z = min.y; z <= max.y; ++z) {
+		for (int x = min.x; x <= max.x; ++x) {
 			ret.push_back(z * numQuadsX + x);
 		}
 	}

rts/Sim/Misc/GlobalConstants.h
@@ -8,7 +8,7 @@
  *
  * Defines the size of 1 heightmap square as 8 elmos.
  */
-const int SQUARE_SIZE = 8;
+static constexpr int SQUARE_SIZE = 8;
 
 /**
  * @brief footprint scale
@@ -18,33 +18,33 @@ const int SQUARE_SIZE = 8;
  * blocking-map is twice that of TA's; a "TA-footprint" square covers
  * SQUARE_SIZE*2 x SQUARE_SIZE*2 elmos.
  */
-const int SPRING_FOOTPRINT_SCALE = 2;
+static constexpr int SPRING_FOOTPRINT_SCALE = 2;
 
 /**
  * conversion factor from elmos to meters
  */
-const float ELMOS_TO_METERS = 1.0f / SQUARE_SIZE;
+static constexpr float ELMOS_TO_METERS = 1.0f / SQUARE_SIZE;
 
 /**
  * @brief game speed
  *
  * Defines the game-/sim-frames per second.
  */
-const int GAME_SPEED = 30;
+static constexpr int GAME_SPEED = 30;
 
 /**
  * @brief unit SlowUpdate rate
  *
  * Defines the interval of SlowUpdate calls in sim-frames.
  */
-const int UNIT_SLOWUPDATE_RATE = 15;
+static constexpr int UNIT_SLOWUPDATE_RATE = 15;
 
 /**
  * @brief team SlowUpdate rate
  *
  * Defines the interval of CTeam::SlowUpdate calls in sim-frames.
  */
-const int TEAM_SLOWUPDATE_RATE = 30;
+static constexpr int TEAM_SLOWUPDATE_RATE = 30;
 
 /**
  * @brief max teams
@@ -52,7 +52,7 @@ const int TEAM_SLOWUPDATE_RATE = 30;
  * Defines the maximum number of teams as 255
  * (254 real teams, and an extra slot for the GAIA team).
  */
-const int MAX_TEAMS = 255;
+static constexpr int MAX_TEAMS = 255;
 
 /**
  * @brief max players
@@ -62,7 +62,7 @@ const int MAX_TEAMS = 255;
  * in the network, which is 1 byte (=> 255), plus 4 slots reserved for special
  * purposes, resulting in 251.
  */
-const int MAX_PLAYERS = 251;
+static constexpr int MAX_PLAYERS = 251;
 
 /**
  * @brief max AIs
@@ -72,7 +72,7 @@ const int MAX_PLAYERS = 251;
  * in the network, which is 1 byte (=> 256), with the value 255 reserved for
  * special purpose, resulting in 255.
  */
-const int MAX_AIS = 255;
+static constexpr int MAX_AIS = 255;
 
 /**
  * @brief max units
@@ -83,46 +83,46 @@ const int MAX_AIS = 255;
  * transmits unit-IDs as signed shorts. The effective global unit limit is
  * stored in UnitHandler::maxUnits, and is always clamped to this value.
  */
-const int MAX_UNITS = 32000;
+static constexpr int MAX_UNITS = 32000;
 
 /**
  * @brief max weapons per unit
  *
  * Defines the maximum weapons per single unit type as 32.
  */
-const int MAX_WEAPONS_PER_UNIT = 32;
+static constexpr int MAX_WEAPONS_PER_UNIT = 32;
 
 /**
  * @brief randint max
  *
  * Defines the maximum random integer as 0x7fff.
  */
-const int RANDINT_MAX = 0x7fff;
+static constexpr int RANDINT_MAX = 0x7fff;
 
 /**
  * maximum speed (elmos/frame) a unit is allowed to have outside the map
  */
-const float MAX_UNIT_SPEED = 1e3f;
+static constexpr float MAX_UNIT_SPEED = 1e3f;
 
 /**
  * maximum impulse strength an explosion is allowed to impart on a unit
  */
-const float MAX_EXPLOSION_IMPULSE = 1e4f;
+static constexpr float MAX_EXPLOSION_IMPULSE = 1e4f;
 
 /**
  * if explosion distance is less than speed of explosion multiplied by
  * this factor, units are damaged directly rather than N>=1 frames later
  */
-const float DIRECT_EXPLOSION_DAMAGE_SPEED_SCALE = 4.0f;
+static constexpr float DIRECT_EXPLOSION_DAMAGE_SPEED_SCALE = 4.0f;
 
 /**
  * maximum range of a weapon-projectile with a flight-time member
  */
-const float MAX_PROJECTILE_RANGE = 1e6f;
+static constexpr float MAX_PROJECTILE_RANGE = 1e6f;
 
 /**
  * maximum absolute height a projectile is allowed to reach
  */
-const float MAX_PROJECTILE_HEIGHT = 1e6f;
+static constexpr float MAX_PROJECTILE_HEIGHT = 1e6f;
 
 #endif // _GLOBAL_CONSTANTS_H

rts/Sim/Misc/QuadField.h
@@ -35,7 +35,7 @@ needed to support dynamic resizing (not used yet)
 	~CQuadField();
 
 	std::vector<int> GetQuads(float3 pos, const float radius);
-	std::vector<int> GetQuadsRectangle(const float3 pos1, const float3 pos2);
+	std::vector<int> GetQuadsRectangle(const float3 mins, const float3 maxs);
 	std::vector<int> GetQuadsOnRay(const float3 start, const float3 dir, const float length);
 
 	void GetUnitsAndFeaturesColVol(

commit a626219ab189b1fe0a27f6a14fd93d82cd467bdb

fix QuadField assert regression introduced in d35221e

rts/Sim/Misc/QuadField.cpp
@@ -137,7 +137,7 @@ std::vector<int> CQuadField::GetQuads(float3 pos, const float radius)
 	std::vector<int> ret;
 
 	const int2 min = WorldPosToQuadField(pos - radius);
-	const int2 max = WorldPosToQuadField(pos + radius) + int2(1,1);
+	const int2 max = WorldPosToQuadField(pos + radius);
 
 	if (max.y < min.y || max.x < min.x)
 		return ret;
@@ -168,7 +168,7 @@ std::vector<int> CQuadField::GetQuadsRectangle(const float3 mins, const float3 m
 	std::vector<int> ret;
 
 	const int2 min = WorldPosToQuadField(mins);
-	const int2 max = WorldPosToQuadField(maxs) + int2(1,1);
+	const int2 max = WorldPosToQuadField(maxs);
 
 	if (max.y < min.y || max.x < min.x)
 		return ret;
@@ -176,6 +176,8 @@ std::vector<int> CQuadField::GetQuadsRectangle(const float3 mins, const float3 m
 	ret.reserve((max.y - min.y) * (max.x - min.x));
 	for (int z = min.y; z <= max.y; ++z) {
 		for (int x = min.x; x <= max.x; ++x) {
+			assert(x < numQuadsX);
+			assert(z < numQuadsZ);
 			ret.push_back(z * numQuadsX + x);
 		}
 	}