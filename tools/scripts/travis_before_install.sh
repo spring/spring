ebe4640e388459428dcedfc4349bb0222b49feed
abma <spring@abma.de>
2015-05-06 23:53:14 +0200

    fix travis: sudo doesn't preserve env-variables (in this case CXX)

--- tools/scripts/travis_before_install.sh
+++ tools/scripts/travis_before_install.sh
@@ -5,11 +5,11 @@
 set -e
 
 # sdl2
-add-apt-repository ppa:bartbes/love-stable --yes
+sudo add-apt-repository ppa:bartbes/love-stable --yes
 # gcc 4.7
 if [ "$CXX" = "g++" ]; then
-	add-apt-repository ppa:ubuntu-toolchain-r/test --yes
+	sudo add-apt-repository ppa:ubuntu-toolchain-r/test --yes
 fi
 
-apt-get update -qq
+sudo apt-get update -qq
 
