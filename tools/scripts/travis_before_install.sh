f5ce371947edd928915adbb8b28004b9497c7d62
abma <spring@abma.de>
2015-05-06 11:26:14 +0200

    only build engine-legacy / fix typo

--- tools/scripts/travis_before_install.sh
+++ tools/scripts/travis_before_install.sh
@@ -7,7 +7,7 @@ set -e
 # sdl2
 add-apt-repository ppa:bartbes/love-stable --yes
 # gcc 4.7
-if [ "$CXX" == "g++" ]; then
+if [ "$CXX" = "g++" ]; then
 	add-apt-repository ppa:ubuntu-toolchain-r/test --yes
 fi
 
