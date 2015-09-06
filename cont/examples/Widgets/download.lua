83c1a5b9464eb0d36c9a9f8cb149d1c90e3d124f
abma <spring@abma.de>
2015-09-06 19:02:00 +0200

    add all dl events to example download-widget

--- cont/examples/Widgets/download.lua
+++ cont/examples/Widgets/download.lua
@@ -14,3 +14,24 @@ function widget:Initialize()
 	Spring.Echo("Starting to download")
 	VFS.DownloadArchive("ba:stable", "game")
 end
+
+function widget:DownloadStarted(id)
+	Spring.Echo("download finished" .. id)
+end
+
+function widget:DownloadQueued(id)
+	Spring.Echo("download finished" .. id)
+end
+
+function widget:DownloadFinished(id)
+	Spring.Echo("download finished" .. id)
+end
+
+function widget:DownloadFailed(id, errorid)
+	Spring.Echo("download failed" .. id .. errorid)
+end
+
+function widget:DownloadProgress(id, downloaded, total)
+	Spring.Echo("download progress" .. id)
+end
+
