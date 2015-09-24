7d0e44cdc599c311957608c66f33240a3c362b51
abma <spring@abma.de>
2015-09-24 20:39:13 +0200

    buildbot: add dummy script to generate docs

---
+++ buildbot/slave/create-doc.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# generates documentation from springs source code
+set -e
+
+DEST=$(mktemp -d)
+mkdir $DEST/engine
+mkdir $DEST/unitsync
+
+echo $DEST
+(
+cat Doxyfile
+echo "OUTPUT_DIRECTORY = $DEST/engine"
+echo "HTML_OUTPUT = ./"
+) | doxygen -
+echo $DEST
+
+#FIXME: find a way to directly generate in $DEST
+cd AI/Interfaces/Java/bin
+ant doc
+cd -
+mv dist/AI/Interfaces/Java/0.1/doc/jdoc/ $DEST/Java
+
+cd AI/Wrappers/JavaOO/bin
+ant doc
+cd -
+mv dist/AI/Wrappers/JavaOO/doc/jdoc/ $DEST/JavaOO
+
+cd tools/unitsync
+(
+cat Doxyfile
+echo "OUTPUT_DIRECTORY = $DEST/engine"
+echo "HTML_OUTPUT = ./"
+) | doxygen -
+
+echo "Wrote doc to $DEST"
+
