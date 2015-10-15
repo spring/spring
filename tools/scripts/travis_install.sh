6b82864e71179be368a7082419a80a06952d5b5f
abma <spring@abma.de>
2015-10-15 18:27:40 +0200

    Revert "Revert "Added lexical_cast for number->string conversions""
    This reverts part of commit e598a4a37831959473c5054fe2deff6ddf7d7814:
    
    try to fix clang-compile

--- tools/scripts/travis_install.sh
+++ tools/scripts/travis_install.sh
@@ -13,7 +13,7 @@ fi
 sudo apt-get install libglew-dev libsdl2-dev libdevil-dev libopenal-dev libogg-dev \
   libvorbis-dev libfreetype6-dev p7zip-full libxcursor-dev libunwind7-dev
 
-sudo apt-get install libboost-thread1.48-dev libboost-regex1.48-dev libboost-system1.48-dev \
-  libboost-program-options1.48-dev libboost-signals1.48-dev libboost-chrono1.48-dev \
-  libboost-filesystem1.48-dev libboost-test1.48-dev binutils-gold cmake cmake-data
+sudo apt-get install libboost-thread1.55-dev libboost-regex1.55-dev libboost-system1.55-dev \
+  libboost-program-options1.55-dev libboost-signals1.55-dev libboost-chrono1.55-dev \
+  libboost-filesystem1.55-dev libboost-test1.55-dev binutils-gold cmake cmake-data
 
