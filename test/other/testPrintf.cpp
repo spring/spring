fc6d3dd276e5ba75c313b4801b0f26b3c720d58f
jK <-jk->
2015-10-16 13:59:47 +0200

    printf UT: add 1.6 test

--- test/other/testPrintf.cpp
+++ test/other/testPrintf.cpp
@@ -3,8 +3,8 @@
 #include "System/Log/ILog.h"
 //FIXME #include "System/Sync/FPUCheck.h"
 
-#define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
+#include <map>
 
 #define BOOST_TEST_MODULE Printf
 #include <boost/test/unit_test.hpp>
@@ -15,32 +15,34 @@ BOOST_AUTO_TEST_CASE( Printf )
 {
 	//FIXME good_fpu_init();
 	char s[32];
+	constexpr const char* FMT_STRING = "%.14g";
+	std::map<float, const char*> testNumbers = {
+		{1e16, "1.0000000272564e+16"},
+		{4266.38916015625, "4266.3891601562"},
+		{1.6, "1.6000000238419"},
+	};
+
 
 #ifdef _WIN32
 	_set_output_format(_TWO_DIGIT_EXPONENT);
-	__mingw_sprintf(s, "%.14g", 1e16);
-	LOG("%s",s);
-	BOOST_CHECK(strcmp(s, "1.0000000272564e+16") == 0);
-
-	__mingw_sprintf(s, "%.14g", 4266.38916015625);
-	LOG("%s",s);
-	BOOST_CHECK(strcmp(s, "4266.3891601562") == 0); //prints 4266.3891601563
+	for (const auto& p: testNumbers) {
+		__mingw_sprintf(s, FMT_STRING, p.first);
+		LOG("%s",s);
+		BOOST_CHECK(strcmp(s, p.second) == 0);
+	}
 #endif
 
-	sprintf(s, "%.14g", 1e16);
-	LOG("%s",s);
-	BOOST_CHECK(strcmp(s, "1.0000000272564e+16") == 0);
 
-	sprintf(s, "%.14g", 4266.38916015625);
-	LOG("%s",s);
-	BOOST_CHECK(strcmp(s, "4266.3891601562") == 0);
+	for (const auto& p: testNumbers) {
+		sprintf(s, FMT_STRING, p.first);
+		LOG("%s",s);
+		BOOST_CHECK(strcmp(s, p.second) == 0);
+	}
 
 
-	__builtin_sprintf(s, "%.14g", 1e16);
-	LOG("%s",s);
-	BOOST_CHECK(strcmp(s, "1.0000000272564e+16") == 0);
-
-	__builtin_sprintf(s, "%.14g", 4266.38916015625);
-	LOG("%s",s);
-	BOOST_CHECK(strcmp(s, "4266.3891601562") == 0);
+	for (const auto& p: testNumbers) {
+		__builtin_sprintf(s, FMT_STRING, p.first);
+		LOG("%s",s);
+		BOOST_CHECK(strcmp(s, p.second) == 0);
+	}
 }
