26be1b40c19b99eba5b98780b2fe02ad129f9802
Eshed <ashdnazg@gmail.com>
2015-10-16 12:27:31 +0300

    Add rounding test case to printf

--- test/other/testPrintf.cpp
+++ test/other/testPrintf.cpp
@@ -16,12 +16,19 @@ BOOST_AUTO_TEST_CASE( Printf )
 	char s[32];
 
 #ifdef _WIN32
+	_set_output_format(_TWO_DIGIT_EXPONENT);
 	__mingw_sprintf(s, "%.14g", 1e16);
 	LOG("%s",s);
 	BOOST_CHECK(strcmp(s, "1.0000000272564e+16") == 0);
+	__mingw_sprintf(s, "%.14g", 4266.38916015625);
+	LOG("%s",s);
+	BOOST_CHECK(strcmp(s, "4266.3891601562") == 0); //prints 4266.3891601563
 #endif
 
 	sprintf(s, "%.14g", 1e16);
 	LOG("%s",s);
 	BOOST_CHECK(strcmp(s, "1.0000000272564e+16") == 0);
+	__mingw_sprintf(s, "%.14g", 4266.38916015625);
+	LOG("%s",s);
+	BOOST_CHECK(strcmp(s, "4266.3891601562") == 0);
 }
