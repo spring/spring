5c2e5d7748f69cd24c41a8308cd95c67880cc3b3
Eshed <ashdnazg@gmail.com>
2015-10-17 00:12:30 +0300

    Fix clang travis build
    clang uses the existing gcc's standard libs
    so gcc 4.7 is needed for C++11 support

--- tools/scripts/travis_before_install.sh
+++ tools/scripts/travis_before_install.sh
@@ -9,9 +9,7 @@ sudo add-apt-repository --yes ppa:bartbes/love-stable
 # cmake
 sudo add-apt-repository --yes ppa:george-edison55/precise-backports
 # gcc 4.7
-if [ "$CXX" = "g++" ]; then
-	sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-fi
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 # boost
 sudo add-apt-repository --yes ppa:boost-latest/ppa
 
