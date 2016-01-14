6f9e5f56132e8545883fcca6e47567e0f80d53a1
Eshed <ashdnazg@gmail.com>
2016-01-14 03:29:21 +0200

    Fix headless compilation

--- rts/Rendering/Textures/S3OTextureHandler.cpp
+++ rts/Rendering/Textures/S3OTextureHandler.cpp
@@ -1,6 +1,7 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 
+#include "Rendering/GL/myGL.h"
 #include "S3OTextureHandler.h"
 
 #include "System/FileSystem/FileHandler.h"
