spring/spring

commit d92b3196d9d0fca573fd97bf02914ca22fa1d9b5

move ShatterParts generation to model loading so creating & rendering FlyingPieces is now `for free` giving free important cpu resources when they are needed (-> many explosions after a nuke or powerplant explosion)

rts/Rendering/Models/3DModel.h
@@ -71,25 +71,30 @@ struct S3DModelPiece {
 	S3DModelPiece();
 	virtual ~S3DModelPiece();
 
-	virtual void DrawForList() const = 0;
-	virtual void UploadGeometryVBOs() = 0;
-
-	virtual void BindVertexAttribVBOs() const = 0;
-	virtual void UnbindVertexAttribVBOs() const = 0;
-
 	virtual float3 GetEmitPos() const;
 	virtual float3 GetEmitDir() const;
 
+	// internal use
 	virtual unsigned int GetVertexCount() const = 0;
 	virtual unsigned int GetVertexDrawIndexCount() const = 0;
-
 	virtual const float3& GetVertexPos(const int) const = 0;
 	virtual const float3& GetNormal(const int) const = 0;
 
-	virtual void Shatter(float, int, int, const float3, const float3, const CMatrix44f&) const = 0;
+	virtual void UploadGeometryVBOs() = 0;
+	virtual void BindVertexAttribVBOs() const = 0;
+	virtual void UnbindVertexAttribVBOs() const = 0;
+
+protected:
+	virtual void DrawForList() const = 0;
+	virtual const std::vector<unsigned>& GetVertexIndices() const = 0;
 
 public:
-	unsigned int CreateDrawForList() const;
+	void DrawStatic() const; //< draw piece and children statically (ie. without script-transforms)
+	void CreateDispList();
+	unsigned int GetDisplayListID() const { return dispListID; }
+
+	void CreateShatterPieces();
+	void Shatter(float, int, int, const float3, const float3, const CMatrix44f&) const;
 
 	CMatrix44f& ComposeRotation(CMatrix44f& m, const float3& r) const {
 		switch (axisMapType) {
@@ -129,8 +134,12 @@ public:
 		return isIdentity;
 	}
 
-	// draw piece and children statically (ie. without script-transforms)
-	void DrawStatic() const;
+	void SetModelMatrix(const CMatrix44f& m) {
+		// assimp only
+		bakedRotMatrix = m;
+		hasIdentityRot = m.IsIdentity();
+		assert(m.IsOrthoNormal() == 0);
+	}
 
 	void SetCollisionVolume(const CollisionVolume& cv) { colvol = cv; }
 	const CollisionVolume* GetCollisionVolume() const { return &colvol; }
@@ -139,13 +148,11 @@ public:
 	unsigned int GetChildCount() const { return children.size(); }
 	S3DModelPiece* GetChild(unsigned int i) const { return children[i]; }
 
-	unsigned int GetDisplayListID() const { return dispListID; }
-	void SetDisplayListID(unsigned int id) { dispListID = id; }
-
 	bool HasGeometryData() const { return GetVertexDrawIndexCount() >= 3; }
 	bool HasIdentityRotation() const { return hasIdentityRot; }
 
-	void SetHasIdentityRotation(bool b) { hasIdentityRot = b; }
+private:
+	void CreateShatterPiecesVariation(const int num);
 
 public:
 	std::string name;
@@ -156,8 +163,6 @@ public:
 
 	AxisMappingType axisMapType;
 
-	CMatrix44f bakedRotMatrix; /// baked local-space rotations (assimp-only)
-
 	float3 offset;             /// local (piece-space) offset wrt. parent piece
 	float3 goffset;            /// global (model-space) offset wrt. root piece
 	float3 scales;             /// baked uniform scaling factors (assimp-only)
@@ -166,12 +171,24 @@ public:
 	float3 rotAxisSigns;
 
 protected:
+	CMatrix44f bakedRotMatrix; /// baked local-space rotations (assimp-only)
 	bool hasIdentityRot;       /// if bakedRotMatrix is identity
 
 	unsigned int dispListID;
 
 	VBO vboIndices;
 	VBO vboAttributes;
+
+public:
+	struct ShatterPartData {
+		float3 dir;
+		size_t vboOffset;
+		size_t indexCount;
+	};
+	static const int SHATTER_MAX_PARTS  = 10;
+	static const int SHATTER_VARIATIONS = 2;
+	VBO vboShatterIndices;
+	std::array<std::vector<ShatterPartData>, SHATTER_VARIATIONS> shatterParts;
 };
 
 

rts/Rendering/Models/AssParser.cpp
@@ -8,7 +8,6 @@
 
 #include "Lua/LuaParser.h"
 #include "Sim/Misc/CollisionVolume.h"
-#include "Sim/Projectiles/ProjectileHandler.h"
 #include "System/Util.h"
 #include "System/Log/ILog.h"
 #include "System/Platform/errorhandler.h"
@@ -353,15 +352,16 @@ void CAssParser::LoadPieceTransformations(
 	//   .dae  : x=Rgt, y=-Fwd, z= Up, as=(-1, -1, 1), am=AXIS_XZY (if Z_UP)
 	//   .dae  : x=Rgt, y=-Fwd, z= Up, as=(-1, -1, 1), am=AXIS_XZY (if Y_UP) [!?]
 	//   .blend: ????
-	piece->bakedRotMatrix = aiMatrixToMatrix(aiMatrix4x4t<float>(aiRotateQuat.GetMatrix()));
+	CMatrix44f bakedMatrix;
+	bakedMatrix = aiMatrixToMatrix(aiMatrix4x4t<float>(aiRotateQuat.GetMatrix()));
 
 	if (piece == model->GetRootPiece()) {
-		const float3 xaxis = pieceTable.GetFloat3("xaxis", piece->bakedRotMatrix.GetX());
-		const float3 yaxis = pieceTable.GetFloat3("yaxis", piece->bakedRotMatrix.GetY());
-		const float3 zaxis = pieceTable.GetFloat3("zaxis", piece->bakedRotMatrix.GetZ());
+		const float3 xaxis = pieceTable.GetFloat3("xaxis", bakedMatrix.GetX());
+		const float3 yaxis = pieceTable.GetFloat3("yaxis", bakedMatrix.GetY());
+		const float3 zaxis = pieceTable.GetFloat3("zaxis", bakedMatrix.GetZ());
 
 		if (math::fabs(xaxis.SqLength() - yaxis.SqLength()) < 0.01f && math::fabs(yaxis.SqLength() - zaxis.SqLength()) < 0.01f) {
-			piece->bakedRotMatrix = CMatrix44f(ZeroVector, xaxis, yaxis, zaxis);
+			bakedMatrix = CMatrix44f(ZeroVector, xaxis, yaxis, zaxis);
 		}
 	}
 
@@ -376,10 +376,8 @@ void CAssParser::LoadPieceTransformations(
 	//
 	// note: for all non-AssImp models this is identity!
 	//
-	piece->ComposeRotation(piece->bakedRotMatrix, pieceRotAngles);
-	piece->SetHasIdentityRotation(piece->bakedRotMatrix.IsIdentity() == 0);
-
-	assert(piece->bakedRotMatrix.IsOrthoNormal() == 0);
+	piece->ComposeRotation(bakedMatrix, pieceRotAngles);
+	piece->SetModelMatrix(bakedMatrix);
 }
 
 void CAssParser::SetPieceName(
@@ -763,15 +761,6 @@ void CAssParser::FindTextures(
 
 	model->invertTexYAxis = modelTable.GetBool("fliptextures", true); // Flip texture upside down
 	model->invertTexAlpha = modelTable.GetBool("invertteamcolor", true); // Reverse teamcolor levels
-}
-
-
-void SAssPiece::Shatter(float pieceChance, int texType, int team, const float3 pos, const float3 speed, const CMatrix44f& m) const
-{
-	const float2  pieceParams = {float3::max(float3::fabs(maxs), float3::fabs(mins)).Length(), pieceChance};
-	const   int2 renderParams = {texType, team};
-
-	projectileHandler->AddFlyingPiece(MODELTYPE_ASS, this,  indices,  m, pos, speed,  pieceParams, renderParams);
 }
 
 

rts/Lua/LuaOpenGLUtils.cpp
@@ -20,7 +20,6 @@
 #include "Rendering/Env/CubeMapHandler.h"
 #include "Rendering/Map/InfoTexture/IInfoTextureHandler.h"
 #include "Rendering/Map/InfoTexture/InfoTexture.h"
-#include "Rendering/Models/3DModel.h"
 #include "Rendering/Textures/NamedTextures.h"
 #include "Rendering/Textures/3DOTextureHandler.h"
 #include "Rendering/Textures/S3OTextureHandler.h"

rts/Rendering/Env/Particles/Classes/FlyingPiece.cpp
@@ -9,10 +9,11 @@
 #include "Rendering/UnitDrawer.h"
 #include "Rendering/Models/3DModel.h"
 #include "Rendering/Textures/S3OTextureHandler.h"
+#include "System/myMath.h"
 
 
 static const float EXPLOSION_SPEED = 2.f;
-static const int   MAX_SPLITTER_PARTS = 10;
+
 
 
 /////////////////////////////////////////////////////////////////////
@@ -21,7 +22,6 @@ static const int   MAX_SPLITTER_PARTS = 10;
 
 FlyingPiece::FlyingPiece(
 	const S3DModelPiece* _piece,
-	const std::vector<unsigned int>& _indices,
 	const CMatrix44f& _pieceMatrix,
 	const float3 pos,
 	const float3 speed,
@@ -32,102 +32,25 @@ FlyingPiece::FlyingPiece(
 , pieceMatrix(_pieceMatrix)
 , age(0)
 , piece(_piece)
-, indices(_indices)
 {
-	assert(piece->HasGeometryData()); // else binding the vbos cause gl errors
 	assert(piece->GetVertexDrawIndexCount() % 3 == 0); // only triangles
 
 	InitCommon(pos, speed, _pieceParams.x, _renderParams.y, _renderParams.x);
 
-	// initialize splitter parts
-	splitterParts.resize(MAX_SPLITTER_PARTS);
-	for (auto& cp: splitterParts) {
-		cp.dir = gu->RandVector().ANormalize();
-		cp.speed = speed + cp.dir * EXPLOSION_SPEED * gu->RandFloat();
-		cp.rotationAxisAndSpeed = float4(gu->RandVector().ANormalize(), gu->RandFloat() * 0.1f);
+	const auto& pieceShatterParts = piece->shatterParts[gu->RandInt() % piece->shatterParts.size()];
+
+	splitterParts.reserve(pieceShatterParts.size());
+	for (const auto& cp: pieceShatterParts) {
+		if (gu->RandFloat() > _pieceParams.x)
+			continue;
+
+		splitterParts.emplace_back();
+		float3 flyDir = (cp.dir + (gu->RandVector() * 0.3f)).ANormalize();
+		splitterParts.back().speed                = speed + flyDir * mix<float>(1.f, EXPLOSION_SPEED, gu->RandFloat());
+		splitterParts.back().rotationAxisAndSpeed = float4(gu->RandVector().ANormalize(), gu->RandFloat() * 0.1f);
+		splitterParts.back().indexCount           = cp.indexCount;
+		splitterParts.back().vboOffset            = cp.vboOffset;
 	}
-
-	// add vertices to splitter parts
-	for (size_t i = 0; i < indices.size(); i += 3) {
-		auto dir = GetPolygonDir(i);
-
-		float md = -2.f;
-		SplitterData* mcp = nullptr;
-		for (auto& cp: splitterParts) {
-			if (cp.dir.dot(dir) < md)
-				continue;
-
-			md = cp.dir.dot(dir);
-			mcp = &cp;
-		}
-
-		mcp->indices.push_back(indices[i + 0]);
-		mcp->indices.push_back(indices[i + 1]);
-		mcp->indices.push_back(indices[i + 2]);
-	}
-
-	// randomly remove pieces depending on the pieceChance
-	for (auto& cp: splitterParts) {
-		if (gu->RandFloat() > _pieceParams.x) {
-			cp.indices.clear();
-		}
-	}
-
-	// calculate needed vbo size for the vertex indices
-	size_t neededVboSize = 0;
-	for (auto& cp: splitterParts) {
-		cp.indexCount = cp.indices.size();
-		cp.vboOffset = neededVboSize;
-		neededVboSize += cp.indexCount * sizeof(unsigned int);
-	}
-
-	// fill the vertex index vbo
-	indexVBO.Bind(GL_ELEMENT_ARRAY_BUFFER);
-	indexVBO.Resize(neededVboSize);
-	auto* memVBO = reinterpret_cast<unsigned char*>(indexVBO.MapBuffer(GL_WRITE_ONLY));
-	for (auto& cp: splitterParts) {
-		memcpy(memVBO + cp.vboOffset, &cp.indices[0], cp.indexCount * sizeof(unsigned int));
-
-		// free memory
-		cp.indices.clear();
-		cp.indices.shrink_to_fit();
-	}
-	indexVBO.UnmapBuffer();
-	indexVBO.Unbind();
-
-	// delete empty splitter parts
-	for (int i = splitterParts.size() - 1; i >= 0; --i) {
-		auto& cp = splitterParts[i];
-		if (cp.indexCount == 0) {
-			cp = splitterParts.back();
-			splitterParts.pop_back();
-		}
-	}
-}
-
-FlyingPiece& FlyingPiece::operator = (FlyingPiece&& fp)
-{
-	pos0 = std::move(fp.pos0);
-	pos = std::move(fp.pos);
-	speed = std::move(fp.speed);
-
-	pieceMatrix = std::move(fp.pieceMatrix);
-
-	team = std::move(fp.team);
-	texture = std::move(fp.texture);
-	age = std::move(fp.age);
-
-	pieceRadius = std::move(fp.pieceRadius);
-	drawRadius = std::move(fp.drawRadius);
-
-	piece = std::move(fp.piece);
-	// indices = std::move(fp.indices); fails, is a const reference
-	const_cast<std::vector<unsigned int>&>(indices) = std::move(fp.indices);
-
-	splitterParts = std::move(fp.splitterParts);
-
-	indexVBO = std::move(fp.indexVBO);
-	return *this;
 }
 
 
@@ -170,24 +93,6 @@ bool FlyingPiece::Update()
 	}
 
 	return false;
-}
-
-
-const float3& FlyingPiece::GetVertexPos(const size_t i) const
-{
-	assert(i < indices.size());
-	return piece->GetVertexPos( indices[i] );
-}
-
-
-float3 FlyingPiece::GetPolygonDir(const size_t idx) const
-{
-	float3 midPos;
-	for (int j: {0,1,2}) {
-		midPos += GetVertexPos(idx + j);
-	}
-	midPos *= 0.333f;
-	return midPos.ANormalize();
 }
 
 
@@ -274,26 +179,30 @@ void FlyingPiece::CheckDrawStateChange(const FlyingPiece* prev) const
 		unitDrawer->SetTeamColour(team);
 		if (texture != -1) texturehandlerS3O->SetS3oTexture(texture);
 		piece->BindVertexAttribVBOs();
+		piece->vboShatterIndices.Bind(GL_ELEMENT_ARRAY_BUFFER);
 		return;
 	}
 
-	if (team != prev->GetTeam()) {
+	if (team != prev->team) {
 		unitDrawer->SetTeamColour(team);
 	}
 
-	if (texture != prev->GetTexture()) {
+	if (texture != prev->texture) {
 		if (texture != -1) texturehandlerS3O->SetS3oTexture(texture);
 	}
 
 	if (piece != prev->piece) {
+		prev->piece->vboShatterIndices.Unbind();
 		prev->piece->UnbindVertexAttribVBOs();
 		piece->BindVertexAttribVBOs();
+		piece->vboShatterIndices.Bind(GL_ELEMENT_ARRAY_BUFFER);
 	}
 }
 
 
 void FlyingPiece::EndDraw() const
 {
+	piece->vboShatterIndices.Unbind();
 	piece->UnbindVertexAttribVBOs();
 }
 
@@ -303,12 +212,10 @@ void FlyingPiece::Draw(const FlyingPiece* prev) const
 	CheckDrawStateChange(prev);
 	const float3 dragFactors = GetDragFactors(); // speedDrag, gravityDrag, interAge
 
-	indexVBO.Bind(GL_ELEMENT_ARRAY_BUFFER);
 	for (auto& cp: splitterParts) {
 		glPushMatrix();
 		glMultMatrixf(GetMatrixOf(cp, dragFactors));
-		glDrawRangeElements(GL_TRIANGLES, 0, piece->GetVertexCount() - 1, cp.indexCount, GL_UNSIGNED_INT, indexVBO.GetPtr(cp.vboOffset));
+		glDrawRangeElements(GL_TRIANGLES, 0, piece->GetVertexCount() - 1, cp.indexCount, GL_UNSIGNED_INT, piece->vboShatterIndices.GetPtr(cp.vboOffset));
 		glPopMatrix();
 	}
-	indexVBO.Unbind();
 }

rts/Rendering/Env/Particles/Classes/FlyingPiece.h
@@ -17,17 +17,12 @@ struct FlyingPiece {
 public:
 	FlyingPiece(
 		const S3DModelPiece* _piece,
-		const std::vector<unsigned int>& _indices,
 		const CMatrix44f& _pieceMatrix,
 		const float3 pos,
 		const float3 speed,
 		const float2 _pieceParams,
 		const int2 _renderParams
 	);
-	// needed for sorting
-	FlyingPiece(FlyingPiece&& fp): indices(std::move(fp.indices)) { *this = std::move(fp); }
-	// needed for updating
-	FlyingPiece& operator = (FlyingPiece&& fp);
 
 	bool Update();
 	void Draw(const FlyingPiece* prev) const;
@@ -35,20 +30,25 @@ public:
 	unsigned GetDrawCallCount() const { return splitterParts.size(); }
 
 public:
-	int GetTeam() const { return team; }
-	int GetTexture() const { return texture; }
+	const int& GetTeam() const { return team; }
+	const float3& GetPos() const { return pos; }
+	const float&  GetRadius() const { return drawRadius; }
 
-	float3 GetPos() const { return pos; }
-	float GetRadius() const { return drawRadius; }
+	// used for sorting to reduce gl state changes
+	bool operator< (const FlyingPiece& fp) const {
+		if (texture != fp.texture)
+			return (texture < fp.texture);
+		if (piece != fp.piece)
+			return (piece < fp.piece);
+		if (team != fp.team)
+			return (team < fp.team);
+		return false;
+	}
 
 private:
 	struct SplitterData {
-		float3 dir;
 		float3 speed;
 		float4 rotationAxisAndSpeed;
-
-		std::vector<unsigned int> indices; // just as a buffer, is cleared once the vbo has been created
-
 		size_t vboOffset;
 		size_t indexCount;
 	};
@@ -58,8 +58,6 @@ private:
 	void CheckDrawStateChange(const FlyingPiece* prev) const;
 	float3 GetDragFactors() const;
 	CMatrix44f GetMatrixOf(const SplitterData& cp, const float3 dragFactors) const;
-	const float3& GetVertexPos(const size_t i) const;
-	float3 GetPolygonDir(const size_t idx) const;
 
 private:
 	float3 pos0;
@@ -76,10 +74,8 @@ private:
 	float drawRadius;
 
 	const S3DModelPiece* piece;
-	const std::vector<unsigned int>& indices;
 
 	std::vector<SplitterData> splitterParts;
-	VBO indexVBO;
 };
 
 #endif // FLYING_PIECE_H

rts/Rendering/Models/3DModel.cpp
@@ -5,8 +5,10 @@
 
 #include "3DOParser.h"
 #include "S3OParser.h"
+#include "Game/GlobalUnsynced.h"
 #include "Rendering/GL/myGL.h"
 #include "Sim/Misc/CollisionVolume.h"
+#include "Sim/Projectiles/ProjectileHandler.h"
 #include "System/Exceptions.h"
 #include "System/Util.h"
 
@@ -80,15 +82,12 @@ S3DModelPiece::~S3DModelPiece()
 	glDeleteLists(dispListID, 1);
 }
 
-unsigned int S3DModelPiece::CreateDrawForList() const
+void S3DModelPiece::CreateDispList()
 {
-	const unsigned int dlistID = glGenLists(1);
-
-	glNewList(dlistID, GL_COMPILE);
+	dispListID = glGenLists(1);
+	glNewList(dispListID, GL_COMPILE);
 	DrawForList();
 	glEndList();
-
-	return dlistID;
 }
 
 void S3DModelPiece::DrawStatic() const
@@ -140,6 +139,110 @@ float3 S3DModelPiece::GetEmitDir() const
 		} break;
 	}
 }
+
+
+void S3DModelPiece::CreateShatterPieces()
+{
+	if (!HasGeometryData())
+		return;
+
+	vboShatterIndices.Bind(GL_ELEMENT_ARRAY_BUFFER);
+	vboShatterIndices.Resize(SHATTER_VARIATIONS * GetVertexIndices().size() * sizeof(unsigned int));
+
+	for (int i = 0; i<SHATTER_VARIATIONS; ++i) {
+		CreateShatterPiecesVariation(i);
+	}
+
+	vboShatterIndices.Unbind();
+}
+
+
+void S3DModelPiece::CreateShatterPiecesVariation(const int num)
+{
+	const std::vector<unsigned>& indices = GetVertexIndices();
+
+	// we just operate on a buffer
+	// (cause the indices aren't needed once the buffer has been created)
+	struct ShatterPartDataBuffer : ShatterPartData {
+		std::vector<unsigned> indices;
+	};
+	std::vector<ShatterPartDataBuffer> shatterPartsBuf;
+
+	// initialize splitter parts
+	shatterPartsBuf.resize(SHATTER_MAX_PARTS);
+	for (auto& cp: shatterPartsBuf) {
+		cp.dir = gu->RandVector().ANormalize();
+	}
+
+	// helper
+	auto GetPolygonDir = [&](const size_t idx) -> float3
+	{
+		float3 midPos;
+		for (int j: {0,1,2}) {
+			midPos += GetVertexPos(indices[idx + j]);
+		}
+		midPos *= 0.333f;
+		return midPos.ANormalize();
+	};
+
+	// add vertices to splitter parts
+	for (size_t i = 0; i < indices.size(); i += 3) {
+		const float3& dir = GetPolygonDir(i);
+
+		// find the closest shatter part (the one that points into same dir)
+		float md = -2.f;
+		ShatterPartDataBuffer* mcp = nullptr;
+		for (auto& cp: shatterPartsBuf) {
+			if (cp.dir.dot(dir) < md)
+				continue;
+
+			md = cp.dir.dot(dir);
+			mcp = &cp;
+		}
+
+		mcp->indices.push_back(indices[i + 0]);
+		mcp->indices.push_back(indices[i + 1]);
+		mcp->indices.push_back(indices[i + 2]);
+	}
+
+	// fill the vertex index vbo
+	auto isize = indices.size() * sizeof(unsigned int);
+	auto* memVBO = reinterpret_cast<unsigned char*>(vboShatterIndices.MapBuffer(num * isize, isize, GL_WRITE_ONLY));
+	size_t curVboPos = 0;
+	for (auto& cp: shatterPartsBuf) {
+		cp.indexCount = cp.indices.size();
+		cp.vboOffset  = num * isize + curVboPos;
+		memcpy(memVBO + curVboPos, &cp.indices[0], cp.indexCount * sizeof(unsigned int));
+		curVboPos    += cp.indexCount * sizeof(unsigned int);
+	}
+	vboShatterIndices.UnmapBuffer();
+
+	// delete empty splitter parts
+	for (int i = shatterPartsBuf.size() - 1; i >= 0; --i) {
+		auto& cp = shatterPartsBuf[i];
+		if (cp.indexCount == 0) {
+			cp = std::move(shatterPartsBuf.back());
+			shatterPartsBuf.pop_back();
+		}
+	}
+
+
+	// finish: copy buffer to actual memory
+	shatterParts[num].reserve(shatterPartsBuf.size());
+	for (auto& cp: shatterPartsBuf) {
+		shatterParts[num].push_back(cp);
+	}
+}
+
+
+void S3DModelPiece::Shatter(float pieceChance, int texType, int team, const float3 pos, const float3 speed, const CMatrix44f& m) const
+{
+	const float2  pieceParams = {float3::max(float3::fabs(maxs), float3::fabs(mins)).Length(), pieceChance};
+	const   int2 renderParams = {texType, team};
+
+	projectileHandler->AddFlyingPiece(MODELTYPE_S3O /*FIXME*/, this, m, pos, speed, pieceParams, renderParams);
+}
+
 
 
 /** ****************************************************************************************************

rts/Rendering/Models/3DOParser.cpp
@@ -2,11 +2,8 @@
 
 #include "3DOParser.h"
 
-#include "Game/GlobalUnsynced.h"
-#include "Rendering/GL/myGL.h"
-#include "Rendering/GL/VertexArray.h"
 #include "Sim/Misc/CollisionVolume.h"
-#include "Sim/Projectiles/ProjectileHandler.h"
+#include "System/Exceptions.h"
 #include "System/Util.h"
 #include "System/Log/ILog.h"
 #include "System/FileSystem/VFSHandler.h"
@@ -505,13 +502,4 @@ void S3DOPiece::SetMinMaxExtends()
 		mins = float3::min(mins, vp);
 		maxs = float3::max(maxs, vp);
 	}
-}
-
-
-void S3DOPiece::Shatter(float pieceChance, int /*texType*/, int team, const float3 pos, const float3 speed, const CMatrix44f& m) const //FIXME move to S3dModel
-{
-	const float2  pieceParams = {float3::max(float3::fabs(maxs), float3::fabs(mins)).Length(), pieceChance};
-	const   int2 renderParams = {-1, team};
-
-	projectileHandler->AddFlyingPiece(MODELTYPE_3DO, this,  vertexIndices,  m, pos, speed,  pieceParams, renderParams);
 }

rts/Rendering/Models/3DOParser.h
@@ -46,17 +46,15 @@ struct S3DOPiece: public S3DModelPiece
 
 	unsigned int GetVertexCount() const override { return vboAttributes.GetSize(); }
 	unsigned int GetVertexDrawIndexCount() const override { return vboIndices.GetSize(); }
-
 	const float3& GetVertexPos(const int idx) const override { return vertexAttribs[idx].pos; }
 	const float3& GetNormal(const int idx)    const override { return vertexAttribs[idx].normal; }
+	const std::vector<unsigned>& GetVertexIndices() const override { return vertexIndices; }
 
 	float3 GetEmitPos() const override { return emitPos; }
 	float3 GetEmitDir() const override { return emitDir; }
 
 	void BindVertexAttribVBOs() const override;
 	void UnbindVertexAttribVBOs() const override;
-
-	void Shatter(float pieceChance, int texType, int team, const float3 pos, const float3 speed, const CMatrix44f& m) const override;
 
 public:
 	void SetMinMaxExtends();

commit 151d7a20565a541fdf4c30b4c501633959aa1626

fix #5057 (broken in d92b3196; IsIdentity returns 0 when true)

rts/Rendering/Models/3DModel.h
@@ -155,7 +155,7 @@ public:
 	void SetModelMatrix(const CMatrix44f& m) {
 		// assimp only
 		bakedRotMatrix = m;
-		hasIdentityRot = m.IsIdentity();
+		hasIdentityRot = (m.IsIdentity() == 0);
 		assert(m.IsOrthoNormal() == 0);
 	}
 

rts/Rendering/Models/AssParser.cpp
@@ -1,7 +1,6 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
 #include "AssParser.h"
-
 #include "3DModel.h"
 #include "3DModelLog.h"
 #include "AssIO.h"
@@ -286,7 +285,8 @@ void CAssParser::LoadPieceTransformations(
 	const aiNode* pieceNode,
 	const LuaTable& pieceTable
 ) {
-	aiVector3D aiScaleVec, aiTransVec;
+	aiVector3D aiScaleVec;
+	aiVector3D aiTransVec;
 	aiQuaternion aiRotateQuat;
 
 	// process transforms
@@ -316,7 +316,7 @@ void CAssParser::LoadPieceTransformations(
 	//   together with the (baked) aiRotateQuad they determine the
 	//   model's pose *before* any animations execute
 	//
-	// float3 rotAngles = pieceTable.GetFloat3("rotate", aiQuaternionToRadianAngles(aiRotateQuat) * RADTODEG);
+	// float3 pieceRotAngles = pieceTable.GetFloat3("rotate", aiQuaternionToRadianAngles(aiRotateQuat) * RADTODEG);
 	float3 pieceRotAngles = pieceTable.GetFloat3("rotate", ZeroVector);
 
 	pieceRotAngles.x = pieceTable.GetFloat("rotatex", pieceRotAngles.x);
@@ -340,20 +340,22 @@ void CAssParser::LoadPieceTransformations(
 	);
 
 	// NOTE:
-	//   at least collada (.dae) files generated by Blender represent
-	//   a coordinate-system that differs from the "standard" formats
-	//   (3DO, S3O, ...) for which existing tools at least have prior
-	//   knowledge of Spring's expectations --> let the user override
-	//   the ROOT rotational transform and the rotation-axis mapping
-	//   used by animation scripts (but re-modelling/re-exporting is
-	//   always preferred!) even though AssImp should convert models
-	//   to its own system which matches that of Spring
+	//   at least collada (.dae) files generated by Blender are stored
+	//   in a coordinate-system that differs from the standard formats
+	//   (3DO, S3O, ...) for which existing tools have prior knowledge
+	//   of Spring's convention, but AssImp's internal system happens
+	//   to match our own so we need no explicit conversion
+	//
+	//   we allow overriding the (baked) ROOT rotational transform and
+	//   the rotation-axis mapping used by animation scripts because it
+	//   can be a benefit to create models wrt. a different orientation
+	//   than that of the 3D editor (but re-modelling / re-exporting is
+	//   always preferred!)
 	//
 	//   .dae  : x=Rgt, y=-Fwd, z= Up, as=(-1, -1, 1), am=AXIS_XZY (if Z_UP)
 	//   .dae  : x=Rgt, y=-Fwd, z= Up, as=(-1, -1, 1), am=AXIS_XZY (if Y_UP) [!?]
 	//   .blend: ????
-	CMatrix44f bakedMatrix;
-	bakedMatrix = aiMatrixToMatrix(aiMatrix4x4t<float>(aiRotateQuat.GetMatrix()));
+	CMatrix44f bakedMatrix = aiMatrixToMatrix(aiMatrix4x4t<float>(aiRotateQuat.GetMatrix()));
 
 	if (piece == model->GetRootPiece()) {
 		const float3 xaxis = pieceTable.GetFloat3("xaxis", bakedMatrix.GetX());