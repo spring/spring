a276f46095c33b4bf0eca95e85f218cb279da15a
jK <-jk->
2016-02-06 18:19:59 +0100

    !fix Spring.GetUnitPieceDirection() (returns now same dir as GetEmitDirPos)

--- rts/Rendering/Models/3DModel.h
+++ rts/Rendering/Models/3DModel.h
@@ -313,7 +313,7 @@ struct LocalModelPiece
 private:
 	float3 pos; // translation relative to parent LMP, *INITIALLY* equal to original->offset
 	float3 rot; // orientation relative to parent LMP, in radians (updated by scripts)
-	float3 dir; // direction from vertex[0] to vertex[1] (constant!)
+	float3 dir; // direction (same as emitdir)
 
 	CMatrix44f pieceSpaceMat; // transform relative to parent LMP (SYNCED), combines <pos> and <rot>
 	CMatrix44f modelSpaceMat; // transform relative to root LMP (SYNCED)
