fd3d5cc8c04c2654574e0df36cbf247aeb425fe7
Eshed <ashdnazg@gmail.com>
2016-04-09 01:57:40 +0300

    minor

--- rts/System/float3.cpp
+++ rts/System/float3.cpp
@@ -11,7 +11,7 @@ CR_REG_METADATA(float3, (CR_MEMBER(x), CR_MEMBER(y), CR_MEMBER(z)))
 float float3::maxxpos = -1.0f;
 float float3::maxzpos = -1.0f;
 
-#if (__cplusplus <= 199711L) && !defined(__GXX_EXPERIMENTAL_CXX0X__) && (!defined(__GNUC__) || defined (__clang__))
+#if (__cplusplus <= 199711L) && !defined(__GXX_EXPERIMENTAL_CXX0X__) && (!defined(__GNUC__) || defined (__clang__)) && !(_MSC_VER >= 1900)
 const float float3::CMP_EPS = 1e-4f;
 const float float3::NORMALIZE_EPS = 1e-12f;
 #endif
