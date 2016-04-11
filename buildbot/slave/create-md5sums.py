6a953ae6060499bcd13a6b514198c5464ee823dd
abma <spring@abma.de>
2016-04-12 00:11:39 +0200

    buildbot: update md5 script for recursive creating / providing suffixes as parameters:
    i.e.
    ./create-md5sums.py /tmp/spring .7z .exe .zip

--- buildbot/slave/create-md5sums.py
+++ buildbot/slave/create-md5sums.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-# creates or verifies .md5 files in specified dir
-
 import hashlib, sys, os
 
+if len(sys.argv) <= 2:
+	print("usage: %s dir [file suffixes]+" %(sys.argv[0]))
+	print("recursively (!!!) creates .md5 files for all files with provided")
+	exit(1)
 def hashfile(filename, blocksize=4096):
 	hasher = hashlib.md5()
 	f = open(filename, "rb")
@@ -23,26 +25,40 @@ def createhash(filename, md5):
 def verify(fn, md5):
 	f = open(md5, "rb").read()
 	hexdigest, filename = f.split(" ", 1)
-	assert(filename == fn)
+	if filename != os.path.basename(fn):
+		print("Invalid file: %s %s"% (filename, fn))
+		return False, ""
 	h = hashfile(fn)
 	return h == hexdigest, h
 
-if len(sys.argv) <= 1:
-	print("usage: %s dir" %(sys.argv[0]))
-	exit(1)
-
-path = sys.argv[1]
-suffix = ".md5"
-
-files = { f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) }
-
-for f in files:
+def handlefile(f, suffixes, suffix):
 	if f.endswith(suffix):
-		continue
+		return
+	skip = True
+
+	basename = os.path.basename(f)
+
+	for s in suffixes: # check if filename matches suffix
+		if basename.endswith(s):
+			skip = False
+			break
+	if skip: return
+
 	md5file = f + suffix
-	if md5file in files:
+	if os.path.isfile(md5file):
 		res, h = verify(f, md5file)
 		print("Verifying %s: %s - %s" % (h, f, res))
 	else:
 		h = createhash(f, md5file)
 		print("Creating %s: %s" % (h, md5file))
+
+
+path = sys.argv[1]
+suffixes = sys.argv[2:]
+suffix = ".md5"
+
+
+for root, directories, filenames in os.walk(path):
+	for filename in filenames:
+		handlefile(os.path.join(root, filename), suffixes, suffix)
+
