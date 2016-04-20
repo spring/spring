a5a0becc272b95ae170223deab1fa8c5009b1344
sprunk <gnurps@o2.pl>
2016-04-20 23:47:10 +0200

    add the license text to infolog

--- rts/System/SpringApp.cpp
+++ rts/System/SpringApp.cpp
@@ -185,6 +185,8 @@ bool SpringApp::Initialize()
 	assert(cmdline != NULL);
 	assert(configHandler != NULL);
 
+	LOG_L(L_ERROR, "Spring is distributed under the GNU General Public License, see license.html for more info");
+
 	// list user's config
 	LOG("============== <User Config> ==============");
 	const std::map<std::string, std::string> settings = configHandler->GetDataWithoutDefaults();
