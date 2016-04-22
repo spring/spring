4207119958c63b8f4b4d377ddc280134247f2214
jK <-jk->
2016-04-22 09:01:40 +0200

    simplify GenericParticleProjectile

--- rts/Rendering/Env/Particles/Classes/GenericParticleProjectile.cpp
+++ rts/Rendering/Env/Particles/Classes/GenericParticleProjectile.cpp
@@ -64,31 +64,21 @@ void CGenericParticleProjectile::Draw()
 {
 	inArray = true;
 
+	float3 dir1 = camera->GetRight();
+	float3 dir2 = camera->GetUp();
 	if (directional) {
-		float3 dif(pos-camera->GetPos());
+		float3 dif(pos - camera->GetPos());
 		dif.ANormalize();
-		float3 dir1(dif.cross(speed));
-		dir1.ANormalize();
-		float3 dir2(dif.cross(dir1));
-
-		unsigned char color[4];
-
-		colorMap->GetColor(color, life);
-
-		va->AddVertexTC(drawPos - dir1 * size - dir2 * size, texture->xstart, texture->ystart, color);
-		va->AddVertexTC(drawPos - dir1 * size + dir2 * size, texture->xend,   texture->ystart, color);
-		va->AddVertexTC(drawPos + dir1 * size + dir2 * size, texture->xend,   texture->yend,   color);
-		va->AddVertexTC(drawPos + dir1 * size - dir2 * size, texture->xstart, texture->yend,   color);
-	} else {
-		unsigned char color[4];
-
-		colorMap->GetColor(color, life);
-
-		va->AddVertexTC(drawPos - camera->GetRight() * size - camera->GetUp() * size, texture->xstart, texture->ystart, color);
-		va->AddVertexTC(drawPos + camera->GetRight() * size - camera->GetUp() * size, texture->xend,   texture->ystart, color);
-		va->AddVertexTC(drawPos + camera->GetRight() * size + camera->GetUp() * size, texture->xend,   texture->yend,   color);
-		va->AddVertexTC(drawPos - camera->GetRight() * size + camera->GetUp() * size, texture->xstart, texture->yend,   color);
+		dir1 = dif.cross(speed).ANormalize();
+		dir2 = dif.cross(dir1);
 	}
+
+	unsigned char color[4];
+	colorMap->GetColor(color, life);
+	va->AddVertexTC(drawPos + (-dir1 - dir2) * size, texture->xstart, texture->ystart, color);
+	va->AddVertexTC(drawPos + (-dir1 + dir2) * size, texture->xend,   texture->ystart, color);
+	va->AddVertexTC(drawPos + ( dir1 + dir2) * size, texture->xend,   texture->yend,   color);
+	va->AddVertexTC(drawPos + ( dir1 - dir2) * size, texture->xstart, texture->yend,   color);
 }
 
 int CGenericParticleProjectile::GetProjectilesCount() const
