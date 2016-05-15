3ce55fba77371550b911486ba18c3d4bfea2b979
MadrMan <madman_49@hotmail.com>
2016-05-15 17:03:54 +0200

    Fix compile for outdated glew

--- rts/Rendering/GL/glMarkers.cpp
+++ rts/Rendering/GL/glMarkers.cpp
@@ -4,16 +4,22 @@
 #include "myGL.h"
 
 void CGraphicsMarker::Begin(const char* name) {
+#if defined(glPushGroupMarkerEXT) && defined(glPopGroupMarkerEXT)
 	if(glPushGroupMarkerEXT && glPopGroupMarkerEXT)
 		glPushGroupMarkerEXT(0, name);
+#endif
 }
 
 void CGraphicsMarker::End() {
+#if defined(glPushGroupMarkerEXT) && defined(glPopGroupMarkerEXT)
 	if (glPushGroupMarkerEXT && glPopGroupMarkerEXT)
 		glPopGroupMarkerEXT();
+#endif
 }
 
 void CGraphicsMarker::Event(const char* name) {
+#if defined(glInsertEventMarkerEXT)
 	if(glInsertEventMarkerEXT)
 		glInsertEventMarkerEXT(0, name);
+#endif
 }
