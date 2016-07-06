35f84ff7a0ee49cd4f437e05107a7b33e54159c9
Eshed <ashdnazg@gmail.com>
2016-07-06 20:22:31 +0300

    Re-add .zip release test

--- buildbot/slave/win/make_installer.sh
+++ buildbot/slave/win/make_installer.sh
@@ -12,6 +12,7 @@ echo "Installing into $DEST"
 #Ultra settings, max number of threads taken from commandline.
 SEVENZIP="nice -19 ionice -c3 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on -mmt=${2:-on}"
 SEVENZIP_NONSOLID="nice -19 ionice -c3 7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=off -mmt=${2:-on}"
+ZIP="zip -r9"
 
 if [ -z $MINGWLIBS_PATH ]; then
 	echo 'MINGWLIBS_PATH is not set'
@@ -63,10 +64,19 @@ MIN_PORTABLE_ARCHIVE=${TMP_PATH}/spring_${VERSION}_${PLATFORM}-minimal-portable.
 INSTALLER_FILENAME=${TMP_PATH}/spring_${VERSION}_${PLATFORM}.exe
 DEBUG_ARCHIVE=${TMP_PATH}/${VERSION}_${PLATFORM}_spring_dbg.7z
 UNITTEST_ARCHIVE=${TMP_PATH}/${VERSION}_${PLATFORM}_UnitTests.7z
+MIN_PORTABLE_ZIP=${TMP_PATH}/spring_${VERSION}_${PLATFORM}_minimal-portable.zip
 
 #create portable spring
 touch ${INSTALLDIR}/springsettings.cfg
 ${SEVENZIP} ${MIN_PORTABLE_ARCHIVE} ${INSTALLDIR}/* -xr!*.dbg &
+
+# TODO: remove creation of the zip package, when Zero-K Lobby switched to 7z (will save a lot of resources)
+# Update 2016/07/06 ZK is yet to support, so only stable releases will have a zipped archive,
+# and that's because we're feeling nice.
+# develop is temporary, to verify this works
+if [ "$OUTPUTDIR" == "win32" ] && [ x${BRANCH} = xdevelop ]; then
+	(cd ${INSTALLDIR} && ${ZIP} ${MIN_PORTABLE_ZIP} * -x spring-headless.exe spring-dedicated.exe \*.dbg) &
+fi
 
 # compress UnitTests
 ${SEVENZIP} ${UNITTEST_ARCHIVE} "${BUILDDIR}"/test/*.exe &
