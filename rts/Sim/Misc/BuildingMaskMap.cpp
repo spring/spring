dcc04d02712d4a51992ff9306972d7224bf7f20e
Eshed <ashdnazg@gmail.com>
2016-07-17 01:02:01 +0300

    buildingMaskMap minor fixes and tweaks.

--- rts/Sim/Misc/BuildingMaskMap.cpp
+++ rts/Sim/Misc/BuildingMaskMap.cpp
@@ -9,16 +9,14 @@ CR_REG_METADATA(BuildingMaskMap, (
 	))
 
 
-bool BuildingMaskMap::CheckBounds(int x, int z)
+bool BuildingMaskMap::CheckBounds(unsigned int x, unsigned int z)
 {
-	return
-		(x >= 0 && x <= mapDims.hmapx) &&
-		(z >= 0 && z <= mapDims.hmapy);
+	return ((x < mapDims.hmapx) && (z < mapDims.hmapy));
 }
 
 // sets mask value for tile[x,z] in 2*SQUARE_SIZE coordinates
-bool BuildingMaskMap::SetTileMask(int x, int z, boost::uint16_t value)
-{	
+bool BuildingMaskMap::SetTileMask(unsigned int x, unsigned int z, boost::uint16_t value)
+{
 	if (!CheckBounds(x, z))
 		return false;
 
@@ -28,7 +26,8 @@ bool BuildingMaskMap::SetTileMask(int x, int z, boost::uint16_t value)
 
 // tests previously set mask for tile[x,z] in 2*SQUARE_SIZE coordinates against supplied value
 // true - construction is allowed, false - it's not
-bool BuildingMaskMap::TestTileMaskUnsafe(int x, int z, boost::uint16_t value)
-{	
+bool BuildingMaskMap::TestTileMaskUnsafe(unsigned int x, unsigned int z, boost::uint16_t value)
+{
+	assert(CheckBounds(x, z));
 	return (maskMap[x + z * mapDims.hmapx] & value) == value;
 }
