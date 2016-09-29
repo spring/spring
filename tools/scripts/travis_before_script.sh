8fc6d9b731bc5b37c516ddd022d82aba13391008
Eshed <ashdnazg@gmail.com>
2016-09-29 23:21:03 +0300

    Compile debug build on travis
    tests break in 10... 9... 8...

--- tools/scripts/travis_before_script.sh
+++ tools/scripts/travis_before_script.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$CXX" = "g++" ]; then
-	cmake -DCMAKE_C_FLAGS=-gtoggle -DCMAKE_CXX_FLAGS=-gtoggle .
+	cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_C_FLAGS=-gtoggle -DCMAKE_CXX_FLAGS=-gtoggle .
 else
-	cmake .
+	cmake -DCMAKE_BUILD_TYPE=DEBUG .
 fi
