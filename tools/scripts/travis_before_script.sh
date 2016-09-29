b2c0e691d89005e76e55167f9f76c915b4c790ac
Eshed <ashdnazg@gmail.com>
2016-09-29 23:27:26 +0300

    Less warnings in travis

--- tools/scripts/travis_before_script.sh
+++ tools/scripts/travis_before_script.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$CXX" = "g++" ]; then
-	cmake -DCMAKE_BUILD_TYPE=DEBUG -DCMAKE_C_FLAGS=-gtoggle -DCMAKE_CXX_FLAGS=-gtoggle .
+	cmake -DCMAKE_BUILD_TYPE=DEBUG -DDEBUG_MAX_WARNINGS=OFF -DCMAKE_C_FLAGS=-gtoggle -DCMAKE_CXX_FLAGS=-gtoggle .
 else
-	cmake -DCMAKE_BUILD_TYPE=DEBUG .
+	cmake -DCMAKE_BUILD_TYPE=DEBUG -DDEBUG_MAX_WARNINGS=OFF .
 fi
