340511f6e4d50e4eec89ab1b816f9a59cabd106c
Eshed <ashdnazg@gmail.com>
2016-10-05 12:43:35 +0300

    try to make ccache work on clang as well

--- tools/scripts/travis_before_script.sh
+++ tools/scripts/travis_before_script.sh
@@ -5,5 +5,5 @@ set -e
 if [ "$CXX" = "g++" ]; then
 	cmake -DCMAKE_BUILD_TYPE=DEBUG -DDEBUG_MAX_WARNINGS=OFF -DCMAKE_C_FLAGS=-gtoggle -DCMAKE_CXX_FLAGS=-gtoggle .
 else
-	cmake -DCMAKE_BUILD_TYPE=DEBUG -DDEBUG_MAX_WARNINGS=OFF .
+	cmake -DCMAKE_BUILD_TYPE=DEBUG -DDEBUG_MAX_WARNINGS=OFF -DCMAKE_C_FLAGS=-Qunused-arguments -DCMAKE_CXX_FLAGS=-Qunused-arguments .
 fi
