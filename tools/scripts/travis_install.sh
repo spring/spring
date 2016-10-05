8cf158209bb128f74d5a38e6f0cbb743ada9896d
Eshed <ashdnazg@gmail.com>
2016-10-05 11:08:12 +0300

    Revert "use ninja for travis builds"
    This reverts commit f531752bcdd884acac0c93f60cf14784f1f8dce3.

--- tools/scripts/travis_install.sh
+++ tools/scripts/travis_install.sh
@@ -13,5 +13,5 @@ sudo apt-get install -y libglew-dev libsdl2-dev libdevil-dev libopenal-dev libog
 
 sudo apt-get install -y libboost-thread1.55-dev libboost-regex1.55-dev libboost-system1.55-dev \
   libboost-program-options1.55-dev libboost-signals1.55-dev libboost-chrono1.55-dev \
-  libboost-filesystem1.55-dev libboost-test1.55-dev binutils-gold cmake cmake-data ninja-build
+  libboost-filesystem1.55-dev libboost-test1.55-dev binutils-gold cmake cmake-data
 
