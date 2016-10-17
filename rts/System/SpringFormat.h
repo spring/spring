64fe2f28ad1ec9f2130dcb65846b1b2a340274ac
Eshed <ashdnazg@gmail.com>
2016-10-17 21:32:19 +0300

    make spring::format safer (thanks abma!)

--- rts/System/SpringFormat.h
+++ rts/System/SpringFormat.h
@@ -10,8 +10,11 @@ namespace spring {
 	template<class... Args>
 	std::string format(const char* fmt, const Args&... args) {
 		char buff[4096];
-		SNPRINTF(buff, 4096, fmt, args...);
-		return std::string(buff);
+		const int len = SNPRINTF(buff, 4096, fmt, args...);
+		if ((len > 0) && (len < 4096))
+			return std::string(buff, len);
+
+		return std::string();
 	}
 	template<class... Args>
 	std::string format(const std::string& fmt, const Args&... args) {
