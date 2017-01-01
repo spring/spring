438f3d34985a2294419a90a9a5f8619c70c3c79a
rtri <none@none>
2017-01-02 00:30:10 +0100

    apparently clang's max_align_t is *not* in std::

--- rts/System/ConcurrentQueue.h
+++ rts/System/ConcurrentQueue.h
@@ -239,7 +239,7 @@ namespace details {
 			: static_cast<T>(-1);
 	};
 
-#if defined(__GNUC__) && !defined( __clang__ )
+#if defined(__GNUC__) || defined( __clang__ )
 	typedef ::max_align_t std_max_align_t;      // GCC forgot to add it to std:: for a while
 #else
 	typedef std::max_align_t std_max_align_t;   // Others (e.g. MSVC) insist it can *only* be accessed via std::
