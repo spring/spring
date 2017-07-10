a597327c55ea0de84c705015459b6085d37699be
rt <none@none>
2017-07-10 17:36:30 +0200

    set default depthrange explicitly

--- rts/Rendering/GlobalRendering.cpp
+++ rts/Rendering/GlobalRendering.cpp
@@ -927,7 +927,10 @@ void CGlobalRendering::UpdateGLGeometry()
 void CGlobalRendering::InitGLState()
 {
 	glShadeModel(GL_SMOOTH);
+
 	glClearDepth(1.0f);
+	glDepthRangef(0.0f, 1.0f);
+
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 
