a6f45e5e5aa66e832493125d1c71e863c2a703dd
abma <spring@abma.de>
2017-09-01 01:07:18 +0200

    fix failing assert:
    SpringTime.cpp:158: const char* spring_clock::GetName(): Assertion `timerInited' failed.

--- rts/System/SpringApp.cpp
+++ rts/System/SpringApp.cpp
@@ -238,10 +238,12 @@ SpringApp::SpringApp(int argc, char** argv)
 	ParseCmdLine(argc, argv);
 
 	FileSystemInitializer::InitializeLogOutput();
+
+	spring_clock::PushTickRate(configHandler->GetBool("UseHighResTimer"));
+
 	CLogOutput::LogConfigInfo();
 	CLogOutput::LogSystemInfo();
 
-	spring_clock::PushTickRate(configHandler->GetBool("UseHighResTimer"));
 	// set the Spring "epoch" to be whatever value the first
 	// call to gettime() returns, should not be 0 (can safely
 	// be done before SDL_Init, we are not using SDL_GetTicks
