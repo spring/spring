064ab96a6e8a9ba15a129391eecc547b37cf3403
rt <none@none>
2017-10-01 00:20:52 +0200

    -

--- rts/Rendering/Env/NullWater.h
+++ rts/Rendering/Env/NullWater.h
@@ -10,7 +10,7 @@ class CNullWater : public IWater
 public:
 	CNullWater() {}
 
-	void Draw() override;
+	void Draw() override {}
 	void UpdateWater(CGame* game) override {}
 	int GetID() const override { return WATER_RENDERER_NULL; }
 	const char* GetName() const override { return "null"; }
