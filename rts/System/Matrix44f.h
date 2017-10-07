a68d4c0e9ed537a4411280fa4a73941d0cd02edf
rt <none@none>
2017-10-07 03:22:43 +0200

    remove unused {new,del}mat{2,3} funcs

--- rts/System/Matrix44f.h
+++ rts/System/Matrix44f.h
@@ -124,40 +124,4 @@ public:
 	};
 };
 
-
-// Templates for simple 2D/3D matrixes that behave
-// pretty much like statically allocated matrixes,
-// but can also be casted to and used as pointers.
-template<class T>
-T **newmat2(int x, int y) {
-	T *mat2 = new T[x*y], **mat = new T *[x];
-	for (int i = 0; i < x; ++i)
-		mat[i] = mat2 + i*y;
-	return mat;
-}
-
-template<class T>
-T ***newmat3(int x, int y, int z) {
-	T *mat3=new T[x*y*z], **mat2=new T *[x*y], ***mat=new T **[x];
-	for (int i = 0; i < x; ++i) {
-		for(int j = 0; j < y; ++j)
-			mat2[i*y+j] = mat3 + (i*y+j)*z;
-		mat[i] = mat2 + i*y;
-	}
-	return mat;
-}
-
-template<class T>
-void delmat2(T** mat) {
-	delete [] *mat;
-	delete [] mat;
-}
-
-template<class T>
-void delmat3(T*** mat) {
-	delete [] **mat;
-	delete [] *mat;
-	delete [] mat;
-}
-
 #endif /* MATRIX44F_H */
