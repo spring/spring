323968da85dfa4779526f7eb90b5ca153807872e
rt <none@none>
2017-10-08 21:57:50 +0200

    s/string/cstring

--- rts/aGui/GuiElement.cpp
+++ rts/aGui/GuiElement.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
-#include <string> // memset
+#include <cstring> // memset
 #include "GuiElement.h"
 
 #include "Rendering/GL/myGL.h"
