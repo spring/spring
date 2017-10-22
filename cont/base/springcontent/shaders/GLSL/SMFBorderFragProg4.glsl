33b75eee2ff77968f9cfce91e311a6309e494216
rt <none@none>
2017-10-22 18:08:23 +0200

    fix border detail

--- cont/base/springcontent/shaders/GLSL/SMFBorderFragProg4.glsl
+++ cont/base/springcontent/shaders/GLSL/SMFBorderFragProg4.glsl
@@ -8,12 +8,15 @@ in vec4 v_color_rgba;
 in vec2 v_diffuse_tc;
 in vec2 v_detail_tc;
 
+// SMF_INTENSITY_MULT
+const vec4 diffuse_mult = vec4(210.0 / 255.0, 210.0 / 255.0, 210.0 / 255.0, 1.0);
+
 layout(location = 0) out vec4 f_color_rgba;
 
 void main() {
-	vec4 diffuse_color = texture(u_diffuse_tex, v_diffuse_tc) * (210.0 / 255.0);
+	vec4 diffuse_color = texture(u_diffuse_tex, v_diffuse_tc) * diffuse_mult;
 	vec4 detail_color = texture(u_detail_tex, v_detail_tc) * 2.0 - 1.0;
 
-	f_color_rgba = (diffuse_color + detail_color * 0.0) * (v_color_rgba * (1.0 / 255.0));
+	f_color_rgba = (diffuse_color + detail_color) * (v_color_rgba * (1.0 / 255.0));
 }
 
