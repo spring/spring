7dab6a7d82668805ee1e28be52a5b8ceea5b1a80
sprunk <spr.ng@o2.pl>
2017-11-06 14:20:28 +0100

    Add `dont_remove` and changelog.

--- cont/base/springcontent/gamedata/defs_post.lua
+++ cont/base/springcontent/gamedata/defs_post.lua
@@ -10,7 +10,7 @@ end
 
 local i = 1
 while i <= #DEFS.moveDefs do
-	if not usedMovedefs[DEFS.moveDefs[i].name] then
+	if not usedMovedefs[DEFS.moveDefs[i].name] and not DEFS.moveDefs[i].dont_remove then
 		DEFS.moveDefs[i] = DEFS.moveDefs[#DEFS.moveDefs]
 		DEFS.moveDefs[#DEFS.moveDefs] = nil
 	else
