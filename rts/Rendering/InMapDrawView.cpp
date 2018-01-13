0375680c795ff4b1d42f8f260d5af731e11a2e56
rt <none@none>
2018-01-13 15:14:15 +0100

    convert InMapDrawView

--- rts/Rendering/InMapDrawView.cpp
+++ rts/Rendering/InMapDrawView.cpp
@@ -4,56 +4,41 @@
 #include "InMapDrawView.h"
 #include "Rendering/Colors.h"
 #include "Rendering/Fonts/glFont.h"
-#include "Rendering/GL/VertexArray.h"
-#include "Rendering/GlobalRendering.h"
+#include "Rendering/GL/RenderDataBuffer.hpp"
 
 #include "Game/Camera.h"
 #include "Game/InMapDrawModel.h"
 #include "Map/ReadMap.h"
 #include "Sim/Misc/TeamHandler.h"
 
-CInMapDrawView* inMapDrawerView = NULL;
+CInMapDrawView* inMapDrawerView = nullptr;
 
-
-/**
- * how far on the way between x and y [0.0f, 1.0f]
- */
-static inline unsigned char smoothStep(int x, int y, int a) {
-	return (unsigned char)((x * (1.0f - a)) + (y * a));
-}
 
 CInMapDrawView::CInMapDrawView()
 {
 	unsigned char tex[64][128][4];
-	for (int y = 0; y < 64; y++) {
-		for (int x = 0; x < 128; x++) {
-			tex[y][x][0] = 0;
-			tex[y][x][1] = 0;
-			tex[y][x][2] = 0;
-			tex[y][x][3] = 0;
-		}
-	}
+
+	std::memset(tex, 0, sizeof(tex));
 
 	for (int y = 0; y < 64; y++) {
 		// circular thingy
 		for (int x = 0; x < 64; x++) {
-			float dist = std::sqrt((float)(x - 32) * (x - 32) + (y - 32) * (y - 32));
+			const float dist = std::sqrt((float)(x - 32) * (x - 32) + (y - 32) * (y - 32));
+
 			if (dist > 31.0f) {
 				// do nothing - leave transparent
 			} else if (dist > 30.0f) {
 				// interpolate (outline -> nothing)
-				float a = (dist - 30.0f);
-				tex[y][x][3] = smoothStep(255,   0, a);
+				tex[y][x][3] = mix(255,   0, dist - 30.0f);
 			} else if (dist > 24.0f) {
 				// black outline
 				tex[y][x][3] = 255;
 			} else if (dist > 23.0f) {
 				// interpolate (inner -> outline)
-				float a = (dist - 23.0f);
-				tex[y][x][0] = smoothStep(255,   0, a);
-				tex[y][x][1] = smoothStep(255,   0, a);
-				tex[y][x][2] = smoothStep(255,   0, a);
-				tex[y][x][3] = smoothStep(200, 255, a);
+				tex[y][x][0] = mix(255,   0, dist - 23.0f);
+				tex[y][x][1] = mix(255,   0, dist - 23.0f);
+				tex[y][x][2] = mix(255,   0, dist - 23.0f);
+				tex[y][x][3] = mix(200, 255, dist - 23.0f);
 			} else {
 				tex[y][x][0] = 255;
 				tex[y][x][1] = 255;
@@ -65,23 +50,22 @@ CInMapDrawView::CInMapDrawView()
 	for (int y = 0; y < 64; y++) {
 		// linear falloff
 		for (int x = 64; x < 128; x++) {
-			float dist = abs(y - 32);
+			const float dist = std::abs(y - 32);
+
 			if (dist > 31.0f) {
 				// do nothing - leave transparent
 			} else if (dist > 30.0f) {
 				// interpolate (outline -> nothing)
-				float a = (dist - 30.0f);
-				tex[y][x][3] = smoothStep(255,   0, a);
+				tex[y][x][3] = mix(255,   0, dist - 30.0f);
 			} else if (dist > 24.0f) {
 				// black outline
 				tex[y][x][3] = 255;
 			} else if (dist > 23.0f) {
 				// interpolate (inner -> outline)
-				float a = (dist - 23.0f);
-				tex[y][x][0] = smoothStep(255,   0, a);
-				tex[y][x][1] = smoothStep(255,   0, a);
-				tex[y][x][2] = smoothStep(255,   0, a);
-				tex[y][x][3] = smoothStep(200, 255, a);
+				tex[y][x][0] = mix(255,   0, dist - 23.0f);
+				tex[y][x][1] = mix(255,   0, dist - 23.0f);
+				tex[y][x][2] = mix(255,   0, dist - 23.0f);
+				tex[y][x][3] = mix(200, 255, dist - 23.0f);
 			} else {
 				tex[y][x][0] = 255;
 				tex[y][x][1] = 255;
@@ -109,85 +93,84 @@ CInMapDrawView::~CInMapDrawView()
 
 struct InMapDraw_QuadDrawer: public CReadMap::IQuadDrawer
 {
-	CVertexArray* pointsVa;
-	CVertexArray* linesVa;
-	std::vector<const CInMapDrawModel::MapPoint*>* visibleLabels;
+	std::vector<const CInMapDrawModel::MapPoint*>* visibleLabels = nullptr;
+
+	GL::RenderDataBufferTC* pointBuffer = nullptr;
+	GL::RenderDataBufferC* linesBuffer = nullptr;
 
 	void ResetState() {
-		pointsVa = nullptr;
-		linesVa = nullptr;
+		pointBuffer = nullptr;
+		linesBuffer = nullptr;
 	}
 	void DrawQuad(int x, int y);
 
 private:
-	void DrawPoint(const CInMapDrawModel::MapPoint* point) const;
-	void DrawLine(const CInMapDrawModel::MapLine* line) const;
+	void DrawPoint(const CInMapDrawModel::MapPoint* point, GL::RenderDataBufferTC* buffer) const;
+	void DrawLine(const CInMapDrawModel::MapLine* line, GL::RenderDataBufferC* buffer) const;
 };
 
-void InMapDraw_QuadDrawer::DrawPoint(const CInMapDrawModel::MapPoint* point) const
+
+
+void InMapDraw_QuadDrawer::DrawPoint(const CInMapDrawModel::MapPoint* point, GL::RenderDataBufferTC* buffer) const
 {
 	const float3& pos = point->GetPos();
-	const float3 dif = (pos - camera->GetPos()).ANormalize();
-	const float3 dir1 = (dif.cross(UpVector)).ANormalize();
-	const float3 dir2 = (dif.cross(dir1));
+	const float3 zdir = (pos - camera->GetPos()).ANormalize();
+	const float3 xdir = (zdir.cross(UpVector)).ANormalize();
+	const float3 ydir = (zdir.cross(xdir));
 
 
-	const unsigned char* color = point->IsBySpectator() ? color4::white : teamHandler->Team(point->GetTeamID())->color;
-	const unsigned char col[4] = {
-		color[0],
-		color[1],
-		color[2],
-		200
-	};
+	const unsigned char* pcolor = point->IsBySpectator() ? color4::white : teamHandler->Team(point->GetTeamID())->color;
+	const unsigned char color[4] = {pcolor[0], pcolor[1], pcolor[2], 200};
 
-	const float size = 6.0f;
+	constexpr float size = 6.0f;
 	const float3 pos1(pos.x,  pos.y  +   5.0f, pos.z);
 	const float3 pos2(pos1.x, pos1.y + 100.0f, pos1.z);
 
-	pointsVa->AddVertexQTC(pos1 - dir1 * size,               0.25f, 0, col);
-	pointsVa->AddVertexQTC(pos1 + dir1 * size,               0.25f, 1, col);
-	pointsVa->AddVertexQTC(pos1 + dir1 * size + dir2 * size, 0.00f, 1, col);
-	pointsVa->AddVertexQTC(pos1 - dir1 * size + dir2 * size, 0.00f, 0, col);
+	buffer->SafeAppend({pos1 - xdir * size,               0.25f, 0.0f, color});
+	buffer->SafeAppend({pos1 + xdir * size,               0.25f, 1.0f, color});
+	buffer->SafeAppend({pos1 + xdir * size + ydir * size, 0.00f, 1.0f, color});
+	buffer->SafeAppend({pos1 - xdir * size + ydir * size, 0.00f, 0.0f, color});
 
-	pointsVa->AddVertexQTC(pos1 - dir1 * size,               0.75f, 0, col);
-	pointsVa->AddVertexQTC(pos1 + dir1 * size,               0.75f, 1, col);
-	pointsVa->AddVertexQTC(pos2 + dir1 * size,               0.75f, 1, col);
-	pointsVa->AddVertexQTC(pos2 - dir1 * size,               0.75f, 0, col);
+	buffer->SafeAppend({pos1 - xdir * size,               0.75f, 0.0f, color});
+	buffer->SafeAppend({pos1 + xdir * size,               0.75f, 1.0f, color});
+	buffer->SafeAppend({pos2 + xdir * size,               0.75f, 1.0f, color});
+	buffer->SafeAppend({pos2 - xdir * size,               0.75f, 0.0f, color});
 
-	pointsVa->AddVertexQTC(pos2 - dir1 * size,               0.25f, 0, col);
-	pointsVa->AddVertexQTC(pos2 + dir1 * size,               0.25f, 1, col);
-	pointsVa->AddVertexQTC(pos2 + dir1 * size - dir2 * size, 0.00f, 1, col);
-	pointsVa->AddVertexQTC(pos2 - dir1 * size - dir2 * size, 0.00f, 0, col);
+	buffer->SafeAppend({pos2 - xdir * size,               0.25f, 0.0f, color});
+	buffer->SafeAppend({pos2 + xdir * size,               0.25f, 1.0f, color});
+	buffer->SafeAppend({pos2 + xdir * size - ydir * size, 0.00f, 1.0f, color});
+	buffer->SafeAppend({pos2 - xdir * size - ydir * size, 0.00f, 0.0f, color});
 
-	if (!point->GetLabel().empty()) {
-		visibleLabels->push_back(point);
-	}
+	if (point->GetLabel().empty())
+		return;
+
+	visibleLabels->push_back(point);
 }
 
-void InMapDraw_QuadDrawer::DrawLine(const CInMapDrawModel::MapLine* line) const
+void InMapDraw_QuadDrawer::DrawLine(const CInMapDrawModel::MapLine* line, GL::RenderDataBufferC* buffer) const
 {
 	const unsigned char* color = line->IsBySpectator() ? color4::white : teamHandler->Team(line->GetTeamID())->color;
-	linesVa->AddVertexQC(line->GetPos1() - (line->GetPos1() - camera->GetPos()).ANormalize() * 26, color);
-	linesVa->AddVertexQC(line->GetPos2() - (line->GetPos2() - camera->GetPos()).ANormalize() * 26, color);
+	buffer->SafeAppend({line->GetPos1() - (line->GetPos1() - camera->GetPos()).ANormalize() * 26, color});
+	buffer->SafeAppend({line->GetPos2() - (line->GetPos2() - camera->GetPos()).ANormalize() * 26, color});
 }
+
+
 
 void InMapDraw_QuadDrawer::DrawQuad(int x, int y)
 {
 	const CInMapDrawModel::DrawQuad* dq = inMapDrawerModel->GetDrawQuad(x, y);
 
-	pointsVa->EnlargeArrays(dq->points.size() * 12, 0, VA_SIZE_TC);
 	//! draw point markers
 	for (const CInMapDrawModel::MapPoint& pi: dq->points) {
 		if (pi.IsVisibleToPlayer(inMapDrawerModel->GetAllMarksVisible())) {
-			DrawPoint(&pi);
+			DrawPoint(&pi, pointBuffer);
 		}
 	}
 
-	linesVa->EnlargeArrays(dq->lines.size() * 2, 0, VA_SIZE_C);
 	//! draw line markers
 	for (const CInMapDrawModel::MapLine& li: dq->lines) {
 		if (li.IsVisibleToPlayer(inMapDrawerModel->GetAllMarksVisible())) {
-			DrawLine(&li);
+			DrawLine(&li, linesBuffer);
 		}
 	}
 }
@@ -196,53 +179,58 @@ void InMapDraw_QuadDrawer::DrawQuad(int x, int y)
 
 void CInMapDrawView::Draw()
 {
-	CVertexArray* pointsVa = GetVertexArray();
-	pointsVa->Initialize();
-	CVertexArray* linesVa = GetVertexArray();
-	linesVa->Initialize();
+	GL::RenderDataBufferTC* pointBuffer = GL::GetRenderBufferTC();
+	GL::RenderDataBufferC* linesBuffer = GL::GetRenderBufferC();
+
+	Shader::IProgramObject* pointShader = pointBuffer->GetShader();
+	Shader::IProgramObject* linesShader = linesBuffer->GetShader();
 
 	InMapDraw_QuadDrawer drawer;
-	drawer.linesVa = linesVa;
-	drawer.pointsVa = pointsVa;
 	drawer.visibleLabels = &visibleLabels;
-
-	glDepthMask(0);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_BLEND);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	drawer.pointBuffer = pointBuffer;
+	drawer.linesBuffer = linesBuffer;
 
 	readMap->GridVisibility(nullptr, &drawer, 1e9, CInMapDrawModel::DRAW_QUAD_SIZE);
 
-	glDisable(GL_TEXTURE_2D);
-	glLineWidth(3.f);
-	linesVa->DrawArrayC(GL_LINES); //! draw lines
 
-	// XXX hopeless drivers, retest in a year or so...
-	// width greater than 2 causes GUI flicker on ATI hardware as of driver version 9.3
-	// so redraw lines with width 1
-	if (globalRendering->atiHacks) {
-		glLineWidth(1.f);
-		linesVa->DrawArrayC(GL_LINES);
+	glDepthMask(GL_FALSE);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable(GL_BLEND);
+
+	{
+		// draw lines
+		glLineWidth(3.0f);
+		linesShader->Enable();
+		linesShader->SetUniformMatrix4x4<const char*, float>("u_movi_mat", false, camera->GetViewMatrix());
+		linesShader->SetUniformMatrix4x4<const char*, float>("u_proj_mat", false, camera->GetProjectionMatrix());
+		linesBuffer->Submit(GL_LINES);
+		linesShader->Disable();
+		glLineWidth(1.0f);
+	}
+	{
+		// draw points
+		glBindTexture(GL_TEXTURE_2D, texture);
+		pointShader->Enable();
+		pointShader->SetUniformMatrix4x4<const char*, float>("u_movi_mat", false, camera->GetViewMatrix());
+		pointShader->SetUniformMatrix4x4<const char*, float>("u_proj_mat", false, camera->GetProjectionMatrix());
+		pointBuffer->Submit(GL_QUADS);
+		pointShader->Disable();
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
-	// draw points
-	glLineWidth(1);
-	glEnable(GL_TEXTURE_2D);
-	pointsVa->DrawArrayTC(GL_QUADS); //! draw point markers
 
 	if (!visibleLabels.empty()) {
-		font->SetColors(); //! default
+		font->SetColors(); // default
 		font->glWorldBegin();
 
-		//! draw point labels
+		// draw point labels
 		for (const CInMapDrawModel::MapPoint* point: visibleLabels) {
-			float3 pos = point->GetPos();
-			pos.y += 111.0f;
+			const float3 pos = point->GetPos() + UpVector * 111.0f;
 
 			const CTeam* team = teamHandler->Team(point->GetTeamID());
 			const unsigned char* color = point->IsBySpectator() ? color4::white : team->color;
 
-			font->SetTextColor(color[0] / 255.0f, color[1] / 255.0f, color[2] / 255.0f, 1.0f); //FIXME (overload!)
+			font->SetTextColor(color[0] / 255.0f, color[1] / 255.0f, color[2] / 255.0f, 1.0f);
 			font->glWorldPrint(pos, 26.0f, point->GetLabel());
 		}
 
@@ -250,5 +238,5 @@ void CInMapDrawView::Draw()
 		visibleLabels.clear();
 	}
 
-	glDepthMask(1);
+	glDepthMask(GL_TRUE);
 }
