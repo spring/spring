a462ab1302b0f69dd21bb98bc548b22a7acc3ab3
rt <none@none>
2018-01-31 18:48:05 +0100

    reuse various buffers in Net/UDP*

--- rts/System/Net/RawPacket.h
+++ rts/System/Net/RawPacket.h
@@ -3,6 +3,9 @@
 #ifndef RAW_PACKET_H
 #define RAW_PACKET_H
 
+#include <cstdint>
+#include <utility>
+
 #include "System/Misc/NonCopyable.h"
 
 namespace netcode
@@ -14,26 +17,50 @@ namespace netcode
 class RawPacket : public spring::noncopyable
 {
 public:
+	RawPacket() = default;
+
 	/**
 	 * @brief create a new packet and store data inside
 	 * @param data the data to store
-	 * @param length the length of the data (is save even if its 0)
+	 * @param length the length of the data (is safe even if 0)
 	 */
-	RawPacket(const unsigned char* const data, const unsigned length);
+	RawPacket(const uint8_t* const data, const uint32_t length);
 
 	/**
 	 * @brief create a new packet without data
-	 * @param length the estimated length of the data (is save even if its 0)
+	 * @param length the estimated length of the data
 	 */
-	RawPacket(const unsigned length);
+	RawPacket(const uint32_t newLength): length(newLength) {
+		if (length == 0)
+			return;
 
-	/**
-	 * @brief Free the memory
-	 */
-	~RawPacket();
+		data = new uint8_t[length];
+	}
 
-	unsigned char* data;
-	const unsigned length;
+	RawPacket(RawPacket&& p) { *this = std::move(p); }
+	~RawPacket() { Delete(); }
+
+	RawPacket& operator = (RawPacket&& p) {
+		data = p.data;
+		p.data = nullptr;
+
+		length = p.length;
+		p.length = 0;
+		return *this;
+	}
+
+	void Delete() {
+		if (length == 0)
+			return;
+
+		delete[] data;
+		data = nullptr;
+
+		length = 0;
+	}
+
+	uint8_t* data = nullptr;
+	uint32_t length = 0;
 };
 
 } // namespace netcode
