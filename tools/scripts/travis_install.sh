8cf0825908eabbffc623ea9d7b0d245b867757fc
abma <spring@abma.de>
2018-05-03 00:16:29 +0200

    try to use gcc5 for travis

--- tools/scripts/travis_install.sh
+++ tools/scripts/travis_install.sh
@@ -4,9 +4,10 @@
 
 set -e
 
-#sudo apt-get install gcc-4.8 g++-4.8;
-#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90;
-#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90;
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get -y install gcc-5 g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
 sudo apt-get install -y libglew-dev libsdl2-dev libdevil-dev libopenal-dev libogg-dev \
   libvorbis-dev libfreetype6-dev p7zip-full libxcursor-dev libunwind8-dev libgles2-mesa-dev
