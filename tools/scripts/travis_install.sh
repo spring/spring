71c2d34e75a0a39c78b17e4af65a00722541ce79
abma <spring@abma.de>
2018-05-03 01:02:04 +0200

    install gcc only when compiling with gcc

--- tools/scripts/travis_install.sh
+++ tools/scripts/travis_install.sh
@@ -4,10 +4,14 @@
 
 set -e
 
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt-get update
-sudo apt-get -y install gcc-7 g++-7
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+if [ "$CXX" = "g++" ]; then
+	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+	sudo apt-get update
+	sudo apt-get -y install gcc-7 g++-7
+	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
+else
+	sudo apt-get update
+fi
 
 sudo apt-get install -y libglew-dev libsdl2-dev libdevil-dev libopenal-dev libogg-dev \
   libvorbis-dev libfreetype6-dev p7zip-full libxcursor-dev libunwind8-dev libgles2-mesa-dev
