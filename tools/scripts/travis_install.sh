226a26127f29a717a30d6e2fc1084e3db4d65120
abma <spring@abma.de>
2018-05-04 00:21:50 +0200

    try to fix clang travis compile

--- tools/scripts/travis_install.sh
+++ tools/scripts/travis_install.sh
@@ -10,7 +10,9 @@ if [ "$CXX" = "g++" ]; then
 	sudo apt-get -y install gcc-7 g++-7
 	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 else
+	sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 	sudo apt-get update
+	sudo apt-get -y install libstdc++-7-dev libgcc-7-dev
 fi
 
 sudo apt-get install -y libglew-dev libsdl2-dev libdevil-dev libopenal-dev libogg-dev \
