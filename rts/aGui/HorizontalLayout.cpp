260e7c72fa15168586c0c068a0b1a9a7bb717098
rt <none@none>
2018-05-04 12:03:01 +0200

    fix aGui FPE
    Conflicts:
    	rts/aGui/HorizontalLayout.cpp

--- rts/aGui/HorizontalLayout.cpp
+++ rts/aGui/HorizontalLayout.cpp
@@ -13,47 +13,51 @@ HorizontalLayout::HorizontalLayout(GuiElement* parent) : GuiElement(parent)
 
 void HorizontalLayout::DrawSelf()
 {
-	if (borderWidth > 0)
-	{
-		glLineWidth(borderWidth);
-		glColor4f(1.f,1.f,1.f, Opacity());
-		DrawBox(GL_LINE_LOOP);
-	}
+	if (borderWidth <= 0.0f)
+		return;
+
+	glLineWidth(borderWidth);
+	glColor4f(1.0f, 1.0f, 1.0f, Opacity());
+	DrawBox(GL_LINE_LOOP);
 }
 
 void HorizontalLayout::GeometryChangeSelf()
 {
 	if (children.empty())
 		return;
-	unsigned numFixed = 0;
+
+	unsigned numFixedObjs = 0;
+	unsigned sumObjWeight = 0;
+
 	float totalFixedSize = 0.0;
-	for (ChildList::const_iterator it = children.begin(); it != children.end(); ++it)
-	{
-		if ((*it)->SizeFixed())
-		{
-			numFixed++;
-			totalFixedSize += (*it)->GetSize()[0];
-		}
+
+	for (const auto& child: children) {
+		if (!child->SizeFixed())
+			continue;
+
+		numFixedObjs += 1;
+		totalFixedSize += child->GetSize()[0];
 	}
 
-	unsigned weightedObjects = 0;
-	for (ChildList::iterator i = children.begin(); i != children.end(); ++i)
-		weightedObjects += (*i)->Weight();
-	
-	const float hspacePerObject = (size[0]-float(weightedObjects-1)*itemSpacing - 2*borderSpacing-totalFixedSize)/float(weightedObjects-numFixed);
+	for (const auto& child: children) {
+		sumObjWeight += child->Weight();
+	}
+
+	const float hspaceBorder = 2.0f * borderSpacing;
+	const float hspaceTotal  = std::max(1.0f, float(sumObjWeight - numFixedObjs));
+	const float hspacePerObj = (size[0] - float(sumObjWeight - 1) * itemSpacing - hspaceBorder - totalFixedSize) / hspaceTotal;
+
 	float startX = pos[0] + borderSpacing;
-	for (ChildList::iterator i = children.begin(); i != children.end(); ++i)
-	{
-		(*i)->SetPos(startX, pos[1] + borderSpacing);
-		if ((*i)->SizeFixed())
-		{
-			(*i)->SetSize((*i)->GetSize()[0], size[1] - 2.0f*borderSpacing, true);
-			startX += (*i)->GetSize()[0] + itemSpacing;
-		}
-		else
-		{
-			(*i)->SetSize(hspacePerObject*float((*i)->Weight()), size[1]- 2.0f*borderSpacing);
-			startX += hspacePerObject*float((*i)->Weight()) + itemSpacing;
+
+	for (const auto& child: children) {
+		child->SetPos(startX, pos[1] + borderSpacing);
+
+		if (child->SizeFixed()) {
+			child->SetSize(child->GetSize()[0], size[1] - hspaceBorder, true);
+			startX += child->GetSize()[0] + itemSpacing;
+		} else {
+			child->SetSize(hspacePerObj * float(child->Weight()), size[1] - hspaceBorder);
+			startX += hspacePerObj*float(child->Weight()) + itemSpacing;
 		}
 	}
 }
