182b618cf1ca08c985f0a02286a2aab9f95cc0b0
Eshed <ashdnazg@gmail.com>
2018-08-17 18:27:24 +0300

    travis boost cleanup

--- tools/scripts/travis_install.sh
+++ tools/scripts/travis_install.sh
@@ -18,7 +18,5 @@ fi
 sudo apt-get install -y libglew-dev libsdl2-dev libdevil-dev libopenal-dev libogg-dev \
   libvorbis-dev libfreetype6-dev p7zip-full libxcursor-dev libunwind8-dev libgles2-mesa-dev
 
-sudo apt-get install -y libboost-thread1.55-dev libboost-regex1.55-dev libboost-system1.55-dev \
-  libboost-program-options1.55-dev libboost-signals1.55-dev libboost-chrono1.55-dev \
-  libboost-filesystem1.55-dev libboost-test1.55-dev binutils-gold cmake cmake-data
+sudo apt-get install -y binutils-gold cmake cmake-data
 
