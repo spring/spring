d492dfbd6f6e7bc9fac677adcae112adf8d8bc91
PicassoCT <PicassoCT@gmx.de>
2018-10-24 14:50:39 +0200

    Update IkChain.h
    Updated Documentation

--- rts/Sim/Units/IkChain.h
+++ rts/Sim/Units/IkChain.h
@@ -32,42 +32,41 @@ public:
 	///Constructors 
 	IkChain();
 	
-	//Create the segments
+	//Create the segments from a startPieceId and an endPieceID
 	IkChain(int id, CUnit* unit, LocalModelPiece* startPiece, unsigned int startPieceID, unsigned int endPieceID);
 
-	//Was the Goal altered
+	//Flag set from outside of the IK-Chain - wether the target has changed, is set to false by the IK-Chain
 	bool GoalChanged=true;
 
-	//is the GoalPoint a World Coordinate?
+	//is the transfered new GoalPoint a World Coordinate?
 	bool isWorldCoordinate =false;
 
 	//Helper Function to inialize the Path recursive
 	bool recPiecePathExplore(LocalModelPiece* parentLocalModel, unsigned int parentPiece, unsigned int endPieceNumber, int depth);
 	bool initializePiecePath(LocalModelPiece* startPiece, unsigned int startPieceID, unsigned int endPieceID);
 	
-	//Checks wether a Piece is part of this chain
+	//Checks wether a Piece is part of this IK-Chain
 	bool isValidIKPiece(float pieceID);
 
 	//Transfers a global Worldspace coordinate into a unitspace coordinate
 	Point3f TransformGoalToUnitspace(Point3f goal);
 
-	//IK is active or paused
+	//IK is active or paused- paused IK-Chains can jolt 
 	bool IKActive ;
 
 	//Setter
 	void SetActive (bool isActive);
 
-	//Solves the Inverse Kinematik Chain for a new goal point
-	//Returns wether a ik-solution could be found
+	//Solves the Inverse Kinematik Chain for a goal point
+	//arguments: frames - current active frame, becomes Startframe on  new motion
 	void solve(float frames );
+	
 	//apply the resolved Kinematics to the actual Model
 	void applyIkTransformation(MotionBlend motionBlendMethod);
 
 	//Set Piece Limitations
-
 	//Get the Next PieceNumber while building the chain
 	int GetNextPieceNumber(float PieceNumber);
-
 
 	//Creates a Jacobi Matrice
 	Matrix<float,1,3> compute_jacovian_segment(int seg_num, Point3f goal_point, Vector3f  angle);
@@ -76,19 +75,18 @@ public:
 	Point3f calculate_end_effector(int segment_num = -1);
 
 	//Gets the basic bone rotation for the piece 
-	//TODO it would be a great performance saviour if there was a flag 
 	Point3f GetBoneBaseRotation(void);
 
 	//unit this IKChain belongs too
 	CUnit* unit;
 
-	//Identifier of the Kinematik Chain
+	//Identifier of the kinematik Chain
 	float IkChainID;
 
 	//The baseposition in WorldCoordinats
 	Point3f base;
 
-	//Set the Anglke for the Transformation matrice
+	//Set the Angle for the Transformation matrice
 	void SetTransformation(float valX, float valY, float valZ);
 
 	// the goal Point also in World Coordinats
@@ -106,9 +104,6 @@ public:
 	//Initial Default direction of the Kinematik system
 	Point3f vecDefaultlDirection;
 
-	//Size of Segment
-	//int segment_size ;
-
 	//Plots the whole IK-Chain
 	void print();
 	
@@ -119,7 +114,7 @@ public:
 	~IkChain();
 private:
 
-	//TODO find out what this one does
+	//Calculates the endpoint in 3d Coords for a given Piece after the movement
 	Point3f calculateEndEffector(int Segment = -1);
 
 	//Get the max Lenfgth of the IK Chain
