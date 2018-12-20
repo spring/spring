fa3eccdf84ae362be48b439269086f1b2cd11186
Gajo Petrovic <gajo01@gmail.com>
2018-12-20 20:32:03 +0900

    Fix simple map generator in form of a few hacks: - workaround (hacky & slow) that prevents segfault when generating maps   - changing mapSeed with Spring.Reload has no effect (probably because of caching? avoid caching somehow altogether?)   - to reload with new mapSeed, specify a different mapName as well - generate maps of arbitrary size (point out to some issues there..)
    Further details in the source code comments.
    
    Not the pretties commit but it gets us something usable and useful right now.
    I currently use it as https://github.com/Spring-SpringBoard/SpringBoard-Core/blob/42a903cd66769f799ecae39eab418dccea7079c2/scen_edit/view/dialog/new_project_dialog.lua#L157-L183

--- rts/Map/Generation/SimpleMapGenerator.cpp
+++ rts/Map/Generation/SimpleMapGenerator.cpp
@@ -2,6 +2,10 @@
 
 #include "SimpleMapGenerator.h"
 
+// #include <string>
+
+// #include "System/Log/ILog.h"
+
 CSimpleMapGenerator::CSimpleMapGenerator(const CGameSetup* setup) : CMapGenerator(setup)
 {
 	GenerateInfo();
@@ -11,7 +15,48 @@ CSimpleMapGenerator::~CSimpleMapGenerator() = default;
 
 void CSimpleMapGenerator::GenerateInfo()
 {
-	mapSize = int2(5, 5);
+// The below block of code doesn't work because mapOpts/modOpts are not parsed at this stage?
+// Even though it seems like they are?
+// .. in any case, this is important and necessary for any serious (Lua-based) map generator
+/*
+
+	const auto& mapOpts = setup->GetMapOptions();
+	for (const auto& mapOpt : mapOpts) {
+		LOG_L(L_WARNING, "%s:%s", mapOpt.first.c_str(), mapOpt.second.c_str());
+	}
+	for (const auto& modOpt : setup->GetModOptions()) {
+		LOG_L(L_WARNING, "%s:%s", modOpt.first.c_str(), modOpt.second.c_str());
+	}
+	const std::string* newMapXStr = mapOpts.try_get("new_map_x");
+	const std::string* newMapZStr = mapOpts.try_get("new_map_z");
+	if (newMapXStr == nullptr || newMapZStr == nullptr) {
+		mapSize = int2(5, 5);
+		return;
+	}
+
+	try {
+		const int newMapX = std::stoi(*newMapXStr);
+		const int newMapZ = std::stoi(*newMapZStr);
+		if (newMapX > 0 && newMapZ > 0) {
+			mapSize = int2(newMapX, newMapZ);
+		}
+	} catch (...) {
+		mapSize = int2(5, 5);
+	}
+*/
+
+	// Viola: our hack. We extract map sizeX and sizeZ from mapSeed (assuming it has it packed)
+	// This is done mainly as a temporary measure, so we can test it out without touching more files
+	int mapSeed = setup->mapSeed;
+	const int newMapX = mapSeed / 1000;
+	const int newMapZ = mapSeed % 1000;
+	// The limit here is at least 1, but I had it crash on small map sizes (<4)
+	// there's probably some memory corruption going on (I got 'corrupted size vs. prev_size)
+	if (newMapX < 1 || newMapZ < 1) {
+		mapSize = int2(5, 5);
+	} else {
+		mapSize = int2(newMapX, newMapZ);
+	}
 }
 
 void CSimpleMapGenerator::GenerateMap()
