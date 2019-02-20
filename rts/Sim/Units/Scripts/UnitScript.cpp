4ed5ddcf61300d6fce17bbca59d7109d3acdf452
Eshed <ashdnazg@gmail.com>
2019-02-20 15:53:06 +0200

    SFX.ABSOLUTE => SFX.GLOBAL, fix changelog

--- rts/Sim/Units/Scripts/UnitScript.cpp
+++ rts/Sim/Units/Scripts/UnitScript.cpp
@@ -551,10 +551,10 @@ bool CUnitScript::EmitAbsSFX(int sfxType, const float3& absPos, const float3& ab
 		} break;
 
 		default: {
-			if ((sfxType & SFX_ABSOLUTE) != 0) {
+			if ((sfxType & SFX_GLOBAL) != 0) {
 				// emit defined explosion-generator (can only be custom, not standard)
 				// index is made valid by callee, an ID of -1 means CEG failed to load
-				explGenHandler.GenExplosion(sfxType - SFX_ABSOLUTE, absPos, absDir, unit->cegDamage, 1.0f, 0.0f, unit, nullptr);
+				explGenHandler.GenExplosion(sfxType - SFX_GLOBAL, absPos, absDir, unit->cegDamage, 1.0f, 0.0f, unit, nullptr);
 				return true;
 			}
 			if ((sfxType & SFX_CEG) != 0) {
