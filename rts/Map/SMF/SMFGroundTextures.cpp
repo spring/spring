f6f05c26301185656f85aa92cfea1f771b40a0d6
rtri <rtri@users.noreply.github.com>
2019-04-16 10:51:03 +0000

    fix #6203

--- rts/Map/SMF/SMFGroundTextures.cpp
+++ rts/Map/SMF/SMFGroundTextures.cpp
@@ -222,7 +222,7 @@ void CSMFGroundTextures::LoadSquareTextures(const int minLevel, const int maxLev
 	#endif
 
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR              );
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
