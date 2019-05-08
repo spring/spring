a2d12aa7d26c075c17a92fd4f898ca7c9d5a0c0f
GoogleFrog <googl3frog@gmail.com>
2019-05-08 11:01:10 +1000

    Waypoint skipping fix for Mantis 6206. (#455)
    * Waypoint skipping fix.
    * invert facePoint condition

--- rts/Sim/MoveTypes/GroundMoveType.cpp
+++ rts/Sim/MoveTypes/GroundMoveType.cpp
@@ -1589,9 +1589,11 @@ bool CGroundMoveType::CanSetNextWayPoint() {
 			// note: can somehow cause units to move in circles near obstacles
 			// (mantis3718) if rectangle is too generous in size
 			const bool rangeTest = owner->moveDef->TestMoveSquareRange(owner, float3::min(cwp, pos), float3::max(cwp, pos), owner->speed, true, true, true);
-			const bool allowSkip = ((pos - cwp).SqLength() <= Square(SQUARE_SIZE) || (pos - cwp).dot(flatFrontDir) < 0.0f);
+			const bool facePoint = ((cwp - pos).dot(flatFrontDir) >= 0.0f);
+			const bool allowSkip = ((cwp - pos).SqLength() <= Square(SQUARE_SIZE));
 
-			if (!rangeTest && !allowSkip)
+			// CanSetNextWayPoint may return true if (allowSkip || (rangeTest && facePoint))
+			if (!allowSkip && (!rangeTest || !facePoint))
 				return false;
 		}
 
