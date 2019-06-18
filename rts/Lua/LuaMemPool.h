26e59ab48ed431aa85ac9a0bb415e8482c4f06be
sprunk <gnurps@o2.pl>
2019-06-18 20:35:27 +0200

    std::array contains an array (`-Wmissing-braces`) (#464)
    * std::array contains an array (-Wmissing-braces)

--- rts/Lua/LuaMemPool.h
+++ rts/Lua/LuaMemPool.h
@@ -114,18 +114,18 @@ private:
 		static constexpr uint32_t NUM_POOLS = 32;
 
 		// all N's intentionally over-dimensioned by a factor 1<<10
-		static constexpr std::array<uint32_t, NUM_POOLS> NUM_CHUNKS = {
+		static constexpr std::array<uint32_t, NUM_POOLS> NUM_CHUNKS = {{
 			1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,  1 << 12,
 			1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,  1 << 13,
 			1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,  1 << 14,
 			1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,  1 << 15,
-		};
-		static constexpr std::array<uint32_t, NUM_POOLS> NUM_PAGES = {
+		}};
+		static constexpr std::array<uint32_t, NUM_POOLS> NUM_PAGES = {{
 			1 << 15,  1 << 15,  1 << 14,  1 << 14,  1 << 13,  1 << 13,  1 << 12,  1 << 12,
 			1 << 11,  1 << 11,  1 << 10,  1 << 10,  1 <<  9,  1 <<  9,  1 <<  8,  1 <<  8,
 			1 <<  7,  1 <<  7,  1 <<  6,  1 <<  6,  1 <<  5,  1 <<  5,  1 <<  4,  1 <<  4,
 			1 <<  3,  1 <<  3,  1 <<  2,  1 <<  2,  1 <<  1,  1 <<  1,  1 <<  0,  1 <<  0,
-		};
+		}};
 
 		std::array<uint8_t[sizeof(FixedDynMemPool<0, NUM_CHUNKS[NUM_POOLS - 1], 0>)], NUM_POOLS> memPools;
 		std::array<void*, NUM_POOLS> poolPtrs;
