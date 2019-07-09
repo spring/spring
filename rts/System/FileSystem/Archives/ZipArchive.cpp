a1ac5bafe5fd76a477b8b0848a1ae81e72ffa5d9
rtri <none@none>
2019-07-09 21:12:33 +0200

    more of 3da923ee

--- rts/System/FileSystem/Archives/ZipArchive.cpp
+++ rts/System/FileSystem/Archives/ZipArchive.cpp
@@ -5,7 +5,7 @@
 
 #include <algorithm>
 #include <stdexcept>
-#include <assert.h>
+#include <cassert>
 
 #include "System/StringUtil.h"
 #include "System/Log/ILog.h"
@@ -19,71 +19,67 @@ IArchive* CZipArchiveFactory::DoCreateArchive(const std::string& filePath) const
 
 CZipArchive::CZipArchive(const std::string& archiveName): CBufferedArchive(archiveName)
 {
-	zip = unzOpen(archiveName.c_str());
+	std::lock_guard<spring::mutex> lck(archiveLock);
 
-	if (zip == nullptr) {
-		LOG_L(L_ERROR, "Error opening \"%s\"", archiveName.c_str());
+	if ((zip = unzOpen(archiveName.c_str())) == nullptr) {
+		LOG_L(L_ERROR, "[%s] error opening \"%s\"", __func__, archiveName.c_str());
 		return;
 	}
 
+	unz_global_info64 globalZipInfo;
+
+	memset(&globalZipInfo, 0, sizeof(globalZipInfo));
+	unzGetGlobalInfo64(zip, &globalZipInfo);
+
 	// We need to map file positions to speed up opening later
+	fileEntries.reserve(globalZipInfo.number_entry);
+
 	for (int ret = unzGoToFirstFile(zip); ret == UNZ_OK; ret = unzGoToNextFile(zip)) {
 		unz_file_info info;
 		char fName[512];
 
 		unzGetCurrentFileInfo(zip, &info, fName, sizeof(fName), nullptr, 0, nullptr, 0);
 
-		const std::string fLowerName = StringToLower(fName);
-		if (fLowerName.empty())
+		if (fName[0] == 0)
 			continue;
 
-		if ((fLowerName.back() == '/') || (fLowerName.back() == '\\'))
-			continue; // exclude directory names
+		const size_t fNameLen = strlen(fName);
 
-		FileData fd;
+		// exclude directory names
+		if ((fName[fNameLen - 1] == '/') || (fName[fNameLen - 1] == '\\'))
+			continue;
+
+		FileEntry fd;
 		unzGetFilePos(zip, &fd.fp);
+
 		fd.size = info.uncompressed_size;
 		fd.origName = fName;
 		fd.crc = info.crc;
-		fileData.push_back(fd);
-		lcNameIndex[fLowerName] = fileData.size() - 1;
+
+		lcNameIndex.emplace(StringToLower(fd.origName), fileEntries.size());
+		fileEntries.emplace_back(std::move(fd));
 	}
 }
 
 CZipArchive::~CZipArchive()
 {
+	std::lock_guard<spring::mutex> lck(archiveLock);
+
 	if (zip != nullptr) {
 		unzClose(zip);
 		zip = nullptr;
 	}
 }
 
-bool CZipArchive::IsOpen()
-{
-	return (zip != nullptr);
-}
-
-unsigned int CZipArchive::NumFiles() const
-{
-	return fileData.size();
-}
 
 void CZipArchive::FileInfo(unsigned int fid, std::string& name, int& size) const
 {
 	assert(IsFileId(fid));
 
-	name = fileData[fid].origName;
-	size = fileData[fid].size;
+	name = fileEntries[fid].origName;
+	size = fileEntries[fid].size;
 }
 
-#if 0
-unsigned int CZipArchive::GetCrc32(unsigned int fid)
-{
-	assert(IsFileId(fid));
-
-	return fileData[fid].crc;
-}
-#endif
 
 // To simplify things, files are always read completely into memory from
 // the zip-file, since zlib does not provide any way of reading more
@@ -94,9 +90,10 @@ int CZipArchive::GetFileImpl(unsigned int fid, std::vector<std::uint8_t>& buffer
 	if (zip == nullptr)
 		return -4;
 
+	// assert(archiveLock.locked());
 	assert(IsFileId(fid));
 
-	unzGoToFilePos(zip, &fileData[fid].fp);
+	unzGoToFilePos(zip, &fileEntries[fid].fp);
 
 	unz_file_info fi;
 	unzGetCurrentFileInfo(zip, &fi, nullptr, 0, nullptr, 0, nullptr, 0);
@@ -109,7 +106,7 @@ int CZipArchive::GetFileImpl(unsigned int fid, std::vector<std::uint8_t>& buffer
 
 	int ret = 1;
 
-	if (!buffer.empty() && unzReadCurrentFile(zip, &buffer[0], fi.uncompressed_size) != fi.uncompressed_size)
+	if (!buffer.empty() && unzReadCurrentFile(zip, buffer.data(), buffer.size()) != buffer.size())
 		ret -= 2;
 	if (unzCloseCurrentFile(zip) == UNZ_CRCERROR)
 		ret -= 1;
@@ -119,3 +116,4 @@ int CZipArchive::GetFileImpl(unsigned int fid, std::vector<std::uint8_t>& buffer
 
 	return ret;
 }
+
