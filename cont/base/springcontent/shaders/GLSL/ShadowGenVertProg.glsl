eb635ed5b155d3c8a67f48960e6f8da674019a56
lhog <ccna@mail.ru>
2019-08-13 00:51:03 +0300

    fix https://github.com/ZeroK-RTS/CrashReports/issues/23539 (#470)
    > error: type mismatch for varying parameter (named {shadowViewMat, shadowProjMat, vertexModelPos}) between shader stages

--- cont/base/springcontent/shaders/GLSL/ShadowGenVertProg.glsl
+++ cont/base/springcontent/shaders/GLSL/ShadowGenVertProg.glsl
@@ -1,3 +1,10 @@
+#if (GL_FRAGMENT_PRECISION_HIGH == 1)
+// ancient GL3 ATI drivers confuse GLSL for GLSL-ES and require this
+precision highp float;
+#else
+precision mediump float;
+#endif
+
 uniform vec4 shadowParams;
 
 #ifdef SHADOWGEN_PROGRAM_TREE_NEAR
