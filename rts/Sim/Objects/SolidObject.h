2462832d993ffa917960088f4344be85dfc9fc55
rtri <none@none>
2019-08-12 23:48:24 +0200

    fix #6277

--- rts/Sim/Objects/SolidObject.h
+++ rts/Sim/Objects/SolidObject.h
@@ -236,6 +236,7 @@ public:
 
 	YardMapStatus GetGroundBlockingMaskAtPos(float3 gpos) const;
 
+	bool FootPrintOnGround() const;
 	bool BlockMapPosChanged() const { return (groundBlockPos != pos); }
 
 	bool IsOnGround   () const { return (HasPhysicalStateBit(PSTATE_BIT_ONGROUND   )); }
