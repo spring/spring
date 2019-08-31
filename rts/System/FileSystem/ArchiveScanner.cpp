5fc162ae0daad7566e4a50b650dabdfe7965e65f
rtri <none@none>
2019-08-31 02:37:44 +0200

    bump ArchiveCache version

--- rts/System/FileSystem/ArchiveScanner.cpp
+++ rts/System/FileSystem/ArchiveScanner.cpp
@@ -51,7 +51,7 @@ LOG_REGISTER_SECTION_GLOBAL(LOG_SECTION_ARCHIVESCANNER)
  * but mapping them all, every time to make the list is)
  */
 
-constexpr static int INTERNAL_VER = 15;
+constexpr static int INTERNAL_VER = 16;
 
 
 /*
