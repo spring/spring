7318ff08e59387c283d1dfa6fea750cb837cd1c6
rtri <none@none>
2019-11-09 16:50:10 +0100

    close #483

--- cont/base/springcontent/shaders/GLSL/SMFFragProg4.glsl
+++ cont/base/springcontent/shaders/GLSL/SMFFragProg4.glsl
@@ -377,10 +377,10 @@ void main() {
 
 		detailColor = vec4(splatDetailStrength.y);
 
-		// convert the splat detail normal to worldspace,
-		// then mix it with the regular one (note: needs
-		// another normalization?)
-		normalVec = mix(normalVec, normalize(stnMatrix * splatDetailNormal.xyz), splatDetailStrength.x);
+		// convert the splat detail normal to world-space, then
+		// mix it with the regular one, then normalize it again
+		// to get correct specular and diffuse highlights
+		normalVec = normalize(mix(normalVec, normalize(stnMatrix * splatDetailNormal.xyz), splatDetailStrength.x));
 	}
 	#endif
 
