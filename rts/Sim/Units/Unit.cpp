5ddef55bba01427292d729731ba24877cf4c7482
GoogleFrog <googl3frog@gmail.com>
2020-03-21 14:27:25 +1100

    Added the modrule decloakRequiresLineOfSight. This controls whether sight distance is used as a filter on proximity cloak checks, allowing for cheap implementations of the behaviour desired by FLOZi for s44 and myself for ZK. Added a missing comment for alwaysVisibleOverridesCloaked.
    See:
     * https://springrts.com/mantis/view.php?id=6306
     * https://github.com/spring/spring/commit/ca1557cdee3fe4075ff9d987dfe0926a229146d6
     * https://springrts.com/mantis/view.php?id=6374

--- rts/Sim/Units/Unit.cpp
+++ rts/Sim/Units/Unit.cpp
@@ -2395,7 +2395,7 @@ bool CUnit::GetNewCloakState(bool stunCheck) {
 	const CUnit* closestEnemy = this;
 
 	if (!stunCheck)
-		closestEnemy = CGameHelper::GetClosestEnemyUnitNoLosTest(this, midPos, decloakDistance, allyteam, unitDef->decloakSpherical, false);
+		closestEnemy = CGameHelper::GetClosestEnemyUnitNoLosTest(this, midPos, decloakDistance, allyteam, unitDef->decloakSpherical, modInfo.decloakRequiresLineOfSight);
 
 	return (eventHandler.AllowUnitCloak(this, closestEnemy));
 }
