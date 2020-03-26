5ad992a1d3c2f43eb63699d7c4e9da38e0be3ee0
Beherith <mysterme@gmail.com>
2020-03-26 16:56:29 +0100

    Fix for RETESSELLATE_MODE == 2 too

--- rts/Map/SMF/ROAM/RoamMeshDrawer.cpp
+++ rts/Map/SMF/ROAM/RoamMeshDrawer.cpp
@@ -205,8 +205,10 @@ void CRoamMeshDrawer::Update()
 			if (p.IsVisible(cam)) {
 				if (tesselMesh |= (pvflags[i] == 0))
 					pvflags[i] = 1;
-				if (tesselMesh |= p.IsDirty())
+				if (p.IsDirty()){
 					p.ComputeVariance();
+					tesselMesh = true;
+				}
 			} else {
 				pvflags[i] = 0;
 			}
