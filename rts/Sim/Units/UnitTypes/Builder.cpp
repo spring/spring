aeb39d69402f81cfe7cc9cbf00b1a45479629230
sprunk <gnurps@o2.pl>
2020-04-13 16:11:47 +0200

    Minor cleanup

--- rts/Sim/Units/UnitTypes/Builder.cpp
+++ rts/Sim/Units/UnitTypes/Builder.cpp
@@ -722,16 +722,15 @@ bool CBuilder::StartBuild(BuildInfo& buildInfo, CFeature*& feature, bool& inWait
 			// closest to us
 			const CGroundBlockingObjectMap::BlockingMapCell& cell = groundBlockingObjectMap.GetCellUnsafeConst(buildInfo.pos);
 
-			const CSolidObject* o = nullptr;
 			const CUnit* u = nullptr;
 
 			// look for any blocking assistable buildee at build.pos
 			for (size_t i = 0, n = cell.size(); i < n; i++) {
-				const CUnit* cu = dynamic_cast<const CUnit*>(o = cell[i]);
+				const CUnit* cu = dynamic_cast<const CUnit*>(cell[i]);
 
 				if (cu == nullptr)
 					continue;
-				if (allyteam != cu->allyteam)
+				if (allyteam != cu->allyteam) // NB: not teamHandler.Ally because allies don't share LoS
 					return false; // Enemy units that block always block the cell
 				if (!CanAssistUnit(cu, buildInfo.def))
 					continue;
