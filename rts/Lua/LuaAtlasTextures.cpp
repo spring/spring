98691e8ceffc955b3f67b2f45631f6192dcb861c
LHoG <1476261+lhog@users.noreply.github.com>
2021-01-13 03:22:30 +0300

    +=rts/Lua/LuaAtlasTextures.*

---
+++ rts/Lua/LuaAtlasTextures.cpp
@@ -0,0 +1,93 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#include <sstream>
+
+#include "LuaTextures.h"
+#include "Rendering/GlobalRendering.h"
+#include "Rendering/Textures/TextureAtlas.h"
+#include "System/Log/ILog.h"
+#include "LuaAtlasTextures.h"
+
+void LuaAtlasTextures::Clear()
+{
+	for (auto& kv : textureAtlasMap)
+		spring::SafeDelete(kv.second);
+
+	textureAtlasMap.clear();
+}
+
+std::string LuaAtlasTextures::Create(const std::string& name, const int xsize, const int ysize, const int allocatorType)
+{
+	CTextureAtlas* atlas = new CTextureAtlas(allocatorType, xsize, ysize);
+	atlas->SetName(name);
+
+	textureAtlasMap[lastIndex++] = atlas;
+
+	std::ostringstream ss;
+	ss << prefix << lastIndex;
+
+	return ss.str();
+}
+
+bool LuaAtlasTextures::Delete(const std::string& idStr)
+{
+	spring::unsynced_map<size_t, CTextureAtlas*>::const_iterator iter;
+	if (GetIterator(idStr, iter))
+		return textureAtlasMap.erase(iter->first);
+
+	return false;
+}
+
+CTextureAtlas* LuaAtlasTextures::GetAtlasByIdx(const size_t idx) const
+{
+	const auto iter = textureAtlasMap.find(idx);
+	if (iter == textureAtlasMap.end())
+		return nullptr;
+
+	return iter->second;
+}
+
+size_t LuaAtlasTextures::GetIdx(const std::string& idStr) const
+{
+	spring::unsynced_map<size_t, CTextureAtlas*>::const_iterator iter;
+	if (GetIterator(idStr, iter))
+		return iter->first;
+
+	return size_t(-1);
+}
+
+bool LuaAtlasTextures::GetIterator(const std::string& idStr, spring::unsynced_map<size_t, CTextureAtlas*>::iterator& iter)
+{
+	if (idStr[0] != prefix)
+		return false;
+
+	try {
+		int numID = std::stoi(idStr.substr(1));
+		iter = textureAtlasMap.find(numID);
+		if (iter == textureAtlasMap.end())
+			return false;
+
+		return true;
+	}
+	catch (const std::exception&) {
+		return false;
+	}
+}
+
+bool LuaAtlasTextures::GetIterator(const std::string& idStr, spring::unsynced_map<size_t, CTextureAtlas*>::const_iterator& iter) const
+{
+	if (idStr[0] != prefix)
+		return false;
+
+	try {
+		int numID = std::stoi(idStr.substr(1));
+		iter = textureAtlasMap.find(numID);
+		if (iter == textureAtlasMap.cend())
+			return false;
+
+		return true;
+	}
+	catch (const std::exception&) {
+		return false;
+	}
+}
---
+++ rts/Lua/LuaAtlasTextures.h
@@ -0,0 +1,42 @@
+/* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
+
+#ifndef LUA_ATLAS_TEXTURES_H
+#define LUA_ATLAS_TEXTURES_H
+
+#include <string>
+#include <vector>
+
+#include "Rendering/GL/myGL.h"
+#include "System/UnorderedMap.hpp"
+#include "System/SafeUtil.h"
+
+struct CTextureAtlas;
+struct AtlasedTexture;
+
+//namespace LuaTextures { struct Texture; }
+
+class LuaAtlasTextures {
+public:
+	static const char prefix = '*';
+
+	~LuaAtlasTextures() { Clear(); }
+	LuaAtlasTextures() {
+		textureAtlasMap.reserve(32);
+	}
+
+	void Clear();
+
+	std::string Create(const std::string& name, const int xsize, const int ysize, const int allocatorType = 0);
+	bool Delete(const std::string& idStr);
+	CTextureAtlas* GetAtlasByIdx(const size_t idx) const;
+	size_t GetIdx(const std::string& idStr) const;
+
+private:
+	bool GetIterator(const std::string& idStr, spring::unsynced_map<size_t, CTextureAtlas*>::iterator& iter);
+	bool GetIterator(const std::string& idStr, spring::unsynced_map<size_t, CTextureAtlas*>::const_iterator& iter) const;
+	// maps names to textureVec indices
+	spring::unsynced_map<size_t, CTextureAtlas*> textureAtlasMap;
+	size_t lastIndex = 1;
+};
+
+#endif /* LUA_ATLAS_TEXTURES_H */
