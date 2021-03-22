7fe0c9a66f04cda5b37f6557c06e993e2c08534a
LHoG <1476261+lhog@users.noreply.github.com>
2021-03-23 00:06:31 +0300

    Remove copy-paste non-sense. Thx Sprunk

--- rts/System/Cpp11Compat.hpp
+++ rts/System/Cpp11Compat.hpp
@@ -16,8 +16,7 @@ namespace spring {
 		typename std::iterator_traits<RandomIt>::difference_type i, n;
 		n = last - first;
 		for (i = n - 1; i > 0; --i) {
-			using std::swap;
-			swap(first[i], first[r(i + 1)]);
+			std::swap(first[i], first[r(i + 1)]);
 		}
 	}
 };
