701b8187a7922f0dbe8695c2478761c43abaac68
LHoG <1476261+lhog@users.noreply.github.com>
2021-05-14 10:19:00 +0300

    Enable saving demos with UTC timestamp in the filename

--- rts/System/TimeUtil.cpp
+++ rts/System/TimeUtil.cpp
@@ -7,31 +7,39 @@
 
 #include <string>
 #include <ctime>
+#include <functional>
 
-std::string CTimeUtil::GetCurrentTimeStr()
+std::string CTimeUtil::GetCurrentTimeStr(bool utc)
 {
-	struct tm* lt;
+	struct tm lt;
 	// Get time as long integer
 	__time64_t long_time = GetCurrentTime();
-	// Convert to local time
-	lt = _localtime64(&long_time);
+
+	// Convert to UTC or local time
+#ifdef _WIN32
+	static decltype(_localtime64_s)* convFunc[] = { &_localtime64_s, &_gmtime64_s };
+	convFunc[utc](&lt, &long_time);
+#else
+	static decltype(localtime_r)* convFunc[] = { &localtime_r, &gmtime_r };
+	convFunc[utc](&long_time, &lt);
+#endif
 
 	// Don't see how this can happen (according to docs _localtime64 only returns
 	// nullptr if long_time is before 1/1/1970...) but a user's stacktrace indicated
 	// nullptr newtime in the snprintf line...
-	if (lt == nullptr) {
+	if (&lt == nullptr) {
 		throw content_error("error: _localtime64 returned NULL");
 	}
 
 	const size_t str_maxSize = 512;
 	char str[str_maxSize];
 	SNPRINTF(str, str_maxSize, "%04i%02i%02i_%02i%02i%02i",
-		lt->tm_year + 1900,
-		lt->tm_mon + 1,
-		lt->tm_mday,
-		lt->tm_hour,
-		lt->tm_min,
-		lt->tm_sec);
+		lt.tm_year + 1900,
+		lt.tm_mon + 1,
+		lt.tm_mday,
+		lt.tm_hour,
+		lt.tm_min,
+		lt.tm_sec);
 
 	return str;
 }
