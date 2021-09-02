211ed1a4e641a6fdeb98711dfe11e35e2b08ae20
LHoG <1476261+lhog@users.noreply.github.com>
2021-09-02 21:03:23 +0300

    locale desync extra safety

--- rts/lib/lua/src/loslib.cpp
+++ rts/lib/lua/src/loslib.cpp
@@ -229,14 +229,7 @@ static int os_difftime (lua_State *L) {
 
 
 static int os_setlocale (lua_State *L) {
-  static const int cat[] = {LC_ALL, LC_COLLATE, LC_CTYPE, LC_MONETARY,
-                      LC_NUMERIC, LC_TIME};
-  static const char *const catnames[] = {"all", "collate", "ctype", "monetary",
-     "numeric", "time", NULL};
-  const char *l = luaL_optstring(L, 1, NULL);
-  int op = luaL_checkoption(L, 2, "all", catnames);
-  lua_pushstring(L, setlocale(cat[op], l));
-  return 1;
+  return 0;
 }
 
 
