207846c2780ea42f25084aa844cbfe163ae1b185
LHoG <1476261+lhog@users.noreply.github.com>
2021-09-16 21:21:14 +0300

    Ported VBO.h from BAR-next

--- rts/Rendering/GL/VAO.h
+++ rts/Rendering/GL/VAO.h
@@ -6,7 +6,7 @@
 #include <cstdint>
 #include <utility>
 
-struct VAO {
+class VAO {
 public:
 	static bool IsSupported();
 public:
@@ -18,7 +18,8 @@ public:
 	VAO& operator = (const VAO& v) = delete;
 	VAO& operator = (VAO&& v) noexcept { id = v.id; v.id = 0; return *this; }
 
-	uint32_t GetId() const { Generate(); return id; }
+	uint32_t GetId() const { Generate(); return GetIdRaw(); }
+	uint32_t GetIdRaw() const { return id; }
 
 	void Generate() const;
 
