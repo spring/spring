ccd8aa9ec6d07b87df7f2a2637b1c1383b506e35
LHoG <1476261+lhog@users.noreply.github.com>
2021-11-23 00:47:32 +0300

    One less magic constant

--- rts/Sim/Weapons/WeaponMemPool.h
+++ rts/Sim/Weapons/WeaponMemPool.h
@@ -5,12 +5,17 @@
 
 #include "Sim/Misc/GlobalConstants.h"
 #include "System/MemPoolTypes.h"
+#include "System/SpringMath.h"
+
+#include "Sim/Weapons/PlasmaRepulser.h"
+
+static constexpr size_t WMP_S = AlignUp(sizeof(CPlasmaRepulser), 4); //biggest in size
 
 #if (defined(__x86_64) || defined(__x86_64__) || defined(_M_X64))
 // NOTE: ~742MB, way too big for 32-bit builds
-typedef StaticMemPool<MAX_UNITS * MAX_WEAPONS_PER_UNIT, 760> WeaponMemPool;
+typedef StaticMemPool<MAX_UNITS * MAX_WEAPONS_PER_UNIT, WMP_S> WeaponMemPool;
 #else
-typedef FixedDynMemPool<760, (MAX_UNITS * MAX_WEAPONS_PER_UNIT) / 4000, (MAX_UNITS * MAX_WEAPONS_PER_UNIT) / 256> WeaponMemPool;
+typedef FixedDynMemPool<WMP_S, (MAX_UNITS * MAX_WEAPONS_PER_UNIT) / 4000, (MAX_UNITS * MAX_WEAPONS_PER_UNIT) / 256> WeaponMemPool;
 #endif
 
 extern WeaponMemPool weaponMemPool;
