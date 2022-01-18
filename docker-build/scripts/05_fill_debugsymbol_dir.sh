4c4b837a74fd7c0f4aaaeac8b37551d6d9996f8f
LHoG <1476261+lhog@users.noreply.github.com>
2022-01-18 22:26:39 +0300

    Do not fail in case binary stripping has failed

--- docker-build/scripts/05_fill_debugsymbol_dir.sh
+++ docker-build/scripts/05_fill_debugsymbol_dir.sh
@@ -9,14 +9,21 @@ else
     exit 1
 fi
 
+set +e
 for tostripfile in ${EXECUTABLES}; do
     if [ -f ${tostripfile} ]; then
-        if ! objdump -h ${tostripfile} | grep -q .gnu_debuglink; then
+        objdump -h ${tostripfile} | grep -q .gnu_debuglink
+		od=$?
+        objcopy --only-keep-debug ${tostripfile} > /dev/null
+		oc=$?
+		echo "od = $od"
+		echo "oc = $oc"
+        if [[ $od -ne 0 && $oc -eq 0 ]]; then
             debugfile=$(dirname $tostripfile)/$(echo $(basename $tostripfile) | cut -f 1 -d '.').dbg
             echo "stripping ${tostripfile}, producing ${debugfile}"
-            (objcopy --only-keep-debug ${tostripfile} ${debugfile} && \
-                strip --strip-debug --strip-unneeded ${tostripfile} && \
-                objcopy --add-gnu-debuglink=${debugfile} ${tostripfile})
+            objcopy --only-keep-debug ${tostripfile} ${debugfile} && \
+            strip --strip-debug --strip-unneeded ${tostripfile} && \
+            objcopy --add-gnu-debuglink=${debugfile} ${tostripfile}
         else
             echo "not stripping ${tostripfile}"
         fi
