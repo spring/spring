adbcc4726f5037397be915849bc0dd48af904f0b
LHoG <1476261+lhog@users.noreply.github.com>
2022-01-23 13:31:32 +0300

    Add spring::UnfreezeSpring() call and add it to ModelPreloader calls

--- rts/Rendering/Models/ModelPreloader.cpp
+++ rts/Rendering/Models/ModelPreloader.cpp
@@ -3,11 +3,13 @@
 #include "Sim/Units/UnitDefHandler.h"
 #include "Sim/Features/FeatureDefHandler.h"
 #include "Sim/Weapons/WeaponDefHandler.h"
+#include "System/Misc/UnfreezeSpring.h"
 
 void ModelPreloader::LoadUnitDefs()
 {
 	for (const auto& def : unitDefHandler->GetUnitDefsVec()) {
 		def.LoadModel();
+		spring::UnfreezeSpring(WDT_LOAD, 10);
 	}
 }
 
@@ -15,6 +17,7 @@ void ModelPreloader::LoadFeatureDefs()
 {
 	for (const auto& def : featureDefHandler->GetFeatureDefsVec()) {
 		def.LoadModel();
+		spring::UnfreezeSpring(WDT_LOAD, 10);
 	}
 }
 
@@ -22,5 +25,6 @@ void ModelPreloader::LoadWeaponDefs()
 {
 	for (const auto& def : weaponDefHandler->GetWeaponDefsVec()) {
 		def.LoadModel();
+		spring::UnfreezeSpring(WDT_LOAD, 10);
 	}
 }
---
+++ rts/System/Misc/UnfreezeSpring.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "System/Platform/Watchdog.h"
+#include "System/Input/InputHandler.h"
+
+namespace spring {
+	static void UnfreezeSpring(WatchdogThreadnum num = WDT_MAIN, int everyN = 0) {
+		static int counter = 0;
+		static int pEveryN = -1;
+		if (everyN != pEveryN) {
+			counter = 0;
+			pEveryN = everyN;
+		}
+
+		if (counter % everyN == 0) {
+			Watchdog::ClearTimer(num);
+			input.PushEvents();
+		}
+		++counter;
+	}
+}
