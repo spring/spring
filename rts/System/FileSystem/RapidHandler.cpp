685455372b7abf90b7267d99cc4b55aedfec8246
Marek Rusinowski <marekrusinowski@gmail.com>
2022-07-29 00:28:33 +0200

    Add option for rapid tag resolution domain preference order (#325)
    * Add option for rapid tag resolution domain preference order
    
    There can be multiple domains from which the packages are being
    downloaded, for example when additonal mirrors are used. This adds
    support for picking from which domain version.gz files should be
    used first.
    
    * Switch to std::vector in ParseRapidTagResolutionOrder
    
    * Add comment about how path to versions.gz looks like
    
    * Make sure repos.springrts.com is always in resolution order

--- rts/System/FileSystem/RapidHandler.cpp
+++ rts/System/FileSystem/RapidHandler.cpp
@@ -1,10 +1,16 @@
 /* This file is part of the Spring engine (GPL v2 or later), see LICENSE.html */
 
+#include <algorithm>
+#include <cstddef>
+#include <filesystem>
+#include <limits>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "DataDirsAccess.h"
 #include "FileQueryFlags.h"
+#include "System/Config/ConfigHandler.h"
 #include "System/Log/ILog.h"
 
 #include <zlib.h>
@@ -82,17 +88,52 @@ static bool GetRapidEntry(const std::string& file, RapidEntry* re, Lambda p)
 	return false;
 }
 
+CONFIG(std::string, RapidTagResolutionOrder)
+	.defaultValue("")
+	.description("';' separated list of domains, preference order for resolving package from rapid tags");
 
+static std::vector<std::string> ParseRapidTagResolutionOrder() {
+	const auto orderStr = configHandler->GetString("RapidTagResolutionOrder");
+	std::vector<std::string> order;
+	std::size_t beg, end;
+	for (beg = 0; (end = orderStr.find(';', beg)) != orderStr.npos; beg = end + 1) {
+		order.emplace_back(orderStr.substr(beg, end - beg));
+	}
+	order.emplace_back(orderStr.substr(beg,orderStr.size() - beg));
 
+	static const std::string springRtsRepo = "repos.springrts.com";
+	if (std::find(order.begin(), order.end(), springRtsRepo) == order.end()) {
+		order.emplace_back(springRtsRepo);
+	}
+	return order;
+}
 
 std::string GetRapidPackageFromTag(const std::string& tag)
 {
+	const auto order = ParseRapidTagResolutionOrder();
+	std::string package = tag;
+	std::size_t rank = std::numeric_limits<std::size_t>::max();
 	for (const std::string& file: dataDirsAccess.FindFiles("rapid", "versions.gz", FileQueryFlags::RECURSE)) {
 		RapidEntry re;
-		if (GetRapidEntry(dataDirsAccess.LocateFile(file), &re, [&](const RapidEntry& re) { return re.GetTag() == tag; }))
-			return re.GetName();
+		if (GetRapidEntry(dataDirsAccess.LocateFile(file), &re, [&](const RapidEntry& re) { return re.GetTag() == tag; })) {
+			// The `file` path looks like `rapid/[domain]/[repo]/versions.gz`
+			// e.g: `rapid/repos.springrts.com/byar-chobby/versions.gz` as created
+			// by pr-downloader. We extract the domain part from it.
+			const auto rapidDomain = std::filesystem::path{file}.parent_path().parent_path().filename().string();
+			std::size_t new_rank = std::numeric_limits<std::size_t>::max() - 1;
+			if (auto it = std::find(order.begin(), order.end(), rapidDomain); it != order.end()) {
+				new_rank = it - order.begin();
+			}
+			if (new_rank == rank)
+				LOG_L(L_WARNING, "Rapid tag %s resolves to multiple versions with the same preference, picking from %s",
+				      tag.c_str(), file.c_str());
+			if (new_rank <= rank) {
+				package = re.GetName();
+				rank = new_rank;
+			}
+		}
 	}
-	return tag;
+	return package;
 }
 
 std::string GetRapidTagFromPackage(const std::string& pkg)
