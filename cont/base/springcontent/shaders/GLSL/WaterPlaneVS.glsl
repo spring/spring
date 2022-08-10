db2a811be68eb1d69577b4964799bf03a18d4eda
LHoG <1476261+lhog@users.noreply.github.com>
2022-08-10 12:45:26 +0400

    Expand water plane a little bit further out

--- cont/base/springcontent/shaders/GLSL/WaterPlaneVS.glsl
+++ cont/base/springcontent/shaders/GLSL/WaterPlaneVS.glsl
@@ -6,13 +6,13 @@ out vec3 worldCameraDir;
 
 //To be drawn with GL_TRIANGLES
 const vec2 POS[6] = vec2[6](
-	vec2(-1.0, -1.0),
-	vec2( 2.0, -1.0),
-	vec2(-1.0,  2.0),
+	vec2(-2.0, -2.0),
+	vec2( 3.0, -2.0),
+	vec2(-2.0,  3.0),
 
-	vec2(-1.0,  2.0),
-	vec2( 2.0, -1.0),
-	vec2( 2.0,  2.0)
+	vec2(-2.0,  3.0),
+	vec2( 3.0, -2.0),
+	vec2( 3.0,  3.0)
 );
 
 void main()
