9c28fac87fd14c17cd84cbe336b97b189a2cdf96
abma <spring@abma.de>
2023-01-05 22:36:21 +0100

    extend whitelist

--- buildbot/slave/linux/static-show-used-dynamic-libraries.py
+++ buildbot/slave/linux/static-show-used-dynamic-libraries.py
@@ -14,6 +14,18 @@ env["LC_ALL"] = "C"
 output = subprocess.check_output(["readelf", "-d", executable], universal_newlines=True, env=env)
 
 allowed_libs = (
+	#unitsync + spring"
+	"libdl.so.2",
+	"libm.so.6",
+	"libpthread.so.0",
+	"libc.so.6",
+	"ld-linux-x86-64.so.2",
+	# spring
+	"libSDL2-2.0.so.0",
+	"libGL.so.1",
+	"libXcursor.so.1",
+	"libX11.so.6",
+	"libopenal.so.1",
 )
 libs = []
 match = 'Shared library: '
