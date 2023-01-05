79720b9fe9792baa633386990da0328d0e395263
abma <spring@abma.de>
2023-01-05 22:28:51 +0100

    add (empty) whitelist

--- buildbot/slave/linux/static-show-used-dynamic-libraries.py
+++ buildbot/slave/linux/static-show-used-dynamic-libraries.py
@@ -13,11 +13,20 @@ env["LC_ALL"] = "C"
 
 output = subprocess.check_output(["readelf", "-d", executable], universal_newlines=True, env=env)
 
+allowed_libs = (
+)
 libs = []
 match = 'Shared library: '
 for line in output.split("\n"):
 	if not match in line:
 		continue
 	libs.append(line.split(match)[1].strip("[]"))
-print("\n".join(libs))
+
+exitcode = 0
+for lib in libs:
+	if not lib in allowed_libs:
+		print("linked to not whitelisted lib:", lib)
+		exitcode = 1
+
+sys.exit(exitcode)
 
