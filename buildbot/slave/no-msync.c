bdb55d0ac527ab7b43a0a402e8f46b35061469c6
abma <spring@abma.de>
2023-01-06 12:06:59 +0100

    remove unused

--- buildbot/slave/no-msync.c
+++
@@ -1,8 +0,0 @@
-/* compile using: gcc -shared -m32 no-msync.c -o no-msync.so */
-#include <stdio.h>
-#include <stdlib.h>
-int msync(void *addr, size_t len, int flags) {}
-static void _no_msync_init(void) __attribute__ ((constructor));
-static void _no_msync_init(void) {
-	putenv("LD_PRELOAD=");
-}
