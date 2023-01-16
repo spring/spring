6b30a082603a65e5d4a397898a5269f870bcae41
CommanderSpice <commanderspice@braincalibration.de>
2023-01-16 19:27:30 +0100

    Split CMake modules
    This silences a warning from CMake about result variable names.

--- rts/build/cmake/FindAsciiDoc.cmake
+++ rts/build/cmake/FindAsciiDoc.cmake
@@ -1,26 +1,16 @@
 # This file is part of the Spring engine (GPL v2 or later), see LICENSE.html
 
-# - Find AsciiDoc, XSLTProc and DocBook
-# These toolds are used to compile man pages
+# - Find AsciiDoc
+# These tool is used to compile man pages
 #
 #  ASCIIDOC_BIN   - will be set to the AsciiDoc executable (eg. asciidoc)
-#  XSLTPROC_BIN   - will be set to the XSLTProc executable (eg. xsltproc)
-#  DOCBOOK_XSL    - will be set to the DocBook XSL Style-Sheet (eg. /usr/share/xml/docbook/stylesheet/nwalsh/manpages/docbook.xsl)
 #  ASCIIDOC_FOUND - TRUE if AsciiDoc was found
-#  XSLTPROC_FOUND - TRUE if XSLTProc was found
-#  DOCBOOK_FOUND  - TRUE if DocBook was found
 
 INCLUDE(FindPackageHandleStandardArgs)
 
 # Already in cache, be silent
 if (ASCIIDOC_BIN)
 	SET(AsciiDoc_FIND_QUIETLY TRUE)
-endif ()
-if (XSLTPROC_BIN)
-	SET(XSLTProc_FIND_QUIETLY TRUE)
-endif ()
-if (DOCBOOK_XSL)
-	SET(DocBook_FIND_QUIETLY TRUE)
 endif ()
 
 find_program(ASCIIDOC_BIN
@@ -29,35 +19,8 @@ find_program(ASCIIDOC_BIN
 	DOC "AsciiDoc executable"
 	)
 
-find_program(XSLTPROC_BIN
-	NAMES xsltproc
-	PATH_SUFFIXES bin
-	DOC "XSLTProc executable"
-	)
-
-find_file(DOCBOOK_XSL
-	NAMES docbook.xsl
-	PATHS /usr /usr/share /usr/local /usr/local/share
-	PATH_SUFFIXES
-		xml/docbook/stylesheet/nwalsh/manpages
-		xml/docbook/stylesheet/nwalsh/1.78.1/manpages
-		xml/docbook/stylesheet/nwalsh/1.79.0/manpages
-		sgml/docbook/xsl-stylesheets/manpages
-		xsl/docbook/manpages
-	DOC "DocBook XSL Style-Sheet"
-	)
-
-if (NOT DOCBOOK_XSL)
-	file(GLOB DOCBOOK_XSL / /usr/share/xml/docbook/xsl-stylesheets-*/manpages/docbook.xsl)
-	if (DOCBOOK_XSL)
-		list(GET DOCBOOK_XSL 0 DOCBOOK_XSL)
-	endif ()
-endif ()
-
 # handle the QUIETLY and REQUIRED arguments and set ASCIIDOC_FOUND to TRUE if
 # all listed variables are TRUE
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(AsciiDoc DEFAULT_MSG ASCIIDOC_BIN)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(XSLTProc DEFAULT_MSG XSLTPROC_BIN)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(DocBook  DEFAULT_MSG DOCBOOK_XSL)
 
-MARK_AS_ADVANCED(ASCIIDOC_BIN XSLTPROC_BIN DOCBOOK_XSL)
+MARK_AS_ADVANCED(ASCIIDOC_BIN)
